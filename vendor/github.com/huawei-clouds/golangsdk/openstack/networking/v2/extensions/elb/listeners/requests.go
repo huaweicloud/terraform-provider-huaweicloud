02e8c03eb2fae4842bce760776ffbcc0bbd7b991
zengchen1024 <chenzeng2@huawei.com>
2018-01-18 18:55:34 +0800

    add elb listener acc test and make update by the api doc of huawei

--- vendor/github.com/huawei-clouds/golangsdk/openstack/networking/v2/extensions/elb/listeners/requests.go
+++ vendor/github.com/huawei-clouds/golangsdk/openstack/networking/v2/extensions/elb/listeners/requests.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/huawei-clouds/golangsdk"
+	"github.com/huawei-clouds/golangsdk/openstack/utils"
 )
 
 // CreateOptsBuilder is the interface options structs have to satisfy in order
@@ -17,24 +18,25 @@ type CreateOptsBuilder interface {
 // CreateOpts is the common options struct used in this package's Create
 // operation.
 type CreateOpts struct {
-	Name               string `json:"name" required:"true"`
-	Description        string `json:"description,omitempty"`
-	LoadbalancerID     string `json:"loadbalancer_id" required:"true"`
-	Protocol           string `json:"protocol" required:"true"`
-	Port               int    `json:"port" required:"true"`
-	BackendProtocol    string `json:"backend_protocol" required:"true"`
-	BackendPort        int    `json:"backend_port" required:"true"`
-	LbAlgorithm        string `json:"lb_algorithm" required:"true"`
-	SessionSticky      bool   `json:"session_sticky,omitempty"`
-	StickySessionType  string `json:"sticky_session_type,omitempty"`
-	CookieTimeout      int    `json:"cookie_timeout,omitempty"`
-	TcpTimeout         int    `json:"tcp_timeout,omitempty"`
-	TcpDraining        bool   `json:"tcp_draining,omitempty"`
-	TcpDrainingTimeout int    `json:"tcp_draining_timeout,omitempty"`
-	CertificateID      string `json:"certificate_id,omitempty"`
-	UdpTimeout         int    `json:"udp_timeout,omitempty"`
-	SslProtocols       string `json:"ssl_protocols,omitempty"`
-	SslCiphers         string `json:"ssl_ciphers,omitempty"`
+	Name               string   `json:"name" required:"true"`
+	Description        string   `json:"description,omitempty"`
+	LoadbalancerID     string   `json:"loadbalancer_id" required:"true"`
+	Protocol           string   `json:"protocol" required:"true"`
+	Port               int      `json:"port" required:"true"`
+	BackendProtocol    string   `json:"backend_protocol" required:"true"`
+	BackendPort        int      `json:"backend_port" required:"true"`
+	LbAlgorithm        string   `json:"lb_algorithm" required:"true"`
+	SessionSticky      bool     `json:"session_sticky" no_default:"y"`
+	StickySessionType  string   `json:"sticky_session_type,omitempty"`
+	CookieTimeout      int      `json:"cookie_timeout,omitempty"`
+	TcpTimeout         int      `json:"tcp_timeout,omitempty"`
+	TcpDraining        bool     `json:"tcp_draining" no_default:"y"`
+	TcpDrainingTimeout int      `json:"tcp_draining_timeout" no_default:"y"`
+	CertificateID      string   `json:"certificate_id,omitempty"`
+	Certificates       []string `json:"certificates,omitempty"`
+	UdpTimeout         int      `json:"udp_timeout,omitempty"`
+	SslProtocols       string   `json:"ssl_protocols,omitempty"`
+	SslCiphers         string   `json:"ssl_ciphers,omitempty"`
 }
 
 // ToListenerCreateMap casts a CreateOpts struct to a map.
@@ -49,12 +51,14 @@ func (opts CreateOpts) ToListenerCreateMap() (map[string]interface{}, error) {
 //
 // Users with an admin role can create loadbalancers on behalf of other tenants by
 // specifying a TenantID attribute different than their own.
-func Create(c *golangsdk.ServiceClientExtension, opts CreateOptsBuilder) (r CreateResult) {
+func Create(c *golangsdk.ServiceClientExtension, opts CreateOptsBuilder, not_pass_param []string) (r CreateResult) {
 	b, err := opts.ToListenerCreateMap()
 	if err != nil {
 		r.Err = err
 		return
 	}
+	log.Printf("[DEBUG] create url befor :%q, body=%#v, not_pass_param=%#v", rootURL(c), b, not_pass_param)
+	utils.DeleteNotPassParams(&b, not_pass_param)
 	log.Printf("[DEBUG] create url:%q, body=%#v", rootURL(c), b)
 	reqOpt := &golangsdk.RequestOpts{OkCodes: []int{200}}
 	_, r.Err = c.Post(rootURL(c), b, &r.Body, reqOpt)
@@ -78,26 +82,19 @@ type UpdateOptsBuilder interface {
 // UpdateOpts is the common options struct used in this package's Update
 // operation.
 type UpdateOpts struct {
-	Name               string `json:"name,omitempty"`
-	Description        string `json:"description,omitempty"`
-	Port               int    `json:"port,omitempty"`
-	BackendPort        int    `json:"backend_port,omitempty"`
-	LbAlgorithm        string `json:"lb_algorithm,omitempty"`
-	TcpTimeout         int    `json:"tcp_timeout,omitempty"`
-	TcpDraining        bool   `json:"tcp_draining,omitempty"`
-	TcpDrainingTimeout int    `json:"tcp_draining_timeout,omitempty"`
-	UdpTimeout         int    `json:"udp_timeout,omitempty"`
-	SslProtocols       string `json:"ssl_protocols,omitempty"`
-	SslCiphers         string `json:"ssl_ciphers,omitempty"`
-	CertificateID      string `json:"certificate_id,omitempty"`
-}
-
-func (u UpdateOpts) IsNeedUpdate() (bool, error) {
-	d, e := u.ToListenerUpdateMap()
-	if e == nil {
-		return len(d) != 0, nil
-	}
-	return false, e
+	Name               string   `json:"name,omitempty"`
+	Description        string   `json:"description"`
+	Port               int      `json:"port,omitempty"`
+	BackendPort        int      `json:"backend_port,omitempty"`
+	LbAlgorithm        string   `json:"lb_algorithm,omitempty"`
+	TcpTimeout         int      `json:"tcp_timeout,omitempty"`
+	TcpDraining        bool     `json:"tcp_draining"`
+	TcpDrainingTimeout int      `json:"tcp_draining_timeout"`
+	UdpTimeout         int      `json:"udp_timeout,omitempty"`
+	SslProtocols       string   `json:"ssl_protocols,omitempty"`
+	SslCiphers         string   `json:"ssl_ciphers,omitempty"`
+	CertificateID      string   `json:"certificate_id,omitempty"`
+	Certificates       []string `json:"certificates,omitempty"`
 }
 
 // ToListenerUpdateMap casts a UpdateOpts struct to a map.
@@ -106,12 +103,13 @@ func (opts UpdateOpts) ToListenerUpdateMap() (map[string]interface{}, error) {
 }
 
 // Update is an operation which modifies the attributes of the specified Listener.
-func Update(c *golangsdk.ServiceClientExtension, id string, opts UpdateOpts) (r UpdateResult) {
+func Update(c *golangsdk.ServiceClientExtension, id string, opts UpdateOpts, not_pass_param []string) (r UpdateResult) {
 	b, err := opts.ToListenerUpdateMap()
 	if err != nil {
 		r.Err = err
 		return
 	}
+	utils.DeleteNotPassParams(&b, not_pass_param)
 	_, r.Err = c.Put(resourceURL(c, id), b, &r.Body, &golangsdk.RequestOpts{
 		OkCodes: []int{200},
 	})
