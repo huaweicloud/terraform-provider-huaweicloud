2fbf0081a5f55d3cfeb9adc5b317cec814acc24a
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2018-01-19 10:12:44 +0800

    Add state check for floating_v2 delete
    This adds a state refresh check to wait the floating ip deleted.

--- huaweicloud/resource_huaweicloud_compute_floatingip_v2.go
+++ huaweicloud/resource_huaweicloud_compute_floatingip_v2.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/floatingips"
+	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
+	"time"
 )
 
 func resourceComputeFloatingIPV2() *schema.Resource {
@@ -95,6 +98,24 @@ func resourceComputeFloatingIPV2Read(d *schema.ResourceData, meta interface{}) e
 	return nil
 }
 
+func FloatingIPV2StateRefreshFunc(computeClient *gophercloud.ServiceClient, d *schema.ResourceData) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		s, err := floatingips.Get(computeClient, d.Id()).Extract()
+		if err != nil {
+			err = CheckDeleted(d, err, "Floating IP")
+			if err != nil {
+				return s, "", err
+			} else {
+				log.Printf("[DEBUG] Successfully deleted Floating IP %s", d.Id())
+				return s, "DELETED", nil
+			}
+		}
+
+		log.Printf("[DEBUG] Floating IP %s still active.\n", d.Id())
+		return s, "ACTIVE", nil
+	}
+}
+
 func resourceComputeFloatingIPV2Delete(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*Config)
 	computeClient, err := config.computeV2Client(GetRegion(d, config))
@@ -102,9 +123,24 @@ func resourceComputeFloatingIPV2Delete(d *schema.ResourceData, meta interface{})
 		return fmt.Errorf("Error creating HuaweiCloud compute client: %s", err)
 	}
 
-	log.Printf("[DEBUG] Deleting Floating IP %s", d.Id())
+	log.Printf("[DEBUG] Attempting to delete Floating IP %s.\n", d.Id())
+
 	if err := floatingips.Delete(computeClient, d.Id()).ExtractErr(); err != nil {
 		return fmt.Errorf("Error deleting Floating IP: %s", err)
+	}
+
+	stateConf := &resource.StateChangeConf{
+		Pending:    []string{"ACTIVE"},
+		Target:     []string{"DELETED"},
+		Refresh:    FloatingIPV2StateRefreshFunc(computeClient, d),
+		Timeout:    d.Timeout(schema.TimeoutDelete),
+		Delay:      10 * time.Second,
+		MinTimeout: 3 * time.Second,
+	}
+
+	_, err = stateConf.WaitForState()
+	if err != nil {
+		return fmt.Errorf("Error deleting HuaweiCloud Floating IP: %s", err)
 	}
 
 	return nil
