6e93cd26941bb427be5d4faaca41aeb28cf97a89
zhongjun2 <jun.zhongjun@huawei.com>
2018-01-25 16:59:30 +0800

    Update data_source_huaweicloud_kms_key_v1_test.go

--- huaweicloud/data_source_huaweicloud_kms_key_v1_test.go
+++ huaweicloud/data_source_huaweicloud_kms_key_v1_test.go
@@ -13,7 +13,7 @@ var keyAlias = fmt.Sprintf("key_alias_%s", acctest.RandString(5))
 
 func TestAccKmsKeyV1DataSource_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheckDNS(t) },
+		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
