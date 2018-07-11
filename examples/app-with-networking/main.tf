1a7e2fead567d48944e1250237e89fac30e9945e
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2018-07-11 17:14:24 +0800

    Replace openstack with huaweicloud on examples

--- examples/app-with-networking/main.tf
+++ examples/app-with-networking/main.tf
@@ -1,33 +1,33 @@
-resource "openstack_compute_keypair_v2" "terraform" {
+resource "huaweicloud_compute_keypair_v2" "terraform" {
   name       = "terraform"
   public_key = "${file("${var.ssh_key_file}.pub")}"
 }
 
-resource "openstack_networking_network_v2" "terraform" {
+resource "huaweicloud_networking_network_v2" "terraform" {
   name           = "terraform"
   admin_state_up = "true"
 }
 
-resource "openstack_networking_subnet_v2" "terraform" {
+resource "huaweicloud_networking_subnet_v2" "terraform" {
   name            = "terraform"
-  network_id      = "${openstack_networking_network_v2.terraform.id}"
+  network_id      = "${huaweicloud_networking_network_v2.terraform.id}"
   cidr            = "10.0.0.0/24"
   ip_version      = 4
   dns_nameservers = ["8.8.8.8", "8.8.4.4"]
 }
 
-resource "openstack_networking_router_v2" "terraform" {
+resource "huaweicloud_networking_router_v2" "terraform" {
   name             = "terraform"
   admin_state_up   = "true"
   external_network_id = "${var.external_gateway}"
 }
 
-resource "openstack_networking_router_interface_v2" "terraform" {
-  router_id = "${openstack_networking_router_v2.terraform.id}"
-  subnet_id = "${openstack_networking_subnet_v2.terraform.id}"
+resource "huaweicloud_networking_router_interface_v2" "terraform" {
+  router_id = "${huaweicloud_networking_router_v2.terraform.id}"
+  subnet_id = "${huaweicloud_networking_subnet_v2.terraform.id}"
 }
 
-resource "openstack_compute_secgroup_v2" "terraform" {
+resource "huaweicloud_compute_secgroup_v2" "terraform" {
   name        = "terraform"
   description = "Security group for the Terraform example instances"
 
@@ -53,31 +53,31 @@ resource "openstack_compute_secgroup_v2" "terraform" {
   }
 }
 
-resource "openstack_compute_floatingip_v2" "terraform" {
+resource "huaweicloud_compute_floatingip_v2" "terraform" {
   pool       = "${var.pool}"
-  depends_on = ["openstack_networking_router_interface_v2.terraform"]
+  depends_on = ["huaweicloud_networking_router_interface_v2.terraform"]
 }
 
-resource "openstack_compute_instance_v2" "terraform" {
+resource "huaweicloud_compute_instance_v2" "terraform" {
   name            = "terraform"
   image_name      = "${var.image}"
   flavor_name     = "${var.flavor}"
-  key_pair        = "${openstack_compute_keypair_v2.terraform.name}"
-  security_groups = ["${openstack_compute_secgroup_v2.terraform.name}"]
+  key_pair        = "${huaweicloud_compute_keypair_v2.terraform.name}"
+  security_groups = ["${huaweicloud_compute_secgroup_v2.terraform.name}"]
 
   network {
-    uuid = "${openstack_networking_network_v2.terraform.id}"
+    uuid = "${huaweicloud_networking_network_v2.terraform.id}"
   }
 
 }
 
-resource "openstack_compute_floatingip_associate_v2" "terraform" {
-  floating_ip = "${openstack_compute_floatingip_v2.terraform.address}"
-  instance_id = "${openstack_compute_instance_v2.terraform.id}"
+resource "huaweicloud_compute_floatingip_associate_v2" "terraform" {
+  floating_ip = "${huaweicloud_compute_floatingip_v2.terraform.address}"
+  instance_id = "${huaweicloud_compute_instance_v2.terraform.id}"
 
   provisioner "remote-exec" {
     connection {
-      host = "${openstack_compute_floatingip_v2.terraform.address}"
+      host = "${huaweicloud_compute_floatingip_v2.terraform.address}"
       user     = "${var.ssh_user_name}"
       private_key = "${file(var.ssh_key_file)}"
     }
