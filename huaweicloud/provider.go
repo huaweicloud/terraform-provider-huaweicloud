c1c1b65cf394668ca98525c91bc6d1bea2b4a527
disha-wani <disha.wani@WSD008>
2018-07-25 12:21:15 +0530

    gofmt provider.go file

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -207,8 +207,8 @@ func Provider() terraform.ResourceProvider {
 			"huaweicloud_vpc_route_ids_v2":          dataSourceVPCRouteIdsV2(),
 			"huaweicloud_vpc_subnet_v1":             dataSourceVpcSubnetV1(),
 			"huaweicloud_vpc_subnet_ids_v1":         dataSourceVpcSubnetIdsV1(),
-			"huaweicloud_cce_cluster_v3":         dataSourceCCEClusterV3(),
-			"huaweicloud_cce_node_v3":            dataSourceCceNodesV3(),
+			"huaweicloud_cce_cluster_v3":            dataSourceCCEClusterV3(),
+			"huaweicloud_cce_node_v3":               dataSourceCceNodesV3(),
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
@@ -261,8 +261,8 @@ func Provider() terraform.ResourceProvider {
 			"huaweicloud_vpc_peering_connection_accepter_v2": resourceVpcPeeringConnectionAccepterV2(),
 			"huaweicloud_vpc_route_v2":                       resourceVPCRouteV2(),
 			"huaweicloud_vpc_subnet_v1":                      resourceVpcSubnetV1(),
-			"huaweicloud_cce_cluster_v3":                  resourceCCEClusterV3(),
-			"huaweicloud_cce_node_v3":                     resourceCCENodeV3(),
+			"huaweicloud_cce_cluster_v3":                     resourceCCEClusterV3(),
+			"huaweicloud_cce_node_v3":                        resourceCCENodeV3(),
 		},
 
 		ConfigureFunc: configureProvider,
