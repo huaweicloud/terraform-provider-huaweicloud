60c9e032b03b5d35032d7e4e582af3ed029e98b7
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2019-02-02 11:38:54 +0800

    Add DHCP opts to the port resource

--- huaweicloud/import_huaweicloud_networking_port_v2_test.go
+++ huaweicloud/import_huaweicloud_networking_port_v2_test.go
@@ -29,3 +29,27 @@ func TestAccNetworkingV2Port_importBasic(t *testing.T) {
 		},
 	})
 }
+
+func TestAccNetworkingV2Port_importDHCPOpts(t *testing.T) {
+	resourceName := "huaweicloud_networking_port_v2.port_1"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNetworkingV2PortDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetworkingV2Port_createExtraDHCPOpts,
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"fixed_ip",
+				},
+			},
+		},
+	})
+}
