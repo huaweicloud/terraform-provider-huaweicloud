a1290d87fa9fce0ee5b542599ad90fb3c571bf12
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2019-06-11 09:46:26 +0000

    Update CCE client

--- vendor/github.com/huaweicloud/golangsdk/openstack/client.go
+++ vendor/github.com/huaweicloud/golangsdk/openstack/client.go
@@ -789,12 +789,9 @@ func NewAntiDDoSV2(client *golangsdk.ProviderClient, eo golangsdk.EndpointOpts) 
 }
 
 func NewCCEV3(client *golangsdk.ProviderClient, eo golangsdk.EndpointOpts) (*golangsdk.ServiceClient, error) {
-	sc, err := initClientOpts(client, eo, "compute")
-	sc.Endpoint = strings.Replace(sc.Endpoint, "ecs", "cce", 1)
-	sc.Endpoint = strings.Replace(sc.Endpoint, "v2", "api/v3/projects", 1)
-	sc.Endpoint = strings.Replace(sc.Endpoint, "myhwclouds", "myhuaweicloud", 1)
-	sc.ResourceBase = sc.Endpoint
-	sc.Type = "cce"
+	sc, err := initClientOpts(client, eo, "network")
+	sc.Endpoint = strings.Replace(sc.Endpoint, "vpc", "cce", 1)
+	sc.ResourceBase = sc.Endpoint + "api/v3/projects/" + client.ProjectID + "/"
 	return sc, err
 }
 
