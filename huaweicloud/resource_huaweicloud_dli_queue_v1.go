7107cb1eae0c8106e89653774f993609e1b0549e
zengchen1024 <chenzeng2@huawei.com>
2019-07-22 10:01:47 +0800

    new version

--- huaweicloud/resource_huaweicloud_dli_queue_v1.go
+++ huaweicloud/resource_huaweicloud_dli_queue_v1.go
@@ -42,24 +42,6 @@ func resourceDliQueueV1() *schema.Resource {
 				ForceNew: true,
 			},
 
-			"cidr_in_mgntsubnet": {
-				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: true,
-			},
-
-			"cidr_in_subnet": {
-				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: true,
-			},
-
-			"cidr_in_vpc": {
-				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: true,
-			},
-
 			"description": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -67,8 +49,26 @@ func resourceDliQueueV1() *schema.Resource {
 				ForceNew: true,
 			},
 
+			"management_subnet_cidr": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+
+			"subnet_cidr": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+
+			"vpc_cidr": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+
 			"create_time": {
-				Type:     schema.TypeInt,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 		},
@@ -78,12 +78,12 @@ func resourceDliQueueV1() *schema.Resource {
 func resourceDliQueueV1UserInputParams(d *schema.ResourceData) map[string]interface{} {
 	return map[string]interface{}{
 		"terraform_resource_data": d,
-		"cidr_in_mgntsubnet":      d.Get("cidr_in_mgntsubnet"),
-		"cidr_in_subnet":          d.Get("cidr_in_subnet"),
-		"cidr_in_vpc":             d.Get("cidr_in_vpc"),
 		"cu_count":                d.Get("cu_count"),
 		"description":             d.Get("description"),
+		"management_subnet_cidr":  d.Get("management_subnet_cidr"),
 		"name":                    d.Get("name"),
+		"subnet_cidr":             d.Get("subnet_cidr"),
+		"vpc_cidr":                d.Get("vpc_cidr"),
 	}
 }
 
@@ -109,7 +109,7 @@ func resourceDliQueueV1Create(d *schema.ResourceData, meta interface{}) error {
 	if err != nil {
 		return fmt.Errorf("Error constructing id, err=%s", err)
 	}
-	d.SetId(id.(string))
+	d.SetId(convertToStr(id))
 
 	return resourceDliQueueV1Read(d, meta)
 }
@@ -123,11 +123,11 @@ func resourceDliQueueV1Read(d *schema.ResourceData, meta interface{}) error {
 
 	res := make(map[string]interface{})
 
-	v, err := sendDliQueueV1ReadRequest(d, client)
+	v, err := fetchDliQueueV1ByList(d, client)
 	if err != nil {
 		return err
 	}
-	res["read"] = fillDliQueueV1ReadRespBody(v)
+	res["list"] = fillDliQueueV1ListRespBody(v)
 
 	states, err := flattenDliQueueV1Options(res)
 	if err != nil {
@@ -170,7 +170,7 @@ func buildDliQueueV1CreateParameters(opts map[string]interface{}, arrayIndex map
 
 	params["charging_mode"] = 1
 
-	v, err := navigateValue(opts, []string{"cidr_in_mgntsubnet"}, arrayIndex)
+	v, err := navigateValue(opts, []string{"management_subnet_cidr"}, arrayIndex)
 	if err != nil {
 		return nil, err
 	}
@@ -180,7 +180,7 @@ func buildDliQueueV1CreateParameters(opts map[string]interface{}, arrayIndex map
 		params["cidr_in_mgntsubnet"] = v
 	}
 
-	v, err = navigateValue(opts, []string{"cidr_in_subnet"}, arrayIndex)
+	v, err = navigateValue(opts, []string{"subnet_cidr"}, arrayIndex)
 	if err != nil {
 		return nil, err
 	}
@@ -190,7 +190,7 @@ func buildDliQueueV1CreateParameters(opts map[string]interface{}, arrayIndex map
 		params["cidr_in_subnet"] = v
 	}
 
-	v, err = navigateValue(opts, []string{"cidr_in_vpc"}, arrayIndex)
+	v, err = navigateValue(opts, []string{"vpc_cidr"}, arrayIndex)
 	if err != nil {
 		return nil, err
 	}
@@ -247,24 +247,43 @@ func sendDliQueueV1CreateRequest(d *schema.ResourceData, params interface{},
 	return r.Body, nil
 }
 
-func sendDliQueueV1ReadRequest(d *schema.ResourceData, client *golangsdk.ServiceClient) (interface{}, error) {
-	url, err := replaceVars(d, "queues/{id}", nil)
+func fetchDliQueueV1ByList(d *schema.ResourceData, client *golangsdk.ServiceClient) (interface{}, error) {
+	link := client.ServiceURL("queues")
+
+	return findDliQueueV1ByList(client, link, d.Id())
+}
+
+func findDliQueueV1ByList(client *golangsdk.ServiceClient, link, resourceID string) (interface{}, error) {
+	r, err := sendDliQueueV1ListRequest(client, link)
 	if err != nil {
 		return nil, err
 	}
-	url = client.ServiceURL(url)
+	for _, item := range r.([]interface{}) {
+		val, ok := item.(map[string]interface{})["queue_name"]
+		if ok && resourceID == convertToStr(val) {
+			return item, nil
+		}
+	}
 
+	return nil, fmt.Errorf("Error finding the resource by list api")
+}
+
+func sendDliQueueV1ListRequest(client *golangsdk.ServiceClient, url string) (interface{}, error) {
 	r := golangsdk.Result{}
 	_, r.Err = client.Get(url, &r.Body, &golangsdk.RequestOpts{
 		MoreHeaders: map[string]string{"Content-Type": "application/json"}})
 	if r.Err != nil {
-		return nil, fmt.Errorf("Error running api(read) for resource(DliQueueV1), err=%s", r.Err)
+		return nil, fmt.Errorf("Error running api(list) for resource(DliQueueV1), err=%s", r.Err)
 	}
 
-	return r.Body, nil
+	v, err := navigateValue(r.Body, []string{"queues"}, nil)
+	if err != nil {
+		return nil, err
+	}
+	return v, nil
 }
 
-func fillDliQueueV1ReadRespBody(body interface{}) interface{} {
+func fillDliQueueV1ListRespBody(body interface{}) interface{} {
 	result := make(map[string]interface{})
 	val, ok := body.(map[string]interface{})
 	if !ok {
@@ -301,31 +320,43 @@ func fillDliQueueV1ReadRespBody(body interface{}) interface{} {
 func flattenDliQueueV1Options(response map[string]interface{}) (map[string]interface{}, error) {
 	opts := make(map[string]interface{})
 
-	v, err := navigateValue(response, []string{"read", "create_time"}, nil)
+	v, err := flattenDliQueueV1CreateTime(response, nil)
 	if err != nil {
 		return nil, fmt.Errorf("Error flattening Queue:create_time, err: %s", err)
 	}
 	opts["create_time"] = v
 
-	v, err = navigateValue(response, []string{"read", "cu_count"}, nil)
+	v, err = navigateValue(response, []string{"list", "cu_count"}, nil)
 	if err != nil {
 		return nil, fmt.Errorf("Error flattening Queue:cu_count, err: %s", err)
 	}
 	opts["cu_count"] = v
 
-	v, err = navigateValue(response, []string{"read", "description"}, nil)
+	v, err = navigateValue(response, []string{"list", "description"}, nil)
 	if err != nil {
 		return nil, fmt.Errorf("Error flattening Queue:description, err: %s", err)
 	}
 	opts["description"] = v
 
-	v, err = navigateValue(response, []string{"read", "queue_name"}, nil)
+	v, err = navigateValue(response, []string{"list", "queue_name"}, nil)
 	if err != nil {
 		return nil, fmt.Errorf("Error flattening Queue:name, err: %s", err)
 	}
 	opts["name"] = v
 
 	return opts, nil
+}
+
+func flattenDliQueueV1CreateTime(d interface{}, arrayIndex map[string]int) (interface{}, error) {
+	v, err := navigateValue(d, []string{"list", "create_time"}, arrayIndex)
+	if err != nil {
+		return nil, err
+	}
+	val, err := convertToInt(v)
+	if err != nil {
+		return nil, err
+	}
+	return convertSeconds2Str(val / 1000), nil
 }
 
 func setDliQueueV1States(d *schema.ResourceData, opts map[string]interface{}) error {
