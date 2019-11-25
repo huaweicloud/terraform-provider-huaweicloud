b4fde14ecec61b421dff334f2b5b63a8db508eab
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2019-11-25 17:40:48 +0800

    Add VPC ids data source (#233)

---
+++ huaweicloud/data_source_huaweicloud_vpc_ids_v1.go
@@ -0,0 +1,50 @@
+package huaweicloud
+
+import (
+	"fmt"
+
+	"github.com/huaweicloud/golangsdk/openstack/networking/v1/vpcs"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceVirtualPrivateCloudVpcIdsV1() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceVirtualPrivateCloudIdsV1Read,
+
+		Schema: map[string]*schema.Schema{
+			"ids": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Set:      schema.HashString,
+			},
+		},
+	}
+}
+
+func dataSourceVirtualPrivateCloudIdsV1Read(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	vpcClient, err := config.networkingV1Client(GetRegion(d, config))
+
+	listOpts := vpcs.ListOpts{}
+	refinedVpcs, err := vpcs.List(vpcClient, listOpts)
+	if err != nil {
+		return fmt.Errorf("Unable to retrieve vpcs: %s", err)
+	}
+
+	if len(refinedVpcs) < 1 {
+		return fmt.Errorf("Your query returned no results. " +
+			"Please change your search criteria and try again.")
+	}
+
+	listVpcs := make([]string, 0)
+
+	for _, vpc := range refinedVpcs {
+		listVpcs = append(listVpcs, vpc.ID)
+	}
+	d.SetId(listVpcs[0])
+	d.Set("ids", listVpcs)
+
+	return nil
+}
