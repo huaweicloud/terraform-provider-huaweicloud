1ca9cb0d26a90734bd24681abe84c08beee56e90
ShiChangkuo <shichangkuo90@gmail.com>
2019-12-04 21:24:17 +0800

    Add huaweicloud_rds_flavors_v3 data source and docs
    Signed-off-by: ShiChangkuo <shichangkuo90@gmail.com>

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -200,6 +200,7 @@ func Provider() terraform.ResourceProvider {
 			"huaweicloud_kms_key_v1":                    dataSourceKmsKeyV1(),
 			"huaweicloud_kms_data_key_v1":               dataSourceKmsDataKeyV1(),
 			"huaweicloud_rds_flavors_v1":                dataSourceRdsFlavorV1(),
+			"huaweicloud_rds_flavors_v3":                dataSourceRdsFlavorV3(),
 			"huaweicloud_sfs_file_system_v2":            dataSourceSFSFileSystemV2(),
 			"huaweicloud_rts_stack_v1":                  dataSourceRTSStackV1(),
 			"huaweicloud_rts_stack_resource_v1":         dataSourceRTSStackResourcesV1(),
