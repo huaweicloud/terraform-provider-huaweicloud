8f4896e071b4909df0bcc2e6dcf9b6d555f13303
smilemusic <yang5mile@163.com>
2020-07-03 17:46:54 +0800

    add gaussdb instance data resource (#354)
    Co-authored-by: yangshuai <yangshuai61@huawei.com>

---
+++ huaweicloud/data_source_huaweicloud_gaussdb_mysql_flavors_test.go
@@ -0,0 +1,46 @@
+package huaweicloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccHuaweiCloudGaussdbMysqlFlavorsDataSource_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccHuaweiCloudGaussdbMysqlFlavorsDataSource_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGaussdbMysqlFlavorsDataSourceID("data.huaweicloud_gaussdb_mysql_flavors.flavor"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckGaussdbMysqlFlavorsDataSourceID(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Can't find GaussDB mysql data source: %s ", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("GaussDB mysql data source ID not set ")
+		}
+
+		return nil
+	}
+}
+
+var testAccHuaweiCloudGaussdbMysqlFlavorsDataSource_basic = `
+data "huaweicloud_gaussdb_mysql_flavors" "flavor" {
+  engine = "gaussdb-mysql"
+  version = "8.0"
+}
+`
