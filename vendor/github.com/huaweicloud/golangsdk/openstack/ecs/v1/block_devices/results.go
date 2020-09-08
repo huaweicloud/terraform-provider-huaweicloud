fe0b86b14493aff6a247f7714312ff7f42676bdb
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2020-09-08 08:37:01 +0800

    Set boot_index to -1 when it's null for attached volumes (#514)

--- vendor/github.com/huaweicloud/golangsdk/openstack/ecs/v1/block_devices/results.go
+++ vendor/github.com/huaweicloud/golangsdk/openstack/ecs/v1/block_devices/results.go
@@ -1,11 +1,37 @@
 package block_devices
 
-import "github.com/huaweicloud/golangsdk"
+import (
+	"encoding/json"
+
+	"github.com/huaweicloud/golangsdk"
+)
 
 type VolumeAttachment struct {
 	PciAddress string `json:"pciAddress"`
 	Size       int    `json:"size"`
-	BootIndex  int    `json:"bootIndex"`
+	BootIndex  int    `json:"-"`
+}
+
+func (r *VolumeAttachment) UnmarshalJSON(b []byte) error {
+	type tmp VolumeAttachment
+	var s struct {
+		tmp
+		BootIndex interface{} `json:"bootIndex"`
+	}
+	err := json.Unmarshal(b, &s)
+	if err != nil {
+		return err
+	}
+
+	*r = VolumeAttachment(s.tmp)
+
+	if s.BootIndex == nil {
+		r.BootIndex = -1
+	} else {
+		r.BootIndex = int(s.BootIndex.(float64))
+	}
+
+	return err
 }
 
 type GetResult struct {
