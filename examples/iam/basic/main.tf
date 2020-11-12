fec93d2e6b6c0db8daec6449f392ab8879951a44
Lance52259 <Overlord52259@gamil.com>
2020-11-12 11:31:57 +0800

    update user password by using random().

--- examples/iam/basic/main.tf
+++ examples/iam/basic/main.tf
@@ -1,14 +1,19 @@
-#Create a User
-resource "huaweicloud_identity_user_v3" "iden_user_default" {
-  name        = var.iden_user_name_default
-  password    = var.iden_user_password_default
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
 }
 
-#Create two Users
+resource "huaweicloud_identity_user_v3" "iden_user_default" {
+  name        = var.iden_user_name_default
+  password    = random_password.password.result
+}
+
 resource "huaweicloud_identity_user_v3" "users" {
   count       = length(var.iden_users)
 
   name        = lookup(var.iden_users[count.index], "name", null)
   description = lookup(var.iden_users[count.index], "description", null)
-  password    = lookup(var.iden_users[count.index], "password", null)
+  password    = random_password.password.result
 }
--- examples/iam/basic/variables.tf
+++ examples/iam/basic/variables.tf
@@ -3,11 +3,6 @@ variable "iden_user_name_default" {
   default = "iden_user_default"
 }
 
-variable "iden_user_password_default" {
-  type    = string
-  default = "hw123456default"
-}
-
 variable "iden_users" {
     description = "List of identity users."
     type = list(object({
@@ -15,7 +10,7 @@ variable "iden_users" {
         description = string
     }))
     default = [
-        {name = "iden_user_A", description = "This is a identity user.", password = "hw123456"},
-        {name = "iden_user_B", description = "This is a identity user.", password = "hw654321"}
+        {name = "iden_user_A", description = "This is a identity user."},
+        {name = "iden_user_B", description = "This is a identity user."}
     ]
 }
