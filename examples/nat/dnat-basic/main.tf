7ddf7269031596af6f0f02b3d7b04423df655ebf
Lance52259 <Overlord52259@gamil.com>
2020-11-24 10:47:30 +0800

    Add new dnat-basic example

---
+++ examples/nat/dnat-basic/main.tf
@@ -0,0 +1,124 @@
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!@$%^-_=+[{}]:,./?~#*"
+  min_numeric      = 1
+  min_lower        = 1
+  min_special      = 1
+}
+
+locals {
+  password = random_password.password.result
+}
+
+resource "huaweicloud_compute_instance" "newCompute_Example" {
+  name              = var.ecs_name
+  image_id          = data.huaweicloud_images_image.newIMS_Example.id
+  flavor_id         = "s6.small.1"
+  security_groups   = [huaweicloud_networking_secgroup.newSecgroup_Example.name]
+  admin_pass        = local.password
+  availability_zone = data.huaweicloud_availability_zones.newAZ_Example.names[0]
+
+  system_disk_type  = "SSD"
+  system_disk_size  = 40
+
+  network {
+    fixed_ip_v4 = var.ecs_ipaddress
+    uuid        = huaweicloud_vpc_subnet.newSubnet_Example.id
+  }
+}
+
+data "huaweicloud_availability_zones" "newAZ_Example" {}
+
+data "huaweicloud_images_image" "newIMS_Example" {
+  name        = var.ims_name
+  visibility  = "public" # "private"、"share"、"community"
+  most_recent = true
+}
+
+resource "huaweicloud_vpc_eip" "newEIP_Example" {
+  publicip {
+    type = "5_bgp"
+  }
+  bandwidth {
+    name        = var.bandwidth_name
+    size        = 5
+    share_type  = "PER"
+    charge_mode = "traffic"
+  }
+}
+
+resource "huaweicloud_vpc" "newVPC_Example" {
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+}
+
+resource "huaweicloud_vpc_subnet" "newSubnet_Example" {
+  name          = var.subnet_name
+  cidr          = var.subnet_cidr
+  gateway_ip    = var.subnet_gateway_ip
+  vpc_id        = huaweicloud_vpc.newVPC_Example.id
+  primary_dns   = "100.125.129.250"
+  secondary_dns = "100.125.1.250"
+}
+
+resource "huaweicloud_networking_secgroup" "newSecgroup_Example" {
+  name        = var.secgroup_name
+  description = "This is a security group"
+}
+
+resource "huaweicloud_networking_secgroup_rule" "newSecgroup_GressRule_Example" {
+  count             = length(var.example_security_group)
+
+  direction         = lookup(var.example_security_group[count.index], "direction", null)
+  ethertype         = lookup(var.example_security_group[count.index], "ethertype", null)
+  protocol          = lookup(var.example_security_group[count.index], "protocol", null)
+  port_range_min    = lookup(var.example_security_group[count.index], "port_range_min", null)
+  port_range_max    = lookup(var.example_security_group[count.index], "port_range_max", null)
+  remote_ip_prefix  = lookup(var.example_security_group[count.index], "remote_ip_prefix", null)
+  security_group_id = huaweicloud_networking_secgroup.newSecgroup_Example.id
+}
+
+resource "huaweicloud_nat_gateway" "newNet_gateway_Example" {
+  name                = var.net_gateway_name
+  description         = "example for net test"
+  spec                = "1"
+  router_id           = huaweicloud_vpc.newVPC_Example.id
+  internal_network_id = huaweicloud_vpc_subnet.newSubnet_Example.id
+}
+
+resource "huaweicloud_nat_snat_rule" "newSNATRule_Example" {
+  nat_gateway_id = huaweicloud_nat_gateway.newNet_gateway_Example.id
+  network_id     = huaweicloud_vpc_subnet.newSubnet_Example.id
+  floating_ip_id = huaweicloud_vpc_eip.newEIP_Example.id
+}
+
+resource "null_resource" "provision" {
+  depends_on = [huaweicloud_nat_snat_rule.newSNATRule_Example]
+
+  provisioner "remote-exec" {
+    connection {
+      user     = "root"
+      password = local.password
+      host     = huaweicloud_vpc_eip.newEIP_Example.address
+    }
+    inline = [
+      "yum -y install nginx",
+      "systemctl enable nginx",
+      "systemctl start nginx",
+      "systemctl status nginx",
+    ]
+  }
+}
+
+resource "huaweicloud_nat_dnat_rule" "newDNATRule_Example" {
+  count = length(var.example_dnat_rule)
+
+  floating_ip_id        = huaweicloud_vpc_eip.newEIP_Example.id
+  nat_gateway_id        = huaweicloud_nat_gateway.newNet_gateway_Example.id
+  port_id               = huaweicloud_compute_instance.newCompute_Example.network[0].port     
+
+  internal_service_port = lookup(var.example_dnat_rule[count.index], "internal_service_port", null)
+  protocol              = lookup(var.example_dnat_rule[count.index], "protocol", null)
+  external_service_port = lookup(var.example_dnat_rule[count.index], "external_service_port", null)
+}
---
+++ examples/nat/dnat-basic/variables.tf
@@ -0,0 +1,86 @@
+variable "ecs_name" {
+  type    = string
+  default = "ECS_Example"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "vpc_Example"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "192.168.0.0/16"
+}
+
+variable "ims_name" {
+  type    = string
+  default = "CentOS 7.3 64bit"
+}
+
+variable "bandwidth_name" {
+  type    = string
+  default = "Bandwidth_Example"
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "subnet_Example"
+}
+
+variable "subnet_cidr" {
+  type    = string
+  default = "192.168.64.0/18"
+}
+
+variable "net_gateway_name" {
+  type    = string
+  default = "net_gateway_Example"
+}
+
+variable "subnet_gateway_ip" {
+  type    = string
+  default = "192.168.64.1"
+}
+
+variable "ecs_ipaddress" {
+  type    = string
+  default = "192.168.64.15"
+}
+
+variable "secgroup_name" {
+  type    = string
+  default = "secgroup_Example"
+}
+
+variable "example_security_group" {
+  type = list(object({
+    direction         = string
+    ethertype         = string
+    protocol          = string
+    port_range_min    = number
+    port_range_max    = number
+    remote_ip_prefix  = string
+  }))
+  default = [
+    {direction="ingress", ethertype="IPv4", protocol="tcp",  port_range_min=80,   port_range_max=80, remote_ip_prefix="0.0.0.0/0"},
+    ]
+}
+
+variable "example_dnat_rule" {
+  type = list(object({
+    private_ip            = string
+    internal_service_port = number
+    protocol              = string
+    external_service_port = number
+  }))
+  default = [
+    {private_ip="192.168.64.15", internal_service_port=80,  protocol="tcp", external_service_port=8080},
+    {private_ip="192.168.64.15", internal_service_port=22,  protocol="tcp", external_service_port=22},
+    ]
+}
+
+variable "evs_name" {
+  type    = string
+  default = "volume_Example"
+}
