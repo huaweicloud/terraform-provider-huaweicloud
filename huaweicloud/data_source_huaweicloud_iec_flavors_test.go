838eae49df85a0ac962ac3693c6ea6247011395a
Lance52259 <overlord52259@gmail.com>
2020-12-26 14:55:42 +0800

    support iec flavors data source and docs

---
+++ huaweicloud/data_source_huaweicloud_iec_flavors_test.go
@@ -0,0 +1,53 @@
+package huaweicloud
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccIECFlavorsDataSource_basic(t *testing.T) {
+	rName := "c6.large.2"
+	resourceName := "data.huaweicloud_iec_flavors.flavors_test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIECFlavorsConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIECFlavorsDataSourceID(resourceName, rName),
+					resource.TestMatchResourceAttr(resourceName, "flavors.#", regexp.MustCompile("[1-9]\\d*")),
+					resource.TestCheckResourceAttr(resourceName, "region", "cn-north-4"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIECFlavorsDataSourceID(n, rName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Root module has no resource called %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("IEC flavors data source ID not set")
+		}
+		return nil
+	}
+}
+
+func testAccIECFlavorsConfig(val string) string {
+	return fmt.Sprintf(`
+data "huaweicloud_iec_flavors" "flavors_test" {
+  region = "cn-north-4"
+  name   = "%s"
+}
+	`, val)
+}
