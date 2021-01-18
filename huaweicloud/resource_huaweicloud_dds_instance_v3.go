d81b3a22ee231fe39671ed4d560ddc59ce2a3f1a
Lance52259 <74246744+Lance52259@users.noreply.github.com>
2021-01-18 14:50:45 +0800

    Update reading tags error return of dds instance (#822)

--- huaweicloud/resource_huaweicloud_dds_instance_v3.go
+++ huaweicloud/resource_huaweicloud_dds_instance_v3.go
@@ -433,13 +433,13 @@ func resourceDdsInstanceV3Read(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	// save tags
-	resourceTags, err := tags.Get(client, "instances", d.Id()).Extract()
-	if err != nil {
-		return fmt.Errorf("Error fetching tags of DDS instance: %s", err)
-	}
-	tagmap := tagsToMap(resourceTags.Tags)
-	if err := d.Set("tags", tagmap); err != nil {
-		return fmt.Errorf("[DEBUG] Error saving tag to state for DDS instance (%s): %s", d.Id(), err)
+	if resourceTags, err := tags.Get(client, "instances", d.Id()).Extract(); err == nil {
+		tagmap := tagsToMap(resourceTags.Tags)
+		if err := d.Set("tags", tagmap); err != nil {
+			return fmt.Errorf("Error saving tags to state for DDS instance (%s): %s", d.Id(), err)
+		}
+	} else {
+		log.Printf("[WARN] Error fetching tags of DDS instance (%s): %s", d.Id(), err)
 	}
 
 	return nil
