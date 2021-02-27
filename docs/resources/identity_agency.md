321f297d33013a160bb3e7ce6423dc9f2cd424a2
Lance52259 <74246744+Lance52259@users.noreply.github.com>
2021-02-27 17:10:12 +0800

    support duration param in Identity agency  (#946)

--- docs/resources/identity_agency.md
+++ docs/resources/identity_agency.md
@@ -9,6 +9,7 @@ This is an alternative to `huaweicloud_iam_agency_v3`
 
 ## Example Usage
 
+### Delegate another HUAWEI CLOUD account to perform operations on your resources
 ```hcl
 resource "huaweicloud_identity_agency" "agency" {
   name                  = "test_agency"
@@ -27,43 +28,73 @@ resource "huaweicloud_identity_agency" "agency" {
 }
 ```
 
+### Delegate a cloud service to access your resources in other cloud services
+```hcl
+resource "huaweicloud_identity_agency" "agency" {
+  name                   = "test_agency"
+  description            = "test agency"
+  delegated_service_name = "op_svc_obs"
+
+  project_role {
+    project = "cn-north-1"
+    roles = [
+      "Tenant Administrator",
+    ]
+  }
+  domain_roles = [
+    "OBS OperateAccess",
+  ]
+}
+```
+
 ## Argument Reference
 
 The following arguments are supported:
 
-* `name` - (Required, String, ForceNew) The name of agency. The name is a string of 1 to 64
-    characters. Changing this will create a new agency.
+* `name` - (Required, String, ForceNew) Specifies the name of agency. The name is a string of 1 to 64 characters.
+    Changing this will create a new agency.
 
-* `description` - (Optional, String) Provides supplementary information about the
-    agency. The value is a string of 0 to 255 characters.
+* `description` - (Optional, String) Specifies the supplementary information about the agency.
+    The value is a string of 0 to 255 characters, excluding these characters: '__@#$%^&*<>\\__'.
 
-* `delegated_domain_name` - (Required, String) The name of delegated domain.
+* `delegated_domain_name` - (Optional, String) Specifies the name of delegated user domain.
+    This parameter and `delegated_service_name` are alternative.
 
-* `project_role` - (Optional, List) An array of roles and projects which are used to
-    grant permissions to agency on project. The structure is documented below.
+* `delegated_service_name` - (Optional, String) Specifies the name of delegated cloud service.
+    This parameter and `delegated_domain_name` are alternative.
 
-* `domain_roles` - (optional, List) An array of role names which stand for the
-    permissionis to be granted to agency on domain.
+* `duration` - (Optional, String) Specifies the validity period of an agency.
+    The valid value are *ONEDAY* and *FOREVER*, defaults to *FOREVER*.
+
+* `project_role` - (Optional, List) Specifies an array of one or more roles and projects which are used to grant
+    permissions to agency on project. The structure is documented below.
+
+* `domain_roles` - (optional, List) Specifies an array of one or more role names which stand for the permissionis to
+    be granted to agency on domain.
 
 The `project_role` block supports:
 
-* `project` - (Required, String) The name of project
+* `project` - (Required, String) Specifies the name of project.
 
-* `roles` - (Required, List) An array of role names
+* `roles` - (Required, List) Specifies an array of role names.
 
-**note**:
-    one or both of `project_role` and `domain_roles` must be input when
-creating an agency.
+**note**: one or both of `project_role` and `domain_roles` must be input when creating an agency.
 
 ## Attributes Reference
 
 In addition to all arguments above, the following attributes are exported:
 
 * `id` - The agency ID.
-* `duration` - Validity period of an agency. The default value is null,
-    indicating that the agency is permanently valid.
 * `expire_time` - The expiration time of agency.
 * `create_time` - The time when the agency was created.
+
+## Import
+
+Agencies can be imported using the `id`, e.g.
+
+```
+$ terraform import huaweicloud_identity_agency.agency 0b97661f9900f23f4fc2c00971ea4dc0
+```
 
 ## Timeouts
 This resource provides the following timeouts configuration options:
---
+++ huaweicloud/resource_huaweicloud_identity_agency_test.go
@@ -0,0 +1,204 @@
+package huaweicloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/huaweicloud/golangsdk/openstack/identity/v3/agency"
+)
+
+func TestAccIdentityAgency_basic(t *testing.T) {
+	var agency agency.Agency
+
+	rName := fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
+	resourceName := "huaweicloud_identity_agency.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckIdentityAgencyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIdentityAgency_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIdentityAgencyExists(resourceName, &agency),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "description", "This is a test agency"),
+					resource.TestCheckResourceAttr(resourceName, "delegated_domain_name", HW_DOMAIN_NAME),
+					resource.TestCheckResourceAttr(resourceName, "duration", "FOREVER"),
+					resource.TestCheckResourceAttr(resourceName, "domain_roles.#", "1"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccIdentityAgency_update(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIdentityAgencyExists(resourceName, &agency),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "description", "This is a updated test agency"),
+					resource.TestCheckResourceAttr(resourceName, "delegated_domain_name", HW_DOMAIN_NAME),
+					resource.TestCheckResourceAttr(resourceName, "duration", "ONEDAY"),
+					resource.TestCheckResourceAttr(resourceName, "domain_roles.#", "2"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccIdentityAgency_service(t *testing.T) {
+	var agency agency.Agency
+
+	rName := fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
+	resourceName := "huaweicloud_identity_agency.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckIdentityAgencyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIdentityAgency_service(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIdentityAgencyExists(resourceName, &agency),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "description", "This is a test agency"),
+					resource.TestCheckResourceAttr(resourceName, "delegated_service_name", "op_svc_obs"),
+					resource.TestCheckResourceAttr(resourceName, "duration", "FOREVER"),
+					resource.TestCheckResourceAttr(resourceName, "domain_roles.#", "1"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccIdentityAgency_serviceUpdate(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIdentityAgencyExists(resourceName, &agency),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "description", "This is a test agency"),
+					resource.TestCheckResourceAttr(resourceName, "delegated_service_name", "op_svc_evs"),
+					resource.TestCheckResourceAttr(resourceName, "duration", "FOREVER"),
+					resource.TestCheckResourceAttr(resourceName, "domain_roles.#", "1"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIdentityAgencyDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*Config)
+	client, err := config.IAMV3Client(HW_REGION_NAME)
+	if err != nil {
+		return fmt.Errorf("Error creating HuaweiCloud IAM client: %s", err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "huaweicloud_identity_agency" {
+			continue
+		}
+
+		v, err := agency.Get(client, rs.Primary.ID).Extract()
+		if err == nil && v.ID == rs.Primary.ID {
+			return fmt.Errorf("Identity Agency <%s> still exists", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckIdentityAgencyExists(n string, ag *agency.Agency) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		config := testAccProvider.Meta().(*Config)
+		client, err := config.IAMV3Client(HW_REGION_NAME)
+		if err != nil {
+			return fmt.Errorf("Error creating HuaweiCloud Identity Agency: %s", err)
+		}
+
+		found, err := agency.Get(client, rs.Primary.ID).Extract()
+		if err != nil {
+			return err
+		}
+		if found.ID != rs.Primary.ID {
+			return fmt.Errorf("Identity Agency <%s> not found", rs.Primary.ID)
+		}
+		ag = found
+
+		return nil
+	}
+}
+
+func testAccIdentityAgency_basic(rName string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_identity_agency" "test" {
+  name                  = "%s"
+  description           = "This is a test agency"
+  delegated_domain_name = "%s"
+
+  domain_roles = [
+    "Anti-DDoS Administrator",
+  ]
+}
+`, rName, HW_DOMAIN_NAME)
+}
+
+func testAccIdentityAgency_update(rName string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_identity_agency" "test" {
+  name                  = "%s"
+  description           = "This is a updated test agency"
+  delegated_domain_name = "%s"
+  duration              = "ONEDAY"
+
+  domain_roles = [
+    "Anti-DDoS Administrator",
+    "Ticket Administrator",
+  ]
+}
+`, rName, HW_DOMAIN_NAME)
+}
+
+func testAccIdentityAgency_service(rName string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_identity_agency" "test" {
+  name                   = "%s"
+  description            = "This is a test agency"
+  delegated_service_name = "op_svc_obs"
+
+  domain_roles = [
+    "OBS OperateAccess",
+  ]
+}
+`, rName)
+}
+
+func testAccIdentityAgency_serviceUpdate(rName string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_identity_agency" "test" {
+  name                   = "%s"
+  description            = "This is a test agency"
+  delegated_service_name = "op_svc_evs"
+
+  domain_roles = [
+    "Anti-DDoS Administrator",
+  ]
+}
+`, rName)
+}
