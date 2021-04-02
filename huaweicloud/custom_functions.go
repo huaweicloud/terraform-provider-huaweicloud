86a4de169bf94d2325c85f7ac94463d14653cbd9
ShiChangkuo <shichangkuo90@gmail.com>
2021-04-02 10:10:55 +0800

    clean up after 0661ae5a

--- huaweicloud/custom_functions.go
+++ huaweicloud/custom_functions.go
@@ -3,7 +3,6 @@ package huaweicloud
 import (
 	"fmt"
 	"reflect"
-	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
@@ -88,21 +87,6 @@ func expandCssClusterV1ExtendClusterVolumeSize(d interface{}, arrayIndex map[str
 		return 0, fmt.Errorf("volume size only supports to be extended")
 	}
 	return v, nil
-}
-
-func expandRdsInstanceV3CreateRegion(d interface{}, arrayIndex map[string]int) (interface{}, error) {
-	return navigateValue(d, []string{"region"}, arrayIndex)
-}
-
-func flattenRdsInstanceV3HAReplicationMode(d interface{}, arrayIndex map[string]int, currentValue interface{}) (interface{}, error) {
-	v, err := navigateValue(d, []string{"list", "flavor_ref"}, nil)
-	if err != nil {
-		return nil, err
-	}
-	if strings.HasSuffix(v.(string), ".ha") {
-		return navigateValue(d, []string{"list", "ha", "replication_mode"}, nil)
-	}
-	return "", nil
 }
 
 func expandDisStreamV2CreateAutoCaleEnable(d interface{}, arrayIndex map[string]int) (interface{}, error) {
