f8847cc446b24424cf783a6f09f9a57e89800a70
ShiChangkuo <shichangkuo90@gmail.com>
2021-04-09 16:14:02 +0800

    make acctest of antiddos be happy

--- huaweicloud/data_source_huaweicloud_antiddos_v1_test.go
+++ huaweicloud/data_source_huaweicloud_antiddos_v1_test.go
@@ -9,6 +9,8 @@ import (
 )
 
 func TestAccAntiDdosV1DataSource_basic(t *testing.T) {
+	resourceName := "data.huaweicloud_antiddos.antiddos"
+
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
@@ -16,9 +18,8 @@ func TestAccAntiDdosV1DataSource_basic(t *testing.T) {
 			{
 				Config: testAccAntiDdosV1DataSource_basic,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAntiDdosV1DataSourceID("data.huaweicloud_antiddos_v1.antiddos"),
-					resource.TestCheckResourceAttr("data.huaweicloud_antiddos_v1.antiddos", "share_type", "PER"),
-					resource.TestCheckResourceAttr("data.huaweicloud_antiddos_v1.antiddos", "charge_mode", "traffic"),
+					testAccCheckAntiDdosV1DataSourceID(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "status", "normal"),
 				),
 			},
 		},
@@ -41,7 +42,7 @@ func testAccCheckAntiDdosV1DataSourceID(n string) resource.TestCheckFunc {
 }
 
 const testAccAntiDdosV1DataSource_basic = `
-resource "huaweicloud_vpc_eip_v1" "eip_1" {
+resource "huaweicloud_vpc_eip" "eip_1" {
   publicip {
     type = "5_bgp"
   }
@@ -53,7 +54,7 @@ resource "huaweicloud_vpc_eip_v1" "eip_1" {
   }
 }
 
-data "huaweicloud_antiddos_v1" "antiddos" {  
-  floating_ip_id = "${huaweicloud_vpc_eip_v1.eip_1.id}"
+data "huaweicloud_antiddos" "antiddos" {
+  floating_ip_id = huaweicloud_vpc_eip.eip_1.id
 }
 `
