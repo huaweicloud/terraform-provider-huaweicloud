4c867f1dcc0a6632c3f2ddf09138e7c4e1bbb139
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2021-05-25 19:04:32 +0800

    Add ELB v3 monitors (#1163)

---
+++ huaweicloud/resource_huaweicloud_elb_monitor_test.go
@@ -0,0 +1,161 @@
+package huaweicloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/huaweicloud/golangsdk/openstack/elb/v3/monitors"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+)
+
+func TestAccElbV3Monitor_basic(t *testing.T) {
+	var monitor monitors.Monitor
+	rName := fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
+	resourceName := "huaweicloud_elb_monitor.monitor_1"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckElbV3MonitorDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccElbV3MonitorConfig_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckElbV3MonitorExists(resourceName, &monitor),
+					resource.TestCheckResourceAttr(resourceName, "interval", "20"),
+					resource.TestCheckResourceAttr(resourceName, "timeout", "10"),
+					resource.TestCheckResourceAttr(resourceName, "max_retries", "5"),
+					resource.TestCheckResourceAttr(resourceName, "url_path", "/aa"),
+					resource.TestCheckResourceAttr(resourceName, "domain_name", "www.aa.com"),
+				),
+			},
+			{
+				Config: testAccElbV3MonitorConfig_update(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "interval", "30"),
+					resource.TestCheckResourceAttr(resourceName, "timeout", "15"),
+					resource.TestCheckResourceAttr(resourceName, "max_retries", "10"),
+					resource.TestCheckResourceAttr(resourceName, "port", "8888"),
+					resource.TestCheckResourceAttr(resourceName, "url_path", "/bb"),
+					resource.TestCheckResourceAttr(resourceName, "domain_name", "www.bb.com"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckElbV3MonitorDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*config.Config)
+	elbClient, err := config.ElbV3Client(HW_REGION_NAME)
+	if err != nil {
+		return fmt.Errorf("Error creating HuaweiCloud elb client: %s", err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "huaweicloud_elb_monitor" {
+			continue
+		}
+
+		_, err := monitors.Get(elbClient, rs.Primary.ID).Extract()
+		if err == nil {
+			return fmt.Errorf("Monitor still exists: %s", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckElbV3MonitorExists(n string, monitor *monitors.Monitor) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		config := testAccProvider.Meta().(*config.Config)
+		elbClient, err := config.ElbV3Client(HW_REGION_NAME)
+		if err != nil {
+			return fmt.Errorf("Error creating HuaweiCloud elb client: %s", err)
+		}
+
+		found, err := monitors.Get(elbClient, rs.Primary.ID).Extract()
+		if err != nil {
+			return err
+		}
+
+		if found.ID != rs.Primary.ID {
+			return fmt.Errorf("Monitor not found")
+		}
+
+		*monitor = *found
+
+		return nil
+	}
+}
+
+func testAccCheckElbV3MonitorConfig(rName string) string {
+	return fmt.Sprintf(`
+data "huaweicloud_vpc_subnet" "test" {
+  name = "subnet-default"
+}
+
+data "huaweicloud_availability_zones" "test" {}
+
+resource "huaweicloud_elb_loadbalancer" "test" {
+  name            = "%s"
+  ipv4_subnet_id  = data.huaweicloud_vpc_subnet.test.subnet_id
+  ipv6_network_id = data.huaweicloud_vpc_subnet.test.id
+
+  availability_zone = [
+    data.huaweicloud_availability_zones.test.names[0]
+  ]
+}
+
+resource "huaweicloud_elb_pool" "test" {
+  name            = "%s"
+  protocol        = "HTTP"
+  lb_method       = "LEAST_CONNECTIONS"
+  loadbalancer_id = huaweicloud_elb_loadbalancer.test.id
+}
+`, rName, rName)
+}
+
+func testAccElbV3MonitorConfig_basic(rName string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_elb_monitor" "monitor_1" {
+  protocol    = "HTTP"
+  interval    = 20
+  timeout     = 10
+  max_retries = 5
+  url_path    = "/aa"
+  domain_name = "www.aa.com"
+  pool_id     = huaweicloud_elb_pool.test.id
+}
+`, testAccCheckElbV3MonitorConfig(rName))
+}
+
+func testAccElbV3MonitorConfig_update(rName string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_elb_monitor" "monitor_1" {
+  protocol    = "HTTP"
+  interval    = 30
+  timeout     = 15
+  max_retries = 10
+  url_path    = "/bb"
+  domain_name = "www.bb.com"
+  port        = 8888
+  pool_id     = huaweicloud_elb_pool.test.id
+}
+`, testAccCheckElbV3MonitorConfig(rName))
+}
