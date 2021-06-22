5c636e3eb48c9c39088a5452d95e8b1d80a8d2d0
ChengXiangdong <chengxiangdong@outlook.com>
2021-06-22 17:20:50 +0800

    huaweicloud_scm_certificate adds support for pushing certificates.(#1179)

--- huaweicloud/resource_huaweicloud_scm_certificate_v3.go
+++ huaweicloud/resource_huaweicloud_scm_certificate_v3.go
@@ -2,7 +2,10 @@ package huaweicloud
 
 import (
 	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/huaweicloud/golangsdk"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
@@ -11,9 +14,19 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
+const (
+	MAX_ERROR_MESSAGE_LEN = 200
+	ELLIPSIS_STRING       = "..."
+
+	TARGET_SERVICE_CDN         = "CDN"
+	TARGET_SERVICE_WAF         = "WAF"
+	TARGET_SERVICE_ENHANCE_ELB = "Enhance_ELB"
+)
+
 func resourceScmCertificateV3() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceScmCertificateV3Create,
+		Update: resourceScmCertificateV3Update,
 		Read:   resourceScmCertificateV3Read,
 		Delete: resourceScmCertificateV3Delete,
 
@@ -53,6 +66,26 @@ func resourceScmCertificateV3() *schema.Resource {
 				ForceNew:         true,
 				DiffSuppressFunc: utils.SuppressNewLineDiffs,
 			},
+			"push_certificate": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"target_service": {
+							Type:     schema.TypeString,
+							Required: true,
+							ValidateFunc: validation.StringInSlice([]string{
+								TARGET_SERVICE_CDN, TARGET_SERVICE_WAF, TARGET_SERVICE_ENHANCE_ELB,
+							}, false),
+						},
+						"target_project": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+					}},
+			},
+
 			"domain": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -170,7 +203,90 @@ func resourceScmCertificateV3Create(d *schema.ResourceData, meta interface{}) er
 	d.SetId(c.CertificateId)
 	log.Printf("[DEBUG] Imported certificate %s: %#v", d.Id(), importOpts)
 
+	pushCert := d.Get("push_certificate").([]interface{})
+	for _, pushInfo := range pushCert {
+		targetService := pushInfo.(map[string]interface{})["target_service"].(string)
+		targetProjectArr := pushInfo.(map[string]interface{})["target_project"].([]interface{})
+
+		for _, targetProject := range targetProjectArr {
+			pushOpts := certificates.PushOpts{
+				TargetProject: targetProject.(string),
+				TargetService: targetService,
+			}
+			log.Printf("[DEBUG] Push certificate to services. %#v", pushOpts)
+			err := doPushCertificateToService(d.Id(), pushOpts, elbClient)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
 	return resourceScmCertificateV3Read(d, meta)
+}
+
+func resourceScmCertificateV3Update(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*config.Config)
+	elbClient, err := config.ScmV3Client(GetRegion(d, config))
+
+	oldVal, newVal := d.GetChange("push_certificate")
+	newPushCert, err := parsePushCertificateToMap(newVal.([]interface{}))
+	if err != nil {
+		return err
+	}
+
+	oldPushCert, _ := parsePushCertificateToMap(oldVal.([]interface{}))
+
+	// extract the new push service
+	for targetService, newProjects := range newPushCert {
+		oldProjects, ok := oldPushCert[targetService]
+		if strings.Compare(targetService, TARGET_SERVICE_CDN) == 0 {
+			if !ok {
+				pushOpts := certificates.PushOpts{
+					TargetService: targetService,
+				}
+				log.Printf("[DEBUG] Find new services and start to push. %#v", pushOpts)
+				err := doPushCertificateToService(d.Id(), pushOpts, elbClient)
+				if err != nil {
+					return err
+				}
+			}
+		} else if len(newProjects) == 0 {
+			return fmt.Errorf("the argument of \"target_project\" cannot be empty, "+
+				"it can be empty when pushed to the CDN service only. "+
+				"\r\ncertificate_id: %s, target_service: %s", d.Id(), targetService)
+		}
+		for project, _ := range newProjects {
+			if _, ok := oldProjects[project]; !ok {
+				pushOpts := certificates.PushOpts{
+					TargetProject: project,
+					TargetService: targetService,
+				}
+				log.Printf("[DEBUG] Find new services and start to push. %#v", pushOpts)
+				err := doPushCertificateToService(d.Id(), pushOpts, elbClient)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	//return fmt.Errorf("Error creating HuaweiCloud SCM client") //
+	return resourceScmCertificateV3Read(d, meta)
+}
+
+func doPushCertificateToService(id string, pushOpts certificates.PushOpts, elbClient *golangsdk.ServiceClient) error {
+	if strings.Compare(pushOpts.TargetService, TARGET_SERVICE_CDN) != 0 && len(pushOpts.TargetProject) == 0 {
+		return fmt.Errorf("the argument of \"target_project\" cannot be empty, "+
+			"it can be empty when pushed to the CDN service only. "+
+			"\r\ncertificate_id: %s, target_service: %s", id, pushOpts.TargetService)
+	}
+	err := certificates.Push(elbClient, id, pushOpts).ExtractErr()
+	if err != nil {
+		// Parse 'err' to print more error messages.
+		errMsg := processErr(err)
+		return fmt.Errorf(errMsg)
+	}
+	return nil
 }
 
 func resourceScmCertificateV3Read(d *schema.ResourceData, meta interface{}) error {
@@ -213,20 +329,6 @@ func resourceScmCertificateV3Read(d *schema.ResourceData, meta interface{}) erro
 	return nil
 }
 
-func convertAuthToArray(authArr []certificates.Authentification) []map[string]interface{} {
-	auths := make([]map[string]interface{}, 0, len(authArr))
-	for _, v := range authArr {
-		auth := map[string]interface{}{
-			"record_name":  v.RecordName,
-			"record_type":  v.RecordType,
-			"record_value": v.RecordValue,
-			"domain":       v.Domain,
-		}
-		auths = append(auths, auth)
-	}
-	return auths
-}
-
 func resourceScmCertificateV3Delete(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*config.Config)
 	scmClient, err := config.ScmV3Client(GetRegion(d, config))
@@ -241,4 +343,45 @@ func resourceScmCertificateV3Delete(d *schema.ResourceData, meta interface{}) er
 	}
 
 	return nil
+}
+
+func convertAuthToArray(authArr []certificates.Authentification) []map[string]interface{} {
+	auths := make([]map[string]interface{}, 0, len(authArr))
+	for _, v := range authArr {
+		auth := map[string]interface{}{
+			"record_name":  v.RecordName,
+			"record_type":  v.RecordType,
+			"record_value": v.RecordValue,
+			"domain":       v.Domain,
+		}
+		auths = append(auths, auth)
+	}
+	return auths
+}
+
+func parsePushCertificateToMap(pushCertificate []interface{}) (map[string]map[string]bool, error) {
+	serviceMapping := make(map[string]map[string]bool)
+
+	for _, pushInfo := range pushCertificate {
+		targetService := pushInfo.(map[string]interface{})["target_service"].(string)
+		targetProjectArr := pushInfo.(map[string]interface{})["target_project"].([]interface{})
+
+		projects, ok := serviceMapping[targetService]
+		if !ok {
+			projects = make(map[string]bool)
+		}
+		for _, proj := range targetProjectArr {
+			projectName := proj.(string)
+			if _, ok := projects[projectName]; ok {
+				return nil, fmt.Errorf("There are duplicate projects for the same service!\n"+
+					"target_service = %s, target_project = %s.", targetService, projectName)
+			} else {
+				projects[projectName] = true
+			}
+		}
+		serviceMapping[targetService] = projects
+
+		log.Printf("[DEBUG] Push certificate service mapping: %#v", serviceMapping)
+	}
+	return serviceMapping, nil
 }
