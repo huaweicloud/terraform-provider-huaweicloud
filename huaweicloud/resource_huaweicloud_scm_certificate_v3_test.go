b7d077466cd9d3da5dad2bc0d25c28830c7bd866
ChengXiangdong <chengxiangdong@outlook.com>
2021-06-22 19:39:09 +0800

    Move pushing certificate to huaweicloud_scm_certificate.(#1179)

--- huaweicloud/resource_huaweicloud_scm_certificate_v3_test.go
+++ huaweicloud/resource_huaweicloud_scm_certificate_v3_test.go
@@ -5,6 +5,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/huaweicloud/golangsdk/openstack/scm/v3/certificates"
+	"log"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -24,7 +26,7 @@ func TestAccScmCertificationV3_basic(t *testing.T) {
 		CheckDestroy: testAccCheckScmV3CertificateDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccScmCertificateV3_config(rName),
+				Config: testAccScmCertificateV3_basic(rName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckScmV3CertificateExists(resourceName, &certInfo),
 					resource.TestCheckResourceAttr(resourceName, "name", rName),
@@ -32,10 +34,72 @@ func TestAccScmCertificationV3_basic(t *testing.T) {
 				),
 			},
 			{
-				Config: testAccScmCertificateV3_config(rUpdateName),
+				Config: testAccScmCertificateV3_basic(rUpdateName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckScmV3CertificateExists(resourceName, &certInfo),
 					resource.TestCheckResourceAttr(resourceName, "name", rUpdateName),
+					resource.TestCheckResourceAttr(resourceName, "status", "UPLOAD"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccScmCertificationV3_push(t *testing.T) {
+	var certInfo certificates.CertificateEscrowInfo
+	resourceName := "huaweicloud_scm_certificate.certificate_2"
+
+	rName := fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
+	targetService := "Enhance_ELB"
+	defaultTargetProject := "ap-southeast-1"
+	newTargetProject := "la-south-2"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheckScm(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckScmV3CertificateDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccScmCertificateV3_push(rName, defaultTargetProject, targetService),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScmV3CertificateExists(resourceName, &certInfo),
+					testAccCheckScmV3CertificatePushExists(resourceName, targetService, defaultTargetProject),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "status", "UPLOAD"),
+				),
+			},
+			{
+				Config: testAccScmCertificateV3_push(rName, newTargetProject, targetService),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScmV3CertificateExists(resourceName, &certInfo),
+					testAccCheckScmV3CertificatePushExists(resourceName, targetService, newTargetProject),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "status", "UPLOAD"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccScmCertificationV3_batchPush(t *testing.T) {
+	var certInfo certificates.CertificateEscrowInfo
+	resourceName := "huaweicloud_scm_certificate.certificate_3"
+
+	rName := fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
+	targetService := "Enhance_ELB"
+	defaultTargetProject := "ap-southeast-1"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheckScm(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckScmV3CertificateDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccScmCertificateV3_batchPush(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScmV3CertificateExists(resourceName, &certInfo),
+					testAccCheckScmV3CertificatePushExists(resourceName, targetService, defaultTargetProject),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
 					resource.TestCheckResourceAttr(resourceName, "status", "UPLOAD"),
 				),
 			},
@@ -75,6 +139,33 @@ func testAccCheckScmV3CertificateExists(n string, c *certificates.CertificateEsc
 	}
 }
 
+func testAccCheckScmV3CertificatePushExists(
+    certResourceName string, targetService string, targetProject string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		log.Printf("terraform.State: %#v", s)
+		// Get the resource info by certificateResorceName
+		certRs, ok := s.RootModule().Resources[certResourceName]
+		if !ok {
+			return fmt.Errorf("Not found: %s", certResourceName)
+		}
+
+		if certRs.Primary.ID == "" {
+			return fmt.Errorf("No id is set for the certificate resource: %s", certResourceName)
+		}
+
+		stateTargetService := certRs.Primary.Attributes["push_certificate.0.target_service"]
+		stateTargetProject := certRs.Primary.Attributes["push_certificate.0.target_project.0"]
+
+		if strings.Compare(targetService, stateTargetService) != 0 ||
+		    strings.Compare(targetProject, stateTargetProject) != 0 {
+			return fmt.Errorf("Push certificate failed! targetService: %s, targetProject: %s",
+			    targetService, targetProject)
+		}
+
+		return nil
+	}
+}
+
 func testAccCheckScmV3CertificateDestroy(s *terraform.State) error {
 	config := testAccProvider.Meta().(*config.Config)
 	scmClient, err := config.ScmV3Client(HW_REGION_NAME)
@@ -96,12 +187,42 @@ func testAccCheckScmV3CertificateDestroy(s *terraform.State) error {
 	return nil
 }
 
-func testAccScmCertificateV3_config(name string) string {
+func testAccScmCertificateV3_basic(name string) string {
 	return fmt.Sprintf(`
 resource "huaweicloud_scm_certificate" "certificate_1" {
   name              = "%s"
   certificate       = file("%s")
   certificate_chain = file("%s")
   private_key       = file("%s")
+}`, name, HW_CERTIFICATE_KEY_PATH, HW_CERTIFICATE_CHAIN_PATH, HW_CERTIFICATE_PRIVATE_KEY_PATH)
+}
+
+func testAccScmCertificateV3_push(name string, targetProject string, targetService string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_scm_certificate" "certificate_2" {
+  name              = "%s"
+  certificate       = file("%s")
+  certificate_chain = file("%s")
+  private_key       = file("%s")
+
+  push_certificate {
+    target_project  = ["%s"]
+    target_service  = "%s"
+  }
+}`, name, HW_CERTIFICATE_KEY_PATH, HW_CERTIFICATE_CHAIN_PATH, HW_CERTIFICATE_PRIVATE_KEY_PATH, targetProject, targetService)
+}
+
+func testAccScmCertificateV3_batchPush(name string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_scm_certificate" "certificate_3" {
+  name              = "%s"
+  certificate       = file("%s")
+  certificate_chain = file("%s")
+  private_key       = file("%s")
+
+  push_certificate {
+    target_project  = ["ap-southeast-1", "la-south-2"]
+    target_service  = "Enhance_ELB"
+  }
 }`, name, HW_CERTIFICATE_KEY_PATH, HW_CERTIFICATE_CHAIN_PATH, HW_CERTIFICATE_PRIVATE_KEY_PATH)
 }
