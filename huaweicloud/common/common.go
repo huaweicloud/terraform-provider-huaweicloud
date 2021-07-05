d309cb1beb5925be813ecfbf9ff05ef040081f9c
ChengXiangdong <chengxiangdong@outlook.com>
2021-07-05 20:34:10 +0800

    add common for services

---
+++ huaweicloud/common/common.go
@@ -0,0 +1,83 @@
+/* Copyright (c) Huawei Technologies Co., Ltd. 2021. All rights resvered. */
+/*
+The common package defines some common functions, which are mainly used for the functions of the following services.
+
+The difference between common package and utils:
+1. Common functions under common are related to the project, and common functions are placed here.
+2. Utils are some stored tool functions, which are not related to the project.
+   Such as: date conversion, type conversion.
+*/
+package common
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/huaweicloud/golangsdk"
+	"github.com/huaweicloud/golangsdk/openstack/bss/v2/orders"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils/fmtp"
+)
+
+// GetRegion returns the region that was specified ina the resource. If a
+// region was not set, the provider-level region is checked. The provider-level
+// region can either be set by the region argument or by HW_REGION_NAME.
+func GetRegion(d *schema.ResourceData, config *config.Config) string {
+	if v, ok := d.GetOk("region"); ok {
+		return v.(string)
+	}
+
+	return config.Region
+}
+
+// GetEnterpriseProjectID returns the enterprise_project_id that was specified in the resource.
+// If it was not set, the provider-level value is checked. The provider-level value can
+// either be set by the `enterprise_project_id` argument or by HW_ENTERPRISE_PROJECT_ID.
+func GetEnterpriseProjectID(d *schema.ResourceData, config *config.Config) string {
+	if v, ok := d.GetOk("enterprise_project_id"); ok {
+		return v.(string)
+	}
+
+	return config.EnterpriseProjectID
+}
+
+// CheckDeleted checks the error to see if it's a 404 (Not Found) and, if so,
+// sets the resource ID to the empty string instead of throwing an error.
+func CheckDeleted(d *schema.ResourceData, err error, msg string) error {
+	if _, ok := err.(golangsdk.ErrDefault404); ok {
+		d.SetId("")
+		return nil
+	}
+
+	return fmtp.Errorf("%s: %s", msg, err)
+}
+
+func checkForRetryableError(err error) *resource.RetryError {
+	switch errCode := err.(type) {
+	case golangsdk.ErrDefault500:
+		return resource.RetryableError(err)
+	case golangsdk.ErrUnexpectedResponseCode:
+		switch errCode.Actual {
+		case 409, 503:
+			return resource.RetryableError(err)
+		default:
+			return resource.NonRetryableError(err)
+		}
+	default:
+		return resource.NonRetryableError(err)
+	}
+}
+
+// UnsubscribePrePaidResource impl the action of unsubscribe resource
+func UnsubscribePrePaidResource(d *schema.ResourceData, config *config.Config, resourceIDs []string) error {
+	bssV2Client, err := config.BssV2Client(GetRegion(d, config))
+	if err != nil {
+		return fmtp.Errorf("Error creating HuaweiCloud bss V2 client: %s", err)
+	}
+
+	unsubscribeOpts := orders.UnsubscribeOpts{
+		ResourceIds:     resourceIDs,
+		UnsubscribeType: 1,
+	}
+	_, err = orders.Unsubscribe(bssV2Client, unsubscribeOpts).Extract()
+	return err
+}
