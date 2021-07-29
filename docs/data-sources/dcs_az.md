54b5958188f683c7e2077cf2acebfb98ab15f7cd
shichangkuo <shichangkuo90@gmail.com>
2021-07-29 19:32:33 +0800

    refactor: mark name and port as attributes for dcs_az data source (#1320)

--- docs/data-sources/dcs_az.md
+++ docs/data-sources/dcs_az.md
@@ -2,37 +2,28 @@
 subcategory: "Distributed Cache Service"
 ---
 
-# huaweicloud\_dcs\_az
+# huaweicloud_dcs_az
 
 Use this data source to get the ID of an available Huaweicloud dcs az.
-This is an alternative to `huaweicloud_dcs_az_v1`
 
 ## Example Usage
 
 ```hcl
-
 data "huaweicloud_dcs_az" "az1" {
-  name = "AZ1"
-  port = "8004"
   code = "cn-north-1a"
 }
 ```
 
 ## Argument Reference
 
-For details, See [Querying AZ Information](https://support.huaweicloud.com/en-us/api-dcs/dcs-api-0312039.html).
-
 * `region` - (Optional, String) The region in which to obtain the dcs az. If omitted, the provider-level region will be used.
 
-* `name` - (Optional, String) Indicates the name of an AZ.
-
-* `code` - (Optional, String) Indicates the code of an AZ.
-
-* `port` - (Optional, String) Indicates the port number of an AZ.
-
+* `code` - (Required, String) Specifies the code of an AZ, e.g. "cn-north-1a".
 
 ## Attributes Reference
 
 In addition to all arguments above, the following attributes are exported:
 
-* `id` - Specifies a data source ID in UUID format.
+* `id` - Indicates a data source ID in UUID format.
+* `name` - Indicates the name of an AZ.
+* `port` - Indicates the port number of an AZ.
--- huaweicloud/data_source_huaweicloud_dcs_az_v1_test.go
+++ huaweicloud/data_source_huaweicloud_dcs_az_v1_test.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils/fmtp"
-
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccDcsAZV1DataSource_basic(t *testing.T) {
+	resourceName := "data.huaweicloud_dcs_az.az1"
+
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
@@ -18,9 +18,10 @@ func TestAccDcsAZV1DataSource_basic(t *testing.T) {
 			{
 				Config: testAccDcsAZV1DataSource_basic,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDcsAZV1DataSourceID("data.huaweicloud_dcs_az_v1.az1"),
-					resource.TestCheckResourceAttr(
-						"data.huaweicloud_dcs_az_v1.az1", "port", "8403"),
+					testAccCheckDcsAZV1DataSourceID(resourceName),
+					resource.TestCheckResourceAttrSet(resourceName, "code"),
+					resource.TestCheckResourceAttrSet(resourceName, "port"),
+					resource.TestCheckResourceAttrSet(resourceName, "name"),
 				),
 			},
 		},
@@ -31,11 +32,11 @@ func testAccCheckDcsAZV1DataSourceID(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
-			return fmtp.Errorf("Can't find Dcs az data source: %s", n)
+			return fmt.Errorf("Can't find DCS az data source: %s", n)
 		}
 
 		if rs.Primary.ID == "" {
-			return fmtp.Errorf("Dcs az data source ID not set")
+			return fmt.Errorf("DCS az data source ID not set")
 		}
 
 		return nil
@@ -45,8 +46,7 @@ func testAccCheckDcsAZV1DataSourceID(n string) resource.TestCheckFunc {
 var testAccDcsAZV1DataSource_basic = fmt.Sprintf(`
 data "huaweicloud_availability_zones" "test" {}
 
-data "huaweicloud_dcs_az_v1" "az1" {
+data "huaweicloud_dcs_az" "az1" {
   code = data.huaweicloud_availability_zones.test.names[0]
-  port = "8403"
 }
 `)
