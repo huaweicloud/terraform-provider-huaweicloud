80e114c0fc177e9f00fb1951f5c7add4ada6377c
ShiChangkuo <shichangkuo90@gmail.com>
2021-08-18 06:25:42 +0300

    code cleanup after merge

--- huaweicloud/services/dli/resource_huaweicloud_dli_queue.go
+++ huaweicloud/services/dli/resource_huaweicloud_dli_queue.go
@@ -6,9 +6,9 @@ import (
 	"regexp"
 	"time"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/huaweicloud/golangsdk/openstack/common/tags"
 	"github.com/huaweicloud/golangsdk/openstack/dli/v1/queues"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
