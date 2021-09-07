9af93baf4216a4a74e836ece52b19242e37ade39
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2021-09-07 08:52:34 +0000

    feat(gaussdb/opengauss): add instances data source

---
+++ huaweicloud/services/acceptance/gaussdb/data_source_huaweicloud_gaussdb_opengauss_instances_test.go
@@ -0,0 +1,106 @@
+package gaussdb
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils/fmtp"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccOpenGaussInstancesDataSource_basic(t *testing.T) {
+	rName := fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { acceptance.TestAccPreCheck(t) },
+		Providers: acceptance.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOpenGaussInstancesDataSource_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOpenGaussInstancesDataSourceID("data.huaweicloud_gaussdb_opengauss_instances.test"),
+					resource.TestCheckResourceAttr("data.huaweicloud_gaussdb_opengauss_instances.test", "instances.#", "1"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckOpenGaussInstancesDataSourceID(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmtp.Errorf("Can't find GaussDB opengauss instances data source: %s ", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmtp.Errorf("GaussDB opengauss data source ID not set ")
+		}
+
+		return nil
+	}
+}
+
+func testAccVpcConfig_Base(rName string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_vpc" "test" {
+  name = "%s"
+  cidr = "192.168.0.0/16"
+}
+
+resource "huaweicloud_vpc_subnet" "test" {
+  name          = "%s"
+  cidr          = "192.168.0.0/16"
+  gateway_ip    = "192.168.0.1"
+
+  primary_dns   = "100.125.1.250"
+  secondary_dns = "100.125.21.250"
+  vpc_id        = huaweicloud_vpc.test.id
+}
+`, rName, rName)
+}
+
+func testAccOpenGaussInstancesDataSource_basic(rName string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_networking_secgroup" "test" {
+  name = "default"
+}
+
+resource "huaweicloud_gaussdb_opengauss_instance" "test" {
+  name        = "%s"
+  password    = "Test@123"
+  flavor      = "gaussdb.opengauss.ee.dn.m6.2xlarge.8.in"
+  vpc_id      = huaweicloud_vpc.test.id
+  subnet_id   = huaweicloud_vpc_subnet.test.id
+
+  availability_zone = "cn-north-4a,cn-north-4a,cn-north-4a"
+  security_group_id = data.huaweicloud_networking_secgroup.test.id
+
+  ha {
+    mode             = "enterprise"
+    replication_mode = "sync"
+    consistency      = "strong"
+  }
+  volume {
+    type = "ULTRAHIGH"
+    size = 40
+  }
+
+  sharding_num = 1
+  coordinator_num = 2
+}
+
+data "huaweicloud_gaussdb_opengauss_instances" "test" {
+  name = huaweicloud_gaussdb_opengauss_instance.test.name
+  depends_on = [
+    huaweicloud_gaussdb_opengauss_instance.test,
+  ]
+}
+`, testAccVpcConfig_Base(rName), rName)
+}
---
+++ huaweicloud/services/gaussdb/data_source_huaweicloud_gaussdb_opengauss_instances.go
@@ -0,0 +1,354 @@
+package gaussdb
+
+import (
+	"context"
+	"sort"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/helper/hashcode"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils/fmtp"
+
+	"github.com/chnsz/golangsdk/openstack/opengauss/v3/instances"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func DataSourceOpenGaussInstances() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceOpenGaussInstancesRead,
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"vpc_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"subnet_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"instances": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"region": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"vpc_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"subnet_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"status": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"security_group_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"enterprise_project_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"db_user_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"time_zone": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"availability_zone": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"flavor": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"port": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"switch_strategy": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"maintenance_window": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"datastore": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"engine": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"version": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"backup_strategy": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"start_time": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"keep_days": {
+										Type:     schema.TypeInt,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"ha": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"replication_mode": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"coordinator_num": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"sharding_num": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"volume": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"type": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"size": {
+										Type:     schema.TypeInt,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"private_ips": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"nodes": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"id": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"name": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"status": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"role": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"availability_zone": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceOpenGaussInstancesRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*config.Config)
+	region := config.GetRegion(d)
+	client, err := config.OpenGaussV3Client(region)
+	if err != nil {
+		return fmtp.DiagErrorf("Error creating HuaweiCloud GaussDB client: %s", err)
+	}
+
+	listOpts := instances.ListGaussDBInstanceOpts{
+		Name:     d.Get("name").(string),
+		VpcId:    d.Get("vpc_id").(string),
+		SubnetId: d.Get("subnet_id").(string),
+	}
+
+	pages, err := instances.List(client, listOpts).AllPages()
+	if err != nil {
+		return fmtp.DiagErrorf("Unable to list instances: %s", err)
+	}
+
+	allInstances, err := instances.ExtractGaussDBInstances(pages)
+	if err != nil {
+		return fmtp.DiagErrorf("Unable to retrieve instances: %s", err)
+	}
+
+	var instancesToSet []map[string]interface{}
+	var instancesIds []string
+
+	for _, instanceInAll := range allInstances.Instances {
+		instanceToSet := map[string]interface{}{
+			"id":                    instanceInAll.Id,
+			"region":                region,
+			"name":                  instanceInAll.Name,
+			"status":                instanceInAll.Status,
+			"type":                  instanceInAll.Type,
+			"vpc_id":                instanceInAll.VpcId,
+			"subnet_id":             instanceInAll.SubnetId,
+			"security_group_id":     instanceInAll.SecurityGroupId,
+			"enterprise_project_id": instanceInAll.EnterpriseProjectId,
+			"db_user_name":          instanceInAll.DbUserName,
+			"time_zone":             instanceInAll.TimeZone,
+			"flavor":                instanceInAll.FlavorRef,
+			"port":                  instanceInAll.Port,
+			"switch_strategy":       instanceInAll.SwitchStrategy,
+			"maintenance_window":    instanceInAll.MaintenanceWindow,
+		}
+
+		instanceID := instanceInAll.Id
+		instancesIds = append(instancesIds, instanceID)
+
+		if len(instanceInAll.PrivateIps) > 0 {
+			private_ips := instanceInAll.PrivateIps[0]
+			ip_list := strings.Split(private_ips, "/")
+			for i := 0; i < len(ip_list); i++ {
+				ip_list[i] = strings.Trim(ip_list[i], " ")
+			}
+			instanceToSet["private_ips"] = ip_list
+		}
+
+		// set data store
+		dbList := make([]map[string]interface{}, 1)
+		db := map[string]interface{}{
+			"version": instanceInAll.DataStore.Version,
+			"engine":  instanceInAll.DataStore.Type,
+		}
+		dbList[0] = db
+		instanceToSet["datastore"] = dbList
+
+		// set nodes
+		sharding_num := 0
+		coordinator_num := 0
+		azList := []string{}
+		nodesList := make([]map[string]interface{}, 0, 1)
+		for _, raw := range instanceInAll.Nodes {
+			node := map[string]interface{}{
+				"id":                raw.Id,
+				"name":              raw.Name,
+				"status":            raw.Status,
+				"role":              raw.Role,
+				"availability_zone": raw.AvailabilityZone,
+			}
+			nodesList = append(nodesList, node)
+			azList = append(azList, raw.AvailabilityZone)
+			if strings.Contains(raw.Name, "_gaussdbv5cn") {
+				coordinator_num += 1
+			} else if strings.Contains(raw.Name, "_gaussdbv5dn") {
+				sharding_num += 1
+			}
+		}
+		instanceToSet["nodes"] = nodesList
+		instanceToSet["coordinator_num"] = coordinator_num
+
+		dn_num := sharding_num / 3
+		instanceToSet["sharding_num"] = dn_num
+
+		//remove duplicate az
+		azList = utils.RemoveDuplicateElem(azList)
+		sort.Strings(azList)
+		instanceToSet["availability_zone"] = strings.Join(azList, ",")
+
+		// set backup_strategy
+		backupStrategyList := make([]map[string]interface{}, 1)
+		backupStrategy := map[string]interface{}{
+			"start_time": instanceInAll.BackupStrategy.StartTime,
+			"keep_days":  instanceInAll.BackupStrategy.KeepDays,
+		}
+		backupStrategyList[0] = backupStrategy
+		instanceToSet["backup_strategy"] = backupStrategyList
+
+		// set ha
+		haList := make([]map[string]interface{}, 1)
+		ha := map[string]interface{}{
+			"replication_mode": instanceInAll.Ha.ReplicationMode,
+		}
+		haList[0] = ha
+		instanceToSet["ha"] = haList
+
+		// set volume
+		volume_size := instanceInAll.Volume.Size
+		dn_size := volume_size / dn_num
+		volumeList := make([]map[string]interface{}, 1)
+		volume := map[string]interface{}{
+			"type": instanceInAll.Volume.Type,
+			"size": dn_size,
+		}
+		volumeList[0] = volume
+		instanceToSet["volume"] = volumeList
+
+		instancesToSet = append(instancesToSet, instanceToSet)
+	}
+
+	d.SetId(hashcode.Strings(instancesIds))
+	d.Set("instances", instancesToSet)
+
+	return nil
+}
