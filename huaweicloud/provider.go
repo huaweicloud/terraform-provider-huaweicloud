aae214a2aaba3eb351077c92edc5a45e1126168e
yangxu1013 <88999738+yangxu1013@users.noreply.github.com>
2021-09-23 11:37:08 +0800

    feat: add huaweicloud_vpc_route_table data source and docs (#1493)
    Co-authored-by: ShiChangkuo <shichangkuo90@gmail.com>

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -335,6 +335,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_vpc_peering_connection":               dataSourceVpcPeeringConnectionV2(),
 			"huaweicloud_vpc_route":                            DataSourceVPCRouteV2(),
 			"huaweicloud_vpc_route_ids":                        dataSourceVPCRouteIdsV2(),
+			"huaweicloud_vpc_route_table":                      DataSourceVPCRouteTable(),
 			"huaweicloud_vpc_subnet":                           DataSourceVpcSubnetV1(),
 			"huaweicloud_vpc_subnet_ids":                       DataSourceVpcSubnetIdsV1(),
 			"huaweicloud_vpcep_public_services":                DataSourceVPCEPPublicServices(),
