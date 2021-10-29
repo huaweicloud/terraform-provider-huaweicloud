61f5dc3db2654e75d590cafb3871b29b39f17e1c
ChengXiangdong <chengxiangdong@outlook.com>
2021-10-29 17:46:39 +0800

    feat(waf/group): add a resource to manage the WAF instance groups (#1628)

---
+++ docs/resources/waf_instance_group.md
@@ -0,0 +1,58 @@
+---
+subcategory: "Web Application Firewall (WAF)"
+---
+
+# huaweicloud_waf_instance_group
+
+Manages WAF instance groups within HuaweiCloud. The groups are used to bind the ELB instance to the ELB mode WAF.
+
+## Example Usage
+
+```hcl
+variable "vpc_id" {}
+
+resource "huaweicloud_waf_instance_group" "group_1" {
+  name   = "example_name"
+  vpc_id = var.vpc_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) The region in which to create the instance group.
+  If omitted, the provider-level region will be used. Changing this setting will create a new resource.
+
+* `name` - (Required, String) Specifies the instance group name.
+  The maximum length is 64 characters. Only letters, digits and underscores (_) are allowed.
+
+* `vpc_id` - (Required, String, ForceNew) Specifies the id of the VPC that the WAF dedicated instances belongs to.
+
+* `description` - (Optional, String) Specifies the description of the instance group.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The rule ID in UUID format.
+
+* `body_limit` - The body limit of the forwarding policy.
+
+* `header_limit` - The header limit of the forwarding policy.
+
+* `connection_timeout` - The time for connection timeout in the forwarding policy.
+
+* `write_timeout` - The time for writing timeout in the forwarding policy.
+
+* `read_timeout` - The time for reading timeout in the forwarding policy.
+
+* `load_balances` - The IDs of the ELB instances that has been bound to the instance group.
+
+## Import
+
+The instance group can be imported using the ID, e.g.:
+
+```sh
+terraform import huaweicloud_waf_instance_group.group_1 0be1e69d-1987-4d9c-9dc5-fc7eed592398
+```
---
+++ huaweicloud/services/waf/resource_huaweicloud_waf_instance_group.go
@@ -0,0 +1,219 @@
+package waf
+
+import (
+	"context"
+	"regexp"
+
+	"github.com/chnsz/golangsdk"
+	"github.com/chnsz/golangsdk/openstack/waf_hw/v1/pools"
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils/fmtp"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils/logp"
+)
+
+func ResourceWafInstanceGroup() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceWafInstanceGroupCreate,
+		ReadContext:   resourceWafInstanceGroupRead,
+		UpdateContext: resourceWafInstanceGroupUpdate,
+		DeleteContext: resourceWafInstanceGroupDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ValidateFunc: validation.StringMatch(regexp.MustCompile(`^[\w]{1,64}$`),
+					"The maximum length is 64 characters. "+
+						"Only letters, digits and underscores (_) are allowed"),
+			},
+			"vpc_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"description": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringLenBetween(0, 256),
+			},
+			"body_limit": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"header_limit": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"connection_timeout": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"write_timeout": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"read_timeout": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"load_balances": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+		},
+	}
+}
+
+func resourceWafInstanceGroupCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conf := meta.(*config.Config)
+	client, err := conf.WafDedicatedV1Client(conf.GetRegion(d))
+	if err != nil {
+		return fmtp.DiagErrorf("error creating HuaweiCloud WAF dedicated client : %s", err)
+	}
+
+	createOpts := pools.CreateOpts{
+		Name:        d.Get("name").(string),
+		Region:      conf.GetRegion(d),
+		Type:        "elb",
+		VpcID:       d.Get("vpc_id").(string),
+		Description: d.Get("description").(string),
+	}
+	logp.Printf("[DEBUG] Create WAF instance group options: %#v", createOpts)
+
+	pool, err := pools.Create(client, createOpts)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId(pool.ID)
+
+	return resourceWafInstanceGroupRead(ctx, d, meta)
+}
+
+func resourceWafInstanceGroupRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conf := meta.(*config.Config)
+	client, err := conf.WafDedicatedV1Client(conf.GetRegion(d))
+	if err != nil {
+		return fmtp.DiagErrorf("error creating HuaweiCloud WAF dedicated client : %s", err)
+	}
+
+	pool, err := pools.Get(client, d.Id())
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "Error querying WAF instance group")
+	}
+
+	mErr := multierror.Append(nil,
+		d.Set("region", pool.Region),
+		d.Set("name", pool.Name),
+		d.Set("vpc_id", pool.VpcID),
+		d.Set("body_limit", pool.Option.BodyLimit),
+		d.Set("header_limit", pool.Option.HeaderLimit),
+		d.Set("connection_timeout", pool.Option.ConnectTimeout),
+		d.Set("write_timeout", pool.Option.SendTimeout),
+		d.Set("read_timeout", pool.Option.ReadTimeout),
+	)
+
+	loadBalances := make([]interface{}, 0, len(pool.Bindings))
+	for _, v := range pool.Bindings {
+		loadBalances = append(loadBalances, v.Name)
+	}
+	d.Set("load_balances", loadBalances)
+	d.Set("description", pool.Description)
+
+	if err = mErr.ErrorOrNil(); err != nil {
+		return fmtp.DiagErrorf("error setting WAF dedicated group attributes: %s", err)
+	}
+
+	return nil
+}
+
+func resourceWafInstanceGroupUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conf := meta.(*config.Config)
+	client, err := conf.WafDedicatedV1Client(conf.GetRegion(d))
+	if err != nil {
+		return fmtp.DiagErrorf("error creating HuaweiCloud WAF dedicated client : %s", err)
+	}
+
+	if d.HasChanges("name", "description") {
+		desc := d.Get("description").(string)
+		updateOpts := pools.UpdatePoolOpts{
+			Name:        d.Get("name").(string),
+			Description: &desc,
+		}
+		logp.Printf("[DEBUG] Create WAF instance group options: %#v", updateOpts)
+
+		_, err = pools.Update(client, d.Id(), updateOpts)
+		if err != nil {
+			return fmtp.DiagErrorf("Error in update the groups[%s] : %s", d.Id(), err)
+		}
+	}
+
+	return resourceWafInstanceGroupRead(ctx, d, meta)
+}
+
+func resourceWafInstanceGroupDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conf := meta.(*config.Config)
+	client, err := conf.WafDedicatedV1Client(conf.GetRegion(d))
+	if err != nil {
+		return fmtp.DiagErrorf("error in creating HuaweiCloud WAF dedicated client : %s", err)
+	}
+	// remove the bound ELB instances before deleting the group
+	elbs := d.Get("load_balances").([]interface{})
+	if len(elbs) > 0 {
+		mErr := batchRemoveELBInstances(client, d.Id(), elbs)
+		if err = mErr.ErrorOrNil(); err != nil {
+			return fmtp.DiagErrorf("error in removing ELB instances from group: %s", err)
+		}
+	}
+
+	_, err = pools.Delete(client, d.Id())
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "error in deleting WAF instance group: %s")
+	}
+
+	d.SetId("")
+	return nil
+}
+
+func batchRemoveELBInstances(c *golangsdk.ServiceClient, poolID string, ids []interface{}) *multierror.Error {
+	mErr := &multierror.Error{}
+
+	page, err := pools.ListELB(c, poolID).AllPages()
+	if err != nil {
+		return multierror.Append(mErr, err)
+	}
+	bindELBs, err := pools.ExtractBindELBs(page)
+	if err != nil {
+		return multierror.Append(mErr, err)
+	}
+
+	idMapping := make(map[string]string)
+	for _, v := range bindELBs {
+		idMapping[v.LoadBalancerID] = v.ID
+	}
+	for _, v := range ids {
+		if bindingID, ok := idMapping[v.(string)]; ok {
+			err = pools.RemoveELB(c, poolID, bindingID)
+			if err != nil {
+				err = fmtp.Errorf("Error in removing load balance[%s] from the group[%s]: %s", v, poolID, err)
+				mErr = multierror.Append(mErr, err)
+			}
+		}
+	}
+	return mErr
+}
