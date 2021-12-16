296d0926f1f28db13da86e6e0b8a0f311d40f4be
ChengXiangdong <chengxiangdong@outlook.com>
2021-12-16 17:42:29 +0800

    feat(dms/maintainwindow): upgrade the API version of the query maintain window to V2

--- huaweicloud/services/acceptance/dms/data_source_huaweicloud_dms_maintainwindow_test.go
+++ huaweicloud/services/acceptance/dms/data_source_huaweicloud_dms_maintainwindow_test.go
@@ -9,16 +9,16 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccDmsMaintainWindowV1DataSource_basic(t *testing.T) {
-	dataSourceName := "data.huaweicloud_dms_maintainwindow_v1.maintainwindow1"
+func TestAccDmsMaintainWindowDataSource_basic(t *testing.T) {
+	dataSourceName := "data.huaweicloud_dms_maintainwindow.maintainwindow1"
 	dc := acceptance.InitDataSourceCheck(dataSourceName)
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:          func() { acceptance.TestAccPreCheckDms(t) },
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDmsMaintainWindowV1DataSource_basic,
+				Config: testAccDmsMaintainWindowDataSource_basic,
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
 					resource.TestCheckResourceAttr(dataSourceName, "seq", "1"),
@@ -29,8 +29,8 @@ func TestAccDmsMaintainWindowV1DataSource_basic(t *testing.T) {
 	})
 }
 
-var testAccDmsMaintainWindowV1DataSource_basic = fmt.Sprintf(`
-data "huaweicloud_dms_maintainwindow_v1" "maintainwindow1" {
+var testAccDmsMaintainWindowDataSource_basic = fmt.Sprintf(`
+data "huaweicloud_dms_maintainwindow" "maintainwindow1" {
   seq = 1
 }
 `)
