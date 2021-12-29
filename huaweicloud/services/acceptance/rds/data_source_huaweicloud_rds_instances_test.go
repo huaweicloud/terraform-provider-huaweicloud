8cfc3ddc1d08d9c4c01d33aed6c9c5e62749de68
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2021-12-29 10:46:48 +0800

    feat(rds/instances): add instances listing data source (#1826)

---
+++ huaweicloud/services/acceptance/rds/data_source_huaweicloud_rds_instances_test.go
@@ -0,0 +1,94 @@
+package rds
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccRdsInstanceDataSource_basic(t *testing.T) {
+	dataSourceName := "data.huaweicloud_rds_instances.test"
+	rName := fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
+
+	dc := acceptance.InitDataSourceCheck(dataSourceName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      dc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRdsInstanceDataSource_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestMatchResourceAttr(dataSourceName, "instances.#", regexp.MustCompile("\\d+")),
+					resource.TestCheckResourceAttrSet(dataSourceName, "instances.0.name"),
+				),
+			},
+		},
+	})
+}
+
+func testAccRdsInstanceDataSource_basic(rName string) string {
+	return fmt.Sprintf(`
+data "huaweicloud_availability_zones" "test" {}
+
+resource "huaweicloud_vpc" "test" {
+  name = "%s"
+  cidr = "192.168.0.0/16"
+}
+
+resource "huaweicloud_vpc_subnet" "test" {
+  name          = "%s"
+  cidr          = "192.168.0.0/24"
+  gateway_ip    = "192.168.0.1"
+  vpc_id        = huaweicloud_vpc.test.id
+}
+
+resource "huaweicloud_networking_secgroup" "test" {
+  name = "%s"
+}
+
+resource "huaweicloud_rds_instance" "test" {
+  name              = "%s"
+  flavor            = "rds.pg.n1.large.2"
+  availability_zone = [data.huaweicloud_availability_zones.test.names[0]]
+  security_group_id = huaweicloud_networking_secgroup.test.id
+  subnet_id         = huaweicloud_vpc_subnet.test.id
+  vpc_id            = huaweicloud_vpc.test.id
+  time_zone         = "UTC+08:00"
+  fixed_ip          = "192.168.0.58"
+
+  db {
+    password = "Huangwei!120521"
+    type     = "PostgreSQL"
+    version  = "12"
+    port     = 8635
+  }
+  volume {
+    type = "CLOUDSSD"
+    size = 50
+  }
+  backup_strategy {
+    start_time = "08:00-09:00"
+    keep_days  = 1
+  }
+
+  tags = {
+    key = "value"
+    foo = "bar"
+  }
+}
+
+data "huaweicloud_rds_instances" "test" {
+  depends_on = [
+    huaweicloud_rds_instance.test,
+  ]
+}
+`, rName, rName, rName, rName)
+}
