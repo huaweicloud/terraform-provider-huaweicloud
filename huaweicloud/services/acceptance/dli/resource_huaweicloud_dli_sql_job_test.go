18c597cf8bcbf0a55cfac6e53f59be516cfec937
hua ming <hmbig2@163.com>
2021-12-30 19:48:12 +0800

    feat(DLI): add status check when job is async mode (#1838)

--- huaweicloud/services/acceptance/dli/resource_huaweicloud_dli_sql_job_test.go
+++ huaweicloud/services/acceptance/dli/resource_huaweicloud_dli_sql_job_test.go
@@ -42,7 +42,7 @@ func TestAccResourceDliSqlJob_basic(t *testing.T) {
 				Config: testAccSqlJobBaseResource_basic(name),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(resourceName, "sql", "DESC acc_test_table"),
+					resource.TestCheckResourceAttr(resourceName, "sql", fmt.Sprint("DESC ", name)),
 					resource.TestCheckResourceAttr(resourceName, "database_name", name),
 					resource.TestCheckResourceAttr(resourceName, "job_type", "DDL"),
 				),
@@ -93,42 +93,54 @@ func TestAccResourceDliSqlJob_query(t *testing.T) {
 	})
 }
 
+func TestAccResourceDliSqlJob_async(t *testing.T) {
+	var sqlJobObj sqljob.SqlJobOpts
+	resourceName := "huaweicloud_dli_sql_job.test"
+	name := acceptance.RandomAccResourceName()
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&sqlJobObj,
+		getDliSqlJobResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      testAccCheckDliSqlJobDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSqlJobResource_aync(name),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "sql", fmt.Sprint("SELECT * FROM ", name)),
+					resource.TestCheckResourceAttr(resourceName, "database_name", name),
+					resource.TestCheckResourceAttr(resourceName, "queue_name", "default"),
+					resource.TestCheckResourceAttr(resourceName, "job_type", "QUERY"),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"rows", "schema", "conf"},
+			},
+		},
+	})
+}
+
 func testAccSqlJobBaseResource_basic(name string) string {
 	return fmt.Sprintf(`
-resource "huaweicloud_dli_database" "test" {
-  name        = "%s"
-  description = "For terraform acc test"
-}
-
-resource "huaweicloud_dli_table" "test" {
-  database_name = huaweicloud_dli_database.test.name
-  name          = "acc_test_table"
-  data_location = "DLI"
-  description   = "dli table test"
-
-  columns {
-    name        = "name"
-    type        = "string"
-    description = "person name"
-  }
-
-  columns {
-    name        = "addrss"
-    type        = "string"
-    description = "home address"
-  }
-
-}
+%s
 
 resource "huaweicloud_dli_sql_job" "test" {
   sql           = "DESC ${huaweicloud_dli_table.test.name}"
   database_name = huaweicloud_dli_database.test.name
 }
-
-`, name)
+`, testAccSqlJobBaseResource(name))
 }
 
-func testAccSqlJobBaseResource_query(name string) string {
+func testAccSqlJobBaseResource(name string) string {
 	return fmt.Sprintf(`
 resource "huaweicloud_dli_database" "test" {
   name        = "%s"
@@ -152,15 +164,35 @@ resource "huaweicloud_dli_table" "test" {
     type        = "string"
     description = "home address"
   }
-
 }
+`, name, name)
+}
+
+func testAccSqlJobBaseResource_query(name string) string {
+	return fmt.Sprintf(`
+%s
 
 resource "huaweicloud_dli_sql_job" "test" {
   sql           = "SELECT * FROM ${huaweicloud_dli_table.test.name}"
   database_name = huaweicloud_dli_database.test.name
+
+}
+`, testAccSqlJobBaseResource(name))
 }
 
-`, name, name)
+func testAccSqlJobResource_aync(name string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_dli_sql_job" "test" {
+  sql           = "SELECT * FROM ${huaweicloud_dli_table.test.name}"
+  database_name = huaweicloud_dli_database.test.name
+
+  conf {
+    dli_sql_sqlasync_enabled = true
+  }
+}
+`, testAccSqlJobBaseResource(name))
 }
 
 func testAccCheckDliSqlJobDestroy(s *terraform.State) error {
