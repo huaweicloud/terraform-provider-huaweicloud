97a2fe617166e98a9aa4637c13fdb064e2949616
Lance <74246744+Lance52259@users.noreply.github.com>
2022-01-11 15:01:47 +0800

    chore: test(acc test): Support multi-resource acceptance check (#1857)
    * chore(acc/check): support multi-resource detection of exist checking
    
    * chore(acc test): optimized for acc test
    
    - correct the description of the word destroy
    - add a error return for invalid resource name
    - adjust the code logic of loop
    
    * test(evs): support multi-resources checks in acc test

--- huaweicloud/services/acceptance/acceptance.go
+++ huaweicloud/services/acceptance/acceptance.go
@@ -196,7 +196,7 @@ func TestCheckResourceAttrWithVariable(resourceName, key, varStr string) resourc
 		// Get the value based on resName and keyName from the state.
 		rs, ok := s.RootModule().Resources[resName]
 		if !ok {
-			return fmtp.Errorf("Can't find %s in state : %s.", resName, ok)
+			return fmtp.Errorf("Can't find %s in state : %v.", resName, ok)
 		}
 		value := rs.Primary.Attributes[keyName]
 
@@ -204,7 +204,7 @@ func TestCheckResourceAttrWithVariable(resourceName, key, varStr string) resourc
 	}
 }
 
-// CheckResourceDestroy check whether resources destroied in HuaweiCloud.
+// CheckResourceDestroy check whether resources destroyed in HuaweiCloud.
 func (rc *resourceCheck) CheckResourceDestroy() resource.TestCheckFunc {
 	if strings.Compare(rc.resourceType, dataSourceTypeCode) == 0 {
 		fmtp.Errorf("Error, you built a resourceCheck with 'InitDataSourceCheck', " +
@@ -221,12 +221,16 @@ func (rc *resourceCheck) CheckResourceDestroy() resource.TestCheckFunc {
 			}
 		}
 
+		if resourceType == "" {
+			return fmtp.Errorf("The format of the resource name is invalid, please check your configuration.")
+		}
+
+		conf := TestAccProvider.Meta().(*config.Config)
 		for _, rs := range s.RootModule().Resources {
 			if rs.Type != resourceType {
 				continue
 			}
 
-			conf := TestAccProvider.Meta().(*config.Config)
 			if rc.getResourceFunc != nil {
 				if _, err := rc.getResourceFunc(conf, rs); err == nil {
 					return fmtp.Errorf("failed to destroy resource. The resource of %s : %s still exists.",
@@ -240,39 +244,64 @@ func (rc *resourceCheck) CheckResourceDestroy() resource.TestCheckFunc {
 	}
 }
 
+func (rc *resourceCheck) checkResourceExists(s *terraform.State) error {
+	rs, ok := s.RootModule().Resources[rc.resourceName]
+	if !ok {
+		return fmtp.Errorf("Can not found the resource or data source in state: %s", rc.resourceName)
+	}
+	if rs.Primary.ID == "" {
+		return fmtp.Errorf("No id set for the resource or data source: %s", rc.resourceName)
+	}
+	if strings.EqualFold(rc.resourceType, dataSourceTypeCode) {
+		return nil
+	}
+
+	if rc.getResourceFunc != nil {
+		conf := TestAccProvider.Meta().(*config.Config)
+		r, err := rc.getResourceFunc(conf, rs)
+		if err != nil {
+			return fmtp.Errorf("checking resource %s %s exists error: %s ",
+				rc.resourceName, rs.Primary.ID, err)
+		}
+		if rc.resourceObject != nil {
+			b, err := json.Marshal(r)
+			if err != nil {
+				return fmtp.Errorf("marshaling resource %s %s error: %s ",
+					rc.resourceName, rs.Primary.ID, err)
+			}
+			json.Unmarshal(b, rc.resourceObject)
+		} else {
+			logp.Printf("[WARN] The 'resourceObject' is nil, please set it during initialization.")
+		}
+	} else {
+		return fmtp.Errorf("The 'getResourceFunc' is nil, please set it.")
+	}
+
+	return nil
+}
+
 // CheckResourceExists check whether resources exist in HuaweiCloud.
 func (rc *resourceCheck) CheckResourceExists() resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[rc.resourceName]
-		if !ok {
-			return fmtp.Errorf("Can not found the resource or data source in state: %s", rc.resourceName)
-		}
-		if rs.Primary.ID == "" {
-			return fmtp.Errorf("No id set for the resource or data source: %s", rc.resourceName)
-		}
-		if strings.EqualFold(rc.resourceType, dataSourceTypeCode) {
-			return nil
-		}
+		return rc.checkResourceExists(s)
+	}
+}
 
-		if rc.getResourceFunc != nil {
-			conf := TestAccProvider.Meta().(*config.Config)
-			r, err := rc.getResourceFunc(conf, rs)
+/*
+CheckMultiResourcesExists checks whether multiple resources created by count in HuaweiCloud are both existed.
+  Parameters:
+    expCount: the expected number of resources that will be created.
+*/
+func (rc *resourceCheck) CheckMultiResourcesExists(expCount int) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		var err error
+		for i := 0; i < expCount; i++ {
+			rcCopy := *rc
+			rcCopy.resourceName = fmt.Sprintf("%s.%d", rcCopy.resourceName, i)
+			err = rcCopy.checkResourceExists(s)
 			if err != nil {
-				return fmtp.Errorf("checking resource %s %s exists error: %s ",
-					rc.resourceName, rs.Primary.ID, err)
+				return err
 			}
-			if rc.resourceObject != nil {
-				b, err := json.Marshal(r)
-				if err != nil {
-					return fmtp.Errorf("marshaling resource %s %s error: %s ",
-						rc.resourceName, rs.Primary.ID, err)
-				}
-				json.Unmarshal(b, rc.resourceObject)
-			} else {
-				logp.Printf("[WARN] The 'resourceObject' is nil, please set it during initialization.")
-			}
-		} else {
-			return fmtp.Errorf("The 'getResourceFunc' is nil, please set it.")
 		}
 
 		return nil
