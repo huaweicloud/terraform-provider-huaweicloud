d4f0e97fc624cc4590fd544347b116b4d83219eb
Jason Zhang <jasonzhang9309@163.com>
2022-03-18 11:35:09 +0800

    add func SuppressVersionDiffs in utils and add test for it

--- huaweicloud/resource_huaweicloud_cce_cluster_v3.go
+++ huaweicloud/resource_huaweicloud_cce_cluster_v3.go
@@ -3,7 +3,6 @@ package huaweicloud
 import (
 	"context"
 	"fmt"
-	"regexp"
 	"strings"
 	"time"
 
@@ -68,23 +67,11 @@ func ResourceCCEClusterV3() *schema.Resource {
 				ForceNew: true,
 			},
 			"cluster_version": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-				ForceNew: true,
-				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
-					oldArray := regexp.MustCompile(`[\.\-]+`).Split(old, -1)
-					newArray := regexp.MustCompile(`[\.\-]+`).Split(new, -1)
-					if len(newArray) > len(oldArray) {
-						return false
-					}
-					for i, v := range newArray {
-						if v != oldArray[i] {
-							return false
-						}
-					}
-					return true
-				},
+				Type:             schema.TypeString,
+				Optional:         true,
+				Computed:         true,
+				ForceNew:         true,
+				DiffSuppressFunc: utils.SuppressVersionDiffs,
 			},
 			"cluster_type": {
 				Type:     schema.TypeString,
