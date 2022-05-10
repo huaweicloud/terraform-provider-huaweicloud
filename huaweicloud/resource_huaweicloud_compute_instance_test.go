249c9c17a7bdc5a9e3e0f5c131d51c95126db0ef
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2022-05-10 14:56:57 +0800

    feat(compute/instance): add agent_list support (#2135)

--- huaweicloud/resource_huaweicloud_compute_instance_test.go
+++ huaweicloud/resource_huaweicloud_compute_instance_test.go
@@ -39,6 +39,7 @@ func TestAccComputeV2Instance_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet(resourceName, "availability_zone"),
 					resource.TestCheckResourceAttr(resourceName, "network.0.source_dest_check", "false"),
 					resource.TestCheckResourceAttr(resourceName, "stop_before_destroy", "true"),
+					resource.TestCheckResourceAttr(resourceName, "agent_list", "hss"),
 				),
 			},
 			{
@@ -358,6 +359,7 @@ resource "huaweicloud_compute_instance" "test" {
   flavor_id           = data.huaweicloud_compute_flavors.test.ids[0]
   security_group_ids  = [data.huaweicloud_networking_secgroup.test.id]
   stop_before_destroy = true
+  agent_list          = "hss"
 
   network {
     uuid              = data.huaweicloud_vpc_subnet.test.id
