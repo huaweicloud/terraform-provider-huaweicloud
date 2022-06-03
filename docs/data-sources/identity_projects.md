534c7bdb505b45ea68599aae27922352692bc52a
Lance52259 <Overlord52259@gmail.com>
2022-06-03 18:23:30 +0800

    feat(iam/projects): add new projcets query resource

---
+++ docs/data-sources/identity_projects.md
@@ -0,0 +1,49 @@
+---
+subcategory: "Identity and Access Management (IAM)"
+---
+
+# huaweicloud_identity_projects
+
+Use this data source to query the project list within HuaweiCloud.
+
+Note: You *must* have admin privileges in your HuaweiCloud cloud to use this data source.
+
+## Example Usage
+
+### Obtain project information by name
+
+```hcl
+resource "huaweicloud_identity_projects" "test" {
+  name = "cn-north-4"
+}
+```
+
+### Obtain special project information by name
+
+```hcl
+resource "huaweicloud_identity_projects" "test" {
+  name = "MOS" // The project for OBS Billing
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Optional, String) Specifies the project name to query.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `projects` - The details of the query projects. The structure is documented below.
+
+The `projects` block supports:
+
+* `id` - The project ID.
+
+* `name` - The project name.
+
+* `enabled` - Whether project is enabled.
---
+++ huaweicloud/services/iam/data_source_huaweicloud_identity_projects.go
@@ -0,0 +1,86 @@
+package iam
+
+import (
+	"context"
+
+	"github.com/chnsz/golangsdk/openstack/identity/v3/projects"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/helper/hashcode"
+)
+
+func DataSourceIdentityProjects() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceIdentityProjectsRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"projects": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"enabled": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func flattenProjectList(projectList []projects.Project) ([]map[string]interface{}, []string) {
+	if len(projectList) < 1 {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, len(projectList))
+	ids := make([]string, len(projectList))
+	for i, val := range projectList {
+		result[i] = map[string]interface{}{
+			"id":      val.ID,
+			"name":    val.Name,
+			"enabled": val.Enabled,
+		}
+	}
+	return result, ids
+}
+
+func dataSourceIdentityProjectsRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*config.Config)
+	client, err := config.IdentityV3Client(config.GetRegion(d))
+	if err != nil {
+		return diag.Errorf("error creating IAM v3 client: %s", err)
+	}
+
+	listOpts := projects.ListOpts{
+		Name: d.Get("name").(string),
+	}
+	pages, err := projects.List(client, listOpts).AllPages()
+	if err != nil {
+		return diag.Errorf("error retrieving project list: %v", err)
+	}
+	projectList, err := projects.ExtractProjects(pages)
+	if err != nil {
+		return diag.Errorf("error fetching project objects: %v", err)
+	}
+
+	result, ids := flattenProjectList(projectList)
+
+	d.SetId(hashcode.Strings(ids))
+	return diag.FromErr(d.Set("projects", result))
+}
