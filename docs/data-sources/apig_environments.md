512583b4e62ce557806fa86cd6c2d9dcbc653757
Lance52259 <Overlord52259@gmail.com>
2022-06-24 18:03:38 +0800

    feat(apig/environment): add new data source support

---
+++ docs/data-sources/apig_environments.md
@@ -0,0 +1,50 @@
+---
+subcategory: "API Gateway (Dedicated APIG)"
+---
+
+# huaweicloud_apig_environments
+
+Use this data source to query the environment list under the APIG instance within Huaweicloud.
+
+## Example Usage
+
+```hcl
+variable "instance_id" {}
+variable "environment_name" {}
+
+data "huaweicloud_apig_environments" "test" {
+  instance_id = var.instance_id
+  name        = var.environment_name
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the APIG environment list.
+  If omitted, the provider-level region will be used.
+
+* `instance_id` - (Required, String) Specifies an ID of the APIG dedicated instance to which the API
+  environment belongs.
+
+* `name` - (Optional, String) Specifies the name of the API environment. The API environment name consists of 3 to 64
+  characters, starting with a letter. Only letters, digits and underscores (_) are allowed.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - Data source ID.
+
+* `environments` - List of APIG environment details. The structure is documented below.
+
+The `environments` block supports:
+
+* `id` - ID of the APIG environment.
+
+* `name` - The environment name.
+
+* `description` - The description about the API environment.
+
+* `create_time` - Time when the APIG environment was created, in RFC-3339 format.
---
+++ huaweicloud/services/acceptance/apig/data_source_huaweicloud_apig_environments_test.go
@@ -0,0 +1,52 @@
+package apig
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataEnvironments_basic(t *testing.T) {
+	var (
+		dataSourceName = "data.huaweicloud_apig_environments.test"
+		dc             = acceptance.InitDataSourceCheck(dataSourceName)
+		rName          = acceptance.RandomAccResourceName()
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckEpsID(t) // The creation of APIG instance needs the enterprise project ID.
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataEnvironments_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestMatchResourceAttr(dataSourceName, "environments.#", regexp.MustCompile(`[1-9]\d*`)),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataEnvironments_basic(rName string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_apig_environment" "test" {
+  name        = "%s"
+  instance_id = huaweicloud_apig_instance.test.id
+  description = "Created by script"
+}
+
+data "huaweicloud_apig_environments" "test" {
+  instance_id = huaweicloud_apig_instance.test.id
+  name        = huaweicloud_apig_environment.test.name
+}
+`, testAccApigApplication_base(rName), rName)
+}
