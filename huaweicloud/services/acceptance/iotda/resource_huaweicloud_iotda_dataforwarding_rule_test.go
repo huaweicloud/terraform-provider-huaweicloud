dd1455950d10eaf9fcba86a69e0bcc1174efaa77
hua ming <hmbig2@163.com>
2022-06-24 10:36:57 +0800

    feat(IoTDA): add a resource to manage data forwarding (#2211)

---
+++ huaweicloud/services/acceptance/iotda/resource_huaweicloud_iotda_dataforwarding_rule_test.go
@@ -0,0 +1,120 @@
+package iotda
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/huaweicloud/huaweicloud-sdk-go-v3/services/iotda/v5/model"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func getDataForwardingRuleResourceFunc(conf *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	client, err := conf.HcIoTdaV5Client(acceptance.HW_REGION_NAME)
+	if err != nil {
+		return nil, fmt.Errorf("error creating IoTDA v5 client: %s", err)
+	}
+
+	return client.ShowRoutingRule(&model.ShowRoutingRuleRequest{RuleId: state.Primary.ID})
+}
+
+func TestAccDataForwardingRule_basic(t *testing.T) {
+	var obj model.ShowRoutingRuleResponse
+
+	name := acceptance.RandomAccResourceNameWithDash()
+	updateName := acceptance.RandomAccResourceNameWithDash()
+	rName := "huaweicloud_iotda_dataforwarding_rule.test"
+
+	rc := acceptance.InitResourceCheck(
+		rName,
+		&obj,
+		getDataForwardingRuleResourceFunc,
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testDataForwardingRule_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(rName, "name", name),
+					resource.TestCheckResourceAttr(rName, "trigger", "product:delete"),
+					resource.TestCheckResourceAttr(rName, "enabled", "true"),
+					resource.TestCheckResourceAttr(rName, "targets.#", "1"),
+					resource.TestCheckResourceAttr(rName, "targets.0.type", "HTTP_FORWARDING"),
+					resource.TestCheckResourceAttr(rName, "targets.0.http_forwarding.0.url", "http://www.exampletest.com"),
+					resource.TestCheckResourceAttrSet(rName, "targets.0.id"),
+				),
+			},
+			{
+				Config: testDataForwardingRule_dis(updateName, acceptance.HW_REGION_NAME, acceptance.HW_PROJECT_ID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(rName, "name", updateName),
+					resource.TestCheckResourceAttr(rName, "trigger", "product:delete"),
+					resource.TestCheckResourceAttr(rName, "enabled", "false"),
+					resource.TestCheckResourceAttr(rName, "targets.#", "1"),
+					resource.TestCheckResourceAttr(rName, "targets.0.type", "DIS_FORWARDING"),
+					resource.TestCheckResourceAttrSet(rName, "targets.0.id"),
+					resource.TestCheckResourceAttrPair(rName, "targets.0.dis_forwarding.0.stream_id",
+						"huaweicloud_dis_stream.test", "id"),
+				),
+			},
+			{
+				ResourceName:      rName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testDataForwardingRule_basic(name string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_iotda_dataforwarding_rule" "test" {
+  name    = "%s"
+  trigger = "product:delete"
+  enabled = true
+  
+  targets {
+    type = "HTTP_FORWARDING"
+    http_forwarding {
+      url = "http://www.exampletest.com"
+    }
+  }
+
+
+}
+`, name)
+}
+
+func testDataForwardingRule_dis(name, region, projectId string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_dis_stream" "test" {
+  stream_name     = "%s"
+  partition_count = 1
+}
+
+resource "huaweicloud_iotda_dataforwarding_rule" "test" {
+  name    = "%s"
+  trigger = "product:delete"
+  enabled = false
+
+  targets {
+    type = "DIS_FORWARDING"
+    dis_forwarding {
+      region     = "%s"
+      project_id = "%s"
+      stream_id  = huaweicloud_dis_stream.test.id
+    }
+  }
+
+
+}
+`, name, name, region, projectId)
+}
