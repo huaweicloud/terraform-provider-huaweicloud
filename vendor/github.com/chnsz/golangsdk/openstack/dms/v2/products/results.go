a581b0ef25465f510c72f09f3bd9bf5c6b2854fb
Lance52259 <Overlord52259@gmail.com>
2022-07-06 20:25:30 +0800

    feat(dms): support a datasource to query product list of new format

--- vendor/github.com/chnsz/golangsdk/openstack/dms/v2/products/results.go
+++ vendor/github.com/chnsz/golangsdk/openstack/dms/v2/products/results.go
@@ -56,3 +56,92 @@ type IO struct {
 	UnavailableZones []string `json:"unavailable_zones"`
 	VolumeType       string   `json:"volume_type"`
 }
+
+// ListResp is the structure that represents the request response of List method.
+type ListResp struct {
+	// The engine type of the DMS products.
+	Engine string `json:"engine"`
+	// The supported product version types.
+	Versions []string `json:"versions"`
+	// The list of product details.
+	Products []Product `json:"products"`
+}
+
+// Product is the structure that represents the details of the product specification.
+type Product struct {
+	// product type. The current product types are stand-alone and cluster.
+	Type string `json:"type"`
+	// Product ID.
+	ProductId string `json:"product_id"`
+	// The underlying resource type.
+	EcsFlavorId string `json:"ecs_flavor_id"`
+	// Billing type.
+	BillingCode string `json:"billing_code"`
+	// List of supported CPU architectures.
+	ArchTypes []string `json:"arch_types"`
+	// List of supported billing modes.
+	//   monthly: yearly/monthly type.
+	//   hourly: On-demand type.
+	ChargingModes []string `json:"charging_mode"`
+	// List of supported disk IO types.
+	IOs []IOEntity `json:"ios"`
+	// A list of features supported by the current specification instance.
+	SupportFeatures []SupportFeatureEntity `json:"support_features"`
+	// Properties of the current specification instance.
+	Properties PropertiesEntity `json:"properties"`
+}
+
+// IOEntity is the structure that represents the disk IO type information.
+type IOEntity struct {
+	// Disk IO encoding.
+	IoSpec string `json:"io_spec"`
+	// Disk type.
+	Type string `json:"type"`
+	// Availability Zone.
+	AvailableZones []string `json:"available_zones"`
+	// Unavailable zone.
+	UnavailableZones []string `json:"unavailable_zones"`
+}
+
+// SupportFeatureEntity is the structure that represents the features supported by the instance.
+type SupportFeatureEntity struct {
+	// function name.
+	Name string `json:"name"`
+	// Description of the function properties supported by the instance.
+	Properties SupportFeaturePropertiesEntity `json:"properties"`
+}
+
+// SupportFeaturePropertiesEntity is the structure that represents the description of the functional properties
+// supported by the instance.
+type SupportFeaturePropertiesEntity struct {
+	// The maximum number of tasks for the dump function.
+	MaxTask string `json:"max_task"`
+	// Minimum number of tasks for dump function.
+	MinTask string `json:"min_task"`
+	// Maximum number of nodes for dump function.
+	MaxNode string `json:"max_node"`
+	// Minimum number of nodes for dump function.
+	MinNode string `json:"min_node"`
+}
+
+// PropertiesEntity is the structure that represents the properties of the current specification instance.
+type PropertiesEntity struct {
+	// Maximum number of partitions per broker.
+	MaxPartitionPerBroker string `json:"max_partition_per_broker"`
+	// The maximum number of brokers.
+	MaxBroker string `json:"max_broker"`
+	// Maximum storage per node. The unit is GB.
+	MaxStoragePerNode string `json:"max_storage_per_node"`
+	// Maximum number of consumers per broker.
+	MaxConsumerPerBroker string `json:"max_consumer_per_broker"`
+	// The minimum number of brokers.
+	MinBroker string `json:"min_broker"`
+	// Maximum bandwidth per broker.
+	MaxBandwidthPerBroker string `json:"max_bandwidth_per_broker"`
+	// Minimum storage per node. The unit is GB.
+	MinStoragePerNode string `json:"min_storage_per_node"`
+	// Maximum TPS per Broker.
+	MaxTpsPerBroker string `json:"max_tps_per_broker"`
+	// Product ID alias.
+	ProductAlias string `json:"product_alias"`
+}
--- vendor/github.com/chnsz/golangsdk/openstack/dms/v2/products/urls.go
+++ vendor/github.com/chnsz/golangsdk/openstack/dms/v2/products/urls.go
@@ -10,3 +10,7 @@ const resourcePath = "products"
 func getURL(client *golangsdk.ServiceClient) string {
 	return client.ServiceURL(resourcePath)
 }
+
+func listURL(client *golangsdk.ServiceClient, engineType string) string {
+	return client.ServiceURL(engineType, resourcePath)
+}
