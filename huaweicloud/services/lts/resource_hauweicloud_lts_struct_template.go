e9283bc48dece03eff1e717115d721aa717c6dcf
ShiChangkuo <shichangkuo90@gmail.com>
2022-09-30 17:27:10 +0800

    enhance(lts): improve on lts struct template resource

--- huaweicloud/services/lts/resource_hauweicloud_lts_struct_template.go
+++ huaweicloud/services/lts/resource_hauweicloud_lts_struct_template.go
@@ -4,15 +4,17 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/hashicorp/go-multierror"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/internal/entity"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/internal/httpclient_go"
 	"io/ioutil"
 	"strings"
 
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/internal/entity"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/internal/httpclient_go"
 )
 
 func ResourceLtsStruct() *schema.Resource {
@@ -33,27 +35,33 @@ func ResourceLtsStruct() *schema.Resource {
 				Computed: true,
 				ForceNew: true,
 			},
-			"content": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
 			"log_group_id": {
 				Type:     schema.TypeString,
 				Required: true,
+				ForceNew: true,
 			},
 			"log_stream_id": {
 				Type:     schema.TypeString,
 				Required: true,
+				ForceNew: true,
+			},
+			"template_type": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"built_in", "custom",
+				}, false),
 			},
 			"template_id": {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
-			"template_type": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
 			"template_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"content": {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
@@ -127,19 +135,23 @@ func ResourceLtsStruct() *schema.Resource {
 
 func resourceLtsStructTemplateCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	cfg := meta.(*config.Config)
-	client, err := httpclient_go.NewHttpClientGo(cfg, "lts", cfg.GetRegion(d))
+	region := cfg.GetRegion(d)
+	client, err := httpclient_go.NewHttpClientGo(cfg, "lts", region)
 	if err != nil {
-		return diag.Errorf("err creating Client； %s", err)
+		return diag.Errorf("error creating LTS client: %s", err)
 	}
+
 	var url string
 	header := make(map[string]string)
 	header["content-type"] = "application/json;charset=UTF8"
-	region := cfg.GetRegion(d)
+
 	opts := entity.StructTemplateRequest{
 		LogGroupId:  d.Get("log_group_id").(string),
 		LogStreamId: d.Get("log_stream_id").(string),
 	}
-	if d.Get("template_type").(string) == "custom" {
+
+	templateType := d.Get("template_type").(string)
+	if templateType == "custom" {
 		url = "v2/" + cfg.GetProjectID(region) + "/lts/struct/template"
 		opts.ToDemoFieldsInfo()
 		opts.ParseType = "split"
@@ -147,15 +159,16 @@ func resourceLtsStructTemplateCreate(ctx context.Context, d *schema.ResourceData
 		opts.Content = "127.0.0.1 10.142.203.101 8080 [18/Aug/2021:15:14:33 +0800] GET /apm HTTP/1.1 404 86 6"
 	} else {
 		url = "v3/" + cfg.GetProjectID(region) + "/lts/struct/template"
-		opts.TemplateId = d.Get("template_id").(string)
-		opts.TemplateType = d.Get("template_type").(string)
+		opts.TemplateType = templateType
 		opts.TemplateName = d.Get("template_name").(string)
+		opts.TemplateId = d.Get("template_id").(string)
 	}
 	client.WithMethod(httpclient_go.MethodPost).WithUrl(url).WithHeader(header).WithBody(opts)
 	response, err := client.Do()
 	if err != nil {
 		return diag.Errorf("error request creating StructTemplate fields %s: %s", opts.LogGroupId, err)
 	}
+
 	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
@@ -169,13 +182,15 @@ func resourceLtsStructTemplateCreate(ctx context.Context, d *schema.ResourceData
 
 func resourceLtsStructTemplateRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	cfg := meta.(*config.Config)
-	client, err := httpclient_go.NewHttpClientGo(cfg, "lts", cfg.GetRegion(d))
+	region := cfg.GetRegion(d)
+	client, err := httpclient_go.NewHttpClientGo(cfg, "lts", region)
 	if err != nil {
-		return diag.Errorf("err creating Client: %s", err)
+		return diag.Errorf("error creating LTS client: %s", err)
 	}
+
 	header := make(map[string]string)
 	header["content-type"] = "application/json;charset=UTF8"
-	region := cfg.GetRegion(d)
+
 	client.WithMethod(httpclient_go.MethodGet).WithUrl("v2/" +
 		cfg.GetProjectID(region) + "/lts/struct/template?logGroupId=" +
 		d.Get("log_group_id").(string) + "&logStreamId=" + d.Get("log_stream_id").(string)).
@@ -194,6 +209,7 @@ func resourceLtsStructTemplateRead(_ context.Context, d *schema.ResourceData, me
 	if err != nil {
 		return diag.Errorf("error unmarshal body on entity.ShowStructTemplateResponse")
 	}
+
 	d.SetId(rlt.Id)
 	mErr := multierror.Append(nil,
 		d.Set("demo_log", rlt.DemoLog),
@@ -208,13 +224,15 @@ func resourceLtsStructTemplateRead(_ context.Context, d *schema.ResourceData, me
 
 func resourceLtsStructTemplateDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	cfg := meta.(*config.Config)
-	client, err := httpclient_go.NewHttpClientGo(cfg, "lts", cfg.GetRegion(d))
+	region := cfg.GetRegion(d)
+	client, err := httpclient_go.NewHttpClientGo(cfg, "lts", region)
 	if err != nil {
-		return diag.Errorf("err creating Client: %s", err)
+		return diag.Errorf("error creating LTS client: %s", err)
 	}
+
 	header := make(map[string]string)
 	header["content-type"] = "application/json;charset=UTF8"
-	region := cfg.GetRegion(d)
+
 	structTemplateDeleteRequest := entity.DeleteStructTemplateReqBody{
 		Id: d.Id(),
 	}
@@ -224,6 +242,7 @@ func resourceLtsStructTemplateDelete(_ context.Context, d *schema.ResourceData, 
 	if err != nil {
 		return diag.Errorf("error delete StructTemplate %s: %s", d.Id(), err)
 	}
+
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -237,13 +256,15 @@ func resourceLtsStructTemplateDelete(_ context.Context, d *schema.ResourceData, 
 
 func resourceLtsStructTemplateUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	cfg := meta.(*config.Config)
-	client, err := httpclient_go.NewHttpClientGo(cfg, "lts", cfg.GetRegion(d))
+	region := cfg.GetRegion(d)
+	client, err := httpclient_go.NewHttpClientGo(cfg, "lts", region)
 	if err != nil {
-		return diag.Errorf("err creating Client: %s", err)
+		return diag.Errorf("error creating LTS client: %s", err)
 	}
+
 	header := make(map[string]string)
 	header["content-type"] = "application/json;charset=UTF8"
-	region := cfg.GetRegion(d)
+
 	structTemplateRequest := entity.StructTemplateRequest{
 		LogGroupId:   d.Get("log_group_id").(string),
 		LogStreamId:  d.Get("log_stream_id").(string),
