ce9fa66c7b4847949a5081da684f9904ccaf15c0
ZhangJishu <jasonzhang9309@163.com>
2022-09-30 11:05:12 +0800

    feat(gaussdb/nosql): add new resource influx and mongo (#2393)

---
+++ huaweicloud/services/acceptance/gaussdb/resource_huaweicloud_gaussdb_influx_instance_test.go
@@ -0,0 +1,155 @@
+package gaussdb
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+
+	"github.com/chnsz/golangsdk/openstack/geminidb/v3/instances"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+)
+
+func getNosqlInstance(conf *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	client, err := conf.GeminiDBV3Client(acceptance.HW_REGION_NAME)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating HuaweiCloud GaussDB client: %s", err)
+	}
+
+	found, err := instances.GetInstanceByID(client, state.Primary.ID)
+	if err != nil {
+		return nil, err
+	}
+	if found.Id == "" {
+		return nil, fmt.Errorf("Instance <%s> not found.", state.Primary.ID)
+	}
+
+	return &found, nil
+}
+
+func TestAccGaussInfluxInstance_basic(t *testing.T) {
+	var instance instances.GeminiDBInstance
+	rName := acceptance.RandomAccResourceName()
+	resourceName := "huaweicloud_gaussdb_influx_instance.test"
+	password := acceptance.RandomPassword()
+	newPassword := acceptance.RandomPassword()
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&instance,
+		getNosqlInstance,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGaussInfluxInstanceConfig_basic(rName, password),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "password", password),
+					resource.TestCheckResourceAttr(resourceName, "node_num", "3"),
+					resource.TestCheckResourceAttr(resourceName, "volume_size", "100"),
+					resource.TestCheckResourceAttr(resourceName, "status", "normal"),
+				),
+			},
+			{
+				Config: testAccGaussInfluxInstanceConfig_update(rName, newPassword),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", fmt.Sprintf("%s-update", rName)),
+					resource.TestCheckResourceAttr(resourceName, "password", newPassword),
+					resource.TestCheckResourceAttr(resourceName, "node_num", "4"),
+					resource.TestCheckResourceAttr(resourceName, "volume_size", "200"),
+					resource.TestCheckResourceAttr(resourceName, "status", "normal"),
+				),
+			},
+		},
+	})
+}
+
+func testAccGaussInfluxInstanceConfig_basic(rName, password string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_availability_zones" "test" {}
+
+data "huaweicloud_networking_secgroup" "test" {
+  name = "default"
+}
+
+data "huaweicloud_gaussdb_nosql_flavors" "test" {
+  vcpus             = 2
+  engine            = "influxdb"
+  availability_zone = data.huaweicloud_availability_zones.test.names[0]
+}
+
+resource "huaweicloud_gaussdb_influx_instance" "test" {
+  name        = "%s"
+  password    = "%s"
+  flavor      = data.huaweicloud_gaussdb_nosql_flavors.test.flavors[0].name
+  volume_size = 100
+  vpc_id      = huaweicloud_vpc.test.id
+  subnet_id   = huaweicloud_vpc_subnet.test.id
+  node_num    = 3
+
+  security_group_id = data.huaweicloud_networking_secgroup.test.id
+  availability_zone = data.huaweicloud_availability_zones.test.names[0]
+
+  backup_strategy {
+    start_time = "03:00-04:00"
+    keep_days  = 14
+  }
+
+  tags = {
+    foo = "bar"
+    key = "value"
+  }
+}
+`, testAccVpcConfig_Base(rName), rName, password)
+}
+
+func testAccGaussInfluxInstanceConfig_update(rName, password string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_availability_zones" "test" {}
+
+data "huaweicloud_networking_secgroup" "test" {
+  name = "default"
+}
+
+data "huaweicloud_gaussdb_nosql_flavors" "test" {
+  vcpus             = 2
+  engine            = "influxdb"
+  availability_zone = data.huaweicloud_availability_zones.test.names[0]
+}
+
+resource "huaweicloud_gaussdb_influx_instance" "test" {
+  name        = "%s-update"
+  password    = "%s"
+  flavor      = data.huaweicloud_gaussdb_nosql_flavors.test.flavors[0].name
+  volume_size = 200
+  vpc_id      = huaweicloud_vpc.test.id
+  subnet_id   = huaweicloud_vpc_subnet.test.id
+  node_num    = 4
+
+  security_group_id = data.huaweicloud_networking_secgroup.test.id
+  availability_zone = data.huaweicloud_availability_zones.test.names[0]
+
+  backup_strategy {
+    start_time = "03:00-04:00"
+    keep_days  = 14
+  }
+
+  tags = {
+    foo = "bar"
+    key = "value"
+  }
+}
+`, testAccVpcConfig_Base(rName), rName, password)
+}
