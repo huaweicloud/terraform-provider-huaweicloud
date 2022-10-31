b7a83c4794768a1efae011c52b87e7724c808a8e
Jason Zhang <jasonzhang9309@163.com>
2022-10-31 16:26:54 +0800

    feat(cce/node_attach): fix the error when attaching a prePaid ECS

--- huaweicloud/services/acceptance/cce/resource_huaweicloud_cce_node_attach_test.go
+++ huaweicloud/services/acceptance/cce/resource_huaweicloud_cce_node_attach_test.go
@@ -58,6 +58,37 @@ func TestAccCCENodeAttachV3_basic(t *testing.T) {
 	})
 }
 
+func TestAccCCENodeAttachV3_prePaid(t *testing.T) {
+	var node nodes.Nodes
+
+	rName := fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
+	resourceName := "huaweicloud_cce_node_attach.test"
+	//clusterName here is used to provide the cluster id to fetch cce node.
+	clusterName := "huaweicloud_cce_cluster.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckChargingMode(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      testAccCheckCCENodeV3Destroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCCENodeAttachV3_prePaid(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCCENodeV3Exists(resourceName, clusterName, &node),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "tags.foo", "bar"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key", "value"),
+					resource.TestCheckResourceAttr(resourceName, "os", "EulerOS 2.5"),
+					resource.TestCheckResourceAttr(resourceName, "charging_mode", "prePaid"),
+				),
+			},
+		},
+	})
+}
+
 func testAccCCENodeAttachV3_Base(rName string) string {
 	return fmt.Sprintf(`
 %s
@@ -167,4 +198,76 @@ resource "huaweicloud_cce_node_attach" "test" {
   }
 }
 `, testAccCCENodeAttachV3_Base(rName), rNameUpdate)
+}
+
+func testAccCCENodeAttachV3_prePaidBase(rName string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_availability_zones" "test" {}
+
+data "huaweicloud_images_image" "test" {
+  name        = "EulerOS 2.5 64bit"
+  most_recent = true
+}
+
+resource "huaweicloud_compute_instance" "test" {
+  name                        = "%s"
+  image_id                    = data.huaweicloud_images_image.test.id
+  flavor_id                   = "sn3.large.2"
+  availability_zone           = data.huaweicloud_availability_zones.test.names[0]
+  admin_pass                  = "Test@123"
+  delete_disks_on_termination = true
+
+  charging_mode = "prePaid"
+  period_unit   = "month"
+  period        = 1
+  
+  system_disk_type = "SAS"
+  system_disk_size = 40
+  
+  data_disks {
+	type = "SAS"
+	size = "100"
+  }
+  
+  network {
+	uuid = huaweicloud_vpc_subnet.test.id
+  }
+
+  lifecycle {
+    ignore_changes = [
+      image_id, tags, name
+    ]
+  }
+}
+
+resource "huaweicloud_cce_cluster" "test" {
+  name                   = "%s"
+  cluster_type           = "VirtualMachine"
+  flavor_id              = "cce.s1.small"
+  vpc_id                 = huaweicloud_vpc.test.id
+  subnet_id              = huaweicloud_vpc_subnet.test.id
+  container_network_type = "overlay_l2"
+}
+`, testAccCCEClusterV3_Base(rName), rName, rName)
+}
+
+func testAccCCENodeAttachV3_prePaid(rName string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_cce_node_attach" "test" {
+  cluster_id = huaweicloud_cce_cluster.test.id
+  server_id  = huaweicloud_compute_instance.test.id
+  password   = "Test@123"
+  os         = "EulerOS 2.5"
+  name       = "%s"
+
+  tags = {
+    foo = "bar"
+    key = "value"
+  }
+}
+`, testAccCCENodeAttachV3_prePaidBase(rName), rName)
 }
