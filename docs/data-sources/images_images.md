911519ff6030efdf553542661be061b704b59abe
houpeng <hp_monkey@163.com>
2022-11-22 20:41:57 +0800

    add ims image images

---
+++ docs/data-sources/images_images.md
@@ -0,0 +1,113 @@
+---
+subcategory: "Image Management Service (IMS)"
+---
+
+# huaweicloud_images_images
+
+Use this data source to get available HuaweiCloud IMS images.
+
+## Example Usage
+
+```hcl
+data "huaweicloud_images_images" "ubuntu" {
+  name        = "Ubuntu 18.04 server 64bit"
+  visibility  = "public"
+}
+
+data "huaweicloud_images_images" "centos-1" {
+  architecture = "x86"
+  os_version   = "CentOS 7.4 64bit"
+  visibility   = "public"
+}
+
+data "huaweicloud_images_images" "centos-2" {
+  architecture = "x86"
+  name_regex   = "^CentOS 7.4"
+  visibility   = "public"
+}
+
+data "huaweicloud_images_images" "bms_image" {
+  architecture = "x86"
+  image_type   = "Ironic"
+  os_version   = "CentOS 7.4 64bit"
+  visibility   = "public"
+}
+```
+
+## Argument Reference
+
+* `region` - (Optional, String) The region in which to obtain the images. If omitted, the provider-level region will be
+  used.
+
+* `name` - (Optional, String) The name of the image. Cannot be used simultaneously with `name_regex`.
+
+* `name_regex` - (Optional, String) The regular expressian of the name of the image.
+  Cannot be used simultaneously with `name`.
+
+* `visibility` - (Optional, String) The visibility of the image. Must be one of
+  **public**, **private**, **market** or **shared**.
+
+* `architecture` (Optional, String) Specifies the image architecture type. The value can be **x86** and **arm**.
+
+* `os` - (Optional, String) Specifies the image OS type. The value can be **Windows**, **Ubuntu**,
+  **RedHat**, **SUSE**, **CentOS**, **Debian**, **OpenSUSE**, **Oracle Linux**, **Fedora**, **Other**,
+  **CoreOS**, or **EulerOS**.
+
+* `os_version` - (Optional, String) Specifies the OS version. For example, *CentOS 7.4 64bit* or *Ubuntu 18.04 server 64bit*.
+
+* `image_type` (Optional, String) Specifies the environment where the image is used. For a BMS image, the value is **Ironic**.
+
+* `owner` - (Optional, String) The owner (UUID) of the image.
+
+* `tag` - (Optional, String) Search for images with a specific tag in "Key=Value" format.
+
+* `sort_direction` - (Optional, String) Order the results in either `asc` or `desc`.
+
+* `sort_key` - (Optional, String) Sort images based on a certain key. Must be one of
+  "name", "container_format", "disk_format", "status", "id" or "size". Defaults to `name`.
+
+* `enterprise_project_id` - (Optional, String) Specifies the enterprise project ID of the image.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `images` - Indicates the images information. Structure is documented below.
+
+The `images` block contains:
+
+* `id` - The ID of the image
+
+* `name` - The name of the image.
+
+* `visibility` - The visibility of the image.
+
+* `container_format` - The format of the image's container.
+
+* `disk_format` - The format of the image's disk.
+
+* `min_disk_gb` - The minimum amount of disk space required to use the image.
+
+* `min_ram_mb` - The minimum amount of ram required to use the image.
+
+* `owner` - The owner (UUID) of the image.
+
+* `protected` - Whether or not the image is protected.
+
+* `image_type` The environment where the image is used. For a BMS image, the value is **Ironic**.
+
+* `os` - Specifies the image OS type.
+
+* `os_version` - The OS version.
+
+* `enterprise_project_id` - The enterprise project ID of the image.
+
+* `status` - The status of the image.
+
+* `created_at` - The date when the image was created.
+
+* `update_at` - The date when the image was last updated.
+
+* `size_bytes` - The size of the image (in bytes).
---
+++ huaweicloud/services/acceptance/ims/data_source_huaweicloud_images_images_test.go
@@ -0,0 +1,194 @@
+package ims
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccImsImagesDataSource_basic(t *testing.T) {
+	imageName := "CentOS 7.4 64bit"
+	dataSourceName := "data.huaweicloud_images_images.test"
+	dc := acceptance.InitDataSourceCheck(dataSourceName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccImsImagesDataSource_publicName(imageName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.name", imageName),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.protected", "true"),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.visibility", "public"),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.status", "active"),
+				),
+			},
+			{
+				Config: testAccImsImagesDataSource_osVersion(imageName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.protected", "true"),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.visibility", "public"),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.status", "active"),
+				),
+			},
+			{
+				Config: testAccImsImagesDataSource_nameRegex("^CentOS 7.4"),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.protected", "true"),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.visibility", "public"),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.status", "active"),
+				),
+			},
+			{
+				Config: testAccImsImagesDataSource_market(),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.protected", "true"),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.visibility", "market"),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.status", "active"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccImsImagesDataSource_testQueries(t *testing.T) {
+	var rName = fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
+	dataSourceName := "data.huaweicloud_images_images.test"
+	dc := acceptance.InitDataSourceCheck(dataSourceName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccImsImagesDataSource_base(rName),
+			},
+			{
+				Config: testAccImsImagesDataSource_queryName(rName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.name", rName),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.protected", "false"),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.visibility", "private"),
+					resource.TestCheckResourceAttr(dataSourceName, "images.0.status", "active"),
+				),
+			},
+			{
+				Config: testAccImsImagesDataSource_queryTag(rName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+				),
+			},
+		},
+	})
+}
+
+func testAccImsImagesDataSource_publicName(imageName string) string {
+	return fmt.Sprintf(`
+data "huaweicloud_images_images" "test" {
+  name        = "%s"
+  visibility  = "public"
+}
+`, imageName)
+}
+
+func testAccImsImagesDataSource_nameRegex(regexp string) string {
+	return fmt.Sprintf(`
+data "huaweicloud_images_images" "test" {
+  architecture = "x86"
+  name_regex   = "%s"
+  visibility   = "public"
+}
+`, regexp)
+}
+
+func testAccImsImagesDataSource_osVersion(osVersion string) string {
+	return fmt.Sprintf(`
+data "huaweicloud_images_images" "test" {
+  architecture = "x86"
+  os_version   = "%s"
+  visibility   = "public"
+}
+`, osVersion)
+}
+
+func testAccImsImagesDataSource_market() string {
+	return `
+data "huaweicloud_images_images" "test" {
+  os          = "CentOS"
+  visibility  = "market"
+}
+`
+}
+
+func testAccImsImagesDataSource_base(rName string) string {
+	return fmt.Sprintf(`
+data "huaweicloud_availability_zones" "test" {}
+
+data "huaweicloud_compute_flavors" "test" {
+  availability_zone = data.huaweicloud_availability_zones.test.names[0]
+  performance_type  = "normal"
+  cpu_core_count    = 2
+  memory_size       = 4
+}
+
+data "huaweicloud_vpc_subnet" "test" {
+  name = "subnet-default"
+}
+
+resource "huaweicloud_networking_secgroup" "test" {
+  name = "%[1]s"
+}
+
+resource "huaweicloud_compute_instance" "test" {
+  name               = "%[1]s"
+  image_name         = "Ubuntu 18.04 server 64bit"
+  flavor_id          = data.huaweicloud_compute_flavors.test.ids[0]
+  security_group_ids = [
+    huaweicloud_networking_secgroup.test.id
+  ]
+  availability_zone  = data.huaweicloud_availability_zones.test.names[0]
+  network {
+    uuid = data.huaweicloud_vpc_subnet.test.id
+  }
+}
+
+resource "huaweicloud_images_image" "test" {
+  name        = "%[1]s"
+  instance_id = huaweicloud_compute_instance.test.id
+  description = "created by Terraform AccTest"
+  tags = {
+    foo = "bar"
+    key = "value"
+  }
+}
+`, rName)
+}
+
+func testAccImsImagesDataSource_queryName(rName string) string {
+	return fmt.Sprintf(`
+%s
+data "huaweicloud_images_images" "test" {
+  name        = huaweicloud_images_image.test.name
+}
+`, testAccImsImagesDataSource_base(rName))
+}
+
+func testAccImsImagesDataSource_queryTag(rName string) string {
+	return fmt.Sprintf(`
+%s
+data "huaweicloud_images_images" "test" {
+  visibility  = "private"
+  tag         = "foo=bar"
+}
+`, testAccImsImagesDataSource_base(rName))
+}
