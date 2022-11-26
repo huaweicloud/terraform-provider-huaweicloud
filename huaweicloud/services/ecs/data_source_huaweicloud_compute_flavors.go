9549408d8d1e308b1c0474f7235123baa3db01da
ShiChangkuo <shichangkuo90@gmail.com>
2022-11-26 14:41:57 +0800

    fix: filter by az is unnecessary as it has done by API

--- huaweicloud/services/ecs/data_source_huaweicloud_compute_flavors.go
+++ huaweicloud/services/ecs/data_source_huaweicloud_compute_flavors.go
@@ -3,7 +3,6 @@ package ecs
 import (
 	"context"
 	"strconv"
-	"strings"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -79,7 +78,6 @@ func dataSourceEcsFlavorsRead(_ context.Context, d *schema.ResourceData, meta in
 	mem := int64(d.Get("memory_size").(int)) * 1024
 	pType := d.Get("performance_type").(string)
 	gen := d.Get("generation").(string)
-	az := d.Get("availability_zone").(string)
 
 	var ids []string
 	for _, flavor := range allFlavors {
@@ -98,22 +96,6 @@ func dataSourceEcsFlavorsRead(_ context.Context, d *schema.ResourceData, meta in
 
 		if gen != "" && flavor.OsExtraSpecs.Generation != gen {
 			continue
-		}
-
-		if az != "" {
-			status := flavor.OsExtraSpecs.OperationStatus
-			azStatusRaw := flavor.OsExtraSpecs.OperationAz
-			azStatusList := strings.Split(azStatusRaw, ",")
-			if strings.Contains(azStatusRaw, az) {
-				for i := 0; i < len(azStatusList); i++ {
-					azStatus := azStatusList[i]
-					if azStatus == (az+"(abandon)") || azStatus == (az+"(sellout)") || azStatus == (az+"obt_sellout") {
-						continue
-					}
-				}
-			} else if status == "abandon" || strings.Contains(status, "sellout") {
-				continue
-			}
 		}
 
 		ids = append(ids, flavor.ID)
