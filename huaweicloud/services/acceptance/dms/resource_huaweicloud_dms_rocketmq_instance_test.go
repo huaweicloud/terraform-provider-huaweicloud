f6b3dace9308c4c6778d54e05c88a34ba836cd07
houpeng <hp_monkey@163.com>
2022-11-29 18:25:02 +0800

    add dms rocketmq instance

---
+++ huaweicloud/services/acceptance/dms/resource_huaweicloud_dms_rocketmq_instance_test.go
@@ -0,0 +1,131 @@
+package dms
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/chnsz/golangsdk"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func getDmsRocketMQInstanceResourceFunc(config *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	region := acceptance.HW_REGION_NAME
+	// getRocketmqInstance: Query DMS rocketmq instance
+	var (
+		getRocketmqInstanceHttpUrl = "v2/{project_id}/instances/{instance_id}"
+		getRocketmqInstanceProduct = "dms"
+	)
+	getRocketmqInstanceClient, err := config.NewServiceClient(getRocketmqInstanceProduct, region)
+	if err != nil {
+		return nil, fmt.Errorf("error creating DmsRocketMQInstance Client: %s", err)
+	}
+
+	getRocketmqInstancePath := getRocketmqInstanceClient.Endpoint + getRocketmqInstanceHttpUrl
+	getRocketmqInstancePath = strings.ReplaceAll(getRocketmqInstancePath, "{project_id}", getRocketmqInstanceClient.ProjectID)
+	getRocketmqInstancePath = strings.ReplaceAll(getRocketmqInstancePath, "{instance_id}", fmt.Sprintf("%v", state.Primary.ID))
+
+	getRocketmqInstanceOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		OkCodes: []int{
+			200,
+		},
+	}
+	getRocketmqInstanceResp, err := getRocketmqInstanceClient.Request("GET", getRocketmqInstancePath, &getRocketmqInstanceOpt)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving DmsRocketMQInstance: %s", err)
+	}
+	return utils.FlattenResponse(getRocketmqInstanceResp)
+}
+
+func TestAccDmsRocketMQInstance_basic(t *testing.T) {
+	var obj interface{}
+
+	rName := acceptance.RandomAccResourceNameWithDash()
+	updateName := acceptance.RandomAccResourceNameWithDash()
+	resourceName := "huaweicloud_dms_rocketmq_instance.test"
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&obj,
+		getDmsRocketMQInstanceResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testDmsRocketMQInstance_basic(rName, rName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "engine_version", "4.8.0"),
+				),
+			},
+			{
+				Config: testDmsRocketMQInstance_basic(rName, updateName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", updateName),
+					resource.TestCheckResourceAttr(resourceName, "engine_version", "4.8.0"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccDmsRocketmqInstance_Base(rName string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_vpc" "test" {
+  name        = "%[1]s"
+  cidr        = "192.168.0.0/24"
+  description = "Test for DMS RocketMQ"
+}
+
+resource "huaweicloud_vpc_subnet" "test" {
+  name       = "%[1]s"
+  cidr       = "192.168.0.0/24"
+  gateway_ip = "192.168.0.1"
+  vpc_id     = huaweicloud_vpc.test.id
+}
+
+resource "huaweicloud_networking_secgroup" "test" {
+  name        = "%[1]s"
+  description = "secgroup for rocketmq"
+}
+
+data "huaweicloud_availability_zones" "test" {}
+`, rName)
+}
+
+func testDmsRocketMQInstance_basic(rName, updateName string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_dms_rocketmq_instance" "test" {
+  name                = "%s"
+  engine_version      = "4.8.0"
+  storage_space       = 600
+  vpc_id              = huaweicloud_vpc.test.id
+  subnet_id           = huaweicloud_vpc_subnet.test.id
+  security_group_id   = huaweicloud_networking_secgroup.test.id
+  availability_zones  = [
+    data.huaweicloud_availability_zones.test.names[0]
+  ]
+  flavor_id           = "c6.4u8g.cluster"
+  storage_spec_code   = "dms.physical.storage.high.v2"
+}
+`, testAccDmsRocketmqInstance_Base(rName), updateName)
+}
