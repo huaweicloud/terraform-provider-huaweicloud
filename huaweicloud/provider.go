3f47228e47a9df4a0e3c353aefc184cd19247176
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2022-12-30 10:42:26 +0800

    feat: add dms rocketmq consumer group resource (#2572)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -685,7 +685,8 @@ func Provider() *schema.Provider {
 			"huaweicloud_dms_kafka_topic":       dms.ResourceDmsKafkaTopic(),
 			"huaweicloud_dms_rabbitmq_instance": dms.ResourceDmsRabbitmqInstance(),
 
-			"huaweicloud_dms_rocketmq_instance": dms.ResourceDmsRocketMQInstance(),
+			"huaweicloud_dms_rocketmq_instance":       dms.ResourceDmsRocketMQInstance(),
+			"huaweicloud_dms_rocketmq_consumer_group": dms.ResourceDmsRocketMQConsumerGroup(),
 
 			"huaweicloud_dns_ptrrecord": ResourceDNSPtrRecordV2(),
 			"huaweicloud_dns_recordset": ResourceDNSRecordSetV2(),
