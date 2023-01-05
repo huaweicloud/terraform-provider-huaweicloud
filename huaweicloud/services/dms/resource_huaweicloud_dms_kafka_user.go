1d61791ef1a231e7d34823447b13160ca263de96
ChengXiangdong <chengxiangdong@outlook.com>
2023-01-05 20:16:19 +0800

    refactor(dms): just sort imports (#2632)

--- huaweicloud/services/dms/resource_huaweicloud_dms_kafka_user.go
+++ huaweicloud/services/dms/resource_huaweicloud_dms_kafka_user.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	"github.com/huaweicloud/huaweicloud-sdk-go-v3/services/kafka/v2/model"
+
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
