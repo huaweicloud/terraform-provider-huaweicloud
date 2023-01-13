f75eb6a66dd997fb355150f768017049ecf0b6ba
huaming <hmbig2@163.com>
2023-01-13 16:09:20 +0800

    feat(DWS): upgrade API of flavor

--- vendor/github.com/chnsz/golangsdk/openstack/dws/v2/flavors/urls.go
+++
@@ -1,8 +0,0 @@
-package flavors
-
-import "github.com/chnsz/golangsdk"
-
-// listNodeTypesURL /v2/{project_id}/node-types
-func listNodeTypesURL(c *golangsdk.ServiceClient) string {
-	return c.ServiceURL("node-types")
-}
