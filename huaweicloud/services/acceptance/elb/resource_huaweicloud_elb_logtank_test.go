76ccc8935fd4aab1797d1509a084c365a8c3959c
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2023-02-10 18:12:37 +0800

    refactor(elb/logtank_member_monitor): fix lint issues (#2683)

--- huaweicloud/services/acceptance/elb/resource_huaweicloud_elb_logtank_test.go
+++ huaweicloud/services/acceptance/elb/resource_huaweicloud_elb_logtank_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
 	"github.com/chnsz/golangsdk/openstack/elb/v3/logtanks"
+
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
@@ -15,7 +16,7 @@ import (
 func getELBLogTankResourceFunc(c *config.Config, state *terraform.ResourceState) (interface{}, error) {
 	client, err := c.ElbV3Client(acceptance.HW_REGION_NAME)
 	if err != nil {
-		return nil, fmt.Errorf("error creating HuaweiCloud ELB v3 client: %s", err)
+		return nil, fmt.Errorf("error creating ELB client: %s", err)
 	}
 	return logtanks.Get(client, state.Primary.ID).Extract()
 }
@@ -23,7 +24,6 @@ func getELBLogTankResourceFunc(c *config.Config, state *terraform.ResourceState)
 func TestAccElbLogTank_basic(t *testing.T) {
 	var logTanks logtanks.LogTank
 	rName := acceptance.RandomAccResourceNameWithDash()
-	rNameUpdate := acceptance.RandomAccResourceNameWithDash()
 	resourceName := "huaweicloud_elb_logtank.test"
 
 	rc := acceptance.InitResourceCheck(
@@ -48,7 +48,7 @@ func TestAccElbLogTank_basic(t *testing.T) {
 				),
 			},
 			{
-				Config: testAccElbLogTankConfig_update(rNameUpdate),
+				Config: testAccElbLogTankConfig_update(rName),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
 					resource.TestCheckResourceAttrPair(resourceName, "log_group_id",
@@ -66,13 +66,15 @@ func TestAccElbLogTank_basic(t *testing.T) {
 	})
 }
 
-func testAccElbLogTankConfig_basic(rName string) string {
+func testAccElbLogTankConfig_base(rName, updateName string) string {
 	return fmt.Sprintf(`
 data "huaweicloud_availability_zones" "test" {}
+
 resource "huaweicloud_vpc" "test" {
   name = "%[1]s"
   cidr = "192.168.0.0/16"
 }
+
 resource "huaweicloud_vpc_subnet" "test" {
   name        = "%[1]s"
   cidr        = "192.168.0.0/24"
@@ -80,64 +82,49 @@ resource "huaweicloud_vpc_subnet" "test" {
   vpc_id      = huaweicloud_vpc.test.id
   ipv6_enable = true
 }
+
 resource "huaweicloud_elb_loadbalancer" "test" {
   name            = "%[1]s"
   ipv4_subnet_id  = huaweicloud_vpc_subnet.test.ipv4_subnet_id
   ipv6_network_id = huaweicloud_vpc_subnet.test.id
+
   availability_zone = [
     data.huaweicloud_availability_zones.test.names[0]
   ]
 }
-resource "huaweicloud_lts_group" "test" {
-  group_name  = "%[1]s"
+
+resource "huaweicloud_lts_group" "%[2]s" {
+  group_name  = "%[2]s"
   ttl_in_days = 1
 }
-resource "huaweicloud_lts_stream" "test" {
-  group_id    = huaweicloud_lts_group.test.id
-  stream_name = "%[1]s"
+
+resource "huaweicloud_lts_stream" "%[2]s" {
+  group_id    = huaweicloud_lts_group.%[2]s.id
+  stream_name = "%[2]s"
 }
+`, rName, updateName)
+}
+
+func testAccElbLogTankConfig_basic(rName string) string {
+	return fmt.Sprintf(`
+%s
+
 resource "huaweicloud_elb_logtank" "test" {
   loadbalancer_id = huaweicloud_elb_loadbalancer.test.id
   log_group_id    = huaweicloud_lts_group.test.id
   log_topic_id    = huaweicloud_lts_stream.test.id
 }
-`, rName)
+`, testAccElbLogTankConfig_base(rName, "test"))
 }
 
 func testAccElbLogTankConfig_update(rName string) string {
 	return fmt.Sprintf(`
-data "huaweicloud_availability_zones" "test" {}
-resource "huaweicloud_vpc" "test" {
-  name = "%[1]s"
-  cidr = "192.168.0.0/16"
-}
-resource "huaweicloud_vpc_subnet" "test" {
-  name        = "%[1]s"
-  cidr        = "192.168.0.0/24"
-  gateway_ip  = "192.168.0.1"
-  vpc_id      = huaweicloud_vpc.test.id
-  ipv6_enable = true
-}
-resource "huaweicloud_elb_loadbalancer" "test" {
-  name            = "%[1]s"
-  ipv4_subnet_id  = huaweicloud_vpc_subnet.test.ipv4_subnet_id
-  ipv6_network_id = huaweicloud_vpc_subnet.test.id
-  availability_zone = [
-    data.huaweicloud_availability_zones.test.names[0]
-  ]
-}
-resource "huaweicloud_lts_group" "test_update" {
-  group_name  = "%[1]s"
-  ttl_in_days = 1
-}
-resource "huaweicloud_lts_stream" "test_update" {
-  group_id    = huaweicloud_lts_group.test_update.id
-  stream_name = "%[1]s"
-}
+%s
+
 resource "huaweicloud_elb_logtank" "test" {
   loadbalancer_id = huaweicloud_elb_loadbalancer.test.id
   log_group_id    = huaweicloud_lts_group.test_update.id
   log_topic_id    = huaweicloud_lts_stream.test_update.id
 }
-`, rName)
+`, testAccElbLogTankConfig_base(rName, "test_update"))
 }
--- huaweicloud/services/elb/resource_huaweicloud_elb_member.go
+++ huaweicloud/services/elb/resource_huaweicloud_elb_member.go
@@ -12,6 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	"github.com/chnsz/golangsdk/openstack/elb/v3/pools"
+
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
 )
@@ -88,10 +89,10 @@ func ResourceMemberV3() *schema.Resource {
 }
 
 func resourceMemberV3Create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	config := meta.(*config.Config)
-	elbClient, err := config.ElbV3Client(config.GetRegion(d))
+	cfg := meta.(*config.Config)
+	elbClient, err := cfg.ElbV3Client(cfg.GetRegion(d))
 	if err != nil {
-		return diag.Errorf("error creating elb client: %s", err)
+		return diag.Errorf("error creating ELB client: %s", err)
 	}
 
 	createOpts := pools.CreateMemberOpts{
@@ -119,10 +120,10 @@ func resourceMemberV3Create(ctx context.Context, d *schema.ResourceData, meta in
 }
 
 func resourceMemberV3Read(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	config := meta.(*config.Config)
-	elbClient, err := config.ElbV3Client(config.GetRegion(d))
+	cfg := meta.(*config.Config)
+	elbClient, err := cfg.ElbV3Client(cfg.GetRegion(d))
 	if err != nil {
-		return diag.Errorf("error creating elb client: %s", err)
+		return diag.Errorf("error creating ELB client: %s", err)
 	}
 
 	member, err := pools.GetMember(elbClient, d.Get("pool_id").(string), d.Id()).Extract()
@@ -138,7 +139,7 @@ func resourceMemberV3Read(_ context.Context, d *schema.ResourceData, meta interf
 		d.Set("subnet_id", member.SubnetID),
 		d.Set("address", member.Address),
 		d.Set("protocol_port", member.ProtocolPort),
-		d.Set("region", config.GetRegion(d)),
+		d.Set("region", cfg.GetRegion(d)),
 	)
 	if err := mErr.ErrorOrNil(); err != nil {
 		return diag.Errorf("error setting Dedicated ELB member fields: %s", err)
@@ -148,10 +149,10 @@ func resourceMemberV3Read(_ context.Context, d *schema.ResourceData, meta interf
 }
 
 func resourceMemberV3Update(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	config := meta.(*config.Config)
-	elbClient, err := config.ElbV3Client(config.GetRegion(d))
+	cfg := meta.(*config.Config)
+	elbClient, err := cfg.ElbV3Client(cfg.GetRegion(d))
 	if err != nil {
-		return diag.Errorf("error creating elb client: %s", err)
+		return diag.Errorf("error creating ELB client: %s", err)
 	}
 
 	var updateOpts pools.UpdateMemberOpts
@@ -173,8 +174,8 @@ func resourceMemberV3Update(ctx context.Context, d *schema.ResourceData, meta in
 }
 
 func resourceMemberV3Delete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	config := meta.(*config.Config)
-	elbClient, err := config.ElbV3Client(config.GetRegion(d))
+	cfg := meta.(*config.Config)
+	elbClient, err := cfg.ElbV3Client(cfg.GetRegion(d))
 	if err != nil {
 		return diag.Errorf("error creating elb client: %s", err)
 	}
@@ -187,7 +188,7 @@ func resourceMemberV3Delete(_ context.Context, d *schema.ResourceData, meta inte
 	return nil
 }
 
-func resourceELBMemberImport(_ context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+func resourceELBMemberImport(_ context.Context, d *schema.ResourceData, _ interface{}) ([]*schema.ResourceData, error) {
 	parts := strings.SplitN(d.Id(), "/", 2)
 	if len(parts) != 2 {
 		err := fmt.Errorf("invalid format specified for member. Format must be <pool_id>/<member_id>")
