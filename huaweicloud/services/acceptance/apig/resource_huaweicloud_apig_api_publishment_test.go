1a83bda468ac9eb6520db296671bc240c851ca75
Lance <Overlord52259@gmail.com>
2023-02-10 18:09:20 +0800

    refactor(apig/publishment): refactor publishment resource (#2646)

--- huaweicloud/services/acceptance/apig/resource_huaweicloud_apig_api_publishment_test.go
+++ huaweicloud/services/acceptance/apig/resource_huaweicloud_apig_api_publishment_test.go
@@ -4,29 +4,31 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/chnsz/golangsdk/openstack/apigw/dedicated/v2/apis"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/chnsz/golangsdk/openstack/apigw/dedicated/v2/apis"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/apig"
 )
 
-func getPublishmentResourceFunc(conf *config.Config, state *terraform.ResourceState) (interface{}, error) {
-	c, err := conf.ApigV2Client(acceptance.HW_REGION_NAME)
+func getPublishmentResourceFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	client, err := cfg.ApigV2Client(acceptance.HW_REGION_NAME)
 	if err != nil {
-		return nil, fmt.Errorf("error creating HuaweiCloud APIG v2 client: %s", err)
+		return nil, fmt.Errorf("error creating APIG v2 client: %s", err)
 	}
-	return apig.GetVersionHistories(c, state.Primary.Attributes["instance_id"], state.Primary.Attributes["env_id"],
+	return apig.GetVersionHistories(client, state.Primary.Attributes["instance_id"], state.Primary.Attributes["env_id"],
 		state.Primary.Attributes["api_id"])
 }
 
-func TestAccApigApiPublishmentV2_basic(t *testing.T) {
-	var histories []apis.ApiVersionInfo
+func TestAccApiPublishment_basic(t *testing.T) {
+	var (
+		histories []apis.ApiVersionInfo
 
-	// The dedicated instance name only allow letters, digits and underscores (_).
-	rName := acceptance.RandomAccResourceName()
-	resourceName := "huaweicloud_apig_api_publishment.test"
+		rName        = acceptance.RandomAccResourceName()
+		resourceName = "huaweicloud_apig_api_publishment.test"
+	)
 
 	rc := acceptance.InitResourceCheck(
 		resourceName,
@@ -37,13 +39,12 @@ func TestAccApigApiPublishmentV2_basic(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acceptance.TestAccPreCheck(t)
-			acceptance.TestAccPreCheckEpsID(t) // The creation of APIG instance needs the enterprise project ID.
 		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccApigApiPublishment_basic(rName),
+				Config: testAccApiPublishment_basic(rName),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
 					acceptance.TestCheckResourceAttrWithVariable(resourceName, "instance_id",
@@ -53,7 +54,7 @@ func TestAccApigApiPublishmentV2_basic(t *testing.T) {
 					acceptance.TestCheckResourceAttrWithVariable(resourceName, "api_id",
 						"${huaweicloud_apig_api.test.id}"),
 					resource.TestCheckResourceAttrSet(resourceName, "env_name"),
-					resource.TestCheckResourceAttrSet(resourceName, "publish_time"),
+					resource.TestCheckResourceAttrSet(resourceName, "published_at"),
 					resource.TestCheckResourceAttrSet(resourceName, "publish_id"),
 				),
 			},
@@ -66,14 +67,13 @@ func TestAccApigApiPublishmentV2_basic(t *testing.T) {
 	})
 }
 
-func testAccApigApiPublishment_basic(rName string) string {
+func testAccApiPublishment_basic(rName string) string {
 	return fmt.Sprintf(`
-%s
+%[1]s
 
 resource "huaweicloud_apig_environment" "test" {
-  name        = "%s"
   instance_id = huaweicloud_apig_instance.test.id
-  description = "Created by script"
+  name        = "%[2]s"
 }
 
 resource "huaweicloud_apig_api_publishment" "test" {
