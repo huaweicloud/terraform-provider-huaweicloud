398f8c89e8c20ca1d1c5c876b8b3822bf1e44832
Lance52259 <Overlord52259@gmail.com>
2023-02-15 14:10:30 +0800

    test(ecs/bind): add new concurrency test for server group

--- huaweicloud/resource_huaweicloud_compute_servergroup_test.go
+++ huaweicloud/resource_huaweicloud_compute_servergroup_test.go
@@ -87,6 +87,25 @@ func TestAccComputeServerGroup_members(t *testing.T) {
 	})
 }
 
+func TestAccComputeServerGroup_concurrency(t *testing.T) {
+	name := fmt.Sprintf("tf_acc_test_%s", acctest.RandString(5))
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeServerGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeServerGroup_concurrency(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckOutput("members_attached", "true"),
+					resource.TestCheckOutput("volumes_attached", "true"),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckComputeServerGroupDestroy(s *terraform.State) error {
 	config := testAccProvider.Meta().(*config.Config)
 	ecsClient, err := config.ComputeV1Client(HW_REGION_NAME)
@@ -211,4 +230,111 @@ resource "huaweicloud_compute_instance" "instance_1" {
   }
 }
 `, testAccCompute_data, rName, rName)
+}
+
+func testAccComputeServerGroup_concurrency(name string) string {
+	return fmt.Sprintf(`
+data "huaweicloud_availability_zones" "test" {}
+
+data "huaweicloud_compute_flavors" "test" {
+  availability_zone = data.huaweicloud_availability_zones.test.names[0]
+  performance_type  = "normal"
+  cpu_core_count    = 2
+  memory_size       = 4
+}
+
+data "huaweicloud_images_images" "test" {
+  flavor_id  = data.huaweicloud_compute_flavors.test.ids[0]
+  os         = "Ubuntu"
+  visibility = "public"
+}
+
+resource "huaweicloud_vpc" "test" {
+  name = "%[1]s"
+  cidr = "192.168.192.0/20"
+}
+
+resource "huaweicloud_vpc_subnet" "test" {
+  name       = "%[1]s"
+  vpc_id     = huaweicloud_vpc.test.id
+  cidr       = cidrsubnet(huaweicloud_vpc.test.cidr, 4, 1)
+  gateway_ip = cidrhost(cidrsubnet(huaweicloud_vpc.test.cidr, 4, 1), 1)
+}
+
+resource "huaweicloud_networking_secgroup" "test" {
+  name = "%[1]s"
+}
+
+resource "huaweicloud_kps_keypair" "test" {
+  name = "%[1]s"
+}
+
+resource "huaweicloud_compute_instance" "test" {
+  count = 2
+
+  name               = format("%[1]s_%%d", count.index)
+  flavor_id          = data.huaweicloud_compute_flavors.test.ids[0]
+  image_id           = data.huaweicloud_images_images.test.images[0].id
+  security_groups    = [huaweicloud_networking_secgroup.test.name]
+  availability_zone  = data.huaweicloud_availability_zones.test.names[0]
+  key_pair = huaweicloud_kps_keypair.test.name
+
+  network {
+    uuid = huaweicloud_vpc_subnet.test.id
+  }
+}
+
+resource "huaweicloud_compute_servergroup" "test" {
+  count = 2
+
+  name     = format("%[1]s_%%d", count.index)
+  policies = ["anti-affinity"]
+
+  members = [
+    huaweicloud_compute_instance.test[count.index].id,
+  ]
+}
+
+resource "huaweicloud_evs_volume" "test" {
+  count = 10
+
+  name              = format("%[1]s-%%d", count.index)
+  availability_zone = data.huaweicloud_availability_zones.test.names[0]
+
+  device_type = "SCSI"
+  volume_type = "SAS"
+  size        = 1000 * (count.index+1)
+  multiattach = true
+}
+
+resource "huaweicloud_compute_volume_attach" "attach_volumes_to_compute_test_1" {
+  count = 10
+
+  instance_id = huaweicloud_compute_instance.test[0].id
+  volume_id   = huaweicloud_evs_volume.test[count.index].id
+}
+
+resource "huaweicloud_compute_volume_attach" "attach_volumes_to_compute_test_2" {
+  count = 10
+
+  instance_id = huaweicloud_compute_instance.test[1].id
+  volume_id   = huaweicloud_evs_volume.test[count.index].id
+}
+
+locals {
+  attach_members_1 = huaweicloud_compute_servergroup.test[0].members
+  attach_members_2 = huaweicloud_compute_servergroup.test[1].members
+
+  attach_devices_1 = [for d in huaweicloud_compute_volume_attach.attach_volumes_to_compute_test_1[*].device : d != ""]
+  attach_devices_2 = [for d in huaweicloud_compute_volume_attach.attach_volumes_to_compute_test_2[*].device : d != ""]
+}
+
+output "members_attached" {
+  value = length(local.attach_members_1) == 1 && length(local.attach_members_2) == 1
+}
+
+output "volumes_attached" {
+  value = length(local.attach_devices_1) == 10 && length(local.attach_devices_2) == 10
+}
+`, name)
 }
