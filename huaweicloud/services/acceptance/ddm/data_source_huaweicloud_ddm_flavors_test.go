954a078c6d68afe5a5e872cc13ae9c04458b89dc
houpeng <hp_monkey@163.com>
2023-02-23 15:54:58 +0800

    add ddm flavors data source

--- huaweicloud/services/acceptance/ddm/data_source_huaweicloud_ddm_flavors_test.go
+++ huaweicloud/services/acceptance/ddm/data_source_huaweicloud_ddm_flavors_test.go
@@ -20,11 +20,10 @@ func TestAccDatasourceDdmFlavors_basic(t *testing.T) {
 				Config: testAccDatasourceDdmFlavors_basic(),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(rName, "flavor_groups.#", "1"),
-					resource.TestCheckResourceAttr(rName, "flavor_groups.0.groupType", "X86"),
-					resource.TestCheckResourceAttr(rName, "flavor_groups.0.flavors.0.type_code", "hws.resource.type.ddm"),
-					resource.TestCheckResourceAttr(rName, "flavor_groups.0.flavors.0.code", "ddm.c6.2xlarge.2"),
-					resource.TestCheckResourceAttr(rName, "flavor_groups.0.flavors.0.iaas_code", "c6s.2xlarge.2"),
+					resource.TestCheckResourceAttr(rName, "flavors.#", "3"),
+					resource.TestCheckResourceAttr(rName, "flavors.0.type_code", "hws.resource.type.ddm"),
+					resource.TestCheckResourceAttr(rName, "flavors.0.code", "ddm.c6.2xlarge.2"),
+					resource.TestCheckResourceAttr(rName, "flavors.0.iaas_code", "c6s.2xlarge.2"),
 				),
 			},
 		},
@@ -36,7 +35,8 @@ func testAccDatasourceDdmFlavors_basic() string {
 data "huaweicloud_ddm_flavors" "test" {
   engine_id  = "8de8fef1-d188-34ea-b7e2-26bbf958476b"
   group_type = "X86"
-  type_code  = "hws.resource.type.ddm"
+  //type_code  = "hws.resource.type.ddm"
+  //code       = "ddm.c6.2xlarge.2"
 }
 `
 }
--- huaweicloud/services/ddm/data_source_huaweicloud_ddm_flavors.go
+++ huaweicloud/services/ddm/data_source_huaweicloud_ddm_flavors.go
@@ -7,17 +7,15 @@ package ddm
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"github.com/chnsz/golangsdk"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"strings"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-
-	"github.com/chnsz/golangsdk/pagination"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
@@ -40,7 +38,7 @@ func DataSourceDdmFlavors() *schema.Resource {
 			},
 			"group_type": {
 				Type:        schema.TypeString,
-				Optional:    true,
+				Required:    true,
 				Description: `Specifies the compute resource architecture type.`,
 				ValidateFunc: validation.StringInSlice([]string{
 					"X86", "ARM",
@@ -61,36 +59,17 @@ func DataSourceDdmFlavors() *schema.Resource {
 				Optional:    true,
 				Description: `Specifies the VM flavor types recorded by the IaaS layer.`,
 			},
-			"flavor_groups": {
-				Type:        schema.TypeList,
-				Elem:        FlavorsFlavorGroupSchema(),
-				Computed:    true,
-				Description: `Indicates the list of DDM flavor.`,
-			},
-		},
-	}
-}
-
-func FlavorsFlavorGroupSchema() *schema.Resource {
-	sc := schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"group_type": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: `Indicates the compute resource architecture type.`,
-			},
 			"flavors": {
 				Type:        schema.TypeList,
-				Elem:        FlavorsFlavorGroupFlavorSchema(),
+				Elem:        FlavorsFlavorSchema(),
 				Computed:    true,
 				Description: `Indicates the list of DDM compute flavors.`,
 			},
 		},
 	}
-	return &sc
 }
 
-func FlavorsFlavorGroupFlavorSchema() *schema.Resource {
+func FlavorsFlavorSchema() *schema.Resource {
 	sc := schema.Resource{
 		Schema: map[string]*schema.Schema{
 			"id": {
@@ -137,28 +116,40 @@ func resourceDdmFlavorsRead(_ context.Context, d *schema.ResourceData, meta inte
 	getDdmFlavorsPath := getDdmFlavorsClient.Endpoint + getDdmFlavorsHttpUrl
 	getDdmFlavorsPath = strings.ReplaceAll(getDdmFlavorsPath, "{project_id}", getDdmFlavorsClient.ProjectID)
 
-	getDdmFlavorsQueryParams := buildGetDdmFlavorsQueryParams(d)
+	offset, limit, total := 0, 0, 0
+
+	getDdmFlavorsQueryParams := buildGetDdmFlavorsQueryParams(d, offset)
 	getDdmFlavorsPath += getDdmFlavorsQueryParams
-
-	getDdmFlavorsResp, err := pagination.ListAllItems(
-		getDdmFlavorsClient,
-		"offset",
-		getDdmFlavorsPath,
-		&pagination.QueryOpts{MarkerField: ""})
-
-	if err != nil {
-		return common.CheckDeletedDiag(d, err, "error retrieving DdmFlavors")
+	getInstanceOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		OkCodes: []int{
+			200,
+		},
 	}
 
-	getDdmFlavorsRespJson, err := json.Marshal(getDdmFlavorsResp)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-	fmt.Println(string(getDdmFlavorsRespJson))
-	var getDdmFlavorsRespBody interface{}
-	err = json.Unmarshal(getDdmFlavorsRespJson, &getDdmFlavorsRespBody)
-	if err != nil {
-		return diag.FromErr(err)
+	groupType := d.Get("group_type").(string)
+	typeCode := d.Get("type_code").(string)
+	code := d.Get("code").(string)
+	iaasCode := d.Get("iaas_code").(string)
+
+	allFlavors := make([]interface{}, 0)
+	var flavors []interface{}
+	for {
+		getDdmFlavorsResp, err := getDdmFlavorsClient.Request("GET", getDdmFlavorsPath, &getInstanceOpt)
+		if err != nil {
+			return common.CheckDeletedDiag(d, err, "error retrieving DdmFlavors")
+		}
+		getDdmFlavorsRespBody, err := utils.FlattenResponse(getDdmFlavorsResp)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		flavors, offset, limit, total = flattenGetFlavorsResponseBodyFlavorGroup(getDdmFlavorsRespBody, groupType,
+			typeCode, code, iaasCode)
+		allFlavors = append(allFlavors, flavors...)
+		if offset+limit >= total {
+			break
+		}
+		getDdmFlavorsPath = updatePathOffset(getDdmFlavorsPath, offset+limit)
 	}
 
 	dataSourceId, err := uuid.GenerateUUID()
@@ -167,43 +158,32 @@ func resourceDdmFlavorsRead(_ context.Context, d *schema.ResourceData, meta inte
 	}
 	d.SetId(dataSourceId)
 
-	groupType := d.Get("group_type").(string)
-	typeCode := d.Get("type_code").(string)
-	code := d.Get("code").(string)
-	iaasCode := d.Get("iaas_code").(string)
-
-	groups := flattenGetFlavorsResponseBodyFlavorGroup(getDdmFlavorsRespBody, groupType, typeCode, code, iaasCode)
-	bytes, err := json.Marshal(groups)
-	fmt.Println("groups info: ", string(bytes))
-
 	mErr = multierror.Append(
 		mErr,
 		d.Set("region", region),
-		d.Set("flavor_groups", flattenGetFlavorsResponseBodyFlavorGroup(getDdmFlavorsRespBody, groupType,
-			typeCode, code, iaasCode)),
+		d.Set("flavors", allFlavors),
 	)
 
 	return diag.FromErr(mErr.ErrorOrNil())
 }
 
-func flattenGetFlavorsResponseBodyFlavorGroup(resp interface{}, groupType, typeCode, code, iaasCode string) []interface{} {
+func flattenGetFlavorsResponseBodyFlavorGroup(resp interface{}, groupType, typeCode, code, iaasCode string) ([]interface{}, int, int, int) {
 	if resp == nil {
-		return nil
+		return nil, 0, 0, 0
 	}
 	curJson := utils.PathSearch("computeFlavorGroups", resp, make([]interface{}, 0))
 	curArray := curJson.([]interface{})
-	rst := make([]interface{}, 0, len(curArray))
 	for _, v := range curArray {
 		flavorGroupType := utils.PathSearch("groupType", v, nil)
 		if groupType != "" && groupType != flavorGroupType {
 			continue
 		}
-		rst = append(rst, map[string]interface{}{
-			"groupType": flavorGroupType,
-			"flavors":   flattenFlavorGroupFlavors(v, typeCode, code, iaasCode),
-		})
+		offset := utils.PathSearch("offset", v, nil).(float64)
+		limit := utils.PathSearch("limit", v, nil).(float64)
+		total := utils.PathSearch("total", v, nil).(float64)
+		return flattenFlavorGroupFlavors(v, typeCode, code, iaasCode), int(offset), int(limit), int(total)
 	}
-	return rst
+	return nil, 0, 0, 0
 }
 
 func flattenFlavorGroupFlavors(resp interface{}, typeCode, code, iaasCode string) []interface{} {
@@ -236,13 +216,19 @@ func flattenFlavorGroupFlavors(resp interface{}, typeCode, code, iaasCode string
 	return rst
 }
 
-func buildGetDdmFlavorsQueryParams(d *schema.ResourceData) string {
+func buildGetDdmFlavorsQueryParams(d *schema.ResourceData, offset int) string {
 	res := ""
 	if v, ok := d.GetOk("engine_id"); ok {
 		res = fmt.Sprintf("%s&engine_id=%v", res, v)
 	}
+	res = fmt.Sprintf("%s&offset=%v", res, offset)
 	if res != "" {
 		res = "?" + res[1:]
 	}
 	return res
+}
+
+func updatePathOffset(path string, offset int) string {
+	index := strings.Index(path, "offset")
+	return fmt.Sprintf("%s&offset=%v", path[:index], offset)
 }
