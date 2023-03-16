d790257ddae57eb2ff6cf91ee86d03dab668dc33
Lance52259 <Overlord52259@gmail.com>
2023-03-16 17:33:26 +0800

    feat(nat/snat): add new private snat rule resource

---
+++ docs/resources/nat_private_snat_rule.md
@@ -0,0 +1,80 @@
+---
+subcategory: "NAT Gateway (NAT)"
+---
+
+# huaweicloud_nat_private_snat_rule
+
+Manages an SNAT rule resource of the **private** NAT within HuaweiCloud.
+
+## Example Usage
+
+### Create an SNAT rule via subnet ID
+
+```hcl
+variable "gateway_id" {}
+variable "transit_ip_id" {}
+variable "subnet_id" {}
+
+resource "huaweicloud_nat_private_snat_rule" "test" {
+  gateway_id    = var.gateway_id
+  transit_ip_id = var.transit_ip_id
+  subnet_id     = var.subnet_id
+}
+```
+
+### Create an SNAT rule via CIDR
+
+```hcl
+variable "gateway_id" {}
+variable "transit_ip_id" {}
+variable "cidr_block" {}
+
+resource "huaweicloud_nat_private_snat_rule" "test" {
+  gateway_id    = var.gateway_id
+  transit_ip_id = var.transit_ip_id
+  cidr          = var.cidr_block
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region where the SNAT rule is located.  
+  If omitted, the provider-level region will be used. Changing this will create a new resource.
+
+* `gateway_id` - (Required, String, ForceNew) Specifies the private NAT gateway ID to which the SNAT rule belongs.  
+  Changing this will create a new resource.
+
+* `transit_ip_id` - (Required, String) Specifies the ID of the transit IP associated with SNAT rule.
+
+* `cidr` - (Optional, String, ForceNew) Specifies the CIDR block of the match rule.  
+  Changing this will create a new resource.  
+  Exactly one of `cidr` and `subnet_id` must be set.
+
+-> SNAT rules under the same private NAT gateway cannot have the same CIDR, but they can be proper subsets of other
+   CIDRs.
+
+* `subnet_id` - (Optional, String, ForceNew) Specifies the subnet ID of the match rule.  
+  Changing this will create a new resource.
+
+* `description` - (Optional, String) Specifies the description of the SNAT rule, which contain maximum of `255`
+  characters, and angle brackets (< and >) are not allowed.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID in UUID format.
+
+* `created_at` - The creation time of the SNAT rule.
+
+* `updated_at` - The latest update time of the SNAT rule.
+
+## Import
+
+SNAT rules can be imported using their `id`, e.g.
+
+```bash
+$ terraform import huaweicloud_nat_private_snat_rule.test df9b61e9-79c1-4a75-bfab-736e224ced71
+```
---
+++ huaweicloud/services/nat/resource_huaweicloud_nat_private_snat_rule.go
@@ -0,0 +1,179 @@
+package nat
+
+import (
+	"context"
+	"log"
+	"regexp"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	"github.com/chnsz/golangsdk/openstack/nat/v3/snats"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func ResourcePrivateSnatRule() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourcePrivateSnatRuleCreate,
+		ReadContext:   resourcePrivateSnatRuleRead,
+		UpdateContext: resourcePrivateSnatRuleUpdate,
+		DeleteContext: resourcePrivateSnatRuleDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				ForceNew:    true,
+				Description: "The region where the SNAT rule is located.",
+			},
+			"gateway_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "The private NAT gateway ID to which the SNAT rule belongs.",
+			},
+			"transit_ip_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The ID of the transit IP associated with SNAT rule.",
+			},
+			"cidr": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				ForceNew:     true,
+				ExactlyOneOf: []string{"subnet_id"},
+				Description:  "The CIDR block of the match rule.",
+			},
+			"subnet_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				ForceNew:    true,
+				Description: "The subnet ID of the match rule.",
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ValidateFunc: validation.All(
+					validation.StringLenBetween(0, 255),
+					validation.StringMatch(regexp.MustCompile(`^[^<>]*$`),
+						"The angle brackets (< and >) are not allowed."),
+				),
+				Description: "The description of the SNAT rule.",
+			},
+			"created_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The creation time of the SNAT rule.",
+			},
+			"updated_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The latest update time of the SNAT rule.",
+			},
+		},
+	}
+}
+
+func resourcePrivateSnatRuleCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	client, err := cfg.NatV3Client(cfg.GetRegion(d))
+	if err != nil {
+		return diag.Errorf("error creating NAT v3 client: %s", err)
+	}
+
+	opts := snats.CreateOpts{
+		GatewayId:    d.Get("gateway_id").(string),
+		TransitIpIds: []string{d.Get("transit_ip_id").(string)},
+		Cidr:         d.Get("cidr").(string),
+		SubnetId:     d.Get("subnet_id").(string),
+		Description:  d.Get("description").(string),
+	}
+
+	log.Printf("[DEBUG] The create options of the SNAT rule (Private NAT) is: %#v", opts)
+	resp, err := snats.Create(client, opts)
+	if err != nil {
+		return diag.Errorf("error creating SNAT rule (Private NAT): %s", err)
+	}
+	d.SetId(resp.ID)
+
+	return resourcePrivateSnatRuleRead(ctx, d, meta)
+}
+
+func resourcePrivateSnatRuleRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	client, err := cfg.NatV3Client(region)
+	if err != nil {
+		return diag.Errorf("error creating NAT v3 client: %s", err)
+	}
+
+	resp, err := snats.Get(client, d.Id())
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "Private SNAT rule")
+	}
+	mErr := multierror.Append(nil,
+		d.Set("region", region),
+		d.Set("gateway_id", resp.GatewayId),
+		d.Set("transit_ip_id", utils.PathSearch("[0].TransitIpId", resp.TransitIpAssociations, nil)),
+		d.Set("description", resp.Description),
+		d.Set("subnet_id", resp.SubnetId),
+		d.Set("cidr", resp.Cidr),
+		d.Set("created_at", resp.CreatedAt),
+		d.Set("updated_at", resp.UpdatedAt),
+	)
+
+	if err = mErr.ErrorOrNil(); err != nil {
+		return diag.Errorf("error saving resource fields of the private SNAT rule: %s", err)
+	}
+	return nil
+}
+
+func resourcePrivateSnatRuleUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	client, err := cfg.NatV3Client(region)
+	if err != nil {
+		return diag.Errorf("error creating NAT v3 client: %s", err)
+	}
+
+	ruleId := d.Id()
+	opts := snats.UpdateOpts{
+		TransitIpIds: []string{d.Get("transit_ip_id").(string)},
+		Description:  utils.String(d.Get("description").(string)),
+	}
+
+	_, err = snats.Update(client, ruleId, opts)
+	if err != nil {
+		return diag.Errorf("error updating private SNAT rule (%s): %s", ruleId, err)
+	}
+
+	return resourcePrivateSnatRuleRead(ctx, d, meta)
+}
+
+func resourcePrivateSnatRuleDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	client, err := cfg.NatV3Client(cfg.GetRegion(d))
+	if err != nil {
+		return diag.Errorf("error creating NAT v3 client: %s", err)
+	}
+
+	ruleId := d.Id()
+	err = snats.Delete(client, ruleId)
+	if err != nil {
+		return diag.Errorf("error deleting private SNAT rule (%s): %s", ruleId, err)
+	}
+
+	return nil
+}
