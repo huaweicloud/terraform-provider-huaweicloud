93a58591ad8c7eff72c30bcdce7fcb6abb99d273
Zhenguo Niu <niuzhenguo@huawei.com>
2023-03-24 07:32:31 +0000

    feat(secgroups): add id to secgroups data source

--- huaweicloud/services/acceptance/vpc/data_source_huaweicloud_networking_secgroups_test.go
+++ huaweicloud/services/acceptance/vpc/data_source_huaweicloud_networking_secgroups_test.go
@@ -66,6 +66,35 @@ func TestAccNetworkingSecGroupsDataSource_description(t *testing.T) {
 	})
 }
 
+func TestAccNetworkingSecGroupsDataSource_id(t *testing.T) {
+	rName := acceptance.RandomAccResourceName()
+	dataSourceName := "data.huaweicloud_networking_secgroups.test"
+
+	dc := acceptance.InitDataSourceCheck(dataSourceName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetworkingSecGroupsDataSource_id(rName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(dataSourceName, "security_groups.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "security_groups.0.name", rName),
+					resource.TestCheckResourceAttr(dataSourceName, "security_groups.0.description",
+						"[Acc Test] The security group created by Terraform."),
+					resource.TestCheckResourceAttrPair(dataSourceName, "security_groups.0.id",
+						"huaweicloud_networking_secgroup.test", "id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "security_groups.0.enterprise_project_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "security_groups.0.created_at"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "security_groups.0.updated_at"),
+				),
+			},
+		},
+	})
+}
+
 func testAccNetworkingSecGroupsDataSource_base(rName string) string {
 	return fmt.Sprintf(`
 resource "huaweicloud_networking_secgroup" "test" {
@@ -91,6 +120,16 @@ func testAccNetworkingSecGroupV3DataSource_description(rName string) string {
 
 data "huaweicloud_networking_secgroups" "test" {
   description = "[Acc Test]"
+}
+`, testAccNetworkingSecGroupsDataSource_base(rName))
+}
+
+func testAccNetworkingSecGroupsDataSource_id(rName string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_networking_secgroups" "test" {
+  id = huaweicloud_networking_secgroup.test.id
 }
 `, testAccNetworkingSecGroupsDataSource_base(rName))
 }
