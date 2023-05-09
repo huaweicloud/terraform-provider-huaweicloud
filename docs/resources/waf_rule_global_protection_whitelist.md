fa1187f101f2ecbcef24403e7a6719b39dd9fc76
luxiaohang <deerhang@163.com>
2023-05-09 14:23:29 +0800

    feat(WAF): waf support global protection whitelist rule resource

---
+++ docs/resources/waf_rule_global_protection_whitelist.md
@@ -0,0 +1,179 @@
+---
+subcategory: "Web Application Firewall (WAF)"
+---
+
+# huaweicloud_waf_rule_global_protection_whitelist
+
+Manages a WAF global protection whitelist rule resource within HuaweiCloud.
+
+-> **NOTE:** All WAF resources depend on WAF instances, and the WAF instances need to be purchased before they can be
+used. The global protection whitelist rule resource can be used in Cloud Mode, Dedicated Mode.
+
+## Example Usage
+
+### WAF Global Protection Whitelist Rule with specified domain names
+
+```hcl
+variable "policy_id" {}
+variable "enterprise_project_id" {}
+variable "domains" {
+  type = list(string)
+}
+
+resource "huaweicloud_waf_rule_global_protection_whitelist" "test" {
+  policy_id             = var.policy_id
+  domains               = var.domains
+  enterprise_project_id = var.enterprise_project_id
+  ignore_waf_protection = "xss;webshell"
+  advanced_field        = "params"
+  advanced_content      = "test_content"
+  description           = "test description"
+
+  conditions {
+    field    = "ip"
+    logic    = "equal"
+    content  = "192.168.0.2"
+    subfield = "x-forwarded-for"
+  }
+}
+```
+
+### WAF Global Protection Whitelist Rule with all domain names
+
+```hcl
+variable "policy_id" {}
+variable "enterprise_project_id" {}
+
+resource "huaweicloud_waf_rule_global_protection_whitelist" "test" {
+  policy_id             = var.policy_id
+  enterprise_project_id = var.enterprise_project_id
+  domains               = []
+  ignore_waf_protection = "xss;webshell"
+  advanced_field        = "params"
+  advanced_content      = "test_content"
+  description           = "test description"
+
+  conditions {
+    field    = "params"
+    logic    = "contain"
+    content  = "test content"
+    subfield = "test_subfield"
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region in which to create the resource.
+  If omitted, the provider-level region will be used. Changing this parameter will create a new resource.
+
+* `policy_id` - (Required, String, ForceNew) Specifies the policy ID of WAF global protection whitelist rule.
+
+  Changing this parameter will create a new resource.
+
+* `domains` - (Required, List) Specifies the protected domain name bound with the policy or manually enter a single
+  domain name corresponding to the wildcard domain name.
+  If the array length is 0, the rule takes effect for all domain names or websites.
+
+* `ignore_waf_protection` - (Required, String) Specifies the rules that need to be ignored. You can provide multiple
+  items and separate them with semicolons (;).
+
+  + If you want to block a specific built-in rule, the value of this parameter is the rule ID.
+  To query the rule ID, go to the WAF console, choose **Policies** and click the target policy name. On the displayed
+  page, in the **Basic Web Protection** area, select the **Protection Rules** tab, and view the ID of the specific rule.
+  You can also query the rule ID in the event details.
+
+  + If you want to mask a type of basic web protection rules, set this parameter to the name of the type of basic web
+  protection rules. Valid values are: **xss**(XSS attacks), **webshell**(Web shells), **vuln**(Other types of attacks),
+  **sqli**(SQL injection attack), **robot**(Malicious crawlers), **rfi**(Remote file inclusion),
+  **lfi**(Local file inclusion), **cmdi**(Command injection attack).
+
+  + To bypass the basic web protection, set this parameter to **all**.
+
+  + To bypass all WAF protection, set this parameter to **bypass**.
+
+* `conditions` - (Required, List) Specifies the match condition list.
+  The [conditions](#RuleGlobalProtectionWhitelist_conditions) structure is documented below.
+
+* `enterprise_project_id` - (Optional, String, ForceNew) Specifies the enterprise project ID of WAF global protection
+  whitelist rule.
+
+  Changing this parameter will create a new resource.
+
+* `advanced_field` - (Optional, String) Specifies the advanced field to ignore attacks of a specific field.
+  After you add the rule, WAF will stop intercepting attack events of the specified field.
+  The following fields are supported: **params**, **cookie**, **header**, **body** and **multipart**.
+
+* `advanced_content` - (Optional, String) Specifies the advanced content value to ignore. This parameter is valid only
+  when `advanced_field` is set to **params**, **cookie** or **header**.
+  If not specified, WAF will ignore all attack events of the specific field.
+
+* `description` - (Optional, String) Specifies the description of WAF global protection whitelist rule.
+
+* `status` - (Optional, Int) Specifies the status of WAF global protection whitelist rule.
+  Valid values are as follows:
+  + **0**: Disabled.
+  + **1**: Enabled.
+
+  The default value is **1**.
+
+<a name="RuleGlobalProtectionWhitelist_conditions"></a>
+The `conditions` block supports:
+
+* `field` - (Required, String) Specifies the field type. The value can be **ip**, **url**, **params**, **cookie**
+  or **header**.
+
+* `logic` - (Required, String) Specifies the condition matching logic.
+
+  + If `field` is set to **ip**: Valid values are **equal** and **not_equal**.
+
+  + If `field` is set to **url** or **header** or **params** or **cookie**: Valid values are **equal**, **not_equal**,
+  **contain**, **not_contain**, **prefix**, **not_prefix**, **suffix** and **not_suffix**.
+
+* `content` - (Required, String) Specifies the content of the match condition.
+
+  + If `field` is set to **ip**, the value must be an IP address or IP address range.
+
+  + If `field` is set to **url**, the value must be in the standard URL format.
+
+  + If `field` is set to **params** or **cookie** or **header**, the content format is not limited.
+
+* `subfield` - (Optional, String) Specifies the subfield of the condition.
+
+  + If `field` is set to **ip** and the subfield is the client IP address, the parameter is not required.
+
+  + If `field` is set to **ip** and the subfield is X-Forwarded-For, the parameter is required and the value should be
+  **x-forwarded-for**.
+
+  + If `field` is set to **params**, **header** or **cookie**, the parameter is required and the value is user-defined.
+
+  + If `field` is set to **url**, the parameter cannot be specified.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The ID of a global protection whitelist rule.
+
+## Import
+
+There are two ways to import state, include `Without Enterprise Project ID` and `With Enterprise Project ID`.
+
+### Without Enterprise Project ID
+
+The waf global protection whitelist rule can be imported using `policy_id`, `rule_id`, separated by a slash, e.g.
+
+```bash
+$ terraform import huaweicloud_waf_rule_global_protection_whitelist.test <policy_id>/<rule_id>
+```
+
+### With Enterprise Project ID
+
+The waf global protection whitelist rule can be imported using `policy_id`, `rule_id`, `enterprise_project_id`,
+separated by slashes, e.g.
+
+```bash
+$ terraform import huaweicloud_waf_rule_global_protection_whitelist.test <policy_id>/<rule_id>/<enterprise_project_id>
+```
---
+++ huaweicloud/services/waf/resource_huaweicloud_waf_rule_global_protection_whitelist.go
@@ -0,0 +1,400 @@
+// ---------------------------------------------------------------
+// *** AUTO GENERATED CODE ***
+// @Product WAF
+// ---------------------------------------------------------------
+
+package waf
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/jmespath/go-jmespath"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func ResourceRuleGlobalProtectionWhitelist() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceRuleGlobalProtectionWhitelistCreate,
+		UpdateContext: resourceRuleGlobalProtectionWhitelistUpdate,
+		ReadContext:   resourceRuleGlobalProtectionWhitelistRead,
+		DeleteContext: resourceRuleGlobalProtectionWhitelistDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceWAFRuleImportState,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"policy_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the policy ID of WAF global protection whitelist rule.`,
+			},
+			"domains": {
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Required: true,
+				Description: `Specifies the protected domain name bound with the policy or manually enter a single
+domain name corresponding to the wildcard domain name.`,
+			},
+			"ignore_waf_protection": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: `Specifies the ignore waf protection rule.`,
+			},
+			"conditions": {
+				Type:        schema.TypeList,
+				Elem:        globalProtectionWhitelistConditionsSchema(),
+				Required:    true,
+				Description: `Specifies the match condition list.`,
+			},
+			"enterprise_project_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+				Description: `Specifies the enterprise project ID of WAF global protection whitelist rule.`,
+			},
+			"advanced_field": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Description: `Specifies the advanced field. The following fields are supported:
+**params**, **cookie**, **header**, **body** and **multipart**.`,
+			},
+			"advanced_content": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `Specifies the advanced content.`,
+			},
+			"description": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `Specifies the description of WAF global protection whitelist rule.`,
+			},
+			"status": {
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      1,
+				ValidateFunc: validation.IntInSlice([]int{0, 1}),
+				Description:  `Specifies the status of WAF global protection whitelist rule.`,
+			},
+		},
+	}
+}
+
+func globalProtectionWhitelistConditionsSchema() *schema.Resource {
+	sc := schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"field": {
+				Type:     schema.TypeString,
+				Required: true,
+				Description: `Specifies the field type. The value can be **ip**, **url**, **params**, **cookie** 
+or **header**.`,
+			},
+			"logic": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: `Specifies the condition matching logic.`,
+			},
+			"content": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: `Specifies the content of the match condition.`,
+			},
+			"subfield": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				Description: `Specifies the subfield of the condition.`,
+			},
+		},
+	}
+	return &sc
+}
+
+func resourceRuleGlobalProtectionWhitelistCreate(ctx context.Context, d *schema.ResourceData,
+	meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		createHttpUrl = "v1/{project_id}/waf/policy/{policy_id}/ignore"
+		createProduct = "waf"
+	)
+	createClient, err := cfg.NewServiceClient(createProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating WAF Client: %s", err)
+	}
+
+	createPath := createClient.Endpoint + createHttpUrl
+	createPath = strings.ReplaceAll(createPath, "{project_id}", createClient.ProjectID)
+	createPath = strings.ReplaceAll(createPath, "{policy_id}", fmt.Sprintf("%v", d.Get("policy_id")))
+	createPath += buildQueryParams(d, cfg)
+
+	createOpt := golangsdk.RequestOpts{
+		MoreHeaders: map[string]string{
+			"Content-Type": "application/json;charset=utf8",
+		},
+		KeepResponseBody: true,
+		OkCodes: []int{
+			200,
+		},
+	}
+	createOpt.JSONBody = utils.RemoveNil(buildCreateOrUpdateGlobalProtectionWhitelistBodyParams(d))
+	createResp, err := createClient.Request("POST", createPath, &createOpt)
+	if err != nil {
+		return diag.Errorf("error creating RuleGlobalProtectionWhitelist: %s", err)
+	}
+
+	createRespBody, err := utils.FlattenResponse(createResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	id, err := jmespath.Search("id", createRespBody)
+	if err != nil {
+		return diag.Errorf("error creating RuleGlobalProtectionWhitelist: ID is not found in API response")
+	}
+	d.SetId(id.(string))
+
+	if d.Get("status").(int) == 0 {
+		if err := updateRuleStatus(createClient, d, cfg, "ignore"); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	return resourceRuleGlobalProtectionWhitelistRead(ctx, d, meta)
+}
+
+func buildCreateOrUpdateGlobalProtectionWhitelistBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"rule":        utils.ValueIngoreEmpty(d.Get("ignore_waf_protection")),
+		"description": utils.ValueIngoreEmpty(d.Get("description")),
+		"domain":      d.Get("domains"),
+		"conditions":  buildGlobalProtectionWhitelistConditions(d.Get("conditions")),
+		"mode":        1,
+	}
+
+	if v, ok := d.GetOk("advanced_field"); ok {
+		advancedMap := map[string]interface{}{
+			"index": v,
+			// the WAF api parameter `contents` needs an empty array as a default value
+			"contents": []string{},
+		}
+		if v1, ok1 := d.GetOk("advanced_content"); ok1 {
+			advancedMap["contents"] = utils.ExpandToStringList([]interface{}{v1})
+		}
+		bodyParams["advanced"] = advancedMap
+	}
+
+	return bodyParams
+}
+
+func buildGlobalProtectionWhitelistConditions(rawParams interface{}) []map[string]interface{} {
+	if rawArray, ok := rawParams.([]interface{}); ok {
+		if len(rawArray) == 0 {
+			return nil
+		}
+
+		rst := make([]map[string]interface{}, len(rawArray))
+		for i, v := range rawArray {
+			raw := v.(map[string]interface{})
+			rst[i] = map[string]interface{}{
+				"category":        utils.ValueIngoreEmpty(raw["field"]),
+				"logic_operation": utils.ValueIngoreEmpty(raw["logic"]),
+				"contents":        buildGlobalProtectionWhitelistContents(raw),
+				"index":           utils.ValueIngoreEmpty(raw["subfield"]),
+			}
+		}
+		return rst
+	}
+	return nil
+}
+
+func buildGlobalProtectionWhitelistContents(raw map[string]interface{}) []string {
+	var contents []string
+	if content := utils.ValueIngoreEmpty(raw["content"]); content != nil {
+		contents = append(contents, content.(string))
+	}
+	return contents
+}
+
+func resourceRuleGlobalProtectionWhitelistRead(_ context.Context, d *schema.ResourceData,
+	meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var mErr *multierror.Error
+
+	var (
+		getHttpUrl = "v1/{project_id}/waf/policy/{policy_id}/ignore/{rule_id}"
+		getProduct = "waf"
+	)
+	getClient, err := cfg.NewServiceClient(getProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating WAF Client: %s", err)
+	}
+
+	getPath := getClient.Endpoint + getHttpUrl
+	getPath = strings.ReplaceAll(getPath, "{project_id}", getClient.ProjectID)
+	getPath = strings.ReplaceAll(getPath, "{policy_id}", fmt.Sprintf("%v", d.Get("policy_id")))
+	getPath = strings.ReplaceAll(getPath, "{rule_id}", d.Id())
+	getPath += buildQueryParams(d, cfg)
+
+	getOpt := golangsdk.RequestOpts{
+		MoreHeaders: map[string]string{
+			"Content-Type": "application/json;charset=utf8",
+		},
+		KeepResponseBody: true,
+		OkCodes: []int{
+			200,
+		},
+	}
+	getResp, err := getClient.Request("GET", getPath, &getOpt)
+
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "error retrieving RuleGlobalProtectionWhitelist")
+	}
+
+	getRespBody, err := utils.FlattenResponse(getResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	mErr = multierror.Append(
+		mErr,
+		d.Set("region", region),
+		d.Set("policy_id", utils.PathSearch("policyid", getRespBody, nil)),
+		d.Set("description", utils.PathSearch("description", getRespBody, nil)),
+		d.Set("status", utils.PathSearch("status", getRespBody, nil)),
+		d.Set("ignore_waf_protection", utils.PathSearch("rule", getRespBody, nil)),
+		d.Set("conditions", flattenGlobalProtectionWhitelistConditions(getRespBody)),
+		d.Set("advanced_field", utils.PathSearch("advanced.index", getRespBody, nil)),
+		d.Set("advanced_content", utils.PathSearch("advanced.contents|[0]", getRespBody, nil)),
+		d.Set("domains", utils.PathSearch("domain", getRespBody, nil)),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func flattenGlobalProtectionWhitelistConditions(resp interface{}) []interface{} {
+	if resp == nil {
+		return nil
+	}
+	curJson := utils.PathSearch("conditions", resp, make([]interface{}, 0))
+	curArray := curJson.([]interface{})
+	rst := make([]interface{}, 0, len(curArray))
+	for _, v := range curArray {
+		rst = append(rst, map[string]interface{}{
+			"field":    utils.PathSearch("category", v, nil),
+			"subfield": utils.PathSearch("index", v, nil),
+			"logic":    utils.PathSearch("logic_operation", v, nil),
+			"content":  utils.PathSearch("contents|[0]", v, nil),
+		})
+	}
+	return rst
+}
+
+func resourceRuleGlobalProtectionWhitelistUpdate(ctx context.Context, d *schema.ResourceData,
+	meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	updateClient, err := cfg.NewServiceClient("waf", region)
+	if err != nil {
+		return diag.Errorf("error creating WAF Client: %s", err)
+	}
+
+	updateRuleGlobalProtectionWhitelistChanges := []string{
+		"policy_id",
+		"domains",
+		"ignore_waf_protection",
+		"conditions",
+		"advanced_field",
+		"advanced_content",
+		"description",
+	}
+
+	if d.HasChanges(updateRuleGlobalProtectionWhitelistChanges...) {
+		updateHttpUrl := "v1/{project_id}/waf/policy/{policy_id}/ignore/{rule_id}"
+
+		policyID := fmt.Sprintf("%v", d.Get("policy_id"))
+		updatePath := updateClient.Endpoint + updateHttpUrl
+		updatePath = strings.ReplaceAll(updatePath, "{project_id}", updateClient.ProjectID)
+		updatePath = strings.ReplaceAll(updatePath, "{policy_id}", policyID)
+		updatePath = strings.ReplaceAll(updatePath, "{rule_id}", d.Id())
+		updatePath += buildQueryParams(d, cfg)
+
+		updateOpt := golangsdk.RequestOpts{
+			MoreHeaders: map[string]string{
+				"Content-Type": "application/json;charset=utf8",
+			},
+			KeepResponseBody: true,
+			OkCodes: []int{
+				200,
+			},
+		}
+		updateOpt.JSONBody = utils.RemoveNil(buildCreateOrUpdateGlobalProtectionWhitelistBodyParams(d))
+		_, err := updateClient.Request("PUT", updatePath, &updateOpt)
+		if err != nil {
+			return diag.Errorf("error updating RuleGlobalProtectionWhitelist: %s", err)
+		}
+	}
+
+	if d.HasChange("status") {
+		if err := updateRuleStatus(updateClient, d, cfg, "ignore"); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	return resourceRuleGlobalProtectionWhitelistRead(ctx, d, meta)
+}
+
+func resourceRuleGlobalProtectionWhitelistDelete(_ context.Context, d *schema.ResourceData,
+	meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		deleteHttpUrl = "v1/{project_id}/waf/policy/{policy_id}/ignore/{rule_id}"
+		deleteProduct = "waf"
+	)
+	deleteClient, err := cfg.NewServiceClient(deleteProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating WAF Client: %s", err)
+	}
+
+	deletePath := deleteClient.Endpoint + deleteHttpUrl
+	deletePath = strings.ReplaceAll(deletePath, "{project_id}", deleteClient.ProjectID)
+	deletePath = strings.ReplaceAll(deletePath, "{policy_id}", fmt.Sprintf("%v", d.Get("policy_id")))
+	deletePath = strings.ReplaceAll(deletePath, "{rule_id}", d.Id())
+	deletePath += buildQueryParams(d, cfg)
+
+	deleteOpt := golangsdk.RequestOpts{
+		MoreHeaders: map[string]string{
+			"Content-Type": "application/json;charset=utf8",
+		},
+		KeepResponseBody: true,
+		OkCodes: []int{
+			200,
+		},
+	}
+	_, err = deleteClient.Request("DELETE", deletePath, &deleteOpt)
+	if err != nil {
+		return diag.Errorf("error deleting RuleGlobalProtectionWhitelist: %s", err)
+	}
+	return nil
+}
