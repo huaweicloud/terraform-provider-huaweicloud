b18e6bbd0312f50bbd2f821933d36b47e33de1c9
shichangkuo <shichangkuo90@gmail.com>
2023-05-12 18:41:08 +0800

    chore: get rid of unused functions (#3010)
    * chore: get rid of unused functions
    
    * fix lint issue

--- huaweicloud/provider_test.go
+++ huaweicloud/provider_test.go
@@ -12,45 +12,35 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils/fmtp"
 )
 
+//nolint:revive
 var (
-	HW_AVAILABILITY_ZONE            = os.Getenv("HW_AVAILABILITY_ZONE")
-	HW_DEPRECATED_ENVIRONMENT       = os.Getenv("HW_DEPRECATED_ENVIRONMENT")
-	HW_DEST_PROJECT_ID              = os.Getenv("HW_DEST_PROJECT_ID")
-	HW_DEST_REGION                  = os.Getenv("HW_DEST_REGION")
-	HW_DNS_ENVIRONMENT              = os.Getenv("HW_DNS_ENVIRONMENT")
-	HW_EXTGW_ID                     = os.Getenv("HW_EXTGW_ID")
-	HW_FLAVOR_ID                    = os.Getenv("HW_FLAVOR_ID")
-	HW_FLAVOR_NAME                  = os.Getenv("HW_FLAVOR_NAME")
-	HW_IMAGE_ID                     = os.Getenv("HW_IMAGE_ID")
-	HW_IMAGE_NAME                   = os.Getenv("HW_IMAGE_NAME")
-	HW_NETWORK_ID                   = os.Getenv("HW_NETWORK_ID")
-	HW_SUBNET_ID                    = os.Getenv("HW_SUBNET_ID")
-	HW_POOL_NAME                    = os.Getenv("HW_POOL_NAME")
-	HW_REGION_NAME                  = os.Getenv("HW_REGION_NAME")
-	HW_CUSTOM_REGION_NAME           = os.Getenv("HW_CUSTOM_REGION_NAME")
-	HW_ACCESS_KEY                   = os.Getenv("HW_ACCESS_KEY")
-	HW_SECRET_KEY                   = os.Getenv("HW_SECRET_KEY")
-	HW_SRC_ACCESS_KEY               = os.Getenv("HW_SRC_ACCESS_KEY")
-	HW_SRC_SECRET_KEY               = os.Getenv("HW_SRC_SECRET_KEY")
-	HW_VPC_ID                       = os.Getenv("HW_VPC_ID")
-	HW_CCI_NAMESPACE                = os.Getenv("HW_CCI_NAMESPACE")
-	HW_PROJECT_ID                   = os.Getenv("HW_PROJECT_ID")
-	HW_DOMAIN_ID                    = os.Getenv("HW_DOMAIN_ID")
-	HW_DOMAIN_NAME                  = os.Getenv("HW_DOMAIN_NAME")
-	HW_DWS_ENVIRONMENT              = os.Getenv("HW_DWS_ENVIRONMENT")
-	HW_MRS_ENVIRONMENT              = os.Getenv("HW_MRS_ENVIRONMENT")
-	HW_DMS_ENVIRONMENT              = os.Getenv("HW_DMS_ENVIRONMENT")
-	HW_NAT_ENVIRONMENT              = os.Getenv("HW_NAT_ENVIRONMENT")
-	HW_KMS_ENVIRONMENT              = os.Getenv("HW_KMS_ENVIRONMENT")
-	HW_CCI_ENVIRONMENT              = os.Getenv("HW_CCI_ENVIRONMENT")
-	HW_CLOUDTABLE_AVAILABILITY_ZONE = os.Getenv("HW_CLOUDTABLE_AVAILABILITY_ZONE")
-	HW_CDN_DOMAIN_NAME              = os.Getenv("HW_CDN_DOMAIN_NAME")
-	HW_CDN_CERT_PATH                = os.Getenv("HW_CDN_CERT_PATH")
-	HW_CDN_PRIVATE_KEY_PATH         = os.Getenv("HW_CDN_PRIVATE_KEY_PATH")
-	HW_ADMIN                        = os.Getenv("HW_ADMIN")
-	HW_ENTERPRISE_PROJECT_ID_TEST   = os.Getenv("HW_ENTERPRISE_PROJECT_ID_TEST")
-	HW_USER_ID                      = os.Getenv("HW_USER_ID")
-	HW_CHARGING_MODE                = os.Getenv("HW_CHARGING_MODE")
+	HW_AVAILABILITY_ZONE          = os.Getenv("HW_AVAILABILITY_ZONE")
+	HW_DEPRECATED_ENVIRONMENT     = os.Getenv("HW_DEPRECATED_ENVIRONMENT")
+	HW_EXTGW_ID                   = os.Getenv("HW_EXTGW_ID")
+	HW_FLAVOR_ID                  = os.Getenv("HW_FLAVOR_ID")
+	HW_FLAVOR_NAME                = os.Getenv("HW_FLAVOR_NAME")
+	HW_IMAGE_ID                   = os.Getenv("HW_IMAGE_ID")
+	HW_IMAGE_NAME                 = os.Getenv("HW_IMAGE_NAME")
+	HW_NETWORK_ID                 = os.Getenv("HW_NETWORK_ID")
+	HW_SUBNET_ID                  = os.Getenv("HW_SUBNET_ID")
+	HW_POOL_NAME                  = os.Getenv("HW_POOL_NAME")
+	HW_REGION_NAME                = os.Getenv("HW_REGION_NAME")
+	HW_ACCESS_KEY                 = os.Getenv("HW_ACCESS_KEY")
+	HW_SECRET_KEY                 = os.Getenv("HW_SECRET_KEY")
+	HW_VPC_ID                     = os.Getenv("HW_VPC_ID")
+	HW_CCI_NAMESPACE              = os.Getenv("HW_CCI_NAMESPACE")
+	HW_PROJECT_ID                 = os.Getenv("HW_PROJECT_ID")
+	HW_DOMAIN_ID                  = os.Getenv("HW_DOMAIN_ID")
+	HW_DOMAIN_NAME                = os.Getenv("HW_DOMAIN_NAME")
+	HW_MRS_ENVIRONMENT            = os.Getenv("HW_MRS_ENVIRONMENT")
+	HW_KMS_ENVIRONMENT            = os.Getenv("HW_KMS_ENVIRONMENT")
+	HW_CCI_ENVIRONMENT            = os.Getenv("HW_CCI_ENVIRONMENT")
+	HW_CDN_DOMAIN_NAME            = os.Getenv("HW_CDN_DOMAIN_NAME")
+	HW_CDN_CERT_PATH              = os.Getenv("HW_CDN_CERT_PATH")
+	HW_CDN_PRIVATE_KEY_PATH       = os.Getenv("HW_CDN_PRIVATE_KEY_PATH")
+	HW_ENTERPRISE_PROJECT_ID_TEST = os.Getenv("HW_ENTERPRISE_PROJECT_ID_TEST")
+	HW_USER_ID                    = os.Getenv("HW_USER_ID")
+	HW_CHARGING_MODE              = os.Getenv("HW_CHARGING_MODE")
 )
 
 var testAccProviders map[string]*schema.Provider
@@ -70,63 +60,15 @@ func testAccPreCheck(t *testing.T) {
 	}
 }
 
-func testAccPrecheckCustomRegion(t *testing.T) {
-	if HW_CUSTOM_REGION_NAME == "" {
-		t.Skip("This environment does not support custom region tests")
-	}
-}
-
 func testAccPreCheckDeprecated(t *testing.T) {
 	if HW_DEPRECATED_ENVIRONMENT == "" {
 		t.Skip("This environment does not support deprecated tests")
 	}
 }
 
-func testAccPreCheckAdminOnly(t *testing.T) {
-	if HW_ADMIN == "" {
-		t.Skip("Skipping test because it requires the admin privileges")
-	}
-}
-
-func testAccPreCheckDNS(t *testing.T) {
-	if HW_DNS_ENVIRONMENT == "" {
-		t.Skip("This environment does not support DNS tests")
-	}
-}
-
-func testAccPreCheckULB(t *testing.T) {
-	if HW_SUBNET_ID == "" {
-		t.Skip("HW_SUBNET_ID must be set for LB acceptance tests")
-	}
-}
-
-func testAccPreCheckOBS(t *testing.T) {
-	if HW_ACCESS_KEY == "" || HW_SECRET_KEY == "" {
-		t.Skip("HW_ACCESS_KEY and HW_SECRET_KEY must be set for OBS acceptance tests")
-	}
-}
-
-func testAccPreCheckDws(t *testing.T) {
-	if HW_DWS_ENVIRONMENT == "" {
-		t.Skip("This environment does not support DWS tests")
-	}
-}
-
-func testAccPreCheckCloudTable(t *testing.T) {
-	if HW_CLOUDTABLE_AVAILABILITY_ZONE == "" {
-		t.Skip("HW_CLOUDTABLE_AVAILABILITY_ZONE must be set for CloudTable tests")
-	}
-}
-
 func testAccPreCheckMrs(t *testing.T) {
 	if HW_MRS_ENVIRONMENT == "" {
 		t.Skip("This environment does not support MRS tests")
-	}
-}
-
-func testAccPreCheckDms(t *testing.T) {
-	if HW_DMS_ENVIRONMENT == "" {
-		t.Skip("This environment does not support DMS tests")
 	}
 }
 
@@ -160,33 +102,9 @@ func testAccPreCheckCCI(t *testing.T) {
 	}
 }
 
-func testAccPreCheckDestProject(t *testing.T) {
-	if HW_DEST_REGION == "" || HW_DEST_PROJECT_ID == "" {
-		t.Skip("This environment does not support destination project tests")
-	}
-}
-
 func testAccPreCheckEpsID(t *testing.T) {
 	if HW_ENTERPRISE_PROJECT_ID_TEST == "" {
 		t.Skip("This environment does not support Enterprise Project ID tests")
-	}
-}
-
-func testAccPreCheckProject(t *testing.T) {
-	if HW_ENTERPRISE_PROJECT_ID_TEST != "" {
-		t.Skip("This environment does not support project tests")
-	}
-}
-
-func testAccPreCheckProjectID(t *testing.T) {
-	if HW_PROJECT_ID == "" {
-		t.Skip("HW_PROJECT_ID must be set for acceptance tests")
-	}
-}
-
-func testAccAsConfigPreCheck(t *testing.T) {
-	if HW_FLAVOR_ID == "" {
-		t.Skip("HW_FLAVOR_ID must be set for acceptance tests")
 	}
 }
 
--- huaweicloud/resource_schema.go
+++ huaweicloud/resource_schema.go
@@ -2,8 +2,6 @@ package huaweicloud
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils/fmtp"
 )
 
 // tagsSchema returns the schema to use for tags.
@@ -13,92 +11,4 @@ func tagsSchema() *schema.Schema {
 		Optional: true,
 		Elem:     &schema.Schema{Type: schema.TypeString},
 	}
-}
-
-// tagsForceNewSchema returns the schema to use for tags with ForceNew
-func tagsForceNewSchema() *schema.Schema {
-	return &schema.Schema{
-		Type:     schema.TypeMap,
-		Optional: true,
-		ForceNew: true,
-		Elem:     &schema.Schema{Type: schema.TypeString},
-	}
-}
-
-func schemaChargingMode(conflicts []string) *schema.Schema {
-	resourceSchema := schema.Schema{
-		Type:     schema.TypeString,
-		Optional: true,
-		ForceNew: true,
-		Computed: true,
-		ValidateFunc: validation.StringInSlice([]string{
-			"prePaid", "postPaid",
-		}, false),
-		ConflictsWith: conflicts,
-	}
-
-	return &resourceSchema
-}
-
-func schemaPeriodUnit(conflicts []string) *schema.Schema {
-	resourceSchema := schema.Schema{
-		Type:         schema.TypeString,
-		Optional:     true,
-		ForceNew:     true,
-		RequiredWith: []string{"period"},
-		ValidateFunc: validation.StringInSlice([]string{
-			"month", "year",
-		}, false),
-		ConflictsWith: conflicts,
-	}
-
-	return &resourceSchema
-}
-
-func schemaPeriod(conflicts []string) *schema.Schema {
-	resourceSchema := schema.Schema{
-		Type:          schema.TypeInt,
-		Optional:      true,
-		ForceNew:      true,
-		RequiredWith:  []string{"period_unit"},
-		ValidateFunc:  validation.IntBetween(1, 9),
-		ConflictsWith: conflicts,
-	}
-
-	return &resourceSchema
-}
-
-func schemaAutoRenew(conflicts []string) *schema.Schema {
-	resourceSchema := schema.Schema{
-		Type:     schema.TypeString,
-		Optional: true,
-		ForceNew: true,
-		ValidateFunc: validation.StringInSlice([]string{
-			"true", "false",
-		}, false),
-		ConflictsWith: conflicts,
-	}
-
-	return &resourceSchema
-}
-
-func schemaAutoPay(conflicts []string) *schema.Schema {
-	resourceSchema := schema.Schema{
-		Type:     schema.TypeString,
-		Optional: true,
-		ForceNew: true,
-		ValidateFunc: validation.StringInSlice([]string{
-			"true", "false",
-		}, false),
-		ConflictsWith: conflicts,
-	}
-
-	return &resourceSchema
-}
-
-func validatePrePaidChargeInfo(d *schema.ResourceData) error {
-	if _, ok := d.GetOk("period_unit"); !ok {
-		return fmtp.Errorf("both of `period, period_unit` must be specified in prePaid charging mode")
-	}
-	return nil
 }
