5c1d930b0796ba00c394995c4fed14857b2828bf
allium <deerhang@163.com>
2023-05-12 15:56:46 +0800

    feat(WAF): waf support rule precise protection resource (#2984)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -995,6 +995,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_waf_policy":                     waf.ResourceWafPolicyV1(),
 			"huaweicloud_waf_rule_blacklist":             waf.ResourceWafRuleBlackListV1(),
 			"huaweicloud_waf_rule_data_masking":          waf.ResourceWafRuleDataMaskingV1(),
+			"huaweicloud_waf_rule_precise_protection":    waf.ResourceRulePreciseProtection(),
 			"huaweicloud_waf_rule_web_tamper_protection": waf.ResourceWafRuleWebTamperProtectionV1(),
 			"huaweicloud_waf_dedicated_instance":         waf.ResourceWafDedicatedInstance(),
 			"huaweicloud_waf_dedicated_domain":           waf.ResourceWafDedicatedDomainV1(),
