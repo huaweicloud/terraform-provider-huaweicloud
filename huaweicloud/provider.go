237899957b4f73d3f5fad9d85e60adeaf4519e64
luxiaohang <deerhang@163.com>
2023-05-18 22:56:51 +0800

    feat(OBS): obs support object acl resource

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -930,6 +930,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_obs_bucket":             obs.ResourceObsBucket(),
 			"huaweicloud_obs_bucket_acl":         obs.ResourceOBSBucketAcl(),
 			"huaweicloud_obs_bucket_object":      obs.ResourceObsBucketObject(),
+			"huaweicloud_obs_bucket_object_acl":  obs.ResourceOBSBucketObjectAcl(),
 			"huaweicloud_obs_bucket_policy":      obs.ResourceObsBucketPolicy(),
 			"huaweicloud_obs_bucket_replication": obs.ResourceObsBucketReplication(),
 
