89c8e4c89c5892895e89d669a9b91ccd39fa4e5c
Jason-Zhang <jasonzhang9309@163.com>
2023-05-22 19:53:14 +0800

    feat(vpn): add new resource connection health check (#3022)

---
+++ docs/resources/vpn_connection_health_check.md
@@ -0,0 +1,48 @@
+---
+subcategory: "Virtual Private Network (VPN)"
+---
+
+# huaweicloud_vpn_connection_health_check
+
+Manages a VPN connection health check resource within HuaweiCloud.
+
+## Example Usage
+
+```hcl
+variable "connection_id" {}
+
+resource "huaweicloud_vpn_connection_health_check" "test" {
+  connection_id = var.connection_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region in which to create the resource.
+  If omitted, the provider-level region will be used. Changing this parameter will create a new resource.
+
+* `connection_id` - (Required, String, ForceNew) Specifies the ID of the VPN connection to monitor.
+
+  Changing this parameter will create a new resource.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID.
+
+* `source_ip` - The source IP address of the VPN connection.
+
+* `destination_ip` - The destination IP address of the VPN connection.
+
+* `status` - The status of the connection health check.
+
+## Import
+
+The health check can be imported using the `id`, e.g.
+
+```bash
+$ terraform import huaweicloud_vpn_connection_health_check.test 21a8d00c-5b10-405c-9d85-3581b96dbc29
+```
---
+++ huaweicloud/services/vpn/resource_huaweicloud_vpn_connection_health_check.go
@@ -0,0 +1,202 @@
+// ---------------------------------------------------------------
+// *** AUTO GENERATED CODE ***
+// @Product VPN
+// ---------------------------------------------------------------
+
+package vpn
+
+import (
+	"context"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/jmespath/go-jmespath"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func ResourceConnectionHealthCheck() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceConnectionHealthCheckCreate,
+		ReadContext:   resourceConnectionHealthCheckRead,
+		DeleteContext: resourceConnectionHealthCheckDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"connection_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the ID of the VPN connection to monitor.`,
+			},
+			"destination_ip": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The destination IP address of the VPN connection.`,
+			},
+			"source_ip": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The source IP address of the VPN connection.`,
+			},
+			"status": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The status of the connection health check.`,
+			},
+		},
+	}
+}
+
+func resourceConnectionHealthCheckCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	// createConnectionHealthCheck: Create a VPN ConnectionHealthCheck.
+	var (
+		createConnectionHealthCheckHttpUrl = "v5/{project_id}/connection-monitors"
+		createConnectionHealthCheckProduct = "vpn"
+	)
+	createConnectionHealthCheckClient, err := cfg.NewServiceClient(createConnectionHealthCheckProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating VPN Client: %s", err)
+	}
+
+	createConnectionHealthCheckPath := createConnectionHealthCheckClient.Endpoint + createConnectionHealthCheckHttpUrl
+	createConnectionHealthCheckPath = strings.ReplaceAll(createConnectionHealthCheckPath, "{project_id}",
+		createConnectionHealthCheckClient.ProjectID)
+
+	createConnectionHealthCheckOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		OkCodes: []int{
+			201,
+		},
+	}
+	createConnectionHealthCheckOpt.JSONBody = utils.RemoveNil(buildCreateConnectionHealthCheckBodyParams(d))
+	createConnectionHealthCheckResp, err := createConnectionHealthCheckClient.Request("POST",
+		createConnectionHealthCheckPath, &createConnectionHealthCheckOpt)
+	if err != nil {
+		return diag.Errorf("error creating ConnectionHealthCheck: %s", err)
+	}
+
+	createConnectionHealthCheckRespBody, err := utils.FlattenResponse(createConnectionHealthCheckResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	id, err := jmespath.Search("connection_monitor.id", createConnectionHealthCheckRespBody)
+	if err != nil {
+		return diag.Errorf("error creating ConnectionHealthCheck: ID is not found in API response")
+	}
+	d.SetId(id.(string))
+
+	return resourceConnectionHealthCheckRead(ctx, d, meta)
+}
+
+func buildCreateConnectionHealthCheckBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"connection_monitor": map[string]interface{}{
+			"vpn_connection_id": utils.ValueIngoreEmpty(d.Get("connection_id")),
+		},
+	}
+	return bodyParams
+}
+
+func resourceConnectionHealthCheckRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var mErr *multierror.Error
+
+	// getConnectionHealthCheck: Query the VPN ConnectionHealthCheck detail
+	var (
+		getConnectionHealthCheckHttpUrl = "v5/{project_id}/connection-monitors/{id}"
+		getConnectionHealthCheckProduct = "vpn"
+	)
+	getConnectionHealthCheckClient, err := cfg.NewServiceClient(getConnectionHealthCheckProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating VPN Client: %s", err)
+	}
+
+	getConnectionHealthCheckPath := getConnectionHealthCheckClient.Endpoint + getConnectionHealthCheckHttpUrl
+	getConnectionHealthCheckPath = strings.ReplaceAll(getConnectionHealthCheckPath, "{project_id}",
+		getConnectionHealthCheckClient.ProjectID)
+	getConnectionHealthCheckPath = strings.ReplaceAll(getConnectionHealthCheckPath, "{id}", d.Id())
+
+	getConnectionHealthCheckOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		OkCodes: []int{
+			200,
+		},
+	}
+	getConnectionHealthCheckResp, err := getConnectionHealthCheckClient.Request("GET",
+		getConnectionHealthCheckPath, &getConnectionHealthCheckOpt)
+
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "error retrieving ConnectionHealthCheck")
+	}
+
+	getConnectionHealthCheckRespBody, err := utils.FlattenResponse(getConnectionHealthCheckResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	mErr = multierror.Append(
+		mErr,
+		d.Set("region", region),
+		d.Set("destination_ip", utils.PathSearch("connection_monitor.destination_ip", getConnectionHealthCheckRespBody, nil)),
+		d.Set("source_ip", utils.PathSearch("connection_monitor.source_ip", getConnectionHealthCheckRespBody, nil)),
+		d.Set("status", utils.PathSearch("connection_monitor.status", getConnectionHealthCheckRespBody, nil)),
+		d.Set("connection_id", utils.PathSearch("connection_monitor.vpn_connection_id", getConnectionHealthCheckRespBody, nil)),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func resourceConnectionHealthCheckDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	// deleteConnectionHealthCheck: Delete an existing VPN ConnectionHealthCheck
+	var (
+		deleteConnectionHealthCheckHttpUrl = "v5/{project_id}/connection-monitors/{id}"
+		deleteConnectionHealthCheckProduct = "vpn"
+	)
+	deleteConnectionHealthCheckClient, err := cfg.NewServiceClient(deleteConnectionHealthCheckProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating VPN Client: %s", err)
+	}
+
+	deleteConnectionHealthCheckPath := deleteConnectionHealthCheckClient.Endpoint + deleteConnectionHealthCheckHttpUrl
+	deleteConnectionHealthCheckPath = strings.ReplaceAll(deleteConnectionHealthCheckPath, "{project_id}",
+		deleteConnectionHealthCheckClient.ProjectID)
+	deleteConnectionHealthCheckPath = strings.ReplaceAll(deleteConnectionHealthCheckPath, "{id}", d.Id())
+
+	deleteConnectionHealthCheckOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		OkCodes: []int{
+			204,
+		},
+	}
+	_, err = deleteConnectionHealthCheckClient.Request("DELETE", deleteConnectionHealthCheckPath,
+		&deleteConnectionHealthCheckOpt)
+	if err != nil {
+		return diag.Errorf("error deleting ConnectionHealthCheck: %s", err)
+	}
+
+	return nil
+}
