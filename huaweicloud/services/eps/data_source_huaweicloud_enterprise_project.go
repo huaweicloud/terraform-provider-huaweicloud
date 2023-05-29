2e0b7c7c284e569c9a96486b4ede0663016b9f1b
luxiaohang <deerhang@163.com>
2023-05-29 17:32:39 +0800

    fix(EPS): eps datasource support filter target project by input name

--- huaweicloud/services/eps/data_source_huaweicloud_enterprise_project.go
+++ huaweicloud/services/eps/data_source_huaweicloud_enterprise_project.go
@@ -2,6 +2,7 @@ package eps
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -66,18 +67,10 @@ func dataSourceEnterpriseProjectRead(_ context.Context, d *schema.ResourceData, 
 		return diag.Errorf("Error retrieving enterprise projects %s", err)
 	}
 
-	if len(projects) < 1 {
-		return diag.Errorf("Your query returned no results. " +
-			"Please change your search criteria and try again.")
+	project, err := flattenEnterpriseProject(d, projects)
+	if err != nil {
+		return diag.FromErr(err)
 	}
-
-	if len(projects) > 1 {
-		return diag.Errorf("Your query returned more than one result." +
-			" Please try a more specific search criteria")
-	}
-
-	project := projects[0]
-
 	d.SetId(project.ID)
 	mErr := multierror.Append(nil,
 		d.Set("name", project.Name),
@@ -88,8 +81,35 @@ func dataSourceEnterpriseProjectRead(_ context.Context, d *schema.ResourceData, 
 	)
 
 	if err := mErr.ErrorOrNil(); err != nil {
-		return diag.Errorf("error setting enterprise project fields: %w", err)
+		return diag.Errorf("Error setting enterprise project fields: %s", err)
 	}
 
 	return nil
+}
+
+func flattenEnterpriseProject(d *schema.ResourceData, projects []enterpriseprojects.Project) (
+	*enterpriseprojects.Project, error) {
+	if len(projects) < 1 {
+		return nil, fmt.Errorf("your query returned no results." +
+			" Please change your search criteria and try again")
+	}
+
+	if len(projects) > 1 {
+		name := d.Get("name").(string)
+		// there is no condition to find the target enterprise project
+		if name == "" {
+			return nil, fmt.Errorf("your query returned more than one result." +
+				" Please specify your enterprise project name or enterprise project id")
+		}
+
+		for _, v := range projects {
+			if v.Name == name {
+				// use name to find the target enterprise project
+				return &v, nil
+			}
+		}
+		return nil, fmt.Errorf("cannot find the target enterprise project through your input name." +
+			" Please change your search criteria and try again")
+	}
+	return &projects[0], nil
 }
