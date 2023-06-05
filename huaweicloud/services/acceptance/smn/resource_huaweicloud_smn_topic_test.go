6cafa0cb1f1b3eea002f721c10f7ecf8c771e819
Jason Zhang <jasonzhang9309@163.com>
2023-06-05 10:52:44 +0800

    feat(smn): support policies in topic

--- huaweicloud/services/acceptance/smn/resource_huaweicloud_smn_topic_test.go
+++ huaweicloud/services/acceptance/smn/resource_huaweicloud_smn_topic_test.go
@@ -68,6 +68,64 @@ func TestAccSMNV2Topic_basic(t *testing.T) {
 	})
 }
 
+func TestAccSMNV2Topic_policies(t *testing.T) {
+	var topic topics.TopicGet
+	resourceName := "huaweicloud_smn_topic.topic_1"
+	rName := acceptance.RandomAccResourceNameWithDash()
+	displayName := fmt.Sprintf("The display name of %s", rName)
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&topic,
+		getResourceSMNTopic,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSMNV2TopicConfig_policies_step1(rName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "display_name", displayName),
+					resource.TestCheckResourceAttr(resourceName, "users_publish_allowed", "*"),
+					resource.TestCheckResourceAttr(resourceName, "services_publish_allowed", "obs,vod"),
+					resource.TestCheckResourceAttr(resourceName, "introduction", "created by terraform"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccSMNV2TopicConfig_policies_step2(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "display_name", displayName),
+					resource.TestCheckResourceAttr(resourceName, "users_publish_allowed",
+						"urn:csp:iam::0970d7b7d400f2470fbec00316a03560:root,urn:csp:iam::0970d7b7d400f2470fbec00316a03561:root"),
+					resource.TestCheckResourceAttr(resourceName, "services_publish_allowed", "obs,vod,cce"),
+					resource.TestCheckResourceAttr(resourceName, "introduction", "created by terraform update"),
+				),
+			},
+			{
+				Config: testAccSMNV2TopicConfig_policies_step3(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "display_name", displayName),
+					resource.TestCheckResourceAttr(resourceName, "users_publish_allowed", ""),
+					resource.TestCheckResourceAttr(resourceName, "services_publish_allowed", ""),
+					resource.TestCheckResourceAttr(resourceName, "introduction", ""),
+				),
+			},
+		},
+	})
+}
+
 func TestAccSMNV2Topic_withEpsID(t *testing.T) {
 	var topic topics.TopicGet
 	resourceName := "huaweicloud_smn_topic.topic_1"
@@ -135,4 +193,37 @@ resource "huaweicloud_smn_topic" "topic_1" {
   enterprise_project_id = "%s"
 }
 `, rName, rName, acceptance.HW_ENTERPRISE_PROJECT_ID_TEST)
+}
+
+func testAccSMNV2TopicConfig_policies_step1(rName string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_smn_topic" "topic_1" {
+  name                     = "%s"
+  display_name             = "The display name of %s"
+  users_publish_allowed    = "*"
+  services_publish_allowed = "obs,vod"
+  introduction             = "created by terraform"
+}
+`, rName, rName)
+}
+
+func testAccSMNV2TopicConfig_policies_step2(rName string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_smn_topic" "topic_1" {
+  name                     = "%s"
+  display_name             = "The display name of %s"
+  users_publish_allowed    = "urn:csp:iam::0970d7b7d400f2470fbec00316a03560:root,urn:csp:iam::0970d7b7d400f2470fbec00316a03561:root"
+  services_publish_allowed = "obs,vod,cce"
+  introduction             = "created by terraform update"
+}
+`, rName, rName)
+}
+
+func testAccSMNV2TopicConfig_policies_step3(rName string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_smn_topic" "topic_1" {
+  name         = "%s"
+  display_name = "The display name of %s"
+}
+`, rName, rName)
 }
