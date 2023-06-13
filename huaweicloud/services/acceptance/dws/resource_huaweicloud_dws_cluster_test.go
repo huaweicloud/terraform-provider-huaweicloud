7f960a9aba7562152cf350fb5c01a4b91f200c23
hua ming <hmbig2@163.com>
2023-06-13 10:52:20 +0800

    feat(DWS): enhance cluster to support apiV2 (#3070)

--- huaweicloud/services/acceptance/dws/resource_huaweicloud_dws_cluster_test.go
+++ huaweicloud/services/acceptance/dws/resource_huaweicloud_dws_cluster_test.go
@@ -2,34 +2,70 @@ package dws
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance/common"
-
-	"github.com/chnsz/golangsdk/openstack/dws/v1/cluster"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/chnsz/golangsdk"
+
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/dws"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
-func getDwsResourceFunc(config *config.Config, state *terraform.ResourceState) (interface{}, error) {
-	client, err := config.DwsV1Client(acceptance.HW_REGION_NAME)
+func getClusterResourceFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	region := acceptance.HW_REGION_NAME
+	// getDwsCluster: Query the DWS cluster.
+	var (
+		getDwsClusterHttpUrl = "v1.0/{project_id}/clusters/{id}"
+		getDwsClusterProduct = "dws"
+	)
+	getDwsClusterClient, err := cfg.NewServiceClient(getDwsClusterProduct, region)
 	if err != nil {
-		return nil, fmt.Errorf("error creating DWS v1 client, err=%s", err)
+		return nil, fmt.Errorf("error creating DWS Client: %s", err)
 	}
-	return cluster.Get(client, state.Primary.ID)
+
+	getDwsClusterPath := getDwsClusterClient.Endpoint + getDwsClusterHttpUrl
+	getDwsClusterPath = strings.ReplaceAll(getDwsClusterPath, "{project_id}", getDwsClusterClient.ProjectID)
+	getDwsClusterPath = strings.ReplaceAll(getDwsClusterPath, "{id}", state.Primary.ID)
+
+	getDwsClusterOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		OkCodes: []int{
+			200,
+		},
+		MoreHeaders: map[string]string{
+			"Content-Type": "application/json;charset=UTF-8",
+		},
+	}
+
+	getDwsClusterResp, err := getDwsClusterClient.Request("GET", getDwsClusterPath, &getDwsClusterOpt)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving DwsCluster: %s", err)
+	}
+
+	getDwsClusterRespBody, err := utils.FlattenResponse(getDwsClusterResp)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving DwsCluster: %s", err)
+	}
+
+	return getDwsClusterRespBody, nil
 }
 
 func TestAccResourceDWS_basic(t *testing.T) {
-	var clusterInstance cluster.CreateOpts
+	var obj interface{}
+
 	resourceName := "huaweicloud_dws_cluster.test"
 	name := acceptance.RandomAccResourceName()
 
 	rc := acceptance.InitResourceCheck(
 		resourceName,
-		&clusterInstance,
-		getDwsResourceFunc,
+		&obj,
+		getClusterResourceFunc,
 	)
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -38,7 +74,7 @@ func TestAccResourceDWS_basic(t *testing.T) {
 		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDwsCluster_basic(name, 3, cluster.PublicBindTypeAuto, "cluster123@!", "bar"),
+				Config: testAccDwsCluster_basic(name, 3, dws.PublicBindTypeAuto, "cluster123@!", "bar"),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
 					resource.TestCheckResourceAttr(resourceName, "name", name),
@@ -48,7 +84,7 @@ func TestAccResourceDWS_basic(t *testing.T) {
 				),
 			},
 			{
-				Config: testAccDwsCluster_basic(name, 6, cluster.PublicBindTypeAuto, "cluster123@!u", "cat"),
+				Config: testAccDwsCluster_basic(name, 6, dws.PublicBindTypeAuto, "cluster123@!u", "cat"),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
 					resource.TestCheckResourceAttr(resourceName, "name", name),
@@ -61,7 +97,7 @@ func TestAccResourceDWS_basic(t *testing.T) {
 				ResourceName:            resourceName,
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"user_pwd", "number_of_cn"},
+				ImportStateVerifyIgnore: []string{"user_pwd", "number_of_cn", "volume", "endpoints"},
 			},
 		},
 	})
@@ -75,15 +111,9 @@ func testAccDwsCluster_basic(rName string, numberOfNode int, publicIpBindType, p
 
 data "huaweicloud_availability_zones" "test" {}
 
-data "huaweicloud_dws_flavors" "test" {
-  vcpus             = 8
-  memory            = 64
-  availability_zone = data.huaweicloud_availability_zones.test.names[0]
-}
-
 resource "huaweicloud_dws_cluster" "test" {
   name              = "%s"
-  node_type         = data.huaweicloud_dws_flavors.test.flavors.0.flavor_id
+  node_type         = "dwsk2.xlarge"
   number_of_node    = %d
   vpc_id            = huaweicloud_vpc.test.id
   network_id        = huaweicloud_vpc_subnet.test.id
@@ -102,4 +132,93 @@ resource "huaweicloud_dws_cluster" "test" {
   }
 }
 `, baseNetwork, rName, numberOfNode, password, publicIpBindType, tag)
+}
+
+func TestAccResourceDWS_basicV2(t *testing.T) {
+	var obj interface{}
+
+	resourceName := "huaweicloud_dws_cluster.test"
+	name := acceptance.RandomAccResourceName()
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&obj,
+		getClusterResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDwsCluster_basicV2(name, 3, dws.PublicBindTypeAuto, "cluster123@!", "bar", 100),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "number_of_node", "3"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key", "val"),
+					resource.TestCheckResourceAttr(resourceName, "tags.foo", "bar"),
+					resource.TestCheckResourceAttr(resourceName, "version", "8.2.0.103"),
+					resource.TestCheckResourceAttr(resourceName, "volume.0.capacity", "100"),
+				),
+			},
+			{
+				Config: testAccDwsCluster_basicV2(name, 6, dws.PublicBindTypeAuto, "cluster123@!u", "cat", 150),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "number_of_node", "6"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key", "val"),
+					resource.TestCheckResourceAttr(resourceName, "tags.foo", "cat"),
+					resource.TestCheckResourceAttr(resourceName, "version", "8.2.0.103"),
+					resource.TestCheckResourceAttr(resourceName, "volume.0.capacity", "150"),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"user_pwd", "number_of_cn", "volume", "endpoints"},
+			},
+		},
+	})
+}
+
+func testAccDwsCluster_basicV2(rName string, numberOfNode int, publicIpBindType, password, tag string, volumeCap int) string {
+	baseNetwork := common.TestBaseNetwork(rName)
+
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_availability_zones" "test" {}
+
+resource "huaweicloud_dws_cluster" "test" {
+  name              = "%s"
+  node_type         = "dwsk2.xlarge"
+  number_of_node    = %d
+  vpc_id            = huaweicloud_vpc.test.id
+  network_id        = huaweicloud_vpc_subnet.test.id
+  security_group_id = huaweicloud_networking_secgroup.test.id
+  availability_zone = data.huaweicloud_availability_zones.test.names[0]
+  user_name         = "test_cluster_admin"
+  user_pwd          = "%s"
+  version           = "8.2.0.103"
+  number_of_cn      = 3
+
+  public_ip {
+    public_bind_type = "%s"
+  }
+
+  volume {
+    type     = "SSD"
+    capacity = %d
+  }
+
+  tags = {
+    key = "val"
+    foo = "%s"
+  }
+}
+`, baseNetwork, rName, numberOfNode, password, publicIpBindType, volumeCap, tag)
 }
