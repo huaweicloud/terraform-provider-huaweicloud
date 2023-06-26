b83d8b1e131ecccc3b9751bdfcf1f98bab19e327
ChengXiangdong <chengxiangdong@outlook.com>
2023-06-26 15:04:36 +0800

    docs(example): use flavor instead of product_id to create a Kafka cluster (#3143)

--- examples/dms/kafka-instance/main.tf
+++ examples/dms/kafka-instance/main.tf
@@ -21,13 +21,17 @@ resource "huaweicloud_networking_secgroup" "secgroup" {
 # List the availability zones in the current region.
 data "huaweicloud_availability_zones" "zones" {}
 
-# Find the product ID according to the Kafka instance information to be created.
-data "huaweicloud_dms_product" "product_1" {
-  engine            = "kafka"
-  instance_type     = "cluster"
-  version           = "2.3.0"
-  bandwidth         = "100MB"
-  storage_spec_code = "dms.physical.storage.high"
+# Query flavor information based on flavorID and storage I/O specification.
+# Make sure the flavors are available in the availability zone.
+data "huaweicloud_dms_kafka_flavors" "test" {
+  type               = "cluster"
+  flavor_id          = "c6.2u4g.cluster"
+  availability_zones = [
+    data.huaweicloud_availability_zones.zones.names[0],
+    data.huaweicloud_availability_zones.zones.names[1],
+    data.huaweicloud_availability_zones.zones.names[2],
+  ]
+  storage_spec_code = "dms.physical.storage.ultra.v2"
 }
 
 # Create the DMS Kafka instance.
@@ -41,9 +45,11 @@ resource "huaweicloud_dms_kafka_instance" "kafka_instance_1" {
     data.huaweicloud_availability_zones.zones.names[2],
   ]
 
-  product_id        = data.huaweicloud_dms_product.product_1.id
-  engine_version    = "2.3.0"
-  storage_spec_code = data.huaweicloud_dms_product.product_1.storage_spec_code
+  engine_version    = "2.7"
+  flavor_id         = data.huaweicloud_dms_kafka_flavors.test.flavor_id
+  storage_spec_code = data.huaweicloud_dms_kafka_flavors.test.flavors[0].ios[0].storage_spec_code
+  storage_space     = 600
+  broker_num        = 3
 
   vpc_id            = huaweicloud_vpc.vpc_1.id
   network_id        = huaweicloud_vpc_subnet.vpc_subnet_1.id
