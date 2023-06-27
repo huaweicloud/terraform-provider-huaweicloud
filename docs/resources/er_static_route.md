020ef72dd37b448b49984c109f2932afe0f80fd7
Lance52259 <Overlord52259@gmail.com>
2023-06-27 20:49:13 +0800

    feat(er/route): add static route resource

---
+++ docs/resources/er_static_route.md
@@ -0,0 +1,79 @@
+---
+subcategory: "Enterprise Router (ER)"
+---
+
+# huaweicloud_er_static_route
+
+Manages a static route under the ER route table within HuaweiCloud.
+
+## Example Usage
+
+### Create a static route and cross the VPC
+
+```hcl
+variable "route_table_id" {}
+variable "destination_vpc_cidr" {}
+variable "source_vpc_attachment_id" {}
+
+resource "huaweicloud_er_static_route" "test" {
+  route_table_id = var.route_table_id
+  destination    = var.destination_vpc_cidr
+  attachment_id  = var.source_vpc_attachment_id
+}
+```
+
+### Create a black hole route
+
+```hcl
+variable "route_table_id" {}
+variable "destination_vpc_cidr" {}
+
+resource "huaweicloud_er_static_route" "test" {
+  route_table_id = var.route_table_id
+  destination    = var.destination_vpc_cidr
+  is_blackhole   = true
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region where the static route and related route table are
+  located.  
+  If omitted, the provider-level region will be used. Changing this parameter will create a new resource.
+
+* `route_table_id` - (Required, String, ForceNew) Specifies the ID of the route table to which the static route
+  belongs.  
+  Changing this parameter will create a new resource.
+
+* `destination` - (Required, String, ForceNew) Specifies the destination of the static route.  
+  Changing this parameter will create a new resource.
+
+* `attachment_id` - (Optional, String) Specifies the ID of the corresponding attachment.
+
+* `is_blackhole` - (Optional, Bool) Specifies whether route is the black hole route, defaults to `false`.  
+  + If the value is empty or `false`, the parameter `attachment_id` is required.
+  + If the value is `true`, the parameter `attachment_id` must be empty.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID.
+
+* `type` - The type of the static route.
+
+* `status` - The current status of the static route.
+
+* `created_at` - The creation time of the static route.
+
+* `updated_at` - The latest update time of the static route.
+
+## Import
+
+Static routes can be imported using the related `route_table_id` and their `id`, separated by a slash (/), e.g.
+
+```bash
+$ terraform import huaweicloud_er_static_route.test <route_table_id>/<id>
+```
---
+++ huaweicloud/services/er/resource_huaweicloud_er_static_route.go
@@ -0,0 +1,212 @@
+package er
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/chnsz/golangsdk/openstack/er/v3/routes"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func ResourceStaticRoute() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceStaticRouteCreate,
+		UpdateContext: resourceStaticRouteUpdate,
+		ReadContext:   resourceStaticRouteRead,
+		DeleteContext: resourceStaticRouteDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceStaticRouteImportState,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				ForceNew:    true,
+				Description: `The region where the static route and related route table are located.`,
+			},
+			"route_table_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `The ID of the route table to which the static route belongs.`,
+			},
+			"destination": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `The destination of the static route.`,
+			},
+			"attachment_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `The ID of the corresponding attachment.`,
+			},
+			"is_blackhole": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: `Whether route is the black hole route.`,
+			},
+			"type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The type of the static route.`,
+			},
+			"status": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The current status of the static route.`,
+			},
+			"created_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The creation time of the static route.`,
+			},
+			"updated_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The latest update time of the static route.`,
+			},
+		},
+	}
+}
+
+func buildStaticRouteCreateOpts(d *schema.ResourceData) routes.CreateOpts {
+	return routes.CreateOpts{
+		Destination:  d.Get("destination").(string),
+		AttachmentId: d.Get("attachment_id").(string),
+		IsBlackHole:  utils.Bool(d.Get("is_blackhole").(bool)),
+	}
+}
+
+func resourceStaticRouteCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	client, err := cfg.ErV3Client(cfg.GetRegion(d))
+	if err != nil {
+		return diag.Errorf("error creating ER v3 client: %s", err)
+	}
+
+	var (
+		routeTableId = d.Get("route_table_id").(string)
+		opts         = buildStaticRouteCreateOpts(d)
+	)
+	resp, err := routes.Create(client, routeTableId, opts)
+	if err != nil {
+		return diag.Errorf("error creating static route: %s", err)
+	}
+	d.SetId(resp.ID)
+
+	return resourceStaticRouteRead(ctx, d, meta)
+}
+
+func resourceStaticRouteRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	client, err := cfg.ErV3Client(region)
+	if err != nil {
+		return diag.Errorf("error creating ER v3 client: %s", err)
+	}
+
+	var (
+		routeTableId  = d.Get("route_table_id").(string)
+		staticRouteId = d.Id()
+	)
+
+	resp, err := routes.Get(client, routeTableId, staticRouteId)
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "ER static route")
+	}
+
+	mErr := multierror.Append(nil,
+		d.Set("region", region),
+		d.Set("destination", resp.Destination),
+		d.Set("is_blackhole", resp.IsBlackHole),
+		// Attributes
+		d.Set("type", resp.Type),
+		d.Set("status", resp.Status),
+		d.Set("created_at", resp.CreatedAt),
+		d.Set("updated_at", resp.UpdatedAt),
+	)
+
+	if len(resp.Attachments) > 0 && resp.Attachments[0].AttachmentId != "" {
+		// If the static route is not a black hole route, set related VPC attachment ID.
+		mErr = multierror.Append(mErr, d.Set("attachment_id", resp.Attachments[0].AttachmentId))
+	} else {
+		// Override 'attachment_id' while static route is the black hole route.
+		mErr = multierror.Append(mErr, d.Set("attachment_id", nil))
+	}
+
+	if mErr.ErrorOrNil() != nil {
+		return diag.Errorf("error saving static route (%s) fields: %s", staticRouteId, mErr)
+	}
+	return nil
+}
+
+func resourceStaticRouteUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	client, err := cfg.ErV3Client(region)
+	if err != nil {
+		return diag.Errorf("error creating ER v3 client: %s", err)
+	}
+
+	var (
+		routeTableId  = d.Get("route_table_id").(string)
+		staticRouteId = d.Id()
+		opts          = routes.UpdateOpts{
+			AttachmentId: d.Get("attachment_id").(string),
+			IsBlackHole:  utils.Bool(d.Get("is_blackhole").(bool)),
+		}
+	)
+	_, err = routes.Update(client, routeTableId, staticRouteId, opts)
+	if err != nil {
+		return diag.Errorf("error updating static route (%s): %s", staticRouteId, err)
+	}
+
+	return resourceStaticRouteRead(ctx, d, meta)
+}
+
+func resourceStaticRouteDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	client, err := cfg.ErV3Client(region)
+	if err != nil {
+		return diag.Errorf("error creating ER v3 client: %s", err)
+	}
+
+	var (
+		routeTableId  = d.Get("route_table_id").(string)
+		staticRouteId = d.Id()
+	)
+	err = routes.Delete(client, routeTableId, staticRouteId)
+	if err != nil {
+		return diag.Errorf("error deleting static route (%s): %s", staticRouteId, err)
+	}
+
+	return nil
+}
+
+func resourceStaticRouteImportState(_ context.Context, d *schema.ResourceData, _ interface{}) ([]*schema.ResourceData,
+	error) {
+	parts := strings.Split(d.Id(), "/")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("Invalid format for import ID, want '<route_table_id>/<id>', but got '%s'", d.Id())
+	}
+
+	d.SetId(parts[1])
+	if err := d.Set("route_table_id", parts[0]); err != nil {
+		return []*schema.ResourceData{d}, err
+	}
+	return []*schema.ResourceData{d}, nil
+}
