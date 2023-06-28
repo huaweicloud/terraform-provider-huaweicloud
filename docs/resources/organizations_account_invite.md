0077b814a5cc16330eebb87eb8e5f99b71ac9588
houpeng <hp_monkey@163.com>
2023-06-28 13:01:57 +0800

    add organizations account invite resource

---
+++ docs/resources/organizations_account_invite.md
@@ -0,0 +1,75 @@
+---
+subcategory: "Organizations"
+---
+
+# huaweicloud_organizations_account_invite
+
+Manages an Organizations account invite resource within HuaweiCloud.
+
+## Example Usage
+
+```hcl
+variable account_id {}
+
+resource "huaweicloud_organizations_account_invite" "test"{
+  account_id = var.account_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `account_id` - (Required, String, ForceNew) Specifies the ID of the target account.
+
+  Changing this parameter will create a new resource.
+
+* `remove_account_on_destroy` - (Optional, Bool) Specifies whether to remove the invited account when delete the
+  invitation (handshake). Defaults to false.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID.
+
+* `urn` - Indicates the uniform resource name of the invitation
+
+* `master_account_id` - Indicates the unique ID of the organization's management account.
+
+* `master_account_name` - Indicates the name of the organization's management account.
+
+* `organization_id` - Indicates the ID of the organization.
+
+* `created_at` - Indicates the date and time when an invitation (handshake) request was made.
+
+* `updated_at` - Indicates the date and time when an invitation (handshake) request was accepted, canceled,
+  declined, or expired.
+
+* `status` - Indicates the current state of the invitation (handshake).
+
+## Import
+
+The organizations account invite can be imported using the `id`, e.g.
+
+```bash
+$ terraform import huaweicloud_organizations_account_invite.test <id>
+```
+
+Note that the imported state may not be identical to your resource definition, due to some attributes missing from the
+API response, security or some other reason. The missing attributes include: `remove_account_on_destroy`. It is
+generally recommended running `terraform plan` after importing an account invite. You can then decide if changes should
+be applied to the account invite, or the resource definition should be updated to align with the account invite.
+Also you can ignore changes as below.
+
+```hcl
+resource "huaweicloud_organizations_account_invite" "test" {
+  ...
+
+  lifecycle {
+    ignore_changes = [
+      remove_account_on_destroy,
+    ]
+  }
+}
+```
---
+++ huaweicloud/services/acceptance/organizations/resource_huaweicloud_organizations_account_invite_test.go
@@ -0,0 +1,124 @@
+package organizations
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func getAccountInviteResourceFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	// getAccountInvite: Query Organizations account invite
+	var (
+		getAccountHttpUrl       = "v1/organizations/accounts/{account_id}"
+		getAccountInviteHttpUrl = "v1/organizations/handshakes/{handshake_id}"
+		getAccountInviteProduct = "organizations"
+	)
+	getAccountInviteClient, err := cfg.NewServiceClient(getAccountInviteProduct, "")
+	if err != nil {
+		return nil, fmt.Errorf("error creating Organizations Client: %s", err)
+	}
+
+	getAccountInvitePath := getAccountInviteClient.Endpoint + getAccountInviteHttpUrl
+	getAccountInvitePath = strings.ReplaceAll(getAccountInvitePath, "{handshake_id}", state.Primary.ID)
+
+	getAccountInviteOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+	getAccountInviteResp, err := getAccountInviteClient.Request("GET", getAccountInvitePath,
+		&getAccountInviteOpt)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving AccountInvite: %s", err)
+	}
+
+	getAccountInviteRespBody, err := utils.FlattenResponse(getAccountInviteResp)
+	if err != nil {
+		return nil, err
+	}
+
+	status := utils.PathSearch("handshake.status", getAccountInviteRespBody, "")
+	if status == "cancelled" || status == "expired" {
+		return nil, fmt.Errorf("failed to get Organizations AccountInvite")
+	}
+
+	accountID := utils.PathSearch("handshake.target.entity", getAccountInviteRespBody, "")
+
+	// the handshake will always exist, so it is necessary to check whether the account can be obtained if the
+	// status is accepted
+	if status == "accepted" {
+		getAccountPath := getAccountInviteClient.Endpoint + getAccountHttpUrl
+		getAccountPath = strings.ReplaceAll(getAccountPath, "{account_id}", accountID.(string))
+
+		getAccountOpt := golangsdk.RequestOpts{
+			KeepResponseBody: true,
+		}
+		_, err = getAccountInviteClient.Request("GET", getAccountPath, &getAccountOpt)
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to get Organizations AccountInvite")
+		}
+	}
+
+	return getAccountInviteRespBody, nil
+}
+
+func TestAccAccountInvite_basic(t *testing.T) {
+	var obj interface{}
+
+	rName := "huaweicloud_organizations_account_invite.test"
+
+	rc := acceptance.InitResourceCheck(
+		rName,
+		&obj,
+		getAccountInviteResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheckOrganizations(t)
+			acceptance.TestAccPreCheckOrganizationsInviteAccountId(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccountInvite_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(rName, "account_id",
+						acceptance.HW_ORGANIZATIONS_INVITE_ACCOUNT_ID),
+					resource.TestCheckResourceAttrSet(rName, "urn"),
+					resource.TestCheckResourceAttrSet(rName, "master_account_id"),
+					resource.TestCheckResourceAttrSet(rName, "master_account_name"),
+					resource.TestCheckResourceAttrSet(rName, "organization_id"),
+					resource.TestCheckResourceAttrSet(rName, "created_at"),
+					resource.TestCheckResourceAttrSet(rName, "updated_at"),
+					resource.TestCheckResourceAttrSet(rName, "status"),
+				),
+			},
+			{
+				ResourceName:            rName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"remove_account_on_destroy"},
+			},
+		},
+	})
+}
+
+func testAccountInvite_basic() string {
+	return fmt.Sprintf(`
+resource "huaweicloud_organizations_account_invite" "test" {
+  account_id                = "%[1]s"
+  remove_account_on_destroy = true
+}
+`, acceptance.HW_ORGANIZATIONS_INVITE_ACCOUNT_ID)
+}
