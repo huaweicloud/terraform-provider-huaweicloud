2d51c7543d7079fb4874a4912f1ac66c14d3b5ee
houpeng <hp_monkey@163.com>
2023-06-28 14:23:26 +0800

    add organizations account invite accepter resource

---
+++ docs/resources/organizations_account_invite_accepter.md
@@ -0,0 +1,77 @@
+---
+subcategory: "Organizations"
+---
+
+# huaweicloud_organizations_account_invite_accepter
+
+Manages an Organizations account invite accepter resource within HuaweiCloud.
+
+## Example Usage
+
+```hcl
+variable invitation_id {}
+
+resource "huaweicloud_organizations_account_invite_accepter" "test"{
+  invitation_id = var.invitation_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `invitation_id` - (Required, String, ForceNew) Specifies the unique ID of an invitation (handshake).
+
+  Changing this parameter will create a new resource.
+
+* `leave_organization_on_destroy` - (Optional, Bool) Specifies whether to leave the organization when delete the
+  invitation (handshake). Defaults to false.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID.
+
+* `urn` - Indicates the uniform resource name of the invitation
+
+* `account_id` - Indicates the ID of the target account.
+
+* `master_account_id` - Indicates the unique ID of the organization's management account.
+
+* `master_account_name` - Indicates the name of the organization's management account.
+
+* `organization_id` - Indicates the ID of the organization.
+
+* `created_at` - Indicates the date and time when an invitation (handshake) request was made.
+
+* `updated_at` - Indicates the date and time when an invitation (handshake) request was accepted, cancelled,
+  declined, or expired.
+
+* `status` - Indicates the current state of the invitation (handshake).
+
+## Import
+
+The organizations account invite accepter can be imported using the `id`, e.g.
+
+```bash
+$ terraform import huaweicloud_organizations_account_invite_accepter.test <id>
+```
+
+Note that the imported state may not be identical to your resource definition, due to some attributes missing from the
+API response, security or some other reason. The missing attributes include: `leave_organization_on_destroy`. It is
+generally recommended running `terraform plan` after importing an account invite accepter. You can then decide if
+changes should be applied to the account invite accepter, or the resource definition should be updated to align with
+the account invite accepter. Also you can ignore changes as below.
+
+```hcl
+resource "huaweicloud_organizations_account_invite_accepter" "test" {
+  ...
+
+  lifecycle {
+    ignore_changes = [
+      leave_organization_on_destroy,
+    ]
+  }
+}
+```
---
+++ huaweicloud/services/organizations/resource_huaweicloud_organizations_account_invite_accepter.go
@@ -0,0 +1,212 @@
+// ---------------------------------------------------------------
+// *** AUTO GENERATED CODE ***
+// @Product Organizations
+// ---------------------------------------------------------------
+
+package organizations
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/jmespath/go-jmespath"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func ResourceAccountInviteAccepter() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceAccountInviteAccepterCreate,
+		UpdateContext: resourceAccountInviteAccepterUpdate,
+		ReadContext:   resourceAccountInviteAccepterRead,
+		DeleteContext: resourceAccountInviteAccepterDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"invitation_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the unique ID of an invitation (handshake).`,
+			},
+			"leave_organization_on_destroy": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: `Specifies whether to leave the organization when delete the invitation (handshake).`,
+			},
+			"urn": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the uniform resource name of the invitation`,
+			},
+			"account_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the ID of the target account.`,
+			},
+			"master_account_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the unique ID of the organization's management account.`,
+			},
+			"master_account_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the name of the organization's management account.`,
+			},
+			"organization_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the ID of the organization.`,
+			},
+			"created_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the date and time when an invitation (handshake) request was made.`,
+			},
+			"updated_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+				Description: `Indicates the date and time when an invitation (handshake) request was accepted,
+cancelled, declined, or expired.`,
+			},
+			"status": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the current state of the invitation (handshake).`,
+			},
+		},
+	}
+}
+
+func resourceAccountInviteAccepterCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+
+	// createAccountInviteAccepter: create Organizations account invite accepter
+	var (
+		createAccountInviteAccepterHttpUrl = "v1/received-handshakes/{handshake_id}/accept"
+		createAccountInviteAccepterProduct = "organizations"
+	)
+	createAccountInviteAccepterClient, err := cfg.NewServiceClient(createAccountInviteAccepterProduct, "")
+	if err != nil {
+		return diag.Errorf("error creating Organizations Client: %s", err)
+	}
+
+	createAccountInviteAccepterPath := createAccountInviteAccepterClient.Endpoint + createAccountInviteAccepterHttpUrl
+
+	createAccountInviteAccepterPath = strings.ReplaceAll(createAccountInviteAccepterPath, "{handshake_id}",
+		fmt.Sprintf("%v", d.Get("invitation_id")))
+
+	createAccountInviteAccepterOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+	createAccountInviteAccepterResp, err := createAccountInviteAccepterClient.Request("POST",
+		createAccountInviteAccepterPath, &createAccountInviteAccepterOpt)
+	if err != nil {
+		return diag.Errorf("error creating AccountInviteAccepter: %s", err)
+	}
+
+	createAccountInviteAccepterRespBody, err := utils.FlattenResponse(createAccountInviteAccepterResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	id, err := jmespath.Search("handshake.id", createAccountInviteAccepterRespBody)
+	if err != nil {
+		return diag.Errorf("error creating AccountInviteAccepter: ID is not found in API response")
+	}
+	d.SetId(id.(string))
+
+	return resourceAccountInviteAccepterRead(ctx, d, meta)
+}
+
+func resourceAccountInviteAccepterRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+
+	var mErr *multierror.Error
+
+	// getAccountInviteAccepter: Query Organizations account invite accepter
+	var (
+		getAccountInviteAccepterProduct = "organizations"
+	)
+	getAccountInviteAccepterClient, err := cfg.NewServiceClient(getAccountInviteAccepterProduct, "")
+	if err != nil {
+		return diag.Errorf("error creating Organizations Client: %s", err)
+	}
+
+	getAccountInviteAccepterRespBody, err := getAccountInvite(getAccountInviteAccepterClient, d.Id())
+	if err != nil {
+		return common.CheckDeletedDiag(d, golangsdk.ErrDefault404{}, "error retrieving AccountInvite")
+	}
+
+	handshake := utils.PathSearch("handshake", getAccountInviteAccepterRespBody, nil)
+	if handshake == nil {
+		return common.CheckDeletedDiag(d, golangsdk.ErrDefault404{}, "")
+	}
+
+	mErr = multierror.Append(
+		mErr,
+		d.Set("invitation_id", d.Id()),
+		d.Set("urn", utils.PathSearch("urn", handshake, nil)),
+		d.Set("account_id", utils.PathSearch("target.entity", handshake, nil)),
+		d.Set("master_account_id", utils.PathSearch("management_account_id", handshake, nil)),
+		d.Set("master_account_name", utils.PathSearch("management_account_name", handshake, nil)),
+		d.Set("organization_id", utils.PathSearch("organization_id", handshake, nil)),
+		d.Set("created_at", utils.PathSearch("created_at", handshake, nil)),
+		d.Set("updated_at", utils.PathSearch("updated_at", handshake, nil)),
+		d.Set("status", utils.PathSearch("status", handshake, nil)),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func resourceAccountInviteAccepterUpdate(_ context.Context, _ *schema.ResourceData, _ interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceAccountInviteAccepterDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+
+	if !d.Get("leave_organization_on_destroy").(bool) {
+		return nil
+	}
+
+	// deleteAccountInviteAccepter: Delete Organizations account invite accepter
+	var (
+		deleteAccountInviteAccepterHttpUrl = "v1/organizations/leave"
+		deleteAccountInviteAccepterProduct = "organizations"
+	)
+	deleteAccountInviteAccepterClient, err := cfg.NewServiceClient(deleteAccountInviteAccepterProduct, "")
+	if err != nil {
+		return diag.Errorf("error creating Organizations Client: %s", err)
+	}
+
+	// if the value of accept is false(decline the invite), it can not be removed
+	_, err = getAccountInvite(deleteAccountInviteAccepterClient, d.Id())
+	if err != nil {
+		return nil
+	}
+
+	deleteAccountInviteAccepterPath := deleteAccountInviteAccepterClient.Endpoint + deleteAccountInviteAccepterHttpUrl
+
+	deleteAccountInviteAccepterOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+	_, err = deleteAccountInviteAccepterClient.Request("POST", deleteAccountInviteAccepterPath,
+		&deleteAccountInviteAccepterOpt)
+	if err != nil {
+		return diag.Errorf("error deleting AccountInviteAccepter: %s", err)
+	}
+
+	return nil
+}
