6ebd36901205aac34f290adf79358fc95d71153c
allium <deerhang@163.com>
2023-07-07 11:37:25 +0800

    feat(CNAD): add a new resource cnad advanced policy (#3158)

---
+++ docs/resources/cnad_advanced_policy.md
@@ -0,0 +1,81 @@
+---
+subcategory: "Cloud Native Anti-DDoS Advanced"
+---
+
+# huaweicloud_cnad_advanced_policy
+
+Manages a CNAD advanced policy resource within HuaweiCloud.
+
+## Example Usage
+
+```hcl
+data "huaweicloud_cnad_advanced_instances" "test" {}
+
+resource "huaweicloud_cnad_advanced_policy" "test" {
+  instance_id = huaweicloud_cnad_advanced_instances.test.instances[0].instance_id
+  name        = "test-policy"
+  threshold   = 100
+  udp         = "block"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `instance_id` - (Required, String, ForceNew) Specifies the CNAD advanced instance ID.
+  You can find it through data source `huaweicloud_cnad_advanced_instances`.
+
+  Changing this parameter will create a new resource.
+
+* `name` - (Required, String) Specifies the policy name, the maximum length is 255 characters.
+
+* `udp` - (Optional, String) Specifies whether to block the UDP protocol. Valid values are **block** and **unblock**.
+
+* `threshold` - (Optional, Int) Specifies the cleaning threshold, the value ranges from 100 Mbps to 1000 Mbps.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID.
+
+* `block_location` - The location block list.
+
+* `block_protocol` - The protocol block list.
+
+* `connection_protection` - Whether enable connection protection.
+
+* `connection_protection_list` - The connection protection list.
+
+* `fingerprint_count` - The fingerprint count.
+
+* `port_block_count` - The number of port blockages.
+
+* `watermark_count` - The number of watermarks.
+
+## Import
+
+The CNAD advanced policy can be imported using the `id`, e.g.
+
+```bash
+$ terraform import huaweicloud_cnad_advanced_policy.test <id>
+```
+
+Note that the imported state may not be identical to your resource definition, due to some attributes missing from the
+API response, security or some other reason. The missing attributes include: `udp`.
+It is generally recommended running `terraform plan` after importing a resource.
+You can then decide if changes should be applied to the resource, or the resource definition should be updated to align
+with the resource. Also, you can ignore changes as below.
+
+```hcl
+resource "huaweicloud_cnad_advanced_policy" "test" {
+  ...
+  
+  lifecycle {
+    ignore_changes = [
+      udp,
+    ]
+  }
+}
+```
---
+++ huaweicloud/services/acceptance/cnad/resource_huaweicloud_cnad_advanced_policy_test.go
@@ -0,0 +1,134 @@
+package cnad
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func getPolicyResourceFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	var (
+		getPolicyDetailHttpUrl = "v1/cnad/policies/{policy_id}"
+		getPolicyDetailProduct = "aad"
+	)
+	getPolicyDetailClient, err := cfg.NewServiceClient(getPolicyDetailProduct, "")
+	if err != nil {
+		return nil, fmt.Errorf("error creating CNAD Client: %s", err)
+	}
+
+	getPolicyDetailPath := getPolicyDetailClient.Endpoint + getPolicyDetailHttpUrl
+	getPolicyDetailPath = strings.ReplaceAll(getPolicyDetailPath, "{policy_id}", state.Primary.ID)
+
+	getPolicyDetailOpt := golangsdk.RequestOpts{
+		MoreHeaders: map[string]string{
+			"Content-Type": "application/json;charset=utf8",
+		},
+		KeepResponseBody: true,
+	}
+
+	getPolicyDetailResp, err := getPolicyDetailClient.Request("GET", getPolicyDetailPath, &getPolicyDetailOpt)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving CNAD advanced policy: %s", err)
+	}
+
+	return utils.FlattenResponse(getPolicyDetailResp)
+}
+
+func TestAccPolicy_basic(t *testing.T) {
+	var obj interface{}
+
+	name := acceptance.RandomAccResourceName()
+	rName := "huaweicloud_cnad_advanced_policy.test"
+
+	rc := acceptance.InitResourceCheck(
+		rName,
+		&obj,
+		getPolicyResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckCNADInstance(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testCNADAdvancedPolicy_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttrPair(rName, "instance_id",
+						"data.huaweicloud_cnad_advanced_instances.test",
+						"instances.0.instance_id"),
+					resource.TestCheckResourceAttr(rName, "name", name),
+					resource.TestCheckResourceAttr(rName, "threshold", "100"),
+					resource.TestCheckResourceAttr(rName, "udp", "block"),
+					resource.TestCheckResourceAttr(rName, "block_location.#", "0"),
+					resource.TestCheckResourceAttr(rName, "block_protocol.#", "1"),
+					resource.TestCheckResourceAttr(rName, "connection_protection_list.#", "0"),
+					resource.TestCheckResourceAttrSet(rName, "connection_protection"),
+					resource.TestCheckResourceAttrSet(rName, "fingerprint_count"),
+					resource.TestCheckResourceAttrSet(rName, "port_block_count"),
+					resource.TestCheckResourceAttrSet(rName, "watermark_count"),
+				),
+			},
+			{
+				Config: testCNADAdvancedPolicy_basic_update(name),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(rName, "name", fmt.Sprintf("%s_update", name)),
+					resource.TestCheckResourceAttr(rName, "threshold", "200"),
+					resource.TestCheckResourceAttr(rName, "udp", "unblock"),
+				),
+			},
+			{
+				ResourceName:      rName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"udp",
+				},
+			},
+		},
+	})
+}
+
+const testCNADAdvancedPolicy_base = `
+data "huaweicloud_cnad_advanced_instances" "test" {}
+`
+
+func testCNADAdvancedPolicy_basic(name string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_cnad_advanced_policy" "test" {
+  instance_id = data.huaweicloud_cnad_advanced_instances.test.instances.0.instance_id
+  name        = "%s"
+  threshold   = 100
+  udp         = "block"
+}
+`, testCNADAdvancedPolicy_base, name)
+}
+
+func testCNADAdvancedPolicy_basic_update(name string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_cnad_advanced_policy" "test" {
+  instance_id = data.huaweicloud_cnad_advanced_instances.test.instances.0.instance_id
+  name        = "%s_update"
+  threshold   = 200
+  udp         = "unblock"
+}
+`, testCNADAdvancedPolicy_base, name)
+}
