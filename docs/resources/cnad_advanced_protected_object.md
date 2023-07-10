772663c709e5debaf735e9600f402167fb2775d5
allium <deerhang@163.com>
2023-07-10 12:37:16 +0800

    feat(CNAD): add a new resource cnad advanced protected object (#3094)

---
+++ docs/resources/cnad_advanced_protected_object.md
@@ -0,0 +1,100 @@
+---
+subcategory: "Cloud Native Anti-DDoS Advanced"
+---
+
+# huaweicloud_cnad_advanced_protected_object
+
+Manages a CNAD advanced protected object resource within HuaweiCloud.
+
+## Example Usage
+
+```hcl
+variable "instance_id" {}
+variable "object_id" {}
+variable "object_ip_address" {}
+variable "type" {}
+
+resource "huaweicloud_cnad_advanced_protected_object" "test" {
+  instance_id = var.instance_id
+  
+  protected_objects {
+    id         = var.object_id
+    ip_address = var.object_ip_address
+    type       = var.type
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `instance_id` - (Required, String, ForceNew) Specifies the CNAD advanced instance ID in which to bind protected
+  objects. You can find it through data source `huaweicloud_cnad_advanced_instances`.
+
+  Changing this parameter will create a new resource.
+
+* `protected_objects` - (Required, List) Specifies the advanced protected objects.
+  The [Protected_Object](#advancedProtectedObject_protected_objects) structure is documented below.
+
+<a name="advancedProtectedObject_protected_objects"></a>
+The `Protected_Object` block supports:
+
+* `id` - (Required, String) Specifies the ID of the protected object. The field must be the ID of the Elastic IP,
+  which in the same region with the CNAD advanced instance. You can find it through data source
+  `huaweicloud_cnad_advanced_available_objects`.
+
+* `ip_address` - (Required, String) Specifies the IP of the protected object. The field must be the IP of the Elastic
+  IP, which in the same region with the CNAD advanced instance. You can find it through data source
+  `huaweicloud_cnad_advanced_available_objects`. This field and `id` must belong to the same protected object.
+
+* `type` - (Required, String) Specifies the type of the protected object. Valid values are **VPN**, **NAT**, **VIP**,
+  **CCI**, **EIP**, **ELB**, **REROUTING_IP**, **SubEni** and **NetInterFace**. You can find it through data source
+  `huaweicloud_cnad_advanced_available_objects`. This field and `id` must belong to the same protected object.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The CNAD advanced instance resource ID.
+
+* `protected_objects` - The advanced protected objects.
+  The [Protected_Object](#advancedProtectedObject_GetAdvancedProtectedObjects) structure is documented below.
+
+<a name="advancedProtectedObject_GetAdvancedProtectedObjects"></a>
+The `Protected_Object` block supports:
+
+* `name` - The name of the protected object.
+
+* `instance_id` - The instance ID which the protected object belongs to.
+
+* `instance_name` - The instance name which the protected object belongs to.
+
+* `instance_version` - The instance version which the protected object belongs to.
+  + **cnad_pro**: Professional Edition.
+  + **cnad_ip**: Standard Edition.
+  + **cnad_ep**: Platinum Edition.
+  + **cnad_full_high**: Unlimited Protection Advanced Edition.
+  + **cnad_vic**: On demand Version.
+  + **cnad_intl_ep**: International Station Platinum Edition.
+
+* `region` - The region to which the protected object belongs.
+
+* `status` - The status of the protected object.
+  + **0**: Normal.
+  + **1**: Cleaning in progress.
+  + **2**: In a black hole.
+
+* `block_threshold` - The blocking threshold of the protected object.
+
+* `clean_threshold` - The cleaning threshold of the protected object.
+
+* `policy_name` - The policy name which the protected object binding.
+
+## Import
+
+The CNAD advanced protected object can be imported using the `id`, e.g.
+
+```bash
+$ terraform import huaweicloud_cnad_advanced_protected_object.test <id>
+```
---
+++ huaweicloud/services/acceptance/cnad/resource_huaweicloud_cnad_advanced_protected_object_test.go
@@ -0,0 +1,134 @@
+package cnad
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/chnsz/golangsdk/pagination"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func getProtectedObjectResourceFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	var (
+		getProtectedObjectHttpUrl = "v1/cnad/protected-ips"
+		getProtectedObjectProduct = "aad"
+	)
+	getProtectedObjectClient, err := cfg.NewServiceClient(getProtectedObjectProduct, "")
+	if err != nil {
+		return nil, fmt.Errorf("error creating CNAD Client: %s", err)
+	}
+
+	queryParam := fmt.Sprintf("?package_id=%v", state.Primary.ID)
+	getProtectedObjectPath := getProtectedObjectClient.Endpoint + getProtectedObjectHttpUrl
+	getProtectedObjectPath += queryParam
+
+	getProtectedObjectsResp, err := pagination.ListAllItems(
+		getProtectedObjectClient,
+		"offset",
+		getProtectedObjectPath,
+		&pagination.QueryOpts{MarkerField: ""})
+
+	if err != nil {
+		// here is no special error code
+		return nil, fmt.Errorf("error retrieving protected objects, %s", err)
+	}
+
+	getProtectedObjectsRespJson, err := json.Marshal(getProtectedObjectsResp)
+	if err != nil {
+		return nil, err
+	}
+	var getProtectedObjectsRespBody interface{}
+	if err := json.Unmarshal(getProtectedObjectsRespJson, &getProtectedObjectsRespBody); err != nil {
+		return nil, err
+	}
+
+	curJson := utils.PathSearch("items", getProtectedObjectsRespBody, make([]interface{}, 0))
+	curArray := curJson.([]interface{})
+	if len(curArray) == 0 {
+		return nil, fmt.Errorf("error retrieving advanced protected objects")
+	}
+	return curArray, nil
+}
+
+// Due to testing environment issues, updating operations cannot be tested
+func TestAccProtectedObject_basic(t *testing.T) {
+	var obj interface{}
+	rName := "huaweicloud_cnad_advanced_protected_object.test"
+
+	rc := acceptance.InitResourceCheck(
+		rName,
+		&obj,
+		getProtectedObjectResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckCNADInstance(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testProtectedObject_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttrPair(rName, "instance_id",
+						"data.huaweicloud_cnad_advanced_instances.test",
+						"instances.0.instance_id"),
+					resource.TestCheckResourceAttrPair(rName, "protected_objects.0.id",
+						"data.huaweicloud_cnad_advanced_available_objects.test",
+						"protected_objects.0.id"),
+					resource.TestCheckResourceAttrPair(rName, "protected_objects.0.ip_address",
+						"data.huaweicloud_cnad_advanced_available_objects.test",
+						"protected_objects.0.ip_address"),
+					resource.TestCheckResourceAttrPair(rName, "protected_objects.0.type",
+						"data.huaweicloud_cnad_advanced_available_objects.test",
+						"protected_objects.0.type"),
+					resource.TestCheckResourceAttrSet(rName, "protected_objects.0.instance_id"),
+					resource.TestCheckResourceAttrSet(rName, "protected_objects.0.instance_name"),
+					resource.TestCheckResourceAttrSet(rName, "protected_objects.0.instance_version"),
+					resource.TestCheckResourceAttrSet(rName, "protected_objects.0.region"),
+					resource.TestCheckResourceAttrSet(rName, "protected_objects.0.status"),
+					resource.TestCheckResourceAttrSet(rName, "protected_objects.0.block_threshold"),
+					resource.TestCheckResourceAttrSet(rName, "protected_objects.0.clean_threshold"),
+				),
+			},
+			{
+				ResourceName:      rName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+const testProtectedObject_base = `
+data "huaweicloud_cnad_advanced_instances" "test" {}
+
+data "huaweicloud_cnad_advanced_available_objects" "test" {
+  instance_id = data.huaweicloud_cnad_advanced_instances.test.instances.0.instance_id
+}
+`
+
+func testProtectedObject_basic() string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_cnad_advanced_protected_object" "test" {
+  instance_id = data.huaweicloud_cnad_advanced_instances.test.instances.0.instance_id
+  protected_objects {
+    id         = data.huaweicloud_cnad_advanced_available_objects.test.protected_objects.0.id
+    ip_address = data.huaweicloud_cnad_advanced_available_objects.test.protected_objects.0.ip_address
+    type       = data.huaweicloud_cnad_advanced_available_objects.test.protected_objects.0.type
+  }
+}
+`, testProtectedObject_base)
+}
