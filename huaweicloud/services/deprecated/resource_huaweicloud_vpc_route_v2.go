a8a6f18bd862917a1494cacfab360ac555f5d55c
shichangkuo <shichangkuo90@gmail.com>
2023-07-14 16:09:53 +0800

    chore: add deprecation message in vpc_route_v2 resource (#3188)
    * chore: add deprecation message for vpc_route_v2 resource
    
    * rename the resource file of vpc_route
    
    * fix lint issues

---
+++ huaweicloud/services/deprecated/resource_huaweicloud_vpc_route_v2.go
@@ -0,0 +1,177 @@
+package deprecated
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/chnsz/golangsdk"
+	"github.com/chnsz/golangsdk/openstack/networking/v2/routes"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func ResourceVPCRouteV2() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceVpcRouteV2Create,
+		ReadContext:   resourceVpcRouteV2Read,
+		DeleteContext: resourceVpcRouteV2Delete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		DeprecationMessage: "use huaweicloud_vpc_route resource instead",
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{ // request and response parameters
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+			},
+			"type": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"nexthop": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"destination": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: utils.ValidateCIDR,
+			},
+			"vpc_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceVpcRouteV2Create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*config.Config)
+	vpcRouteClient, err := config.NetworkingV2Client(config.GetRegion(d))
+
+	if err != nil {
+		return diag.Errorf("error creating vpc route client: %s", err)
+	}
+
+	createOpts := routes.CreateOpts{
+		Type:        d.Get("type").(string),
+		NextHop:     d.Get("nexthop").(string),
+		Destination: d.Get("destination").(string),
+		VPC_ID:      d.Get("vpc_id").(string),
+	}
+
+	n, err := routes.Create(vpcRouteClient, createOpts).Extract()
+
+	if err != nil {
+		return diag.Errorf("error creating VPC route: %s", err)
+	}
+	d.SetId(n.RouteID)
+
+	log.Printf("[INFO] Vpc Route ID: %s", n.RouteID)
+
+	d.SetId(n.RouteID)
+
+	return resourceVpcRouteV2Read(ctx, d, meta)
+
+}
+
+func resourceVpcRouteV2Read(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*config.Config)
+	vpcRouteClient, err := config.NetworkingV2Client(config.GetRegion(d))
+	if err != nil {
+		return diag.Errorf("error creating Vpc route client: %s", err)
+	}
+
+	n, err := routes.Get(vpcRouteClient, d.Id()).Extract()
+	if err != nil {
+		if _, ok := err.(golangsdk.ErrDefault404); ok {
+			d.SetId("")
+			return nil
+		}
+
+		return diag.Errorf("error retrieving Vpc route: %s", err)
+	}
+
+	d.Set("type", n.Type)
+	d.Set("nexthop", n.NextHop)
+	d.Set("destination", n.Destination)
+	d.Set("vpc_id", n.VPC_ID)
+	d.Set("region", config.GetRegion(d))
+
+	return nil
+}
+
+func resourceVpcRouteV2Delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+
+	config := meta.(*config.Config)
+	vpcRouteClient, err := config.NetworkingV2Client(config.GetRegion(d))
+	if err != nil {
+		return diag.Errorf("error creating vpc route: %s", err)
+	}
+
+	stateConf := &resource.StateChangeConf{
+		Pending:    []string{"ACTIVE"},
+		Target:     []string{"DELETED"},
+		Refresh:    waitForVpcRouteDelete(vpcRouteClient, d.Id()),
+		Timeout:    d.Timeout(schema.TimeoutDelete),
+		Delay:      5 * time.Second,
+		MinTimeout: 3 * time.Second,
+	}
+
+	_, err = stateConf.WaitForStateContext(ctx)
+	if err != nil {
+		return diag.Errorf("error deleting Vpc route: %s", err)
+	}
+
+	d.SetId("")
+	return nil
+}
+
+func waitForVpcRouteDelete(vpcRouteClient *golangsdk.ServiceClient, routeId string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+
+		r, err := routes.Get(vpcRouteClient, routeId).Extract()
+
+		if err != nil {
+			if _, ok := err.(golangsdk.ErrDefault404); ok {
+				log.Printf("[INFO] Successfully deleted vpc route %s", routeId)
+				return r, "DELETED", nil
+			}
+			return r, "ACTIVE", err
+		}
+
+		err = routes.Delete(vpcRouteClient, routeId).ExtractErr()
+		log.Printf("[DEBUG] Value if error: %#v", err)
+
+		if err != nil {
+			if _, ok := err.(golangsdk.ErrDefault404); ok {
+				log.Printf("[INFO] Successfully deleted vpc route %s", routeId)
+				return r, "DELETED", nil
+			}
+			if errCode, ok := err.(golangsdk.ErrUnexpectedResponseCode); ok {
+				if errCode.Actual == 409 {
+					return r, "ACTIVE", nil
+				}
+			}
+			return r, "ACTIVE", err
+		}
+
+		return r, "ACTIVE", nil
+	}
+}
--- huaweicloud/services/vpc/resource_huaweicloud_vpc_route.go
+++ huaweicloud/services/vpc/resource_huaweicloud_vpc_route.go
@@ -2,25 +2,36 @@ package vpc
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"regexp"
+	"strings"
 	"time"
 
-	"github.com/chnsz/golangsdk"
-	"github.com/chnsz/golangsdk/openstack/networking/v2/routes"
+	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	"github.com/chnsz/golangsdk"
+	"github.com/chnsz/golangsdk/openstack/networking/v1/routetables"
+	"github.com/chnsz/golangsdk/openstack/networking/v2/routes"
+	"github.com/chnsz/golangsdk/pagination"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
-func ResourceVPCRouteV2() *schema.Resource {
+func ResourceVPCRouteTableRoute() *schema.Resource {
 	return &schema.Resource{
-		CreateContext: resourceVpcRouteV2Create,
-		ReadContext:   resourceVpcRouteV2Read,
-		DeleteContext: resourceVpcRouteV2Delete,
+		CreateContext: resourceVpcRTBRouteCreate,
+		ReadContext:   resourceVpcRTBRouteRead,
+		UpdateContext: resourceVpcRTBRouteUpdate,
+		DeleteContext: resourceVpcRTBRouteDelete,
+
 		Importer: &schema.ResourceImporter{
-			StateContext: schema.ImportStatePassthroughContext,
+			StateContext: resourceVpcRTBRouteImportState,
 		},
 
 		Timeouts: &schema.ResourceTimeout{
@@ -28,19 +39,14 @@ func ResourceVPCRouteV2() *schema.Resource {
 			Delete: schema.DefaultTimeout(10 * time.Minute),
 		},
 
-		Schema: map[string]*schema.Schema{ // request and response parameters
+		Schema: map[string]*schema.Schema{
 			"region": {
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
 				Computed: true,
 			},
-			"type": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
-			"nexthop": {
+			"vpc_id": {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
@@ -51,126 +57,325 @@ func ResourceVPCRouteV2() *schema.Resource {
 				ForceNew:     true,
 				ValidateFunc: utils.ValidateCIDR,
 			},
-			"vpc_id": {
+			"type": {
 				Type:     schema.TypeString,
 				Required: true,
+			},
+			"nexthop": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ValidateFunc: validation.All(
+					validation.StringLenBetween(0, 255),
+					validation.StringMatch(regexp.MustCompile("^[^<>]*$"),
+						"The angle brackets (< and >) are not allowed."),
+				),
+			},
+			"route_table_id": {
+				Type:     schema.TypeString,
+				Optional: true,
 				ForceNew: true,
+				Computed: true,
+			},
+			"route_table_name": {
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 		},
 	}
 }
 
-func resourceVpcRouteV2Create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	config := meta.(*config.Config)
-	vpcRouteClient, err := config.NetworkingV2Client(config.GetRegion(d))
-
+func resourceVpcRTBRouteCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	vpcClient, err := cfg.NetworkingV1Client(cfg.GetRegion(d))
 	if err != nil {
-		return diag.Errorf("error creating vpc route client: %s", err)
+		return diag.Errorf("error creating VPC client: %s", err)
 	}
 
-	createOpts := routes.CreateOpts{
+	var routeTableID string
+	vpcID := d.Get("vpc_id").(string)
+	if v, ok := d.GetOk("route_table_id"); ok {
+		routeTableID = v.(string)
+	} else {
+		routeTableID, err = getDefaultRouteTable(vpcClient, vpcID)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	destination := d.Get("destination").(string)
+	desc := d.Get("description").(string)
+	routeOpts := routetables.RouteOpts{
 		Type:        d.Get("type").(string),
 		NextHop:     d.Get("nexthop").(string),
-		Destination: d.Get("destination").(string),
-		VPC_ID:      d.Get("vpc_id").(string),
+		Destination: destination,
+		Description: &desc,
 	}
 
-	n, err := routes.Create(vpcRouteClient, createOpts).Extract()
+	updateOpts := routetables.UpdateOpts{
+		Routes: map[string][]routetables.RouteOpts{
+			"add": {routeOpts},
+		},
+	}
 
+	log.Printf("[DEBUG] Add route in VPC route table[%s]: %#v", routeTableID, updateOpts)
+	_, err = routetables.Update(vpcClient, routeTableID, updateOpts).Extract()
 	if err != nil {
 		return diag.Errorf("error creating VPC route: %s", err)
 	}
-	d.SetId(n.RouteID)
 
-	log.Printf("[INFO] Vpc Route ID: %s", n.RouteID)
+	routeID := fmt.Sprintf("%s/%s", routeTableID, destination)
+	d.SetId(routeID)
 
-	d.SetId(n.RouteID)
-
-	return resourceVpcRouteV2Read(ctx, d, meta)
-
+	return resourceVpcRTBRouteRead(ctx, d, meta)
 }
 
-func resourceVpcRouteV2Read(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	config := meta.(*config.Config)
-	vpcRouteClient, err := config.NetworkingV2Client(config.GetRegion(d))
+func resourceVpcRTBRouteRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	vpcClient, err := cfg.NetworkingV1Client(region)
 	if err != nil {
-		return diag.Errorf("error creating Vpc route client: %s", err)
+		return diag.Errorf("error creating VPC client: %s", err)
 	}
 
-	n, err := routes.Get(vpcRouteClient, d.Id()).Extract()
-	if err != nil {
-		if _, ok := err.(golangsdk.ErrDefault404); ok {
-			d.SetId("")
-			return nil
+	var diags diag.Diagnostics
+	routeTableID, destination := parseResourceID(d.Id())
+
+	// Compatible with previous versions: conver ID to new format
+	if routeTableID == "" {
+		oldID := d.Id()
+		log.Printf("[WARN] The resource ID %s is in the old format, try to upgrade it to the new format", oldID)
+
+		newID, subDiags := convertRouteIDtoNewFormat(d, cfg, oldID)
+		if subDiags.HasError() {
+			return subDiags
 		}
 
-		return diag.Errorf("error retrieving Vpc route: %s", err)
+		diags = subDiags
+		d.SetId(newID)
+		log.Printf("[DEBUG] The resource ID %s has upgraded to %s", oldID, d.Id())
+		routeTableID, destination = parseResourceID(newID)
 	}
 
-	d.Set("type", n.Type)
-	d.Set("nexthop", n.NextHop)
-	d.Set("destination", n.Destination)
-	d.Set("vpc_id", n.VPC_ID)
-	d.Set("region", config.GetRegion(d))
+	routeTable, err := routetables.Get(vpcClient, routeTableID).Extract()
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "VPC route table")
+	}
 
-	return nil
+	var route *routetables.Route
+	for index := range routeTable.Routes {
+		if routeTable.Routes[index].DestinationCIDR == destination {
+			route = &routeTable.Routes[index]
+			break
+		}
+	}
+
+	if route == nil {
+		log.Printf("[INFO] Since can not find destination %s in the vpc route %s, remove %s from state",
+			routeTableID, destination, d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	mErr := multierror.Append(nil,
+		d.Set("region", region),
+		d.Set("type", route.Type),
+		d.Set("nexthop", route.NextHop),
+		d.Set("destination", route.DestinationCIDR),
+		d.Set("description", route.Description),
+		d.Set("vpc_id", routeTable.VpcID),
+		d.Set("route_table_id", routeTable.ID),
+		d.Set("route_table_name", routeTable.Name),
+	)
+
+	if err := mErr.ErrorOrNil(); err != nil {
+		diags = append(diags, diag.Errorf("error saving VPC route: %s", err)[0])
+	}
+
+	return diags
 }
 
-func resourceVpcRouteV2Delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-
-	config := meta.(*config.Config)
-	vpcRouteClient, err := config.NetworkingV2Client(config.GetRegion(d))
+func resourceVpcRTBRouteUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	vpcClient, err := cfg.NetworkingV1Client(cfg.GetRegion(d))
 	if err != nil {
-		return diag.Errorf("error creating vpc route: %s", err)
+		return diag.Errorf("error creating VPC client: %s", err)
 	}
 
-	stateConf := &resource.StateChangeConf{
-		Pending:    []string{"ACTIVE"},
-		Target:     []string{"DELETED"},
-		Refresh:    waitForVpcRouteDelete(vpcRouteClient, d.Id()),
-		Timeout:    d.Timeout(schema.TimeoutDelete),
-		Delay:      5 * time.Second,
-		MinTimeout: 3 * time.Second,
+	routeTableID, _ := parseResourceID(d.Id())
+	desc := d.Get("description").(string)
+	routeOpts := routetables.RouteOpts{
+		Type:        d.Get("type").(string),
+		NextHop:     d.Get("nexthop").(string),
+		Destination: d.Get("destination").(string),
+		Description: &desc,
 	}
 
-	_, err = stateConf.WaitForStateContext(ctx)
+	updateOpts := routetables.UpdateOpts{
+		Routes: map[string][]routetables.RouteOpts{
+			"mod": {routeOpts},
+		},
+	}
+
+	log.Printf("[DEBUG] update route in vpc route table[%s]: %#v", routeTableID, updateOpts)
+	if _, err := routetables.Update(vpcClient, routeTableID, updateOpts).Extract(); err != nil {
+		return diag.Errorf("error updating VPC route: %s", err)
+	}
+
+	return resourceVpcRTBRouteRead(ctx, d, meta)
+}
+
+func resourceVpcRTBRouteDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	vpcClient, err := cfg.NetworkingV1Client(cfg.GetRegion(d))
 	if err != nil {
-		return diag.Errorf("error deleting Vpc route: %s", err)
+		return diag.Errorf("error creating VPC client: %s", err)
+	}
+
+	routeTableID, _ := parseResourceID(d.Id())
+	routeOpts := routetables.RouteOpts{
+		Destination: d.Get("destination").(string),
+		Type:        d.Get("type").(string),
+		NextHop:     d.Get("nexthop").(string),
+	}
+
+	updateOpts := routetables.UpdateOpts{
+		Routes: map[string][]routetables.RouteOpts{
+			"del": {routeOpts},
+		},
+	}
+
+	log.Printf("[DEBUG] delete route in vpc route table[%s]: %#v", routeTableID, updateOpts)
+	if _, err := routetables.Update(vpcClient, routeTableID, updateOpts).Extract(); err != nil {
+		return diag.Errorf("error deleting VPC route: %s", err)
 	}
 
 	d.SetId("")
 	return nil
 }
 
-func waitForVpcRouteDelete(vpcRouteClient *golangsdk.ServiceClient, routeId string) resource.StateRefreshFunc {
-	return func() (interface{}, string, error) {
-
-		r, err := routes.Get(vpcRouteClient, routeId).Extract()
-
-		if err != nil {
-			if _, ok := err.(golangsdk.ErrDefault404); ok {
-				log.Printf("[INFO] Successfully deleted vpc route %s", routeId)
-				return r, "DELETED", nil
-			}
-			return r, "ACTIVE", err
-		}
-
-		err = routes.Delete(vpcRouteClient, routeId).ExtractErr()
-		log.Printf("[DEBUG] Value if error: %#v", err)
-
-		if err != nil {
-			if _, ok := err.(golangsdk.ErrDefault404); ok {
-				log.Printf("[INFO] Successfully deleted vpc route %s", routeId)
-				return r, "DELETED", nil
-			}
-			if errCode, ok := err.(golangsdk.ErrUnexpectedResponseCode); ok {
-				if errCode.Actual == 409 {
-					return r, "ACTIVE", nil
-				}
-			}
-			return r, "ACTIVE", err
-		}
-
-		return r, "ACTIVE", nil
+func getDefaultRouteTable(client *golangsdk.ServiceClient, vpcID string) (string, error) {
+	listOpts := routetables.ListOpts{
+		VpcID: vpcID,
 	}
+	pager := routetables.List(client, &listOpts)
+
+	defaultID := ""
+	err := pager.EachPage(func(page pagination.Page) (b bool, err error) {
+		tableList, err := routetables.ExtractRouteTables(page)
+		if err != nil {
+			return false, err
+		}
+		for _, table := range tableList {
+			if table.Default {
+				// find the default route table, stop iterating
+				defaultID = table.ID
+				return false, nil
+			}
+		}
+		return true, nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+	return defaultID, nil
+}
+
+func parseResourceID(id string) (tableID, destination string) {
+	parts := strings.SplitN(id, "/", 2)
+	if len(parts) != 2 {
+		return
+	}
+
+	tableID, destination = parts[0], parts[1]
+	return
+}
+
+func resourceVpcRTBRouteImportState(_ context.Context, d *schema.ResourceData, _ interface{}) ([]*schema.ResourceData, error) {
+	routeID, _ := parseResourceID(d.Id())
+	if routeID == "" {
+		return nil, fmt.Errorf("invalid format specified for import id, must be <route_table_id>/<destination>")
+	}
+
+	return []*schema.ResourceData{d}, nil
+}
+
+func convertRouteIDtoNewFormat(d *schema.ResourceData, conf *config.Config, oldID string) (string, diag.Diagnostics) {
+	var diags = diag.Diagnostics{
+		diag.Diagnostic{
+			Severity: diag.Warning,
+			Summary:  "Deprecated ID format",
+			Detail:   fmt.Sprintf("the resource ID %s is in the old format, try to upgrade it to the new format", oldID),
+		},
+	}
+
+	region := conf.GetRegion(d)
+	vpcClient, err := conf.NetworkingV1Client(region)
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Error creating VPC client",
+			Detail:   err.Error(),
+		})
+		return "", diags
+	}
+	networkClient, err := conf.NetworkingV2Client(region)
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Error creating network client",
+			Detail:   err.Error(),
+		})
+		return "", diags
+	}
+
+	if _, err := routes.Get(networkClient, oldID).Extract(); err != nil {
+		if _, ok := err.(golangsdk.ErrDefault404); ok {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Warning,
+				Summary:  fmt.Sprintf("the resource %s does not exist", oldID),
+			})
+			d.SetId("")
+		} else {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  "Error retrieving VPC route",
+				Detail:   err.Error(),
+			})
+		}
+
+		return "", diags
+	}
+
+	destination := d.Get("destination").(string)
+	rtbID := d.Get("route_table_id").(string)
+	if destination == "" || rtbID != "" {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "can not get the destination or the route_table_id is not empty",
+		})
+		return "", diags
+	}
+
+	vpcID := d.Get("vpc_id").(string)
+	routeTableID, err := getDefaultRouteTable(vpcClient, vpcID)
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "failed to get the default route table",
+			Detail:   err.Error(),
+		})
+		return "", diags
+	}
+
+	newRouteID := fmt.Sprintf("%s/%s", routeTableID, destination)
+	diags = append(diags, diag.Diagnostic{
+		Severity: diag.Warning,
+		Summary:  fmt.Sprintf("the resource ID is upgraded to %s", newRouteID),
+	})
+	return newRouteID, diags
 }
