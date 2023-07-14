6238bcdcde9114519fa19b6aa3e58e29c3d27838
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2023-07-14 17:50:43 +0800

    feat: add param action, redirect_listener_id to ELB l7 policy (#3183)

--- docs/resources/elb_l7policy.md
+++ docs/resources/elb_l7policy.md
@@ -14,6 +14,7 @@ variable pool_id {}
 
 resource "huaweicloud_elb_l7policy" "policy_1" {
   name             = "policy_1"
+  action           = "REDIRECT_TO_POOL"
   description      = "test description"
   listener_id      = var.listener_id
   redirect_pool_id = var.pool_id
@@ -34,7 +35,24 @@ The following arguments are supported:
 * `listener_id` - (Required, String, ForceNew) The Listener on which the L7 Policy will be associated with. Changing
   this creates a new L7 Policy.
 
-* `redirect_pool_id` - (Required, String) Requests matching this policy will be redirected to the pool with this ID.
+* `action` - (Optional, String, ForceNew) Specifies whether requests are forwarded to another backend server group
+  or redirected to an HTTPS listener. Changing this creates a new L7 Policy. The value ranges:
+  + **REDIRECT_TO_POOL**: Requests are forwarded to the backend server group specified by `redirect_pool_id`.
+  + **REDIRECT_TO_LISTENER**: Requests are redirected from the HTTP listener specified by `listener_id` to the
+    HTTPS listener specified by `redirect_listener_id`.
+  Defaults to **REDIRECT_TO_POOL**.
+
+* `redirect_pool_id` - (Optional, String) Specifies the ID of the backend server group to which traffic is forwarded.
+  This parameter is mandatory when `action` is set to **REDIRECT_TO_POOL**. The backend server group must meet the
+  following requirements:
+  + Cannot be the default backend server group of the listener.
+  + Cannot be the backend server group used by forwarding policies of other listeners.
+
+* `redirect_listener_id` - (Optional, String) Specifies the ID of the listener to which the traffic is redirected.
+  This parameter is mandatory when `action` is set to **REDIRECT_TO_LISTENER**. The listener must meet the
+  following requirements:
+  + Can only be an HTTPS listener.
+  + Can only be a listener of the same load balancer.
 
 ## Attributes Reference
 
@@ -46,14 +64,14 @@ In addition to all arguments above, the following attributes are exported:
 
 This resource provides the following timeouts configuration options:
 
-* `create` - Default is 10 minute.
-* `update` - Default is 10 minute.
-* `delete` - Default is 10 minute.
+* `create` - Default is 10 minutes.
+* `update` - Default is 10 minutes.
+* `delete` - Default is 10 minutes.
 
 ## Import
 
-ELB policy can be imported using the policy ID, e.g.
+ELB policy can be imported using the `id`, e.g.
 
-```
-$ terraform import huaweicloud_elb_policy.policy_1 5c20fdad-7288-11eb-b817-0255ac10158b
+```bash
+$ terraform import huaweicloud_elb_policy.policy_1 <id>
 ```
--- huaweicloud/services/acceptance/elb/resource_huaweicloud_elb_l7policy_test.go
+++ huaweicloud/services/acceptance/elb/resource_huaweicloud_elb_l7policy_test.go
@@ -2,10 +2,8 @@ package elb
 
 import (
 	"fmt"
-	"regexp"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
@@ -15,24 +13,56 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
+func getELBl7PolicyResourceFunc(c *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	lbClient, err := c.ElbV3Client(acceptance.HW_REGION_NAME)
+	if err != nil {
+		return nil, fmt.Errorf("error creating ELB client: %s", err)
+	}
+
+	return l7policies.Get(lbClient, state.Primary.ID).Extract()
+}
+
 func TestAccElbV3L7Policy_basic(t *testing.T) {
 	var l7Policy l7policies.L7Policy
-	rName := fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
+	rName := acceptance.RandomAccResourceName()
+	updateName := acceptance.RandomAccResourceName()
 	resourceName := "huaweicloud_elb_l7policy.test"
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&l7Policy,
+		getELBl7PolicyResourceFunc,
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
 		ProviderFactories: acceptance.TestAccProviderFactories,
-		CheckDestroy:      testAccCheckElbV3L7PolicyDestroy,
+		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckElbV3L7PolicyConfig_basic(rName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckElbV3L7PolicyExists(resourceName, &l7Policy),
+					rc.CheckResourceExists(),
 					resource.TestCheckResourceAttr(resourceName, "name", rName),
 					resource.TestCheckResourceAttr(resourceName, "description", "test description"),
-					resource.TestMatchResourceAttr(resourceName, "listener_id",
-						regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$")),
+					resource.TestCheckResourceAttr(resourceName, "action", "REDIRECT_TO_POOL"),
+					resource.TestCheckResourceAttrPair(resourceName, "listener_id",
+						"huaweicloud_elb_listener.test", "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "redirect_pool_id",
+						"huaweicloud_elb_pool.test", "id"),
+				),
+			},
+			{
+				Config: testAccCheckElbV3L7PolicyConfig_basic_update(rName, updateName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", updateName),
+					resource.TestCheckResourceAttr(resourceName, "description", "test description update"),
+					resource.TestCheckResourceAttr(resourceName, "action", "REDIRECT_TO_POOL"),
+					resource.TestCheckResourceAttrPair(resourceName, "listener_id",
+						"huaweicloud_elb_listener.test", "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "redirect_pool_id",
+						"huaweicloud_elb_pool.test_update", "id"),
 				),
 			},
 			{
@@ -44,101 +74,162 @@ func TestAccElbV3L7Policy_basic(t *testing.T) {
 	})
 }
 
-func testAccCheckElbV3L7PolicyDestroy(s *terraform.State) error {
-	cfg := acceptance.TestAccProvider.Meta().(*config.Config)
-	elbClient, err := cfg.ElbV3Client(acceptance.HW_REGION_NAME)
-	if err != nil {
-		return fmt.Errorf("error creating ELB client: %s", err)
-	}
+func TestAccElbV3L7Policy_listener(t *testing.T) {
+	var l7Policy l7policies.L7Policy
+	rName := acceptance.RandomAccResourceName()
+	updateName := acceptance.RandomAccResourceName()
+	resourceName := "huaweicloud_elb_l7policy.test"
 
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "huaweicloud_elb_l7policy" {
-			continue
-		}
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&l7Policy,
+		getELBl7PolicyResourceFunc,
+	)
 
-		_, err := l7policies.Get(elbClient, rs.Primary.ID).Extract()
-		if err == nil {
-			return fmt.Errorf("the L7 Policy still exists: %s", rs.Primary.ID)
-		}
-	}
-
-	return nil
-}
-
-func testAccCheckElbV3L7PolicyExists(n string, l7Policy *l7policies.L7Policy) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("not found: %s", n)
-		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("no ID is set")
-		}
-
-		cfg := acceptance.TestAccProvider.Meta().(*config.Config)
-		lbClient, err := cfg.ElbV3Client(acceptance.HW_REGION_NAME)
-		if err != nil {
-			return fmt.Errorf("error creating ELB client: %s", err)
-		}
-
-		found, err := l7policies.Get(lbClient, rs.Primary.ID).Extract()
-		if err != nil {
-			return err
-		}
-
-		if found.ID != rs.Primary.ID {
-			return fmt.Errorf("policy not found")
-		}
-
-		*l7Policy = *found
-
-		return nil
-	}
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckElbV3L7PolicyConfig_listener(rName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "description", "test description"),
+					resource.TestCheckResourceAttr(resourceName, "action", "REDIRECT_TO_LISTENER"),
+					resource.TestCheckResourceAttrPair(resourceName, "listener_id",
+						"huaweicloud_elb_listener.test", "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "redirect_listener_id",
+						"huaweicloud_elb_listener.test_redirect", "id"),
+				),
+			},
+			{
+				Config: testAccCheckElbV3L7PolicyConfig_listener_update(rName, updateName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", updateName),
+					resource.TestCheckResourceAttr(resourceName, "description", "test description update"),
+					resource.TestCheckResourceAttr(resourceName, "action", "REDIRECT_TO_LISTENER"),
+					resource.TestCheckResourceAttrPair(resourceName, "listener_id",
+						"huaweicloud_elb_listener.test", "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "redirect_listener_id",
+						"huaweicloud_elb_listener.test_redirect_update", "id"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
 }
 
 func testAccCheckElbV3L7PolicyConfig_basic(rName string) string {
 	return fmt.Sprintf(`
-data "huaweicloud_vpc_subnet" "test" {
-  name = "subnet-default"
-}
-
-data "huaweicloud_availability_zones" "test" {}
-
-resource "huaweicloud_elb_loadbalancer" "test" {
-  name            = "%s"
-  ipv4_subnet_id  = data.huaweicloud_vpc_subnet.test.ipv4_subnet_id
-  ipv6_network_id = data.huaweicloud_vpc_subnet.test.id
-
-  availability_zone = [
-    data.huaweicloud_availability_zones.test.names[0]
-  ]
-}
-
-resource "huaweicloud_elb_listener" "test" {
-  name             = "%s"
-  description      = "test description"
-  protocol         = "HTTP"
-  protocol_port    = 8080
-  loadbalancer_id  = huaweicloud_elb_loadbalancer.test.id
-  forward_eip      = true
-  idle_timeout     = 60
-  request_timeout  = 60
-  response_timeout = 60
-}
+%[1]s
 
 resource "huaweicloud_elb_pool" "test" {
-  name            = "%s"
+  name            = "%[2]s"
   protocol        = "HTTP"
   lb_method       = "LEAST_CONNECTIONS"
   loadbalancer_id = huaweicloud_elb_loadbalancer.test.id
 }
 
 resource "huaweicloud_elb_l7policy" "test" {
-  name             = "%s"
+  name             = "%[2]s"
   description      = "test description"
+  action           = "REDIRECT_TO_POOL"
   listener_id      = huaweicloud_elb_listener.test.id
   redirect_pool_id = huaweicloud_elb_pool.test.id
 }
-`, rName, rName, rName, rName)
+`, testAccElbV3ListenerConfig_basic(rName), rName)
+}
+
+func testAccCheckElbV3L7PolicyConfig_basic_update(rName, updateName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_elb_pool" "test" {
+  name            = "%[2]s"
+  protocol        = "HTTP"
+  lb_method       = "LEAST_CONNECTIONS"
+  loadbalancer_id = huaweicloud_elb_loadbalancer.test.id
+}
+
+resource "huaweicloud_elb_pool" "test_update" {
+  name            = "%[2]s"
+  protocol        = "HTTP"
+  lb_method       = "LEAST_CONNECTIONS"
+  loadbalancer_id = huaweicloud_elb_loadbalancer.test.id
+}
+
+resource "huaweicloud_elb_l7policy" "test" {
+  name             = "%[2]s"
+  description      = "test description update"
+  action           = "REDIRECT_TO_POOL"
+  listener_id      = huaweicloud_elb_listener.test.id
+  redirect_pool_id = huaweicloud_elb_pool.test_update.id
+}
+`, testAccElbV3ListenerConfig_basic(rName), updateName)
+}
+
+func testAccCheckElbV3L7PolicyConfig_listener(rName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+%[2]s
+
+resource "huaweicloud_elb_listener" "test_redirect" {
+  name                        = "%[3]s"
+  protocol                    = "HTTPS"
+  protocol_port               = 443
+  loadbalancer_id             = huaweicloud_elb_loadbalancer.test.id
+  advanced_forwarding_enabled = true
+  server_certificate          = huaweicloud_elb_certificate.test.id
+}
+
+resource "huaweicloud_elb_l7policy" "test" {
+  name                 = "%[3]s"
+  description          = "test description"
+  action               = "REDIRECT_TO_LISTENER"
+  listener_id          = huaweicloud_elb_listener.test.id
+  redirect_listener_id = huaweicloud_elb_listener.test_redirect.id
+}
+`, testAccElbV3ListenerConfig_basic(rName), testAccElbV3CertificateConfig_basic(rName), rName)
+}
+
+func testAccCheckElbV3L7PolicyConfig_listener_update(rName, updateName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+%[2]s
+
+resource "huaweicloud_elb_listener" "test_redirect" {
+  name                        = "%[3]s"
+  protocol                    = "HTTPS"
+  protocol_port               = 443
+  loadbalancer_id             = huaweicloud_elb_loadbalancer.test.id
+  advanced_forwarding_enabled = true
+  server_certificate          = huaweicloud_elb_certificate.test.id
+}
+
+resource "huaweicloud_elb_listener" "test_redirect_update" {
+  name                        = "%[4]s"
+  protocol                    = "HTTPS"
+  protocol_port               = 448
+  loadbalancer_id             = huaweicloud_elb_loadbalancer.test.id
+  advanced_forwarding_enabled = true
+  server_certificate          = huaweicloud_elb_certificate.test.id
+}
+
+resource "huaweicloud_elb_l7policy" "test" {
+  name                 = "%[4]s"
+  description          = "test description update"
+  action               = "REDIRECT_TO_LISTENER"
+  listener_id          = huaweicloud_elb_listener.test.id
+  redirect_listener_id = huaweicloud_elb_listener.test_redirect_update.id
+}
+`, testAccElbV3ListenerConfig_basic(rName), testAccElbV3CertificateConfig_basic(rName), rName, updateName)
 }
