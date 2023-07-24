d201feb63034554e59db6fa81419af71a65f4c71
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2023-07-24 20:24:38 +0800

    feat(ELB): elb policy support priority and redirect to url and fixed response (#3212)

--- docs/resources/elb_l7policy.md
+++ docs/resources/elb_l7policy.md
@@ -8,6 +8,8 @@ Manages an ELB L7 Policy resource within HuaweiCloud.
 
 ## Example Usage
 
+### ELB L7 Policy redirect to pool
+
 ```hcl
 variable listener_id {}
 variable pool_id {}
@@ -15,9 +17,78 @@ variable pool_id {}
 resource "huaweicloud_elb_l7policy" "policy_1" {
   name             = "policy_1"
   action           = "REDIRECT_TO_POOL"
+  priority         = 20
   description      = "test description"
   listener_id      = var.listener_id
   redirect_pool_id = var.pool_id
+
+  redirect_pools_extend_config {
+    rewrite_url_enabled = true
+    
+    rewrite_url_config {
+      host  = "test.com"
+      path  = "/path"
+      query = "abc"
+    }
+  }
+}
+```
+
+### ELB L7 Policy redirect to listener
+
+```hcl
+variable listener_id {}
+variable redirect_listener_id {}
+
+resource "huaweicloud_elb_l7policy" "policy_1" {
+  name                 = "policy_1"
+  action               = "REDIRECT_TO_LISTENER"
+  description          = "test description"
+  listener_id          = var.listener_id
+  redirect_listener_id = var.redirect_listener_id
+}
+```
+
+### ELB L7 Policy redirect to URL
+
+```hcl
+variable listener_id {}
+
+resource "huaweicloud_elb_l7policy" "policy_1" {
+  name        = "policy_1"
+  action      = "REDIRECT_TO_URL"
+  priority    = 20
+  description = "test description"
+  listener_id = var.listener_id
+  
+  redirect_url_config {
+    protocol    = "HTTP"
+    host        = "test.com"
+    port        = "6666"
+    path        = "/test_policy"
+    query       = "test_query"
+    status_code = "301"
+  }
+}
+```
+
+### ELB L7 Policy redirect to fixed response
+
+```hcl
+variable listener_id {}
+
+resource "huaweicloud_elb_l7policy" "policy_1" {
+  name        = "policy_1"
+  action      = "FIXED_RESPONSE"
+  priority    = 20
+  description = "test description"
+  listener_id = var.listener_id
+  
+  fixed_response_config {
+    status_code  = "200"
+    content_type = "application/json"
+    message_body = "it is a test"
+  }
 }
 ```
 
@@ -30,29 +101,110 @@ The following arguments are supported:
 
 * `name` - (Optional, String) Human-readable name for the L7 Policy. Does not have to be unique.
 
+* `priority` - (Optional, Int) The forwarding policy priority. A smaller value indicates a higher priority. The value
+  must be unique for forwarding policies of the same listener. This parameter will take effect only when
+  `enhance_l7policy_enable` of the listener is set to **true**. If `action` is set to **REDIRECT_TO_LISTENER**,
+  the value can only be 0.
+
 * `description` - (Optional, String) Human-readable description for the L7 Policy.
 
 * `listener_id` - (Required, String, ForceNew) The Listener on which the L7 Policy will be associated with. Changing
   this creates a new L7 Policy.
 
-* `action` - (Optional, String, ForceNew) Specifies whether requests are forwarded to another backend server group
+* `action` - (Optional, String, ForceNew) Whether requests are forwarded to another backend server group
   or redirected to an HTTPS listener. Changing this creates a new L7 Policy. The value ranges:
   + **REDIRECT_TO_POOL**: Requests are forwarded to the backend server group specified by `redirect_pool_id`.
   + **REDIRECT_TO_LISTENER**: Requests are redirected from the HTTP listener specified by `listener_id` to the
     HTTPS listener specified by `redirect_listener_id`.
+  + **REDIRECT_TO_URL**: Requests are forwarded to another URL whose config specified by `redirect_url_config`.
+  + **FIXED_RESPONSE**: Requests are forwarded to a fixed response body specified by `fixed_response_config`.
   Defaults to **REDIRECT_TO_POOL**.
 
-* `redirect_pool_id` - (Optional, String) Specifies the ID of the backend server group to which traffic is forwarded.
+* `redirect_pool_id` - (Optional, String) The ID of the backend server group to which traffic is forwarded.
   This parameter is mandatory when `action` is set to **REDIRECT_TO_POOL**. The backend server group must meet the
   following requirements:
   + Cannot be the default backend server group of the listener.
   + Cannot be the backend server group used by forwarding policies of other listeners.
 
-* `redirect_listener_id` - (Optional, String) Specifies the ID of the listener to which the traffic is redirected.
+* `redirect_pools_extend_config` - (Optional, List) The config of the backend server group to which the
+  traffic is redirected. This parameter will take effect when `action` is set to **REDIRECT_TO_POOL**.
+  The [redirect_pools_extend_config](#redirect_pools_extend_config_object) structure is documented below.
+
+* `redirect_listener_id` - (Optional, String) The ID of the listener to which the traffic is redirected.
   This parameter is mandatory when `action` is set to **REDIRECT_TO_LISTENER**. The listener must meet the
   following requirements:
   + Can only be an HTTPS listener.
   + Can only be a listener of the same load balancer.
+
+* `redirect_url_config` - (Optional, List) The URL config to which the traffic is redirected.
+  This parameter is mandatory when `action` is set to **REDIRECT_TO_URL**. The `enhance_l7policy_enable` of the
+  listener must be set to **true**.
+  The [redirect_url_config](#redirect_url_config_object) structure is documented below.
+
+* `fixed_response_config` - (Optional, List) The fixed configuration of the page to which the traffic is
+  redirected. This parameter is mandatory when `action` is set to **FIXED_RESPONSE**. The `enhance_l7policy_enable` of
+  the listener must be set to **true**.
+  The [fixed_response_config](#fixed_response_config_object) structure is documented below.
+
+<a name="redirect_pools_extend_config_object"></a>
+The `redirect_pools_extend_config` block supports:
+
+* `rewrite_url_enabled` - (Optional, Bool) Whether the rewrite url is enabled.
+
+* `rewrite_url_config` - (Optional, List) The rewrite url config. This parameter is mandatory when `rewrite_url_enabled`
+  is set to **true**.
+  The [rewrite_url_config](#rewrite_url_config_object) structure is documented below.
+
+<a name="rewrite_url_config_object"></a>
+The `rewrite_url_config` block supports:
+
+* `host` - (Optional, String) The host name of the rewrite URL. The value can contain only letters, digits, and
+  periods (.) and must start with a letter or digit. Defaults to **${host}**, indicates inherit original value.
+
+* `path` - (Optional, String) The path of the rewrite URL. The value can contain only letters, digits, and special
+  characters _~';@^-%#&$.+?,=!:|\/(), and must start with a slash (/). Defaults to **${path}**, indicates inherit
+  original value.
+
+* `query` - (Optional, String) The query of the rewrite URL. The value can contain only letters, digits, and special
+  characters !$&'()+,-./:;=?@^_`. Defaults to **${query}**, indicates inherit original value.
+
+<a name="redirect_url_config_object"></a>
+The `redirect_url_config` block supports:
+
+* `status_code` - (Required, String) The status code returned after the requests are redirected.
+  Value options: **301**, **302**, **303**, **307**, **308**.
+
+* `protocol` - (Optional, String) The protocol for redirection. Value options: **HTTP**, **HTTPS**, **${protocol}**.
+  Defaults to **${protocol}**, indicating that the path of the request will be used.
+
+* `host` - (Optional, String) The host name that requests are redirected to. The value can contain only letters,
+  digits, hyphens (-), and periods (.) and must start with a letter or digit. Defaults to **${host}**, indicating
+  that the host of the request will be used.
+
+* `port` - (Optional, String) The  port that requests are redirected to. Defaults to **${port}**, indicating that
+  the port of the request will be used.
+
+* `path` - (Optional, String) The path that requests are redirected to. The value can contain only letters, digits,
+  and special characters _~';@^- %#&$.*+?,=!:|/()[]{} and must start with a slash (/).
+  Defaults to **${path}**, indicating that the path of the request will be used.
+
+* `query` - (Optional, String) The query string set in the URL for redirection. The value is case-sensitive and can
+  contain only letters, digits, and special characters !$&'()*+,-./:;=?@^_\`. Defaults to **${query}**, indicating that
+  the query string of the request will be used.
+  For example, in the URL `https://www.xxx.com:8080/elb?type=loadbalancer`, **${query}** indicates **type=loadbalancer**.
+  If this parameter is set to **${query}&name=my_name**, the URL will be redirected to
+  URL `https://www.xxx.com:8080/elb?type=loadbalancer&name=my_name`.
+
+<a name="fixed_response_config_object"></a>
+The `fixed_response_config` block supports:
+
+* `status_code` - (Required, String) The fixed HTTP status code configured in the forwarding rule. The value can be
+  any integer in the range of **200–299**, **400–499**, or **500–599**.
+
+* `content_type` - (Optional, String) The format of the response body. Value options: **text/plain**, **text/css**,
+  **text/html**, **application/javascript**, or **application/json**. Defaults to: **text/plain**.
+
+* `message_body` - (Optional, String) The content of the response message body.
 
 ## Attributes Reference
 
--- huaweicloud/services/acceptance/elb/resource_huaweicloud_elb_l7policy_test.go
+++ huaweicloud/services/acceptance/elb/resource_huaweicloud_elb_l7policy_test.go
@@ -46,10 +46,19 @@ func TestAccElbV3L7Policy_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "name", rName),
 					resource.TestCheckResourceAttr(resourceName, "description", "test description"),
 					resource.TestCheckResourceAttr(resourceName, "action", "REDIRECT_TO_POOL"),
+					resource.TestCheckResourceAttr(resourceName, "priority", "20"),
 					resource.TestCheckResourceAttrPair(resourceName, "listener_id",
 						"huaweicloud_elb_listener.test", "id"),
 					resource.TestCheckResourceAttrPair(resourceName, "redirect_pool_id",
 						"huaweicloud_elb_pool.test", "id"),
+					resource.TestCheckResourceAttr(resourceName,
+						"redirect_pools_extend_config.0.rewrite_url_enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName,
+						"redirect_pools_extend_config.0.rewrite_url_config.0.host", "testhost.com"),
+					resource.TestCheckResourceAttr(resourceName,
+						"redirect_pools_extend_config.0.rewrite_url_config.0.path", "/test_path"),
+					resource.TestCheckResourceAttr(resourceName,
+						"redirect_pools_extend_config.0.rewrite_url_config.0.query", "test_query"),
 				),
 			},
 			{
@@ -59,10 +68,19 @@ func TestAccElbV3L7Policy_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "name", updateName),
 					resource.TestCheckResourceAttr(resourceName, "description", "test description update"),
 					resource.TestCheckResourceAttr(resourceName, "action", "REDIRECT_TO_POOL"),
+					resource.TestCheckResourceAttr(resourceName, "priority", "50"),
 					resource.TestCheckResourceAttrPair(resourceName, "listener_id",
 						"huaweicloud_elb_listener.test", "id"),
 					resource.TestCheckResourceAttrPair(resourceName, "redirect_pool_id",
 						"huaweicloud_elb_pool.test_update", "id"),
+					resource.TestCheckResourceAttr(resourceName,
+						"redirect_pools_extend_config.0.rewrite_url_enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName,
+						"redirect_pools_extend_config.0.rewrite_url_config.0.host", "testhostupdate.com"),
+					resource.TestCheckResourceAttr(resourceName,
+						"redirect_pools_extend_config.0.rewrite_url_config.0.path", "/test_path_update"),
+					resource.TestCheckResourceAttr(resourceName,
+						"redirect_pools_extend_config.0.rewrite_url_config.0.query", "test_query_update"),
 				),
 			},
 			{
@@ -126,6 +144,153 @@ func TestAccElbV3L7Policy_listener(t *testing.T) {
 	})
 }
 
+func TestAccElbV3L7Policy_url(t *testing.T) {
+	var l7Policy l7policies.L7Policy
+	rName := acceptance.RandomAccResourceName()
+	updateName := acceptance.RandomAccResourceName()
+	resourceName := "huaweicloud_elb_l7policy.test"
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&l7Policy,
+		getELBl7PolicyResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckElbV3L7PolicyConfig_url(rName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "description", "test description"),
+					resource.TestCheckResourceAttr(resourceName, "action", "REDIRECT_TO_URL"),
+					resource.TestCheckResourceAttr(resourceName, "priority", "20"),
+					resource.TestCheckResourceAttrPair(resourceName, "listener_id",
+						"huaweicloud_elb_listener.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "redirect_url_config.0.status_code", "301"),
+					resource.TestCheckResourceAttr(resourceName, "redirect_url_config.0.protocol", "HTTP"),
+					resource.TestCheckResourceAttr(resourceName, "redirect_url_config.0.host", "testhost.com"),
+					resource.TestCheckResourceAttr(resourceName, "redirect_url_config.0.port", "6666"),
+					resource.TestCheckResourceAttr(resourceName, "redirect_url_config.0.path", "/test_policy"),
+					resource.TestCheckResourceAttr(resourceName, "redirect_url_config.0.query", "abcd"),
+				),
+			},
+			{
+				Config: testAccCheckElbV3L7PolicyConfig_url_update(rName, updateName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", updateName),
+					resource.TestCheckResourceAttr(resourceName, "description", "test description update"),
+					resource.TestCheckResourceAttr(resourceName, "action", "REDIRECT_TO_URL"),
+					resource.TestCheckResourceAttr(resourceName, "priority", "50"),
+					resource.TestCheckResourceAttrPair(resourceName, "listener_id",
+						"huaweicloud_elb_listener.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "redirect_url_config.0.status_code", "302"),
+					resource.TestCheckResourceAttr(resourceName, "redirect_url_config.0.protocol", "HTTPS"),
+					resource.TestCheckResourceAttr(resourceName,
+						"redirect_url_config.0.host", "testhostupdate.com"),
+					resource.TestCheckResourceAttr(resourceName, "redirect_url_config.0.port", "8888"),
+					resource.TestCheckResourceAttr(resourceName,
+						"redirect_url_config.0.path", "/test_policy_update"),
+					resource.TestCheckResourceAttr(resourceName, "redirect_url_config.0.query", "efgh"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccElbV3L7Policy_fixed_response(t *testing.T) {
+	var l7Policy l7policies.L7Policy
+	rName := acceptance.RandomAccResourceName()
+	updateName := acceptance.RandomAccResourceName()
+	resourceName := "huaweicloud_elb_l7policy.test"
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&l7Policy,
+		getELBl7PolicyResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckElbV3L7PolicyConfig_fixed_response(rName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "description", "test description"),
+					resource.TestCheckResourceAttr(resourceName, "action", "FIXED_RESPONSE"),
+					resource.TestCheckResourceAttr(resourceName, "priority", "20"),
+					resource.TestCheckResourceAttrPair(resourceName, "listener_id",
+						"huaweicloud_elb_listener.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "fixed_response_config.0.status_code", "200"),
+					resource.TestCheckResourceAttr(resourceName,
+						"fixed_response_config.0.content_type", "application/json"),
+					resource.TestCheckResourceAttr(resourceName,
+						"fixed_response_config.0.message_body", "it is a test"),
+				),
+			},
+			{
+				Config: testAccCheckElbV3L7PolicyConfig_fixed_response_update(rName, updateName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", updateName),
+					resource.TestCheckResourceAttr(resourceName, "description", "test description update"),
+					resource.TestCheckResourceAttr(resourceName, "action", "FIXED_RESPONSE"),
+					resource.TestCheckResourceAttr(resourceName, "priority", "50"),
+					resource.TestCheckResourceAttrPair(resourceName, "listener_id",
+						"huaweicloud_elb_listener.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "fixed_response_config.0.status_code", "202"),
+					resource.TestCheckResourceAttr(resourceName,
+						"fixed_response_config.0.content_type", "text/css"),
+					resource.TestCheckResourceAttr(resourceName,
+						"fixed_response_config.0.message_body", "it is a test update"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckElbV3L7PolicyConfig_base(rName string) string {
+	return fmt.Sprintf(`
+%[1]s
+resource "huaweicloud_elb_listener" "test" {
+  name                        = "%s"
+  description                 = "test description"
+  protocol                    = "HTTP"
+  protocol_port               = 8080
+  loadbalancer_id             = huaweicloud_elb_loadbalancer.test.id
+  advanced_forwarding_enabled = true
+  forward_eip                 = true
+  idle_timeout                = 62
+  request_timeout             = 63
+  response_timeout            = 64
+
+  tags = {
+    key   = "value"
+    owner = "terraform"
+  }
+}
+`, testAccElbV3LoadBalancerConfig_basic(rName), rName)
+}
+
 func testAccCheckElbV3L7PolicyConfig_basic(rName string) string {
 	return fmt.Sprintf(`
 %[1]s
@@ -141,10 +306,21 @@ resource "huaweicloud_elb_l7policy" "test" {
   name             = "%[2]s"
   description      = "test description"
   action           = "REDIRECT_TO_POOL"
+  priority         = 20
   listener_id      = huaweicloud_elb_listener.test.id
   redirect_pool_id = huaweicloud_elb_pool.test.id
+
+  redirect_pools_extend_config {
+    rewrite_url_enabled = true
+
+    rewrite_url_config {
+      host  = "testhost.com"
+      path  = "/test_path"
+      query = "test_query"
+    }
+  }
 }
-`, testAccElbV3ListenerConfig_basic(rName), rName)
+`, testAccCheckElbV3L7PolicyConfig_base(rName), rName)
 }
 
 func testAccCheckElbV3L7PolicyConfig_basic_update(rName, updateName string) string {
@@ -169,10 +345,21 @@ resource "huaweicloud_elb_l7policy" "test" {
   name             = "%[2]s"
   description      = "test description update"
   action           = "REDIRECT_TO_POOL"
+  priority         = 50
   listener_id      = huaweicloud_elb_listener.test.id
   redirect_pool_id = huaweicloud_elb_pool.test_update.id
+
+  redirect_pools_extend_config {
+    rewrite_url_enabled = true
+
+    rewrite_url_config {
+      host  = "testhostupdate.com"
+      path  = "/test_path_update"
+      query = "test_query_update"
+    }
+  }
 }
-`, testAccElbV3ListenerConfig_basic(rName), updateName)
+`, testAccCheckElbV3L7PolicyConfig_base(rName), updateName)
 }
 
 func testAccCheckElbV3L7PolicyConfig_listener(rName string) string {
@@ -197,7 +384,7 @@ resource "huaweicloud_elb_l7policy" "test" {
   listener_id          = huaweicloud_elb_listener.test.id
   redirect_listener_id = huaweicloud_elb_listener.test_redirect.id
 }
-`, testAccElbV3ListenerConfig_basic(rName), testAccElbV3CertificateConfig_basic(rName), rName)
+`, testAccCheckElbV3L7PolicyConfig_base(rName), testAccElbV3CertificateConfig_basic(rName), rName)
 }
 
 func testAccCheckElbV3L7PolicyConfig_listener_update(rName, updateName string) string {
@@ -231,5 +418,91 @@ resource "huaweicloud_elb_l7policy" "test" {
   listener_id          = huaweicloud_elb_listener.test.id
   redirect_listener_id = huaweicloud_elb_listener.test_redirect_update.id
 }
-`, testAccElbV3ListenerConfig_basic(rName), testAccElbV3CertificateConfig_basic(rName), rName, updateName)
+`, testAccCheckElbV3L7PolicyConfig_base(rName), testAccElbV3CertificateConfig_basic(rName), rName, updateName)
+}
+
+func testAccCheckElbV3L7PolicyConfig_url(rName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_elb_l7policy" "test" {
+  name        = "%[2]s"
+  description = "test description"
+  action      = "REDIRECT_TO_URL"
+  priority    = 20
+  listener_id = huaweicloud_elb_listener.test.id
+
+  redirect_url_config {
+    protocol    = "HTTP"
+    host        = "testhost.com"
+    port        = "6666"
+    path        = "/test_policy"
+    query       = "abcd"
+    status_code = "301"
+  }
+}
+`, testAccCheckElbV3L7PolicyConfig_base(rName), rName)
+}
+
+func testAccCheckElbV3L7PolicyConfig_url_update(rName, updateName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_elb_l7policy" "test" {
+  name        = "%[2]s"
+  description = "test description update"
+  action      = "REDIRECT_TO_URL"
+  priority    = 50
+  listener_id = huaweicloud_elb_listener.test.id
+
+  redirect_url_config {
+    protocol    = "HTTPS"
+    host        = "testhostupdate.com"
+    port        = "8888"
+    path        = "/test_policy_update"
+    query       = "efgh"
+    status_code = "302"
+  }
+}
+`, testAccCheckElbV3L7PolicyConfig_base(rName), updateName)
+}
+
+func testAccCheckElbV3L7PolicyConfig_fixed_response(rName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_elb_l7policy" "test" {
+  name        = "%[2]s"
+  description = "test description"
+  action      = "FIXED_RESPONSE"
+  priority    = 20
+  listener_id = huaweicloud_elb_listener.test.id
+
+  fixed_response_config {
+    status_code  = "200"
+    content_type = "application/json"
+    message_body = "it is a test"
+  }
+}
+`, testAccCheckElbV3L7PolicyConfig_base(rName), rName)
+}
+
+func testAccCheckElbV3L7PolicyConfig_fixed_response_update(rName, updateName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_elb_l7policy" "test" {
+  name        = "%[2]s"
+  description = "test description update"
+  action      = "FIXED_RESPONSE"
+  priority    = 50
+  listener_id = huaweicloud_elb_listener.test.id
+
+  fixed_response_config {
+    status_code  = "202"
+    content_type = "text/css"
+    message_body = "it is a test update"
+  }
+}
+`, testAccCheckElbV3L7PolicyConfig_base(rName), updateName)
 }
