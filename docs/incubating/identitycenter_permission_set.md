3a332d4661d318bedb768c519727bb95438f03d6
shichangkuo <shichangkuo90@gmail.com>
2023-07-31 17:22:01 +0800

    feat(identitycenter): add permission set resource (#3240)

---
+++ docs/incubating/identitycenter_permission_set.md
@@ -0,0 +1,55 @@
+---
+subcategory: "IAM Identity Center"
+---
+
+# huaweicloud_identitycenter_permission_set
+
+Manages an Identity Center permission set resource within HuaweiCloud.  
+
+## Example Usage
+
+```hcl
+data "huaweicloud_identitycenter_instance" "system" {}
+
+resource "huaweicloud_identitycenter_permission_set" "demo" {
+  instance_id      = data.huaweicloud_identitycenter_instance.system.id
+  name             = "demo"
+  session_duration = "PT8H"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `instance_id` - (Required, String, ForceNew) Specifies the ID of the IAM Identity Center instance.
+  Changing this parameter will create a new resource.
+
+* `name` - (Required, String, ForceNew) Specifies the name of the permission set.
+  Changing this parameter will create a new resource.
+
+* `session_duration` - (Required, String) Specifies the length of time that the user sessions are valid in the
+  ISO-8601 standard, e.g. **PT4H**.
+
+* `relay_state` - (Optional, String) Specifies the relay state URL used to redirect users within the application during
+  the federation authentication process.
+
+* `description` - (Optional, String) Specifies the description of the permission set.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID.
+
+* `urn` - The Uniform Resource Name of the permission set.
+
+* `created_at` - The date the permission set was created in RFC3339 format.
+
+## Import
+
+The Identity Center permission set can be imported using the `instance_id` and `id` separated by a slash, e.g.
+
+```bash
+$ terraform import huaweicloud_identitycenter_permission_set.demo <instance_id>/<id>
+```
---
+++ huaweicloud/services/identitycenter/resource_huaweicloud_identitycenter_permission_set.go
@@ -0,0 +1,256 @@
+// ---------------------------------------------------------------
+// *** AUTO GENERATED CODE ***
+// @Product IdentityCenter
+// ---------------------------------------------------------------
+
+package identitycenter
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/jmespath/go-jmespath"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func ResourcePermissionSet() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourcePermissionSetCreate,
+		UpdateContext: resourcePermissionSetUpdate,
+		ReadContext:   resourcePermissionSetRead,
+		DeleteContext: resourcePermissionSetDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: resourcePermissionSetImport,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"instance_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"session_duration": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"relay_state": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"urn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"created_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourcePermissionSetCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		createPermissionSetHttpUrl = "v1/instances/{instance_id}/permission-sets"
+		createPermissionSetProduct = "identitycenter"
+	)
+	createPermissionSetClient, err := cfg.NewServiceClient(createPermissionSetProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating Identity Center client: %s", err)
+	}
+
+	createPermissionSetPath := createPermissionSetClient.Endpoint + createPermissionSetHttpUrl
+	createPermissionSetPath = strings.ReplaceAll(createPermissionSetPath, "{instance_id}", d.Get("instance_id").(string))
+
+	createPermissionSetOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+	createPermissionSetOpt.JSONBody = utils.RemoveNil(buildCreatePermissionSetBodyParams(d))
+	createPermissionSetResp, err := createPermissionSetClient.Request("POST", createPermissionSetPath, &createPermissionSetOpt)
+	if err != nil {
+		return diag.Errorf("error creating permission set: %s", err)
+	}
+
+	createPermissionSetRespBody, err := utils.FlattenResponse(createPermissionSetResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	id, err := jmespath.Search("permission_set.permission_set_id", createPermissionSetRespBody)
+	if err != nil {
+		return diag.Errorf("error creating permission set: ID is not found in API response")
+	}
+
+	d.SetId(id.(string))
+	return resourcePermissionSetRead(ctx, d, meta)
+}
+
+func buildCreatePermissionSetBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"name":             utils.ValueIngoreEmpty(d.Get("name")),
+		"session_duration": utils.ValueIngoreEmpty(d.Get("session_duration")),
+		"description":      utils.ValueIngoreEmpty(d.Get("description")),
+		"relay_state":      utils.ValueIngoreEmpty(d.Get("relay_state")),
+	}
+	return bodyParams
+}
+
+func resourcePermissionSetRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		getPermissionSetHttpUrl = "v1/instances/{instance_id}/permission-sets/{id}"
+		getPermissionSetProduct = "identitycenter"
+	)
+	getPermissionSetClient, err := cfg.NewServiceClient(getPermissionSetProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating Identity Center client: %s", err)
+	}
+
+	getPermissionSetPath := getPermissionSetClient.Endpoint + getPermissionSetHttpUrl
+	getPermissionSetPath = strings.ReplaceAll(getPermissionSetPath, "{instance_id}", d.Get("instance_id").(string))
+	getPermissionSetPath = strings.ReplaceAll(getPermissionSetPath, "{id}", d.Id())
+
+	getPermissionSetOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+	getPermissionSetResp, err := getPermissionSetClient.Request("GET", getPermissionSetPath, &getPermissionSetOpt)
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "error retrieving permission set")
+	}
+
+	getPermissionSetRespBody, err := utils.FlattenResponse(getPermissionSetResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	timeStamp := utils.PathSearch("permission_set.created_date", getPermissionSetRespBody, 0).(float64)
+	mErr := multierror.Append(nil,
+		d.Set("name", utils.PathSearch("permission_set.name", getPermissionSetRespBody, nil)),
+		d.Set("session_duration", utils.PathSearch("permission_set.session_duration", getPermissionSetRespBody, nil)),
+		d.Set("relay_state", utils.PathSearch("permission_set.relay_state", getPermissionSetRespBody, nil)),
+		d.Set("description", utils.PathSearch("permission_set.description", getPermissionSetRespBody, nil)),
+		d.Set("urn", utils.PathSearch("permission_set.permission_urn", getPermissionSetRespBody, nil)),
+		d.Set("created_at", utils.FormatTimeStampRFC3339(int64(timeStamp)/1000, false)),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func resourcePermissionSetUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	updatePermissionSetChanges := []string{
+		"session_duration",
+		"description",
+		"relay_state",
+	}
+
+	if d.HasChanges(updatePermissionSetChanges...) {
+		var (
+			updatePermissionSetHttpUrl = "v1/instances/{instance_id}/permission-sets/{id}"
+			updatePermissionSetProduct = "identitycenter"
+		)
+		updatePermissionSetClient, err := cfg.NewServiceClient(updatePermissionSetProduct, region)
+		if err != nil {
+			return diag.Errorf("error creating Identity Center client: %s", err)
+		}
+
+		updatePermissionSetPath := updatePermissionSetClient.Endpoint + updatePermissionSetHttpUrl
+		updatePermissionSetPath = strings.ReplaceAll(updatePermissionSetPath, "{instance_id}", d.Get("instance_id").(string))
+		updatePermissionSetPath = strings.ReplaceAll(updatePermissionSetPath, "{id}", d.Id())
+
+		updatePermissionSetOpt := golangsdk.RequestOpts{
+			KeepResponseBody: true,
+		}
+		updatePermissionSetOpt.JSONBody = utils.RemoveNil(buildUpdatePermissionSetBodyParams(d))
+		_, err = updatePermissionSetClient.Request("PUT", updatePermissionSetPath, &updatePermissionSetOpt)
+		if err != nil {
+			return diag.Errorf("error updating permission set: %s", err)
+		}
+	}
+
+	return resourcePermissionSetRead(ctx, d, meta)
+}
+
+func buildUpdatePermissionSetBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"session_duration": utils.ValueIngoreEmpty(d.Get("session_duration")),
+		"relay_state":      utils.ValueIngoreEmpty(d.Get("relay_state")),
+		// the description parameter can be cleared
+		"description": d.Get("description"),
+	}
+	return bodyParams
+}
+
+func resourcePermissionSetDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		deletePermissionSetHttpUrl = "v1/instances/{instance_id}/permission-sets/{id}"
+		deletePermissionSetProduct = "identitycenter"
+	)
+	deletePermissionSetClient, err := cfg.NewServiceClient(deletePermissionSetProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating Identity Center client: %s", err)
+	}
+
+	deletePermissionSetPath := deletePermissionSetClient.Endpoint + deletePermissionSetHttpUrl
+	deletePermissionSetPath = strings.ReplaceAll(deletePermissionSetPath, "{instance_id}", d.Get("instance_id").(string))
+	deletePermissionSetPath = strings.ReplaceAll(deletePermissionSetPath, "{id}", d.Id())
+
+	deletePermissionSetOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		OkCodes: []int{
+			200,
+		},
+	}
+	_, err = deletePermissionSetClient.Request("DELETE", deletePermissionSetPath, &deletePermissionSetOpt)
+	if err != nil {
+		return diag.Errorf("error deleting permission set: %s", err)
+	}
+
+	return nil
+}
+
+func resourcePermissionSetImport(_ context.Context, d *schema.ResourceData, _ interface{}) ([]*schema.ResourceData, error) {
+	parts := strings.Split(d.Id(), "/")
+	if len(parts) != 2 {
+		err := fmt.Errorf("invalid format: the format must be <instance id>/<permission set id>")
+		return nil, err
+	}
+
+	instanceID := parts[0]
+	psID := parts[1]
+
+	d.SetId(psID)
+	d.Set("instance_id", instanceID)
+
+	return []*schema.ResourceData{d}, nil
+}
