93e5b5756724bcd03f210294fcfbc4f1eb49b9f9
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2023-09-08 22:06:15 +0800

    feat: add identity center groups data source (#3381)

---
+++ docs/incubating/identitycenter_groups.md
@@ -0,0 +1,51 @@
+---
+subcategory: "IAM Identity Center"
+---
+
+# huaweicloud_identitycenter_groups
+
+Use this data source to get the Identity Center groups.
+
+## Example Usage
+
+```hcl
+data "huaweicloud_identitycenter_instance" "system" {}
+
+data "huaweicloud_identitycenter_groups" "test"{
+  identity_store_id = data.huaweicloud_identitycenter_instance.system.identity_store_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the data source.
+  If omitted, the provider-level region will be used.
+
+* `identity_store_id` - (Required, String) Specifies the ID of the identity store that associated with IAM Identity
+  Center.
+
+* `name` - (Optional, String) Specifies the name of the group.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID.
+
+* `groups` - Indicates the list of IdentityCenter group.
+  The [groups](#IdentityCenterGroups_Group) structure is documented below.
+
+<a name="IdentityCenterGroups_Group"></a>
+The `groups` block supports:
+
+* `id` - Indicates the ID of the group.
+
+* `name` - Indicates the name of the group.
+
+* `description` - Indicates the description of the group.
+
+* `created_at` - Indicates the creation time.
+
+* `updated_at` - Indicates the last update time.
---
+++ huaweicloud/services/acceptance/identitycenter/data_source_huaweicloud_identitycenter_groups_test.go
@@ -0,0 +1,60 @@
+package identitycenter
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDatasourceIdentityCenterGroups_basic(t *testing.T) {
+	name := acceptance.RandomAccResourceName()
+	rName := "data.huaweicloud_identitycenter_groups.test"
+	dc := acceptance.InitDataSourceCheck(rName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourceIdentityCenterGroups_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(rName, "groups.0.id"),
+					resource.TestCheckResourceAttrSet(rName, "groups.0.name"),
+					resource.TestCheckResourceAttrSet(rName, "groups.0.description"),
+					resource.TestCheckResourceAttrSet(rName, "groups.0.created_at"),
+					resource.TestCheckResourceAttrSet(rName, "groups.0.updated_at"),
+					resource.TestCheckOutput("name_filter_is_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDatasourceIdentityCenterGroups_basic(name string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_identitycenter_groups" "test" {
+  identity_store_id = data.huaweicloud_identitycenter_instance.test.identity_store_id
+  name              = huaweicloud_identitycenter_group.test.name
+}
+
+data "huaweicloud_identitycenter_groups" "name_filter" {
+  identity_store_id = data.huaweicloud_identitycenter_instance.test.identity_store_id
+  name              = huaweicloud_identitycenter_group.test.name
+}
+
+locals {
+  name_filter_result = [for v in data.huaweicloud_identitycenter_groups.name_filter.groups[*].name:
+  v == data.huaweicloud_identitycenter_groups.test.groups.0.name]
+}
+
+output "name_filter_is_useful" {
+  value = alltrue(local.name_filter_result) && length(local.name_filter_result) > 0
+}
+`, testIdentityCenterGroup_basic(name))
+}
