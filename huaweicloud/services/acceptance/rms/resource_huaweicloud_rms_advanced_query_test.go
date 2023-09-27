9fca3c178e867a99701fd8128f144b49ab1be1c9
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2023-09-27 11:14:12 +0800

    feat: add rms advanced query resource (#3430)

---
+++ huaweicloud/services/acceptance/rms/resource_huaweicloud_rms_advanced_query_test.go
@@ -0,0 +1,113 @@
+package rms
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func getAdvancedQueryResourceFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	region := acceptance.HW_REGION_NAME
+	// getAdvancedQuery: Query the RMS advanced query
+	var (
+		getAdvancedQueryHttpUrl = "v1/resource-manager/domains/{domain_id}/stored-queries/{query_id}"
+		getAdvancedQueryProduct = "rms"
+	)
+	getAdvancedQueryClient, err := cfg.NewServiceClient(getAdvancedQueryProduct, region)
+	if err != nil {
+		return nil, fmt.Errorf("error creating RMS client: %s", err)
+	}
+
+	getAdvancedQueryPath := getAdvancedQueryClient.Endpoint + getAdvancedQueryHttpUrl
+	getAdvancedQueryPath = strings.ReplaceAll(getAdvancedQueryPath, "{domain_id}", cfg.DomainID)
+	getAdvancedQueryPath = strings.ReplaceAll(getAdvancedQueryPath, "{query_id}", state.Primary.ID)
+
+	getAdvancedQueryOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+
+	getAdvancedQueryResp, err := getAdvancedQueryClient.Request("GET", getAdvancedQueryPath, &getAdvancedQueryOpt)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving RMS advanced query: %s", err)
+	}
+
+	getAdvancedQueryRespBody, err := utils.FlattenResponse(getAdvancedQueryResp)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving RMS advanced query: %s", err)
+	}
+
+	return getAdvancedQueryRespBody, nil
+}
+
+func TestAccAdvancedQuery_basic(t *testing.T) {
+	var obj interface{}
+
+	name := acceptance.RandomAccResourceName()
+	rName := "huaweicloud_rms_advanced_query.test"
+
+	rc := acceptance.InitResourceCheck(
+		rName,
+		&obj,
+		getAdvancedQueryResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAdvancedQuery_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(rName, "name", name),
+					resource.TestCheckResourceAttr(rName, "expression", "select colume_1 from table_1"),
+					resource.TestCheckResourceAttr(rName, "description", "test_description"),
+				),
+			},
+			{
+				Config: testAdvancedQuery_basic_update(name),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(rName, "name", name),
+					resource.TestCheckResourceAttr(rName, "expression", "update table_1 set volume_1 = 5"),
+					resource.TestCheckResourceAttr(rName, "description", "test_description_update"),
+				),
+			},
+			{
+				ResourceName:      rName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAdvancedQuery_basic(name string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_rms_advanced_query" "test" {
+  name        = "%s"
+  expression  = "select colume_1 from table_1"
+  description = "test_description"
+}
+`, name)
+}
+
+func testAdvancedQuery_basic_update(name string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_rms_advanced_query" "test" {
+  name        = "%s"
+  expression  = "update table_1 set volume_1 = 5"
+  description = "test_description_update"
+}
+`, name)
+}
