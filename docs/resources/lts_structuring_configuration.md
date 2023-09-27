c13d38fb2581225fa6f6a991a56e2a101ca078ae
luxiaohang <deerhang@163.com>
2023-09-27 15:54:45 +0800

    feat(LTS): add new resource LTS structuring configuration

---
+++ docs/resources/lts_structuring_configuration.md
@@ -0,0 +1,159 @@
+---
+subcategory: "Log Tank Service (LTS)"
+---
+
+# huaweicloud_lts_structuring_configuration
+
+Manages an LTS structuring configuration resource within HuaweiCloud.
+
+## Example Usage
+
+### Creating with system template
+
+```hcl
+variable "log_group_id" {}
+variable "log_stream_id" {}
+
+resource "huaweicloud_lts_structuring_configuration" "test" {
+  log_group_id  = var.log_group_id
+  log_stream_id = var.log_stream_id
+  template_name = "CTS"
+  template_type = "built_in"
+}
+```
+
+### Creating with custom template
+
+```hcl
+variable "log_group_id" {}
+variable "log_stream_id" {}
+variable "template_name" {}
+variable "template_id" {}
+
+resource "huaweicloud_lts_structuring_configuration" "test" {
+  log_group_id  = var.log_group_id
+  log_stream_id = var.log_stream_id
+  template_name = var.template_name
+  template_id   = var.template_id
+  template_type = "custom"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region in which to create the resource.
+  If omitted, the provider-level region will be used. Changing this parameter will create a new resource.
+
+* `log_group_id` - (Required, String, ForceNew) Specifies the log group ID.
+  Changing this parameter will create a new resource.
+
+* `log_stream_id` - (Required, String, ForceNew) Specifies the log stream ID.
+  Changing this parameter will create a new resource.
+
+* `template_type` - (Required, String) Specifies the type of the template. The valid values are as follows:
+  + **built_in**: System templates.
+  + **custom**:   Custom templates.
+
+* `template_name` - (Required, String) Specifies the template name. When `template_type` is set to **built_in**,
+  valid values are:
+  + **ELB**
+  + **VPC**
+  + **CTS**
+  + **APIG**
+  + **DCS_AUDIT**: DCS audit log.
+  + **TOMCAT**
+  + **NGINX**
+  + **GAUSSDB_OPENGAUSS_AUDIT**: GAUSSV5 audit log.
+  + **DDS_AUDIT**: DDS audit log.
+  + **MONGODB_ERROR**: DDS error log.
+  + **MONGODB_SLOW**: DDS slow log.
+  + **CFW_ACCESS**: CFW access control log.
+  + **CFW_ATTACK**: CFW attack log.
+  + **CFW_FLOW**: CFW traffic log.
+  + **MYSQL_ERROR**: MYSQL error log.
+  + **MYSQL_SLOW**: MYSQL slow log:
+  + **POSTGRESQL_SLOW**: POSTGRESQL slow log.
+  + **POSTGRESQL_ERROR**: POSTGRESQL error log.
+  + **SQLSERVER_ERROR**: SQLSERVER error log.
+  + **GAUSSDB_REDIS_SLOW**: GAUSSDB_REDIS slow log.
+  + **CDN**
+  + **SMN**
+  + **GAUSSDB_MYSQL_ERROR**: GAUSSDB_MYSQL error log.
+  + **GAUSSDB_MYSQL_SLOW**: GAUSSDB_MYSQL slow log.
+  + **ER**: ER Enterprise Router.
+  + **MYSQL_AUDIT**: MYSQL audit log.
+  + **GAUSSDB_CASSANDRA_SLOW**: GaussDBforCassandra slow log.
+  + **GAUSSDB_MONGO_SLOW**: GaussDBforMongo slow log.
+  + **GAUSSDB_MONGO_ERROR**: GaussDBforMongo error log.
+  + **WAF_ACCESS**: WAF access log.
+  + **WAF_ATTACK**: WAF attack log.
+  + **DMS_REBALANCED**:DMS rebalancing log.
+  + **CCE_AUDIT**: CCE audit log.
+  + **CCE_EVENT**: CCE event log.
+  + **GAUSSDB_REDIS_AUDIT**: GaussDBforRedis audit log.
+
+* `template_id` - (Optional, String) Specifies the template ID. The field is valid and required only when
+  `template_type` is set to **custom**.
+
+* `demo_fields` - (Optional, List) Specifies the example fields. Use to set quick analysis configurations for fields.
+  Only need to enter the fields whose status is different from that of `is_analysis` in the template.
+The [demo_fields](#StructConfig_fields) structure is documented below.
+
+* `tag_fields` - (Optional, List) Specifies the tag fields. Use to set quick analysis configurations for fields.
+  Only need to enter the fields whose status is different from that of `is_analysis` in the template.
+The [tag_fields](#StructConfig_fields) structure is documented below.
+
+* `quick_analysis` - (Optional, Bool) Specifies whether to enable `demo_fields` and `tag_fields` quick analysis.
+  + If this parameter is set to **true**, quick analysis is enabled for all `demo_fields` and `tag_fields`.
+  + If this parameter is set to **false**, `is_analysis` in `demo_fields` and `tag_fields` in the template determines
+    whether to enable quick analysis.
+
+  Defaults to **false**.
+
+<a name="StructConfig_fields"></a>
+The `demo_fields` and `tag_fields` block supports:
+
+* `is_analysis` - (Optional, Bool) Specifies whether quick analysis is enabled. Defaults to **false**.
+
+* `field_name` - (Required, String) Specifies the field name. The valid length is limited from `1` to `64`.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID.
+
+* `demo_log` - The sample log event.
+
+## Import
+
+The LTS structuring configuration can be imported using `log_group_id` and `log_stream_id`, separated by a slash, e.g.
+
+```bash
+$ terraform import huaweicloud_lts_structuring_configuration.test <log_group_id>/<log_stream_id>
+```
+
+Note that the imported state may not be identical to your resource definition, due to some attributes missing from the
+API response, security or some other reason. The missing attributes include: `template_type`, `template_id`,
+`demo_fields`, `tag_fields`, `quick_analysis`.
+It is generally recommended running `terraform plan` after importing a resource.
+You can then decide if changes should be applied to the resource, or the resource definition should be updated to align
+with the resource. Also, you can ignore changes as below.
+
+```hcl
+resource "huaweicloud_lts_structuring_configuration" "test" {
+  ...
+  
+  lifecycle {
+    ignore_changes = [
+      template_type,
+      template_id,
+      demo_fields,
+      tag_fields,
+      quick_analysis,
+    ]
+  }
+}
+```
---
+++ huaweicloud/services/lts/resource_huaweicloud_lts_structuring_configuration.go
@@ -0,0 +1,342 @@
+// ---------------------------------------------------------------
+// *** AUTO GENERATED CODE ***
+// @Product LTS
+// ---------------------------------------------------------------
+
+package lts
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/jmespath/go-jmespath"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func ResourceStructConfig() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceStructConfigCreate,
+		UpdateContext: resourceStructConfigUpdate,
+		ReadContext:   resourceStructConfigRead,
+		DeleteContext: resourceStructConfigDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceLtsStructConfigImportState,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"log_group_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the log group ID.`,
+			},
+			"log_stream_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the log stream ID.`,
+			},
+			"template_type": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: `Specifies the type of the template.`,
+			},
+			"template_name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: `Specifies the template name.`,
+			},
+			"template_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `Specifies the template ID.`,
+			},
+			"demo_fields": {
+				Type:        schema.TypeList,
+				Elem:        structConfigFieldSchema(),
+				Optional:    true,
+				Description: `Specifies the example field array.`,
+			},
+			"tag_fields": {
+				Type:        schema.TypeList,
+				Elem:        structConfigFieldSchema(),
+				Optional:    true,
+				Description: `Specifies the tag field array.`,
+			},
+			"quick_analysis": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: `Specifies whether to enable demo_fields and tag_fields quick analysis.`,
+			},
+			"demo_log": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The sample log event.`,
+			},
+		},
+	}
+}
+
+func structConfigFieldSchema() *schema.Resource {
+	sc := schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"field_name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: `Specifies the field name.`,
+			},
+			"is_analysis": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: `Specifies whether quick analysis is enabled.`,
+			},
+		},
+	}
+	return &sc
+}
+
+func resourceStructConfigCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg     = meta.(*config.Config)
+		region  = cfg.GetRegion(d)
+		httpUrl = "v3/{project_id}/lts/struct/template"
+		product = "lts"
+	)
+	client, err := cfg.NewServiceClient(product, region)
+	if err != nil {
+		return diag.Errorf("error creating LTS client: %s", err)
+	}
+
+	createPath := client.Endpoint + httpUrl
+	createPath = strings.ReplaceAll(createPath, "{project_id}", client.ProjectID)
+	createOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		JSONBody:         utils.RemoveNil(buildCreateOrUpdateStructConfigBodyParams(d)),
+	}
+
+	_, err = client.Request("POST", createPath, &createOpt)
+	if err != nil {
+		return diag.Errorf("error creating LTS structuring configuration: %s", err)
+	}
+
+	// Since the creation API response body is empty, the resource ID is obtained from the detailed API.
+	detailRespBody, err := queryStructConfigDetail(client, d)
+	if err != nil {
+		return diag.Errorf("error creating LTS structuring configuration (failed to query detail API): %s", err)
+	}
+
+	id, err := jmespath.Search("id", detailRespBody)
+	if err != nil || id == nil {
+		return diag.Errorf("error creating LTS structuring configuration: ID is not found in detail API response")
+	}
+	d.SetId(id.(string))
+
+	return resourceStructConfigRead(ctx, d, meta)
+}
+
+func buildCreateOrUpdateStructConfigBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"log_group_id":   d.Get("log_group_id"),
+		"log_stream_id":  d.Get("log_stream_id"),
+		"template_type":  d.Get("template_type"),
+		"template_id":    d.Get("template_id"),
+		"template_name":  d.Get("template_name"),
+		"quick_analysis": d.Get("quick_analysis"),
+		"demo_fields":    buildStructConfigFields(d.Get("demo_fields")),
+		"tag_fields":     buildStructConfigFields(d.Get("tag_fields")),
+	}
+	return bodyParams
+}
+
+func buildStructConfigFields(rawParams interface{}) []map[string]interface{} {
+	rawArray, ok := rawParams.([]interface{})
+	if !ok || len(rawArray) == 0 {
+		return nil
+	}
+
+	rst := make([]map[string]interface{}, 0, len(rawArray))
+	for _, v := range rawArray {
+		if raw, isMap := v.(map[string]interface{}); isMap {
+			rst = append(rst, map[string]interface{}{
+				"field_name":  raw["field_name"],
+				"is_analysis": raw["is_analysis"],
+			})
+		}
+	}
+	return rst
+}
+
+func resourceStructConfigRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		mErr    *multierror.Error
+		cfg     = meta.(*config.Config)
+		region  = cfg.GetRegion(d)
+		product = "lts"
+	)
+	client, err := cfg.NewServiceClient(product, region)
+	if err != nil {
+		return diag.Errorf("error creating LTS client: %s", err)
+	}
+
+	detailRespBody, err := queryStructConfigDetail(client, d)
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "error retrieving LTS structuring configuration")
+	}
+
+	// update the resource ID for import operation
+	id := utils.PathSearch("id", detailRespBody, "")
+	if id != "" {
+		d.SetId(id.(string))
+	}
+
+	mErr = multierror.Append(
+		mErr,
+		d.Set("region", region),
+		d.Set("log_group_id", utils.PathSearch("logGroupId", detailRespBody, nil)),
+		d.Set("log_stream_id", utils.PathSearch("logStreamId", detailRespBody, nil)),
+		d.Set("template_name", utils.PathSearch("templateName", detailRespBody, nil)),
+		d.Set("demo_log", utils.PathSearch("demoLog", detailRespBody, nil)),
+	)
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+// queryStructConfigDetail use to query structuring configuration detail.
+// The response of detail API is string value.
+// If the response string value is empty, means the structuring configuration is not exist.
+func queryStructConfigDetail(client *golangsdk.ServiceClient, d *schema.ResourceData) (interface{}, error) {
+	getPath := client.Endpoint + "v2/{project_id}/lts/struct/template"
+	getPath = strings.ReplaceAll(getPath, "{project_id}", client.ProjectID)
+	getPath += buildGetStructConfigQueryParams(d)
+	getOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		MoreHeaders:      map[string]string{"Content-Type": "application/json;charset=UTF-8"},
+	}
+
+	getResp, err := client.Request("GET", getPath, &getOpt)
+	if err != nil {
+		return nil, err
+	}
+
+	getRespBody, err := utils.FlattenResponse(getResp)
+	if err != nil {
+		return nil, err
+	}
+
+	rawString, isString := getRespBody.(string)
+	if !isString {
+		return nil, fmt.Errorf("the detail API response is not string")
+	}
+
+	if rawString == "" {
+		// the structuring configuration is not exist
+		return nil, golangsdk.ErrDefault404{}
+	}
+
+	var rst map[string]interface{}
+	if err := json.Unmarshal([]byte(rawString), &rst); err != nil {
+		return nil, err
+	}
+	return rst, nil
+}
+
+func buildGetStructConfigQueryParams(d *schema.ResourceData) string {
+	logGroupId := d.Get("log_group_id").(string)
+	logStreamId := d.Get("log_stream_id").(string)
+	return fmt.Sprintf("?logGroupId=%s&logStreamId=%s", logGroupId, logStreamId)
+}
+
+func resourceStructConfigUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg     = meta.(*config.Config)
+		region  = cfg.GetRegion(d)
+		httpUrl = "v3/{project_id}/lts/struct/template"
+		product = "lts"
+	)
+	client, err := cfg.NewServiceClient(product, region)
+	if err != nil {
+		return diag.Errorf("error creating LTS client: %s", err)
+	}
+
+	updatePath := client.Endpoint + httpUrl
+	updatePath = strings.ReplaceAll(updatePath, "{project_id}", client.ProjectID)
+	updateOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		JSONBody:         utils.RemoveNil(buildCreateOrUpdateStructConfigBodyParams(d)),
+	}
+
+	_, err = client.Request("PUT", updatePath, &updateOpt)
+	if err != nil {
+		return diag.Errorf("error updating LTS structuring configuration: %s", err)
+	}
+	return resourceStructConfigRead(ctx, d, meta)
+}
+
+func resourceStructConfigDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg     = meta.(*config.Config)
+		region  = cfg.GetRegion(d)
+		httpUrl = "v2/{project_id}/lts/struct/template"
+		product = "lts"
+	)
+	client, err := cfg.NewServiceClient(product, region)
+	if err != nil {
+		return diag.Errorf("error creating LTS client: %s", err)
+	}
+
+	deletePath := client.Endpoint + httpUrl
+	deletePath = strings.ReplaceAll(deletePath, "{project_id}", client.ProjectID)
+	deleteOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		JSONBody:         buildDeleteStructConfigBodyParams(d),
+	}
+
+	_, err = client.Request("DELETE", deletePath, &deleteOpt)
+	if err != nil {
+		return diag.Errorf("error deleting LTS structuring configuration: %s", err)
+	}
+
+	// The response to deletion API is always successful.
+	// Determine whether the deletion is successful through the details API
+	_, err = queryStructConfigDetail(client, d)
+	if err == nil {
+		return diag.Errorf("error deleting LTS structuring configuration: detailed information can still be found")
+	}
+	return common.CheckDeletedDiag(d, err, "error deleting LTS structuring configuration")
+}
+
+func buildDeleteStructConfigBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"id": d.Id(),
+	}
+	return bodyParams
+}
+
+func resourceLtsStructConfigImportState(_ context.Context, d *schema.ResourceData, _ interface{}) (
+	[]*schema.ResourceData, error) {
+	parts := strings.Split(d.Id(), "/")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid resource ID format for LTS structuring configuration,"+
+			" want '<log_group_id>/<log_stream_id>', but got '%s'", d.Id())
+	}
+	mErr := multierror.Append(nil,
+		d.Set("log_group_id", parts[0]),
+		d.Set("log_stream_id", parts[1]),
+	)
+	return []*schema.ResourceData{d}, mErr.ErrorOrNil()
+}
