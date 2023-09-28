6d5a909a4ea9720ba1176d4ef91b99557ce0a830
houpeng <hp_monkey@163.com>
2023-09-28 10:25:56 +0800

    fix identitycenter test

--- huaweicloud/services/acceptance/identitycenter/data_source_huaweicloud_identitycenter_users_test.go
+++ huaweicloud/services/acceptance/identitycenter/data_source_huaweicloud_identitycenter_users_test.go
@@ -15,7 +15,10 @@ func TestAccDatasourceIdentityCenterUsers_basic(t *testing.T) {
 	dc := acceptance.InitDataSourceCheck(rName)
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckMultiAccount(t)
+		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
