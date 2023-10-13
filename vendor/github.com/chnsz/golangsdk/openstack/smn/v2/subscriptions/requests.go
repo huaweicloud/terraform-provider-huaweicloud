f2210f19fbebc6ec4dde614651303586ffa1d360
Jason-Zhang <jasonzhang9309@163.com>
2023-10-13 16:14:07 +0800

    feat(smn): add extension in SMN subscription (#3497)

--- vendor/github.com/chnsz/golangsdk/openstack/smn/v2/subscriptions/requests.go
+++ vendor/github.com/chnsz/golangsdk/openstack/smn/v2/subscriptions/requests.go
@@ -9,13 +9,13 @@ var RequestOpts golangsdk.RequestOpts = golangsdk.RequestOpts{
 	MoreHeaders: map[string]string{"Content-Type": "application/json", "X-Language": "en-us"},
 }
 
-//CreateOpsBuilder is used for creating subscription parameters.
-//any struct providing the parameters should implement this interface
+// CreateOpsBuilder is used for creating subscription parameters.
+// any struct providing the parameters should implement this interface
 type CreateOpsBuilder interface {
 	ToSubscriptionCreateMap() (map[string]interface{}, error)
 }
 
-//CreateOps is a struct that contains all the parameters.
+// CreateOps is a struct that contains all the parameters.
 type CreateOps struct {
 	//Message endpoint
 	Endpoint string `json:"endpoint" required:"true"`
@@ -23,13 +23,22 @@ type CreateOps struct {
 	Protocol string `json:"protocol" required:"true"`
 	//Description of the subscription
 	Remark string `json:"remark,omitempty"`
+	// Extension config
+	Extension *ExtensionSpec `json:"extension,omitempty"`
+}
+
+type ExtensionSpec struct {
+	ClientID     string `json:"client_id,omitempty"`
+	ClientSecret string `json:"client_secret,omitempty"`
+	Keyword      string `json:"keyword,omitempty"`
+	SignSecret   string `json:"sign_secret,omitempty"`
 }
 
 func (ops CreateOps) ToSubscriptionCreateMap() (map[string]interface{}, error) {
 	return golangsdk.BuildRequestBody(ops, "")
 }
 
-//Create a subscription with given parameters.
+// Create a subscription with given parameters.
 func Create(client *golangsdk.ServiceClient, ops CreateOpsBuilder, topicUrn string) (r CreateResult) {
 	b, err := ops.ToSubscriptionCreateMap()
 	if err != nil {
@@ -45,7 +54,7 @@ func Create(client *golangsdk.ServiceClient, ops CreateOpsBuilder, topicUrn stri
 	return
 }
 
-//delete a subscription via subscription urn
+// delete a subscription via subscription urn
 func Delete(client *golangsdk.ServiceClient, subscriptionUrn string) (r DeleteResult) {
 	_, r.Err = client.Delete(deleteURL(client, subscriptionUrn), &golangsdk.RequestOpts{
 		OkCodes:     []int{200},
@@ -54,7 +63,7 @@ func Delete(client *golangsdk.ServiceClient, subscriptionUrn string) (r DeleteRe
 	return
 }
 
-//list all the subscriptions
+// list all the subscriptions
 func List(client *golangsdk.ServiceClient) (r ListResult) {
 	pages, err := pagination.NewPager(client, listURL(client),
 		func(r pagination.PageResult) pagination.Page {
@@ -70,7 +79,7 @@ func List(client *golangsdk.ServiceClient) (r ListResult) {
 	return
 }
 
-//list all the subscriptions of a topic
+// list all the subscriptions of a topic
 func ListFromTopic(client *golangsdk.ServiceClient, topicUrn string) (r ListResult) {
 	pages, err := pagination.NewPager(client, listFromTopicURL(client, topicUrn),
 		func(r pagination.PageResult) pagination.Page {
