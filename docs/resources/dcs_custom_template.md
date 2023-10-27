77e2a51539299d2cc4b07320f27036ef2ebce0d4
houpeng80 <hp_monkey@163.com>
2023-10-27 15:53:15 +0800

    feat(DCS): add dcs custom template resource

---
+++ docs/resources/dcs_custom_template.md
@@ -0,0 +1,119 @@
+---
+subcategory: "Distributed Cache Service (DCS)"
+---
+
+# huaweicloud_dcs_custom_template
+
+Manages a DCS custom template resource within HuaweiCloud.
+
+## Example Usage
+
+```hcl
+variable "source_template_id" {}
+
+resource "huaweicloud_dcs_custom_template" "test"{
+  template_id = var.source_template_id
+  name        = "template_name"
+  type        = "sys"
+
+  params {
+    param_name  = "timeout"
+    param_value = "100"
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region in which to create the resource.
+  If omitted, the provider-level region will be used. Changing this parameter will create a new resource.
+
+* `template_id` - (Required, String, ForceNew) Specifies the ID of the source template.
+
+  Changing this parameter will create a new resource.
+
+* `source_type` - (Required, String, ForceNew) Specifies the type of the source template. Value options:
+  + **sys**: system template.
+  + **user**: custom template.
+
+  Changing this parameter will create a new resource.
+
+* `name` - (Required, String) Specifies the name of the template.
+
+* `params` - (Required, List) Specifies the list of the template params.
+The [params](#CustomTemplate_Param) structure is documented below.
+
+* `description` - (Optional, String) Specifies the description of the template.
+
+<a name="CustomTemplate_Param"></a>
+The `params` block supports:
+
+* `param_name` - (Required, String) Indicates the name of the param. You can find it through data source
+  `huaweicloud_dcs_template_detail`.
+
+* `param_value` - (Required, String) Indicates the value of the param.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID.
+
+* `type` - Indicates the type of the template. The value can be **sys**, **user**.
+
+* `engine` - Indicates the cache engine. Currently, only **Redis** is supported.
+
+* `engine_version` - Indicates the cache engine version. The value can be **4.0**, **5.0.**, **6.0.**.
+
+* `cache_mode` - Indicates the DCS instance type. The value can be **single**, **ha**, **cluster**, **proxy**,
+  **ha_rw_split**.
+
+* `product_type` - Indicates the product edition. The value can be **generic**, **enterprise**.
+
+* `storage_type` - Indicates the storage type. The value can be **DRAM**, **SSD**.
+
+* `params` - Indicates the list of the template params.
+  The [params](#CustomTemplate_Param) structure is documented below.
+
+<a name="CustomTemplate_Param"></a>
+The `params` block supports:
+
+* `param_id` - (Optional, String) Indicates the ID of the param.
+
+* `default_value` - (Optional, String) Indicates the default value of the param.
+
+* `value_range` - (Optional, String) Indicates the value range of the param.
+
+* `value_type` - (Optional, String) Indicates the value type of the param.
+
+* `description` - (Optional, String) Indicates the description of the param.
+
+* `need_restart` - (Optional, Bool) Indicates whether the DCS instance need restart.
+
+## Import
+
+The dcs custom template can be imported using the `id`, e.g.
+
+```bash
+$ terraform import huaweicloud_dcs_custom_template.test <id>
+```
+
+Note that the imported state may not be identical to your resource definition, due to some attributes missing from the
+API response, security or some other reason. The missing attributes include: `template_id`, `source_type`, `params`. It
+is generally recommended running `terraform plan` after importing a custom template. You can then decide if changes
+should be applied to the custom template, or the resource definition should be updated to align with the DCS custom
+template. Also you can ignore changes as below.
+
+```hcl
+resource "huaweicloud_dcs_custom_template" "test" {
+  ...
+
+  lifecycle {
+    ignore_changes = [
+      template_id, source_type, params,
+    ]
+  }
+}
+```
---
+++ huaweicloud/services/dcs/resource_huaweicloud_dcs_custom_template.go
@@ -0,0 +1,431 @@
+// ---------------------------------------------------------------
+// *** AUTO GENERATED CODE ***
+// @Product DCS
+// ---------------------------------------------------------------
+
+package dcs
+
+import (
+	"context"
+	"encoding/json"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/jmespath/go-jmespath"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func ResourceCustomTemplate() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceCustomTemplateCreate,
+		UpdateContext: resourceCustomTemplateUpdate,
+		ReadContext:   resourceCustomTemplateRead,
+		DeleteContext: resourceCustomTemplateDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"template_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the ID of the source template.`,
+			},
+			"source_type": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the type of the source template.`,
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: `Specifies the name of the template.`,
+			},
+			"params": {
+				Type:        schema.TypeSet,
+				Elem:        customTemplateParamSchema(),
+				Required:    true,
+				Description: `Specifies the list of the template params.`,
+			},
+			"description": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `Specifies the description of the template.`,
+			},
+			"type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the type of the template.`,
+			},
+			"engine": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the cache engine.`,
+			},
+			"engine_version": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the cache engine version.`,
+			},
+			"cache_mode": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the DCS instance type.`,
+			},
+			"product_type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the product edition.`,
+			},
+			"storage_type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the storage type.`,
+			},
+		},
+	}
+}
+
+func customTemplateParamSchema() *schema.Resource {
+	sc := schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"param_name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: `Indicates the name of the param.`,
+			},
+			"param_value": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: `Indicates the value of the param.`,
+			},
+			"param_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the ID of the param.`,
+			},
+			"default_value": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the default value of the param.`,
+			},
+			"value_range": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the value range of the param.`,
+			},
+			"value_type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the value type of the param.`,
+			},
+			"description": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Indicates the description of the param.`,
+			},
+			"need_restart": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: `Indicates whether the DCS instance need restart.`,
+			},
+		},
+	}
+	return &sc
+}
+
+func resourceCustomTemplateCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	// createCustomTemplate: create DCS custom template
+	var (
+		createCustomTemplateHttpUrl = "v2/{project_id}/config-templates"
+		createCustomTemplateProduct = "dcs"
+	)
+	createCustomTemplateClient, err := cfg.NewServiceClient(createCustomTemplateProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating DCS client: %s", err)
+	}
+
+	createCustomTemplatePath := createCustomTemplateClient.Endpoint + createCustomTemplateHttpUrl
+	createCustomTemplatePath = strings.ReplaceAll(createCustomTemplatePath, "{project_id}",
+		createCustomTemplateClient.ProjectID)
+
+	createCustomTemplateOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		MoreHeaders:      map[string]string{"Content-Type": "application/json"},
+	}
+
+	createCustomTemplateOpt.JSONBody = utils.RemoveNil(buildCreateCustomTemplateBodyParams(d))
+	createCustomTemplateResp, err := createCustomTemplateClient.Request("POST", createCustomTemplatePath,
+		&createCustomTemplateOpt)
+	if err != nil {
+		return diag.Errorf("error creating DCS custom template: %s", err)
+	}
+
+	createCustomTemplateRespBody, err := utils.FlattenResponse(createCustomTemplateResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	id, err := jmespath.Search("id", createCustomTemplateRespBody)
+	if err != nil {
+		return diag.Errorf("error creating DCS custom template: ID is not found in API response")
+	}
+	d.SetId(id.(string))
+
+	return resourceCustomTemplateRead(ctx, d, meta)
+}
+
+func buildCreateCustomTemplateBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"template_id": d.Get("template_id"),
+		"type":        d.Get("source_type"),
+		"name":        d.Get("name"),
+		"description": d.Get("description"),
+		"params":      buildCreateCustomTemplateRequestBodyParam(d.Get("params").(*schema.Set).List()),
+	}
+	return bodyParams
+}
+
+func buildCreateCustomTemplateRequestBodyParam(rawParams interface{}) map[string]string {
+	if rawArray, ok := rawParams.([]interface{}); ok {
+		if len(rawArray) == 0 {
+			return nil
+		}
+
+		rst := make(map[string]string)
+		for _, v := range rawArray {
+			if raw, ok := v.(map[string]interface{}); ok {
+				rst[raw["param_name"].(string)] = raw["param_value"].(string)
+			}
+		}
+		return rst
+	}
+	return nil
+}
+
+func resourceCustomTemplateRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var mErr *multierror.Error
+
+	// getCustomTemplate: Query DCS custom template
+	var (
+		getCustomTemplateHttpUrl = "v2/{project_id}/config-templates/{template_id}?type=user"
+		getCustomTemplateProduct = "dcs"
+	)
+	getCustomTemplateClient, err := cfg.NewServiceClient(getCustomTemplateProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating DCS client: %s", err)
+	}
+
+	getCustomTemplatePath := getCustomTemplateClient.Endpoint + getCustomTemplateHttpUrl
+	getCustomTemplatePath = strings.ReplaceAll(getCustomTemplatePath, "{project_id}", getCustomTemplateClient.ProjectID)
+	getCustomTemplatePath = strings.ReplaceAll(getCustomTemplatePath, "{template_id}", d.Id())
+
+	getCustomTemplateOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		MoreHeaders:      map[string]string{"Content-Type": "application/json"},
+	}
+
+	getCustomTemplateResp, err := getCustomTemplateClient.Request("GET", getCustomTemplatePath, &getCustomTemplateOpt)
+
+	if err != nil {
+		return common.CheckDeletedDiag(d, parseDcsErrorToError404(err), "error retrieving DCS custom template")
+	}
+
+	getCustomTemplateRespBody, err := utils.FlattenResponse(getCustomTemplateResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	mErr = multierror.Append(
+		mErr,
+		d.Set("region", region),
+		d.Set("name", utils.PathSearch("name", getCustomTemplateRespBody, nil)),
+		d.Set("type", utils.PathSearch("type", getCustomTemplateRespBody, nil)),
+		d.Set("engine", utils.PathSearch("engine", getCustomTemplateRespBody, nil)),
+		d.Set("engine_version", utils.PathSearch("engine_version", getCustomTemplateRespBody, nil)),
+		d.Set("cache_mode", utils.PathSearch("cache_mode", getCustomTemplateRespBody, nil)),
+		d.Set("product_type", utils.PathSearch("product_type", getCustomTemplateRespBody, nil)),
+		d.Set("storage_type", utils.PathSearch("storage_type", getCustomTemplateRespBody, nil)),
+		d.Set("description", utils.PathSearch("description", getCustomTemplateRespBody, nil)),
+		d.Set("params", flattenGetCustomTemplateResponseBodyParam(d, getCustomTemplateRespBody)),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func parseDcsErrorToError404(err error) error {
+	if errCode, ok := err.(golangsdk.ErrDefault400); ok {
+		var apiError interface{}
+		if jsonErr := json.Unmarshal(errCode.Body, &apiError); jsonErr != nil {
+			return err
+		}
+
+		errorCode, errorCodeErr := jmespath.Search("error_code", apiError)
+		if errorCodeErr != nil {
+			return err
+		}
+
+		if errorCode == "DCS.4988" {
+			return golangsdk.ErrDefault404(errCode)
+		}
+	}
+
+	return err
+}
+
+func flattenGetCustomTemplateResponseBodyParam(d *schema.ResourceData, resp interface{}) []interface{} {
+	if resp == nil {
+		return nil
+	}
+	paramsMap := buildParamsMap(d)
+	curJson := utils.PathSearch("params", resp, make([]interface{}, 0))
+	curArray := curJson.([]interface{})
+	rst := make([]interface{}, 0, len(curArray))
+	for _, v := range curArray {
+		paramName := utils.PathSearch("param_name", v, "").(string)
+		if !paramsMap[paramName] {
+			continue
+		}
+		rst = append(rst, map[string]interface{}{
+			"param_id":      utils.PathSearch("param_id", v, nil),
+			"param_name":    paramName,
+			"param_value":   utils.PathSearch("param_value", v, nil),
+			"default_value": utils.PathSearch("default_value", v, nil),
+			"value_range":   utils.PathSearch("value_range", v, nil),
+			"value_type":    utils.PathSearch("value_type", v, nil),
+			"description":   utils.PathSearch("description", v, nil),
+			"need_restart":  utils.PathSearch("need_restart", v, nil),
+		})
+	}
+	return rst
+}
+
+func buildParamsMap(d *schema.ResourceData) map[string]bool {
+	params := d.Get("params").(*schema.Set).List()
+	paramsMap := make(map[string]bool)
+	for _, param := range params {
+		if v, ok := param.(map[string]interface{}); ok {
+			paramsMap[v["param_name"].(string)] = true
+		}
+	}
+	return paramsMap
+}
+
+func resourceCustomTemplateUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	updateCustomTemplateChanges := []string{
+		"name",
+		"description",
+		"params",
+	}
+
+	if d.HasChanges(updateCustomTemplateChanges...) {
+		// updateCustomTemplate: update DCS custom template
+		var (
+			updateCustomTemplateHttpUrl = "v2/{project_id}/config-templates/{template_id}"
+			updateCustomTemplateProduct = "dcs"
+		)
+		updateCustomTemplateClient, err := cfg.NewServiceClient(updateCustomTemplateProduct, region)
+		if err != nil {
+			return diag.Errorf("error creating DCS client: %s", err)
+		}
+
+		updateCustomTemplatePath := updateCustomTemplateClient.Endpoint + updateCustomTemplateHttpUrl
+		updateCustomTemplatePath = strings.ReplaceAll(updateCustomTemplatePath, "{project_id}",
+			updateCustomTemplateClient.ProjectID)
+		updateCustomTemplatePath = strings.ReplaceAll(updateCustomTemplatePath, "{template_id}", d.Id())
+
+		updateCustomTemplateOpt := golangsdk.RequestOpts{
+			KeepResponseBody: true,
+			MoreHeaders:      map[string]string{"Content-Type": "application/json"},
+		}
+
+		updateCustomTemplateOpt.JSONBody = utils.RemoveNil(buildUpdateCustomTemplateBodyParams(d))
+		_, err = updateCustomTemplateClient.Request("PUT", updateCustomTemplatePath, &updateCustomTemplateOpt)
+		if err != nil {
+			return diag.Errorf("error updating DCS custom template: %s", err)
+		}
+	}
+	return resourceCustomTemplateRead(ctx, d, meta)
+}
+
+func buildUpdateCustomTemplateBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"name":        utils.ValueIngoreEmpty(d.Get("name")),
+		"description": utils.ValueIngoreEmpty(d.Get("description")),
+		"params":      buildUpdateCustomTemplateRequestBodyParam(d.Get("params").(*schema.Set).List()),
+	}
+	return bodyParams
+}
+
+func buildUpdateCustomTemplateRequestBodyParam(rawParams interface{}) map[string]string {
+	if rawArray, ok := rawParams.([]interface{}); ok {
+		if len(rawArray) == 0 {
+			return nil
+		}
+
+		rst := make(map[string]string)
+		for _, v := range rawArray {
+			if raw, ok := v.(map[string]interface{}); ok {
+				rst[raw["param_name"].(string)] = raw["param_value"].(string)
+			}
+		}
+		return rst
+	}
+	return nil
+}
+
+func resourceCustomTemplateDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	// deleteCustomTemplate: Delete DCS custom template
+	var (
+		deleteCustomTemplateHttpUrl = "v2/{project_id}/config-templates/{template_id}"
+		deleteCustomTemplateProduct = "dcs"
+	)
+	deleteCustomTemplateClient, err := cfg.NewServiceClient(deleteCustomTemplateProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating DCS client: %s", err)
+	}
+
+	deleteCustomTemplatePath := deleteCustomTemplateClient.Endpoint + deleteCustomTemplateHttpUrl
+	deleteCustomTemplatePath = strings.ReplaceAll(deleteCustomTemplatePath, "{project_id}",
+		deleteCustomTemplateClient.ProjectID)
+	deleteCustomTemplatePath = strings.ReplaceAll(deleteCustomTemplatePath, "{template_id}", d.Id())
+
+	deleteCustomTemplateOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		MoreHeaders:      map[string]string{"Content-Type": "application/json"},
+	}
+
+	_, err = deleteCustomTemplateClient.Request("DELETE", deleteCustomTemplatePath, &deleteCustomTemplateOpt)
+	if err != nil {
+		return diag.Errorf("error deleting DCS custom template: %s", err)
+	}
+
+	return nil
+}
