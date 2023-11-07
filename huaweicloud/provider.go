80931168c594ba23955bd11c682a058594d1db1f
wangyuancheng01 <wangyuancheng01@outlook.com>
2023-11-07 16:25:23 +0800

    move server into services/iec

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -968,12 +968,13 @@ func Provider() *schema.Provider {
 			"huaweicloud_iec_keypair":             iec.ResourceKeypair(),
 			"huaweicloud_iec_network_acl":         resourceIecNetworkACL(),
 			"huaweicloud_iec_network_acl_rule":    resourceIecNetworkACLRule(),
-			"huaweicloud_iec_security_group":      resourceIecSecurityGroup(),
 			"huaweicloud_iec_security_group_rule": iec.ResourceIecSecurityGroupRule(),
-			"huaweicloud_iec_server":              iec.ResourceIecServer(),
-			"huaweicloud_iec_vip":                 iec.ResourceIecVip(),
-			"huaweicloud_iec_vpc":                 iec.ResourceIecVpc(),
-			"huaweicloud_iec_vpc_subnet":          iec.ResourceIecSubnet(),
+			"huaweicloud_iec_security_group":      iec.ResourceIecSecurityGroup(),
+
+			"huaweicloud_iec_server":     iec.ResourceIecServer(),
+			"huaweicloud_iec_vip":        iec.ResourceIecVip(),
+			"huaweicloud_iec_vpc":        iec.ResourceIecVpc(),
+			"huaweicloud_iec_vpc_subnet": iec.ResourceIecSubnet(),
 
 			"huaweicloud_images_image":                ims.ResourceImsImage(),
 			"huaweicloud_images_image_copy":           ims.ResourceImsImageCopy(),
