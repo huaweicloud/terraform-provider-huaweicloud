4e0642dff61f0f99c637a4540848f1e2f5b012de
ruwenqiang123 <148871570+ruwenqiang123@users.noreply.github.com>
2023-11-09 20:46:51 +0800

    refactor(iec/keypair): move to the service folder (#3636)
    * chore(iec/keypair): move files to the service folder
    
    * refactor(iec/keypair): adjust code style

---
+++ huaweicloud/services/acceptance/iec/resource_huaweicloud_iec_keypair_test.go
@@ -0,0 +1,69 @@
+package iec
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/chnsz/golangsdk/openstack/iec/v1/common"
+	"github.com/chnsz/golangsdk/openstack/iec/v1/keypairs"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func getKeypairResourceFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	c, err := cfg.IECV1Client(acceptance.HW_REGION_NAME)
+	if err != nil {
+		return nil, fmt.Errorf("error creating IEC v1 client: %s", err)
+	}
+	return keypairs.Get(c, state.Primary.ID).Extract()
+}
+
+func TestAccKeypairResource_basic(t *testing.T) {
+	var (
+		keypair      common.KeyPair
+		name         = acceptance.RandomAccResourceName()
+		resourceName = "huaweicloud_iec_keypair.test"
+	)
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&keypair,
+		getKeypairResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccKeypair_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttrSet(resourceName, "public_key"),
+					resource.TestCheckResourceAttrSet(resourceName, "fingerprint"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccKeypair_basic(name string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_iec_keypair" "test" {
+  name = "%s"
+}
+`, name)
+}
---
+++ huaweicloud/services/iec/resource_huaweicloud_iec_keypair.go
@@ -0,0 +1,111 @@
+package iec
+
+import (
+	"context"
+	"log"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/chnsz/golangsdk/openstack/iec/v1/keypairs"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+)
+
+func ResourceKeypair() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceKeypairCreate,
+		ReadContext:   resourceKeypairRead,
+		DeleteContext: resourceKeypairDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"public_key": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"fingerprint": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceKeypairCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	iecClient, err := cfg.IECV1Client(cfg.GetRegion(d))
+	if err != nil {
+		return diag.Errorf("error creating IEC v1 client: %s", err)
+	}
+
+	createOpts := keypairs.CreateOpts{
+		Name:      d.Get("name").(string),
+		PublicKey: d.Get("public_key").(string),
+	}
+
+	log.Printf("[DEBUG] Create IEC keypair options: %#v", createOpts)
+	kp, err := keypairs.Create(iecClient, createOpts).Extract()
+	if err != nil {
+		return diag.Errorf("error creating IEC keypair: %s", err)
+	}
+
+	d.SetId(kp.Name)
+
+	return resourceKeypairRead(ctx, d, meta)
+}
+
+func resourceKeypairRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	iecClient, err := cfg.IECV1Client(cfg.GetRegion(d))
+	if err != nil {
+		return diag.Errorf("error creating IEC v1 client: %s", err)
+	}
+
+	kp, err := keypairs.Get(iecClient, d.Id()).Extract()
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "IEC keypair")
+	}
+
+	mErr := multierror.Append(
+		nil,
+		d.Set("name", kp.Name),
+		d.Set("public_key", kp.PublicKey),
+		d.Set("fingerprint", kp.Fingerprint),
+	)
+	if err = mErr.ErrorOrNil(); err != nil {
+		return diag.Errorf("error setting IEC keypair: %s", err)
+	}
+	return nil
+}
+
+func resourceKeypairDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	iecClient, err := cfg.IECV1Client(cfg.GetRegion(d))
+	if err != nil {
+		return diag.Errorf("error creating IEC v1 client: %s", err)
+	}
+
+	err = keypairs.Delete(iecClient, d.Id()).ExtractErr()
+	if err != nil {
+		return diag.Errorf("error deleting IEC keypair: %s", err)
+	}
+	return nil
+}
