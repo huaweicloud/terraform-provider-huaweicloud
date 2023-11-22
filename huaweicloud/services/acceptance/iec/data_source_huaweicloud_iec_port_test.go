b0798d90384a1af22de7d17a60518b5092e4d4a3
wuzhuanhong <wuzhuanhong1@outlook.com>
2023-11-22 09:07:58 +0800

    chore(iec): adjust code style

--- huaweicloud/services/acceptance/iec/data_source_huaweicloud_iec_port_test.go
+++ huaweicloud/services/acceptance/iec/data_source_huaweicloud_iec_port_test.go
@@ -1,4 +1,4 @@
-package huaweicloud
+package iec
 
 import (
 	"fmt"
@@ -6,16 +6,19 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
 func TestAccIECPortDataSource_basic(t *testing.T) {
 	rName := fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
 	resourceName := "data.huaweicloud_iec_port.port_1"
+	rc := acceptance.InitDataSourceCheck(resourceName)
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckIecVpcSubnetV1Destroy,
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      testAccCheckIecVpcSubnetV1Destroy,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccIECNetworkConfig_base(rName),
@@ -23,12 +26,41 @@ func TestAccIECPortDataSource_basic(t *testing.T) {
 			{
 				Config: testAccIECPortDataSource_basic(rName),
 				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
 					resource.TestCheckResourceAttrSet(resourceName, "mac_address"),
 					resource.TestCheckResourceAttrSet(resourceName, "site_id"),
 				),
 			},
 		},
 	})
+}
+
+func testAccIECNetworkConfig_base(rName string) string {
+	return fmt.Sprintf(`
+data "huaweicloud_iec_sites" "sites_test" {}
+
+resource "huaweicloud_iec_vpc" "vpc_test" {
+  name = "%[1]s"
+  cidr = "192.168.0.0/16"
+  mode = "CUSTOMER"
+}
+
+resource "huaweicloud_iec_vpc_subnet" "subnet_1" {
+  name       = "%[1]s-1"
+  cidr       = "192.168.0.0/24"
+  vpc_id     = huaweicloud_iec_vpc.vpc_test.id
+  site_id    = data.huaweicloud_iec_sites.sites_test.sites[0].id
+  gateway_ip = "192.168.0.1"
+}
+
+resource "huaweicloud_iec_vpc_subnet" "subnet_2" {
+  name       = "%[1]s-2"
+  cidr       = "192.168.1.0/24"
+  vpc_id     = huaweicloud_iec_vpc.vpc_test.id
+  site_id    = data.huaweicloud_iec_sites.sites_test.sites[1].id
+  gateway_ip = "192.168.1.1"
+}
+`, rName)
 }
 
 func testAccIECPortDataSource_basic(rName string) string {
