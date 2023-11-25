927c31c67999d9ec9856bc176ee63226c3e587ba
Jason-Zhang <jasonzhang9309@163.com>
2023-11-25 11:40:02 +0800

    feat(secmaster): add new resource alert rule (#3678)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1126,10 +1126,11 @@ func Provider() *schema.Provider {
 			"huaweicloud_sdrs_protected_instance": sdrs.ResourceProtectedInstance(),
 			"huaweicloud_sdrs_replication_attach": sdrs.ResourceReplicationAttach(),
 
-			"huaweicloud_secmaster_incident":  secmaster.ResourceIncident(),
-			"huaweicloud_secmaster_indicator": secmaster.ResourceIndicator(),
-			"huaweicloud_secmaster_alert":     secmaster.ResourceAlert(),
-			"huaweicloud_secmaster_playbook":  secmaster.ResourcePlaybook(),
+			"huaweicloud_secmaster_incident":   secmaster.ResourceIncident(),
+			"huaweicloud_secmaster_indicator":  secmaster.ResourceIndicator(),
+			"huaweicloud_secmaster_alert":      secmaster.ResourceAlert(),
+			"huaweicloud_secmaster_alert_rule": secmaster.ResourceAlertRule(),
+			"huaweicloud_secmaster_playbook":   secmaster.ResourcePlaybook(),
 
 			"huaweicloud_servicestage_application":                 servicestage.ResourceApplication(),
 			"huaweicloud_servicestage_component_instance":          servicestage.ResourceComponentInstance(),
