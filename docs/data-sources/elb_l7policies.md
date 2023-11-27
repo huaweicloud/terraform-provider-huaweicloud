dbc20835b89e3642d5cfafa50f65c727ba23a86c
Administrator <15105657658@163.com>
2023-11-27 16:28:07 +0800

    feat(ELB): add elb l7policies data source

---
+++ docs/data-sources/elb_l7policies.md
@@ -0,0 +1,136 @@
+---
+subcategory: Dedicated Load Balance (Dedicated ELB)
+---
+
+# huaweicloud_elb_l7policies
+
+Use this data source to get the list of ELB L7 policies.
+
+## Example Usage
+
+```hcl
+variable "policy_name" {}
+
+data "huaweicloud_elb_l7policies" "test" {
+  name = var.policy_name
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the data source. If omitted, the provider-level
+  region will be used.
+
+* `l7policy_id` - (Optional, String) Specifies the forwarding policy ID.
+
+* `name` - (Optional, String) Specifies the forwarding policy name.
+
+* `description` - (Optional, String) Specifies the supplementary information about the forwarding policy.
+
+* `listener_id` - (Optional, String) Specifies the ID of the listener to which the forwarding policy is added.
+
+* `action` - (Optional, String) Specifies the requests are forwarded. The value can be one of the following:
+  + **REDIRECT_TO_POOL**: Requests are forwarded to another backend server group.
+  + **REDIRECT_TO_LISTENER**: Requests are redirected to an HTTPS listener.
+  + **REDIRECT_TO_URL**: Requests are redirected to another URL.
+  + **FIXED_RESPONSE**: A fixed response body is returned.
+
+* `priority` - (Optional, Int) Specifies the forwarding policy priority.
+
+* `redirect_listener_id` - (Optional, String) Specifies the ID of the listener to which requests are redirected.
+
+* `redirect_pool_id` - (Optional, String) Specifies the ID of the backend server group to which requests will be forwarded.
+
+* `enterprise_project_id` - (Optional, String) Specifies the enterprise project ID.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `l7policies` - Lists the L7 policies.
+  The [l7policies](#Elb_l7policies) structure is documented below.
+
+<a name="Elb_l7policies"></a>
+The `l7policies` block supports:
+
+* `id` - The forwarding policy ID.
+
+* `name` - The forwarding policy name.
+
+* `description` - The supplementary information about the forwarding policy.
+
+* `action` - The requests will be forwarded. The value can be one of the following:
+  **REDIRECT_TO_POOL**, **REDIRECT_TO_LISTENER**, **REDIRECT_TO_URL**, **FIXED_RESPONSE**.
+
+* `listener_id` - The ID of the listener to which the forwarding policy is added.
+
+* `priority` - The forwarding policy priority.
+
+* `redirect_pool_id` - The ID of the backend server group that requests will be forwarded to.
+
+* `redirect_listener_id` - The ID of the listener to which requests are redirected.
+
+* `rules` - The forwarding rules in the forwarding policy. The [rules](#Elb_l7policies_rules) structure is documented below.
+
+* `redirect_url_config` - The URL to which requests are forwarded. The [redirect_url_config](#Elb_l7policies_redirect_url_config)
+  structure is documented below.
+
+* `redirect_pools_extend_config` - The backend server group that the requests are forwarded to.
+  The [redirect_pools_extend_config](#Elb_l7policies_redirect_pools_extend_config) structure is documented below.
+
+* `fixed_response_config` - The configuration of the page that will be returned.
+  The [fixed_response_config](#Elb_l7policies_fixed_response_config) structure is documented below.
+
+* `created_at` - The time when the forwarding policy was created.
+
+* `updated_at` - The time when the forwarding policy was updated.
+
+<a name="Elb_l7policies_rules"></a>
+The `rules` block supports:
+
+* `id` - The forwarding rule ID.
+
+<a name="Elb_l7policies_redirect_url_config"></a>
+The `redirect_url_config` block supports:
+
+* `protocol` - The protocol for redirection.The value can be **HTTP**, **HTTPS**, or **${protocol}**.
+
+* `host` - The host name that requests are redirected to.
+
+* `port` - The port that requests are redirected to.
+
+* `path` - The path that requests are redirected to.
+
+* `query` - The query string set in the URL for redirection.
+
+* `status_code` - The status code returned after the requests are redirected.
+
+<a name="Elb_l7policies_redirect_pools_extend_config"></a>
+The `redirect_pools_extend_config` block supports:
+
+* `rewrite_url_enabled` - Whether to enable URL redirection.
+
+* `rewrite_url_config` - The URL for the backend server group that requests are forwarded to.
+  The [rewrite_url_config](#Elb_l7policies_rewrite_url_config) structure is documented below.
+
+<a name="Elb_l7policies_fixed_response_config"></a>
+The `fixed_response_config` block supports:
+
+* `status_code` -The HTTP status code configured in the forwarding policy.
+
+* `content_type` - The format of the response body.
+
+* `message_body` - The content of the response message body.
+
+<a name="Elb_l7policies_rewrite_url_config"></a>
+The `rewrite_url_config` block supports:
+
+* `host` - The url host.
+
+* `path` - The URL path.
+
+* `query` - The URL query character string.
---
+++ huaweicloud/services/elb/data_source_huaweicloud_elb_l7policies.go
@@ -0,0 +1,408 @@
+package elb
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/chnsz/golangsdk/pagination"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func DataSourceElbL7policies() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceElbL7policiesRead,
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"l7policy_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"listener_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"action": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"priority": {
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"redirect_listener_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"redirect_pool_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"enterprise_project_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"l7policies": {
+				Type:     schema.TypeList,
+				Elem:     l7policiesSchema(),
+				Computed: true,
+			},
+		},
+	}
+}
+
+func l7policiesSchema() *schema.Resource {
+	sc := schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"priority": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"listener_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"redirect_pool_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"redirect_listener_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"action": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"rules": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"redirect_url_config": {
+				Type:     schema.TypeList,
+				Elem:     redirectUrlSchema(),
+				Computed: true,
+			},
+			"fixed_response_config": {
+				Type:     schema.TypeList,
+				Elem:     fixedResponseSchema(),
+				Computed: true,
+			},
+			"redirect_pools_extend_config": {
+				Type:     schema.TypeList,
+				Elem:     redirectPoolsExtendSchema(),
+				Computed: true,
+			},
+			"created_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"updated_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+	return &sc
+}
+
+func redirectUrlSchema() *schema.Resource {
+	sc := schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"protocol": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"host": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"port": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"path": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"query": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"status_code": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+	return &sc
+}
+
+func fixedResponseSchema() *schema.Resource {
+	sc := schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"status_code": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"content_type": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"message_body": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+	return &sc
+}
+
+func redirectPoolsExtendSchema() *schema.Resource {
+	sc := schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"rewrite_url_enabled": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"rewrite_url_config": {
+				Type:     schema.TypeList,
+				Elem:     rewriteUrlSchema(),
+				Computed: true,
+			},
+		},
+	}
+	return &sc
+}
+
+func rewriteUrlSchema() *schema.Resource {
+	sc := schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"host": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"path": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"query": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+	return &sc
+}
+
+func dataSourceElbL7policiesRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	var (
+		listL7policiesHttpUrl = "v3/{project_id}/elb/l7policies"
+		listL7policiesProduct = "elb"
+	)
+	listL7policiesClient, err := cfg.NewServiceClient(listL7policiesProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating ELB client: %s", err)
+	}
+	listL7policiesPath := listL7policiesClient.Endpoint + listL7policiesHttpUrl
+	listL7policiesPath = strings.ReplaceAll(listL7policiesPath, "{project_id}", listL7policiesClient.ProjectID)
+	listL7policiesQueryParams := buildListL7policiesQueryParams(d)
+	listL7policiesPath += listL7policiesQueryParams
+	listL7policiesResp, err := pagination.ListAllItems(
+		listL7policiesClient,
+		"marker",
+		listL7policiesPath,
+		&pagination.QueryOpts{MarkerField: ""})
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "error retrieving ELB L7 policies")
+	}
+
+	listL7policiesRespJson, err := json.Marshal(listL7policiesResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	var listL7policiesRespBody interface{}
+	err = json.Unmarshal(listL7policiesRespJson, &listL7policiesRespBody)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	dataSourceId, err := uuid.GenerateUUID()
+	if err != nil {
+		return diag.Errorf("unable to generate ID: %s", err)
+	}
+	d.SetId(dataSourceId)
+
+	mErr := multierror.Append(
+		d.Set("region", region),
+		d.Set("l7policies", flattenListL7policiesBody(listL7policiesRespBody)),
+	)
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func buildListL7policiesQueryParams(d *schema.ResourceData) string {
+	res := ""
+	if v, ok := d.GetOk("l7policy_id"); ok {
+		res = fmt.Sprintf("%s&id=%v", res, v)
+	}
+	if v, ok := d.GetOk("name"); ok {
+		res = fmt.Sprintf("%s&name=%v", res, v)
+	}
+	if v, ok := d.GetOk("description"); ok {
+		res = fmt.Sprintf("%s&description=%v", res, v)
+	}
+	if v, ok := d.GetOk("listener_id"); ok {
+		res = fmt.Sprintf("%s&listener_id=%v", res, v)
+	}
+	if v, ok := d.GetOk("action"); ok {
+		res = fmt.Sprintf("%s&action=%v", res, v)
+	}
+	if v, ok := d.GetOk("priority"); ok {
+		res = fmt.Sprintf("%s&priority=%v", res, v)
+	}
+	if v, ok := d.GetOk("redirect_listener_id"); ok {
+		res = fmt.Sprintf("%s&redirect_listener_id=%v", res, v)
+	}
+	if v, ok := d.GetOk("redirect_pool_id"); ok {
+		res = fmt.Sprintf("%s&redirect_pool_id=%v", res, v)
+	}
+	if v, ok := d.GetOk("enterprise_project_id"); ok {
+		res = fmt.Sprintf("%s&enterprise_project_id=%v", res, v)
+	}
+	if res != "" {
+		res = "?" + res[1:]
+	}
+	return res
+}
+
+func flattenListL7policiesBody(resp interface{}) []interface{} {
+	if resp == nil {
+		return nil
+	}
+	curJson := utils.PathSearch("l7policies", resp, make([]interface{}, 0))
+	if curJson == nil {
+		return nil
+	}
+	curArray := curJson.([]interface{})
+	rst := make([]interface{}, 0, len(curArray))
+	for _, v := range curArray {
+		rst = append(rst, map[string]interface{}{
+			"id":                           utils.PathSearch("id", v, nil),
+			"name":                         utils.PathSearch("name", v, nil),
+			"description":                  utils.PathSearch("description", v, nil),
+			"priority":                     utils.PathSearch("priority", v, nil),
+			"listener_id":                  utils.PathSearch("listener_id", v, nil),
+			"redirect_pool_id":             utils.PathSearch("redirect_pool_id", v, nil),
+			"redirect_listener_id":         utils.PathSearch("redirect_listener_id", v, nil),
+			"action":                       utils.PathSearch("action", v, nil),
+			"rules":                        utils.PathSearch("rules", v, nil),
+			"redirect_url_config":          flattenRedirectUrlConfigBody(v),
+			"fixed_response_config":        flattenFixedResponseConfigBody(v),
+			"redirect_pools_extend_config": flattenRedirectPoolsExtendConfigBody(v),
+			"created_at":                   utils.PathSearch("created_at", v, nil),
+			"updated_at":                   utils.PathSearch("updated_at", v, nil),
+		})
+	}
+	return rst
+}
+
+func flattenRedirectUrlConfigBody(resp interface{}) []map[string]interface{} {
+	if resp == nil {
+		return nil
+	}
+	curJson := utils.PathSearch("redirect_url_config", resp, nil)
+	if curJson == nil {
+		return nil
+	}
+	curMap := curJson.(map[string]interface{})
+	rst := make([]map[string]interface{}, 0)
+	rst = append(rst, map[string]interface{}{
+		"protocol":    curMap["protocol"],
+		"host":        curMap["host"],
+		"port":        curMap["port"],
+		"path":        curMap["path"],
+		"query":       curMap["query"],
+		"status_code": curMap["status_code"],
+	})
+	return rst
+}
+
+func flattenFixedResponseConfigBody(resp interface{}) []map[string]interface{} {
+	if resp == nil {
+		return nil
+	}
+	curJson := utils.PathSearch("fixed_response_config", resp, nil)
+	if curJson == nil {
+		return nil
+	}
+	curMap := curJson.(map[string]interface{})
+	rst := make([]map[string]interface{}, 0)
+	rst = append(rst, map[string]interface{}{
+		"status_code":  curMap["status_code"],
+		"content_type": curMap["content_type"],
+		"message_body": curMap["message_body"],
+	})
+	return rst
+}
+
+func flattenRedirectPoolsExtendConfigBody(resp interface{}) []map[string]interface{} {
+	if resp == nil {
+		return nil
+	}
+	curJson := utils.PathSearch("redirect_pools_extend_config", resp, nil)
+	if curJson == nil {
+		return nil
+	}
+	curMap := curJson.(map[string]interface{})
+	rst := make([]map[string]interface{}, 0)
+	rewriteUrl := make([]map[string]interface{}, 0, len(curMap))
+	rst = append(rst, map[string]interface{}{
+		"rewrite_url_enabled": curMap["rewrite_url_enabled"],
+		"rewrite_url_config":  append(rewriteUrl, curMap["rewrite_url_config"].(map[string]interface{})),
+	})
+	return rst
+}
