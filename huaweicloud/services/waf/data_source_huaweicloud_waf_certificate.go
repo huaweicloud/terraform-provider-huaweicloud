bbf377efb0e4e96f50089117a516ac30830277b5
wanglong3710 <145308253+wanglong3710@users.noreply.github.com>
2023-11-27 14:18:04 +0800

    chore(waf): fix data source lint error (#3705)

--- huaweicloud/services/waf/data_source_huaweicloud_waf_certificate.go
+++ huaweicloud/services/waf/data_source_huaweicloud_waf_certificate.go
@@ -1,33 +1,35 @@
 package waf
 
 import (
+	"context"
+	"log"
 	"time"
 
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 
 	"github.com/chnsz/golangsdk/openstack/waf/v1/certificates"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils/fmtp"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils/logp"
 )
 
 const (
-	// EXP_STATUS_NOT_EXPIRED not expired
-	EXP_STATUS_NOT_EXPIRED = 0
-	// EXP_STATUS_EXPIRED has expired
-	EXP_STATUS_EXPIRED = 1
-	// EXP_STATUS_EXPIRED_SOON will expire soon
-	EXP_STATUS_EXPIRED_SOON = 2
+	// ExpStatusNotExpired not expired
+	ExpStatusNotExpired = 0
+	// ExpStatusExpired has expired
+	ExpStatusExpired = 1
+	// ExpStatusExpiredSoon will expire soon
+	ExpStatusExpiredSoon = 2
 
-	DEFAULT_PAGE_NUM  = 1
-	DEFAULT_PAGE_SIZE = 5
+	DefaultPageNum  = 1
+	DefaultPageSize = 5
 )
 
 func DataSourceWafCertificateV1() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceWafCertificateV1Read,
+		ReadContext: dataSourceWafCertificateV1Read,
 
 		Schema: map[string]*schema.Schema{
 			"region": {
@@ -42,9 +44,9 @@ func DataSourceWafCertificateV1() *schema.Resource {
 			"expire_status": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  EXP_STATUS_NOT_EXPIRED,
+				Default:  ExpStatusNotExpired,
 				ValidateFunc: validation.IntInSlice([]int{
-					EXP_STATUS_NOT_EXPIRED, EXP_STATUS_EXPIRED, EXP_STATUS_EXPIRED_SOON,
+					ExpStatusNotExpired, ExpStatusExpired, ExpStatusExpiredSoon,
 				}),
 			},
 			"enterprise_project_id": {
@@ -59,42 +61,46 @@ func DataSourceWafCertificateV1() *schema.Resource {
 	}
 }
 
-func dataSourceWafCertificateV1Read(d *schema.ResourceData, meta interface{}) error {
-	config := meta.(*config.Config)
-	wafClient, err := config.WafV1Client(config.GetRegion(d))
+func dataSourceWafCertificateV1Read(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conf := meta.(*config.Config)
+	wafClient, err := conf.WafV1Client(conf.GetRegion(d))
 	if err != nil {
-		return fmtp.Errorf("error creating HuaweiCloud WAF Client: %s", err)
+		return diag.Errorf("error creating WAF client: %s", err)
 	}
 
 	expStatus := d.Get("expire_status").(int)
 	listOpts := certificates.ListOpts{
-		Page:                DEFAULT_PAGE_NUM,
-		Pagesize:            DEFAULT_PAGE_SIZE,
+		Page:                DefaultPageNum,
+		Pagesize:            DefaultPageSize,
 		Name:                d.Get("name").(string),
 		ExpStatus:           &expStatus,
-		EnterpriseProjectID: config.GetEnterpriseProjectID(d),
+		EnterpriseProjectID: conf.GetEnterpriseProjectID(d),
 	}
 
 	page, err := certificates.List(wafClient, listOpts).AllPages()
+	if err != nil {
+		return diag.Errorf("error restrieving WAF certificates: %s", err)
+	}
 
 	listCertificates, err := certificates.ExtractCertificates(page)
 	if err != nil {
-		return fmtp.Errorf("Unable to retrieve certificates: %s", err)
+		return diag.Errorf("Unable to retrieve certificates: %s", err)
 	}
-	logp.Printf("[DEBUG] Get certificate list: %#v", listCertificates)
+	log.Printf("[DEBUG] Get certificate list: %#v", listCertificates)
 
-	if len(listCertificates) > 0 {
-		c := listCertificates[0]
-		d.SetId(c.Id)
-		d.Set("name", c.Name)
-		d.Set("expire_status", c.ExpStatus)
-
-		expires := time.Unix(int64(c.ExpireTime/1000), 0).UTC().Format("2006-01-02 15:04:05 MST")
-		d.Set("expiration", expires)
-	} else {
-		return fmtp.Errorf("Your query returned no results. " +
+	if len(listCertificates) == 0 {
+		return diag.Errorf("Your query returned no results. " +
 			"Please change your search criteria and try again.")
 	}
+	c := listCertificates[0]
+	d.SetId(c.Id)
+	expires := time.Unix(int64(c.ExpireTime/1000), 0).UTC().Format("2006-01-02 15:04:05 MST")
+	mErr := multierror.Append(
+		nil,
+		d.Set("name", c.Name),
+		d.Set("expire_status", c.ExpStatus),
+		d.Set("expiration", expires),
+	)
 
-	return nil
+	return diag.FromErr(mErr.ErrorOrNil())
 }
