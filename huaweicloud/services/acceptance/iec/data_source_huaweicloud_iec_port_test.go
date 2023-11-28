64bcf6026950467e712c97323073455e4b398fbb
wuzhuanhong <wuzhuanhong1@outlook.com>
2023-11-28 09:56:56 +0800

    chore(iec): move port data source to services dir and fix lint errors  (#3713)

---
+++ huaweicloud/services/acceptance/iec/data_source_huaweicloud_iec_port_test.go
@@ -0,0 +1,66 @@
+package iec
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccPortDataSource_basic(t *testing.T) {
+	rName := acceptance.RandomAccResourceName()
+	resourceName := "data.huaweicloud_iec_port.test"
+	dc := acceptance.InitDataSourceCheck(resourceName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      testAccCheckIecVpcSubnetV1Destroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIECNetworkConfig_base(rName),
+			},
+			{
+				Config: testAccIECPortDataSource_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(resourceName, "mac_address"),
+					resource.TestCheckResourceAttrSet(resourceName, "site_id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccIECNetworkConfig_base(rName string) string {
+	return fmt.Sprintf(`
+data "huaweicloud_iec_sites" "test" {}
+
+resource "huaweicloud_iec_vpc" "test" {
+  name = "%[1]s"
+  cidr = "192.168.0.0/16"
+  mode = "CUSTOMER"
+}
+
+resource "huaweicloud_iec_vpc_subnet" "test" {
+  name       = "%[1]s"
+  cidr       = "192.168.0.0/24"
+  vpc_id     = huaweicloud_iec_vpc.test.id
+  site_id    = data.huaweicloud_iec_sites.test.sites[0].id
+  gateway_ip = "192.168.0.1"
+}
+`, rName)
+}
+
+func testAccIECPortDataSource_basic(rName string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_iec_port" "test" {
+  fixed_ip  = huaweicloud_iec_vpc_subnet.test.gateway_ip
+  subnet_id = huaweicloud_iec_vpc_subnet.test.id
+}
+`, testAccIECNetworkConfig_base(rName))
+}
