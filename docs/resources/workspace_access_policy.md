0a90fe55f1299bf942040c15c5c9ddcdb84cee98
Lance52259 <Overlord52259@gmail.com>
2023-11-28 11:20:42 +0800

    feat(workspace): support the access policy resource

---
+++ docs/resources/workspace_access_policy.md
@@ -0,0 +1,82 @@
+---
+subcategory: "Workspace"
+---
+
+# huaweicloud_workspace_access_policy
+
+Manages access policy resource within HuaweiCloud.
+
+## Example Usage
+
+### Create a private access policy
+
+```hcl
+variable "workspace_user_id" {}
+
+resource "huaweicloud_workspace_access_policy" "test" {
+  name           = "PRIVATE_ACCESS"
+  blacklist_type = "INTERNET"
+
+  blacklist {
+    object_type = "USER"
+    object_id   = var.workspace_user_id
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) The region where the access policy is located.
+  If omitted, the provider-level region will be used. Changing this will create a new resource.
+
+* `name` - (Required, String, ForceNew) Specifies the name of the access policy.
+  + **PRIVATE_ACCESS**
+
+  Changing this will create a new resource.
+
+  -> Custom names are not supported, and only one resource can be created with each name at the same time.
+
+* `blacklist_type` - (Required, String, ForceNew) Specifies the type of access policy blacklist.  
+  The valid values are as follows:
+  + **INTERNET**
+
+  Changing this will create a new resource.
+
+* `blacklist` - (Optional, List) Specifies the blacklist configuration to which the policy applies.  
+  The [blacklist](#access_policy_blacklist_objects_args) structure is documented below.
+
+<a name="access_policy_blacklist_objects_args"></a>
+The `blacklist` block supports:
+
+* `object_type` - (Required, String) Specifies the object type.
+  The valid values are as follows:
+  + **USER**
+  + **USERGROUP**
+
+* `object_id` - (Required, String) Specifies the object ID.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The access policy ID in UUID format.
+
+* `blacklist` - The blacklist configuration to which the policy applies.  
+  The [blacklist](#access_policy_blacklist_objects_attr) structure is documented below.
+
+* `created_at` - The creation time of the access policy.
+
+<a name="access_policy_blacklist_objects_attr"></a>
+The `blacklist` block supports:
+
+* `object_name` - The object name.
+
+## Import
+
+Access policies can be imported using their `name`, e.g.
+
+```bash
+$ terraform import huaweicloud_workspace_access_policy.test <name>
+```
---
+++ huaweicloud/services/acceptance/workspace/resource_huaweicloud_workspace_access_policy_test.go
@@ -0,0 +1,142 @@
+package workspace
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/chnsz/golangsdk/openstack/workspace/v2/accesspolicies"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/workspace"
+)
+
+func getAccessPolicyFunc(conf *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	client, err := conf.WorkspaceV2Client(acceptance.HW_REGION_NAME)
+	if err != nil {
+		return nil, fmt.Errorf("error creating Workspace v2 client: %s", err)
+	}
+
+	return workspace.GetAccessPolicyByPolicyName(client, state.Primary.Attributes["name"])
+}
+
+func TestAccAccessPolicy_basic(t *testing.T) {
+	var (
+		accessPolicy *accesspolicies.AccessPolicyDetailInfo
+		resourceName = "huaweicloud_workspace_access_policy.test"
+		rName        = acceptance.RandomAccResourceNameWithDash()
+		rc           = acceptance.InitResourceCheck(resourceName, &accessPolicy, getAccessPolicyFunc)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAccessPolicy_basic_step1(rName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", "PRIVATE_ACCESS"),
+					resource.TestCheckResourceAttr(resourceName, "blacklist_type", "INTERNET"),
+					resource.TestCheckResourceAttr(resourceName, "blacklist.#", "2"),
+				),
+			},
+			{
+				Config: testAccAccessPolicy_basic_step2(rName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", "PRIVATE_ACCESS"),
+					resource.TestCheckResourceAttr(resourceName, "blacklist_type", "INTERNET"),
+					resource.TestCheckResourceAttr(resourceName, "blacklist.#", "2"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateIdFunc: testAccAccessPolicyImportStateFunc(resourceName),
+			},
+		},
+	})
+}
+
+func testAccAccessPolicyImportStateFunc(rName string) resource.ImportStateIdFunc {
+	return func(s *terraform.State) (string, error) {
+		rs, ok := s.RootModule().Resources[rName]
+		if !ok {
+			return "", fmt.Errorf("resource (%s) not found: %s", rName, rs)
+		}
+		if rs.Primary.Attributes["name"] == "" {
+			return "", fmt.Errorf("invalid format specified for import ID, want '<name>', but got '%s'", rs.Primary.Attributes["name"])
+		}
+		return rs.Primary.Attributes["name"], nil
+	}
+}
+
+func testAccAccessPolicy_basic_config(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_workspace_service" "test" {
+  access_mode = "BOTH"
+  vpc_id      = huaweicloud_vpc.test.id
+  network_ids = [
+    huaweicloud_vpc_subnet.test.id,
+  ]
+}
+
+resource "huaweicloud_workspace_user" "test" {
+  depends_on = [huaweicloud_workspace_service.test]
+
+  count = 3
+
+  name  = format("%[2]s-%%d", count.index)
+  email = "basic@example.com"
+}
+`, common.TestBaseNetwork(name), name)
+}
+
+func testAccAccessPolicy_basic_step1(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_workspace_access_policy" "test" {
+  blacklist_type = "INTERNET"
+  name           = "PRIVATE_ACCESS"
+
+  dynamic "blacklist" {
+    for_each = slice(huaweicloud_workspace_user.test[*].id, 0, 2)
+
+    content {
+      object_type = "USER"
+      object_id   = blacklist.value
+    }
+  }
+}
+`, testAccAccessPolicy_basic_config(name), name)
+}
+
+func testAccAccessPolicy_basic_step2(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_workspace_access_policy" "test" {
+  blacklist_type = "INTERNET"
+  name           = "PRIVATE_ACCESS"
+
+  dynamic "blacklist" {
+    for_each = slice(huaweicloud_workspace_user.test[*].id, 1, 3)
+
+    content {
+      object_type = "USER"
+      object_id   = blacklist.value
+    }
+  }
+}
+`, testAccAccessPolicy_basic_config(name), name)
+}
