2f74b09da2d4a1aadc07d6d48ae07f3ba9f0dd9a
Administrator <15105657658@163.com>
2023-11-29 09:53:28 +0800

    feat(RDS): add rds mysql binlog resource

---
+++ docs/resources/rds_mysql_binlog.md
@@ -0,0 +1,43 @@
+---
+subcategory: "Relational Database Service (RDS)"
+---
+
+# huaweicloud_rds_mysql_binlog
+
+Manages RDS MySQL binlog resource within HuaweiCloud.
+
+## Example Usage
+
+```hcl
+variable "instance_id" {}
+
+resource "huaweicloud_rds_mysql_binlog" "test" {
+  instance_id            = var.instance_id
+  binlog_retention_hours = 6
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) The region in which to create the RDS binlog resource. If omitted, the
+  provider-level region will be used. Changing this creates a new resource.
+
+* `instance_id` - (Required, String, ForceNew) Specifies the RDS MySQL instance ID. Changing this will create a new resource.
+
+* `binlog_retention_hours` - (Required, Int) Specifies the binlog retention period. Value range: 1 to 168 (7x24).
+
+## Attribute Reference
+
+In addition to all arguments above, the following attribute is exported:
+
+* `id` - The resource ID .
+
+## Import
+
+RDS MySQL binlog can be imported using the `instance id`, e.g.
+
+```bash
+$ terraform import huaweicloud_rds_mysql_binlog.test <instance_id>
+```
---
+++ huaweicloud/services/rds/resource_huaweicloud_rds_mysql_binlog.go
@@ -0,0 +1,201 @@
+// ---------------------------------------------------------------
+// *** AUTO GENERATED CODE ***
+// @Product RDS
+// ---------------------------------------------------------------
+
+package rds
+
+import (
+	"context"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func ResourceMysqlBinlog() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceMysqlBinlogCreateOrUpdate,
+		UpdateContext: resourceMysqlBinlogCreateOrUpdate,
+		ReadContext:   resourceMysqlBinlogRead,
+		DeleteContext: resourceMysqlBinlogDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"instance_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"binlog_retention_hours": {
+				Type:         schema.TypeInt,
+				Required:     true,
+				ValidateFunc: validation.IntAtLeast(1),
+			},
+		},
+	}
+}
+
+func resourceMysqlBinlogCreateOrUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		mysqlBinlogHttpUrl = "v3/{project_id}/instances/{instance_id}/binlog/clear-policy"
+		mysqlBinlogProduct = "rds"
+	)
+	mysqlBinlogClient, err := cfg.NewServiceClient(mysqlBinlogProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating RDS client: %s", err)
+	}
+
+	instanceId := d.Get("instance_id").(string)
+	mysqlBinlogPath := mysqlBinlogClient.Endpoint + mysqlBinlogHttpUrl
+	mysqlBinlogPath = strings.ReplaceAll(mysqlBinlogPath, "{project_id}",
+		mysqlBinlogClient.ProjectID)
+	mysqlBinlogPath = strings.ReplaceAll(mysqlBinlogPath, "{instance_id}", instanceId)
+
+	mysqlBinlogOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+	requestBody := buildCreateMysqlBinlogBodyParams(d)
+	log.Printf("[DEBUG] Create or update RDS Mysql binlog request body: %#v", requestBody)
+	mysqlBinlogOpt.JSONBody = utils.RemoveNil(requestBody)
+
+	retryFunc := func() (interface{}, bool, error) {
+		_, err = mysqlBinlogClient.Request("PUT", mysqlBinlogPath, &mysqlBinlogOpt)
+		retry, err := handleMultiOperationsError(err)
+		return nil, retry, err
+	}
+	_, err = common.RetryContextWithWaitForState(&common.RetryContextWithWaitForStateParam{
+		Ctx:          ctx,
+		RetryFunc:    retryFunc,
+		WaitFunc:     rdsInstanceStateRefreshFunc(mysqlBinlogClient, instanceId),
+		WaitTarget:   []string{"ACTIVE"},
+		Timeout:      d.Timeout(schema.TimeoutCreate),
+		DelayTimeout: 1 * time.Second,
+		PollInterval: 2 * time.Second,
+	})
+	if err != nil {
+		return diag.Errorf("error creating or updating RDS Mysql binlog: %s", err)
+	}
+
+	d.SetId(instanceId)
+
+	return resourceMysqlBinlogRead(ctx, d, meta)
+}
+
+func buildCreateMysqlBinlogBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"binlog_retention_hours": d.Get("binlog_retention_hours"),
+	}
+	return bodyParams
+}
+
+func resourceMysqlBinlogRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	var mErr *multierror.Error
+	var (
+		getMysqlBinlogHttpUrl = "v3/{project_id}/instances/{instance_id}/binlog/clear-policy"
+		getMysqlBinlogProduct = "rds"
+	)
+	getMysqlBinlogClient, err := cfg.NewServiceClient(getMysqlBinlogProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating RDS client: %s", err)
+	}
+	instanceId := d.Id()
+	getMysqlBinlogPath := getMysqlBinlogClient.Endpoint + getMysqlBinlogHttpUrl
+	getMysqlBinlogPath = strings.ReplaceAll(getMysqlBinlogPath, "{project_id}", getMysqlBinlogClient.ProjectID)
+	getMysqlBinlogPath = strings.ReplaceAll(getMysqlBinlogPath, "{instance_id}", instanceId)
+	getMysqlBinlogOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		OkCodes: []int{
+			200,
+		},
+		MoreHeaders: map[string]string{"Content-Type": "application/json"},
+	}
+	getMysqlBinlogResp, err := getMysqlBinlogClient.Request("GET", getMysqlBinlogPath, &getMysqlBinlogOpt)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	getMysqlBinlogRespBody, err := utils.FlattenResponse(getMysqlBinlogResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	retentionHours := utils.PathSearch("binlog_retention_hours", getMysqlBinlogRespBody, nil)
+
+	if retentionHours == nil || int(retentionHours.(float64)) == 0 {
+		return common.CheckDeletedDiag(d, golangsdk.ErrDefault404{}, "")
+	}
+	mErr = multierror.Append(
+		mErr,
+		d.Set("region", region),
+		d.Set("instance_id", instanceId),
+		d.Set("binlog_retention_hours", retentionHours),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func resourceMysqlBinlogDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	var (
+		deleteMysqlBinlogHttpUrl = "v3/{project_id}/instances/{instance_id}/binlog/clear-policy"
+		deleteMysqlBinlogProduct = "rds"
+	)
+	deleteMysqlBinlogClient, err := cfg.NewServiceClient(deleteMysqlBinlogProduct, region)
+	if err != nil {
+		return diag.Errorf("error deleting RDS client: %s", err)
+	}
+	instanceId := d.Get("instance_id").(string)
+	deleteMysqlBinlogPath := deleteMysqlBinlogClient.Endpoint + deleteMysqlBinlogHttpUrl
+	deleteMysqlBinlogPath = strings.ReplaceAll(deleteMysqlBinlogPath, "{project_id}",
+		deleteMysqlBinlogClient.ProjectID)
+	deleteMysqlBinlogPath = strings.ReplaceAll(deleteMysqlBinlogPath, "{instance_id}", instanceId)
+	deleteMysqlBinlogOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+	requestBody := map[string]interface{}{
+		"binlog_retention_hours": 0,
+	}
+	deleteMysqlBinlogOpt.JSONBody = requestBody
+	retryFunc := func() (interface{}, bool, error) {
+		_, err = deleteMysqlBinlogClient.Request("PUT", deleteMysqlBinlogPath, &deleteMysqlBinlogOpt)
+		retry, err := handleMultiOperationsError(err)
+		return nil, retry, err
+	}
+	_, err = common.RetryContextWithWaitForState(&common.RetryContextWithWaitForStateParam{
+		Ctx:          ctx,
+		RetryFunc:    retryFunc,
+		WaitFunc:     rdsInstanceStateRefreshFunc(deleteMysqlBinlogClient, instanceId),
+		WaitTarget:   []string{"ACTIVE"},
+		Timeout:      d.Timeout(schema.TimeoutDelete),
+		DelayTimeout: 1 * time.Second,
+		PollInterval: 2 * time.Second,
+	})
+	if err != nil {
+		return diag.Errorf("error deleting RDS Mysql binlog: %s", err)
+	}
+	return nil
+}
