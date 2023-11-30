519ebff48dea685d0e083e9d2957e8ab9eda36a7
shichangkuo <shichangkuo90@gmail.com>
2023-11-30 19:56:23 +0800

    feat(coc): add COC script execute resource (#3757)

---
+++ docs/incubating/coc_script_execute.md
@@ -0,0 +1,100 @@
+---
+subcategory: "Cloud Operations Center (COC)"
+---
+
+# huaweicloud_coc_script_execute
+
+Execute a COC script on a specified ECS instance within HuaweiCloud.
+
+-> Please make sure the ECS instance has installed the [UniAgent](https://support.huaweicloud.com/intl/en-us/usermanual-aom2/agent_01_0005.html).
+
+## Example Usage
+
+```hcl
+variable "script_id" {}
+variable "instance_id" {}
+
+resource "huaweicloud_coc_script_execute" "test" {
+  script_id    = var.script_id
+  instance_id  = var.instance_id
+  timeout      = 600
+  execute_user = "root"
+
+  parameters {
+    name  = "param1"
+    value = "value1"
+  }
+  parameters {
+    name  = "param2"
+    value = "value2"
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `script_id` - (Required, String, ForceNew) Specifies the COC script ID.
+  Changing this creates a new resource.
+
+* `instance_id` - (Required, String, ForceNew) Specifies the ECS instance ID.
+  Changing this creates a new resource.
+
+* `timeout` - (Required, Int) Specifies the maximum time to execute the script in seconds.
+
+* `execute_user` - (Required, String) Specifies the user to execute the script.
+
+* `parameters` - (Optional, List) Specifies the input parameters of the script.
+  Up to 20 script parameters can be added. The [parameters](#block--parameters) structure is documented below.
+
+<a name="block--parameters"></a>
+The `parameters` block supports:
+
+* `name` - (Required, String) Specifies the name of the parameter.
+
+* `value` - (Required, String) Specifies the value of the parameter.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` -  The resource ID.
+* `script_name` - The script name.
+* `status` - The status of the script execution.
+* `created_at` - The start time of the script execution.
+* `finished_at` - The end time of the script execution.
+
+## Timeouts
+
+This resource provides the following timeouts configuration options:
+
+* `create` - Default is 10 minutes.
+* `delete` - Default is 10 minutes.
+
+## Import
+
+The COC script execution can be imported using `id`, e.g.
+
+```bash
+$ terraform import huaweicloud_coc_script_execute.test <id>
+```
+
+Note that the imported state may not be identical to your resource definition, due to some attributes missing from the
+API response, security or some other reason. The missing attributes include `instance_id` and `parameters`.
+
+It is generally recommended running `terraform plan` after importing the resource.
+You can then decide if changes should be applied to the instance, or the resource definition should be updated to
+align with the resource. Also you can ignore changes as below.
+
+```hcl
+resource "huaweicloud_coc_script_execute" "test" {
+    ...
+
+  lifecycle {
+    ignore_changes = [
+      instance_id, parameters
+    ]
+  }
+}
+```
---
+++ huaweicloud/services/acceptance/coc/resource_huaweicloud_coc_script_execute_test.go
@@ -0,0 +1,108 @@
+package coc
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func getScriptExecuteResourceFunc(conf *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	client, err := conf.NewServiceClient("coc", acceptance.HW_REGION_NAME)
+	if err != nil {
+		return nil, fmt.Errorf("error creating COC client: %s", err)
+	}
+
+	getScriptExecuteHttpUrl := "v1/job/script/orders/{id}"
+	getScriptExecutePath := client.Endpoint + getScriptExecuteHttpUrl
+	getScriptExecutePath = strings.ReplaceAll(getScriptExecutePath, "{id}", state.Primary.ID)
+
+	getScriptExecuteOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		MoreHeaders:      map[string]string{"Content-Type": "application/json"},
+	}
+
+	getScriptExecuteResp, err := client.Request("GET", getScriptExecutePath, &getScriptExecuteOpt)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving COC script execute: %s", err)
+	}
+
+	respBody, err := utils.FlattenResponse(getScriptExecuteResp)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving COC script execute: %s", err)
+	}
+
+	return respBody, nil
+}
+
+func TestAccScriptExecute_basic(t *testing.T) {
+	var obj interface{}
+	rName := acceptance.RandomAccResourceName()
+	resourceName := "huaweicloud_coc_script_execute.test"
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&obj,
+		getScriptExecuteResourceFunc,
+	)
+
+	// lintignore:AT001
+	// without CheckDestroy because the ticket ID always exits.
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckInternal(t)
+			acceptance.TestAccPreCheckCocInstanceID(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: tesScriptExecute_basic(rName, acceptance.HW_COC_INSTANCE_ID),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "script_name", rName),
+					resource.TestCheckResourceAttr(resourceName, "status", "FINISHED"),
+					resource.TestCheckResourceAttr(resourceName, "parameters.#", "2"),
+					resource.TestCheckResourceAttrPair(resourceName, "script_id", "huaweicloud_coc_script.test", "id"),
+					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(resourceName, "finished_at"),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"instance_id", "parameters"},
+			},
+		},
+	})
+}
+
+func tesScriptExecute_basic(name, instanceID string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_coc_script_execute" "test" {
+  script_id    = huaweicloud_coc_script.test.id
+  instance_id  = "%s"
+  timeout      = 600
+  execute_user = "root"
+
+  parameters {
+    name  = "name"
+    value = "somebody"
+  }
+  parameters {
+    name  = "company"
+    value = "HuaweiCloud"
+  }
+}`, tesScript_updated(name), instanceID)
+}
