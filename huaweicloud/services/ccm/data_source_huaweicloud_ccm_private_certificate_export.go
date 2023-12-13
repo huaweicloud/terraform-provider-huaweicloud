b95bdc9534db90d345b8258325631de10f313b23
zhangtao505 <zhangtao505@h-partners.com>
2023-12-13 16:47:40 +0800

    feat(CCM): support export certificate for tomcat and iis servers (#3803)

--- huaweicloud/services/ccm/data_source_huaweicloud_ccm_private_certificate_export.go
+++ huaweicloud/services/ccm/data_source_huaweicloud_ccm_private_certificate_export.go
@@ -1,7 +1,11 @@
 package ccm
 
 import (
+	"archive/zip"
+	"bytes"
 	"context"
+	"fmt"
+	"io"
 	"strings"
 
 	"github.com/hashicorp/go-multierror"
@@ -70,6 +74,18 @@ func DataSourceCcmPrivateCertificateExport() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"keystore_pass": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"server_pfx": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"server_jks": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 		},
 	}
 }
@@ -82,44 +98,94 @@ func ccmPrivateCertificateExport(_ context.Context, d *schema.ResourceData, meta
 		return diag.Errorf("error creating CCM client: %s", err)
 	}
 
+	certId := d.Get("certificate_id").(string)
 	expCertificateHttpUrl := "v1/private-certificates/{certificate_id}/export"
 	expCertificatePath := client.Endpoint + expCertificateHttpUrl
-	expCertificatePath = strings.ReplaceAll(expCertificatePath, "{certificate_id}", d.Get("certificate_id").(string))
+	expCertificatePath = strings.ReplaceAll(expCertificatePath, "{certificate_id}", certId)
 
 	expCertificateOpt := golangsdk.RequestOpts{
 		KeepResponseBody: true,
 		MoreHeaders:      map[string]string{"Content-Type": "application/json"},
 	}
-	expCertificateOpt.JSONBody = utils.RemoveNil(buildExpCertificateBodyParams(d))
+	// get the server type, iis and tomcat export zip certificate file
+	serverType := d.Get("type").(string)
+	isCompressed := false
+	if strings.ToUpper(serverType) == "IIS" || strings.ToUpper(serverType) == "TOMCAT" {
+		isCompressed = true
+	}
+	expCertificateOpt.JSONBody = utils.RemoveNil(buildExpCertificateBodyParams(d, serverType, isCompressed))
 	expCertificateResp, err := client.Request("POST", expCertificatePath, &expCertificateOpt)
 	if err != nil {
 		return diag.Errorf("error export CCM private certificate: %s", err)
 	}
-	expCertificateRespBody, err := utils.FlattenResponse(expCertificateResp)
-	if err != nil {
-		return diag.FromErr(err)
-	}
+	d.SetId(certId)
 
-	d.SetId(d.Get("certificate_id").(string))
-	mErr := multierror.Append(nil,
-		d.Set("private_key", utils.PathSearch("private_key", expCertificateRespBody, nil)),
-		d.Set("certificate", utils.PathSearch("certificate", expCertificateRespBody, nil)),
-		d.Set("certificate_chain", utils.PathSearch("certificate_chain", expCertificateRespBody, nil)),
-		d.Set("enc_private_key", utils.PathSearch("enc_private_key", expCertificateRespBody, nil)),
-		d.Set("enc_certificate", utils.PathSearch("enc_certificate", expCertificateRespBody, nil)),
-		d.Set("enc_sm2_enveloped_key", utils.PathSearch("enc_sm2_enveloped_key", expCertificateRespBody, nil)),
-		d.Set("signed_and_enveloped_data", utils.PathSearch("signed_and_enveloped_data", expCertificateRespBody, nil)),
-	)
-	if err := mErr.ErrorOrNil(); err != nil {
-		return diag.Errorf("error setting CCM private certificate export fields: %s", err)
+	if isCompressed {
+		// save certificate file from file stream
+		data, err := io.ReadAll(expCertificateResp.Body)
+		if err != nil {
+			return diag.Errorf("error reading response certificate: %s", err)
+		}
+		zipReader, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+		if err != nil {
+			return diag.Errorf("error reading zip certificate: %s", err)
+		}
+		for _, f := range zipReader.File {
+			err := saveCertificateFromZipFile(f, d)
+			if err != nil {
+				return diag.Errorf("error reading unzip certificate: %s", err)
+			}
+		}
+	} else {
+		expCertificateRespBody, err := utils.FlattenResponse(expCertificateResp)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		// save certificate file from json
+		mErr := multierror.Append(nil,
+			d.Set("private_key", utils.PathSearch("private_key", expCertificateRespBody, nil)),
+			d.Set("certificate", utils.PathSearch("certificate", expCertificateRespBody, nil)),
+			d.Set("certificate_chain", utils.PathSearch("certificate_chain", expCertificateRespBody, nil)),
+			d.Set("enc_private_key", utils.PathSearch("enc_private_key", expCertificateRespBody, nil)),
+			d.Set("enc_certificate", utils.PathSearch("enc_certificate", expCertificateRespBody, nil)),
+			d.Set("enc_sm2_enveloped_key", utils.PathSearch("enc_sm2_enveloped_key", expCertificateRespBody, nil)),
+			d.Set("signed_and_enveloped_data", utils.PathSearch("signed_and_enveloped_data", expCertificateRespBody, nil)),
+		)
+		if err := mErr.ErrorOrNil(); err != nil {
+			return diag.Errorf("error setting CCM private certificate export fields: %s", err)
+		}
 	}
 	return nil
 }
 
-func buildExpCertificateBodyParams(d *schema.ResourceData) map[string]interface{} {
+func saveCertificateFromZipFile(f *zip.File, d *schema.ResourceData) error {
+	fileName := f.Name
+	inFile, err := f.Open()
+	if err != nil {
+		return fmt.Errorf("error openning certificate file: %s", err)
+	}
+	defer inFile.Close()
+	content, err := io.ReadAll(inFile)
+	if err != nil {
+		return fmt.Errorf("error reading unzip certificate file: %s", err)
+	}
+	var mErr *multierror.Error
+	if fileName == "keystorePass.txt" {
+		mErr = multierror.Append(mErr, d.Set("keystore_pass", string(content)))
+	}
+	if fileName == "server.pfx" {
+		mErr = multierror.Append(mErr, d.Set("server_pfx", utils.Base64EncodeString(string(content))))
+	}
+	if fileName == "server.jks" {
+		mErr = multierror.Append(mErr, d.Set("server_jks", utils.Base64EncodeString(string(content))))
+	}
+	return mErr.ErrorOrNil()
+}
+
+func buildExpCertificateBodyParams(d *schema.ResourceData, serverType string, isCompressed bool) map[string]interface{} {
 	bodyParams := map[string]interface{}{
-		"is_compressed":  "false",
-		"type":           d.Get("type"),
+		"is_compressed":  isCompressed,
+		"type":           serverType,
 		"is_sm_standard": d.Get("sm_standard"),
 		"password":       d.Get("password"),
 	}
