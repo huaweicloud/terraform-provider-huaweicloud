afdf76a2dc2caebe831437664cf79dd06fe87fb4
ruwenqiang123 <ruwenqiang123@outlook.com>
2023-12-27 16:17:30 +0800

    feat(nat): add a new datasource to query the list of transit ip (#3879)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -555,9 +555,10 @@ func Provider() *schema.Provider {
 			"huaweicloud_elb_l7policies":        elb.DataSourceElbL7policies(),
 			"huaweicloud_elb_security_policies": elb.DataSourceElbSecurityPolicies(),
 
-			"huaweicloud_nat_gateway":          nat.DataSourcePublicGateway(),
-			"huaweicloud_nat_gateways":         nat.DataSourcePublicGateways(),
-			"huaweicloud_nat_private_gateways": nat.DataSourcePrivateGateways(),
+			"huaweicloud_nat_gateway":             nat.DataSourcePublicGateway(),
+			"huaweicloud_nat_gateways":            nat.DataSourcePublicGateways(),
+			"huaweicloud_nat_private_gateways":    nat.DataSourcePrivateGateways(),
+			"huaweicloud_nat_private_transit_ips": nat.DataSourcePrivateTransitIps(),
 
 			"huaweicloud_networking_port":      vpc.DataSourceNetworkingPortV2(),
 			"huaweicloud_networking_secgroup":  vpc.DataSourceNetworkingSecGroup(),
