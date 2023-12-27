d145e878486d9fd70d962528b28bdccd43495a5b
madanton1994 <61697522+madanton1994@users.noreply.github.com>
2023-12-27 12:27:02 +0500

    Update resource_huaweicloud_cce_node.go

--- huaweicloud/services/cce/resource_huaweicloud_cce_node.go
+++ huaweicloud/services/cce/resource_huaweicloud_cce_node.go
@@ -104,7 +104,7 @@ func ResourceNode() *schema.Resource {
 						},
 						"value": {
 							Type:     schema.TypeString,
-							Required: false,
+							Optional:  true,
 							ForceNew: true,
 						},
 						"effect": {
