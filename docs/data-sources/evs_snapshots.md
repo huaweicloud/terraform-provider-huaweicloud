9cca6f7e05526f2b9d9455c1c0bc219c7aa5b999
zhangting951007 <1719626126@qq.com>
2024-01-02 17:10:50 +0800

    feat(EVS): add data source to query the list of EVS snapshots (#3907)

---
+++ docs/data-sources/evs_snapshots.md
@@ -0,0 +1,91 @@
+---
+subcategory: "Elastic Volume Service (EVS)"
+---
+
+# huaweicloud_evs_snapshots
+
+Use this data source to get a list of EVS cloudvolume snapshots.
+
+## Example Usage
+
+```hcl
+variable "volume_id" {}
+
+data "huaweicloud_evs_snapshots" "snapshots" {
+  volume_id = var.volume_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) The region in which to query the WAF dedicated domains.
+  If omitted, the provider-level region will be used.
+  
+* `enterprise_project_id` - (Optional, String) Specify the enterprise project ID to filter.
+  If this field value is not specified, cloudvolume snapshots of all enterprise projects within
+  authority scope will be queried.
+
+* `name` - (Optional, String)  The name of cloudvolume snapshot. Maximum supported is 64 characters.
+
+* `status` - (Optional, String) The status of cloudvolume snapshot. Valid values are as follows:
+  + **creating**: The cloudvolume snapshot is in the process of being created.
+  + **available**: The cloudvolume snapshot is created successfully and can be used.
+  + **error**: An error occurred during the creation process of the cloudvolume snapshot.
+  + **deleting**: The cloudvolume snapshot is in the process of being deleted.
+  + **error_deleting**: An error occurred during the deletion process of the cloudvolume snapshot.
+  + **rollbacking**: The cloudvolume snapshot is in the process of rolling back data.
+  + **backing-up**: The status of the following two snapshots is **backing_up**:
+  The snapshots that can create backup directly through the OpenStack native API.
+  The snapshots created automatically during the process of creating a backup.
+
+* `volume_id` - (Optional, String) The ID of the cloudvolume to which the snapshot belongs.
+
+* `availability_zone` - (Optional, String) The availability zone of the cloudvolume to which the snapshot belongs.
+
+* `snapshot_id` - (Optional, String) Specify the snapshot ID to filter.
+  You can pass in multiple ids to filter the query, for example: id=id1&id=id2&id=id3.
+
+* `dedicated_storage_name` - (Optional, String) The name of the dedicated storage.
+
+* `dedicated_storage_id` - (Optional, String) The ID of the dedicated storage.
+
+* `service_type` - (Optional, String) Service type. Only **EVS**, **DSS**, and **DESS** are supported.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID, in UUID format.
+
+* `snapshots` - A list of EVS cloudvolume snapshots.
+
+The `snapshots` block supports:
+
+* `id` - The resource ID of EVS cloudvolume snapshot.
+
+* `name` - The name of cloudvolume snapshot. Maximum supported is 64 characters.
+
+* `size` - The cloudvolume snapshot size. Unit is GiB.
+
+* `status` - The status of cloudvolume snapshot. Valid values are as follows:
+  + **creating**: The cloudvolume snapshot is in the process of being created.
+  + **available**: The cloudvolume snapshot is created successfully and can be used.
+  + **error**: An error occurred during the creation process of the cloudvolume snapshot.
+  + **deleting**: The cloudvolume snapshot is in the process of being deleted.
+  + **error_deleting**: An error occurred during the deletion process of the cloudvolume snapshot.
+  + **rollbacking**: The cloudvolume snapshot is in the process of rolling back data.
+  + **backing-up**: The status of the following two snapshots is **backing_up**:
+  The snapshots that can create backup directly through the OpenStack native API.
+  The snapshots created automatically during the process of creating a backup.
+
+* `description` - The cloudvolume snapshot description information.
+
+* `created_at` - The cloudvolume snapshot creation time.
+
+* `updated_at` - The cloudvolume snapshot update time.
+
+* `volume_id` - The ID of the cloudvolume to which the snapshot belongs.
+
+* `service_type` - Service type. Valid values can be **EVS**, **DSS**, and **DESS**.
---
+++ huaweicloud/services/acceptance/evs/data_source_huaweicloud_evs_snapshots_test.go
@@ -0,0 +1,144 @@
+package evs
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDatasourceEVSsnapshots_basic(t *testing.T) {
+	name := acceptance.RandomAccResourceName()
+	rName := "data.huaweicloud_evs_snapshots.test"
+	epsIDTestName := "data.huaweicloud_evs_snapshots.enterprise_project_id_filter"
+	serviceTypeTestName := "data.huaweicloud_evs_snapshots.service_type_filter"
+	availabilityZoneTestName := "data.huaweicloud_evs_snapshots.availability_zone_filter"
+	dedicatedStorageNameTestName := "data.huaweicloud_evs_snapshots.dedicated_storage_name_filter"
+	dedicatedStorageIDTestName := "data.huaweicloud_evs_snapshots.dedicated_storage_id_filter"
+	dc := acceptance.InitDataSourceCheck(rName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourceSnapshots_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(rName, "snapshots.0.id"),
+					resource.TestCheckResourceAttrSet(rName, "snapshots.0.name"),
+					resource.TestCheckResourceAttrSet(rName, "snapshots.0.status"),
+					resource.TestCheckResourceAttrSet(rName, "snapshots.0.description"),
+					resource.TestCheckResourceAttrSet(rName, "snapshots.0.size"),
+					resource.TestCheckResourceAttrSet(rName, "snapshots.0.created_at"),
+					resource.TestCheckResourceAttrSet(rName, "snapshots.0.updated_at"),
+					resource.TestCheckResourceAttrSet(rName, "snapshots.0.volume_id"),
+					resource.TestCheckResourceAttrSet(rName, "snapshots.0.service_type"),
+					resource.TestCheckResourceAttrSet(epsIDTestName, "snapshots.#"),
+					resource.TestCheckResourceAttrSet(serviceTypeTestName, "snapshots.#"),
+					resource.TestCheckResourceAttrSet(availabilityZoneTestName, "snapshots.#"),
+					resource.TestCheckResourceAttrSet(dedicatedStorageNameTestName, "snapshots.#"),
+					resource.TestCheckResourceAttrSet(dedicatedStorageIDTestName, "snapshots.#"),
+
+					resource.TestCheckOutput("name_filter_is_useful", "true"),
+
+					resource.TestCheckOutput("status_filter_is_useful", "true"),
+
+					resource.TestCheckOutput("volume_id_filter_is_useful", "true"),
+
+					resource.TestCheckOutput("snapshot_id_filter_is_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDatasourceSnapshots_basic(name string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_evs_snapshots" "test" {
+  depends_on = [huaweicloud_evs_snapshot.test]
+}
+
+data "huaweicloud_evs_snapshots" "enterprise_project_id_filter" {
+  enterprise_project_id = huaweicloud_evs_volume.test.enterprise_project_id
+}
+
+data "huaweicloud_evs_snapshots" "name_filter" {
+  name = data.huaweicloud_evs_snapshots.test.snapshots.0.name
+}
+
+locals {
+  name = data.huaweicloud_evs_snapshots.test.snapshots.0.name
+}
+
+output "name_filter_is_useful" {
+  value = length(data.huaweicloud_evs_snapshots.name_filter.snapshots) > 0 && alltrue(
+    [for v in data.huaweicloud_evs_snapshots.name_filter.snapshots[*].name : v == local.name]
+  )  
+}
+
+data "huaweicloud_evs_snapshots" "status_filter" {
+  status = data.huaweicloud_evs_snapshots.test.snapshots.0.status
+}
+
+locals {
+  status = data.huaweicloud_evs_snapshots.test.snapshots.0.status
+}
+
+output "status_filter_is_useful" {
+  value = length(data.huaweicloud_evs_snapshots.status_filter.snapshots) > 0 && alltrue(
+    [for v in data.huaweicloud_evs_snapshots.status_filter.snapshots[*].status : v == local.status]
+  )  
+}
+
+data "huaweicloud_evs_snapshots" "volume_id_filter" {
+  volume_id = data.huaweicloud_evs_snapshots.test.snapshots.0.volume_id
+}
+
+locals {
+  volume_id = data.huaweicloud_evs_snapshots.test.snapshots.0.volume_id
+}
+
+output "volume_id_filter_is_useful" {
+  value = length(data.huaweicloud_evs_snapshots.volume_id_filter.snapshots) > 0 && alltrue(
+    [for v in data.huaweicloud_evs_snapshots.volume_id_filter.snapshots[*].volume_id : v == local.volume_id]
+  )  
+}
+
+data "huaweicloud_evs_snapshots" "snapshot_id_filter" {
+  snapshot_id = data.huaweicloud_evs_snapshots.test.snapshots.0.id
+}
+
+locals {
+  snapshot_id = data.huaweicloud_evs_snapshots.test.snapshots.0.id
+}
+
+output "snapshot_id_filter_is_useful" {
+  value = length(data.huaweicloud_evs_snapshots.snapshot_id_filter.snapshots) > 0 && alltrue(
+    [for v in data.huaweicloud_evs_snapshots.snapshot_id_filter.snapshots[*].id : v == local.snapshot_id]
+  )  
+}
+
+data "huaweicloud_evs_snapshots" "service_type_filter" {
+  service_type = "evs"
+}
+
+data "huaweicloud_evs_snapshots" "availability_zone_filter" {
+  availability_zone = data.huaweicloud_availability_zones.test.names[0]
+}
+
+data "huaweicloud_evs_snapshots" "dedicated_storage_name_filter" {
+  dedicated_storage_name = huaweicloud_evs_volume.test.dedicated_storage_name
+}
+
+data "huaweicloud_evs_snapshots" "dedicated_storage_id_filter" {
+  dedicated_storage_id = huaweicloud_evs_volume.test.dedicated_storage_id
+}
+`, testAccEvsSnapshotV2_basic(name))
+}
