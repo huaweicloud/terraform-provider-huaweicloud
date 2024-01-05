8aa2d50aa3f1ad9ca2c11341cde6b9a45a4b39b5
jinyangyang222 <jinyangyang222@outlook.com>
2024-01-05 15:03:29 +0800

    feat(as/policy): action structure support instance_percentage parameter (#3942)

--- vendor/github.com/chnsz/golangsdk/openstack/autoscaling/v1/policies/urls.go
+++ vendor/github.com/chnsz/golangsdk/openstack/autoscaling/v1/policies/urls.go
@@ -6,23 +6,27 @@ import (
 
 const resourcePath = "scaling_policy"
 
-//createURL will build the rest query url of creation
-//the create url is endpoint/scaling_policy
+// createURL will build the rest query url of creation
+// the create url is endpoint/scaling_policy
 func createURL(client *golangsdk.ServiceClient) string {
 	return client.ServiceURL(resourcePath)
 }
 
-//deleteURL will build the url of deletion
-//its pattern is endpoint/scaling_policy/<policy-id>
+// deleteURL will build the url of deletion
+// its pattern is endpoint/scaling_policy/<policy-id>
 func deleteURL(client *golangsdk.ServiceClient, id string) string {
 	return client.ServiceURL(resourcePath, id)
 }
 
-//getURL will build the get url of get function
+// getURL will build the get url of get function
 func getURL(client *golangsdk.ServiceClient, id string) string {
 	return client.ServiceURL(resourcePath, id)
 }
 
 func updateURL(c *golangsdk.ServiceClient, id string) string {
 	return c.ServiceURL(resourcePath, id)
+}
+
+func listURL(client *golangsdk.ServiceClient, groupID string) string {
+	return client.ServiceURL(resourcePath, groupID, "list")
 }
