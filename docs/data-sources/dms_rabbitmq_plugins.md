aa9a76a457edc72e157953e05f61a0eca2c6186c
zhaohongmei <346010762@qq.com>
2024-01-12 17:21:07 +0800

    feat(DMS): add data source rabbitmq plugins (#4000)

---
+++ docs/data-sources/dms_rabbitmq_plugins.md
@@ -0,0 +1,58 @@
+---
+subcategory: "Distributed Message Service (DMS)"
+---
+
+# huaweicloud_dms_rabbitmq_plugins
+
+Use this data source to get the list of DMS RabbitMQ plugins.
+
+## Example Usage
+
+```hcl
+var "instance_id" {}
+
+data "huaweicloud_dms_rabbitmq_plugins" "test" {
+  instance_id  = var.instance_id
+  name         = "rabbitmq_mqtt"
+  enable       = true
+  running      = true
+  version      = "3.8.35"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the data source.
+  If omitted, the provider-level region will be used.
+
+* `instance_id` - (Required, String) Specifies the ID of the RabbitMQ instance.
+
+* `name` - (Optional, String) Specifies the name of the plugin.
+
+* `enable` - (Optional, Bool) Specifies whether the plugin is enabled. Defaults to **false**.
+
+* `running` - (Optional, Bool) Specifies whether the plugin is running. Defaults to **false**.
+
+* `version` - (Optional, String) Specifies the version of the plugin.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `plugins` - The list of the RabbitMQ plugins.
+  The [plugins](#DMS_rabbitmq_plugins) structure is documented below.
+
+<a name="DMS_rabbitmq_plugins"></a>
+The `plugins` block supports:
+
+* `name` - Indicates the name of the plugin.
+
+* `enable` - Indicates whether the plugin is enabled.
+
+* `running` - Indicates whether the plugin is running.
+
+* `version` - Indicates the version of the plugin.
---
+++ huaweicloud/services/acceptance/dms/data_source_huaweicloud_dms_rabbitmq_plugins_test.go
@@ -0,0 +1,105 @@
+package dms
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDmsRabbitmqPlugins_basic(t *testing.T) {
+	name := acceptance.RandomAccResourceNameWithDash()
+	dataSourceName := "data.huaweicloud_dms_rabbitmq_plugins.test"
+	dc := acceptance.InitDataSourceCheck(dataSourceName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDmsRabbitmqPlugins_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSourceName, "plugins.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "plugins.0.name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "plugins.0.enable"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "plugins.0.running"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "plugins.0.version"),
+					resource.TestCheckOutput("name_filter_is_useful", "true"),
+					resource.TestCheckOutput("enable_filter_is_useful", "true"),
+					resource.TestCheckOutput("running_filter_is_useful", "true"),
+					resource.TestCheckOutput("version_filter_is_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testDmsRabbitmqPlugins_basic(name string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_dms_rabbitmq_plugins" "test" {
+  depends_on  = [huaweicloud_dms_rabbitmq_plugin.test]
+  instance_id = huaweicloud_dms_rabbitmq_instance.test.id
+}
+
+locals {
+  name    = huaweicloud_dms_rabbitmq_plugin.test.name
+  enable  = huaweicloud_dms_rabbitmq_plugin.test.enable
+  running = huaweicloud_dms_rabbitmq_plugin.test.running
+  version = huaweicloud_dms_rabbitmq_plugin.test.version
+}
+
+data "huaweicloud_dms_rabbitmq_plugins" "name_filter" {
+  depends_on  = [huaweicloud_dms_rabbitmq_plugin.test]
+  instance_id = huaweicloud_dms_rabbitmq_instance.test.id
+  name        = local.name
+}
+  
+output "name_filter_is_useful" {
+  value = length(data.huaweicloud_dms_rabbitmq_plugins.name_filter.plugins) > 0 && alltrue(
+  [for v in data.huaweicloud_dms_rabbitmq_plugins.name_filter.plugins[*].name : v == local.name]
+  )
+}
+
+data "huaweicloud_dms_rabbitmq_plugins" "enable_filter" {
+  depends_on  = [huaweicloud_dms_rabbitmq_plugin.test]
+  instance_id = huaweicloud_dms_rabbitmq_instance.test.id
+  enable      = local.enable
+}
+	
+output "enable_filter_is_useful" {
+  value = length(data.huaweicloud_dms_rabbitmq_plugins.enable_filter.plugins) > 0 && alltrue(
+  [for v in data.huaweicloud_dms_rabbitmq_plugins.enable_filter.plugins[*].enable : v == local.enable]
+  ) 
+}
+
+data "huaweicloud_dms_rabbitmq_plugins" "running_filter" {
+  depends_on  = [huaweicloud_dms_rabbitmq_plugin.test]
+  instance_id = huaweicloud_dms_rabbitmq_instance.test.id
+  running     = local.running
+}
+	  
+output "running_filter_is_useful" {
+  value = length(data.huaweicloud_dms_rabbitmq_plugins.running_filter.plugins) > 0 && alltrue(
+  [for v in data.huaweicloud_dms_rabbitmq_plugins.running_filter.plugins[*].running : v == local.running]
+  ) 
+}
+
+data "huaweicloud_dms_rabbitmq_plugins" "version_filter" {
+  depends_on  = [huaweicloud_dms_rabbitmq_plugin.test]
+  instance_id = huaweicloud_dms_rabbitmq_instance.test.id
+  version     = local.version
+}
+		
+output "version_filter_is_useful" {
+  value = length(data.huaweicloud_dms_rabbitmq_plugins.version_filter.plugins) > 0 && alltrue(
+  [for v in data.huaweicloud_dms_rabbitmq_plugins.version_filter.plugins[*].version : v == local.version]
+  ) 
+}
+
+`, testRabbitmqPlugin_basic(name))
+}
