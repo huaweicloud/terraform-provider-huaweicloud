9e73b382217a45dbd536a2409c77b0ad40b90971
zhangyuhong <15105657658@163.com>
2024-01-26 11:47:36 +0800

    feat(rds): add rds pg accounts data source (#4068)

---
+++ huaweicloud/services/acceptance/rds/data_source_huaweicloud_rds_pg_accounts_test.go
@@ -0,0 +1,72 @@
+package rds
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDatasourcePgAccounts_basic(t *testing.T) {
+	name := acceptance.RandomAccResourceName()
+	rName := "data.huaweicloud_rds_pg_accounts.test"
+	dbPwd := fmt.Sprintf("%s%s%d", acctest.RandString(5),
+		acctest.RandStringFromCharSet(2, "!#%^*"), acctest.RandIntRange(10, 99))
+	dc := acceptance.InitDataSourceCheck(rName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourcePgAccounts_basic(name, dbPwd),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(rName, "users.#"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.name"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.attributes.0.rolsuper"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.attributes.0.rolinherit"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.attributes.0.rolcreaterole"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.attributes.0.rolcreatedb"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.attributes.0.rolcanlogin"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.attributes.0.rolconnlimit"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.attributes.0.rolreplication"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.attributes.0.rolbypassrls"),
+					resource.TestCheckResourceAttrSet(rName, "users.1.description"),
+					resource.TestCheckOutput("user_name_filter_is_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDatasourcePgAccounts_basic(name string, dbPwd string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_rds_pg_accounts" "test" {
+  depends_on  = [huaweicloud_rds_pg_account.test]
+  instance_id = huaweicloud_rds_pg_account.test.instance_id
+}
+
+data "huaweicloud_rds_pg_accounts" "user_name_filter" {
+  depends_on  = [huaweicloud_rds_pg_account.test]
+  instance_id = huaweicloud_rds_pg_account.test.instance_id
+  user_name   = huaweicloud_rds_pg_account.test.name
+}
+
+locals {
+  user_name = huaweicloud_rds_pg_account.test.name
+}
+
+output "user_name_filter_is_useful" {
+  value = length(data.huaweicloud_rds_pg_accounts.user_name_filter.users) > 0 && alltrue(
+    [for v in data.huaweicloud_rds_pg_accounts.user_name_filter.users[*].name : v == local.user_name]
+  )
+}
+
+`, testPgAccount_basic(name, dbPwd, "test"))
+}
---
+++ huaweicloud/services/rds/data_source_huaweicloud_rds_pg_accounts.go
@@ -0,0 +1,228 @@
+package rds
+
+import (
+	"context"
+	"encoding/json"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/jmespath/go-jmespath"
+
+	"github.com/chnsz/golangsdk/pagination"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+// @API RDS GET /v3/{project_id}/instances/{instance_id}/db_user/detail
+func DataSourcePgAccounts() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourcePgAccountsRead,
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"instance_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"user_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"users": {
+				Type:     schema.TypeList,
+				Elem:     pgAccountsSchema(),
+				Computed: true,
+			},
+		},
+	}
+}
+
+func pgAccountsSchema() *schema.Resource {
+	sc := schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"attributes": {
+				Type:     schema.TypeList,
+				Elem:     attributesSchema(),
+				Computed: true,
+			},
+			"memberof": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+	return &sc
+}
+
+func attributesSchema() *schema.Resource {
+	sc := schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"rolsuper": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"rolinherit": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"rolcreaterole": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"rolcreatedb": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"rolcanlogin": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"rolconnlimit": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"rolreplication": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"rolbypassrls": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+		},
+	}
+	return &sc
+}
+
+func dataSourcePgAccountsRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var mErr *multierror.Error
+
+	var (
+		listPgAccountsHttpUrl = "v3/{project_id}/instances/{instance_id}/db_user/detail?page=1&limit=100"
+		listPgAccountsProduct = "rds"
+	)
+	listPgAccountsClient, err := cfg.NewServiceClient(listPgAccountsProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating RDS client: %s", err)
+	}
+
+	listPgAccountsPath := listPgAccountsClient.Endpoint + listPgAccountsHttpUrl
+	listPgAccountsPath = strings.ReplaceAll(listPgAccountsPath, "{project_id}", listPgAccountsClient.ProjectID)
+	listPgAccountsPath = strings.ReplaceAll(listPgAccountsPath, "{instance_id}", d.Get("instance_id").(string))
+
+	listPgAccountsResp, err := pagination.ListAllItems(
+		listPgAccountsClient,
+		"page",
+		listPgAccountsPath,
+		&pagination.QueryOpts{MarkerField: ""})
+
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "error retrieving RDS PostgreSQL accounts")
+	}
+
+	listPgAccountsRespJson, err := json.Marshal(listPgAccountsResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	var listPgAccountsRespBody interface{}
+	err = json.Unmarshal(listPgAccountsRespJson, &listPgAccountsRespBody)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	dataSourceId, err := uuid.GenerateUUID()
+	if err != nil {
+		return diag.Errorf("unable to generate ID: %s", err)
+	}
+	d.SetId(dataSourceId)
+
+	mErr = multierror.Append(
+		mErr,
+		d.Set("region", region),
+		d.Set("users", flattenPgAccountsBody(filterPgAccounts(d, listPgAccountsRespBody))),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func flattenPgAccountsBody(resp []interface{}) []interface{} {
+	if resp == nil {
+		return nil
+	}
+	rst := make([]interface{}, 0, len(resp))
+
+	for _, v := range resp {
+		rst = append(rst, map[string]interface{}{
+			"name":        utils.PathSearch("name", v, nil),
+			"attributes":  flattenAttributes(v),
+			"memberof":    utils.PathSearch("memberof", v, nil),
+			"description": utils.PathSearch("comment", v, nil),
+		})
+	}
+	return rst
+}
+
+func filterPgAccounts(d *schema.ResourceData, resp interface{}) []interface{} {
+	accountsJson := utils.PathSearch("users", resp, make([]interface{}, 0))
+	accountArray := accountsJson.([]interface{})
+	if len(accountArray) < 1 {
+		return nil
+	}
+	result := make([]interface{}, 0)
+
+	rawUserName, rawUserNameOK := d.GetOk("user_name")
+
+	for _, account := range accountArray {
+		name := utils.PathSearch("name", account, nil)
+		if rawUserNameOK && rawUserName != name {
+			continue
+		}
+		result = append(result, account)
+	}
+	return result
+}
+
+func flattenAttributes(resp interface{}) []interface{} {
+	var rst []interface{}
+	curJson, err := jmespath.Search("attributes", resp)
+	if err != nil {
+		return rst
+	}
+	if curJson == nil {
+		return nil
+	}
+
+	rst = []interface{}{
+		map[string]interface{}{
+			"rolsuper":       utils.PathSearch("rolsuper", curJson, nil),
+			"rolinherit":     utils.PathSearch("rolinherit", curJson, nil),
+			"rolcreaterole":  utils.PathSearch("rolcreaterole", curJson, nil),
+			"rolcreatedb":    utils.PathSearch("rolcreatedb", curJson, nil),
+			"rolcanlogin":    utils.PathSearch("rolcanlogin", curJson, nil),
+			"rolconnlimit":   utils.PathSearch("rolconnlimit", curJson, nil),
+			"rolreplication": utils.PathSearch("rolreplication", curJson, nil),
+			"rolbypassrls":   utils.PathSearch("rolbypassrls", curJson, nil),
+		},
+	}
+	return rst
+}
