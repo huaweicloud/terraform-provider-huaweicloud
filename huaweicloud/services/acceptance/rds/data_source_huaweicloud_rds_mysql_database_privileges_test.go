0913b2f328ebab29add678ff678c603e6638720e
Administrator <15105657658@163.com>
2024-01-26 15:09:26 +0800

    feat(RDS): add rds MySQL database privileges data source

---
+++ huaweicloud/services/acceptance/rds/data_source_huaweicloud_rds_mysql_database_privileges_test.go
@@ -0,0 +1,85 @@
+package rds
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccMysqlDatabasePrivileges_basic(t *testing.T) {
+	name := acceptance.RandomAccResourceName()
+	rName := "data.huaweicloud_rds_mysql_database_privileges.test"
+	dbPwd := fmt.Sprintf("%s%s%d", acctest.RandString(5),
+		acctest.RandStringFromCharSet(2, "!#%^*"), acctest.RandIntRange(10, 99))
+
+	dc := acceptance.InitDataSourceCheck(rName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMysqlDatabasePrivileges_basic(name, dbPwd),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(rName, "users.#"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.name"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.readonly"),
+					resource.TestCheckOutput("user_name_filter_is_useful", "true"),
+					resource.TestCheckOutput("readonly_filter_is_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testAccMysqlDatabasePrivileges_basic(name string, dbPwd string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_rds_mysql_database_privileges" "test" {
+  depends_on  = [huaweicloud_rds_mysql_database_privilege.test]
+  instance_id = huaweicloud_rds_mysql_database_privilege.test.instance_id
+  db_name     = huaweicloud_rds_mysql_database_privilege.test.db_name
+}
+
+locals {
+  user_name = huaweicloud_rds_mysql_account.test_1.name
+}
+
+data "huaweicloud_rds_mysql_database_privileges" "user_name_filter" {
+  depends_on  = [huaweicloud_rds_mysql_database_privilege.test]
+  instance_id = huaweicloud_rds_mysql_database_privilege.test.instance_id
+  db_name     = huaweicloud_rds_mysql_database_privilege.test.db_name
+  user_name   = local.user_name
+}
+
+output "user_name_filter_is_useful" {
+  value = length(data.huaweicloud_rds_mysql_database_privileges.user_name_filter.users) > 0 && alltrue(
+  [for v in data.huaweicloud_rds_mysql_database_privileges.user_name_filter.users[*].name : v == local.user_name]
+  )
+}
+
+locals {
+  readonly = tolist(huaweicloud_rds_mysql_database_privilege.test.users)[0].readonly
+}
+
+data "huaweicloud_rds_mysql_database_privileges" "readonly_filter" {
+  depends_on  = [huaweicloud_rds_mysql_database_privilege.test]
+  instance_id = huaweicloud_rds_mysql_database_privilege.test.instance_id
+  db_name     = huaweicloud_rds_mysql_database_privilege.test.db_name
+  readonly    = local.readonly
+}
+
+output "readonly_filter_is_useful" {
+  value = length(data.huaweicloud_rds_mysql_database_privileges.readonly_filter.users) > 0 && alltrue(
+  [for v in data.huaweicloud_rds_mysql_database_privileges.readonly_filter.users[*].readonly : v == local.readonly]
+)
+}
+
+`, testAccRdsDatabasePrivilege_basic(name, dbPwd))
+}
---
+++ huaweicloud/services/rds/data_source_huaweicloud_rds_mysql_database_privileges.go
@@ -0,0 +1,173 @@
+package rds
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/chnsz/golangsdk/pagination"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+// @API RDS GET /v3/{project_id}/instances/{instance_id}/database/db_user
+func DataSourceRdsMysqlDatabasePrivileges() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceRdsMysqlDatabasePrivilegesRead,
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"instance_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"db_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"user_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"readonly": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"users": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     usersSchema(),
+				Set:      resourceRdsMysqlDbPrivilegeHash,
+			},
+		},
+	}
+}
+
+func usersSchema() *schema.Resource {
+	sc := schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"readonly": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+		},
+	}
+	return &sc
+}
+
+func dataSourceRdsMysqlDatabasePrivilegesRead(_ context.Context, d *schema.ResourceData,
+	meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		listMysqlDatabasePrivilegesHttpUrl = "v3/{project_id}/instances/{instance_id}/database/db_user"
+		listMysqlDatabasePrivilegesProduct = "rds"
+	)
+	listMysqlDatabasePrivilegesClient, err := cfg.NewServiceClient(listMysqlDatabasePrivilegesProduct, region)
+	if err != nil {
+		return diag.Errorf("error creating RDS client: %s", err)
+	}
+
+	listMysqlDatabasePrivilegesPath := listMysqlDatabasePrivilegesClient.Endpoint + listMysqlDatabasePrivilegesHttpUrl
+	listMysqlDatabasePrivilegesPath = strings.ReplaceAll(listMysqlDatabasePrivilegesPath, "{project_id}",
+		listMysqlDatabasePrivilegesClient.ProjectID)
+	listMysqlDatabasePrivilegesPath = strings.ReplaceAll(listMysqlDatabasePrivilegesPath, "{instance_id}",
+		fmt.Sprintf("%v", d.Get("instance_id")))
+
+	listMysqlDatabasePrivilegesQueryParams := buildGetMysqlDatabasePrivilegesQueryParams(
+		fmt.Sprintf("%v", d.Get("db_name")))
+	listMysqlDatabasePrivilegesPath += listMysqlDatabasePrivilegesQueryParams
+
+	listMysqlDatabasePrivilegesResp, err := pagination.ListAllItems(
+		listMysqlDatabasePrivilegesClient,
+		"page",
+		listMysqlDatabasePrivilegesPath,
+		&pagination.QueryOpts{MarkerField: ""})
+
+	if err != nil {
+		return diag.Errorf("error retrieving RDS MySQL database privileges, %s", err)
+	}
+
+	listMysqlDatabasePrivilegesRespJson, err := json.Marshal(listMysqlDatabasePrivilegesResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	var listMysqlDatabasePrivilegesRespBody interface{}
+	err = json.Unmarshal(listMysqlDatabasePrivilegesRespJson, &listMysqlDatabasePrivilegesRespBody)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	dataSourceId, err := uuid.GenerateUUID()
+	if err != nil {
+		return diag.Errorf("unable to generate ID: %s", err)
+	}
+	d.SetId(dataSourceId)
+
+	mErr := multierror.Append(
+		d.Set("region", region),
+		d.Set("users", flattenListDatabasePrivilegesBody(
+			filterListDatabasePrivileges(d, listMysqlDatabasePrivilegesRespBody))),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func buildGetMysqlDatabasePrivilegesQueryParams(dbName string) string {
+	return fmt.Sprintf("?db-name=%s&page=1&limit=100", dbName)
+}
+
+func flattenListDatabasePrivilegesBody(resp []interface{}) []interface{} {
+	if resp == nil {
+		return nil
+	}
+	rst := make([]interface{}, 0, len(resp))
+
+	for _, v := range resp {
+		rst = append(rst, map[string]interface{}{
+			"name":     utils.PathSearch("name", v, nil),
+			"readonly": utils.PathSearch("readonly", v, nil),
+		})
+	}
+	return rst
+}
+
+func filterListDatabasePrivileges(d *schema.ResourceData, resp interface{}) []interface{} {
+	usersJson := utils.PathSearch("users", resp, make([]interface{}, 0))
+	userArray := usersJson.([]interface{})
+	if len(userArray) < 1 {
+		return nil
+	}
+	result := make([]interface{}, 0, len(userArray))
+
+	rawUserName, rawUserNameOK := d.GetOk("user_name")
+	rawReadonly := d.Get("readonly")
+	for _, user := range userArray {
+		name := utils.PathSearch("name", user, nil)
+		readonly := utils.PathSearch("readonly", user, nil)
+		if rawUserNameOK && rawUserName != name {
+			continue
+		}
+		if rawReadonly != readonly {
+			continue
+		}
+		result = append(result, user)
+	}
+
+	return result
+}
