968392f5291692780c20706eb39527db69d28f7d
Administrator <15105657658@163.com>
2024-02-01 15:41:12 +0800

    feat(RDS): add RDS SQLServer database privileges data source

---
+++ docs/data-sources/rds_sqlserver_database_privileges.md
@@ -0,0 +1,54 @@
+---
+subcategory: "Relational Database Service (RDS)"
+---
+
+# huaweicloud_rds_sqlserver_database_privileges
+
+Use this data source to get the list of RDS SQLServer database privileges.
+
+## Example Usage
+
+```hcl
+var "instance_id" {}
+var "db_name" {}
+
+data "huaweicloud_rds_sqlserver_database_privileges" "test" {
+  instance_id = var.instance_id
+  db_name     = var.db_name
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the data source.
+  If omitted, the provider-level region will be used.
+
+* `instance_id` - (Required, String) Specifies the ID of the RDS SQLServer instance.
+
+* `db_name` - (Required, String) Specifies the database name.
+
+* `user_name` - (Optional, String) Specifies the username of the database account.
+
+* `readonly` - (Optional, Bool) Specifies whether the database permission is **read-only**. Values option:
+  + **true**: indicates the read-only permission.
+  + **false**: indicates the read and write permission.
+
+  Defaults to **false**.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `users` - The list of the accounts that accociated with the database.
+  The [users](#RDS_sqlserver_database_privileges) structure is documented below.
+
+<a name="RDS_sqlserver_database_privileges"></a>
+The `users` block supports:
+
+* `name` - The username of the database account.
+
+* `readonly` - The read-only database permission.
---
+++ huaweicloud/services/acceptance/rds/data_source_huaweicloud_rds_sqlserver_database_privileges_test.go
@@ -0,0 +1,82 @@
+package rds
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccSQLServerDatabasePrivileges_basic(t *testing.T) {
+	name := acceptance.RandomAccResourceName()
+	rName := "data.huaweicloud_rds_sqlserver_database_privileges.test"
+
+	dc := acceptance.InitDataSourceCheck(rName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSQLServerDatabasePrivileges_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(rName, "users.#"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.name"),
+					resource.TestCheckResourceAttrSet(rName, "users.0.readonly"),
+					resource.TestCheckOutput("user_name_filter_is_useful", "true"),
+					resource.TestCheckOutput("readonly_filter_is_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testAccSQLServerDatabasePrivileges_basic(name string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_rds_sqlserver_database_privileges" "test" {
+  depends_on  = [huaweicloud_rds_sqlserver_database_privilege.test]
+  instance_id = huaweicloud_rds_sqlserver_database_privilege.test.instance_id
+  db_name     = huaweicloud_rds_sqlserver_database_privilege.test.db_name
+}
+
+locals {
+  user_name = huaweicloud_rds_sqlserver_account.account_1.name
+}
+
+data "huaweicloud_rds_sqlserver_database_privileges" "user_name_filter" {
+  depends_on  = [huaweicloud_rds_sqlserver_database_privilege.test]
+  instance_id = huaweicloud_rds_sqlserver_database_privilege.test.instance_id
+  db_name     = huaweicloud_rds_sqlserver_database_privilege.test.db_name
+  user_name   = local.user_name
+}
+
+output "user_name_filter_is_useful" {
+  value = length(data.huaweicloud_rds_sqlserver_database_privileges.user_name_filter.users) > 0 && alltrue(
+  [for v in data.huaweicloud_rds_sqlserver_database_privileges.user_name_filter.users[*].name : v == local.user_name]
+  )
+}
+
+locals {
+  readonly = tolist(huaweicloud_rds_sqlserver_database_privilege.test.users)[0].readonly
+}
+
+data "huaweicloud_rds_sqlserver_database_privileges" "readonly_filter" {
+  depends_on  = [huaweicloud_rds_sqlserver_database_privilege.test]
+  instance_id = huaweicloud_rds_sqlserver_database_privilege.test.instance_id
+  db_name     = huaweicloud_rds_sqlserver_database_privilege.test.db_name
+  readonly    = local.readonly
+}
+
+output "readonly_filter_is_useful" {
+  value = length(data.huaweicloud_rds_sqlserver_database_privileges.readonly_filter.users) > 0 && alltrue(
+  [for v in data.huaweicloud_rds_sqlserver_database_privileges.readonly_filter.users[*].readonly : v == local.readonly]
+)
+}
+
+`, testSQLServerDatabasePrivilege_basic(name))
+}
