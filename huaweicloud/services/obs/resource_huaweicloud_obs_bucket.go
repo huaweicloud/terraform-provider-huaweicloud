f0c7c9200cd22a5994fd3df2e7038a7bdabe8b8d
allium <deerhang@163.com>
2024-02-02 10:17:05 +0800

    fix(OBS): OBS bucket logging supports agency configuration(4138)
    * chore: Move the logic that generates OBS client forward
    
    * fix: OBS bucket logging supports agency configuration

--- huaweicloud/services/obs/resource_huaweicloud_obs_bucket.go
+++ huaweicloud/services/obs/resource_huaweicloud_obs_bucket.go
@@ -121,6 +121,12 @@ func ResourceObsBucket() *schema.Resource {
 							Optional: true,
 							Default:  "logs/",
 						},
+						"agency": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Computed:    true,
+							Description: "schema: Required",
+						},
 					},
 				},
 			},
@@ -467,7 +473,7 @@ func resourceObsBucketUpdate(ctx context.Context, d *schema.ResourceData, meta i
 	}
 
 	if d.HasChange("logging") {
-		if err := resourceObsBucketLoggingUpdate(obsClient, d); err != nil {
+		if err := resourceObsBucketLoggingUpdate(obsClientWithSignature, d); err != nil {
 			return diag.FromErr(err)
 		}
 	}
@@ -519,6 +525,11 @@ func resourceObsBucketRead(_ context.Context, d *schema.ResourceData, meta inter
 	obsClient, err := conf.ObjectStorageClient(region)
 	if err != nil {
 		return diag.Errorf("Error creating OBS client: %s", err)
+	}
+
+	obsClientWithSignature, err := conf.ObjectStorageClientWithSignature(region)
+	if err != nil {
+		return diag.Errorf("Error creating OBS client with signature: %s", err)
 	}
 
 	bucket := d.Id()
@@ -573,7 +584,7 @@ func resourceObsBucketRead(_ context.Context, d *schema.ResourceData, meta inter
 	}
 
 	// Read the logging configuration
-	if err := setObsBucketLogging(obsClient, d); err != nil {
+	if err := setObsBucketLogging(obsClientWithSignature, d); err != nil {
 		return diag.FromErr(err)
 	}
 
@@ -601,10 +612,7 @@ func resourceObsBucketRead(_ context.Context, d *schema.ResourceData, meta inter
 	policyClient := obsClient
 	format := d.Get("policy_format").(string)
 	if format == "obs" {
-		policyClient, err = conf.ObjectStorageClientWithSignature(region)
-		if err != nil {
-			return diag.Errorf("Error creating OBS policy client: %s", err)
-		}
+		policyClient = obsClientWithSignature
 	}
 	if err := setObsBucketPolicy(policyClient, d); err != nil {
 		return diag.FromErr(err)
@@ -822,6 +830,10 @@ func resourceObsBucketLoggingUpdate(obsClient *obs.ObsClient, d *schema.Resource
 
 		if val := c["target_prefix"].(string); val != "" {
 			loggingStatus.TargetPrefix = val
+		}
+
+		if val := c["agency"].(string); val != "" {
+			loggingStatus.Agency = val
 		}
 	}
 	log.Printf("[DEBUG] set logging of OBS bucket %s: %#v", bucket, loggingStatus)
@@ -1365,6 +1377,9 @@ func setObsBucketLogging(obsClient *obs.ObsClient, d *schema.ResourceData) error
 		logging["target_bucket"] = output.TargetBucket
 		if output.TargetPrefix != "" {
 			logging["target_prefix"] = output.TargetPrefix
+		}
+		if output.Agency != "" {
+			logging["agency"] = output.Agency
 		}
 		lcList = append(lcList, logging)
 	}
