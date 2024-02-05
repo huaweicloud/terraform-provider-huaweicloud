509c6b680e811d350fc798d990fa7c587f3e5118
346010762 <346010762@qq.com>
2024-02-05 16:28:05 +0800

    feat(DMS): add resource dds lts log

---
+++ docs/resources/dds_lts_log.md
@@ -0,0 +1,63 @@
+---
+subcategory: "Document Database Service (DDS)"
+---
+
+# huaweicloud_dds_lts_log
+
+Manages a DDS LTS log resource within HuaweiCloud.
+
+## Example Usage
+
+```hcl
+variable "instance_id" {}
+variable "lts_group_id" {}
+variable "lts_stream_id" {}
+
+resource "huaweicloud_dds_lts_log" "test" {
+  instance_id   = var.instance_id
+  log_type      = "audit_log"
+  lts_group_id  = var.lts_group_id
+  lts_stream_id = var.lts_stream_id
+}
+
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region in which to create the resource.
+  If omitted, the provider-level region will be used. Changing this creates a new resource.
+
+* `instance_id` - (Required, String, ForceNew) Specifies the ID of the DDS instance.
+  Changing this creates a new resource.
+
+* `log_type` - (Required, String, ForceNew) Specifies the type of the LTS log. The value can be **audit_log**.
+  Changing this creates a new resource.
+
+* `lts_group_id` - (Required, String, ForceNew) Specifies the ID of the LTS log group.
+  Changing this creates a new resource.
+
+* `lts_stream_id` - (Required, String, ForceNew) Specifies the ID of the LTS log stream.
+  Changing this creates a new resource.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attribute is exported:
+
+* `id` - The resource ID. The value is DDS instance ID.
+
+## Timeouts
+
+This resource provides the following timeouts configuration options:
+
+* `create` - Default is 60 minutes.
+* `delete` - Default is 60 minutes.
+
+## Import
+
+The DDS LTS log config can be imported using DDS instance ID, e.g.
+
+```bash
+$ terraform import huaweicloud_dds_lts_log.test <instance_id>
+```
---
+++ huaweicloud/services/acceptance/dds/resource_huaweicloud_dds_lts_log_test.go
@@ -0,0 +1,119 @@
+package dds
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/chnsz/golangsdk/openstack/dds/v3/instances"
+	"github.com/chnsz/golangsdk/pagination"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func getDdsLtsLogResourceFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	var (
+		getDdsLtsLogHttpUrl = "v3/{project_id}/instances/logs/lts-configs"
+		getDdsLtsLogProduct = "dds"
+	)
+
+	getDdsLtsLogClient, err := cfg.NewServiceClient(getDdsLtsLogProduct, acceptance.HW_REGION_NAME)
+	if err != nil {
+		return nil, fmt.Errorf("error creating DDS client: %s", err)
+	}
+
+	getDdsLtsLogPath := getDdsLtsLogClient.Endpoint + getDdsLtsLogHttpUrl
+	getDdsLtsLogPath = strings.ReplaceAll(getDdsLtsLogPath, "{project_id}", getDdsLtsLogClient.ProjectID)
+
+	getDdsLtsLogResp, err := pagination.ListAllItems(
+		getDdsLtsLogClient,
+		"offset",
+		getDdsLtsLogPath,
+		&pagination.QueryOpts{MarkerField: ""})
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving DDS LTS configs: %s", err)
+	}
+
+	getDdsLtsLogRespJson, err := json.Marshal(getDdsLtsLogResp)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling DDS LTS configs: %s", err)
+	}
+
+	var getDdsLtsLogRespBody interface{}
+	err = json.Unmarshal(getDdsLtsLogRespJson, &getDdsLtsLogRespBody)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling DDS LTS configs: %s", err)
+	}
+
+	jsonPath := fmt.Sprintf("instance_lts_configs[?instance.id=='%s']|[0].lts_configs|[0]", state.Primary.ID)
+	ltsConfig := utils.PathSearch(jsonPath, getDdsLtsLogRespBody, nil)
+	if !utils.PathSearch("enabled", ltsConfig, false).(bool) {
+		return nil, fmt.Errorf("error retrieving DDS LTS config: the LTS config can not be found")
+	}
+
+	return ltsConfig, nil
+}
+
+func TestAccDdsLtsLog_basic(t *testing.T) {
+	var instance instances.InstanceResponse
+	rName := acceptance.RandomAccResourceName()
+	resourceName := "huaweicloud_dds_lts_log.test"
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&instance,
+		getDdsLtsLogResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDdsLtsLog_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttrPair(resourceName, "instance_id", "huaweicloud_dds_instance.instance", "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "lts_group_id", "huaweicloud_lts_group.test", "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "lts_stream_id", "huaweicloud_lts_stream.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "log_type", "audit_log"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccDdsLtsLog_basic(rName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_lts_group" "test" {
+  group_name  = "%[2]s"
+  ttl_in_days = 1
+}
+
+resource "huaweicloud_lts_stream" "test" {
+  depends_on  = [huaweicloud_lts_group.test]
+  group_id    = huaweicloud_lts_group.test.id
+  stream_name = "%[2]s"
+}
+
+resource "huaweicloud_dds_lts_log" "test" {
+  instance_id   = huaweicloud_dds_instance.instance.id
+  log_type      = "audit_log"
+  lts_group_id  = huaweicloud_lts_group.test.id
+  lts_stream_id = huaweicloud_lts_stream.test.id
+}`, testAccDDSInstanceV3Config_basic(rName, 8800), rName)
+}
