276d49b90a1d01942e48f1888e8061aae1bb7dbe
luoping-12345 <luoping-12345@outlook.com>
2024-02-07 11:37:28 +0800

    feat(VPN): VPN connection resource support ha_role field(4197)

--- huaweicloud/services/acceptance/vpn/resource_huaweicloud_vpn_connection_test.go
+++ huaweicloud/services/acceptance/vpn/resource_huaweicloud_vpn_connection_test.go
@@ -156,6 +156,52 @@ func TestAccConnection_policy(t *testing.T) {
 	})
 }
 
+func TestAccConnection_haRole(t *testing.T) {
+	var obj interface{}
+
+	name := acceptance.RandomAccResourceName()
+	rName := "huaweicloud_vpn_connection.test"
+	ipAddress := "172.16.1.3"
+
+	rc := acceptance.InitResourceCheck(
+		rName,
+		&obj,
+		getConnectionResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testConnection_haRole(name, ipAddress),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(rName, "name", name),
+					resource.TestCheckResourceAttr(rName, "vpn_type", "POLICY"),
+					resource.TestCheckResourceAttr(rName, "policy_rules.0.source", "192.168.11.0/24"),
+					resource.TestCheckResourceAttr(rName, "policy_rules.0.destination.0", "192.168.12.0/24"),
+					resource.TestCheckResourceAttr(rName, "policy_rules.0.destination.1", "192.168.13.0/24"),
+					resource.TestCheckResourceAttr(rName, "ikepolicy.0.authentication_algorithm", "sha2-512"),
+					resource.TestCheckResourceAttr(rName, "ikepolicy.0.encryption_algorithm", "aes-256"),
+					resource.TestCheckResourceAttr(rName, "ikepolicy.0.lifetime_seconds", "172800"),
+					resource.TestCheckResourceAttr(rName, "ipsecpolicy.0.authentication_algorithm", "sha2-512"),
+					resource.TestCheckResourceAttr(rName, "ipsecpolicy.0.encryption_algorithm", "aes-256"),
+					resource.TestCheckResourceAttr(rName, "ipsecpolicy.0.lifetime_seconds", "7200"),
+					resource.TestCheckResourceAttrPair(rName, "gateway_id",
+						"huaweicloud_vpn_gateway.test", "id"),
+					resource.TestCheckResourceAttrPair(rName, "gateway_ip",
+						"huaweicloud_vpn_gateway.test", "eip2.0.id"),
+					resource.TestCheckResourceAttrPair(rName, "customer_gateway_id",
+						"huaweicloud_vpn_customer_gateway.test", "id"),
+					resource.TestCheckResourceAttr(rName, "ha_role", "slave"),
+				),
+			},
+		},
+	})
+}
+
 func testConnection_basic(name, ipAddress string) string {
 	return fmt.Sprintf(`
 %s
@@ -245,4 +291,40 @@ resource "huaweicloud_vpn_connection" "test" {
   }
 }
 `, testGateway_basic(name), testCustomerGateway_basic(name, ipAddress), name)
+}
+
+func testConnection_haRole(name, ipAddress string) string {
+	return fmt.Sprintf(`
+%s
+
+%s
+
+resource "huaweicloud_vpn_connection" "test" {
+  name                = "%s"
+  gateway_id          = huaweicloud_vpn_gateway.test.id
+  gateway_ip          = huaweicloud_vpn_gateway.test.eip2[0].id
+  customer_gateway_id = huaweicloud_vpn_customer_gateway.test.id
+  peer_subnets        = ["192.168.55.0/24"]
+  vpn_type            = "policy"
+  psk                 = "Test@123"
+  ha_role             = "slave"
+
+  policy_rules {
+    source      = "192.168.11.0/24"
+    destination = ["192.168.12.0/24", "192.168.13.0/24"]
+  }
+
+  ikepolicy {
+    authentication_algorithm = "sha2-512"
+    encryption_algorithm     = "aes-256"
+    lifetime_seconds         = 172800
+  }
+
+  ipsecpolicy {
+    authentication_algorithm = "sha2-512"
+    encryption_algorithm     = "aes-256"
+    lifetime_seconds         = 7200
+  }
+}
+`, testGateway_activeStandbyHAMode(name), testCustomerGateway_basic(name, ipAddress), name)
 }
