4fa82e41a09d82986bad0ee631282f7c4831155e
luoping-12345 <luoping-12345@outlook.com>
2024-02-27 14:46:41 +0800

    feat(CSS): add CSS logstash configuration resource

---
+++ docs/resources/css_logstash_configuration.md
@@ -0,0 +1,119 @@
+---
+subcategory: "Cloud Search Service (CSS)"
+---
+
+# huaweicloud_css_logstash_configuration
+
+Manages a CSS logstah configuration resource within HuaweiCloud.
+
+## Example Usage
+
+variable "cluster_id" {}
+variable "logstash_conf_name" {}
+variable "conf_content" {}
+
+```hcl
+resource "huaweicloud_css_logstash_configuration" "test"  {
+  cluster_id   = var.cluster_id
+  name         = var.logstash_conf_name
+  conf_content = var.conf_content
+
+  setting {
+    queue_type = "memory"
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region in which to create the resource.
+  If omitted, the provider-level region will be used.
+  Changing this creates a new resource.
+
+* `cluster_id` - (Required, String, ForceNew) Specifies ID of the CSS logstash cluster.
+  Changing this creates a new resource.
+
+* `name` - (Required, String, ForceNew) Specifies the configuration file name of the CSS logstash cluster.
+  Changing this creates a new resource.
+
+* `conf_content` - (Required, String) Specifies the configuration file content of the CSS logstash cluster.
+
+* `setting` - (Required, List) Specifies configuration file setting information of the CSS logstash cluster.
+  The [setting](#Css_logstash_configuration_setting) structure is documented below.
+
+* `sensitive_words` - (Optional, List) Specifies the input list of sensitive strings that need to be hidden.
+  After configuring the hidden string list, all strings in the list will be hidden as `***` in the returned
+  configuration content (the list supports a maximum of **20** items, and the maximum length of a single string
+  is 512 bytes).
+  -> **NOTE:** When this field is used, the configuration file content will also trigger update changes when the
+    update operation is performed again, restoring the hidden content. If you import resources, you need to manually
+    restore sensitive characters hidden in the configuration content.
+
+<a name="Css_logstash_configuration_setting"></a>
+The `setting` block supports:
+
+* `workers` - (Optional, Int) Specifies the number of worker threads in the **Filters** + **Outputs** stage of
+  the execution pipeline. The default value is the number of CPU cores.
+
+* `batch_size` - (Optional, Int) Specifies the maximum number of events a single worker thread will collect
+  from inputs before attempting to execute its **Filters** and **Outputs**. Larger values ​​are generally more
+  efficient but increase memory overhead. Default is **125**.
+
+* `batch_delay_ms` - (Optional, Int) Specifies the minimum time in the unit of milliseconds for each event to be
+  waited for by pipeline scheduling.
+
+* `queue_type` - (Required, String) Specifies internal queue model for event buffering.
+  + **memory:** a traditional memory-based queue.
+  + **persisted:** a disk-based ACKed persistence queue.
+
+* `queue_check_point_writes` - (Optional, Int) Specifies the maximum number of events to be written before forcing
+  a checkpoint when using a persistent queue, default is **1024**.
+
+* `queue_max_bytes_mb` - (Optional, Int) Specifies the total capacity of the persistent queue in megabytes (MB) when
+  using a persistent queue. Make sure the disk is larger than this value. The default value is **1024**.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID.
+
+* `status` - The configuration file content check status.
+
+* `updated_at` - The update time of configuration file.
+
+## Timeouts
+
+This resource provides the following timeouts configuration options:
+
+* `create` - Default is 10 minutes.
+* `update` - Default is 10 minutes.
+
+## Import
+
+The CSS logstash configuration can be imported using `cluster_id` and `name` separated by a slash, e.g.
+
+```bash
+$ terraform import huaweicloud_css_logstash_configuration.test <cluster_id>/<name>
+```
+
+Note that the imported state may not be identical to your resource definition, due to some attributes missing from the
+API response, security or some other reason.
+The missing attributes include: `sensitive_words`.
+It is generally recommended running `terraform plan` after importing a CSS logstash configuration.
+You can then decide if changes should be applied to the CSS logstash configuration, or the resource definition should
+be updated to align with the CSS logstash configuration. Also you can ignore changes as below.
+
+```hcl
+resource "huaweicloud_css_logstash_configuration" "test" {
+    ...
+
+  lifecycle {
+    ignore_changes = [
+      sensitive_words,
+    ]
+  }
+}
+```
---
+++ huaweicloud/services/css/resource_huaweicloud_css_logstash_configuration.go
@@ -0,0 +1,292 @@
+package css
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	cssv1 "github.com/huaweicloud/huaweicloud-sdk-go-v3/services/css/v1"
+	"github.com/huaweicloud/huaweicloud-sdk-go-v3/services/css/v1/model"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+// @API CSS POST /v1.0/{project_id}/clusters/{cluster_id}/lgsconf/submit
+// @API CSS GET /v1.0/{project_id}/clusters/{cluster_id}/lgsconf/confdetail
+// @API CSS POST /v1.0/{project_id}/clusters/{cluster_id}/lgsconf/update
+// @API CSS DELETE /v1.0/{project_id}/clusters/{cluster_id}/lgsconf/delete
+func ResourceLogstashConfiguration() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceLogstashConfigurationCreate,
+		ReadContext:   resourceLogstashConfigurationRead,
+		UpdateContext: resourceLogstashConfigurationUpdate,
+		DeleteContext: resourceLogstashConfigurationDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceLogstashConfigurationImportState,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(10 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"cluster_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"conf_content": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"setting": {
+				Type:     schema.TypeList,
+				Required: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"workers": {
+							Type:     schema.TypeInt,
+							Optional: true,
+						},
+						"batch_size": {
+							Type:     schema.TypeInt,
+							Optional: true,
+						},
+						"batch_delay_ms": {
+							Type:     schema.TypeInt,
+							Optional: true,
+						},
+						"queue_type": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"queue_check_point_writes": {
+							Type:     schema.TypeInt,
+							Optional: true,
+						},
+						"queue_max_bytes_mb": {
+							Type:     schema.TypeInt,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"sensitive_words": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"updated_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceLogstashConfigurationCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conf := meta.(*config.Config)
+	region := conf.GetRegion(d)
+	cssV1Client, err := conf.HcCssV1Client(region)
+	if err != nil {
+		return diag.Errorf("error creating CSS V1 client: %s", err)
+	}
+
+	createCnfOpts := buildCreateLogstashCnfParameters(d)
+	clusterId := d.Get("cluster_id").(string)
+
+	_, err = cssV1Client.CreateCnf(&model.CreateCnfRequest{
+		ClusterId: clusterId,
+		Body:      createCnfOpts,
+	})
+	if err != nil {
+		return diag.Errorf("error creating CSS logstash cluster configuration: %s", err)
+	}
+
+	d.SetId(fmt.Sprintf("%s/%s", clusterId, d.Get("name").(string)))
+
+	checkErr := configFileStatusCheck(ctx, d, cssV1Client)
+	if checkErr != nil {
+		return diag.FromErr(checkErr)
+	}
+
+	return resourceLogstashConfigurationRead(ctx, d, meta)
+}
+
+func buildCreateLogstashCnfParameters(d *schema.ResourceData) *model.CreateCnfReq {
+	createOpts := &model.CreateCnfReq{
+		Name:        d.Get("name").(string),
+		ConfContent: d.Get("conf_content").(string),
+		Setting: &model.Setting{
+			QueueType:             d.Get("setting.0.queue_type").(string),
+			Workers:               utils.Int32IgnoreEmpty(int32(d.Get("setting.0.workers").(int))),
+			BatchSize:             utils.Int32IgnoreEmpty(int32(d.Get("setting.0.batch_size").(int))),
+			BatchDelayMs:          utils.Int32IgnoreEmpty(int32(d.Get("setting.0.batch_delay_ms").(int))),
+			QueueCheckPointWrites: utils.Int32IgnoreEmpty(int32(d.Get("setting.0.queue_check_point_writes").(int))),
+			QueueMaxBytesMb:       utils.Int32IgnoreEmpty(int32(d.Get("setting.0.queue_max_bytes_mb").(int))),
+		},
+	}
+
+	sensitiveWordsRaw := d.Get("sensitive_words").([]interface{})
+	if len(sensitiveWordsRaw) > 0 {
+		createOpts.SensitiveWords = utils.ExpandToStringListPointer(sensitiveWordsRaw)
+	}
+
+	return createOpts
+}
+
+func resourceLogstashConfigurationRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conf := meta.(*config.Config)
+	region := conf.GetRegion(d)
+	cssV1Client, err := conf.HcCssV1Client(region)
+	if err != nil {
+		return diag.Errorf("error creating CSS V1 client: %s", err)
+	}
+
+	var mErr *multierror.Error
+
+	logstashConfDetail, err := cssV1Client.ShowGetConfDetail(&model.ShowGetConfDetailRequest{
+		ClusterId: d.Get("cluster_id").(string),
+		Name:      d.Get("name").(string),
+	})
+	if err != nil {
+		common.CheckDeletedDiag(d, err, "CSS logstash cluster configuration")
+	}
+
+	mErr = multierror.Append(
+		mErr,
+		d.Set("region", region),
+		d.Set("name", logstashConfDetail.Name),
+		d.Set("conf_content", logstashConfDetail.ConfContent),
+		d.Set("setting", flattenLogstashConfSetting(logstashConfDetail)),
+		d.Set("status", logstashConfDetail.Status),
+		d.Set("updated_at", logstashConfDetail.UpdateAt),
+	)
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func flattenLogstashConfSetting(logstashConfDetail *model.ShowGetConfDetailResponse) []map[string]interface{} {
+	result := make([]map[string]interface{}, 0)
+	result = append(result, map[string]interface{}{
+		"workers":                  logstashConfDetail.Setting.Workers,
+		"batch_size":               logstashConfDetail.Setting.BatchSize,
+		"batch_delay_ms":           logstashConfDetail.Setting.BatchDelayMs,
+		"queue_type":               logstashConfDetail.Setting.QueueType,
+		"queue_check_point_writes": logstashConfDetail.Setting.QueueCheckPointWrites,
+		"queue_max_bytes_mb":       logstashConfDetail.Setting.QueueMaxBytesMb,
+	})
+
+	return result
+}
+
+func resourceLogstashConfigurationUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conf := meta.(*config.Config)
+	region := conf.GetRegion(d)
+	cssV1Client, err := conf.HcCssV1Client(region)
+	if err != nil {
+		return diag.Errorf("error creating CSS V1 client: %s", err)
+	}
+
+	updateCnfOpts := buildCreateLogstashCnfParameters(d)
+
+	_, err = cssV1Client.UpdateCnf(&model.UpdateCnfRequest{
+		ClusterId: d.Get("cluster_id").(string),
+		Body:      updateCnfOpts,
+	})
+	if err != nil {
+		diag.Errorf("error updating CSS logstash cluster configuration: %s", err)
+	}
+
+	checkErr := configFileStatusCheck(ctx, d, cssV1Client)
+	if checkErr != nil {
+		return diag.FromErr(checkErr)
+	}
+
+	return resourceLogstashConfigurationRead(ctx, d, meta)
+}
+
+func resourceLogstashConfigurationDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conf := meta.(*config.Config)
+	region := conf.GetRegion(d)
+	cssV1Client, err := conf.HcCssV1Client(region)
+	if err != nil {
+		return diag.Errorf("error creating CSS V1 client: %s", err)
+	}
+
+	_, err = cssV1Client.DeleteConf(&model.DeleteConfRequest{
+		ClusterId: d.Get("cluster_id").(string),
+		Body: &model.DeleteConfReq{
+			Name: d.Get("name").(string),
+		},
+	})
+	if err != nil {
+		return diag.Errorf("error deleting CSS logstash cluster configuration: %s", err)
+	}
+
+	return nil
+}
+
+func configFileStatusCheck(ctx context.Context, d *schema.ResourceData, cssV1Client *cssv1.CssClient) error {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{"checking"},
+		Target:  []string{"available", "unavailable"},
+		Refresh: func() (interface{}, string, error) {
+			resp, err := cssV1Client.ShowGetConfDetail(&model.ShowGetConfDetailRequest{
+				ClusterId: d.Get("cluster_id").(string),
+				Name:      d.Get("name").(string),
+			})
+			if err != nil {
+				return nil, "failed", err
+			}
+			return resp, *resp.Status, err
+		},
+		Timeout:      d.Timeout(schema.TimeoutCreate),
+		Delay:        10 * time.Second,
+		PollInterval: 10 * time.Second,
+	}
+	_, err := stateConf.WaitForStateContext(ctx)
+	if err != nil {
+		return fmt.Errorf("error waiting for the CSS logstash cluster config to check completed: %s", err)
+	}
+	return nil
+}
+
+func resourceLogstashConfigurationImportState(_ context.Context, d *schema.ResourceData, _ interface{}) ([]*schema.ResourceData, error) {
+	parts := strings.Split(d.Id(), "/")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid format of import ID, must be <cluster_id>/<name>")
+	}
+
+	mErr := multierror.Append(nil,
+		d.Set("cluster_id", parts[0]),
+		d.Set("name", parts[1]),
+	)
+	return []*schema.ResourceData{d}, mErr.ErrorOrNil()
+}
