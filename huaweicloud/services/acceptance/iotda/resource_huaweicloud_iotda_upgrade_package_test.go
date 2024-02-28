3be6f4f743cba0ce93c5814c604c0e51e99e7cde
jinyangyang222 <jinyangyang222@outlook.com>
2024-02-28 11:58:39 +0800

    add OTA upgrade package resource support

---
+++ huaweicloud/services/acceptance/iotda/resource_huaweicloud_iotda_upgrade_package_test.go
@@ -0,0 +1,130 @@
+package iotda
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/huaweicloud/huaweicloud-sdk-go-v3/services/iotda/v5/model"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func getUpgradePackageResourceFunc(conf *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	client, err := conf.HcIoTdaV5Client(acceptance.HW_REGION_NAME)
+	if err != nil {
+		return nil, fmt.Errorf("error creating IoTDA v5 client: %s", err)
+	}
+
+	getOpts := &model.ShowOtaPackageRequest{
+		PackageId: state.Primary.ID,
+	}
+
+	resp, err := client.ShowOtaPackage(getOpts)
+	if err != nil {
+		return nil, fmt.Errorf("error querying IoTDA OTA upgrade package")
+	}
+
+	return resp, nil
+}
+
+func TestAccUpgradePackage_basic(t *testing.T) {
+	var (
+		obj          interface{}
+		resourceName = "huaweicloud_iotda_upgrade_package.test"
+		rName        = acceptance.RandomAccResourceName()
+	)
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&obj,
+		getUpgradePackageResourceFunc,
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testUpgradePackage_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttrPair(resourceName, "space_id", "huaweicloud_iotda_space.test", "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "product_id", "huaweicloud_iotda_product.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "type", "softwarePackage"),
+					resource.TestCheckResourceAttr(resourceName, "version", "v1.0"),
+					resource.TestCheckResourceAttr(resourceName, "file_location.0.obs_location.0.region", acceptance.HW_REGION_NAME),
+					resource.TestCheckResourceAttrPair(resourceName, "file_location.0.obs_location.0.bucket_name",
+						"huaweicloud_obs_bucket_object.test", "bucket"),
+					resource.TestCheckResourceAttrPair(resourceName, "file_location.0.obs_location.0.object_key",
+						"huaweicloud_obs_bucket_object.test", "key"),
+					resource.TestCheckResourceAttr(resourceName, "support_source_versions.#", "3"),
+					resource.TestCheckResourceAttr(resourceName, "description", "description test"),
+					resource.TestCheckResourceAttr(resourceName, "custom_info", "custom_info test"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testUpgradePackageWithObsBucket_base() string {
+	randInt := acctest.RandInt()
+
+	return fmt.Sprintf(`
+resource "huaweicloud_obs_bucket" "test" {
+  bucket = "tf-acc-test-bucket-%d"
+}
+
+resource "huaweicloud_obs_bucket_object" "test" {
+  bucket  = huaweicloud_obs_bucket.test.bucket
+  key     = "test-key.img"
+  content = "some_bucket_content"
+}
+`, randInt)
+}
+
+func testUpgradePackage_basic(name string) string {
+	obsBucketBasic := testUpgradePackageWithObsBucket_base()
+	productbasic := testProduct_basic(name)
+
+	return fmt.Sprintf(`
+%[1]s
+%[2]s
+
+resource "huaweicloud_iotda_upgrade_package" "test" {
+  space_id   = huaweicloud_iotda_space.test.id
+  type       = "softwarePackage"
+  product_id = huaweicloud_iotda_product.test.id
+  version    = "v1.0"
+
+  file_location {
+    obs_location {
+      region      = "%[3]s"
+      bucket_name = huaweicloud_obs_bucket_object.test.bucket
+      object_key  = huaweicloud_obs_bucket_object.test.key
+    }
+  }
+
+  support_source_versions = [
+    "v1.0",
+    "v2.0",
+    "v3.0",
+  ]
+
+  description = "description test"
+  custom_info = "custom_info test"
+}
+`, productbasic, obsBucketBasic, acceptance.HW_REGION_NAME)
+}
---
+++ huaweicloud/services/iotda/resource_huaweicloud_iotda_upgrade_package.go
@@ -0,0 +1,260 @@
+package iotda
+
+import (
+	"context"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/huaweicloud/huaweicloud-sdk-go-v3/services/iotda/v5/model"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+// @API IoTDA POST /v5/iot/{project_id}/ota-upgrades/packages
+// @API IoTDA GET /v5/iot/{project_id}/ota-upgrades/packages/{package_id}
+// @API IoTDA DELETE /v5/iot/{project_id}/ota-upgrades/packages/{package_id}
+func ResourceUpgradePackage() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceUpgradePackageCreate,
+		ReadContext:   resourceUpgradePackageRead,
+		DeleteContext: resourceUpgradePackageDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"space_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"type": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"product_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"version": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"file_location": {
+				Type:     schema.TypeList,
+				Required: true,
+				ForceNew: true,
+				MinItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"obs_location": {
+							Type:     schema.TypeList,
+							Optional: true,
+							ForceNew: true,
+							MaxItems: 1,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"region": {
+										Type:     schema.TypeString,
+										Required: true,
+										ForceNew: true,
+									},
+									"bucket_name": {
+										Type:     schema.TypeString,
+										Required: true,
+										ForceNew: true,
+									},
+									"object_key": {
+										Type:     schema.TypeString,
+										Required: true,
+										ForceNew: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"support_source_versions": {
+				Type:     schema.TypeList,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"custom_info": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"created_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func buildObsLocationParam(raw interface{}) *model.ObsLocation {
+	obsLocationMap := raw.([]interface{})[0].(map[string]interface{})
+	obsLocationParam := model.ObsLocation{
+		RegionName: obsLocationMap["region"].(string),
+		BucketName: obsLocationMap["bucket_name"].(string),
+		ObjectKey:  obsLocationMap["object_key"].(string),
+	}
+
+	return &obsLocationParam
+}
+
+func buildFileLocationParam(raw []interface{}) *model.FileLocation {
+	if raw[0] == nil {
+		return nil
+	}
+
+	obsLocationParam := buildObsLocationParam(raw[0].(map[string]interface{})["obs_location"])
+	fileLocationParam := model.FileLocation{
+		ObsLocation: obsLocationParam,
+	}
+
+	return &fileLocationParam
+}
+
+func buildUpgradePackageCreateParams(d *schema.ResourceData) *model.CreateOtaPackageRequest {
+	req := model.CreateOtaPackageRequest{
+		Body: &model.CreateOtaPackage{
+			AppId:                 d.Get("space_id").(string),
+			PackageType:           d.Get("type").(string),
+			ProductId:             d.Get("product_id").(string),
+			Version:               d.Get("version").(string),
+			FileLocation:          buildFileLocationParam(d.Get("file_location").([]interface{})),
+			SupportSourceVersions: utils.ExpandToStringListPointer(d.Get("support_source_versions").([]interface{})),
+			Description:           utils.StringIgnoreEmpty(d.Get("description").(string)),
+			CustomInfo:            utils.StringIgnoreEmpty(d.Get("custom_info").(string)),
+		},
+	}
+
+	return &req
+}
+
+func resourceUpgradePackageCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	client, err := cfg.HcIoTdaV5Client(region)
+	if err != nil {
+		return diag.Errorf("error creating IoTDA v5 client: %s", err)
+	}
+
+	createOpts := buildUpgradePackageCreateParams(d)
+	resp, err := client.CreateOtaPackage(createOpts)
+	if err != nil {
+		return diag.Errorf("error creating IoTDA OTA upgrade package: %s", err)
+	}
+
+	if resp == nil || resp.PackageId == nil {
+		return diag.Errorf("error creating IoTDA OTA upgrade package: ID is not found in API response")
+	}
+
+	d.SetId(*resp.PackageId)
+
+	return resourceUpgradePackageRead(ctx, d, meta)
+}
+
+func resourceUpgradePackageRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	client, err := cfg.HcIoTdaV5Client(region)
+	if err != nil {
+		return diag.Errorf("error creating IoTDA v5 client: %s", err)
+	}
+
+	getOpts := &model.ShowOtaPackageRequest{
+		PackageId: d.Id(),
+	}
+	resp, err := client.ShowOtaPackage(getOpts)
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "error querying IoTDA OTA upgrade package")
+	}
+
+	mErr := multierror.Append(nil,
+		d.Set("region", region),
+		d.Set("space_id", resp.AppId),
+		d.Set("type", resp.PackageType),
+		d.Set("product_id", resp.ProductId),
+		d.Set("version", resp.Version),
+		d.Set("file_location", flattenFileLocation(resp.FileLocation)),
+		d.Set("support_source_versions", resp.SupportSourceVersions),
+		d.Set("description", resp.Description),
+		d.Set("custom_info", resp.CustomInfo),
+		d.Set("created_at", resp.CreateTime),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func flattenFileLocation(resp *model.FileLocation) []interface{} {
+	if resp == nil {
+		return nil
+	}
+
+	obsLocation := flattenObsLocation(resp.ObsLocation)
+	fileLocation := []interface{}{
+		map[string]interface{}{
+			"obs_location": obsLocation,
+		},
+	}
+
+	return fileLocation
+}
+
+func flattenObsLocation(resp *model.ObsLocation) []interface{} {
+	if resp == nil {
+		return nil
+	}
+
+	obsLocation := []interface{}{
+		map[string]interface{}{
+			"region":      resp.RegionName,
+			"bucket_name": resp.BucketName,
+			"object_key":  resp.ObjectKey,
+		},
+	}
+
+	return obsLocation
+}
+
+func resourceUpgradePackageDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	client, err := cfg.HcIoTdaV5Client(region)
+	if err != nil {
+		return diag.Errorf("error creating IoTDA v5 client: %s", err)
+	}
+
+	deleteOpts := model.DeleteOtaPackageRequest{
+		PackageId: d.Id(),
+	}
+	_, err = client.DeleteOtaPackage(&deleteOpts)
+	if err != nil {
+		return diag.Errorf("error deleting IoTDA OTA upgrade package: %s", err)
+	}
+
+	return nil
+}
