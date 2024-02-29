736b08a2cc1b619c3bfe2f648259a346d7e8d094
houpeng80 <hp_monkey@163.com>
2024-02-29 18:26:48 +0800

    chore(SMS): fix sms service api comments

--- huaweicloud/services/sms/resource_huaweicloud_sms_server_template.go
+++ huaweicloud/services/sms/resource_huaweicloud_sms_server_template.go
@@ -26,12 +26,12 @@ var AutoCreate string = "autoCreate"
 
 // ResourceServerTemplate is the impl of huaweicloud_sms_server_template
 // @API SMS POST /v3/vm/templates
-// @API SMS DELETE /v3/vm/templates/{id}
 // @API SMS GET /v3/vm/templates/{id}
 // @API SMS PUT /v3/vm/templates/{id}
-// @API VPC GET /v1/{project_id}/security-groups/{securityGroupId}
-// @API VPC GET /v1/{project_id}/subnets/{id}
-// @API VPC GET /v1/{project_id}/vpcs/{id}
+// @API SMS DELETE /v3/vm/templates/{id}
+// @API VPC GET /v1/{project_id}/security-groups/{security_group_id}
+// @API VPC GET /v1/{project_id}/subnets/{subnet_id}
+// @API VPC GET /v1/{project_id}/vpcs/{vpc_id}
 func ResourceServerTemplate() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceServerTemplateCreate,
