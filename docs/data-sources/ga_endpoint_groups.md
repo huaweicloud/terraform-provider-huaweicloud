62ebf44c2d6657c26badab14d3c6ef0e7cbdb2e1
ruwenqiang123 <ruwenqiang123@outlook.com>
2024-03-01 11:36:36 +0800

    feat(ga): add a new datasource to get the list of the endpoint groups (#4306)

---
+++ docs/data-sources/ga_endpoint_groups.md
@@ -0,0 +1,62 @@
+---
+subcategory: "Global Accelerator (GA)"
+---
+
+# huaweicloud_ga_endpoint_groups
+
+Use this data source to get the list of endpoint groups.
+
+## Example Usage
+
+```hcl
+variable "endpoint_group_name" {}
+
+data "huaweicloud_ga_endpoint_groups" "test" {
+  name = var.endpoint_group_name
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `endpoint_group_id` - (Optional, String) Specifies the ID of the endpoint group.
+
+* `name` - (Optional, String) Specifies the name of the endpoint group.
+
+* `status` - (Optional, String) Specifies the status of the endpoint group.
+  The valid values are as follows:
+  + **ACTIVE**: The status of the endpoint group is normal operation.
+  + **ERROR**: The status of the endpoint group is error.
+
+* `listener_id` - (Optional, String) Specifies the ID of the listener to which the endpoint group belongs.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `endpoint_groups` - The list of the endpoint groups.
+  The [endpoint_groups](#ga_endpoint_groups) structure is documented below.
+
+<a name="ga_endpoint_groups"></a>
+The `endpoint_groups` block supports:
+
+* `id` - The ID of the endpoint group.
+
+* `name` - The name of the endpoint group.  
+
+* `description` - The description of the endpoint group.
+
+* `status` - The status of the endpoint group.
+
+* `traffic_dial_percentage` - The percentage of traffic distributed to the endpoint group.
+
+* `region_id` - The region where the endpoint group belongs.
+
+* `listener_id` - The ID of the listener to which the endpoint group belongs.
+
+* `created_at` - The creation time of the endpoint group.
+
+* `updated_at` - The latest update time of the endpoint group.
---
+++ huaweicloud/services/acceptance/ga/data_source_huaweicloud_ga_endpoint_groups_test.go
@@ -0,0 +1,192 @@
+package ga
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDatasourceEndpointGroups_basic(t *testing.T) {
+	var (
+		name           = acceptance.RandomAccResourceNameWithDash()
+		dataSourceName = "data.huaweicloud_ga_endpoint_groups.test"
+		dc             = acceptance.InitDataSourceCheck(dataSourceName)
+
+		byEndpointGroupId   = "data.huaweicloud_ga_endpoint_groups.filter_by_endpoint_group_id"
+		dcByEndpointGroupId = acceptance.InitDataSourceCheck(byEndpointGroupId)
+
+		byName   = "data.huaweicloud_ga_endpoint_groups.filter_by_name"
+		dcByName = acceptance.InitDataSourceCheck(byName)
+
+		byStatus   = "data.huaweicloud_ga_endpoint_groups.filter_by_status"
+		dcByStatus = acceptance.InitDataSourceCheck(byStatus)
+
+		byListenerId   = "data.huaweicloud_ga_endpoint_groups.filter_by_listener_id"
+		dcByListenerId = acceptance.InitDataSourceCheck(byListenerId)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceEndpointGroups_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSourceName, "endpoint_groups.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "endpoint_groups.0.id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "endpoint_groups.0.name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "endpoint_groups.0.description"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "endpoint_groups.0.traffic_dial_percentage"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "endpoint_groups.0.region_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "endpoint_groups.0.listener_id"),
+					dcByEndpointGroupId.CheckResourceExists(),
+					resource.TestCheckOutput("endpoint_group_id_filter_is_useful", "true"),
+
+					dcByName.CheckResourceExists(),
+					resource.TestCheckOutput("name_filter_is_useful", "true"),
+
+					dcByStatus.CheckResourceExists(),
+					resource.TestCheckOutput("status_filter_is_useful", "true"),
+
+					dcByListenerId.CheckResourceExists(),
+					resource.TestCheckOutput("listener_id_filter_is_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceEndpointGroups_base(name string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_ga_accelerator" "test" {
+  name        = "%[1]s"
+  description = "Terraform test"
+
+  ip_sets {
+    area = "CM"
+  }
+
+  tags = {
+    foo = "bar"
+    key = "value"
+  }
+}
+
+resource "huaweicloud_ga_listener" "test" {
+  accelerator_id = huaweicloud_ga_accelerator.test.id
+  name           = "%[1]s"
+  protocol       = "TCP"
+  description    = "Terraform test"
+
+  port_ranges {
+    from_port = 90
+    to_port   = 99
+  }
+
+  tags = {
+    foo = "bar"
+    key = "value"
+  }
+}
+
+resource "huaweicloud_ga_endpoint_group" "test" {
+  name        = "%[1]s"
+  description = "created by terraform"
+  region_id   = "cn-south-1"
+
+  listeners {
+    id = huaweicloud_ga_listener.test.id
+  }
+}
+`, name)
+}
+
+func testAccDataSourceEndpointGroups_basic(name string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_ga_endpoint_groups" "test" {
+  depends_on = [
+    huaweicloud_ga_endpoint_group.test
+  ]
+}
+
+locals {
+  endpoint_group_id = data.huaweicloud_ga_endpoint_groups.test.endpoint_groups[0].id
+}
+
+data "huaweicloud_ga_endpoint_groups" "filter_by_endpoint_group_id" {
+  endpoint_group_id = local.endpoint_group_id
+}
+
+locals {
+  endpoint_group_id_filter_result = [
+    for v in data.huaweicloud_ga_endpoint_groups.filter_by_endpoint_group_id.endpoint_groups[*].id : 
+    v == local.endpoint_group_id
+  ]
+}
+
+output "endpoint_group_id_filter_is_useful" {
+  value = alltrue(local.endpoint_group_id_filter_result) && length(local.endpoint_group_id_filter_result) > 0
+}
+
+locals {
+  name = data.huaweicloud_ga_endpoint_groups.test.endpoint_groups[0].name
+}
+
+data "huaweicloud_ga_endpoint_groups" "filter_by_name" {
+  name = local.name
+}
+
+locals {
+  name_filter_result = [
+    for v in data.huaweicloud_ga_endpoint_groups.filter_by_name.endpoint_groups[*].name : v == local.name
+  ]
+}
+
+output "name_filter_is_useful" {
+  value = alltrue(local.name_filter_result) && length(local.name_filter_result) > 0
+}
+
+locals {
+  status = data.huaweicloud_ga_endpoint_groups.test.endpoint_groups[0].status
+}
+
+data "huaweicloud_ga_endpoint_groups" "filter_by_status" {
+  status = local.status
+}
+
+locals {
+  status_filter_result = [
+    for v in data.huaweicloud_ga_endpoint_groups.filter_by_status.endpoint_groups[*].status : v == local.status
+  ]
+}
+
+output "status_filter_is_useful" {
+  value = alltrue(local.status_filter_result) && length(local.status_filter_result) > 0
+}
+
+locals {
+  listener_id = data.huaweicloud_ga_endpoint_groups.test.endpoint_groups[0].listener_id
+}
+
+data "huaweicloud_ga_endpoint_groups" "filter_by_listener_id" {
+  listener_id = local.listener_id
+}
+
+locals {
+  listener_id_filter_result = [
+    for v in data.huaweicloud_ga_endpoint_groups.filter_by_listener_id.endpoint_groups[*].listener_id : 
+    v == local.listener_id
+  ]
+}
+
+output "listener_id_filter_is_useful" {
+  value = alltrue(local.listener_id_filter_result) && length(local.listener_id_filter_result) > 0
+}
+`, testAccDataSourceEndpointGroups_base(name))
+}
