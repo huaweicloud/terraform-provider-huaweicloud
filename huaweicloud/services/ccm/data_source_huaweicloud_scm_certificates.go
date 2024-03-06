6faeb050812e7ba430da7d1346848d0bd28d257a
shichangkuo <shichangkuo90@gmail.com>
2024-03-06 23:51:16 +0800

    Revert "fix(CCM): modify api comments (#4216)" (#4362)
    This reverts commit 4eba2f6a8ca7186f57a724f74beeb0565600a7e6.

--- huaweicloud/services/ccm/data_source_huaweicloud_scm_certificates.go
+++ huaweicloud/services/ccm/data_source_huaweicloud_scm_certificates.go
@@ -23,7 +23,7 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
-// @API CCM GET /v3/scm/certificates
+// @API SCM GET /v3/scm/certificates
 func DataSourceCertificates() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: resourceCertificatesRead,
