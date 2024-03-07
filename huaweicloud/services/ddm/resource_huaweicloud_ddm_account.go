74bb322e49bc312f7bab8d437b1a641aa065cef6
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2024-03-07 15:09:33 +0800

    chore(DDM): fix ddm api comments (#4365)

--- huaweicloud/services/ddm/resource_huaweicloud_ddm_account.go
+++ huaweicloud/services/ddm/resource_huaweicloud_ddm_account.go
@@ -30,11 +30,11 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
-// @API DDM GET /v1/{project_id}/instances/{instance_id}/users
 // @API DDM POST /v1/{project_id}/instances/{instance_id}/users
-// @API DDM DELETE /v1/{project_id}/instances/{instance_id}/users/{username}
 // @API DDM PUT /v1/{project_id}/instances/{instance_id}/users/{username}
 // @API DDM POST /v2/{project_id}/instances/{instance_id}/users/{username}/password
+// @API DDM GET /v1/{project_id}/instances/{instance_id}/users
+// @API DDM DELETE /v1/{project_id}/instances/{instance_id}/users/{username}
 func ResourceDdmAccount() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceDdmAccountCreate,
