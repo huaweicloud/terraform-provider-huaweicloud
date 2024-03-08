78799cd3d04c22cd60c0a76191c0605938cc4ede
Jason-Zhang <jasonzhang9309@163.com>
2024-03-08 17:36:46 +0800

    chore(vpn): fix api comments of vpn (#4369)

--- huaweicloud/services/vpn/resource_huaweicloud_vpn_gateway.go
+++ huaweicloud/services/vpn/resource_huaweicloud_vpn_gateway.go
@@ -36,7 +36,7 @@ import (
 // @API VPN GET /v5/{project_id}/vpn-gateways/{gateway_id}/certificate
 // @API VPN DELETE /v5/{project_id}/vpn-gateways/{id}
 // @API VPN POST /v5/{project_id}/{resource_type}/{resource_id}/tags/create
-// @API VPN DELETE /v5/{project_id}/{resource_type}/{resource_id}/tags/delete
+// @API VPN POST /v5/{project_id}/{resource_type}/{resource_id}/tags/delete
 func ResourceGateway() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceGatewayCreate,
