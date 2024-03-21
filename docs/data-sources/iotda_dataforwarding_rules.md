49d06ddbe02a3fcea3e092886a9069060a34635c
ruwenqiang123 <ruwenqiang123@outlook.com>
2024-03-21 09:33:16 +0800

    feat(iotda): add a datasource to get the list of dataforwarding rules

---
+++ docs/data-sources/iotda_dataforwarding_rules.md
@@ -0,0 +1,117 @@
+---
+subcategory: "IoT Device Access (IoTDA)"
+---
+
+# huaweicloud_iotda_dataforwarding_rules
+
+Use this data source to get the list of IoTDA dataforwarding rules.
+
+-> When accessing an IoTDA **standard** or **enterprise** edition instance, you need to specify the IoTDA service
+  endpoint in `provider` block.
+  You can login to the IoTDA console, choose the instance **Overview** and click **Access Details**
+  to view the HTTPS application access address. An example of the access address might be
+  **9bc34xxxxx.st1.iotda-app.ap-southeast-1.myhuaweicloud.com**, then you need to configure the
+  `provider` block as follows:
+
+  ```hcl
+  provider "huaweicloud" {
+    endpoints = {
+      iotda = "https://9bc34xxxxx.st1.iotda-app.ap-southeast-1.myhuaweicloud.com"
+    }
+  }
+  ```
+
+## Example Usage
+
+```hcl
+variable "rule_name" {}
+
+data "huaweicloud_iotda_dataforwarding_rules" "test" {
+  name = var.rule_name
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the dataforwarding rules.
+  If omitted, the provider-level region will be used.
+
+* `rule_id` - (Optional, String) Specifies the ID of the dataforwarding rule.
+
+* `name` - (Optional, String) Specifies the name of the dataforwarding rule.
+
+* `resource` - (Optional, String) Specifies the data source of the dataforwarding rule.
+  This parameter must be used together with `trigger`. The valid values are as follows:
+  + **device**
+  + **device.property**
+  + **device.message**
+  + **device.message.status**
+  + **device.status**
+  + **batchtask**
+  + **product**
+  + **device.command.status**
+
+* `trigger` - (Optional, String) Specifies the triggering event of the data source corresponding to
+  the dataforwarding rule. This parameter must be used together with `resource`. The valid values are as follows:
+  + **device:create**: Device added.
+  + **device:delete**: Device deleted.
+  + **device:update**: Device updated.
+  + **device.status:update**: Device status changed.
+  + **device.property:report**: Device property reported.
+  + **device.message:report**: Device message reported.
+  + **device.message.status:update**: Device message status changed.
+  + **batchtask:update**: Batch task status changed.
+  + **product:create**: Product added.
+  + **product:delete**: Product deleted.
+  + **product:update**: Product updated.
+  + **device.command.status:update**: Device asynchronous command status updated.
+
+* `app_type` - (Optional, String) Specifies the validity scope of the dataforwarding rule.
+  The valid values are as follows:
+  + **GLOBAL**: The validity scope is tenant level.
+  + **APP**: The validity scope is resource space level.
+
+  -> If the `app_type` value is **APP**, this parameter can be used together with the `space_id` to query 
+    the dataforwarding rules in the corresponding resource space, if not associated with the `space_id`,
+    will be query the dataforwarding rules in the default resource space.
+
+* `space_id` - (Optional, String) Specifies the ID of the resource space to which the dataforwarding rule belongs.
+
+  -> If use this parameter to query, the parameter `app_type` must be set to **APP**.
+
+* `enabled` - (Optional, String) Specifies whether to enable the dataforwarding rule.
+  The value can be **true** or **false**.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `rules` - All rules that match the filter parameters.
+  The [rules](#iotda_rules) structure is documented below.
+
+<a name="iotda_rules"></a>
+The `rules` block supports:
+
+* `id` - The ID of the dataforwarding rule.
+
+* `name` - The name of the dataforwarding rule.
+
+* `description` - The description of the dataforwarding rule.
+
+* `resource` - The data source of the dataforwarding rule.
+
+* `trigger` - The triggering event of the data source corresponding to the dataforwarding rule.
+
+* `app_type` - The validity scope of the dataforwarding rule.
+
+* `space_id` - The ID of the resource space to which the dataforwarding rule belongs.
+
+* `enabled` - Whether to enable the dataforwarding rule.
+
+* `select` - The user defined SQL **select** statement.
+
+* `where` - The user defined SQL **where** statement.
---
+++ huaweicloud/services/acceptance/iotda/data_source_huaweicloud_iotda_dataforwarding_rules_test.go
@@ -0,0 +1,181 @@
+package iotda
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceDataForwardingRules_basic(t *testing.T) {
+	var (
+		dataSourceName = "data.huaweicloud_iotda_dataforwarding_rules.test"
+		dc             = acceptance.InitDataSourceCheck(dataSourceName)
+		name           = acceptance.RandomAccResourceName()
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceDataForwardingRules_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.resource"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.trigger"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.app_type"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.enabled"),
+
+					resource.TestCheckOutput("rule_id_filter_is_useful", "true"),
+					resource.TestCheckOutput("name_filter_is_useful", "true"),
+					resource.TestCheckOutput("resource_and_trigger_filter_is_useful", "true"),
+					resource.TestCheckOutput("app_type_filter_is_useful", "true"),
+					resource.TestCheckOutput("enabled_filter_is_useful", "true"),
+					resource.TestCheckOutput("not_found_validation_pass", "true"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceDataForwardingRules_derived(t *testing.T) {
+	var (
+		dataSourceName = "data.huaweicloud_iotda_dataforwarding_rules.test"
+		dc             = acceptance.InitDataSourceCheck(dataSourceName)
+		name           = acceptance.RandomAccResourceName()
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckHWIOTDAAccessAddress(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceDataForwardingRules_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.resource"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.trigger"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.app_type"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.enabled"),
+
+					resource.TestCheckOutput("rule_id_filter_is_useful", "true"),
+					resource.TestCheckOutput("app_type_filter_is_useful", "true"),
+					resource.TestCheckOutput("enabled_filter_is_useful", "true"),
+					resource.TestCheckOutput("not_found_validation_pass", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceDataForwardingRules_basic(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+%[2]s
+
+data "huaweicloud_iotda_dataforwarding_rules" "test" {
+  depends_on = [
+    huaweicloud_iotda_dataforwarding_rule.test
+  ]
+}
+
+locals {
+  rule_id = data.huaweicloud_iotda_dataforwarding_rules.test.rules[0].id
+}
+
+data "huaweicloud_iotda_dataforwarding_rules" "rule_id_filter" {
+  rule_id = local.rule_id
+}
+
+output "rule_id_filter_is_useful" {
+  value = length(data.huaweicloud_iotda_dataforwarding_rules.rule_id_filter.rules) > 0 && alltrue(
+    [for v in data.huaweicloud_iotda_dataforwarding_rules.rule_id_filter.rules[*].id : v == local.rule_id]
+  )
+}
+
+locals {
+  name = data.huaweicloud_iotda_dataforwarding_rules.test.rules[0].name
+}
+
+data "huaweicloud_iotda_dataforwarding_rules" "name_filter" {
+  name = local.name
+}
+
+output "name_filter_is_useful" {
+  value = length(data.huaweicloud_iotda_dataforwarding_rules.name_filter.rules) > 0 && alltrue(
+    [for v in data.huaweicloud_iotda_dataforwarding_rules.name_filter.rules[*].name : v == local.name]
+  )
+}
+
+locals {
+  resource = data.huaweicloud_iotda_dataforwarding_rules.test.rules[0].resource
+  trigger  = data.huaweicloud_iotda_dataforwarding_rules.test.rules[0].trigger
+}
+
+data "huaweicloud_iotda_dataforwarding_rules" "resource_and_trigger_filter" {
+  resource = local.resource
+  trigger  = local.trigger
+}
+
+locals {
+  resource_and_trigger_filter_result = [
+    for v in data.huaweicloud_iotda_dataforwarding_rules.resource_and_trigger_filter.rules : 
+    v if(v.trigger == local.trigger && v.resource == local.resource)
+  ]
+}
+
+output "resource_and_trigger_filter_is_useful" {
+  value = length(local.resource_and_trigger_filter_result) > 0 
+}
+
+locals {
+  app_type = data.huaweicloud_iotda_dataforwarding_rules.test.rules[0].app_type
+}
+
+data "huaweicloud_iotda_dataforwarding_rules" "app_type_filter" {
+  app_type = local.app_type
+}
+
+output "app_type_filter_is_useful" {
+  value = length(data.huaweicloud_iotda_dataforwarding_rules.app_type_filter.rules) > 0 && alltrue(
+    [for v in data.huaweicloud_iotda_dataforwarding_rules.app_type_filter.rules[*].app_type : v == local.app_type]
+  )
+}
+
+locals {
+  enabled = data.huaweicloud_iotda_dataforwarding_rules.test.rules[0].enabled
+}
+
+data "huaweicloud_iotda_dataforwarding_rules" "enabled_filter" {
+  enabled = local.enabled
+}
+
+output "enabled_filter_is_useful" {
+  value = length(data.huaweicloud_iotda_dataforwarding_rules.enabled_filter.rules) > 0 && alltrue(
+    [for v in data.huaweicloud_iotda_dataforwarding_rules.enabled_filter.rules[*].enabled : v == local.enabled]
+  )
+}
+
+data "huaweicloud_iotda_dataforwarding_rules" "not_found" {
+  name = "not_found"
+}
+
+output "not_found_validation_pass" {
+  value = length(data.huaweicloud_iotda_dataforwarding_rules.not_found.rules) == 0
+}
+`, testDataForwardingRule_basic(name), buildIoTDAEndpoint())
+}
