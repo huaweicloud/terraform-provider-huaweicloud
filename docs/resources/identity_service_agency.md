2ad47664b4a24e146a534d17aa011f6eb2828819
saf3dfsa <caiwanting@huawei.com>
2024-03-26 15:28:18 +0800

    feat(iam): add resource service agency

---
+++ docs/resources/identity_service_agency.md
@@ -0,0 +1,73 @@
+---
+subcategory: "Identity and Access Management (IAM)"
+---
+
+# huaweicloud_identity_service_agency
+
+Manages a service agency resource within HuaweiCloud.
+
+-> **NOTE:** You *must* have admin privileges to use this resource.
+
+## Example Usage
+
+### Delegate another HUAWEI CLOUD service to perform operations on your resources
+
+```hcl
+variable "agency_name" {}
+variable "delegated_service_name" {}
+variable "policy_name" {}
+
+resource "huaweicloud_identity_service_agency" "test" {
+  name                   = var.agency_name
+  delegated_service_name = var.delegated_service_name
+  policy_names           = [var.policy_name]
+  description            = "test demo"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required, String, ForceNew) Specifies the name of service agency. The name is a string of `1` to `64`
+  characters. Only English letters, digits, underscores (_), plus (+), equals (=), commas (,), dots (.), ats (@) and
+  hyphens (-) are allowed. Changing this will create a new service agency.
+
+* `delegated_service_name` - (Required, String, ForceNew) Specifies the name of delegated service name.
+  Prefix is `service.`. Such as **service.APIG**. Changing this will create a new service agency.
+
+* `policy_names` - (Required, List) Specifies a string list of one or more policy names that you would like to attach to
+  the service agency.
+
+* `path` - (Optional, String, ForceNew) Specifies the resource path. It is made of several strings, each containing one
+  or more English letters, digits, underscores (_), plus (+), equals (=), comma (,), dots (.), at (@) and hyphens (-),
+  and must be ended with slash (/). Such as **foo/bar/**. It's a part of the uniform resource name. Default is empty.
+  Changing this will create a new service agency.
+
+* `duration` - (Optional, Int) Specifies the validity period of a service agency.
+  Default value is **3600**. The unit is seconds.
+
+* `tags` - (Optional, Map) Specifies the tags of the service agency.
+
+* `description` - (Optional, String) Specifies the description of the service agency.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The service agency ID.
+
+* `trust_policy` - The trust policy of the service agency. It's a JSON string.
+
+* `urn` - The uniform resource name of the service agency. Format is `iam::$accountID:agency:$path$agencyName` where
+  `$accountID` is IAM account ID, `$path` is `path`, `$agencyName` is `name`.
+
+* `created_at` - The time when the service agency was created.
+
+## Import
+
+Service agencies can be imported using the `id`, e.g.
+
+```
+$ terraform import huaweicloud_identity_service_agency.test <id>
+```
---
+++ huaweicloud/services/acceptance/iam/resource_huaweicloud_identity_service_agency_test.go
@@ -0,0 +1,123 @@
+package iam
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/chnsz/golangsdk"
+	"github.com/chnsz/golangsdk/openstack/identity/v3/agency"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func getIdentityServiceAgencyResourceFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	client, err := cfg.NewServiceClient("iam_no_version", acceptance.HW_REGION_NAME)
+	if err != nil {
+		return nil, fmt.Errorf("error creating IAM client: %s", err)
+	}
+	getAgencyHttpUrl := "v5/agencies/{agency_id}"
+	getAgencyPath := client.Endpoint + getAgencyHttpUrl
+	getAgencyPath = strings.ReplaceAll(getAgencyPath, "{agency_id}", state.Primary.ID)
+	getAgencyOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+	getAgencyResp, err := client.Request("GET", getAgencyPath, &getAgencyOpt)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving IAM service agency: %s", err)
+	}
+	return utils.FlattenResponse(getAgencyResp)
+}
+
+func TestAccIdentityServiceAgency_basic(t *testing.T) {
+	var object agency.Agency
+	rName := acceptance.RandomAccResourceName()
+	resourceName := "huaweicloud_identity_service_agency.test"
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&object,
+		getIdentityServiceAgencyResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckAdminOnly(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIdentityServiceAgency_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "delegated_service_name", "service.APIG"),
+					resource.TestCheckResourceAttr(resourceName, "policy_names.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "duration", "3600"),
+					resource.TestCheckResourceAttr(resourceName, "tags.foo", "bar"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key", "value"),
+					resource.TestCheckResourceAttr(resourceName, "description", "test for terraform"),
+					resource.TestCheckResourceAttrSet(resourceName, "trust_policy"),
+					resource.TestCheckResourceAttrSet(resourceName, "urn"),
+					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
+				),
+			},
+			{
+				Config: testAccIdentityServiceAgency_update(rName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "policy_names.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "duration", "7200"),
+					resource.TestCheckResourceAttr(resourceName, "tags.foo1", "bar1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.key1", "value1"),
+					resource.TestCheckResourceAttr(resourceName, "description", "test for terraform update"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccIdentityServiceAgency_basic(rName string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_identity_service_agency" "test" {
+  name                   = "%s"
+  delegated_service_name = "service.APIG"
+  policy_names           = ["NATReadOnlyPolicy"]
+  description            = "test for terraform"
+
+  tags = {
+    foo = "bar"
+    key = "value"
+  }
+}
+`, rName)
+}
+
+func testAccIdentityServiceAgency_update(rName string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_identity_service_agency" "test" {
+  name                   = "%s"
+  delegated_service_name = "service.APIG"
+  policy_names           = ["NATReadOnlyPolicy", "RDSReadOnlyPolicy"]
+  duration               = 7200
+  description            = "test for terraform update"
+
+  tags = {
+    foo1 = "bar1"
+    key1 = "value1"
+  }
+}
+`, rName)
+}
