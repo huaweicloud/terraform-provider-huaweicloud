b02be0c5781c3d5999c7d24598fc1c7ac843810e
Jason Zhang <jasonzhang9309@163.com>
2024-03-28 11:03:00 +0800

    feat(rms): add new data_source rms schemas

---
+++ docs/data-sources/rms_stored_query_schemas.md
@@ -0,0 +1,38 @@
+---
+subcategory: "Config"
+---
+
+# huaweicloud_rms_stored_query_schemas
+
+Use this data source to get the list of RMS stored query schemas.
+
+## Example Usage
+
+```hcl
+data "huaweicloud_rms_stored_query_schemas" "test" {
+  type = "aad.instances"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `type` - (Optional, String) Specifies the type of the schema.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `schemas` - The list of schema.
+
+  The [schemas](#schemas_struct) structure is documented below.
+
+<a name="schemas_struct"></a>
+The `schemas` block supports:
+
+* `type` - The schema type.
+
+* `schema` - The schema detail.
---
+++ huaweicloud/services/acceptance/rms/data_source_huaweicloud_rms_stored_query_schemas_test.go
@@ -0,0 +1,54 @@
+package rms
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceRmsStoredQuerySchemas_basic(t *testing.T) {
+	dataSource1 := "data.huaweicloud_rms_stored_query_schemas.basic"
+	dataSource2 := "data.huaweicloud_rms_stored_query_schemas.filter_by_type"
+	dc1 := acceptance.InitDataSourceCheck(dataSource1)
+	dc2 := acceptance.InitDataSourceCheck(dataSource2)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceDataSourceRmsStoredQuerySchemas_basic,
+				Check: resource.ComposeTestCheckFunc(
+					dc1.CheckResourceExists(),
+					dc2.CheckResourceExists(),
+					resource.TestCheckOutput("is_results_not_empty", "true"),
+					resource.TestCheckOutput("is_type_filter_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+const testDataSourceDataSourceRmsStoredQuerySchemas_basic = `
+data "huaweicloud_rms_stored_query_schemas" "basic" {}
+
+data "huaweicloud_rms_stored_query_schemas" "filter_by_type" {
+  type = "ecs.cloudservers"
+}
+
+locals {
+  type_filter_result = [for v in data.huaweicloud_rms_stored_query_schemas.filter_by_type.schemas[*].type : v == "ecs.cloudservers"]
+}
+
+output "is_results_not_empty" {
+  value = length(data.huaweicloud_rms_stored_query_schemas.basic.schemas) > 0
+}
+
+output "is_type_filter_useful" {
+  value = alltrue(local.type_filter_result) && length(local.type_filter_result) > 0
+}
+`
