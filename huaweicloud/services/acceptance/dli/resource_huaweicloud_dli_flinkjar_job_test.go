141fded15f140edc52ea46faa2d04eaa2dad94a6
wuzhuanhong <wuzhuanhong1@outlook.com>
2024-03-29 17:13:03 +0800

    feat(dli): the flink job resources supports updating tags (#4511)

--- huaweicloud/services/acceptance/dli/resource_huaweicloud_dli_flinkjar_job_test.go
+++ huaweicloud/services/acceptance/dli/resource_huaweicloud_dli_flinkjar_job_test.go
@@ -44,12 +44,21 @@ func TestAccResourceDliFlinkJarJob_basic(t *testing.T) {
 		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccFlinkJarJobResource_basic(name, bucketName, acceptance.HW_REGION_NAME,
-					acceptance.HW_ACCESS_KEY, acceptance.HW_SECRET_KEY, acceptance.HW_DLI_FLINK_JAR_OBS_PATH),
+				Config: testAccFlinkJarJobResource_basic(name, bucketName),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
 					resource.TestCheckResourceAttr(resourceName, "name", name),
 					resource.TestCheckResourceAttr(resourceName, "status", "job_running"),
+					resource.TestCheckResourceAttr(resourceName, "tags.foo", "bar"),
+				),
+			},
+			{
+				Config: testAccFlinkJarJobResource_update(name, bucketName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.owner", "terraform"),
 				),
 			},
 			{
@@ -62,40 +71,40 @@ func TestAccResourceDliFlinkJarJob_basic(t *testing.T) {
 	})
 }
 
-func testAccFlinkJarJobResource_basic(name, bucketName, region, ak, sk, jarObsPath string) string {
+func testAccFlinkJarJobResource_base(bucketName, name string) string {
 	return fmt.Sprintf(`
 variable "ak" {
   type        = string
   description = "value"
-  default     = "%s"
+  default     = "%[1]s"
 }
 
 variable "sk" {
   type        = string
   description = "value"
-  default     = "%s"
+  default     = "%[2]s"
 }
 
 variable "jarObsPath" {
   type        = string
   description = "value"
-  default     = "%s"
+  default     = "%[3]s"
 }
 
 resource "huaweicloud_obs_bucket" "test" {
-  bucket        = "%s"
+  bucket        = "%[4]s"
   acl           = "private"
   force_destroy = true
 }
 
 resource "huaweicloud_dli_package" "test" {
-  group_name  = "jarPackage"
+  group_name  = "%[5]s"
   type        = "jar"
   object_path = var.jarObsPath
 }
 
 resource "huaweicloud_dli_queue" "test" {
-  name       = "%s"
+  name       = "%[5]s"
   cu_count   = 16
   queue_type = "general"
 
@@ -103,16 +112,45 @@ resource "huaweicloud_dli_queue" "test" {
     foo = "bar"
   }
 }
+`, acceptance.HW_ACCESS_KEY, acceptance.HW_SECRET_KEY, acceptance.HW_DLI_FLINK_JAR_OBS_PATH, bucketName, name)
+}
+
+func testAccFlinkJarJobResource_basic(name, bucketName string) string {
+	return fmt.Sprintf(`
+%[1]s
 
 resource "huaweicloud_dli_flinkjar_job" "test" {
-  name            = "%s"
-  queue_name      = huaweicloud_dli_queue.test.name
+  name            = "%[2]s"
+  queue_name      = "common_test"
   entrypoint      = "${huaweicloud_dli_package.test.group_name}/${huaweicloud_dli_package.test.object_name}"
-  entrypoint_args = "--output.path obs://${var.ak}:${var.sk}@obs.%s.myhuaweicloud.com/%s/output"
+  entrypoint_args = "--output.path obs://${var.ak}:${var.sk}@obs.%[3]s.myhuaweicloud.com/${huaweicloud_obs_bucket.test.bucket}/output"
   obs_bucket      = huaweicloud_obs_bucket.test.bucket
   log_enabled     = true
+
+  tags = {
+    foo = "bar"
+  }
 }
-`, ak, sk, jarObsPath, bucketName, name, name, region, name)
+`, testAccFlinkJarJobResource_base(bucketName, name), name, acceptance.HW_REGION_NAME)
+}
+
+func testAccFlinkJarJobResource_update(name, bucketName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_dli_flinkjar_job" "test" {
+  name            = "%[2]s"
+  queue_name      = huaweicloud_dli_queue.test.name
+  entrypoint      = "${huaweicloud_dli_package.test.group_name}/${huaweicloud_dli_package.test.object_name}"
+  entrypoint_args = "--output.path obs://${var.ak}:${var.sk}@obs.%[3]s.myhuaweicloud.com/${huaweicloud_obs_bucket.test.bucket}/output"
+  obs_bucket      = huaweicloud_obs_bucket.test.bucket
+  log_enabled     = true
+
+  tags = {
+    owner = "terraform"
+  }
+}
+`, testAccFlinkJarJobResource_base(bucketName, name), name, acceptance.HW_REGION_NAME)
 }
 
 func TestAccResourceDliFlinkJarJob_all(t *testing.T) {
@@ -136,8 +174,7 @@ func TestAccResourceDliFlinkJarJob_all(t *testing.T) {
 		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccFlinkJarJobResource_all(name, bucketName, acceptance.HW_REGION_NAME, acceptance.HW_ACCESS_KEY,
-					acceptance.HW_SECRET_KEY, acceptance.HW_DLI_FLINK_JAR_OBS_PATH),
+				Config: testAccFlinkJarJobResource_all(name, bucketName),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
 					resource.TestCheckResourceAttr(resourceName, "name", name),
@@ -167,54 +204,20 @@ func TestAccResourceDliFlinkJarJob_all(t *testing.T) {
 	})
 }
 
-func testAccFlinkJarJobResource_all(name, bucketName, region, ak, sk, jarObsPath string) string {
+func testAccFlinkJarJobResource_all(name, bucketName string) string {
 	return fmt.Sprintf(`
-variable "ak" {
-  type        = string
-  description = "value"
-  default     = "%s"
-}
-
-variable "sk" {
-  type        = string
-  description = "value"
-  default     = "%s"
-}
-
-variable "jarObsPath" {
-  type        = string
-  description = "value"
-  default     = "%s"
-}
-
-resource "huaweicloud_obs_bucket" "test" {
-  bucket        = "%s"
-  acl           = "private"
-  force_destroy = true
-}
-
-resource "huaweicloud_dli_package" "test" {
-  group_name  = "jarPackage"
-  type        = "jar"
-  object_path = var.jarObsPath
-}
-
-resource "huaweicloud_dli_queue" "test" {
-  name       = "%s"
-  cu_count   = 16
-  queue_type = "general"
-}
+%[1]s
 
 resource "huaweicloud_smn_topic" "test" {
-  name         = "%s"
+  name         = "%[2]s"
   display_name = "The display name of topic_1"
 }
 
 resource "huaweicloud_dli_flinkjar_job" "test" {
-  name                   = "%s"
+  name                   = "%[2]s"
   queue_name             = huaweicloud_dli_queue.test.name
   entrypoint             = "${huaweicloud_dli_package.test.group_name}/${huaweicloud_dli_package.test.object_name}"
-  entrypoint_args        = "--output.path obs://${var.ak}:${var.sk}@obs.%s.myhuaweicloud.com/%s/output"
+  entrypoint_args        = "--output.path obs://${var.ak}:${var.sk}@obs.%[3]s.myhuaweicloud.com/${huaweicloud_obs_bucket.test.bucket}/output"
   obs_bucket             = huaweicloud_obs_bucket.test.bucket
   log_enabled            = true
   description            = "description of dli job"
@@ -233,5 +236,5 @@ resource "huaweicloud_dli_flinkjar_job" "test" {
   resume_max_num         = 2
   checkpoint_path        = "${huaweicloud_obs_bucket.test.bucket}/checkpoint/"
 }
-`, ak, sk, jarObsPath, bucketName, name, name, name, region, name)
+`, testAccFlinkJarJobResource_base(bucketName, name), name, acceptance.HW_REGION_NAME)
 }
