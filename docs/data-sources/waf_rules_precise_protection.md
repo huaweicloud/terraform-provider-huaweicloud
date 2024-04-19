0e14bcbe3a4782fcb53e628ba712afb5439c8647
ruwenqiang123 <ruwenqiang123@outlook.com>
2024-04-19 14:52:32 +0800

    feat(waf): add a datasource to get the list of precise protection rules (#4655)

---
+++ docs/data-sources/waf_rules_precise_protection.md
@@ -0,0 +1,97 @@
+---
+subcategory: "Web Application Firewall (WAF)"
+layout: "huaweicloud"
+page_title: "HuaweiCloud: huaweicloud_waf_rules_precise_protection"
+description: |-
+  Use this data source to get a list of precise protection rules.
+---
+
+# huaweicloud_waf_rules_precise_protection
+
+Use this data source to get a list of precise protection rules.
+
+## Example Usage
+
+```hcl
+variable "policy_id" {}
+variable "rule_id" {}
+
+data "huaweicloud_waf_rules_precise_protection" "test" {
+  policy_id = var.policy_id
+  rule_id   = var.rule_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the resource.
+  If omitted, the provider-level region will be used.
+
+* `policy_id` - (Required, String) Specifies the ID of the policy to which the precise protection rules belong.
+
+* `rule_id` - (Optional, String) Specifies the ID of the precise protection rule.
+
+* `name` - (Optional, String) Specifies the name of the precise protection rule.
+
+* `enterprise_project_id` - (Optional, String) Specifies the enterprise project ID to which the protection policies
+  belong.
+
+* `status` - (Optional, String) Specifies the status of the precise protection rule.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `rules` - The list of the precise protection rules.
+
+  The [rules](#rules_struct) structure is documented below.
+
+<a name="rules_struct"></a>
+The `rules` block supports:
+
+* `id` - The ID of the precise protection rule.
+
+* `policy_id` - The ID of the policy to which the precise protection rule belongs.
+
+* `name` - The name of the precise protection rule.
+
+* `description` - The description of the precise protection rule.
+
+* `status` - The status of the precise protection rule.
+
+* `priority` - The priority of the precise protection rule.
+
+* `time` - The effective time of the precision protection rule.
+
+* `start_time` - The start time for the implementation of precision protection rule.
+  This parameter will only be returned when the `time` value is **true**.
+
+* `end_time` - The end time for the implementation of precision protection rule.
+  This parameter will only be returned when the `time` value is **true**.
+
+* `conditions` - The matching condition list of the precision protection rule.
+
+  The [conditions](#rules_conditions_struct) structure is documented below.
+
+* `action` - The protective action of the precise protection rule.
+
+* `known_attack_source_id` - The known attack source ID.
+
+* `created_at` - The creation time of the precise protection rule.
+
+<a name="rules_conditions_struct"></a>
+The `conditions` block supports:
+
+* `field` - The field of the condition.
+
+* `subfield` - The subfield of the condition.
+
+* `logic` - The condition matching logic.
+
+* `content` - The content of the match condition.
+
+* `reference_table_id` - The reference table id.
---
+++ huaweicloud/services/acceptance/waf/data_source_huaweicloud_waf_rules_precise_protection_test.go
@@ -0,0 +1,144 @@
+package waf
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceRulesPreciseProtection_basic(t *testing.T) {
+	var (
+		dataSourceName = "data.huaweicloud_waf_rules_precise_protection.test"
+		rName          = acceptance.RandomAccResourceName()
+		dc             = acceptance.InitDataSourceCheck(dataSourceName)
+
+		byRuleId   = "data.huaweicloud_waf_rules_precise_protection.filter_by_rule_id"
+		dcByRuleId = acceptance.InitDataSourceCheck(byRuleId)
+
+		byName           = "data.huaweicloud_waf_rules_precise_protection.filter_by_rule_id"
+		dcByName         = acceptance.InitDataSourceCheck(byName)
+		byNameNotFound   = "data.huaweicloud_waf_rules_precise_protection.not_found"
+		dcByNameNotFound = acceptance.InitDataSourceCheck(byNameNotFound)
+
+		byStatus   = "data.huaweicloud_waf_rules_precise_protection.filter_by_status"
+		dcByStatus = acceptance.InitDataSourceCheck(byStatus)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPrecheckWafInstance(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceRulesPreciseProtection_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.status"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.priority"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.conditions.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "rules.0.created_at"),
+
+					dcByRuleId.CheckResourceExists(),
+					resource.TestCheckOutput("rule_id_filter_is_useful", "true"),
+
+					dcByName.CheckResourceExists(),
+					resource.TestCheckOutput("name_filter_is_useful", "true"),
+
+					dcByNameNotFound.CheckResourceExists(),
+					resource.TestCheckOutput("is_not_found", "true"),
+
+					dcByStatus.CheckResourceExists(),
+					resource.TestCheckOutput("status_filter_is_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testDataSourceRulesPreciseProtection_basic(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+data "huaweicloud_waf_rules_precise_protection" "test" {
+  depends_on = [
+    huaweicloud_waf_rule_precise_protection.test
+  ]
+
+  policy_id = huaweicloud_waf_policy.policy_1.id
+}
+
+locals {
+  rule_id = data.huaweicloud_waf_rules_precise_protection.test.rules[0].id
+}
+
+data "huaweicloud_waf_rules_precise_protection" "filter_by_rule_id" {
+  policy_id = huaweicloud_waf_policy.policy_1.id
+  rule_id   = local.rule_id
+}
+
+locals {
+  rule_id_filter_result = [
+    for v in data.huaweicloud_waf_rules_precise_protection.filter_by_rule_id.rules[*].id : v == local.rule_id
+  ]
+}
+
+output "rule_id_filter_is_useful" {
+  value = alltrue(local.rule_id_filter_result) && length(local.rule_id_filter_result) > 0
+}
+
+locals {
+  name = data.huaweicloud_waf_rules_precise_protection.test.rules[0].name
+}
+
+data "huaweicloud_waf_rules_precise_protection" "filter_by_name" {
+  policy_id = huaweicloud_waf_policy.policy_1.id
+  name      = local.name
+}
+
+locals {
+  name_filter_result = [
+    for v in data.huaweicloud_waf_rules_precise_protection.filter_by_name.rules[*].name : v == local.name
+  ]
+}
+
+output "name_filter_is_useful" {
+  value = alltrue(local.name_filter_result) && length(local.name_filter_result) > 0
+}
+
+data "huaweicloud_waf_rules_precise_protection" "not_found" {
+  policy_id = huaweicloud_waf_policy.policy_1.id
+  name      = "not_found"
+}
+  
+output "is_not_found" {
+  value = length(data.huaweicloud_waf_rules_precise_protection.not_found.rules) == 0
+}
+
+locals {
+  status = data.huaweicloud_waf_rules_precise_protection.test.rules[0].status
+}
+
+data "huaweicloud_waf_rules_precise_protection" "filter_by_status" {
+  policy_id = huaweicloud_waf_policy.policy_1.id
+  status    = local.status
+}
+
+locals {
+  status_filter_result = [ 
+    for v in data.huaweicloud_waf_rules_precise_protection.filter_by_status.rules[*].status : v == local.status
+  ]
+}
+
+output "status_filter_is_useful" {
+  value = alltrue(local.status_filter_result) && length(local.status_filter_result) > 0
+}
+`, testRulePreciseProtection_basic(name))
+}
