e7810689dafa2a427ec29b3e4d7631a596c6ebb1
jinyangyang222 <jinyangyang222@outlook.com>
2024-04-26 14:38:33 +0800

    feat(cbh/instance): support update `enterprise_project_id` (#4695)
    * feat(cbh/instance): support update `enterprise_project_id`
    
    * refactor(cbh/instance): encapsulate a new function to get instance `resource_id`

--- huaweicloud/services/cbh/resource_huaweicloud_cbh_instance.go
+++ huaweicloud/services/cbh/resource_huaweicloud_cbh_instance.go
@@ -15,6 +15,7 @@ import (
 	"github.com/jmespath/go-jmespath"
 
 	"github.com/chnsz/golangsdk"
+	"github.com/chnsz/golangsdk/openstack/eps/v1/enterpriseprojects"
 	"github.com/chnsz/golangsdk/openstack/networking/v1/eips"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
@@ -153,7 +154,6 @@ func ResourceCBHInstance() *schema.Resource {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Computed:    true,
-				ForceNew:    true,
 				Description: "Specifies the enterprise project ID to which the CBH instance belongs.",
 			},
 			"public_ip": {
@@ -575,12 +575,11 @@ func resourceCBHInstanceUpdate(ctx context.Context, d *schema.ResourceData, meta
 			return diag.Errorf("error creating BSS V2 client: %s", err)
 		}
 
-		instances, err := getCBHInstanceList(client)
+		resourceId, err := getInstanceResourceIdById(client, ID)
 		if err != nil {
 			return diag.FromErr(err)
 		}
-		expression := fmt.Sprintf("[?server_id == '%s']|[0].resource_info.resource_id", ID)
-		resourceId := utils.PathSearch(expression, instances, "").(string)
+
 		if resourceId == "" {
 			return diag.Errorf("error updating the auto-renew of the CBH instance (%s): "+
 				"resource ID is not found in list API response", ID)
@@ -590,7 +589,41 @@ func resourceCBHInstanceUpdate(ctx context.Context, d *schema.ResourceData, meta
 			return diag.Errorf("error updating the auto-renew of the CBH instance (%s): %s", ID, err)
 		}
 	}
+
+	if d.HasChange("enterprise_project_id") {
+		resourceId, err := getInstanceResourceIdById(client, ID)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		if resourceId == "" {
+			return diag.Errorf("error updating the enterprise project ID of the CBH instance (%s): "+
+				"resource ID is not found in list API response", ID)
+		}
+
+		migrateOpts := enterpriseprojects.MigrateResourceOpts{
+			ResourceId:   resourceId,
+			ResourceType: "cbh",
+			RegionId:     region,
+			ProjectId:    client.ProjectID,
+		}
+		if err := common.MigrateEnterpriseProject(ctx, cfg, d, migrateOpts); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
 	return resourceCBHInstanceRead(ctx, d, meta)
+}
+
+func getInstanceResourceIdById(client *golangsdk.ServiceClient, instanceId string) (string, error) {
+	instances, err := getCBHInstanceList(client)
+	if err != nil {
+		return "", err
+	}
+	expression := fmt.Sprintf("[?server_id == '%s']|[0].resource_info.resource_id", instanceId)
+	resourceId := utils.PathSearch(expression, instances, "").(string)
+
+	return resourceId, nil
 }
 
 func updateFlavorId(client *golangsdk.ServiceClient, resourceId, flavorId string) (string, error) {
