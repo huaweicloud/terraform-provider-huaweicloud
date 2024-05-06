1e1728d01068308e8f714d106a038ddc8837c018
profoundwu <profoundwu@outlook.com>
2024-05-06 20:39:25 +0800

    feat(cfw): add data source to get the list of CFW domain name groups

---
+++ docs/data-sources/cfw_domain_name_groups.md
@@ -0,0 +1,107 @@
+---
+subcategory: "Cloud Firewall (CFW)"
+layout: "huaweicloud"
+page_title: "HuaweiCloud: huaweicloud_cfw_domain_name_groups"
+description: |-
+  Use this data source to get the list of CFW domain name groups.
+---
+
+# huaweicloud_cfw_domain_name_groups
+
+Use this data source to get the list of CFW domain name groups.
+
+## Example Usage
+
+```hcl
+variable "fw_instance_id" {}
+variable "object_id" {}
+
+data "huaweicloud_cfw_domain_name_groups" "test" {
+  fw_instance_id = var.fw_instance_id
+  object_id      = var.object_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the resource.
+  If omitted, the provider-level region will be used.
+
+* `fw_instance_id` - (Required, String) Specifies the firewall instance ID.
+
+* `object_id` - (Required, String) Specifies the protected object ID.
+
+* `key_word` - (Optional, String) Specifies the key word.
+
+* `type` - (Optional, String) Specifies the domain name group type.
+  **0** means URL filtering, **1** means domain parse.
+
+* `config_status` - (Optional, String) Specifies the configuration status.
+  The valid values are as follows:
+  + **-1**: not configured.
+  + **0**: configuration failed.
+  + **1**: configuration succeeded.
+  + **2**: configuration in progress.
+  + **3**: normal.
+  + **4**: configuration exception - domain group usage.
+
+* `ref_count` - (Optional, Int) Specifies the domain name group reference count.
+
+* `enterprise_project_id` - (Optional, String) Specifies the enterprise project ID.
+
+* `group_id` - (Optional, String) Specifies the domain name group ID.
+
+* `name` - (Optional, String) Specifies the name of a domain name group.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `records` - The domain name group list.
+
+  The [records](#data_records_struct) structure is documented below.
+
+<a name="data_records_struct"></a>
+The `records` block supports:
+
+* `group_id` - The domain name group ID.
+
+* `name` - The name of the domain name group.
+
+* `description` - The domain name group description.
+
+* `ref_count` - The domain name group reference count.
+
+* `type` - The domain name group type.
+
+* `config_status` - The configuration status.
+
+* `message` - The configuration message.
+
+* `rules` - The used rule list.
+
+  The [rules](#records_rules_struct) structure is documented below.
+
+* `domain_names` - The list of domain names.
+
+  The [domain_names](#records_domain_names_struct) structure is documented below.
+
+<a name="records_rules_struct"></a>
+The `rules` block supports:
+
+* `id` - The rule ID.
+
+* `name` - The rule name.
+
+<a name="records_domain_names_struct"></a>
+The `domain_names` block supports:
+
+* `domain_name` - The domain name.
+
+* `description` - The description.
+
+* `domain_address_id` - The domain address ID.
---
+++ huaweicloud/services/acceptance/cfw/data_source_huaweicloud_cfw_domain_name_groups_test.go
@@ -0,0 +1,228 @@
+package cfw
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceCfwDomainNameGroups_basic(t *testing.T) {
+	dataSource := "data.huaweicloud_cfw_domain_name_groups.filter_by_id"
+	rName := acceptance.RandomAccResourceName()
+	dc := acceptance.InitDataSourceCheck(dataSource)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceCfwDomainNameGroups_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.group_id"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.name"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.type"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.domain_names.0.domain_name"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.domain_names.0.domain_address_id"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.rules.0.id"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.rules.0.name"),
+					resource.TestCheckOutput("is_default_filter_useful", "true"),
+					resource.TestCheckOutput("is_id_filter_useful", "true"),
+					resource.TestCheckOutput("is_name_filter_useful", "true"),
+					resource.TestCheckOutput("is_type_filter_useful", "true"),
+					resource.TestCheckOutput("is_config_status_filter_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testDataSourceCfwDomainNameGroups_basic(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+locals {
+  id            = huaweicloud_cfw_domain_name_group.g2.id
+  name          = huaweicloud_cfw_domain_name_group.g2.name
+  type          = tostring(huaweicloud_cfw_domain_name_group.g1.type)
+  config_status = "3"
+}
+
+data "huaweicloud_cfw_domain_name_groups" "test" {
+  fw_instance_id = "%[2]s"
+  object_id      = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+
+  depends_on = [
+    huaweicloud_cfw_protection_rule.test,
+  ]
+}
+
+output "is_default_filter_useful" {
+  value = length(data.huaweicloud_cfw_domain_name_groups.test.records) >= 2
+}
+
+data "huaweicloud_cfw_domain_name_groups" "filter_by_id" {
+  fw_instance_id = "%[2]s"
+  object_id      = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  group_id       = local.id
+
+  depends_on = [
+    huaweicloud_cfw_protection_rule.test,
+  ]
+}
+
+output "is_id_filter_useful" {
+  value = length(data.huaweicloud_cfw_domain_name_groups.filter_by_id.records) >= 1 && alltrue(
+    [for v in data.huaweicloud_cfw_domain_name_groups.filter_by_id.records[*] : v.group_id == local.id]
+  )
+}
+
+data "huaweicloud_cfw_domain_name_groups" "filter_by_name" {
+  fw_instance_id = "%[2]s"
+  object_id      = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  name           = local.name
+
+  depends_on = [
+    huaweicloud_cfw_protection_rule.test,
+  ]
+}
+
+output "is_name_filter_useful" {
+  value = length(data.huaweicloud_cfw_domain_name_groups.filter_by_name.records) >= 1 && alltrue(
+    [for v in data.huaweicloud_cfw_domain_name_groups.filter_by_name.records[*] : v.name == local.name]
+  )
+}
+
+data "huaweicloud_cfw_domain_name_groups" "filter_by_type" {
+  fw_instance_id = "%[2]s"
+  object_id      = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  type           = local.type
+
+  depends_on = [
+    huaweicloud_cfw_protection_rule.test,
+  ]
+}
+
+output "is_type_filter_useful" {
+  value = length(data.huaweicloud_cfw_domain_name_groups.filter_by_type.records) >= 1 && alltrue(
+    [for v in data.huaweicloud_cfw_domain_name_groups.filter_by_type.records[*] : v.type == local.type]
+  )
+}
+
+data "huaweicloud_cfw_domain_name_groups" "filter_by_config_status" {
+  fw_instance_id = "%[2]s"
+  object_id      = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  config_status  = local.config_status
+
+  depends_on = [
+    huaweicloud_cfw_protection_rule.test,
+  ]
+}
+
+output "is_config_status_filter_useful" {
+  value = length(data.huaweicloud_cfw_domain_name_groups.filter_by_config_status.records) >= 1 && alltrue([
+    for v in data.huaweicloud_cfw_domain_name_groups.filter_by_config_status.records[*] : 
+      v.config_status == local.config_status
+  ])
+}
+`, testDataSourceCfwDomainNameGroups_base(name), acceptance.HW_CFW_INSTANCE_ID)
+}
+
+func testDataSourceCfwDomainNameGroups_base(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_cfw_domain_name_group" "g1" {
+  fw_instance_id = "%[2]s"
+  object_id      = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  name           = "%[3]s1"
+  type           = 0
+  description    = "created by terraform"
+	  
+  domain_names {
+   domain_name = "www.test1.com"
+   description = "test domain 1"
+  }
+
+  domain_names {
+    domain_name = "www.test2.com"
+    description = "test domain 2"
+  }
+}
+
+resource "huaweicloud_cfw_domain_name_group" "g2" {
+  fw_instance_id = "%[2]s"
+  object_id      = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  name           = "%[3]s2"
+  type           = 1
+  description    = "created by terraform"
+		
+  domain_names {
+    domain_name = "www.test3.com"
+    description = "test domain 3"
+  }
+
+  domain_names {
+   domain_name = "www.test4.com"
+   description = "test domain 4"
+  }
+}
+
+resource "huaweicloud_cfw_protection_rule" "test" {
+  name                = "%[3]s"
+  object_id           = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  description         = "terraform test"
+  type                = 0
+  address_type        = 0
+  action_type         = 0
+  long_connect_enable = 0
+  status              = 1
+  direction           = 1
+
+  source {
+    type    = 0
+    address = "1.1.1.1"
+  }
+
+  destination {
+    type            = 4
+    domain_set_id   = huaweicloud_cfw_domain_name_group.g2.id
+    domain_set_name = huaweicloud_cfw_domain_name_group.g2.name
+  }
+
+  service {
+    type = 2
+
+    custom_service {
+      protocol    = 6
+      source_port = 80
+      dest_port   = 80			
+    }
+
+    custom_service {
+      protocol    = 6
+      source_port = 8080
+      dest_port   = 8080
+    }
+  }
+
+  sequence {
+    top = 1
+  }
+
+  tags = {
+    key = "value"
+  }
+
+  depends_on = [
+    huaweicloud_cfw_domain_name_group.g1,
+    huaweicloud_cfw_domain_name_group.g2,
+  ] 
+}
+`, testAccDatasourceFirewalls_basic(), acceptance.HW_CFW_INSTANCE_ID, name)
+}
