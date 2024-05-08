c3eb9c8ab7e90c86b31f55f1fe7c58c98b6f7d8a
profoundwu <profoundwu@outlook.com>
2024-05-08 17:42:41 +0800

    feat(cfw): add data source to get the list of CFW black white lists

---
+++ docs/data-sources/cfw_black_white_lists.md
@@ -0,0 +1,87 @@
+---
+subcategory: "Cloud Firewall (CFW)"
+layout: "huaweicloud"
+page_title: "HuaweiCloud: huaweicloud_cfw_black_white_lists"
+description: |-
+   Use this data source to get the list of CFW blacklists and whitelists.
+---
+
+# huaweicloud_cfw_black_white_lists
+
+Use this data source to get the list of CFW blacklists and whitelists.
+
+## Example Usage
+
+```hcl
+variable "object_id" {}
+variable "list_type" {}
+
+data "huaweicloud_cfw_black_white_lists" "test" {
+  object_id = var.object_id
+  list_type = var.list_type      
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the resource.
+  If omitted, the provider-level region will be used.
+
+* `object_id` - (Required, String) Specifies the protected object ID.
+
+* `list_type` - (Required, Int) Specifies the blacklist/whitelist type.
+  The options are **4** (blacklist) and **5** (whitelist).
+
+* `address_type` - (Optional, String) Specifies the IP address type.
+  The valid value can be **0** (IPv4).
+
+* `list_id` - (Optional, String) Specifies the blacklist/whitelist ID.
+
+* `address` - (Optional, String) Specifies the IP address.
+
+* `direction` - (Optional, String) Specifies the direction of a black or white address.
+  The options are as follows:
+  + **0**: source address;
+  + **1**: destination address;
+
+* `port` - (Optional, String) Specifies the port.
+
+* `protocol` - (Optional, Int) Specifies The protocol type.
+  The options are as follows:
+  + **6**: TCP;
+  + **17**: UDP;
+  + **1**: ICMP;
+  + **-1**: any protocol;
+
+* `description` - (Optional, String) Specifies the description.
+
+* `fw_instance_id` - (Optional, String) Specifies the firewall instance ID.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `records` - The blacklist and whitelist records.
+
+  The [records](#data_records_struct) structure is documented below.
+
+<a name="data_records_struct"></a>
+The `records` block supports:
+
+* `list_id` - The blacklist/whitelist ID.
+
+* `direction` - The direction of a black or white address.
+
+* `address_type` - The IP address type.
+
+* `address` - The IP address.
+
+* `protocol` - The protocol type.
+
+* `port` - The port.
+
+* `description` - The description.
---
+++ huaweicloud/services/acceptance/cfw/data_source_huaweicloud_cfw_black_white_lists_test.go
@@ -0,0 +1,198 @@
+package cfw
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceCfwBlackWhiteLists_basic(t *testing.T) {
+	dataSource := "data.huaweicloud_cfw_black_white_lists.test"
+	dc := acceptance.InitDataSourceCheck(dataSource)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceCfwBlackWhiteLists_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.list_id"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.address_type"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.direction"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.protocol"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.port"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.address"),
+					resource.TestCheckOutput("is_default_filter_useful", "true"),
+					resource.TestCheckOutput("is_id_filter_useful", "true"),
+					resource.TestCheckOutput("is_protocol_filter_useful", "true"),
+					resource.TestCheckOutput("is_address_filter_useful", "true"),
+					resource.TestCheckOutput("is_direction_filter_useful", "true"),
+					resource.TestCheckOutput("is_port_filter_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testDataSourceCfwBlackWhiteLists_basic() string {
+	return fmt.Sprintf(`
+%[1]s
+
+locals {
+  id        = huaweicloud_cfw_black_white_list.l1.id
+  protocol  = huaweicloud_cfw_black_white_list.l2.protocol
+  address   = huaweicloud_cfw_black_white_list.l3.address
+  direction = tostring(huaweicloud_cfw_black_white_list.l3.direction)
+  port      = huaweicloud_cfw_black_white_list.l3.port
+}
+
+data "huaweicloud_cfw_black_white_lists" "test" {
+  object_id = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  list_type = 4
+
+  depends_on = [
+    huaweicloud_cfw_black_white_list.l1,
+    huaweicloud_cfw_black_white_list.l2,
+    huaweicloud_cfw_black_white_list.l3,
+  ]
+}
+
+output "is_default_filter_useful" {
+  value = length(data.huaweicloud_cfw_black_white_lists.test.records) >= 2
+}
+
+data "huaweicloud_cfw_black_white_lists" "filter_by_id" {
+  object_id = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  list_type = 4
+  list_id   = local.id
+
+  depends_on = [
+    huaweicloud_cfw_black_white_list.l1,
+    huaweicloud_cfw_black_white_list.l2,
+    huaweicloud_cfw_black_white_list.l3,
+  ]
+}
+
+output "is_id_filter_useful" {
+  value = length(data.huaweicloud_cfw_black_white_lists.filter_by_id.records) >= 1 && alltrue(
+    [for v in data.huaweicloud_cfw_black_white_lists.filter_by_id.records[*] : v.list_id == local.id]
+  )
+}
+
+data "huaweicloud_cfw_black_white_lists" "filter_by_protocol" {
+  object_id = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  list_type = 4
+  protocol  = local.protocol
+
+  depends_on = [
+    huaweicloud_cfw_black_white_list.l1,
+    huaweicloud_cfw_black_white_list.l2,
+    huaweicloud_cfw_black_white_list.l3,
+  ]
+}
+
+output "is_protocol_filter_useful" {
+  value = length(data.huaweicloud_cfw_black_white_lists.filter_by_protocol.records) >= 1 && alltrue(
+    [for v in data.huaweicloud_cfw_black_white_lists.filter_by_protocol.records[*] : v.protocol == local.protocol]
+  )
+}
+
+
+data "huaweicloud_cfw_black_white_lists" "filter_by_address" {
+  object_id = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  list_type = 5
+  address   = local.address
+
+  depends_on = [
+    huaweicloud_cfw_black_white_list.l1,
+    huaweicloud_cfw_black_white_list.l2,
+    huaweicloud_cfw_black_white_list.l3,
+  ]
+}
+
+output "is_address_filter_useful" {
+  value = length(data.huaweicloud_cfw_black_white_lists.filter_by_address.records) >= 1 && alltrue(
+    [for v in data.huaweicloud_cfw_black_white_lists.filter_by_address.records[*] : v.address == local.address]
+  )
+}
+
+data "huaweicloud_cfw_black_white_lists" "filter_by_direction" {
+  object_id = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  list_type = 5
+  direction = local.direction
+
+  depends_on = [
+    huaweicloud_cfw_black_white_list.l1,
+    huaweicloud_cfw_black_white_list.l2,
+    huaweicloud_cfw_black_white_list.l3,
+  ]
+}
+
+output "is_direction_filter_useful" {
+  value = length(data.huaweicloud_cfw_black_white_lists.filter_by_direction.records) >= 1 && alltrue(
+    [for v in data.huaweicloud_cfw_black_white_lists.filter_by_direction.records[*] : v.direction == local.direction]
+  )
+}
+
+data "huaweicloud_cfw_black_white_lists" "filter_by_port" {
+  object_id = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  list_type = 5
+  port      = local.port
+
+  depends_on = [
+    huaweicloud_cfw_black_white_list.l1,
+    huaweicloud_cfw_black_white_list.l2,
+    huaweicloud_cfw_black_white_list.l3,
+  ]
+}
+
+output "is_port_filter_useful" {
+  value = length(data.huaweicloud_cfw_black_white_lists.filter_by_port.records) >= 1 && alltrue(
+    [for v in data.huaweicloud_cfw_black_white_lists.filter_by_port.records[*] : v.port == local.port]
+  )
+}
+`, testDataSourceCfwBlackWhiteLists_base())
+}
+
+func testDataSourceCfwBlackWhiteLists_base() string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_cfw_black_white_list" "l1" {
+  object_id    = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  list_type    = 4
+  direction    = 1
+  protocol     = 6
+  port         = "22"
+  address_type = 0
+  address      = "1.3.1.3"
+}
+
+resource "huaweicloud_cfw_black_white_list" "l2" {
+  object_id    = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  list_type    = 4
+  direction    = 1
+  protocol     = 17
+  port         = "80"
+  address_type = 0
+  address      = "1.2.1.1"
+}
+
+resource "huaweicloud_cfw_black_white_list" "l3" {
+  object_id    = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  list_type    = 5
+  direction    = 0
+  protocol     = 6
+  port         = "80"
+  address_type = 0
+  address      = "2.2.1.1"
+}
+`, testAccDatasourceFirewalls_basic())
+}
