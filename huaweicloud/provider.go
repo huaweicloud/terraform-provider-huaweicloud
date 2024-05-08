5a6880662993ea1cc6ba305d369debc2d5ec061a
CaiWanting <caiwanting@huawei.com>
2024-05-08 10:36:28 +0800

    feat(dds): support instance to associate with eip (#4741)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1069,6 +1069,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_dds_lts_log":                     dds.ResourceDdsLtsLog(),
 			"huaweicloud_dds_instance_restart":            dds.ResourceDDSInstanceRestart(),
 			"huaweicloud_dds_instance_internal_ip_modify": dds.ResourceDDSInstanceModifyIP(),
+			"huaweicloud_dds_instance_eip_associate":      dds.ResourceDDSInstanceBindEIP(),
 
 			"huaweicloud_ddm_instance":               ddm.ResourceDdmInstance(),
 			"huaweicloud_ddm_schema":                 ddm.ResourceDdmSchema(),
