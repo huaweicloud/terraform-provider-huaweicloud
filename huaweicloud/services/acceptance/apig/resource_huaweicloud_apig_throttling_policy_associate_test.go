36710ff057c1d49ed9d763613159a239be365d6e
Lance52259 <Overlord52259@gmail.com>
2024-05-10 12:19:20 +0800

    test(apig/throttle): no need to check eps id for throttle associate test

--- huaweicloud/services/acceptance/apig/resource_huaweicloud_apig_throttling_policy_associate_test.go
+++ huaweicloud/services/acceptance/apig/resource_huaweicloud_apig_throttling_policy_associate_test.go
@@ -44,7 +44,6 @@ func TestAccThrottlingPolicyAssociate_basic(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acceptance.TestAccPreCheck(t)
-			acceptance.TestAccPreCheckEpsID(t) // The creation of APIG instance needs the enterprise project ID.
 		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		CheckDestroy:      rc.CheckResourceDestroy(),
