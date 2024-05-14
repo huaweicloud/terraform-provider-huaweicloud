cbeb4af64b24f36be8150cd1149ac9e25272b79d
CaiWanting <caiwanting@huawei.com>
2024-05-14 10:32:38 +0800

    feat(dds): support instance recovery (#4803)

---
+++ docs/resources/dds_instance_recovery.md
@@ -0,0 +1,72 @@
+---
+subcategory: "Document Database Service (DDS)"
+layout: "huaweicloud"
+page_title: "HuaweiCloud: huaweicloud_dds_instance_recovery"
+description: |-
+  Manages a DDS instance recovery resource within HuaweiCloud.
+---
+
+# huaweicloud_dds_instance_recovery
+
+Manages a DDS instance recovery resource within HuaweiCloud.
+
+## Example Usage
+
+### Recovery insatnce by backup ID
+
+```hcl
+variable "source_id" {}
+variable "backup_id" {}
+variable "target_id" {}
+
+resource "huaweicloud_dds_instance_recovery" "test" {
+  source_id = var.source_id
+  backup_id = var.backup_id
+  target_id = var.target_id
+}
+```
+
+### Recovery insatnce by time stamp
+
+```hcl
+variable "source_id" {}
+variable "restore_time" {}
+variable "target_id" {}
+
+resource "huaweicloud_dds_instance_recovery" "test" {
+  source_id    = var.source_id
+  restore_time = var.restore_time
+  target_id    = var.target_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region in which to create the resource.
+  If omitted, the provider-level region will be used. Changing this creates a new resource.
+
+* `source_id` - (Required, String, ForceNew) Specifies the ID of the source DDS instance.
+  Changing this creates a new resource.
+
+* `target_id` - (Required, String, ForceNew) Specifies the ID of the target DDS instance.
+
+* `backup_id` - (Optional, String, ForceNew) Specifies the backup ID of the source DDS instance.
+  Changing this creates a new resource.
+
+* `restore_time` - (Optional, String, ForceNew) Specifies the restore time of the source DDS instance. The unit is
+  millisecond and the time zone is UTC. This parameter takes effect only for replica set instances.
+  Changing this creates a new resource.
+
+-> Only one of `backup_id` and `restore_time`can be set and it will return a validation error if none are specified.
+
+## Attribute Reference
+
+* `id` - Indicates the resource ID. It's same as the target instance ID.
+
+## Timeouts
+
+This resource provides the following timeouts configuration options:
+
+* `create` - Default is 30 minutes.
---
+++ huaweicloud/services/acceptance/dds/resource_huaweicloud_dds_instance_recovery_test.go
@@ -0,0 +1,92 @@
+package dds
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance/common"
+)
+
+func TestAccDDSV3InstanceRecovery_basic(t *testing.T) {
+	rName := acceptance.RandomAccResourceName()
+
+	// Avoid CheckDestroy
+	// lintignore:AT001
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDDSInstanceRecovery_basic(rName),
+			},
+		},
+	})
+}
+
+func testAccDDSInstanceRecovery_basic(rName string) string {
+	return fmt.Sprintf(`
+data "huaweicloud_availability_zones" "test" {}
+
+%s
+
+%s
+
+%s
+
+resource "huaweicloud_dds_backup" "test" {
+  instance_id = huaweicloud_dds_instance.instance2.id
+  name        = "%s"
+}
+
+resource "huaweicloud_dds_instance_recovery" "test" {
+  depends_on = [huaweicloud_dds_instance.instance1, huaweicloud_dds_backup.test]
+
+  target_id = huaweicloud_dds_instance.instance1.id
+  source_id = huaweicloud_dds_instance.instance2.id
+  backup_id = huaweicloud_dds_backup.test.id
+}`, common.TestBaseNetwork(rName), testAccDDSInstanceRecoveryBase(rName, 1), testAccDDSInstanceRecoveryBase(rName, 2), rName)
+}
+
+func testAccDDSInstanceRecoveryBase(rName string, i int) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_dds_instance" "instance%[1]v" {
+  name              = "%[2]s"
+  availability_zone = data.huaweicloud_availability_zones.test.names[0]
+  vpc_id            = huaweicloud_vpc.test.id
+  subnet_id         = huaweicloud_vpc_subnet.test.id
+  security_group_id = huaweicloud_networking_secgroup.test.id
+  password          = "Terraform@123"
+  mode              = "Sharding"
+
+  datastore {
+    type           = "DDS-Community"
+    version        = "4.0"
+    storage_engine = "wiredTiger"
+  }
+
+  flavor {
+    type      = "mongos"
+    num       = 2
+    spec_code = "dds.mongodb.s6.large.2.mongos"
+  }
+
+  flavor {
+    type      = "shard"
+    num       = 2
+    storage   = "ULTRAHIGH"
+    size      = 20
+    spec_code = "dds.mongodb.s6.large.2.shard"
+  }
+
+  flavor {
+    type      = "config"
+    num       = 1
+    storage   = "ULTRAHIGH"
+    size      = 20
+    spec_code = "dds.mongodb.s6.large.2.config"
+  }
+}`, i, rName)
+}
