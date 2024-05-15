77d6dfa19906e484f988dd74bfa72aff9a2120cd
profoundwu <profoundwu@outlook.com>
2024-05-15 16:39:37 +0800

    feat(cfw): add data source to get a list of CFW service group members

---
+++ docs/data-sources/cfw_service_group_members.md
@@ -0,0 +1,69 @@
+---
+subcategory: "Cloud Firewall (CFW)"
+layout: "huaweicloud"
+page_title: "HuaweiCloud: huaweicloud_cfw_service_group_members"
+description: |-
+  Use this data source to get the list of CFW service group members.
+---
+
+# huaweicloud_cfw_service_group_members
+
+Use this data source to get the list of CFW service group members.
+
+## Example Usage
+
+```hcl
+variable "group_id" {}
+
+data "huaweicloud_cfw_service_group_members" "test" {
+  group_id = var.group_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the resource.
+  If omitted, the provider-level region will be used.
+
+* `group_id` - (Required, String) Specifies the service group ID.
+
+* `key_word` - (Optional, String) Specifies the key word.
+
+* `item_id` - (Optional, String) Specifies the service group member ID.
+
+* `fw_instance_id` - (Optional, String) Specifies the firewall instance ID.
+
+* `protocol` - (Optional, Int) Specifies the protocol type.
+  The options are as follows:
+  + **6**: TCP;
+  + **17**: UDP;
+  + **1**: ICMP.
+
+* `source_port` - (Optional, String) Specifies the source port.
+
+* `dest_port` - (Optional, String) Specifies the destination port.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `records` - The service group member list.
+
+  The [records](#data_records_struct) structure is documented below.
+
+<a name="data_records_struct"></a>
+The `records` block supports:
+
+* `item_id` - The service group member ID.
+
+* `protocol` - The protocol type.
+
+* `source_port` - The source port.
+
+* `dest_port` - The destination port.
+
+* `description` - The service group member description.
---
+++ huaweicloud/services/acceptance/cfw/data_source_huaweicloud_cfw_service_group_members_test.go
@@ -0,0 +1,151 @@
+package cfw
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceCfwServiceGroupMembers_basic(t *testing.T) {
+	dataSource := "data.huaweicloud_cfw_service_group_members.test"
+	rName := acceptance.RandomAccResourceName()
+	dc := acceptance.InitDataSourceCheck(dataSource)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceCfwServiceGroupMembers_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.item_id"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.protocol"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.source_port"),
+					resource.TestCheckResourceAttrSet(dataSource, "records.0.dest_port"),
+					resource.TestCheckOutput("is_default_filter_useful", "true"),
+					resource.TestCheckOutput("is_id_filter_useful", "true"),
+					resource.TestCheckOutput("is_source_port_filter_useful", "true"),
+					resource.TestCheckOutput("is_dest_port_filter_useful", "true"),
+					resource.TestCheckOutput("is_protocol_filter_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testDataSourceCfwServiceGroupMembers_basic(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+locals {
+  id          = huaweicloud_cfw_service_group_member.m2.id
+  source_port = huaweicloud_cfw_service_group_member.m2.source_port
+  dest_port   = huaweicloud_cfw_service_group_member.m1.dest_port
+  protocol    = huaweicloud_cfw_service_group_member.m1.protocol
+}
+
+data "huaweicloud_cfw_service_group_members" "test" {
+  group_id = huaweicloud_cfw_service_group.test.id
+
+  depends_on = [
+    huaweicloud_cfw_service_group_member.m1,
+    huaweicloud_cfw_service_group_member.m2,
+  ]
+}
+
+output "is_default_filter_useful" {
+  value = length(data.huaweicloud_cfw_service_group_members.test.records) >= 2
+}
+
+data "huaweicloud_cfw_service_group_members" "filter_by_id" {
+  group_id = huaweicloud_cfw_service_group.test.id
+  item_id  = local.id
+
+  depends_on = [
+    huaweicloud_cfw_service_group_member.m1,
+    huaweicloud_cfw_service_group_member.m2,
+  ]
+}
+
+output "is_id_filter_useful" {
+  value = length(data.huaweicloud_cfw_service_group_members.filter_by_id.records) >= 1 && alltrue(
+    [for v in data.huaweicloud_cfw_service_group_members.filter_by_id.records[*] : v.item_id == local.id]
+  )
+}
+
+data "huaweicloud_cfw_service_group_members" "filter_by_source_port" {
+  group_id    = huaweicloud_cfw_service_group.test.id
+  source_port = local.source_port
+  
+  depends_on = [
+    huaweicloud_cfw_service_group_member.m1,
+    huaweicloud_cfw_service_group_member.m2,
+  ]
+}
+
+output "is_source_port_filter_useful" {
+  value = length(data.huaweicloud_cfw_service_group_members.filter_by_source_port.records) >= 1 && alltrue(
+    [for v in data.huaweicloud_cfw_service_group_members.filter_by_source_port.records[*] : v.source_port == local.source_port]
+  )
+}
+
+data "huaweicloud_cfw_service_group_members" "filter_by_dest_port" {
+  group_id  = huaweicloud_cfw_service_group.test.id
+  dest_port = local.dest_port
+  
+  depends_on = [
+    huaweicloud_cfw_service_group_member.m1,
+    huaweicloud_cfw_service_group_member.m2,
+  ]
+}
+
+output "is_dest_port_filter_useful" {
+  value = length(data.huaweicloud_cfw_service_group_members.filter_by_dest_port.records) >= 1 && alltrue(
+    [for v in data.huaweicloud_cfw_service_group_members.filter_by_dest_port.records[*] : v.dest_port == local.dest_port]
+  )
+}
+
+data "huaweicloud_cfw_service_group_members" "filter_by_protocol" {
+  group_id = huaweicloud_cfw_service_group.test.id
+  protocol = local.protocol
+
+  depends_on = [
+    huaweicloud_cfw_service_group_member.m1,
+    huaweicloud_cfw_service_group_member.m2,
+  ]
+}
+
+output "is_protocol_filter_useful" {
+  value = length(data.huaweicloud_cfw_service_group_members.filter_by_protocol.records) >= 1 && alltrue([
+    for v in data.huaweicloud_cfw_service_group_members.filter_by_protocol.records[*] : 
+      v.protocol == local.protocol
+  ])
+}
+`, testDataSourceCfwServiceGroupMembers_base(name))
+}
+
+func testDataSourceCfwServiceGroupMembers_base(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_cfw_service_group_member" "m1" {
+  group_id    = huaweicloud_cfw_service_group.test.id
+  protocol    = 6
+  source_port = "80"
+  dest_port   = "8080"
+}
+
+resource "huaweicloud_cfw_service_group_member" "m2" {
+  group_id    = huaweicloud_cfw_service_group.test.id
+  protocol    = 17
+  source_port = "80"
+  dest_port   = "81"
+}
+`, testServiceGroup_basic(name))
+}
