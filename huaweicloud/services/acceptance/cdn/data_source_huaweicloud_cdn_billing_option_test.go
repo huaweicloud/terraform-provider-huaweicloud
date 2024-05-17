20fc34fa13d59b24d7c38f56a785000a0646ecca
luxiaohang <deerhang@163.com>
2024-05-17 14:26:20 +0800

    feat: cdn domain support new datasource to query billing option

---
+++ huaweicloud/services/acceptance/cdn/data_source_huaweicloud_cdn_billing_option_test.go
@@ -0,0 +1,75 @@
+package cdn
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDatasourceBillingOption_basic(t *testing.T) {
+	var (
+		rName = "data.huaweicloud_cdn_billing_option.test"
+		dc    = acceptance.InitDataSourceCheck(rName)
+
+		allFilterRName = "data.huaweicloud_cdn_billing_option.all_filter"
+		allFilterDc    = acceptance.InitDataSourceCheck(allFilterRName)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourceBillingOption_basic,
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(rName, "product_type"),
+					resource.TestCheckResourceAttrSet(rName, "service_area"),
+					resource.TestCheckResourceAttrSet(rName, "status"),
+					resource.TestCheckResourceAttrSet(rName, "charge_mode"),
+					resource.TestCheckResourceAttrSet(rName, "created_at"),
+					resource.TestCheckResourceAttrSet(rName, "effective_time"),
+
+					allFilterDc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(allFilterRName, "product_type"),
+					resource.TestCheckResourceAttrSet(allFilterRName, "service_area"),
+					resource.TestCheckResourceAttrSet(allFilterRName, "status"),
+					resource.TestCheckResourceAttrSet(allFilterRName, "charge_mode"),
+					resource.TestCheckResourceAttrSet(allFilterRName, "created_at"),
+					resource.TestCheckResourceAttrSet(allFilterRName, "effective_time"),
+				),
+			},
+			{
+				Config: testAccDatasourceBillingOption_expectError,
+				ExpectError: regexp.MustCompile("Your query returned no results. " +
+					"Please change your search criteria and try again."),
+			},
+		},
+	})
+}
+
+const testAccDatasourceBillingOption_basic = `
+data "huaweicloud_cdn_billing_option" "test" {
+  product_type = "base"
+}
+
+data "huaweicloud_cdn_billing_option" "all_filter" {
+  product_type = "base"
+  status       = data.huaweicloud_cdn_billing_option.test.status
+  service_area = data.huaweicloud_cdn_billing_option.test.service_area
+}
+`
+
+const testAccDatasourceBillingOption_expectError = `
+data "huaweicloud_cdn_billing_option" "test" {
+  product_type = "base"
+}
+
+data "huaweicloud_cdn_billing_option" "expect_error" {
+  product_type = "base"
+  status       = data.huaweicloud_cdn_billing_option.test.status == "active" ? "upcoming" : "active"
+}
+`
