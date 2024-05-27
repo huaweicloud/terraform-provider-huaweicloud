c320a0370f3341ac127382c685e686e3630604f4
profoundwu <profoundwu@outlook.com>
2024-05-27 11:19:53 +0800

    fix(cfw): add pre-check function for some CFW test cases

--- huaweicloud/services/acceptance/cfw/data_source_huaweicloud_cfw_protection_rules_test.go
+++ huaweicloud/services/acceptance/cfw/data_source_huaweicloud_cfw_protection_rules_test.go
@@ -17,6 +17,7 @@ func TestAccDataSourceCfwProtectionRules_basic(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckCfw(t)
 		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
