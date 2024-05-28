48676619314b1c71253a72a313e2f085ff2ff212
houpeng80 <hp_monkey@163.com>
2024-05-28 17:04:42 +0800

    feat(RDS): add rds instance primary standby switch resource

---
+++ docs/resources/rds_primary_standby_switch.md
@@ -0,0 +1,49 @@
+---
+subcategory: "Relational Database Service (RDS)"
+layout: "huaweicloud"
+page_title: "HuaweiCloud: huaweicloud_rds_primary_standby_switch"
+description: |-
+  Manages an RDS instance primary standby switch resource within HuaweiCloud.
+---
+
+# huaweicloud_rds_primary_standby_switch
+
+Manages an RDS instance primary standby switch resource within HuaweiCloud.
+
+## Example Usage
+
+```hcl
+variable "instance_id" {}
+
+resource "huaweicloud_rds_primary_standby_switch" "test" {
+  instance_id = var.target_instance_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) The region in which to create the rds instance resource. If omitted, the
+  provider-level region will be used. Changing this creates a new resource.
+
+* `instance_id` - (Required, String, ForceNew) Specifies the ID of instance.
+
+  Changing this creates a new resource.
+
+* `force` - (Optional, Bool, ForceNew) Specifies whether to perform a forcible primary/standby switchover. This parameter
+  is valid only for the PostgreSQL DB engine.
+
+  Changing this creates a new resource.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attribute is exported:
+
+* `id` - The resource ID. The value is the instance ID.
+
+## Timeouts
+
+This resource provides the following timeouts configuration options:
+
+* `create` - Default is 30 minutes.
---
+++ huaweicloud/services/rds/resource_huaweicloud_rds_primary_standby_switch.go
@@ -0,0 +1,133 @@
+package rds
+
+import (
+	"context"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+// @API RDS PUT /v3/{project_id}/instances/{instance_id}/failover
+// @API RDS GET /v3/{project_id}/instances
+func ResourceRdsInstanceSwitch() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceRdsPrimaryStandbySwitchCreate,
+		ReadContext:   resourceRdsPrimaryStandbySwitchRead,
+		DeleteContext: resourceRdsPrimaryStandbySwitchDelete,
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(30 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"instance_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the ID of instance.`,
+			},
+			"force": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				ForceNew:    true,
+				Description: `Specifies whether to perform a forcible primary/standby switchover.`,
+			},
+		},
+	}
+}
+
+func resourceRdsPrimaryStandbySwitchCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		httpUrl = "v3/{project_id}/instances/{instance_id}/failover"
+		product = "rds"
+	)
+	client, err := cfg.NewServiceClient(product, region)
+	if err != nil {
+		return diag.Errorf("error creating RDS client: %s", err)
+	}
+
+	instanceId := d.Get("instance_id").(string)
+	createPath := client.Endpoint + httpUrl
+	createPath = strings.ReplaceAll(createPath, "{project_id}", client.ProjectID)
+	createPath = strings.ReplaceAll(createPath, "{instance_id}", instanceId)
+
+	createOpt := golangsdk.RequestOpts{KeepResponseBody: true}
+	createOpt.JSONBody = utils.RemoveNil(buildCreatePrimaryStandbySwitchBodyParams(d))
+
+	retryFunc := func() (interface{}, bool, error) {
+		res, err := client.Request("PUT", createPath, &createOpt)
+		retry, err := handleMultiOperationsError(err)
+		return res, retry, err
+	}
+	r, err := common.RetryContextWithWaitForState(&common.RetryContextWithWaitForStateParam{
+		Ctx:          ctx,
+		RetryFunc:    retryFunc,
+		WaitFunc:     rdsInstanceStateRefreshFunc(client, instanceId),
+		WaitTarget:   []string{"ACTIVE"},
+		Timeout:      d.Timeout(schema.TimeoutCreate),
+		DelayTimeout: 10 * time.Second,
+		PollInterval: 10 * time.Second,
+	})
+	if err != nil {
+		return diag.Errorf("error switching primary/standby RDS instance (%s): %s", instanceId, err)
+	}
+
+	createRespBody, err := utils.FlattenResponse(r.(*http.Response))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	jobId := utils.PathSearch("workflowId", createRespBody, nil)
+	if jobId == nil {
+		return diag.Errorf("unable to find the workflowId from the response: %s", err)
+	}
+
+	d.SetId(instanceId)
+
+	err = checkRDSInstanceJobFinish(client, jobId.(string), d.Timeout(schema.TimeoutCreate))
+	if err != nil {
+		return diag.Errorf("error waiting for switching primary/standby RDS instance(%s) to complete: %s",
+			instanceId, err)
+	}
+
+	return nil
+}
+
+func buildCreatePrimaryStandbySwitchBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"force": d.Get("force"),
+	}
+	return bodyParams
+}
+
+func resourceRdsPrimaryStandbySwitchRead(_ context.Context, _ *schema.ResourceData, _ interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceRdsPrimaryStandbySwitchDelete(_ context.Context, _ *schema.ResourceData, _ interface{}) diag.Diagnostics {
+	errorMsg := "Deleting primary standby switch is not supported. The restoration record is only removed from the state," +
+		" but it remains in the cloud. And the instance doesn't return to the state before switch."
+	return diag.Diagnostics{
+		diag.Diagnostic{
+			Severity: diag.Warning,
+			Summary:  errorMsg,
+		},
+	}
+}
