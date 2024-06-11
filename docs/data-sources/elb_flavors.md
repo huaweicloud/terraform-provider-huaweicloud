a172ace89353b2437b146beadd10fd2d47e72722
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2024-06-11 19:48:38 +0800

    feat(ELB): fix elb flavors (#4981)

--- docs/data-sources/elb_flavors.md
+++ docs/data-sources/elb_flavors.md
@@ -2,7 +2,8 @@
 subcategory: "Dedicated Load Balance (Dedicated ELB)"
 layout: "huaweicloud"
 page_title: "HuaweiCloud: huaweicloud_elb_flavors"
-description: ""
+description: |-
+  Use this data source to get the available ELB Flavors.
 ---
 
 # huaweicloud_elb_flavors
@@ -18,14 +19,6 @@ data "huaweicloud_elb_flavors" "flavors" {
   cps             = 2000
   bandwidth       = 50
 }
-
-# Create Dedicated Load Balancer with the first matched flavor
-
-resource "huaweicloud_elb_loadbalancer" "lb" {
-  l7_flavor_id = data.huaweicloud_elb_flavors.flavors.ids[0]
-
-  # Other properties...
-}
 ```
 
 ## Argument Reference
@@ -33,7 +26,15 @@ resource "huaweicloud_elb_loadbalancer" "lb" {
 * `region` - (Optional, String) The region in which to obtain the flavors. If omitted, the provider-level region will be
   used.
 
-* `type` - (Optional, String) Specifies the flavor type. Valid values are L4 and L7.
+* `type` - (Optional, String) Specifies the flavor type. Values options:
+  + **L4**: indicates Layer-4 flavor.
+  + **L7**: indicates Layer-7 flavor.
+  + **L4_elastic**: indicates minimum Layer-4 flavor for elastic scaling.
+  + **L7_elastic**: indicates minimum Layer-7 flavor for elastic scaling.
+  + **L4_elastic_max**: indicates maximum Layer-4 flavor for elastic scaling.
+  + **L7_elastic_max**: indicates maximum Layer-7 flavor for elastic scaling
+
+* `name` - (Optional, String) Specifies the flavor name.
 
 * `max_connections` - (Optional, Int) Specifies the maximum connections in the flavor.
 
@@ -52,10 +53,22 @@ In addition to all arguments above, the following attributes are exported:
 * `ids` - A list of flavor IDs.
 
 * `flavors` - A list of flavors. Each element contains the following attributes:
-  + `id` - ID of the flavor.
-  + `name` - Name of the flavor.
-  + `type` - Type of the flavor.
-  + `max_connections` - Maximum connections of the flavor.
-  + `cps` - Cps of the flavor.
-  + `qps` - Qps of the L7 flavor.
-  + `bandwidth` - Bandwidth size(Mbit/s) of the flavor.
+
+  The [flavors](#flavors_struct) structure is documented below.
+
+<a name="flavors_struct"></a>
+The `flavors` block supports:
+
+* `id` - ID of the flavor.
+
+* `name` - Name of the flavor.
+
+* `type` - Type of the flavor.
+
+* `max_connections` - Maximum connections of the flavor.
+
+* `cps` - Cps of the flavor.
+
+* `qps` - Qps of the L7 flavor.
+
+* `bandwidth` - Bandwidth size(Mbit/s) of the flavor.
--- huaweicloud/services/acceptance/elb/data_source_huaweicloud_elb_flavors_test.go
+++ huaweicloud/services/acceptance/elb/data_source_huaweicloud_elb_flavors_test.go
@@ -1,47 +1,120 @@
 package elb
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
 func TestAccElbFlavorsDataSource_basic(t *testing.T) {
+	dataSource := "data.huaweicloud_elb_flavors.test"
+	dc := acceptance.InitDataSourceCheck(dataSource)
+
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccElbFlavorsDataSource_basic,
+				Config: testAccElbFlavorsDataSource_basic(),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckElbFlavorDataSourceID("data.huaweicloud_elb_flavors.this"),
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "flavors.#"),
+					resource.TestCheckResourceAttrSet(dataSource, "flavors.0.id"),
+					resource.TestCheckResourceAttrSet(dataSource, "flavors.0.name"),
+					resource.TestCheckResourceAttrSet(dataSource, "flavors.0.max_connections"),
+					resource.TestCheckResourceAttrSet(dataSource, "flavors.0.cps"),
+					resource.TestCheckResourceAttrSet(dataSource, "flavors.0.qps"),
+					resource.TestCheckResourceAttrSet(dataSource, "flavors.0.bandwidth"),
+					resource.TestCheckResourceAttrSet(dataSource, "ids.#"),
+
+					resource.TestCheckOutput("type_filter_is_useful", "true"),
+					resource.TestCheckOutput("name_filter_is_useful", "true"),
+					resource.TestCheckOutput("max_connections_filter_is_useful", "true"),
+					resource.TestCheckOutput("cps_filter_is_useful", "true"),
+					resource.TestCheckOutput("qps_filter_is_useful", "true"),
+					resource.TestCheckOutput("bandwidth_filter_is_useful", "true"),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckElbFlavorDataSourceID(n string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("can't find elb flavors data source: %s", n)
-		}
+func testAccElbFlavorsDataSource_basic() string {
+	return `
+data "huaweicloud_elb_flavors" "test" {}
 
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("ELB Flavors data source ID not set")
-		}
-
-		return nil
-	}
+locals {
+  type = "L4"
+}
+data "huaweicloud_elb_flavors" "type_filter" {
+  type = "L4"
+}
+output "type_filter_is_useful" {
+  value = length(data.huaweicloud_elb_flavors.type_filter.flavors) > 0 && alltrue(
+  [for v in data.huaweicloud_elb_flavors.type_filter.flavors[*].type : v == local.type]
+  )
 }
 
-const testAccElbFlavorsDataSource_basic = `
-data "huaweicloud_elb_flavors" "this" {
-  type = "L7"
+locals {
+  name = "L4_flavor.elb.s2.medium"
+}
+data "huaweicloud_elb_flavors" "name_filter" {
+  name = "L4_flavor.elb.s2.medium"
+}
+output "name_filter_is_useful" {
+  value = length(data.huaweicloud_elb_flavors.name_filter.flavors) > 0 && alltrue(
+  [for v in data.huaweicloud_elb_flavors.name_filter.flavors[*].name : v == local.name]
+  )
+}
+
+locals {
+  max_connections = 4000000
+}
+data "huaweicloud_elb_flavors" "max_connections_filter" {
+  max_connections = 4000000
+}
+output "max_connections_filter_is_useful" {
+  value = length(data.huaweicloud_elb_flavors.max_connections_filter.flavors) > 0 && alltrue(
+  [for v in data.huaweicloud_elb_flavors.max_connections_filter.flavors[*].max_connections : v == local.max_connections]
+  )
+}
+
+locals {
+  cps = 10000
+}
+data "huaweicloud_elb_flavors" "cps_filter" {
+  cps = 10000
+}
+output "cps_filter_is_useful" {
+  value = length(data.huaweicloud_elb_flavors.cps_filter.flavors) > 0 && alltrue(
+  [for v in data.huaweicloud_elb_flavors.cps_filter.flavors[*].cps : v == local.cps]
+  )
+}
+
+locals {
+  qps = 80000
+}
+data "huaweicloud_elb_flavors" "qps_filter" {
+  qps = 80000
+}
+output "qps_filter_is_useful" {
+  value = length(data.huaweicloud_elb_flavors.qps_filter.flavors) > 0 && alltrue(
+  [for v in data.huaweicloud_elb_flavors.qps_filter.flavors[*].qps : v == local.qps]
+  )
+}
+
+locals {
+  bandwidth = 1000
+}
+data "huaweicloud_elb_flavors" "bandwidth_filter" {
+  bandwidth = 1000
+}
+output "bandwidth_filter_is_useful" {
+  value = length(data.huaweicloud_elb_flavors.bandwidth_filter.flavors) > 0 && alltrue(
+  [for v in data.huaweicloud_elb_flavors.bandwidth_filter.flavors[*].bandwidth : v == local.bandwidth]
+  )
 }
 `
+}
