f68a47a8d840e6d5e55219949b1e44036b9279b7
ruwenqiang123 <ruwenqiang123@outlook.com>
2024-06-17 11:06:33 +0800

    feat(as): add a datasource to get list of AS instances hanging information (#5009)

---
+++ docs/data-sources/as_hook_instances.md
@@ -0,0 +1,69 @@
+---
+subcategory: "Auto Scaling"
+layout: "huaweicloud"
+page_title: "HuaweiCloud: huaweicloud_as_hook_instances"
+description: |-
+  Use this data source to get a list of AS instances hanging information.
+---
+
+# huaweicloud_as_hook_instances
+
+Use this data source to get a list of AS instances hanging information.
+
+## Example Usage
+
+```hcl
+variable "scaling_group_id" {}
+variable "instance_id" {}
+
+data "huaweicloud_as_hook_instances" "test" {
+  scaling_group_id = var.scaling_group_id
+  instance_id      = var.instance_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the resource.
+  If omitted, the provider-level region will be used.
+
+* `scaling_group_id` - (Required, String) Specifies the ID of the AS group to which the AS instances belong.
+
+* `instance_id` - (Optional, String) Specifies the ID of the AS instance.
+
+* `lifecycle_hook_name` - (Optional, String) Specifies the name of the lifecycle hook.
+
+* `lifecycle_hook_status` - (Optional, String) Specifies the status of the lifecycle hook.
+  The valid values are as follows:
+  + **HANGING**: Suspends the instance.
+  + **CONTINUE**: Continues the instance.
+  + **ABANDON**: Terminates the instance.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `instance_hanging_info` - All lifecycle hook information about the AS instances.
+
+  The [instance_hanging_info](#instance_hanging_info_struct) structure is documented below.
+
+<a name="instance_hanging_info_struct"></a>
+The `instance_hanging_info` block supports:
+
+* `scaling_group_id` - The ID of the AS group to which the AS instance belongs.
+
+* `instance_id` - The ID of the AS instance.
+
+* `lifecycle_hook_name` - The name of the lifecycle hook.
+
+* `lifecycle_hook_status` - The status of the lifecycle hook.
+
+* `lifecycle_action_key` - The lifecycle action key, which determines the lifecycle callback object.
+
+* `default_result` - The default lifecycle hook callback operation.
+
+* `timeout` - The timeout duration, in RFC3339 format.
---
+++ huaweicloud/services/acceptance/as/data_source_huaweicloud_as_hook_instances_test.go
@@ -0,0 +1,128 @@
+package as
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+// Before running this test case, you need to prepare an AS group ID with hook instances.
+func TestAccDataSourceHookInstances_basic(t *testing.T) {
+	var (
+		dataSource = "data.huaweicloud_as_hook_instances.test"
+		dc         = acceptance.InitDataSourceCheck(dataSource)
+
+		byInstanceId   = "data.huaweicloud_as_hook_instances.filter_by_instance_id"
+		dcByInstanceId = acceptance.InitDataSourceCheck(byInstanceId)
+
+		byLifecycleHookName   = "data.huaweicloud_as_hook_instances.filter_by_name"
+		dcByLifecycleHookName = acceptance.InitDataSourceCheck(byLifecycleHookName)
+
+		byLifecycleHookStatus   = "data.huaweicloud_as_hook_instances.filter_by_status"
+		dcByLifecycleHookStatus = acceptance.InitDataSourceCheck(byLifecycleHookStatus)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckASScalingGroupID(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceHookInstances_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "instance_hanging_info.#"),
+					resource.TestCheckResourceAttrSet(dataSource, "instance_hanging_info.0.scaling_group_id"),
+					resource.TestCheckResourceAttrSet(dataSource, "instance_hanging_info.0.instance_id"),
+					resource.TestCheckResourceAttrSet(dataSource, "instance_hanging_info.0.lifecycle_hook_name"),
+					resource.TestCheckResourceAttrSet(dataSource, "instance_hanging_info.0.lifecycle_hook_status"),
+					resource.TestCheckResourceAttrSet(dataSource, "instance_hanging_info.0.lifecycle_action_key"),
+					resource.TestCheckResourceAttrSet(dataSource, "instance_hanging_info.0.default_result"),
+					resource.TestCheckResourceAttrSet(dataSource, "instance_hanging_info.0.timeout"),
+
+					dcByInstanceId.CheckResourceExists(),
+					resource.TestCheckOutput("instance_id_filter_is_useful", "true"),
+
+					dcByLifecycleHookName.CheckResourceExists(),
+					resource.TestCheckOutput("lifecycle_hook_name_filter_is_useful", "true"),
+
+					dcByLifecycleHookStatus.CheckResourceExists(),
+					resource.TestCheckOutput("lifecycle_hook_status_filter_is_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testDataSourceHookInstances_basic() string {
+	return fmt.Sprintf(`
+data "huaweicloud_as_hook_instances" "test" {
+  scaling_group_id = "%[1]s"
+}
+
+locals {
+  instance_id = data.huaweicloud_as_hook_instances.test.instance_hanging_info[0].instance_id
+}
+
+data "huaweicloud_as_hook_instances" "filter_by_instance_id" {
+  scaling_group_id = "%[1]s"
+  instance_id      = local.instance_id
+}
+
+locals {
+  instance_id_filter_result = [
+    for v in data.huaweicloud_as_hook_instances.filter_by_instance_id.instance_hanging_info[*].instance_id : 
+    v == local.instance_id
+  ]
+}
+
+output "instance_id_filter_is_useful" {
+  value = alltrue(local.instance_id_filter_result) && length(local.instance_id_filter_result) > 0
+}
+
+locals {
+  lifecycle_hook_name = data.huaweicloud_as_hook_instances.test.instance_hanging_info[0].lifecycle_hook_name
+}
+
+data "huaweicloud_as_hook_instances" "filter_by_name" {
+  scaling_group_id    = "%[1]s"
+  lifecycle_hook_name = local.lifecycle_hook_name
+}
+
+locals {
+  lifecycle_hook_name_filter_result = [
+    for v in data.huaweicloud_as_hook_instances.filter_by_name.instance_hanging_info[*].lifecycle_hook_name : 
+    v == local.lifecycle_hook_name
+  ]
+}
+
+output "lifecycle_hook_name_filter_is_useful" {
+  value = alltrue(local.lifecycle_hook_name_filter_result) && length(local.lifecycle_hook_name_filter_result) > 0
+}
+
+locals {
+  lifecycle_hook_status = data.huaweicloud_as_hook_instances.test.instance_hanging_info[0].lifecycle_hook_status
+}
+
+data "huaweicloud_as_hook_instances" "filter_by_status" {
+  scaling_group_id      = "%[1]s"
+  lifecycle_hook_status = local.lifecycle_hook_status
+}
+
+locals {
+  lifecycle_hook_status_filter_result = [
+    for v in data.huaweicloud_as_hook_instances.filter_by_status.instance_hanging_info[*].lifecycle_hook_status : 
+    v == local.lifecycle_hook_status
+  ]
+}
+
+output "lifecycle_hook_status_filter_is_useful" {
+  value = alltrue(local.lifecycle_hook_status_filter_result) && length(local.lifecycle_hook_status_filter_result) > 0
+}
+`, acceptance.HW_AS_SCALING_GROUP_ID)
+}
