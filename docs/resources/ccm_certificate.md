88641dac3d0af876d05c867da06e03ffc03cbed8
allium <deerhang@163.com>
2024-06-25 15:54:33 +0800

    feat(CCM): CCM support new resource to buy SSL certificate (#5058)

---
+++ docs/resources/ccm_certificate.md
@@ -0,0 +1,213 @@
+---
+subcategory: "Cloud Certificate Manager (CCM)"
+layout: "huaweicloud"
+page_title: "HuaweiCloud: huaweicloud_ccm_certificate"
+description: |-
+  Manages a CCM SSL certificate resource within HuaweiCloud.
+---
+
+# huaweicloud_ccm_certificate
+
+Manages a CCM SSL certificate resource within HuaweiCloud.
+
+-> Refer to [document](https://support.huaweicloud.com/intl/en-us/productdesc-ccm/ccm_01_0219.html) to see the
+differences between different types of certificates.
+
+## Example Usage
+
+### Single Domain Certificate
+
+```hcl
+variable "cert_brand" {}
+variable "cert_type" {}
+
+resource "huaweicloud_ccm_certificate" "test" {
+  cert_brand     = var.cert_brand
+  cert_type      = var.cert_type
+  domain_type    = "SINGLE_DOMAIN"
+  effective_time = 1
+  domain_numbers = 1
+}
+```
+
+### Multi Domain Certificate
+
+```hcl
+variable "cert_brand" {}
+variable "cert_type" {}
+
+resource "huaweicloud_ccm_certificate" "test" {
+  cert_brand             = var.cert_brand
+  cert_type              = var.cert_type
+  domain_type            = "MULTI_DOMAIN"
+  effective_time         = 1
+  domain_numbers         = 4
+  primary_domain_type    = "SINGLE_DOMAIN"
+  single_domain_number   = 1
+  wildcard_domain_number = 2
+}
+```
+
+### Wildcard Domain Certificate
+
+```hcl
+variable "cert_brand" {}
+variable "cert_type" {}
+
+resource "huaweicloud_ccm_certificate" "test" {
+  cert_brand     = var.cert_brand
+  cert_type      = var.cert_type
+  domain_type    = "WILDCARD"
+  effective_time = 1
+  domain_numbers = 1
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region in which to create the resource.
+  If omitted, the provider-level region will be used. Changing this parameter will create a new resource.
+
+* `cert_brand` - (Required, String, ForceNew) Specifies the certificate authority. Valid values are **GEOTRUST**,
+  **GLOBALSIGN**, **SYMANTEC**, **CFCA**, **TRUSTASIA**, and **VTRUS**.
+
+  Changing this parameter will create a new resource.
+
+* `cert_type` - (Required, String, ForceNew) Specifies the certificate type. Valid values are **DV_SSL_CERT**,
+  **DV_SSL_CERT_BASIC**, **EV_SSL_CERT**, **EV_SSL_CERT_PRO**, **OV_SSL_CERT**, and **OV_SSL_CERT_PRO**.
+
+  Changing this parameter will create a new resource.
+
+* `domain_type` - (Required, String, ForceNew) Specifies the type of domain name. Valid values are **SINGLE_DOMAIN**,
+  **MULTI_DOMAIN**, and **WILDCARD**.
+
+  Changing this parameter will create a new resource.
+
+* `effective_time` - (Required, Int, ForceNew) Specifies the validity period (year). Valid values are `1`, `2`, and `3`.
+
+  Changing this parameter will create a new resource.
+
+* `domain_numbers` - (Required, Int, ForceNew) Specifies the quantity of domain name.
+  + When `domain_type` is set to **SINGLE_DOMAIN** or **WILDCARD**, this field can only be set to `1`.
+  + When `domain_type` is set to **MULTI_DOMAIN**, the value of this field ranges from `2` to `250`. The value of this
+    field should be the number of additional domain names plus one main domain name.
+    For example, if field `single_domain_number` is set to `1`, and field `wildcard_domain_number` is set to `2`, then
+    the value of this field should be `4`.
+
+  Changing this parameter will create a new resource.
+
+* `primary_domain_type` - (Optional, String, ForceNew) Specifies the type of primary domain name in multiple domains.
+  Valid values are **SINGLE_DOMAIN** and **WILDCARD_DOMAIN**.
+
+  Changing this parameter will create a new resource.
+
+* `single_domain_number` - (Optional, Int, ForceNew) Specifies the number of additional single domain names.
+  The value of this field ranges from `1` to `249`.
+
+  Changing this parameter will create a new resource.
+
+* `wildcard_domain_number` - (Optional, Int, ForceNew) Specifies the number of additional wildcard domain names.
+  The value of this field ranges from `0` to `248`.
+
+  Changing this parameter will create a new resource.
+
+-> Fields `primary_domain_type`, `single_domain_number`, and `wildcard_domain_number` are required when `domain_type`
+is set to **MULTI_DOMAIN**.
+
+* `enterprise_project_id` - (Optional, String) Specifies the enterprise project ID.
+  For enterprise users, if omitted, default enterprise project will be used.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID, also the SSL certificate ID.
+
+* `validity_period` - The validity period (month).
+
+* `status` - The certificate status. Valid values are:
+  + **PAID**: The certificate has been paid and the certificate is waiting to be applied for.
+  + **ISSUED**: The certificate has been issued.
+  + **CHECKING**: The certificate application is under review.
+  + **CANCELCHECKING**: The certificate cancellation application is under review.
+  + **UNPASSED**: The certificate application failed.
+  + **EXPIRED**: The certificate has expired.
+  + **REVOKING**: The certificate revocation application is under review.
+  + **REVOKED**: The certificate has been revoked.
+  + **UPLOAD**: The certificate in custody.
+  + **SUPPLEMENTCHECKING**: Additional domain names added to multi-domain certificates are under review.
+  + **CANCELSUPPLEMENTING**: Cancel the addition of additional domain names under review.
+
+* `order_id` - The order ID.
+
+* `name` - The certificate name.
+
+* `push_support` - Whether the certificate supports push.
+
+* `revoke_reason` - The reason for certificate revocation.
+
+* `signature_algorithm` - The signature algorithm.
+
+* `issue_time` - The certificate issuance time.
+
+* `not_before` - The certificate validity time.
+
+* `not_after` - The certificate expiration time.
+
+* `validation_method` - The authentication method of domain name.
+
+* `domain` - The domain name bound to the certificate.
+
+* `sans` - The information of additional domain name for the bound certificate.
+
+* `fingerprint` - The SHA-1 fingerprint of the certificate.
+
+* `authentification` - The ownership certification information of domain name.
+  The [authentification](#CCMCertificate_authentification) structure is documented below.
+
+<a name="CCMCertificate_authentification"></a>
+The `authentification` block supports:
+
+* `record_name` - The name of the domain name check value.
+
+* `record_type` - The type of the domain name check value.
+
+* `record_value` - The domain name check value.
+
+* `domain` - The domain name corresponding to the check value.
+
+## Timeouts
+
+This resource provides the following timeouts configuration options:
+
+* `create` - Default is 10 minutes.
+* `update` - Default is 10 minutes.
+* `delete` - Default is 10 minutes.
+
+## Import
+
+The CCM certificate can be imported using the `id`, e.g.
+
+```bash
+$ terraform import huaweicloud_ccm_certificate.test <id>
+```
+
+Note that the imported state may not be identical to your resource definition, due to some attributes missing from the
+API response, security or some other reason. The missing attributes include: `effective_time`, `single_domain_number`.
+It is generally recommended running `terraform plan` after importing a resource.
+You can then decide if changes should be applied to the resource, or the resource definition
+should be updated to align with the resource. Also, you can ignore changes as below.
+
+```hcl
+resource "huaweicloud_ccm_certificate" "test" { 
+  ...
+
+  lifecycle {
+    ignore_changes = [
+      effective_time, single_domain_number,
+    ]
+  }
+}
+```
---
+++ huaweicloud/services/ccm/resource_huaweicloud_ccm_certificate.go
@@ -0,0 +1,522 @@
+// ---------------------------------------------------------------
+// *** AUTO GENERATED CODE ***
+// @Product CCM
+// ---------------------------------------------------------------
+
+package ccm
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/jmespath/go-jmespath"
+
+	"github.com/chnsz/golangsdk"
+	"github.com/chnsz/golangsdk/openstack/eps/v1/enterpriseprojects"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+// @API CCM POST /v3/scm/certificates/buy
+// @API CCM GET /v3/scm/certificates/{certificate_id}
+// @API CCM DELETE /v3/scm/certificates/{cert_id}/unsubscribe
+// @API EPS POST /v1.0/enterprise-projects/{enterprise_project_id}/resources-migrate
+func ResourceCCMCertificate() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceCCMCertificateCreate,
+		UpdateContext: resourceCCMCertificateUpdate,
+		ReadContext:   resourceCCMCertificateRead,
+		DeleteContext: resourceCCMCertificateDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(10 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"cert_brand": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the certificate authority.`,
+			},
+			"cert_type": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the certificate type.`,
+			},
+			"domain_type": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the type of domain name.`,
+			},
+			"effective_time": {
+				Type:        schema.TypeInt,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the validity period (year).`,
+			},
+			"domain_numbers": {
+				Type:        schema.TypeInt,
+				Required:    true,
+				ForceNew:    true,
+				Description: `Specifies the quantity of domain name.`,
+			},
+			"primary_domain_type": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				ForceNew:    true,
+				Description: `Specifies the type of primary domain name in multiple domains.`,
+			},
+			"single_domain_number": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Computed:    true,
+				ForceNew:    true,
+				Description: `Specifies the number of additional single domain names.`,
+			},
+			"wildcard_domain_number": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Computed:    true,
+				ForceNew:    true,
+				Description: `Specifies the number of additional wildcard domain names.`,
+			},
+			"enterprise_project_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				Description: `Specifies the enterprise project ID.`,
+			},
+			"validity_period": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: `The validity period (month).`,
+			},
+			"status": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The certificate status.`,
+			},
+			"order_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The order ID.`,
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The certificate name.`,
+			},
+			"push_support": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `Whether the certificate supports push.`,
+			},
+			"revoke_reason": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The reason for certificate revocation.`,
+			},
+			"signature_algorithm": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The signature algorithm.`,
+			},
+			"issue_time": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The certificate issuance time.`,
+			},
+			"not_before": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The certificate validity time.`,
+			},
+			"not_after": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The certificate expiration time.`,
+			},
+			"validation_method": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The authentication method of domain name.`,
+			},
+			"domain": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The domain name bound to the certificate.`,
+			},
+			"sans": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The information of additional domain name for the bound certificate.`,
+			},
+			"fingerprint": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The SHA-1 fingerprint of the certificate.`,
+			},
+			"authentification": {
+				Type:        schema.TypeList,
+				Elem:        authentificationSchema(),
+				Computed:    true,
+				Description: `The ownership certification information of domain name.`,
+			},
+		},
+	}
+}
+
+func authentificationSchema() *schema.Resource {
+	sc := schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"record_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The name of the domain name check value.`,
+			},
+			"record_type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The type of the domain name check value.`,
+			},
+			"record_value": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The domain name check value.`,
+			},
+			"domain": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The domain name corresponding to the check value.`,
+			},
+		},
+	}
+	return &sc
+}
+
+// The certificate status can only be used normally after it changes to `PAID`.
+func waitingForCCMCertificatePaid(ctx context.Context, client *golangsdk.ServiceClient, certID string,
+	timeout time.Duration) error {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{"PENDING"},
+		Target:  []string{"COMPLETED"},
+		Refresh: func() (interface{}, string, error) {
+			getRespBody, err := ReadCCMCertificate(client, certID)
+			if err != nil {
+				return nil, "ERROR", err
+			}
+
+			status := utils.PathSearch("status", getRespBody, "").(string)
+			if status == "" {
+				return nil, "ERROR", fmt.Errorf("status is not found in API response")
+			}
+
+			if status == "PAID" {
+				return "success", "COMPLETED", nil
+			}
+
+			return getRespBody, "PENDING", nil
+		},
+		Timeout:      timeout,
+		Delay:        10 * time.Second,
+		PollInterval: 10 * time.Second,
+	}
+
+	_, err := stateConf.WaitForStateContext(ctx)
+	return err
+}
+
+func resourceCCMCertificateCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg     = meta.(*config.Config)
+		region  = cfg.GetRegion(d)
+		httpUrl = "v3/scm/certificates/buy"
+		product = "scm"
+	)
+
+	client, err := cfg.NewServiceClient(product, region)
+	if err != nil {
+		return diag.Errorf("error creating CCM client: %s", err)
+	}
+
+	createPath := client.Endpoint + httpUrl
+	createOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		JSONBody:         utils.RemoveNil(buildCreateCCMCertificateBodyParams(d, cfg)),
+	}
+
+	createResp, err := client.Request("POST", createPath, &createOpt)
+	if err != nil {
+		return diag.Errorf("error creating CCM certificate: %s", err)
+	}
+
+	createRespBody, err := utils.FlattenResponse(createResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	id, err := jmespath.Search("cert|[0].cert_id", createRespBody)
+	if err != nil || id == nil {
+		return diag.Errorf("error creating CCM certificate: ID is not found in API response")
+	}
+	d.SetId(id.(string))
+
+	if err := waitingForCCMCertificatePaid(ctx, client, d.Id(), d.Timeout(schema.TimeoutCreate)); err != nil {
+		return diag.Errorf("error waiting for CCM certificate (%s) creation to PAID: %s", d.Id(), err)
+	}
+
+	return resourceCCMCertificateRead(ctx, d, meta)
+}
+
+func buildCreateCCMCertificateBodyParams(d *schema.ResourceData, cfg *config.Config) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"cert_brand":               d.Get("cert_brand"),
+		"cert_type":                d.Get("cert_type"),
+		"domain_type":              d.Get("domain_type"),
+		"effective_time":           d.Get("effective_time"),
+		"domain_numbers":           d.Get("domain_numbers"),
+		"primary_domain_type":      utils.ValueIgnoreEmpty(d.Get("primary_domain_type")),
+		"single_domain_number":     utils.ValueIgnoreEmpty(d.Get("single_domain_number")),
+		"wildcard_domain_number":   utils.ValueIgnoreEmpty(d.Get("wildcard_domain_number")),
+		"enterprise_project_id":    utils.ValueIgnoreEmpty(cfg.GetEnterpriseProjectID(d)),
+		"order_number":             1,
+		"agree_privacy_protection": true,
+		"is_auto_pay":              true,
+	}
+	return bodyParams
+}
+
+func flattenPrimaryDomainTypeAttribute(respBody interface{}) string {
+	multiDomainType := utils.PathSearch("multi_domain_type", respBody, "").(string)
+	switch multiDomainType {
+	case "primary_single":
+		return "SINGLE_DOMAIN"
+	case "primary_wildcard":
+		return "WILDCARD_DOMAIN"
+	}
+
+	return ""
+}
+
+func flattenAuthentification(resp interface{}) []interface{} {
+	if resp == nil {
+		return nil
+	}
+	curJson := utils.PathSearch("authentification", resp, make([]interface{}, 0))
+	curArray := curJson.([]interface{})
+	rst := make([]interface{}, 0, len(curArray))
+	for _, v := range curArray {
+		rst = append(rst, map[string]interface{}{
+			"record_name":  utils.PathSearch("record_name", v, nil),
+			"record_type":  utils.PathSearch("record_type", v, nil),
+			"record_value": utils.PathSearch("record_value", v, nil),
+			"domain":       utils.PathSearch("domain", v, nil),
+		})
+	}
+	return rst
+}
+
+// ReadCCMCertificate Test cases use this method, so the first letter is capitalized
+func ReadCCMCertificate(client *golangsdk.ServiceClient, certID string) (interface{}, error) {
+	getPath := client.Endpoint + "v3/scm/certificates/{certificate_id}"
+	getPath = strings.ReplaceAll(getPath, "{certificate_id}", certID)
+	getOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+
+	getResp, err := client.Request("GET", getPath, &getOpt)
+	if err != nil {
+		return nil, err
+	}
+
+	return utils.FlattenResponse(getResp)
+}
+
+func resourceCCMCertificateRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg     = meta.(*config.Config)
+		region  = cfg.GetRegion(d)
+		mErr    *multierror.Error
+		product = "scm"
+	)
+
+	client, err := cfg.NewServiceClient(product, region)
+	if err != nil {
+		return diag.Errorf("error creating CCM client: %s", err)
+	}
+
+	getRespBody, err := ReadCCMCertificate(client, d.Id())
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "error retrieving CCM certificate")
+	}
+
+	mErr = multierror.Append(
+		mErr,
+		d.Set("region", region),
+		d.Set("cert_brand", utils.PathSearch("brand", getRespBody, nil)),
+		d.Set("cert_type", utils.PathSearch("type", getRespBody, nil)),
+		d.Set("domain_type", utils.PathSearch("domain_type", getRespBody, nil)),
+		d.Set("validity_period", utils.PathSearch("validity_period", getRespBody, nil)),
+		d.Set("domain_numbers", utils.PathSearch("domain_count", getRespBody, nil)),
+		d.Set("primary_domain_type", flattenPrimaryDomainTypeAttribute(getRespBody)),
+		d.Set("wildcard_domain_number", utils.PathSearch("wildcard_count", getRespBody, nil)),
+		d.Set("enterprise_project_id", utils.PathSearch("enterprise_project_id", getRespBody, nil)),
+		d.Set("status", utils.PathSearch("status", getRespBody, nil)),
+		d.Set("order_id", utils.PathSearch("order_id", getRespBody, nil)),
+		d.Set("name", utils.PathSearch("name", getRespBody, nil)),
+		d.Set("push_support", utils.PathSearch("push_support", getRespBody, nil)),
+		d.Set("revoke_reason", utils.PathSearch("revoke_reason", getRespBody, nil)),
+		d.Set("signature_algorithm", utils.PathSearch("signature_algorithm", getRespBody, nil)),
+		d.Set("issue_time", utils.PathSearch("issue_time", getRespBody, nil)),
+		d.Set("not_before", utils.PathSearch("not_before", getRespBody, nil)),
+		d.Set("not_after", utils.PathSearch("not_after", getRespBody, nil)),
+		d.Set("validation_method", utils.PathSearch("validation_method", getRespBody, nil)),
+		d.Set("domain", utils.PathSearch("domain", getRespBody, nil)),
+		d.Set("sans", utils.PathSearch("sans", getRespBody, nil)),
+		d.Set("fingerprint", utils.PathSearch("fingerprint", getRespBody, nil)),
+		d.Set("authentification", flattenAuthentification(getRespBody)),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func resourceCCMCertificateUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg    = meta.(*config.Config)
+		region = cfg.GetRegion(d)
+	)
+
+	if d.HasChange("enterprise_project_id") {
+		migrateOpts := enterpriseprojects.MigrateResourceOpts{
+			ResourceId:   d.Id(),
+			ResourceType: "scm",
+			RegionId:     region,
+			ProjectId:    cfg.GetProjectID(region),
+		}
+		if err := common.MigrateEnterpriseProject(ctx, cfg, d, migrateOpts); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	return resourceCCMCertificateRead(ctx, d, meta)
+}
+
+func waitingForCCMCertificateDeleted(ctx context.Context, client *golangsdk.ServiceClient, certID string,
+	timeout time.Duration) error {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{"PENDING"},
+		Target:  []string{"COMPLETED"},
+		Refresh: func() (interface{}, string, error) {
+			getRespBody, err := ReadCCMCertificate(client, certID)
+			if err != nil {
+				var errDefault404 golangsdk.ErrDefault404
+				if errors.As(err, &errDefault404) {
+					return "success", "COMPLETED", nil
+				}
+				return nil, "ERROR", err
+			}
+
+			return getRespBody, "PENDING", nil
+		},
+		Timeout:      timeout,
+		Delay:        20 * time.Second,
+		PollInterval: 10 * time.Second,
+	}
+
+	_, err := stateConf.WaitForStateContext(ctx)
+	return err
+}
+
+// buildDeleteRetryFunc When the time interval between creation and deletion is too short, deletion will fail,
+// so deletion needs to be retried.
+// An example of the response body for deletion retry is as follows: {"error_code": "SCM.0016","error_msg": "订单异常"}
+func buildDeleteRetryFunc(client *golangsdk.ServiceClient, deletePath string, deleteOpt *golangsdk.RequestOpts) common.RetryFunc {
+	retryFunc := func() (interface{}, bool, error) {
+		deleteResp, err := client.Request("DELETE", deletePath, deleteOpt)
+		if err != nil {
+			var errCode golangsdk.ErrDefault500
+			if errors.As(err, &errCode) {
+				var apiError interface{}
+				if jsonErr := json.Unmarshal(errCode.Body, &apiError); jsonErr != nil {
+					return nil, false, err
+				}
+
+				errorCode := utils.PathSearch("error_code", apiError, "").(string)
+				if errorCode == "SCM.0016" {
+					return nil, true, err
+				}
+			}
+			return nil, false, err
+		}
+
+		deleteRespBody, err := utils.FlattenResponse(deleteResp)
+		return deleteRespBody, false, err
+	}
+	return retryFunc
+}
+
+func resourceCCMCertificateDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg     = meta.(*config.Config)
+		region  = cfg.GetRegion(d)
+		httpUrl = "v3/scm/certificates/{cert_id}/unsubscribe"
+		product = "scm"
+	)
+	client, err := cfg.NewServiceClient(product, region)
+	if err != nil {
+		return diag.Errorf("error creating CCM client: %s", err)
+	}
+
+	deletePath := client.Endpoint + httpUrl
+	deletePath = strings.ReplaceAll(deletePath, "{cert_id}", d.Id())
+	deleteOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+
+	deleteRespBody, err := common.RetryContextWithWaitForState(&common.RetryContextWithWaitForStateParam{
+		Ctx:          ctx,
+		RetryFunc:    buildDeleteRetryFunc(client, deletePath, &deleteOpt),
+		Timeout:      d.Timeout(schema.TimeoutDelete),
+		DelayTimeout: 10 * time.Second,
+		PollInterval: 10 * time.Second,
+	})
+
+	if err != nil {
+		return diag.Errorf("error deleting CCM certificate: %s", err)
+	}
+
+	unsubscribeResult := utils.PathSearch("unsubscribe_results", deleteRespBody, "").(string)
+	if unsubscribeResult != "SUCCESS" {
+		return diag.Errorf("error deleting CCM certificate: Unsubscribe result is not SUCCESS in deletion API response")
+	}
+
+	if err := waitingForCCMCertificateDeleted(ctx, client, d.Id(), d.Timeout(schema.TimeoutDelete)); err != nil {
+		return diag.Errorf("error waiting for CCM certificate (%s) deleted: %s", d.Id(), err)
+	}
+
+	return nil
+}
