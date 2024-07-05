270c65919f92096cce2224692616e5fb2920f345
Jason Zhang <jasonzhang9309@163.com>
2024-07-05 15:34:56 +0800

    test(iam): improve iam test cases

--- huaweicloud/services/acceptance/iam/resource_huaweicloud_identity_user_token_test.go
+++ huaweicloud/services/acceptance/iam/resource_huaweicloud_identity_user_token_test.go
@@ -36,12 +36,50 @@ func TestAccIdentityUserToken_basic(t *testing.T) {
 
 func testAccIdentityUserToken_basic(userName, initPassword string) string {
 	return fmt.Sprintf(`
-%s
+%[1]s
 
 resource "huaweicloud_identity_user_token" "test" {
-  account_name = "%s"
+  account_name = "%[2]s"
   user_name    = huaweicloud_identity_user.user_1.name
-  password     = "%s"
+  password     = "%[3]s"
 }
 `, testAccIdentityUser_basic(userName, initPassword), acceptance.HW_DOMAIN_NAME, initPassword)
+}
+
+func TestAccIdentityUserToken_project(t *testing.T) {
+	userName := acceptance.RandomAccResourceName()
+	initPassword := acceptance.RandomPassword()
+	resourceName := "huaweicloud_identity_user_token.test"
+
+	// Avoid CheckDestroy because the token can not be destroyed.
+	// lintignore:AT001
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckAdminOnly(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIdentityUserToken_project(userName, initPassword),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "token"),
+					resource.TestCheckResourceAttrSet(resourceName, "expires_at"),
+				),
+			},
+		},
+	})
+}
+
+func testAccIdentityUserToken_project(userName, initPassword string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_identity_user_token" "test" {
+  account_name = "%[2]s"
+  user_name    = huaweicloud_identity_user.user_1.name
+  password     = "%[3]s"
+  project_name = "%[4]s"
+}
+`, testAccIdentityUser_basic(userName, initPassword), acceptance.HW_DOMAIN_NAME, initPassword, acceptance.HW_REGION_NAME)
 }
