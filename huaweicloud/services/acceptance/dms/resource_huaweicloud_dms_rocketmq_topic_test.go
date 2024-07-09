99701fbf53ef63a9851af6fbdafe59945486bbe0
saf3dfsa <caiwanting@huawei.com>
2024-07-09 16:41:26 +0800

    feat(dms): support new params for rocketmq topic

--- huaweicloud/services/acceptance/dms/resource_huaweicloud_dms_rocketmq_topic_test.go
+++ huaweicloud/services/acceptance/dms/resource_huaweicloud_dms_rocketmq_topic_test.go
@@ -101,6 +101,80 @@ func TestAccDmsRocketMQTopic_basic(t *testing.T) {
 	})
 }
 
+func TestAccDmsRocketMQTopic_withQueues(t *testing.T) {
+	var obj interface{}
+
+	name := acceptance.RandomAccResourceName()
+	rName := "huaweicloud_dms_rocketmq_topic.test"
+
+	rc := acceptance.InitResourceCheck(
+		rName,
+		&obj,
+		getDmsRocketMQTopicResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testDmsRocketMQTopic_with_queues(name),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(rName, "name", name),
+					resource.TestCheckResourceAttr(rName, "permission", "sub"),
+					resource.TestCheckResourceAttr(rName, "total_read_queue_num", "3"),
+					resource.TestCheckResourceAttr(rName, "total_write_queue_num", "3"),
+					resource.TestCheckResourceAttr(rName, "brokers.0.write_queue_num", "3"),
+					resource.TestCheckResourceAttr(rName, "brokers.0.write_queue_num", "3"),
+				),
+			},
+			{
+				ResourceName:            rName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"instance_id", "queues"},
+			},
+		},
+	})
+}
+
+func TestAccDmsRocketMQTopic_version5(t *testing.T) {
+	var obj interface{}
+
+	name := acceptance.RandomAccResourceName()
+	rName := "huaweicloud_dms_rocketmq_topic.test"
+
+	rc := acceptance.InitResourceCheck(
+		rName,
+		&obj,
+		getDmsRocketMQTopicResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testDmsRocketMQTopic_verison5(name),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(rName, "name", name),
+					resource.TestCheckResourceAttr(rName, "message_type", "NORMAL"),
+				),
+			},
+			{
+				ResourceName:            rName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"instance_id"},
+			},
+		},
+	})
+}
+
 func testAccDmsRocketmqTopic_Base(rName string) string {
 	return fmt.Sprintf(`
 %s
@@ -122,6 +196,30 @@ resource "huaweicloud_dms_rocketmq_instance" "test" {
   flavor_id         = "c6.4u8g.cluster"
   storage_spec_code = "dms.physical.storage.high.v2"
   broker_num        = 1
+}
+`, common.TestBaseNetwork(rName), rName)
+}
+
+func testAccDmsRocketmqTopic_verison5(rName string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_availability_zones" "test" {}
+
+resource "huaweicloud_dms_rocketmq_instance" "test" {
+  name              = "%s"
+  engine_version    = "5.x"
+  storage_space     = 200
+  vpc_id            = huaweicloud_vpc.test.id
+  subnet_id         = huaweicloud_vpc_subnet.test.id
+  security_group_id = huaweicloud_networking_secgroup.test.id
+
+  availability_zones = [
+    data.huaweicloud_availability_zones.test.names[0]
+  ]
+
+  flavor_id         = "rocketmq.b1.large.1"
+  storage_spec_code = "dms.physical.storage.high.v2"
 }
 `, common.TestBaseNetwork(rName), rName)
 }
@@ -154,4 +252,33 @@ resource "huaweicloud_dms_rocketmq_topic" "test" {
   total_write_queue_num = "5"
 }
 `, testAccDmsRocketmqTopic_Base(name), name)
+}
+
+func testDmsRocketMQTopic_with_queues(name string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_dms_rocketmq_topic" "test" {
+  instance_id = huaweicloud_dms_rocketmq_instance.test.id
+  name        = "%s"
+  permission  = "sub"
+
+  queues {
+    broker    = "broker-0"
+    queue_num = 3
+  }
+}
+`, testAccDmsRocketmqTopic_Base(name), name)
+}
+
+func testDmsRocketMQTopic_verison5(name string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_dms_rocketmq_topic" "test" {
+  instance_id  = huaweicloud_dms_rocketmq_instance.test.id
+  name         = "%s"
+  message_type = "NORMAL"
+}
+`, testAccDmsRocketmqTopic_verison5(name), name)
 }
