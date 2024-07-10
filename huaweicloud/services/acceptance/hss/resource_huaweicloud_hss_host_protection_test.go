30f70397ed8fa7acdbd0abdff751306148265d90
jinyangyang222 <jinyangyang222@outlook.com>
2024-07-10 10:47:44 +0800

    feat(hss/host_protection): support `is_wait_host_available` parameter (#5139)

--- huaweicloud/services/acceptance/hss/resource_huaweicloud_hss_host_protection_test.go
+++ huaweicloud/services/acceptance/hss/resource_huaweicloud_hss_host_protection_test.go
@@ -67,14 +67,15 @@ func TestAccHostProtection_basic(t *testing.T) {
 		getHostProtectionFunc,
 	)
 
+	// Because after closing the protection, the ECS instance will automatically switch to free basic protection,
+	// so avoid CheckDestroy here.
+	// lintignore:AT001
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acceptance.TestAccPreCheck(t)
 			acceptance.TestAccPreCheckHSSHostProtectionHostId(t)
-			acceptance.TestAccPreCheckHSSHostProtectionQuotaId(t)
 		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
-		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccHostProtection_basic(),
@@ -83,8 +84,7 @@ func TestAccHostProtection_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(rName, "host_id", acceptance.HW_HSS_HOST_PROTECTION_HOST_ID),
 					resource.TestCheckResourceAttr(rName, "version", "hss.version.basic"),
 					resource.TestCheckResourceAttr(rName, "charging_mode", "prePaid"),
-					resource.TestCheckResourceAttr(rName, "quota_id", acceptance.HW_HSS_HOST_PROTECTION_QUOTA_ID),
-					resource.TestCheckResourceAttr(rName, "enterprise_project_id", acceptance.HW_ENTERPRISE_PROJECT_ID_TEST),
+					resource.TestCheckResourceAttrSet(rName, "enterprise_project_id"),
 					resource.TestCheckResourceAttrSet(rName, "host_name"),
 					resource.TestCheckResourceAttrSet(rName, "host_status"),
 					resource.TestCheckResourceAttrSet(rName, "private_ip"),
@@ -95,6 +95,7 @@ func TestAccHostProtection_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet(rName, "detect_result"),
 					resource.TestCheckResourceAttrSet(rName, "asset_value"),
 					resource.TestCheckResourceAttrSet(rName, "open_time"),
+					resource.TestCheckResourceAttrPair(rName, "quota_id", "huaweicloud_hss_quota.test", "id"),
 				),
 			},
 			{
@@ -104,7 +105,7 @@ func TestAccHostProtection_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(rName, "host_id", acceptance.HW_HSS_HOST_PROTECTION_HOST_ID),
 					resource.TestCheckResourceAttr(rName, "version", "hss.version.enterprise"),
 					resource.TestCheckResourceAttr(rName, "charging_mode", "postPaid"),
-					resource.TestCheckResourceAttr(rName, "enterprise_project_id", acceptance.HW_ENTERPRISE_PROJECT_ID_TEST),
+					resource.TestCheckResourceAttrSet(rName, "enterprise_project_id"),
 					resource.TestCheckResourceAttrSet(rName, "host_name"),
 					resource.TestCheckResourceAttrSet(rName, "host_status"),
 					resource.TestCheckResourceAttrSet(rName, "private_ip"),
@@ -122,34 +123,42 @@ func TestAccHostProtection_basic(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: true,
 				ImportStateVerifyIgnore: []string{
-					"quota_id",
+					"quota_id", "is_wait_host_available",
 				},
 			},
 		},
 	})
 }
 
+func testAccHostProtection_base() string {
+	return `
+resource "huaweicloud_hss_quota" "test" {
+  version     = "hss.version.basic"
+  period_unit = "month"
+  period      = 1
+}`
+}
+
 func testAccHostProtection_basic() string {
 	return fmt.Sprintf(`
+%[1]s
 
 resource "huaweicloud_hss_host_protection" "test" {
-  host_id               = "%[1]s"
-  version               = "hss.version.basic"
-  charging_mode         = "prePaid"
-  quota_id              = "%[2]s"
-  enterprise_project_id = "%[3]s"
+  host_id                = "%[2]s"
+  version                = "hss.version.basic"
+  charging_mode          = "prePaid"
+  quota_id               = huaweicloud_hss_quota.test.id
+  is_wait_host_available = true
 }
-`, acceptance.HW_HSS_HOST_PROTECTION_HOST_ID, acceptance.HW_HSS_HOST_PROTECTION_QUOTA_ID, acceptance.HW_ENTERPRISE_PROJECT_ID_TEST)
+`, testAccHostProtection_base(), acceptance.HW_HSS_HOST_PROTECTION_HOST_ID)
 }
 
 func testAccHostProtection_update() string {
 	return fmt.Sprintf(`
-
 resource "huaweicloud_hss_host_protection" "test" {
-  host_id               = "%[1]s"
-  version               = "hss.version.enterprise"
-  charging_mode         = "postPaid"
-  enterprise_project_id = "%[2]s"
+  host_id       = "%[1]s"
+  version       = "hss.version.enterprise"
+  charging_mode = "postPaid"
 }
-`, acceptance.HW_HSS_HOST_PROTECTION_HOST_ID, acceptance.HW_ENTERPRISE_PROJECT_ID_TEST)
+`, acceptance.HW_HSS_HOST_PROTECTION_HOST_ID)
 }
