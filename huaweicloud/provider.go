d3707b65238344fa743e66f94dc4d05d8c96103d
allium <deerhang@163.com>
2024-07-24 14:36:41 +0800

    feat(ccm): ccm support a new datasource to query private cas (#5264)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -872,6 +872,7 @@ func Provider() *schema.Provider {
 
 			// Deprecated, use `huaweicloud_ccm_certificates` instead
 			"huaweicloud_ccm_private_certificates": ccm.DataSourcePrivateCertificates(),
+			"huaweicloud_ccm_private_cas":          ccm.DataSourcePrivateCas(),
 			"huaweicloud_scm_certificates":         ccm.DataSourceCertificates(),
 
 			"huaweicloud_sfs_file_system":       sfs.DataSourceSFSFileSystemV2(),
