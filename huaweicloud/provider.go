111bcf81cc5f6dc1ccb905ccc801df9b6a3aaab8
houpeng80 <hp_monkey@163.com>
2024-07-25 10:56:14 +0800

    feat(Organizations): add organizations received invitations data source

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1007,6 +1007,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_organizations_accounts":             organizations.DataSourceAccounts(),
 			"huaweicloud_organizations_policies":             organizations.DataSourcePolicies(),
 			"huaweicloud_organizations_sent_invitations":     organizations.DataSourceOrganizationsSentInvitations(),
+			"huaweicloud_organizations_received_invitations": organizations.DataSourceOrganizationsReceivedInvitations(),
 
 			// Deprecated ongoing (without DeprecationMessage), used by other providers
 			"huaweicloud_vpc_route":                  vpc.DataSourceVpcRouteV2(),
