8d5c735c52534c8304762db93d38aa604f80ff25
Jason Zhang <jasonzhang9309@163.com>
2024-08-02 15:41:03 +0800

    feat(rms): add new data_source policy_states

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -884,6 +884,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_rms_resources_summary":                     rms.DataSourceResourcesSummary(),
 			"huaweicloud_rms_resource_aggregation_pending_requests": rms.DataSourceRmsAggregationPendingRequests(),
 			"huaweicloud_rms_resource_aggregator_source_statuses":   rms.DataSourceRmsAggregatorSourceStatuses(),
+			"huaweicloud_rms_policy_states":                         rms.DataSourcePolicyStates(),
 
 			"huaweicloud_sdrs_domain": sdrs.DataSourceSDRSDomain(),
 
