b215e96a8a4dac9ecd4d5bcfb5b4e36f1d6eced1
saf3dfsa <caiwanting@huawei.com>
2024-08-06 16:10:44 +0800

    feat(aom): support dashboards folder management

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -415,6 +415,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_aom_alarm_action_rules":              aom.DataSourceAomAlarmActionRules(),
 			"huaweicloud_aom_prom_instances":                  aom.DataSourceAomPromInstances(),
 			"huaweicloud_aom_multi_account_aggregation_rules": aom.DataSourceMultiAccountAggregationRules(),
+			"huaweicloud_aom_dashboards_folders":              aom.DataSourceDashboardsFolders(),
 
 			"huaweicloud_apig_acl_policies":                       apig.DataSourceAclPolicies(),
 			"huaweicloud_apig_api_associated_acl_policies":        apig.DataSourceApiAssociatedAclPolicies(),
@@ -1085,6 +1086,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_aom_cmdb_environment":               aom.ResourceCmdbEnvironment(),
 			"huaweicloud_aom_prom_instance":                  aom.ResourcePromInstance(),
 			"huaweicloud_aom_multi_account_aggregation_rule": aom.ResourceMultiAccountAggregationRule(),
+			"huaweicloud_aom_dashboards_folder":              aom.ResourceDashboardsFolder(),
 
 			"huaweicloud_rfs_stack": rfs.ResourceStack(),
 
