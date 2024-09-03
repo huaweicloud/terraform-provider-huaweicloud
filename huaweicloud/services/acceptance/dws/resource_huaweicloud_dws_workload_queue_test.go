5f40e0685440f935d493a25817c2e77a3ec30826
wuzhuanhong <wuzhuanhong1@outlook.com>
2024-09-03 14:32:24 +0800

    chore(dws/queue): replacing the list api with the query detail api and adjust acceptance test (#5436)
    * chore(dws/queue): adjust acceptance test of the data source
    
    * chore(dws/queue): replacing the list api with the query detail api

--- huaweicloud/services/acceptance/dws/resource_huaweicloud_dws_workload_queue_test.go
+++ huaweicloud/services/acceptance/dws/resource_huaweicloud_dws_workload_queue_test.go
@@ -2,24 +2,19 @@ package dws
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
-	"github.com/chnsz/golangsdk"
-
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance/common"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/dws"
 )
 
 func getWorkloadQueueResourceFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
 	var (
 		region  = acceptance.HW_REGION_NAME
-		httpUrl = "v2/{project_id}/clusters/{cluster_id}/workload/queues"
 		product = "dws"
 	)
 
@@ -28,31 +23,9 @@ func getWorkloadQueueResourceFunc(cfg *config.Config, state *terraform.ResourceS
 		return nil, fmt.Errorf("error creating DWS client: %s", err)
 	}
 
-	getPath := client.Endpoint + httpUrl
-	getPath = strings.ReplaceAll(getPath, "{project_id}", client.ProjectID)
-	getPath = strings.ReplaceAll(getPath, "{cluster_id}", state.Primary.Attributes["cluster_id"])
-	getOpt := golangsdk.RequestOpts{
-		MoreHeaders:      map[string]string{"Content-Type": "application/json;charset=UTF-8"},
-		KeepResponseBody: true,
-	}
-
-	getResp, err := client.Request("GET", getPath, &getOpt)
-	if err != nil {
-		return nil, fmt.Errorf("error retrieving DWS workload queue: %s", err)
-	}
-
-	getRespBody, err := utils.FlattenResponse(getResp)
-	if err != nil {
-		return nil, fmt.Errorf("error retrieving DWS workload queue: %s", err)
-	}
-
-	expression := fmt.Sprintf("workload_queue_name_list[?@=='%s']|[0]", state.Primary.ID)
-	resp := utils.PathSearch(expression, getRespBody, nil)
-	if resp == nil {
-		return nil, golangsdk.ErrDefault404{}
-	}
-
-	return resp, nil
+	clusterId := state.Primary.Attributes["cluster_id"]
+	logicalClusterName := state.Primary.Attributes["logical_cluster_name"]
+	return dws.GetWorkloadQueueByName(client, clusterId, state.Primary.ID, logicalClusterName)
 }
 
 func TestAccResourceWorkloadQueue_basic(t *testing.T) {
@@ -69,17 +42,20 @@ func TestAccResourceWorkloadQueue_basic(t *testing.T) {
 	)
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckDwsClusterId(t)
+		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccWorkloadQueue_basic(name, "Admin_user@123"),
+				Config: testAccWorkloadQueue_basic(name),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
 					resource.TestCheckResourceAttr(resourceName, "name", name),
 					resource.TestCheckResourceAttr(resourceName, "configuration.#", "4"),
-					resource.TestCheckResourceAttrPair(resourceName, "cluster_id", "huaweicloud_dws_cluster.test", "id"),
+					resource.TestCheckResourceAttr(resourceName, "cluster_id", acceptance.HW_DWS_CLUSTER_ID),
 				),
 			},
 			{
@@ -88,39 +64,17 @@ func TestAccResourceWorkloadQueue_basic(t *testing.T) {
 				ImportStateVerify: true,
 				ImportStateIdFunc: testWorkloadQueueImportState(resourceName),
 				ImportStateVerifyIgnore: []string{
-					"configuration", "logical_cluster_name",
+					"configuration",
 				},
 			},
 		},
 	})
 }
 
-func testAccWorkloadQueue_base(name, password string) string {
+func testAccWorkloadQueue_basic(name string) string {
 	return fmt.Sprintf(`
-%[1]s
-
-data "huaweicloud_availability_zones" "test" {}
-
-resource "huaweicloud_dws_cluster" "test" {
-  name              = "%[2]s"
-  node_type         = "dwsk2.xlarge"
-  number_of_node    = 3
-  vpc_id            = huaweicloud_vpc.test.id
-  network_id        = huaweicloud_vpc_subnet.test.id
-  security_group_id = huaweicloud_networking_secgroup.test.id
-  availability_zone = data.huaweicloud_availability_zones.test.names[0]
-  user_name         = "admin_user"
-  user_pwd          = "%[3]s"
-}
-`, common.TestBaseNetwork(name), name, password)
-}
-
-func testAccWorkloadQueue_basic(name, password string) string {
-	return fmt.Sprintf(`
-%[1]s
-
 resource "huaweicloud_dws_workload_queue" "test" {
-  cluster_id = huaweicloud_dws_cluster.test.id
+  cluster_id = "%[1]s"
   name       = "%[2]s"
 
   configuration {
@@ -140,7 +94,78 @@ resource "huaweicloud_dws_workload_queue" "test" {
     resource_value = -1
   }
 }
-`, testAccWorkloadQueue_base(name, password), name)
+`, acceptance.HW_DWS_CLUSTER_ID, name)
+}
+
+func TestAccResourceWorkloadQueue_logicalClusterName(t *testing.T) {
+	var (
+		obj          interface{}
+		resourceName = "huaweicloud_dws_workload_queue.test"
+		name         = acceptance.RandomAccResourceName()
+	)
+
+	rc := acceptance.InitResourceCheck(
+		resourceName,
+		&obj,
+		getWorkloadQueueResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckDwsLogicalModeClusterId(t)
+			acceptance.TestAccPreCheckDwsLogicalClusterName(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceWorkloadQueue_logicalClusterName(name),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "cluster_id", acceptance.HW_DWS_LOGICAL_MODE_CLUSTER_ID),
+					resource.TestCheckResourceAttr(resourceName, "logical_cluster_name", acceptance.HW_DWS_LOGICAL_CLUSTER_NAME),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateIdFunc: testWorkloadQueueImportState(resourceName),
+				ImportStateVerifyIgnore: []string{
+					"configuration",
+				},
+			},
+		},
+	})
+}
+
+func testAccResourceWorkloadQueue_logicalClusterName(name string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_dws_workload_queue" "test" {
+  cluster_id           = "%[1]s"
+  name                 = "%[2]s"
+  logical_cluster_name = "%[3]s"
+
+  configuration {
+    resource_name  = "cpu_limit"
+    resource_value = 10
+  }
+  configuration {
+    resource_name  = "memory"
+    resource_value = 10
+  }
+  configuration {
+    resource_name  = "tablespace"
+    resource_value = -1
+  }
+  configuration {
+    resource_name  = "activestatements"
+    resource_value = -1
+  }
+}
+`, acceptance.HW_DWS_LOGICAL_MODE_CLUSTER_ID, name, acceptance.HW_DWS_LOGICAL_CLUSTER_NAME)
 }
 
 func testWorkloadQueueImportState(name string) resource.ImportStateIdFunc {
@@ -155,6 +180,10 @@ func testWorkloadQueueImportState(name string) resource.ImportStateIdFunc {
 			return "", fmt.Errorf("the workload queue is not exist or related cluster ID is missing")
 		}
 
+		logicalClusterName := rs.Primary.Attributes["logical_cluster_name"]
+		if logicalClusterName != "" {
+			return fmt.Sprintf("%s/%s/%s", clusterId, id, logicalClusterName), nil
+		}
 		return fmt.Sprintf("%s/%s", clusterId, id), nil
 	}
 }
