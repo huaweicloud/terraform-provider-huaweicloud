1ec3b549c2409f4c0ecc6bc2b85604bc157591cd
profoundwu <profoundwu@outlook.com>
2024-09-10 15:48:22 +0800

    feat(ces): add data source to get the list of CES one-click alarms

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -530,6 +530,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_ces_events":                           ces.DataSourceCesEvents(),
 			"huaweicloud_ces_event_details":                    ces.DataSourceCesEventDetails(),
 			"huaweicloud_ces_metrics":                          ces.DataSourceCesMetrics(),
+			"huaweicloud_ces_one_click_alarms":                 ces.DataSourceCesOneClickAlarms(),
 			"huaweicloud_ces_resource_groups":                  ces.DataSourceCesGroups(),
 			"huaweicloud_ces_resource_group_service_resources": ces.DataSourceCesGroupServiceResources(),
 
