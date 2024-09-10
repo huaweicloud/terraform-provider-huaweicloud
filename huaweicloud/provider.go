2a2e3108b8a17b0f05dcecde78185ffff6664eae
ruwenqiang123 <ruwenqiang123@outlook.com>
2024-09-10 10:36:28 +0800

    feat(dc): add a new datasource to get list of DC resource quotas (#5511)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -605,6 +605,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_dbss_flavors": dbss.DataSourceDbssFlavors(),
 
 			"huaweicloud_dc_connections":        dc.DataSourceDcConnections(),
+			"huaweicloud_dc_quotas":             dc.DataSourceDcQuotas(),
 			"huaweicloud_dc_virtual_gateways":   dc.DataSourceDCVirtualGateways(),
 			"huaweicloud_dc_virtual_interfaces": dc.DataSourceDCVirtualInterfaces(),
 
