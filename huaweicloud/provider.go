c40d44d72bb2b0624fca071f7509827e031cd07e
ruwenqiang123 <ruwenqiang123@outlook.com>
2024-09-25 16:25:45 +0800

    feat(dbss): add a new datasource to get list of SQL injection rules

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -609,10 +609,11 @@ func Provider() *schema.Provider {
 			// DataArts Factory
 			"huaweicloud_dataarts_factory_jobs": dataarts.DataSourceFactoryJobs(),
 
-			"huaweicloud_dbss_audit_data_masking_rules": dbss.DataSourceDbssAuditDataMaskingRules(),
-			"huaweicloud_dbss_audit_risk_rules":         dbss.DataSourceDbssAuditRiskRules(),
-			"huaweicloud_dbss_audit_rule_scopes":        dbss.DataSourceDbssAuditRuleScopes(),
-			"huaweicloud_dbss_flavors":                  dbss.DataSourceDbssFlavors(),
+			"huaweicloud_dbss_audit_data_masking_rules":  dbss.DataSourceDbssAuditDataMaskingRules(),
+			"huaweicloud_dbss_audit_risk_rules":          dbss.DataSourceDbssAuditRiskRules(),
+			"huaweicloud_dbss_audit_rule_scopes":         dbss.DataSourceDbssAuditRuleScopes(),
+			"huaweicloud_dbss_audit_sql_injection_rules": dbss.DataSourceDbssAuditSqlInjectionRules(),
+			"huaweicloud_dbss_flavors":                   dbss.DataSourceDbssFlavors(),
 
 			"huaweicloud_dc_connections":        dc.DataSourceDcConnections(),
 			"huaweicloud_dc_quotas":             dc.DataSourceDcQuotas(),
