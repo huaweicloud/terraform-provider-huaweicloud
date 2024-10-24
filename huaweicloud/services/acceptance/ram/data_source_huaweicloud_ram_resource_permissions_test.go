8ba9907ec761687834e67f399cd38bf430f07380
allium <deerhang@163.com>
2024-10-24 16:50:49 +0800

    chore(ram): ram resource permissions datasource supports paging query and new fields (#5752)
    * chore(ram): ram resource permissions datasource supports paging query
    
    * chore(ram): ram resource permissions datasource supports new fields

--- huaweicloud/services/acceptance/ram/data_source_huaweicloud_ram_resource_permissions_test.go
+++ huaweicloud/services/acceptance/ram/data_source_huaweicloud_ram_resource_permissions_test.go
@@ -9,8 +9,19 @@ import (
 )
 
 func TestAccDatasourceRAMPermissions_basic(t *testing.T) {
-	rName := "data.huaweicloud_ram_resource_permissions.test"
-	dc := acceptance.InitDataSourceCheck(rName)
+	var (
+		rName = "data.huaweicloud_ram_resource_permissions.test"
+		dc    = acceptance.InitDataSourceCheck(rName)
+
+		byResourceType   = "data.huaweicloud_ram_resource_permissions.filter_by_resource_type"
+		dcByResourceType = acceptance.InitDataSourceCheck(byResourceType)
+
+		byPermissionType   = "data.huaweicloud_ram_resource_permissions.filter_by_permission_type"
+		dcByPermissionType = acceptance.InitDataSourceCheck(byPermissionType)
+
+		byName   = "data.huaweicloud_ram_resource_permissions.filter_by_name"
+		dcByName = acceptance.InitDataSourceCheck(byName)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
@@ -26,9 +37,20 @@ func TestAccDatasourceRAMPermissions_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet(rName, "permissions.0.is_resource_type_default"),
 					resource.TestCheckResourceAttrSet(rName, "permissions.0.created_at"),
 					resource.TestCheckResourceAttrSet(rName, "permissions.0.updated_at"),
+					resource.TestCheckResourceAttrSet(rName, "permissions.0.permission_urn"),
+					resource.TestCheckResourceAttrSet(rName, "permissions.0.permission_type"),
+					resource.TestCheckResourceAttrSet(rName, "permissions.0.default_version"),
+					resource.TestCheckResourceAttrSet(rName, "permissions.0.version"),
+					resource.TestCheckResourceAttrSet(rName, "permissions.0.status"),
 
-					resource.TestCheckOutput("resource_type_filter_is_useful", "true"),
-					resource.TestCheckOutput("name_filter_is_useful", "true"),
+					dcByResourceType.CheckResourceExists(),
+					resource.TestCheckOutput("is_resource_type_filter_useful", "true"),
+
+					dcByPermissionType.CheckResourceExists(),
+					resource.TestCheckOutput("is_permission_type_filter_useful", "true"),
+
+					dcByName.CheckResourceExists(),
+					resource.TestCheckOutput("is_name_filter_useful", "true"),
 				),
 			},
 		},
@@ -40,18 +62,61 @@ func testAccDatasourceRAMPermissions_basic() string {
 data "huaweicloud_ram_resource_permissions" "test" {
 }
 
-data "huaweicloud_ram_resource_permissions" "resource_type_filter" {
-  resource_type = "vpc:subnets"
-}
-output "resource_type_filter_is_useful" {
-  value = alltrue([for v in data.huaweicloud_ram_resource_permissions.resource_type_filter.permissions[*].resource_type : v == "vpc:subnets"])
+# Filter by resource_type
+locals {
+  resource_type = data.huaweicloud_ram_resource_permissions.test.permissions[0].resource_type
 }
 
-data "huaweicloud_ram_resource_permissions" "name_filter" {
-  name = "default vpc subnets statement"
+data "huaweicloud_ram_resource_permissions" "filter_by_resource_type" {
+  resource_type = local.resource_type
 }
-output "name_filter_is_useful" {
-  value = alltrue([for v in data.huaweicloud_ram_resource_permissions.name_filter.permissions[*].name : v == "default vpc subnets statement"])
+
+locals {
+  resource_type_filter_result = [
+    for v in data.huaweicloud_ram_resource_permissions.filter_by_resource_type.permissions[*].resource_type : v == local.resource_type
+  ]
+}
+
+output "is_resource_type_filter_useful" {
+  value = length(local.resource_type_filter_result) > 0 && alltrue(local.resource_type_filter_result)
+}
+
+# Filter by permission_type
+locals {
+  permission_type = data.huaweicloud_ram_resource_permissions.test.permissions[0].permission_type
+}
+
+data "huaweicloud_ram_resource_permissions" "filter_by_permission_type" {
+  permission_type = local.permission_type
+}
+
+locals {
+  permission_type_filter_result = [
+    for v in data.huaweicloud_ram_resource_permissions.filter_by_permission_type.permissions[*].permission_type : v == local.permission_type
+  ]
+}
+
+output "is_permission_type_filter_useful" {
+  value = length(local.permission_type_filter_result) > 0 && alltrue(local.permission_type_filter_result)
+}
+
+# Filter by name
+locals {
+  name = data.huaweicloud_ram_resource_permissions.test.permissions[0].name
+}
+
+data "huaweicloud_ram_resource_permissions" "filter_by_name" {
+  name = local.name
+}
+
+locals {
+  name_filter_result = [
+    for v in data.huaweicloud_ram_resource_permissions.filter_by_name.permissions[*].name : v == local.name
+  ]
+}
+
+output "is_name_filter_useful" {
+  value = length(local.name_filter_result) > 0 && alltrue(local.name_filter_result)
 }
 `
 }
