3ca8867004ee822226bf14139961272f694a118b
luoping-12345 <luoping-12345@outlook.com>
2024-10-25 17:37:10 +0800

    feat(cph): add resource cph phone reset

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1386,6 +1386,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_cph_adb_command":    cph.ResourceAdbCommand(),
 			"huaweicloud_cph_phone_stop":     cph.ResourcePhoneStop(),
 			"huaweicloud_cph_server_restart": cph.ResourceServerRestart(),
+			"huaweicloud_cph_phone_reset":    cph.ResourcePhoneReset(),
 
 			"huaweicloud_cse_microservice":                      cse.ResourceMicroservice(),
 			"huaweicloud_cse_microservice_engine":               cse.ResourceMicroserviceEngine(),
