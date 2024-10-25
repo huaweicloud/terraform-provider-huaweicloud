233b0f329a216284e21e4b79900e54cd4f2c0568
wuzhuanhong <wuzhuanhong1@outlook.com>
2024-10-25 17:33:16 +0800

    feat(workspace/app): add resource for publishing application

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2009,6 +2009,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_waf_reference_table":                     waf.ResourceWafReferenceTableV1(),
 
 			"huaweicloud_workspace_app_group":         workspace.ResourceWorkspaceAppGroup(),
+			"huaweicloud_workspace_app_publishment":   workspace.ResourceAppPublishment(),
 			"huaweicloud_workspace_user_group":        workspace.ResourceUserGroup(),
 			"huaweicloud_workspace_access_policy":     workspace.ResourceAccessPolicy(),
 			"huaweicloud_workspace_desktop_name_rule": workspace.ResourceDesktopNameRule(),
