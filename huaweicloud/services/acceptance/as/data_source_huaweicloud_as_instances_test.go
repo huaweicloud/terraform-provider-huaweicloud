f59dc7044071f2cbf424aade1b584cfd5bb00e55
luxiaohang <deerhang@163.com>
2024-10-26 16:16:13 +0800

    chore(as): Enhance the datasource as instances test case.

--- huaweicloud/services/acceptance/as/data_source_huaweicloud_as_instances_test.go
+++ huaweicloud/services/acceptance/as/data_source_huaweicloud_as_instances_test.go
@@ -12,16 +12,29 @@ import (
 func TestAccDatasourceASInstances_basic(t *testing.T) {
 	var (
 		rName = "data.huaweicloud_as_instances.test"
-		name  = acceptance.RandomAccResourceName()
 		dc    = acceptance.InitDataSourceCheck(rName)
+
+		byLifeCycleState   = "data.huaweicloud_as_instances.life_cycle_state_filter"
+		dcByLifeCycleState = acceptance.InitDataSourceCheck(byLifeCycleState)
+
+		byHealthStatus   = "data.huaweicloud_as_instances.health_status_filter"
+		dcByHealthStatus = acceptance.InitDataSourceCheck(byHealthStatus)
+
+		byProtectFromScalingDown   = "data.huaweicloud_as_instances.protect_from_scaling_down_filter"
+		dcByProtectFromScalingDown = acceptance.InitDataSourceCheck(byProtectFromScalingDown)
 	)
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			// Please prepare the AS group containing the instance in advance and configure the AS group ID into the
+			// environment variable.
+			acceptance.TestAccPreCheckASScalingGroupID(t)
+		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDatasourceASInstances_basic(name),
+				Config: testAccDatasourceASInstances_basic(),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
 					resource.TestCheckResourceAttrSet(rName, "instances.0.instance_id"),
@@ -33,10 +46,13 @@ func TestAccDatasourceASInstances_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet(rName, "instances.0.created_at"),
 					resource.TestCheckResourceAttrSet(rName, "instances.0.protect_from_scaling_down"),
 
+					dcByLifeCycleState.CheckResourceExists(),
 					resource.TestCheckOutput("life_cycle_state_filter_is_useful", "true"),
 
+					dcByHealthStatus.CheckResourceExists(),
 					resource.TestCheckOutput("health_status_filter_is_useful", "true"),
 
+					dcByProtectFromScalingDown.CheckResourceExists(),
 					resource.TestCheckOutput("protect_from_scaling_down_filter_is_useful", "true"),
 				),
 			},
@@ -44,12 +60,10 @@ func TestAccDatasourceASInstances_basic(t *testing.T) {
 	})
 }
 
-func testAccDatasourceASInstances_basic(name string) string {
+func testAccDatasourceASInstances_basic() string {
 	return fmt.Sprintf(`
-%[1]s
-
 data "huaweicloud_as_instances" "test" {
-  scaling_group_id = huaweicloud_as_group.acc_as_group.id
+  scaling_group_id = "%[1]s"
 }
 
 # Test with life_cycle_state
@@ -58,14 +72,18 @@ locals {
 }
 
 data "huaweicloud_as_instances" "life_cycle_state_filter" {
-  scaling_group_id = huaweicloud_as_group.acc_as_group.id
+  scaling_group_id = "%[1]s"
   life_cycle_state = local.life_cycle_state
 }
 
+locals {
+  life_cycle_state_filter_result = [
+    for v in data.huaweicloud_as_instances.life_cycle_state_filter.instances[*].life_cycle_state : v == local.life_cycle_state
+  ]
+}
+
 output "life_cycle_state_filter_is_useful" {
-  value = length(data.huaweicloud_as_instances.life_cycle_state_filter.instances) > 0 && alltrue(
-    [for v in data.huaweicloud_as_instances.life_cycle_state_filter.instances[*].life_cycle_state : v == local.life_cycle_state]
-  )  
+  value = length(local.life_cycle_state_filter_result) > 0 && alltrue(local.life_cycle_state_filter_result)  
 }
 
 # Test with health_status
@@ -74,14 +92,18 @@ locals {
 }
 
 data "huaweicloud_as_instances" "health_status_filter" {
-  scaling_group_id = huaweicloud_as_group.acc_as_group.id
+  scaling_group_id = "%[1]s"
   health_status    = local.health_status
 }
 
+locals {
+  health_status_filter_result = [
+    for v in data.huaweicloud_as_instances.health_status_filter.instances[*].health_status : v == local.health_status
+  ]
+}
+
 output "health_status_filter_is_useful" {
-  value = length(data.huaweicloud_as_instances.health_status_filter.instances) > 0 && alltrue(
-    [for v in data.huaweicloud_as_instances.health_status_filter.instances[*].health_status : v == local.health_status]
-  )  
+  value = length(local.health_status_filter_result) > 0 && alltrue(local.health_status_filter_result)  
 }
 
 # Test with protect_from_scaling_down
@@ -90,15 +112,19 @@ locals {
 }
 
 data "huaweicloud_as_instances" "protect_from_scaling_down_filter" {
-  scaling_group_id          = huaweicloud_as_group.acc_as_group.id
+  scaling_group_id          = "%[1]s"
   protect_from_scaling_down = local.protect_from_scaling_down
 }
 
-output "protect_from_scaling_down_filter_is_useful" {
-  value = length(data.huaweicloud_as_instances.protect_from_scaling_down_filter.instances) > 0 && alltrue(
-    [for v in data.huaweicloud_as_instances.protect_from_scaling_down_filter.instances[*].protect_from_scaling_down :
-    v == local.protect_from_scaling_down]
-  )  
+locals {
+  protect_from_scaling_down_filter_result = [
+    for v in data.huaweicloud_as_instances.protect_from_scaling_down_filter.instances[*].protect_from_scaling_down :
+    v == local.protect_from_scaling_down
+  ]
 }
-`, testASGroup_forceDelete(name))
+
+output "protect_from_scaling_down_filter_is_useful" {
+  value = length(local.protect_from_scaling_down_filter_result) > 0 && alltrue(local.protect_from_scaling_down_filter_result)  
+}
+`, acceptance.HW_AS_SCALING_GROUP_ID)
 }
