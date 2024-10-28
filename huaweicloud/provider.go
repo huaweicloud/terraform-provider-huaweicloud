075097a4cc6a6e1ea5991c6e67b741241c1a4c56
saf3dfsa <caiwanting@huawei.com>
2024-10-28 14:53:37 +0800

    feat(dds): add data source to get error logs

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -652,6 +652,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_dds_restore_time_ranges":        dds.DataSourceDdsRestoreTimeRanges(),
 			"huaweicloud_dds_backups":                    dds.DataSourceDDSBackups(),
 			"huaweicloud_dds_database_roles":             dds.DateSourceDDSDatabaseRoles(),
+			"huaweicloud_dds_error_logs":                 dds.DataSourceDDSErrorLogs(),
 
 			"huaweicloud_dli_datasource_auths":       dli.DataSourceAuths(),
 			"huaweicloud_dli_datasource_connections": dli.DataSourceConnections(),
