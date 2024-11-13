48aed6eec7901cac74422a185c345aaf2ebe38ef
luxiaohang <deerhang@163.com>
2024-11-13 17:03:48 +0800

    chore(dsc): modify the code style and document description

--- huaweicloud/services/dsc/resource_huaweicloud_dsc_asset_obs.go
+++ huaweicloud/services/dsc/resource_huaweicloud_dsc_asset_obs.go
@@ -63,68 +63,54 @@ func ResourceAssetObs() *schema.Resource {
 	}
 }
 
+func queryAssetObsBucket(client *golangsdk.ServiceClient) (interface{}, error) {
+	requestPath := client.Endpoint + "v1/{project_id}/sdg/asset/obs/buckets"
+	requestPath = strings.ReplaceAll(requestPath, "{project_id}", client.ProjectID)
+	requestPath += "?added=true"
+	requestOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+	resp, err := client.Request("GET", requestPath, &requestOpt)
+	if err != nil {
+		return nil, err
+	}
+
+	return utils.FlattenResponse(resp)
+}
+
 func resourceAssetObsCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	cfg := meta.(*config.Config)
-	region := cfg.GetRegion(d)
-
-	// createAssetObs: create an OBS asset.
 	var (
-		createAssetObsHttpUrl = "v1/{project_id}/sdg/asset/obs/buckets"
-		createAssetObsProduct = "dsc"
+		cfg     = meta.(*config.Config)
+		region  = cfg.GetRegion(d)
+		httpUrl = "v1/{project_id}/sdg/asset/obs/buckets"
+		product = "dsc"
+		name    = d.Get("name").(string)
 	)
-	createAssetObsClient, err := cfg.NewServiceClient(createAssetObsProduct, region)
+	client, err := cfg.NewServiceClient(product, region)
 	if err != nil {
-		return diag.Errorf("error creating AssetObs Client: %s", err)
+		return diag.Errorf("error creating DSC client: %s", err)
 	}
 
-	createAssetObsPath := createAssetObsClient.Endpoint + createAssetObsHttpUrl
-	createAssetObsPath = strings.ReplaceAll(createAssetObsPath, "{project_id}", createAssetObsClient.ProjectID)
-
-	createAssetObsOpt := golangsdk.RequestOpts{
+	requestPath := client.Endpoint + httpUrl
+	requestPath = strings.ReplaceAll(requestPath, "{project_id}", client.ProjectID)
+	requestOpt := golangsdk.RequestOpts{
 		KeepResponseBody: true,
-		OkCodes: []int{
-			200,
-		},
+		JSONBody:         utils.RemoveNil(buildCreateAssetObsBodyParams(d, cfg)),
 	}
-	createAssetObsOpt.JSONBody = utils.RemoveNil(buildCreateAssetObsBodyParams(d, cfg))
-	_, err = createAssetObsClient.Request("POST", createAssetObsPath, &createAssetObsOpt)
+	_, err = client.Request("POST", requestPath, &requestOpt)
 	if err != nil {
-		return diag.Errorf("error creating AssetObs: %s", err)
+		return diag.Errorf("error adding DSC asset OBS bucket: %s", err)
 	}
 
-	// getAssetObs: Query the asset OBS
-	var (
-		getAssetObsHttpUrl = "v1/{project_id}/sdg/asset/obs/buckets"
-		getAssetObsProduct = "dsc"
-	)
-	getAssetObsClient, err := cfg.NewServiceClient(getAssetObsProduct, region)
+	respBody, err := queryAssetObsBucket(client)
 	if err != nil {
-		return diag.Errorf("error creating AssetObs Client: %s", err)
+		return diag.Errorf("error retrieving DSC asset OBS buckets: %s", err)
 	}
 
-	getAssetObsPath := getAssetObsClient.Endpoint + getAssetObsHttpUrl
-	getAssetObsPath = strings.ReplaceAll(getAssetObsPath, "{project_id}", getAssetObsClient.ProjectID)
-	getAssetObsPath += "?added=true"
-
-	getAssetObsOpt := golangsdk.RequestOpts{
-		KeepResponseBody: true,
-		OkCodes: []int{
-			200,
-		},
-	}
-	getAssetObsResp, err := getAssetObsClient.Request("GET", getAssetObsPath, &getAssetObsOpt)
-	if err != nil {
-		return diag.Errorf("error creating AssetObs: %s", err)
-	}
-
-	getAssetObsRespBody, err := utils.FlattenResponse(getAssetObsResp)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	assetObsId := utils.PathSearch(fmt.Sprintf("buckets[?asset_name=='%s']|[0].id", d.Get("name").(string)), getAssetObsRespBody, "").(string)
+	expression := fmt.Sprintf("buckets[?asset_name=='%s']|[0].id", name)
+	assetObsId := utils.PathSearch(expression, respBody, "").(string)
 	if assetObsId == "" {
-		return diag.Errorf("error creating AssetObs: ID is not found in API response")
+		return diag.Errorf("error adding DSC asset OBS bucket: ID is not found in API response")
 	}
 	d.SetId(assetObsId)
 
@@ -145,44 +131,24 @@ func buildCreateAssetObsBodyParams(d *schema.ResourceData, cfg *config.Config) m
 }
 
 func resourceAssetObsRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	cfg := meta.(*config.Config)
-	region := cfg.GetRegion(d)
-
-	var mErr *multierror.Error
-
-	// getAssetObs: Query the asset OBS
 	var (
-		getAssetObsHttpUrl = "v1/{project_id}/sdg/asset/obs/buckets"
-		getAssetObsProduct = "dsc"
+		cfg     = meta.(*config.Config)
+		region  = cfg.GetRegion(d)
+		mErr    *multierror.Error
+		product = "dsc"
 	)
-	getAssetObsClient, err := cfg.NewServiceClient(getAssetObsProduct, region)
+	client, err := cfg.NewServiceClient(product, region)
 	if err != nil {
-		return diag.Errorf("error creating AssetObs Client: %s", err)
+		return diag.Errorf("error creating DSC client: %s", err)
 	}
 
-	getAssetObsPath := getAssetObsClient.Endpoint + getAssetObsHttpUrl
-	getAssetObsPath = strings.ReplaceAll(getAssetObsPath, "{project_id}", getAssetObsClient.ProjectID)
-	getAssetObsPath += "?added=true"
-
-	getAssetObsOpt := golangsdk.RequestOpts{
-		KeepResponseBody: true,
-		OkCodes: []int{
-			200,
-		},
-	}
-	getAssetObsResp, err := getAssetObsClient.Request("GET", getAssetObsPath, &getAssetObsOpt)
-
+	respBody, err := queryAssetObsBucket(client)
 	if err != nil {
-		return common.CheckDeletedDiag(d, err, "error retrieving AssetObs")
+		return common.CheckDeletedDiag(d, err, "error retrieving DSC asset OBS buckets")
 	}
 
-	getAssetObsRespBody, err := utils.FlattenResponse(getAssetObsResp)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	assetObs := utils.PathSearch(fmt.Sprintf("buckets[?id=='%s']|[0]", d.Id()), getAssetObsRespBody, nil)
-
+	expression := fmt.Sprintf("buckets[?id=='%s']|[0]", d.Id())
+	assetObs := utils.PathSearch(expression, respBody, nil)
 	mErr = multierror.Append(
 		mErr,
 		d.Set("region", region),
@@ -198,35 +164,26 @@ func resourceAssetObsUpdate(ctx context.Context, d *schema.ResourceData, meta in
 	cfg := meta.(*config.Config)
 	region := cfg.GetRegion(d)
 
-	updateAssetObshasChanges := []string{
-		"name",
-	}
-
-	if d.HasChanges(updateAssetObshasChanges...) {
-		// updateAssetObs: update the OBS asset name.
+	if d.HasChange("name") {
 		var (
-			updateAssetObsHttpUrl = "v1/{project_id}/sdg/asset/{id}/name"
-			updateAssetObsProduct = "dsc"
+			httpUrl = "v1/{project_id}/sdg/asset/{id}/name"
+			product = "dsc"
 		)
-		updateAssetObsClient, err := cfg.NewServiceClient(updateAssetObsProduct, region)
+		client, err := cfg.NewServiceClient(product, region)
 		if err != nil {
-			return diag.Errorf("error creating AssetObs Client: %s", err)
+			return diag.Errorf("error creating DSC client: %s", err)
 		}
 
-		updateAssetObsPath := updateAssetObsClient.Endpoint + updateAssetObsHttpUrl
-		updateAssetObsPath = strings.ReplaceAll(updateAssetObsPath, "{project_id}", updateAssetObsClient.ProjectID)
-		updateAssetObsPath = strings.ReplaceAll(updateAssetObsPath, "{id}", d.Id())
-
-		updateAssetObsOpt := golangsdk.RequestOpts{
+		requestPath := client.Endpoint + httpUrl
+		requestPath = strings.ReplaceAll(requestPath, "{project_id}", client.ProjectID)
+		requestPath = strings.ReplaceAll(requestPath, "{id}", d.Id())
+		requestOpt := golangsdk.RequestOpts{
 			KeepResponseBody: true,
-			OkCodes: []int{
-				200,
-			},
+			JSONBody:         utils.RemoveNil(buildUpdateAssetObsBodyParams(d, cfg)),
 		}
-		updateAssetObsOpt.JSONBody = utils.RemoveNil(buildUpdateAssetObsBodyParams(d, cfg))
-		_, err = updateAssetObsClient.Request("PUT", updateAssetObsPath, &updateAssetObsOpt)
+		_, err = client.Request("PUT", requestPath, &requestOpt)
 		if err != nil {
-			return diag.Errorf("error updating AssetObs: %s", err)
+			return diag.Errorf("error updating DSC asset name: %s", err)
 		}
 	}
 	return resourceAssetObsRead(ctx, d, meta)
@@ -240,31 +197,26 @@ func buildUpdateAssetObsBodyParams(d *schema.ResourceData, _ *config.Config) map
 }
 
 func resourceAssetObsDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	cfg := meta.(*config.Config)
-	region := cfg.GetRegion(d)
-
 	var (
-		deleteAssetObsHttpUrl = "v1/{project_id}/sdg/asset/obs/bucket/{id}"
-		deleteAssetObsProduct = "dsc"
+		cfg     = meta.(*config.Config)
+		region  = cfg.GetRegion(d)
+		httpUrl = "v1/{project_id}/sdg/asset/obs/bucket/{id}"
+		product = "dsc"
 	)
-	deleteAssetObsClient, err := cfg.NewServiceClient(deleteAssetObsProduct, region)
+	client, err := cfg.NewServiceClient(product, region)
 	if err != nil {
-		return diag.Errorf("error creating AssetObs Client: %s", err)
+		return diag.Errorf("error creating DSC client: %s", err)
 	}
 
-	deleteAssetObsPath := deleteAssetObsClient.Endpoint + deleteAssetObsHttpUrl
-	deleteAssetObsPath = strings.ReplaceAll(deleteAssetObsPath, "{project_id}", deleteAssetObsClient.ProjectID)
-	deleteAssetObsPath = strings.ReplaceAll(deleteAssetObsPath, "{id}", d.Id())
-
-	deleteAssetObsOpt := golangsdk.RequestOpts{
+	requestPath := client.Endpoint + httpUrl
+	requestPath = strings.ReplaceAll(requestPath, "{project_id}", client.ProjectID)
+	requestPath = strings.ReplaceAll(requestPath, "{id}", d.Id())
+	requestOpt := golangsdk.RequestOpts{
 		KeepResponseBody: true,
-		OkCodes: []int{
-			200,
-		},
 	}
-	_, err = deleteAssetObsClient.Request("DELETE", deleteAssetObsPath, &deleteAssetObsOpt)
+	_, err = client.Request("DELETE", requestPath, &requestOpt)
 	if err != nil {
-		return diag.Errorf("error deleting AssetObs: %s", err)
+		return diag.Errorf("error deleting DSC asset OBS bucket: %s", err)
 	}
 
 	return nil
