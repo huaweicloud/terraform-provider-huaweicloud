bac964df8c3ee87feda2397fcb4bb92f8842205d
wuzhuanhong <wuzhuanhong1@outlook.com>
2024-11-27 10:36:27 +0800

    feat(workspace/app): add new resource to manage image server (#5908)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2101,6 +2101,7 @@ func Provider() *schema.Provider {
 
 			"huaweicloud_workspace_app_group_authorization": workspace.ResourceAppGroupAuthorization(),
 			"huaweicloud_workspace_app_group":               workspace.ResourceWorkspaceAppGroup(),
+			"huaweicloud_workspace_app_image_server":        workspace.ResourceAppImageServer(),
 			"huaweicloud_workspace_app_nas_storage":         workspace.ResourceAppNasStorage(),
 			"huaweicloud_workspace_app_policy_group":        workspace.ResourceAppPolicyGroup(),
 			"huaweicloud_workspace_app_publishment":         workspace.ResourceAppPublishment(),
