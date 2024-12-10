011a7cebec024b74cde9c35280d91e5ffec462e2
profoundwu <profoundwu@outlook.com>
2024-12-10 20:22:32 +0800

    feat(rms): add this data source to get the list of RMS latest remediation execution statuses (#5981)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1029,6 +1029,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_rms_resource_aggregator_policy_assignments":   rms.DataSourceAggregatorPolicyAssignments(),
 			"huaweicloud_rms_resource_histories":                       rms.DataSourceRmsHistories(),
 			"huaweicloud_rms_resource_relations_details":               rms.DataSourceRmsRelationsDetails(),
+			"huaweicloud_rms_remediation_execution_statuses":           rms.DataSourceRemediationExecutionStatuses(),
 
 			"huaweicloud_sdrs_domain": sdrs.DataSourceSDRSDomain(),
 
