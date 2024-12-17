53ddad87155c3ba5d9581ccc7126d47e54ba8211
allium <deerhang@163.com>
2024-12-17 20:44:45 +0800

    chore(live): Optimize resource code to domain resource (#6043)

--- huaweicloud/services/acceptance/live/resource_huaweicloud_live_domain_test.go
+++ huaweicloud/services/acceptance/live/resource_huaweicloud_live_domain_test.go
@@ -37,7 +37,10 @@ func TestAccDomain_basic(t *testing.T) {
 	)
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckEpsID(t)
+		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
@@ -48,10 +51,15 @@ func TestAccDomain_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(pushResourceName, "name", pushDomainName),
 					resource.TestCheckResourceAttr(pushResourceName, "type", "push"),
 					resource.TestCheckResourceAttr(pushResourceName, "status", "on"),
+					resource.TestCheckResourceAttr(pushResourceName, "service_area", "mainland_china"),
+					resource.TestCheckResourceAttr(pushResourceName, "enterprise_project_id", acceptance.HW_ENTERPRISE_PROJECT_ID_TEST),
+
 					resource.TestCheckResourceAttr(pullResourceName, "name", pullDomainName),
 					resource.TestCheckResourceAttr(pullResourceName, "type", "pull"),
 					resource.TestCheckResourceAttr(pullResourceName, "status", "on"),
 					resource.TestCheckResourceAttr(pullResourceName, "ingest_domain_name", pushDomainName),
+					resource.TestCheckResourceAttr(pullResourceName, "service_area", "outside_mainland_china"),
+					resource.TestCheckResourceAttr(pullResourceName, "enterprise_project_id", acceptance.HW_ENTERPRISE_PROJECT_ID_TEST),
 				),
 			},
 			{
@@ -60,10 +68,15 @@ func TestAccDomain_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(pushResourceName, "name", pushDomainName),
 					resource.TestCheckResourceAttr(pushResourceName, "type", "push"),
 					resource.TestCheckResourceAttr(pushResourceName, "status", "on"),
+					resource.TestCheckResourceAttr(pushResourceName, "service_area", "mainland_china"),
+					resource.TestCheckResourceAttr(pushResourceName, "enterprise_project_id", acceptance.HW_ENTERPRISE_PROJECT_ID_TEST),
+
 					resource.TestCheckResourceAttr(pullResourceName, "name", pullDomainName),
 					resource.TestCheckResourceAttr(pullResourceName, "type", "pull"),
 					resource.TestCheckResourceAttr(pullResourceName, "status", "off"),
 					resource.TestCheckResourceAttr(pullResourceName, "ingest_domain_name", ""),
+					resource.TestCheckResourceAttr(pullResourceName, "service_area", "outside_mainland_china"),
+					resource.TestCheckResourceAttr(pullResourceName, "enterprise_project_id", acceptance.HW_ENTERPRISE_PROJECT_ID_TEST),
 				),
 			},
 			{
@@ -78,29 +91,37 @@ func TestAccDomain_basic(t *testing.T) {
 func testDomain_basic(pushDomain, pullDomain string) string {
 	return fmt.Sprintf(`
 resource "huaweicloud_live_domain" "ingestDomain" {
-  name = "%s"
-  type = "push"
+  name                  = "%[1]s"
+  type                  = "push"
+  service_area          = "mainland_china"
+  enterprise_project_id = "%[3]s"
 }
 
 resource "huaweicloud_live_domain" "streamingDomain" {
-  name               = "%s"
-  type               = "pull"
-  ingest_domain_name = huaweicloud_live_domain.ingestDomain.name
+  name                  = "%[2]s"
+  type                  = "pull"
+  ingest_domain_name    = huaweicloud_live_domain.ingestDomain.name
+  service_area          = "outside_mainland_china"
+  enterprise_project_id = "%[3]s"
 }
-`, pushDomain, pullDomain)
+`, pushDomain, pullDomain, acceptance.HW_ENTERPRISE_PROJECT_ID_TEST)
 }
 
 func testDomain_basic_update(pushDomain, pullDomain string) string {
 	return fmt.Sprintf(`
 resource "huaweicloud_live_domain" "ingestDomain" {
-  name = "%s"
-  type = "push"
+  name                  = "%[1]s"
+  type                  = "push"
+  service_area          = "mainland_china"
+  enterprise_project_id = "%[3]s"
 }
 
 resource "huaweicloud_live_domain" "streamingDomain" {
-  name   = "%s"
-  type   = "pull"
-  status = "off"
+  name                  = "%[2]s"
+  type                  = "pull"
+  status                = "off"
+  service_area          = "outside_mainland_china"
+  enterprise_project_id = "%[3]s"
 }
-`, pushDomain, pullDomain)
+`, pushDomain, pullDomain, acceptance.HW_ENTERPRISE_PROJECT_ID_TEST)
 }
