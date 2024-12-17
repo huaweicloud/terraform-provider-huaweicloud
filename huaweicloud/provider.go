1e16c9d24a0393edece127c003e69508222edd6d
houpeng80 <hp_monkey@163.com>
2024-12-17 15:24:40 +0800

    feat(GaussDB): add gaussdb opengauss backup stop resource

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1747,6 +1747,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_gaussdb_opengauss_instance":               gaussdb.ResourceOpenGaussInstance(),
 			"huaweicloud_gaussdb_opengauss_database":               gaussdb.ResourceOpenGaussDatabase(),
 			"huaweicloud_gaussdb_opengauss_backup":                 gaussdb.ResourceGaussDBOpenGaussBackup(),
+			"huaweicloud_gaussdb_opengauss_backup_stop":            gaussdb.ResourceOpenGaussBackupStop(),
 			"huaweicloud_gaussdb_opengauss_eip_associate":          gaussdb.ResourceOpenGaussEipAssociate(),
 			"huaweicloud_gaussdb_opengauss_primary_standby_switch": gaussdb.ResourceOpenGaussPrimaryStandbySwitch(),
 
