605f4fa6ac5959715270b39ccacd33ab23a0c07b
saf3dfsa <caiwanting@huawei.com>
2024-12-26 15:51:40 +0800

    feat(CodeArts/Deploy): support to copy application

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2281,6 +2281,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_codearts_repository": codearts.ResourceRepository(),
 
 			"huaweicloud_codearts_deploy_application":            codeartsdeploy.ResourceDeployApplication(),
+			"huaweicloud_codearts_deploy_application_copy":       codeartsdeploy.ResourceDeployApplicationCopy(),
 			"huaweicloud_codearts_deploy_application_permission": codeartsdeploy.ResourceDeployApplicationPermission(),
 			"huaweicloud_codearts_deploy_application_group":      codeartsdeploy.ResourceDeployApplicationGroup(),
 			"huaweicloud_codearts_deploy_application_group_move": codeartsdeploy.ResourceDeployApplicationGroupMove(),
