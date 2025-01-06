49f2bf29a0223f4f3825caf7563739cde344c550
houpeng80 <hp_monkey@163.com>
2025-01-06 16:13:09 +0800

    feat(GaussDB): add gaussdb opengauss ssl cert download link data source

---
+++ huaweicloud/services/acceptance/gaussdb/data_source_huaweicloud_gaussdb_opengauss_ssl_cert_download_link_test.go
@@ -0,0 +1,103 @@
+package gaussdb
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance/common"
+)
+
+func TestAccDataSourceGaussdbOpengaussSslCertDownloadLink_basic(t *testing.T) {
+	dataSource := "data.huaweicloud_gaussdb_opengauss_ssl_cert_download_link.test"
+	rName := acceptance.RandomAccResourceName()
+	dc := acceptance.InitDataSourceCheck(dataSource)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceGaussdbOpengaussSslCertDownloadLink_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "download_link"),
+				),
+			},
+		},
+	})
+}
+
+func testDataSourceGaussdbOpengaussSslCertDownloadLink_base(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+data "huaweicloud_availability_zones" "test" {}
+
+data "huaweicloud_gaussdb_opengauss_flavors" "test" {
+  version = "8.201"
+  ha_mode = "centralization_standard"
+}
+
+resource "huaweicloud_networking_secgroup_rule" "in_v4_tcp_opengauss" {
+  security_group_id = huaweicloud_networking_secgroup.test.id
+  ethertype         = "IPv4"
+  direction         = "ingress"
+  protocol          = "tcp"
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+resource "huaweicloud_networking_secgroup_rule" "in_v4_tcp_opengauss_egress" {
+  security_group_id = huaweicloud_networking_secgroup.test.id
+  ethertype         = "IPv4"
+  direction         = "egress"
+  protocol          = "tcp"
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+resource "huaweicloud_gaussdb_opengauss_instance" "test" {
+  depends_on = [
+    huaweicloud_networking_secgroup_rule.in_v4_tcp_opengauss,
+    huaweicloud_networking_secgroup_rule.in_v4_tcp_opengauss_egress
+  ]
+
+  vpc_id                = huaweicloud_vpc.test.id
+  subnet_id             = huaweicloud_vpc_subnet.test.id
+  security_group_id     = huaweicloud_networking_secgroup.test.id
+  flavor                = data.huaweicloud_gaussdb_opengauss_flavors.test.flavors[0].spec_code
+  name                  = "%[2]s"
+  password              = "Huangwei!120521"
+  enterprise_project_id = "%[3]s"
+
+  availability_zone = join(",", [data.huaweicloud_availability_zones.test.names[0], 
+                      data.huaweicloud_availability_zones.test.names[1], 
+                      data.huaweicloud_availability_zones.test.names[2]])
+
+  ha {
+    mode             = "centralization_standard"
+    replication_mode = "sync"
+    consistency      = "eventual"
+    instance_mode    = "basic"
+  }
+
+  volume {
+    type = "ULTRAHIGH"
+    size = 40
+  }
+}
+`, common.TestBaseNetwork(name), name, acceptance.HW_ENTERPRISE_PROJECT_ID_TEST)
+}
+
+func testDataSourceGaussdbOpengaussSslCertDownloadLink_basic(name string) string {
+	return fmt.Sprintf(`
+%s
+
+data "huaweicloud_gaussdb_opengauss_ssl_cert_download_link" "test" {
+  instance_id = huaweicloud_gaussdb_opengauss_instance.test.id
+}
+`, testDataSourceGaussdbOpengaussSslCertDownloadLink_base(name))
+}
