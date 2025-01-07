12ae3ed0671526a91ae5ce075573f217e0151f92
profoundwu <profoundwu@outlook.com>
2025-01-07 20:05:09 +0800

    feat(cfw): add this data source to get the list of CFW tags

---
+++ huaweicloud/services/acceptance/cfw/data_source_huaweicloud_cfw_tags_test.go
@@ -0,0 +1,34 @@
+package cfw
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceCfwTags_basic(t *testing.T) {
+	dataSource := "data.huaweicloud_cfw_tags.test"
+	dc := acceptance.InitDataSourceCheck(dataSource)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckCfw(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceDataSourceCfwTags_basic,
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "tags.0.key"),
+					resource.TestCheckResourceAttrSet(dataSource, "tags.0.values.#"),
+				),
+			},
+		},
+	})
+}
+
+const testDataSourceDataSourceCfwTags_basic = `data "huaweicloud_cfw_tags" "test" {}`
