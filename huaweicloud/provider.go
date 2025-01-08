c52f688655a83cb16d8d3dc70f5ccfda284539e7
CaiWanting <caiwanting@huawei.com>
2025-01-08 10:50:32 +0800

    feat(CodeArts/Deploy): support to get environments (#6137)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -586,6 +586,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_codearts_deploy_hosts":              codeartsdeploy.DataSourceCodeartsDeployHosts(),
 			"huaweicloud_codearts_deploy_application_groups": codeartsdeploy.DataSourceCodeartsDeployApplicationGroups(),
 			"huaweicloud_codearts_deploy_applications":       codeartsdeploy.DataSourceCodeartsDeployApplications(),
+			"huaweicloud_codearts_deploy_environments":       codeartsdeploy.DataSourceCodeartsDeployEnvironments(),
 
 			"huaweicloud_cts_notifications": cts.DataSourceNotifications(),
 			"huaweicloud_cts_traces":        cts.DataSourceCtsTraces(),
