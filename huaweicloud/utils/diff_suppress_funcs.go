8f92032ed12fa927b78de88dac9175211f1c89e9
Lance52259 <Overlord52259@gmail.com>
2025-01-09 15:38:28 +0800

    feat(servicestage/component): supports new diffsuppress func for json params

--- huaweicloud/utils/diff_suppress_funcs.go
+++ huaweicloud/utils/diff_suppress_funcs.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"log"
 	"reflect"
 	"regexp"
@@ -152,4 +153,139 @@ func SuppressStringSepratedByCommaDiffs(_, old, new string, _ *schema.ResourceDa
 	sort.Strings(newArray)
 
 	return reflect.DeepEqual(oldArray, newArray)
+}
+
+// ContainsAllKeyValues ​​checks whether object A (type map[string]interface{}) recursively contains all the keys and
+// corresponding values ​​of object B (type map[string]interface{}).
+// If the key-value pair in object B exists in object A and the values ​​are equal (recursively processing nested maps),
+// return true; otherwise return false.
+func ContainsAllKeyValues(objA, objB map[string]interface{}) bool {
+	for key, bVal := range objB {
+		aVal, exists := objA[key]
+		if !exists {
+			return false // A is missing the key of B.
+		}
+
+		// Check if the values ​​are both nested maps, if so, recursively compare.
+		aMap, aIsMap := aVal.(map[string]interface{})
+		bMap, bIsMap := bVal.(map[string]interface{})
+		if aIsMap && bIsMap {
+			if !ContainsAllKeyValues(aMap, bMap) {
+				return false
+			}
+		} else {
+			// Non-map types are compared directly via DeepEqual().
+			if !reflect.DeepEqual(bVal, aVal) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// FindDecreaseKeys is a method that used to find out the key that objB is missing compared to objA.
+// Will ignore the increase parts.
+func FindDecreaseKeys(objA, objB map[string]interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	for key, valA := range objA {
+		if valB, exists := objB[key]; !exists {
+			// If the key does not exist in objB, it's considered as a decrease key and is added directly to the result.
+			result[key] = valA
+		} else {
+			// Check if the current values (valA and valB) are both type map for recursive processing.
+			mapA, okA := valA.(map[string]interface{})
+			mapB, okB := valB.(map[string]interface{})
+			// If either valA or valB is not of type map, the subsequent recursive comparison is performed.
+			if okA && okB {
+				subResult := FindDecreaseKeys(mapA, mapB)
+				if len(subResult) > 0 {
+					result[key] = subResult
+				}
+			}
+		}
+	}
+	return result
+}
+
+// SuppressObjectDiffs is a method that determines whether a parameter triggers a change based on the comparison between
+// the server return value, the old value of the script, and the new value of the script.
+// The following three scenarios will determine whether the parameter has changed (method return false):
+//  1. The new value of the script adds some keys compared to the server return value (which must include keys that do
+//     not exist in the value returned by the server).
+//  2. The new value of the script modifies some (or all) key/value compared to the server return value.
+//  3. The new value of the script removes some (or all) key/value compared to the old value of the script (the key can
+//     be a nested structure).
+//
+// The following are examples of related scenarios:
+//
+// Service result:
+//
+//	{
+//		"A": {
+//			"Aa": "aa_aa",
+//			"Ab": "aa_bb"
+//		},
+//		"B": "bb",
+//		"C": "cc",
+//		"D": "dd"
+//	}
+//
+// Example 1 (Key 'D' add but the value is the same as the service result, so return true):
+//
+//	{					{
+//		"B": "bb",			"B": "bb",
+//		"C": "cc"	->		"C": "cc",
+//	}						"D": "dd"
+//						}
+//
+// Example 2 (New key 'D' addreturn false):
+//
+//	{					{
+//		"B": "bb",			"B": "bb",
+//		"C": "cc",	->		"C": "cc",
+//	}						"E": "ee"
+//						}
+//
+// Example 3 (The value of key 'C' changed, return false):
+//
+//	{					{
+//		"B": "bb",			"B": "bb",
+//		"C": "cc",	->		"C": "ccc"
+//	}					}
+//
+// Example 4 (The value of key 'A.Aa' changed, return false):
+//
+//	{							{
+//		"A": {						"A": {
+//			"Aa": "aa_aa"				"Aa": "aa_aaa"
+//		},					->		},
+//		"B": "bb"					"B": "bb"
+//	}							}
+//
+// Example 5 (Key 'D' removed, even it is exist in the service result, return false):
+//
+//	{					{
+//		"B": "bb",			"B": "bb",
+//		"C": "cc",	->		"C": "cc"
+//		"D": "dd"		}
+//	}
+func SuppressObjectDiffs() schema.SchemaDiffSuppressFunc {
+	return func(paramKey, _, _ string, d *schema.ResourceData) bool {
+		var (
+			consoleVal, newScriptVal, originVal map[string]interface{}
+
+			originParamKey           = fmt.Sprintf("%s_origin", paramKey)
+			oldParamVal, newParamVal = d.GetChange(paramKey)
+		)
+
+		// After refresh phase, the value from the service side will be stored in the tfstate, and as old value in the
+		// next d.GetChange() method returns.
+		consoleVal = TryMapValueAnalysis(oldParamVal)
+		newScriptVal = TryMapValueAnalysis(newParamVal)
+		// The script value of the last update (if it has) is used as a reference for the historical value of this
+		// change.
+		originVal = TryMapValueAnalysis(d.Get(originParamKey))
+
+		return ContainsAllKeyValues(consoleVal, newScriptVal) && len(FindDecreaseKeys(originVal, newScriptVal)) < 1
+	}
 }
---
+++ huaweicloud/utils/diff_suppress_funcs_test.go
@@ -0,0 +1,125 @@
+package utils_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+func TestDiffSuppressFunc_ContainsAllKeyValues(t *testing.T) {
+	var (
+		compareObject = map[string]interface{}{
+			"A": map[string]interface{}{
+				"Aa": "aa_aa",
+				"Ab": "aa_bb",
+			},
+			"B": map[string]interface{}{
+				"Ba": true,
+				"Bb": 123,
+			},
+			"C": "cc",
+			"D": 456,
+		}
+		objects = []map[string]interface{}{
+			{
+				"A": map[string]interface{}{
+					"Aa": "aa_aa",
+				},
+				"B": map[string]interface{}{
+					"Bb": 123,
+				},
+				"C": "cc",
+			},
+			{
+				"A": map[string]interface{}{
+					"Aa": "aa_aaa", // Value changed.
+				},
+			},
+			{
+				"B": map[string]interface{}{
+					"Ba": false,
+				},
+			},
+			{
+				"B": map[string]interface{}{
+					"Ba": nil,
+				},
+			},
+			{
+				"B": map[string]interface{}{
+					"Bb": true, // Change the value type from integer to boolean.
+				},
+			},
+		}
+	)
+
+	testOutput := utils.ContainsAllKeyValues(compareObject, objects[0])
+	if !reflect.DeepEqual(testOutput, true) {
+		t.Fatalf("[Check 1] The processing result of the ContainsAllKeyValues method is not as expected, want %s, but got %s",
+			utils.Green(true), utils.Yellow(testOutput))
+	}
+
+	testOutput = utils.ContainsAllKeyValues(compareObject, objects[1])
+	if !reflect.DeepEqual(testOutput, false) {
+		t.Fatalf("[Check 2] The processing result of the ContainsAllKeyValues method is not as expected, want %s, but got %s",
+			utils.Green(false), utils.Yellow(testOutput))
+	}
+
+	testOutput = utils.ContainsAllKeyValues(compareObject, objects[2])
+	if !reflect.DeepEqual(testOutput, false) {
+		t.Fatalf("[Check 3] The processing result of the ContainsAllKeyValues method is not as expected, want %s, but got %s",
+			utils.Green(false), utils.Yellow(testOutput))
+	}
+
+	testOutput = utils.ContainsAllKeyValues(compareObject, objects[3])
+	if !reflect.DeepEqual(testOutput, false) {
+		t.Fatalf("[Check 4] The processing result of the ContainsAllKeyValues method is not as expected, want %s, but got %s",
+			utils.Green(false), utils.Yellow(testOutput))
+	}
+
+	t.Logf("All processing results of the ContainsAllKeyValues method meets expectation")
+}
+
+func TestDiffSuppressFunc_FindDecreaseKeys(t *testing.T) {
+	var (
+		decreaseCacls = []map[string]interface{}{
+			{
+				"A": map[string]interface{}{
+					"Aa": "aa_aa",
+					"Ab": "aa_bb",
+				},
+				"B": map[string]interface{}{
+					"Ba": true,
+					"Bb": 123,
+				},
+				"C": "cc",
+			},
+			{
+				"A": map[string]interface{}{
+					"Aa": "aa_aaa", // The key is exist, but the value changed.
+					"Ac": "aa_cc",
+				},
+				"B": map[string]interface{}{
+					"Ba": 123, // The key is exist, but the value changed.
+					"Bb": nil, // The key is exist, but the value changed.
+				},
+				// The key 'C' is removed.
+			},
+			{
+				"A": map[string]interface{}{
+					"Ab": "aa_bb",
+				},
+				"C": "cc",
+			},
+		}
+	)
+
+	testOutput := utils.FindDecreaseKeys(decreaseCacls[0], decreaseCacls[1])
+	if !reflect.DeepEqual(testOutput, decreaseCacls[2]) {
+		t.Fatalf("The processing result of the FindDecreaseKeys method is not as expected, want %s, but got %s",
+			utils.Green(decreaseCacls[2]), utils.Yellow(testOutput))
+	}
+
+	t.Logf("All processing results of the FindDecreaseKeys method meets expectation")
+}
