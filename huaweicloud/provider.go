e6b5e18cc1213895eee49804160776f3b95f9c60
profoundwu <profoundwu@outlook.com>
2025-01-09 17:32:33 +0800

    feat(cfw): add this data source to get the list of CFW tags (#6160)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -571,6 +571,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_cfw_flow_logs":             cfw.DataSourceCfwFlowLogs(),
 			"huaweicloud_cfw_regions":               cfw.DataSourceCfwRegions(),
 			"huaweicloud_cfw_ips_rules":             cfw.DataSourceCfwIpsRules(),
+			"huaweicloud_cfw_tags":                  cfw.DataSourceCfwTags(),
 
 			"huaweicloud_cnad_advanced_instances":         cnad.DataSourceInstances(),
 			"huaweicloud_cnad_advanced_available_objects": cnad.DataSourceAvailableProtectedObjects(),
