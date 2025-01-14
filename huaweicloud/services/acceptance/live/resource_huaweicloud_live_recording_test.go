8c4391fd6b94badcf50c0d0360fbb0ec1f895fb9
ruwenqiang123 <ruwenqiang123@outlook.com>
2025-01-14 14:32:17 +0800

    refactor(live): refactor the recording resource code style

--- huaweicloud/services/acceptance/live/resource_huaweicloud_live_recording_test.go
+++ huaweicloud/services/acceptance/live/resource_huaweicloud_live_recording_test.go
@@ -2,34 +2,55 @@ package live
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
-	"github.com/huaweicloud/huaweicloud-sdk-go-v3/services/live/v1/model"
+	"github.com/chnsz/golangsdk"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
-func getRecordingResourceFunc(conf *config.Config, state *terraform.ResourceState) (interface{}, error) {
-	client, err := conf.HcLiveV1Client(acceptance.HW_REGION_NAME)
+func getRecordingFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	region := acceptance.HW_REGION_NAME
+	client, err := cfg.NewServiceClient("live", region)
 	if err != nil {
-		return nil, fmt.Errorf("error creating Live v1 client: %s", err)
+		return nil, fmt.Errorf("error creating Live client: %s", err)
 	}
-	return client.ShowRecordRule(&model.ShowRecordRuleRequest{Id: state.Primary.ID})
+
+	getHttpUrl := "v1/{project_id}/record/rules/{id}"
+	getPath := client.Endpoint + getHttpUrl
+	getPath = strings.ReplaceAll(getPath, "{project_id}", client.ProjectID)
+	getPath = strings.ReplaceAll(getPath, "{id}", state.Primary.ID)
+
+	getOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+
+	getResp, err := client.Request("GET", getPath, &getOpt)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving Live recording: %s", err)
+	}
+
+	return utils.FlattenResponse(getResp)
 }
 
 func TestAccRecording_basic(t *testing.T) {
-	var obj model.ShowRecordRuleRequest
-
-	rName := "huaweicloud_live_recording.test"
+	var (
+		randInt      = acctest.RandInt()
+		rName        = "huaweicloud_live_recording.test"
+		recordingObj interface{}
+	)
 
 	rc := acceptance.InitResourceCheck(
 		rName,
-		&obj,
-		getRecordingResourceFunc,
+		&recordingObj,
+		getRecordingFunc,
 	)
 
 	resource.Test(t, resource.TestCase{
@@ -41,7 +62,7 @@ func TestAccRecording_basic(t *testing.T) {
 		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: testRecording_basic(),
+				Config: testRecording_basic(randInt),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
 					resource.TestCheckResourceAttr(rName, "domain_name", acceptance.HW_LIVE_INGEST_DOMAIN_NAME),
@@ -49,22 +70,30 @@ func TestAccRecording_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(rName, "stream_name", "streamname"),
 					resource.TestCheckResourceAttr(rName, "type", "CONTINUOUS_RECORD"),
 					resource.TestCheckResourceAttr(rName, "obs.0.region", acceptance.HW_REGION_NAME),
-					resource.TestCheckResourceAttr(rName, "mp4.0.recording_length", "120"),
+					resource.TestCheckResourceAttr(rName, "hls.0.recording_length", "60"),
+					resource.TestCheckResourceAttr(rName, "hls.0.record_slice_duration", "3"),
+					resource.TestCheckResourceAttr(rName, "hls.0.max_stream_pause_length", "-1"),
+					resource.TestCheckResourceAttrSet(rName, "hls.0.file_naming"),
+					resource.TestCheckResourceAttrSet(rName, "hls.0.ts_file_naming"),
+					resource.TestCheckResourceAttr(rName, "flv.0.recording_length", "80"),
+					resource.TestCheckResourceAttr(rName, "flv.0.max_stream_pause_length", "0"),
+					resource.TestCheckResourceAttrSet(rName, "flv.0.file_naming"),
+					resource.TestCheckResourceAttr(rName, "mp4.0.recording_length", "100"),
+					resource.TestCheckResourceAttr(rName, "mp4.0.max_stream_pause_length", "10"),
 					resource.TestCheckResourceAttrSet(rName, "mp4.0.file_naming"),
 				),
 			},
 			{
-				Config: testRecording_basic_update(),
+				Config: testRecording_update(randInt),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(rName, "domain_name", acceptance.HW_LIVE_INGEST_DOMAIN_NAME),
-					resource.TestCheckResourceAttr(rName, "app_name", "live"),
-					resource.TestCheckResourceAttr(rName, "stream_name", "streamname"),
-					resource.TestCheckResourceAttr(rName, "type", "CONTINUOUS_RECORD"),
-					resource.TestCheckResourceAttr(rName, "obs.0.region", acceptance.HW_REGION_NAME),
 					resource.TestCheckResourceAttr(rName, "hls.0.recording_length", "120"),
-					resource.TestCheckResourceAttr(rName, "hls.0.record_slice_duration", "5"),
+					resource.TestCheckResourceAttr(rName, "hls.0.record_slice_duration", "15"),
+					resource.TestCheckResourceAttr(rName, "hls.0.max_stream_pause_length", "8"),
 					resource.TestCheckResourceAttrSet(rName, "hls.0.file_naming"),
+					resource.TestCheckResourceAttr(rName, "mp4.0.recording_length", "90"),
+					resource.TestCheckResourceAttr(rName, "mp4.0.max_stream_pause_length", "0"),
+					resource.TestCheckResourceAttrSet(rName, "mp4.0.file_naming"),
 				),
 			},
 			{
@@ -76,29 +105,21 @@ func TestAccRecording_basic(t *testing.T) {
 	})
 }
 
-func testRecording_base() string {
+func testRecording_base(randInt int) string {
 	return fmt.Sprintf(`
-resource "huaweicloud_obs_bucket" "bucket" {
-  bucket        = "%[1]s"
+resource "huaweicloud_obs_bucket" "test" {
+  bucket        = "tf-bucket-%d"
+  storage_class = "STANDARD"
   acl           = "private"
-  force_destroy = true
-
-  lifecycle {
-    ignore_changes = [
-      cors_rule,
-    ]
-  }
 }
 
 resource "huaweicloud_live_bucket_authorization" "test" {
-  depends_on = [huaweicloud_obs_bucket.bucket]
-
-  bucket = "%[1]s"
+  bucket = huaweicloud_obs_bucket.test.bucket
 }
-`, acceptance.RandomAccResourceNameWithDash())
+`, randInt)
 }
 
-func testRecording_basic() string {
+func testRecording_basic(randInt int) string {
 	return fmt.Sprintf(`
 %[1]s
 
@@ -111,38 +132,55 @@ resource "huaweicloud_live_recording" "test" {
   type        = "CONTINUOUS_RECORD"
 
   obs {
-    region = huaweicloud_obs_bucket.bucket.region
-    bucket = huaweicloud_obs_bucket.bucket.bucket
-  }
-
-  mp4 {
-    recording_length = 120
-  }
-}
-`, testRecording_base(), acceptance.HW_LIVE_INGEST_DOMAIN_NAME)
-}
-
-func testRecording_basic_update() string {
-	return fmt.Sprintf(`
-%[1]s
-
-resource "huaweicloud_live_recording" "test" {
-  depends_on = [huaweicloud_live_bucket_authorization.test]
-
-  domain_name = "%[2]s"
-  app_name    = "live"
-  stream_name = "streamname"
-  type        = "CONTINUOUS_RECORD"
-
-  obs {
-    region = huaweicloud_obs_bucket.bucket.region
-    bucket = huaweicloud_obs_bucket.bucket.bucket
+    region = huaweicloud_obs_bucket.test.region
+    bucket = huaweicloud_obs_bucket.test.bucket
   }
 
   hls {
-    recording_length      = 120
-    record_slice_duration = 5
+    recording_length        = 60
+    record_slice_duration   = 3
+    max_stream_pause_length = -1
+  }
+
+  flv {
+    recording_length = 80
+  }
+
+  mp4 {
+    recording_length        = 100
+    max_stream_pause_length = 10
   }
 }
-`, testRecording_base(), acceptance.HW_LIVE_INGEST_DOMAIN_NAME)
+`, testRecording_base(randInt), acceptance.HW_LIVE_INGEST_DOMAIN_NAME)
+}
+
+func testRecording_update(randInt int) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_live_recording" "test" {
+  depends_on = [huaweicloud_live_bucket_authorization.test]
+
+  domain_name = "%[2]s"
+  app_name    = "live"
+  stream_name = "streamname"
+  type        = "CONTINUOUS_RECORD"
+
+  obs {
+    region = huaweicloud_obs_bucket.test.region
+    bucket = huaweicloud_obs_bucket.test.bucket
+  }
+
+  hls {
+    recording_length        = 120
+    record_slice_duration   = 15
+    max_stream_pause_length = 8
+  }
+
+  mp4 {
+    recording_length        = 90
+    max_stream_pause_length = 0
+  }
+}
+`, testRecording_base(randInt), acceptance.HW_LIVE_INGEST_DOMAIN_NAME)
 }
