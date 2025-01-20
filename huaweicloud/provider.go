3daf90994cfa44564449a37f3741a38d30a261ae
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-01-20 09:48:31 +0800

    feat(cae): add new resource to manage domain name (#6179)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1438,6 +1438,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_cae_component":                cae.ResourceComponent(),
 			"huaweicloud_cae_component_configurations": cae.ResourceComponentConfigurations(),
 			"huaweicloud_cae_component_deployment":     cae.ResourceComponentDeployment(),
+			"huaweicloud_cae_domain":                   cae.ResourceDomain(),
 			"huaweicloud_cae_environment":              cae.ResourceEnvironment(),
 			"huaweicloud_cae_notification_rule":        cae.ResourceNotificationRule(),
 			"huaweicloud_cae_vpc_egress":               cae.ResourceVpcEgress(),
