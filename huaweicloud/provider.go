c8f09947c4be1b72212b73d7434fad4e5b6dacc8
Lance52259 <Overlord52259@gmail.com>
2025-01-20 11:03:02 +0800

    feat(fgs/function): add new resource for function topping

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1804,6 +1804,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_fgs_dependency_version":         fgs.ResourceDependencyVersion(),
 			"huaweicloud_fgs_function":                   fgs.ResourceFgsFunctionV2(),
 			"huaweicloud_fgs_function_event":             fgs.ResourceFunctionEvent(),
+			"huaweicloud_fgs_function_topping":           fgs.ResourceFunctionTopping(),
 			"huaweicloud_fgs_function_trigger":           fgs.ResourceFunctionTrigger(),
 
 			"huaweicloud_ga_accelerator":    ga.ResourceAccelerator(),
