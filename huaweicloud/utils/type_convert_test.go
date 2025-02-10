2912eff9d3a159740b0d0a4ee3e621147656b3fb
luxiaohang <deerhang@163.com>
2025-02-10 19:43:05 +0800

    feat: add new utils function to unmarshal json array string

--- huaweicloud/utils/type_convert_test.go
+++ huaweicloud/utils/type_convert_test.go
@@ -42,6 +42,43 @@ func TestTypeConvertFunc_StringToJson(t *testing.T) {
 	t.Logf("All processing results of the JsonToString method meets expectation")
 }
 
+func TestTypeConvertFunc_StringToJsonArray(t *testing.T) {
+	var (
+		emptyInput           = "[]"
+		correctInput         = "[{\"key1\":\"value1\"},{\"key2\":\"value2\"}]"
+		incorrectInput       = "{\"foo\":\"bar\"}"
+		emptyInputExpected   = make([]map[string]interface{}, 0)
+		correctInputExpected = []map[string]interface{}{
+			{
+				"key1": "value1",
+			},
+			{
+				"key2": "value2",
+			},
+		}
+	)
+
+	testOutput := utils.StringToJsonArray(emptyInput)
+	if !reflect.DeepEqual(testOutput, emptyInputExpected) {
+		t.Fatalf("The processing result of the StringToJsonArray method is not as expected, want %s, but got %s",
+			utils.Green(emptyInputExpected), utils.Yellow(testOutput))
+	}
+
+	testOutput = utils.StringToJsonArray(correctInput)
+	if !reflect.DeepEqual(testOutput, correctInputExpected) {
+		t.Fatalf("The processing result of the StringToJsonArray method is not as expected, want %s, but got %s",
+			utils.Green(correctInputExpected), utils.Yellow(testOutput))
+	}
+
+	testOutput = utils.StringToJsonArray(incorrectInput)
+	if !reflect.DeepEqual(testOutput, make([]map[string]interface{}, 0)) {
+		t.Fatalf("The processing result of the StringToJsonArray method is not as expected, want \"\", but got %s",
+			utils.Yellow(testOutput))
+	}
+
+	t.Logf("All processing results of the StringToJsonArray method meets expectation")
+}
+
 func TestTypeConvertFunc_JsonToString(t *testing.T) {
 	type Test struct {
 		Foo string `json:"foo,omitempty"`
