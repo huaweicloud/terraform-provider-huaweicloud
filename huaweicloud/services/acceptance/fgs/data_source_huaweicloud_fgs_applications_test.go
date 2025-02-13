8a7e40b03e7f7e1e6af0886e94aca87f37fa17e5
Lance <Overlord52259@gmail.com>
2025-02-13 15:34:47 +0800

    refactor(fgs/application): adjust the data source coding and the acceptance test (#6316)
    * test(fgs/application): adjust the acceptance test coding
    
    * refactor(fgs/application): adjust the data source coding

--- huaweicloud/services/acceptance/fgs/data_source_huaweicloud_fgs_applications_test.go
+++ huaweicloud/services/acceptance/fgs/data_source_huaweicloud_fgs_applications_test.go
@@ -2,17 +2,41 @@ package fgs
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
+	"github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccDatasourceFunctionGraphApplication_basic(t *testing.T) {
-	name := acceptance.RandomAccResourceName()
-	rName := "data.huaweicloud_fgs_applications.test"
-	dc := acceptance.InitDataSourceCheck(rName)
+func TestAccDataApplications_basic(t *testing.T) {
+	var (
+		rcName       = "huaweicloud_fgs_application.test"
+		all          = "data.huaweicloud_fgs_applications.all"
+		dcForAllApps = acceptance.InitDataSourceCheck(all)
+
+		byAppId           = "data.huaweicloud_fgs_applications.filter_by_app_id"
+		dcByAppId         = acceptance.InitDataSourceCheck(byAppId)
+		byNotFoundAppId   = "data.huaweicloud_fgs_applications.filter_by_not_found_app_id"
+		dcByNotFoundAppId = acceptance.InitDataSourceCheck(byNotFoundAppId)
+
+		byAppName           = "data.huaweicloud_fgs_applications.filter_by_app_name"
+		dcByAppName         = acceptance.InitDataSourceCheck(byAppName)
+		byNotFoundAppName   = "data.huaweicloud_fgs_applications.filter_by_not_found_app_name"
+		dcByNotFoundAppName = acceptance.InitDataSourceCheck(byNotFoundAppName)
+
+		byAppStatus           = "data.huaweicloud_fgs_applications.filter_by_app_status"
+		dcByAppStatus         = acceptance.InitDataSourceCheck(byAppStatus)
+		byNotFoundAppStatus   = "data.huaweicloud_fgs_applications.filter_by_not_found_app_status"
+		dcByNotFoundAppStatus = acceptance.InitDataSourceCheck(byNotFoundAppStatus)
+
+		byAppDesc           = "data.huaweicloud_fgs_applications.filter_by_app_desc"
+		dcByAppDesc         = acceptance.InitDataSourceCheck(byAppDesc)
+		byNotFoundAppDesc   = "data.huaweicloud_fgs_applications.filter_by_not_found_app_desc"
+		dcByNotFoundAppDesc = acceptance.InitDataSourceCheck(byNotFoundAppDesc)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
@@ -21,72 +45,192 @@ func TestAccDatasourceFunctionGraphApplication_basic(t *testing.T) {
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDatasourceFunctionGraphApplications_basic(name),
+				Config: testAccDataApplications_basic(),
 				Check: resource.ComposeTestCheckFunc(
-					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttrSet(rName, "applications.0.id"),
-					resource.TestCheckResourceAttrSet(rName, "applications.0.name"),
-					resource.TestCheckResourceAttrSet(rName, "applications.0.status"),
-
-					resource.TestCheckOutput("application_id_filter_is_useful", "true"),
-					resource.TestCheckOutput("name_filter_is_useful", "true"),
-					resource.TestCheckOutput("status_filter_is_useful", "true"),
+					dcForAllApps.CheckResourceExists(),
+					resource.TestMatchResourceAttr(all, "applications.#", regexp.MustCompile(`[1-9][0-9]*`)),
+					// Filter by application ID.
+					dcByAppId.CheckResourceExists(),
+					resource.TestCheckOutput("is_app_id_filter_useful", "true"),
+					dcByNotFoundAppId.CheckResourceExists(),
+					resource.TestCheckOutput("app_id_not_found_validation_pass", "true"),
+					// Filter by application name.
+					dcByAppName.CheckResourceExists(),
+					resource.TestCheckOutput("is_app_name_filter_useful", "true"),
+					dcByNotFoundAppName.CheckResourceExists(),
+					resource.TestCheckOutput("app_name_not_found_validation_pass", "true"),
+					// Filter by application status.
+					dcByAppStatus.CheckResourceExists(),
+					resource.TestCheckOutput("is_app_status_filter_useful", "true"),
+					dcByNotFoundAppStatus.CheckResourceExists(),
+					resource.TestCheckOutput("app_status_not_found_validation_pass", "true"),
+					// Filter by application description.
+					dcByAppDesc.CheckResourceExists(),
+					resource.TestCheckOutput("is_app_desc_filter_useful", "true"),
+					dcByNotFoundAppDesc.CheckResourceExists(),
+					resource.TestCheckOutput("app_desc_not_found_validation_pass", "true"),
+					// Check attributes.
+					resource.TestCheckResourceAttrPair(byAppId, "applications.0.id", rcName, "id"),
+					resource.TestCheckResourceAttrPair(byAppId, "applications.0.name", rcName, "name"),
+					resource.TestCheckResourceAttrPair(byAppId, "applications.0.status", rcName, "status"),
+					resource.TestCheckResourceAttrPair(byAppId, "applications.0.description", rcName, "description"),
+					resource.TestMatchResourceAttr(byAppId, "applications.0.updated_at",
+						regexp.MustCompile(`^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}?(Z|([+-]\d{2}:\d{2}))$`)),
 				),
 			},
 		},
 	})
 }
 
-func testAccDatasourceFunctionGraphApplications_basic(name string) string {
-	return fmt.Sprintf(`
-%s
+func testAccDataApplications_basic() string {
+	name := acceptance.RandomAccResourceName()
+	randAppId, _ := uuid.GenerateUUID()
 
-data "huaweicloud_fgs_applications" "test" {
+	return fmt.Sprintf(`
+%[1]s
+
+# Without any filter parameter.
+data "huaweicloud_fgs_applications" "all" {
+  // Query applications after application resource create.
   depends_on = [
-    huaweicloud_fgs_application.test
+    huaweicloud_fgs_application.test,
   ]
 }
 
-data "huaweicloud_fgs_applications" "application_id_filter" {
-  application_id = local.application_id
-}
-  
+// Filter by application ID.
 locals {
-  application_id = data.huaweicloud_fgs_applications.test.applications[0].id
-}
-  
-output "application_id_filter_is_useful" {
-  value = length(data.huaweicloud_fgs_applications.application_id_filter.applications) > 0 && alltrue(
-    [for v in data.huaweicloud_fgs_applications.application_id_filter.applications[*].id : v == local.application_id]
-  )
+  app_id = huaweicloud_fgs_application.test.id
 }
 
-data "huaweicloud_fgs_applications" "name_filter" {
-  name = local.name
-}
-  
-locals {
-  name = data.huaweicloud_fgs_applications.test.applications[0].name
-}
-  
-output "name_filter_is_useful" {
-  value = length(data.huaweicloud_fgs_applications.name_filter.applications) > 0 && alltrue(
-    [for v in data.huaweicloud_fgs_applications.name_filter.applications[*].name : v == local.name]
-  )
+data "huaweicloud_fgs_applications" "filter_by_app_id" {
+  application_id = local.app_id
 }
 
-data "huaweicloud_fgs_applications" "status_filter" {
-  status = local.status
+data "huaweicloud_fgs_applications" "filter_by_not_found_app_id" {
+  // Query applications using a not exist ID after application resource create.
+  depends_on = [
+    huaweicloud_fgs_application.test,
+  ]
+
+  application_id = "%[2]s"
 }
 
 locals {
-  status = data.huaweicloud_fgs_applications.test.applications[0].status
+  app_id_filter_result = [for v in data.huaweicloud_fgs_applications.filter_by_app_id.applications[*].id :
+    v == local.app_id]
 }
 
-output "status_filter_is_useful" {
-  value = length(data.huaweicloud_fgs_applications.status_filter.applications) > 0 && alltrue(
-    [for v in data.huaweicloud_fgs_applications.status_filter.applications[*].status : v == local.status]
-  )
+output "is_app_id_filter_useful" {
+  value = length(local.app_id_filter_result) > 0 && alltrue(local.app_id_filter_result)
 }
-`, testAccApplication_basic(name))
+
+output "app_id_not_found_validation_pass" {
+  value = length(data.huaweicloud_fgs_applications.filter_by_not_found_app_id.applications) == 0
+}
+
+// Filter by application name.
+locals {
+  app_name = huaweicloud_fgs_application.test.name
+}
+
+data "huaweicloud_fgs_applications" "filter_by_app_name" {
+  // The behavior of parameter 'name' of the application resource is 'Required', means this parameter does not
+  // have 'Know After Apply' behavior.
+  depends_on = [
+    huaweicloud_fgs_application.test,
+  ]
+
+  name = local.app_name
+}
+
+data "huaweicloud_fgs_applications" "filter_by_not_found_app_name" {
+  // Query applications using a not exist name after application resource create.
+  depends_on = [
+    huaweicloud_fgs_application.test,
+  ]
+
+  name = "app_name_not_found"
+}
+
+locals {
+  app_name_filter_result = [for v in data.huaweicloud_fgs_applications.filter_by_app_name.applications[*].name :
+    v == local.app_name]
+}
+
+output "is_app_name_filter_useful" {
+  value = length(local.app_name_filter_result) > 0 && alltrue(local.app_name_filter_result)
+}
+
+output "app_name_not_found_validation_pass" {
+  value = length(data.huaweicloud_fgs_applications.filter_by_not_found_app_name.applications) == 0
+}
+
+// Filter by application status.
+locals {
+  app_status = huaweicloud_fgs_application.test.status
+}
+
+data "huaweicloud_fgs_applications" "filter_by_app_status" {
+  status = local.app_status
+}
+
+data "huaweicloud_fgs_applications" "filter_by_not_found_app_status" {
+  // Query application using a not exist status after application resource create.
+  depends_on = [
+    huaweicloud_fgs_application.test,
+  ]
+
+  status = "app_status_not_found"
+}
+
+locals {
+  app_status_filter_result = [for v in data.huaweicloud_fgs_applications.filter_by_app_status.applications[*].status :
+    v == local.app_status]
+}
+
+output "is_app_status_filter_useful" {
+  value = length(local.app_status_filter_result) > 0 && alltrue(local.app_status_filter_result)
+}
+
+output "app_status_not_found_validation_pass" {
+  value = length(data.huaweicloud_fgs_applications.filter_by_not_found_app_status.applications) == 0
+}
+
+// Filter by application description.
+locals {
+  app_desc = huaweicloud_fgs_application.test.description
+}
+
+data "huaweicloud_fgs_applications" "filter_by_app_desc" {
+  // The behavior of parameter 'description' of the resource is not have the 'Computed', means this parameter does not
+  // have 'Know After Apply' behavior.
+  depends_on = [
+    huaweicloud_fgs_application.test,
+  ]
+
+  description = local.app_desc
+}
+
+data "huaweicloud_fgs_applications" "filter_by_not_found_app_desc" {
+  // Query applications using a not exist description after application resource create.
+  depends_on = [
+    huaweicloud_fgs_application.test,
+  ]
+
+  description = "app_desc_not_found"
+}
+
+locals {
+  app_desc_filter_result = [for v in data.huaweicloud_fgs_applications.filter_by_app_desc.applications[*].description :
+    v == local.app_desc]
+}
+
+output "is_app_desc_filter_useful" {
+  value = length(local.app_desc_filter_result) > 0 && alltrue(local.app_desc_filter_result)
+}
+
+output "app_desc_not_found_validation_pass" {
+  value = length(data.huaweicloud_fgs_applications.filter_by_not_found_app_desc.applications) == 0
+}
+`, testAccApplication_basic(name), randAppId)
 }
--- huaweicloud/services/fgs/data_source_huaweicloud_fgs_applications.go
+++ huaweicloud/services/fgs/data_source_huaweicloud_fgs_applications.go
@@ -2,6 +2,7 @@ package fgs
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	"github.com/hashicorp/go-multierror"
@@ -16,35 +17,41 @@ import (
 )
 
 // @API FunctionGraph GET /v2/{project_id}/fgs/applications
-func DataSourceFunctionGraphApplications() *schema.Resource {
+func DataSourceApplications() *schema.Resource {
 	return &schema.Resource{
-		ReadContext: datasourceFunctionGraphApplicationsRead,
+		ReadContext: dataSourceApplicationsRead,
 		Schema: map[string]*schema.Schema{
 			"region": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				Description: `The region where the applications are located.`,
 			},
 			"application_id": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `The application ID used to query specified application.`,
 			},
 			"name": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `The application name used to query specified application.`,
 			},
 			"status": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `The status of the application to be queried.`,
 			},
 			"description": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `The description of the application to be queried.`,
 			},
 			"applications": {
-				Type:     schema.TypeList,
-				Elem:     applicationSchema(),
-				Computed: true,
+				Type:        schema.TypeList,
+				Elem:        applicationSchema(),
+				Computed:    true,
+				Description: `All applications that match the filter parameters.`,
 			},
 		},
 	}
@@ -54,69 +61,131 @@ func applicationSchema() *schema.Resource {
 	sc := schema.Resource{
 		Schema: map[string]*schema.Schema{
 			"id": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The ID of application.`,
 			},
 			"name": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The name of application.`,
 			},
 			"status": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The status of application.`,
 			},
 			"updated_at": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The description of application.`,
 			},
 			"description": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: `The latest update time of the application, in RFC3339 format.`,
 			},
 		},
 	}
 	return &sc
 }
 
-func datasourceFunctionGraphApplicationsRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func getApplications(client *golangsdk.ServiceClient) ([]interface{}, error) {
 	var (
-		cfg                     = meta.(*config.Config)
-		region                  = cfg.GetRegion(d)
-		mErr                    *multierror.Error
-		httpUrl                 = "v2/{project_id}/fgs/applications"
-		listApplicationsProduct = "fgs"
+		httpUrl = "v2/{project_id}/fgs/applications"
+		marker  float64
+		result  = make([]interface{}, 0)
 	)
 
-	client, err := cfg.NewServiceClient(listApplicationsProduct, region)
+	listPath := client.Endpoint + httpUrl
+	listPath = strings.ReplaceAll(listPath, "{project_id}", client.ProjectID)
+	listOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+
+	for {
+		listPathWithMarker := fmt.Sprintf("%s?marker=%v", listPath, marker)
+		requestResp, err := client.Request("GET", listPathWithMarker, &listOpt)
+		if err != nil {
+			return nil, err
+		}
+		respBody, err := utils.FlattenResponse(requestResp)
+		if err != nil {
+			return nil, err
+		}
+		applications := utils.PathSearch("applications", respBody, make([]interface{}, 0)).([]interface{})
+		if len(applications) < 1 {
+			break
+		}
+		result = append(result, applications...)
+		// The attribute page_info.next_marker is offline.
+		// In this API, marker has the same meaning as offset.
+		nextMarker := utils.PathSearch("next_marker", respBody, float64(0)).(float64)
+		if nextMarker == marker || nextMarker == 0 {
+			// Make sure the next marker value is correct, not the previous marker or zero (in the last page).
+			break
+		}
+		marker = nextMarker
+	}
+
+	return result, nil
+}
+
+func flattenApplications(applications []interface{}) []interface{} {
+	if len(applications) < 1 {
+		return nil
+	}
+
+	result := make([]interface{}, 0, len(applications))
+	for _, application := range applications {
+		result = append(result, map[string]interface{}{
+			"id":     utils.PathSearch("id", application, nil),
+			"name":   utils.PathSearch("name", application, nil),
+			"status": utils.PathSearch("status", application, nil),
+			"updated_at": utils.FormatTimeStampRFC3339(int64(utils.PathSearch("last_modified_time",
+				application, float64(0)).(float64))/1000, false),
+			"description": utils.PathSearch("description", application, nil),
+		})
+	}
+	return result
+}
+
+func filterApplications(d *schema.ResourceData, functions []interface{}) []interface{} {
+	result := functions
+
+	if appId, ok := d.GetOk("application_id"); ok && len(result) > 0 {
+		result = utils.PathSearch(fmt.Sprintf("[?id=='%v']", appId), result, make([]interface{}, 0)).([]interface{})
+	}
+
+	if name, ok := d.GetOk("name"); ok && len(result) > 0 {
+		result = utils.PathSearch(fmt.Sprintf("[?name=='%v']", name), result, make([]interface{}, 0)).([]interface{})
+	}
+
+	if status, ok := d.GetOk("status"); ok && len(result) > 0 {
+		result = utils.PathSearch(fmt.Sprintf("[?status=='%v']", status), result, make([]interface{}, 0)).([]interface{})
+	}
+
+	if desc, ok := d.GetOk("description"); ok && len(result) > 0 {
+		result = utils.PathSearch(fmt.Sprintf("[?description=='%v']", desc), result, make([]interface{}, 0)).([]interface{})
+	}
+
+	return result
+}
+
+func dataSourceApplicationsRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg    = meta.(*config.Config)
+		region = cfg.GetRegion(d)
+	)
+
+	client, err := cfg.NewServiceClient("fgs", region)
 	if err != nil {
 		return diag.Errorf("error creating FunctionGraph client: %s", err)
 	}
 
-	listApplicationsPath := client.Endpoint + httpUrl
-	listApplicationsPath = strings.ReplaceAll(listApplicationsPath, "{project_id}", client.ProjectID)
-	listApplicationsOpt := golangsdk.RequestOpts{
-		KeepResponseBody: true,
-	}
-
-	var applications []interface{}
-	var marker string
-	for {
-		getApplicationsResp, err := client.Request("GET",
-			listApplicationsPath, &listApplicationsOpt)
-
-		if err != nil {
-			return diag.Errorf("error querying the applications: %s", err)
-		}
-
-		getApplicationsRespBody, err := utils.FlattenResponse(getApplicationsResp)
-		if err != nil {
-			return diag.FromErr(err)
-		}
-		applications = append(applications, flattenListApplicationsBody(getApplicationsRespBody)...)
-		marker = utils.PathSearch("page_info.next_marker", getApplicationsRespBody, "").(string)
-		if marker == "" {
-			break
-		}
+	applications, err := getApplications(client)
+	if err != nil {
+		return diag.Errorf("error querying the applications: %s", err)
 	}
 
 	generateUUID, err := uuid.GenerateUUID()
@@ -125,54 +194,10 @@ func datasourceFunctionGraphApplicationsRead(_ context.Context, d *schema.Resour
 	}
 	d.SetId(generateUUID)
 
-	mErr = multierror.Append(
-		mErr,
+	mErr := multierror.Append(nil,
 		d.Set("region", region),
-		d.Set("applications", filterListApplicationsBody(applications, d)),
+		d.Set("applications", flattenApplications(filterApplications(d, applications))),
 	)
 
 	return diag.FromErr(mErr.ErrorOrNil())
-}
-
-func flattenListApplicationsBody(resp interface{}) []interface{} {
-	if resp == nil {
-		return nil
-	}
-	curJson := utils.PathSearch("applications", resp, make([]interface{}, 0))
-	curArray := curJson.([]interface{})
-	rst := make([]interface{}, 0, len(curArray))
-	for _, v := range curArray {
-		rst = append(rst, map[string]interface{}{
-			"id":          utils.PathSearch("id", v, nil),
-			"name":        utils.PathSearch("name", v, nil),
-			"status":      utils.PathSearch("status", v, nil),
-			"updated_at":  utils.FormatTimeStampRFC3339(int64(utils.PathSearch("last_modified_time", v, float64(0)).(float64))/1000, false),
-			"description": utils.PathSearch("description", v, nil),
-		})
-	}
-	return rst
-}
-
-func filterListApplicationsBody(all []interface{}, d *schema.ResourceData) []interface{} {
-	rst := make([]interface{}, 0, len(all))
-	for _, v := range all {
-		if applicationId, ok := d.GetOk("application_id"); ok && applicationId.(string) != utils.PathSearch("id", v, "").(string) {
-			continue
-		}
-
-		if name, ok := d.GetOk("name"); ok && name.(string) != utils.PathSearch("name", v, "").(string) {
-			continue
-		}
-
-		if status, ok := d.GetOk("status"); ok && status.(string) != utils.PathSearch("status", v, "").(string) {
-			continue
-		}
-
-		if description, ok := d.GetOk("description"); ok && description.(string) != utils.PathSearch("description", v, "").(string) {
-			continue
-		}
-
-		rst = append(rst, v)
-	}
-	return rst
 }
