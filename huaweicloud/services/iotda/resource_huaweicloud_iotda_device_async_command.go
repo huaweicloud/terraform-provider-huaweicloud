1217d0c63088cb749d0d4cfb7c1b84a0ddf9598a
ruwenqiang123 <ruwenqiang123@outlook.com>
2025-02-17 14:57:54 +0800

    refactor(iotda): refactor iotda device async command resource code style

--- huaweicloud/services/iotda/resource_huaweicloud_iotda_device_async_command.go
+++ huaweicloud/services/iotda/resource_huaweicloud_iotda_device_async_command.go
@@ -2,12 +2,13 @@ package iotda
 
 import (
 	"context"
+	"strings"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
-	"github.com/huaweicloud/huaweicloud-sdk-go-v3/services/iotda/v5/model"
+	"github.com/chnsz/golangsdk"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
@@ -92,78 +93,105 @@ func ResourceDeviceAsyncCommand() *schema.Resource {
 	}
 }
 
-func buildDeviceAsyncCommandBodyParams(d *schema.ResourceData) *model.CreateAsyncCommandRequest {
-	commandParas := utils.ValueIgnoreEmpty(d.Get("paras"))
-	bodyParams := model.CreateAsyncCommandRequest{
-		DeviceId: d.Get("device_id").(string),
-		Body: &model.AsyncDeviceCommandRequest{
-			ServiceId:    utils.StringIgnoreEmpty(d.Get("service_id").(string)),
-			CommandName:  utils.StringIgnoreEmpty(d.Get("name").(string)),
-			SendStrategy: d.Get("send_strategy").(string),
-			//nolint:gosec
-			ExpireTime: utils.Int32IgnoreEmpty(int32(d.Get("expire_time").(int))),
-		},
+func buildDeviceAsyncCommandBodyParams(d *schema.ResourceData) map[string]interface{} {
+	return map[string]interface{}{
+		"service_id":    utils.ValueIgnoreEmpty(d.Get("service_id")),
+		"command_name":  utils.ValueIgnoreEmpty(d.Get("name")),
+		"paras":         utils.ValueIgnoreEmpty(d.Get("paras")),
+		"expire_time":   utils.ValueIgnoreEmpty(d.Get("expire_time")),
+		"send_strategy": d.Get("send_strategy"),
 	}
-	if commandParas != nil {
-		bodyParams.Body.Paras = &commandParas
-	}
-
-	return &bodyParams
 }
 
 func resourceDeviceAsyncCommandCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	cfg := meta.(*config.Config)
-	region := cfg.GetRegion(d)
-	isDerived := WithDerivedAuth(cfg, region)
-	client, err := cfg.HcIoTdaV5Client(region, isDerived)
+	var (
+		cfg       = meta.(*config.Config)
+		region    = cfg.GetRegion(d)
+		isDerived = WithDerivedAuth(cfg, region)
+		deviceId  = d.Get("device_id").(string)
+		httpUrl   = "v5/iot/{project_id}/devices/{device_id}/async-commands"
+	)
+
+	client, err := cfg.NewServiceClientWithDerivedAuth("iotda", region, isDerived)
 	if err != nil {
-		return diag.Errorf("error creating IoTDA v5 client: %s", err)
+		return diag.Errorf("error creating IoTDA client: %s", err)
 	}
 
-	createOpts := buildDeviceAsyncCommandBodyParams(d)
-	resp, err := client.CreateAsyncCommand(createOpts)
-	if err != nil {
-		return diag.Errorf("error creating device asynchronous command : %s", err)
+	createPath := client.Endpoint + httpUrl
+	createPath = strings.ReplaceAll(createPath, "{project_id}", client.ProjectID)
+	createPath = strings.ReplaceAll(createPath, "{device_id}", deviceId)
+
+	createOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		JSONBody:         utils.RemoveNil(buildDeviceAsyncCommandBodyParams(d)),
 	}
 
-	if resp == nil || resp.CommandId == nil {
+	resp, err := client.Request("POST", createPath, &createOpt)
+	if err != nil {
+		return diag.Errorf("error creating device asynchronous command: %s", err)
+	}
+
+	respBody, err := utils.FlattenResponse(resp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	commandId := utils.PathSearch("command_id", respBody, "").(string)
+	if commandId == "" {
 		return diag.Errorf("error creating device asynchronous command: ID is not found in API response")
 	}
 
-	d.SetId(*resp.CommandId)
+	d.SetId(commandId)
 
 	return resourceDeviceAsyncCommandRead(ctx, d, meta)
 }
 
 func resourceDeviceAsyncCommandRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	c := meta.(*config.Config)
-	region := c.GetRegion(d)
-	isDerived := WithDerivedAuth(c, region)
-	client, err := c.HcIoTdaV5Client(region, isDerived)
+	var (
+		cfg       = meta.(*config.Config)
+		region    = cfg.GetRegion(d)
+		isDerived = WithDerivedAuth(cfg, region)
+		deviceId  = d.Get("device_id").(string)
+		httpUrl   = "v5/iot/{project_id}/devices/{device_id}/async-commands/{command_id}"
+	)
+
+	client, err := cfg.NewServiceClientWithDerivedAuth("iotda", region, isDerived)
 	if err != nil {
-		return diag.Errorf("error creating IoTDA v5 client: %s", err)
+		return diag.Errorf("error creating IoTDA client: %s", err)
 	}
 
-	deviceId := d.Get("device_id").(string)
+	getPath := client.Endpoint + httpUrl
+	getPath = strings.ReplaceAll(getPath, "{project_id}", client.ProjectID)
+	getPath = strings.ReplaceAll(getPath, "{device_id}", deviceId)
+	getPath = strings.ReplaceAll(getPath, "{command_id}", d.Id())
+	getOpts := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
 
-	response, err := client.ShowAsyncDeviceCommand(&model.ShowAsyncDeviceCommandRequest{DeviceId: deviceId, CommandId: d.Id()})
+	getResp, err := client.Request("GET", getPath, &getOpts)
 	if err != nil {
+		// The `device_id` or `command_id` does not exist, query API will return `404`.
 		return common.CheckDeletedDiag(d, err, "error retrieving device asynchronous command")
+	}
+
+	getRespBody, err := utils.FlattenResponse(getResp)
+	if err != nil {
+		return diag.FromErr(err)
 	}
 
 	mErr := multierror.Append(
 		d.Set("region", region),
-		d.Set("device_id", response.DeviceId),
-		d.Set("service_id", response.ServiceId),
-		d.Set("name", response.CommandName),
-		d.Set("status", response.Status),
-		d.Set("result", response.Result),
-		d.Set("send_strategy", response.SendStrategy),
-		d.Set("expire_time", response.ExpireTime),
-		d.Set("sent_time", response.SentTime),
-		d.Set("delivered_time", response.DeliveredTime),
-		d.Set("response_time", response.ResponseTime),
-		d.Set("created_at", response.CreatedTime),
+		d.Set("device_id", utils.PathSearch("device_id", getRespBody, nil)),
+		d.Set("service_id", utils.PathSearch("service_id", getRespBody, nil)),
+		d.Set("name", utils.PathSearch("command_name", getRespBody, nil)),
+		d.Set("status", utils.PathSearch("status", getRespBody, nil)),
+		d.Set("result", utils.PathSearch("result", getRespBody, nil)),
+		d.Set("send_strategy", utils.PathSearch("send_strategy", getRespBody, nil)),
+		d.Set("expire_time", utils.PathSearch("expire_time", getRespBody, nil)),
+		d.Set("sent_time", utils.PathSearch("sent_time", getRespBody, nil)),
+		d.Set("delivered_time", utils.PathSearch("delivered_time", getRespBody, nil)),
+		d.Set("response_time", utils.PathSearch("response_time", getRespBody, nil)),
+		d.Set("created_at", utils.PathSearch("created_time", getRespBody, nil)),
 	)
 
 	return diag.FromErr(mErr.ErrorOrNil())
