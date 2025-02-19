cbed35e4d9f15da29a5cc15d5af5eeff9c28b158
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-02-19 09:35:26 +0800

    refactor(cae): adjust code and optimize acceptance test

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1465,9 +1465,9 @@ func Provider() *schema.Provider {
 			"huaweicloud_bms_instance": bms.ResourceBmsInstance(),
 			"huaweicloud_bcs_instance": bcs.ResourceInstance(),
 
-			"huaweicloud_cae_component_action":         cae.ResourceComponentAction(),
 			"huaweicloud_cae_application":              cae.ResourceApplication(),
 			"huaweicloud_cae_certificate":              cae.ResourceCertificate(),
+			"huaweicloud_cae_component_action":         cae.ResourceComponentAction(),
 			"huaweicloud_cae_component":                cae.ResourceComponent(),
 			"huaweicloud_cae_component_configurations": cae.ResourceComponentConfigurations(),
 			"huaweicloud_cae_domain":                   cae.ResourceDomain(),
