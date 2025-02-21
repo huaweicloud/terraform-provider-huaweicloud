189f47238a04ea75e8ce60bf8777a9e485cd6448
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-02-21 14:10:26 +0800

    refactor: docs(cae): adjust code and add document for operating component resource (#6318)
    * refactor(cae): adjust code and optimize acceptance test
    
    * docs(cae): add document for operating componrnt resource

--- huaweicloud/services/acceptance/cae/resource_huaweicloud_cae_component_action_test.go
+++ huaweicloud/services/acceptance/cae/resource_huaweicloud_cae_component_action_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestAccComponentAction_basic(t *testing.T) {
-	baseConfig := testAccComponentConfiguration_base()
+	baseConfig := testAccComponentAction_base()
 	// Avoid CheckDestroy because this resource is a one-time action resource and there is nothing in the destroy
 	// method.
 	// lintignore:AT001
@@ -24,7 +24,7 @@ func TestAccComponentAction_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				// One-time action resource do not need to be checked and no processing is performed in the Read method.
-				Config: testAccComponentAction_basic_step1(baseConfig),
+				Config: testAccComponentAction_basic_step1(baseConfig, "deploy"),
 			},
 			{
 				Config: testAccComponentAction_basic_step2(baseConfig),
@@ -35,38 +35,68 @@ func TestAccComponentAction_basic(t *testing.T) {
 			{
 				Config: testAccComponentAction_basic_step4(baseConfig),
 			},
+			{
+				Config: testAccComponentAction_basic_step5(baseConfig),
+			},
+			{
+				Config: testAccComponentAction_basic_step6(baseConfig),
+			},
+			{
+				Config: testAccComponentAction_basic_step7(baseConfig),
+			},
 		},
 	})
 }
 
-func testAccComponentAction_basic_step1(baseConfig string) string {
+func testAccComponentAction_base() string {
+	name := acceptance.RandomAccResourceNameWithDash()
+
 	return fmt.Sprintf(`
-%[1]s
+data "huaweicloud_swr_repositories" "test" {}
 
-resource "huaweicloud_cae_component_configurations" "test" {
-  environment_id = "%[2]s"
-  application_id = "%[3]s"
-  component_id   = huaweicloud_cae_component.test.id
+locals {
+  swr_repositories = [for v in data.huaweicloud_swr_repositories.test.repositories : v if length(v.tags) > 0]
+}
 
-  items {
-    type = "lifecycle"
-    data = jsonencode({
-      "spec": {
-        "postStart": {
-          "exec": {
-            "command": [
-              "/bin/bash",
-              "-c",
-              "sleep",
-              "10",
-              "done",
-            ]
-          }
-        }
-      }
-    })
+resource "huaweicloud_cae_component" "test" {
+  environment_id = "%[1]s"
+  application_id = "%[2]s"
+
+  metadata {
+    name = "%[3]s"
+
+    annotations = {
+      version = "1.0.0"
+    }
+  }
+
+  spec {
+    replica = 1
+    runtime = "Docker"
+
+    source {
+      type = "image"
+      url  = try(format("%%s:%%s", local.swr_repositories[0].path, local.swr_repositories[0].tags[0]), "")
+    }
+
+    resource_limit {
+      cpu    = "500m"
+      memory = "1Gi"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata.0.annotations.version, spec.0.source, spec.0.resource_limit
+    ]
   }
 }
+`, acceptance.HW_CAE_ENVIRONMENT_ID, acceptance.HW_CAE_APPLICATION_ID, name)
+}
+
+func testAccComponentAction_basic_step1(baseConfig, action string) string {
+	return fmt.Sprintf(`
+%[1]s
 
 resource "huaweicloud_cae_component_action" "test" {
   environment_id = "%[2]s"
@@ -74,72 +104,39 @@ resource "huaweicloud_cae_component_action" "test" {
   component_id   = huaweicloud_cae_component.test.id
 
   metadata {
-    name = "deploy"
-  }
-
-  lifecycle {
-    replace_triggered_by = [
-      huaweicloud_cae_component_configurations.test.items
-    ]
+    name = "%[4]s"
   }
 }
-`, baseConfig, acceptance.HW_CAE_ENVIRONMENT_ID, acceptance.HW_CAE_APPLICATION_ID)
+`, baseConfig, acceptance.HW_CAE_ENVIRONMENT_ID, acceptance.HW_CAE_APPLICATION_ID, action)
 }
 
 func testAccComponentAction_basic_step2(baseConfig string) string {
 	return fmt.Sprintf(`
 %[1]s
 
-resource "huaweicloud_cae_component_configurations" "test" {
-  environment_id = "%[2]s"
-  application_id = "%[3]s"
-  component_id   = huaweicloud_cae_component.test.id
-
-  items {
-    type = "lifecycle"
-    data = jsonencode({
-      "spec": {
-        "postStart": {
-          "exec": {
-            "command": [
-              "/bin/bash",
-              "-c",
-              "sleep",
-              "10",
-              "done",
-            ]
-          }
-        }
-      }
-    })
-  }
-  items {
-    type = "env"
-    data = jsonencode({
-      "spec": {
-        "envs": {
-            "key": "value",
-            "foo": "bar"
-        }
-      }
-    })
-  }
-}
-
 resource "huaweicloud_cae_component_action" "test" {
   environment_id = "%[2]s"
   application_id = "%[3]s"
   component_id   = huaweicloud_cae_component.test.id
 
   metadata {
-    name = "configure"
+    name = "upgrade"
+
+    annotations = {
+      version = "2.0.0"
+    }
   }
 
-  lifecycle {
-    replace_triggered_by = [
-      huaweicloud_cae_component_configurations.test.items
-    ]
-  }
+  spec = jsonencode({
+    "source" : {
+      "type" : "image",
+      "url" : "nginx:alpine-perl"
+    },
+    "resource_limit" : {
+      "cpu_limit" : "1000m",
+      "memory_limit" : "2Gi"
+    }
+  })
 }
 `, baseConfig, acceptance.HW_CAE_ENVIRONMENT_ID, acceptance.HW_CAE_APPLICATION_ID)
 }
@@ -148,56 +145,22 @@ func testAccComponentAction_basic_step3(baseConfig string) string {
 	return fmt.Sprintf(`
 %[1]s
 
-resource "huaweicloud_cae_component_configurations" "test" {
-  environment_id = "%[2]s"
-  application_id = "%[3]s"
-  component_id   = huaweicloud_cae_component.test.id
-
-  items {
-    type = "lifecycle"
-    data = jsonencode({
-      "spec": {
-        "postStart": {
-          "exec": {
-            "command": [
-              "/bin/bash",
-              "-c",
-              "sleep",
-              "10",
-              "done",
-            ]
-          }
-        }
-      }
-    })
-  }
-  items {
-    type = "env"
-    data = jsonencode({
-      "spec": {
-        "envs": {
-            "key": "value",
-            "foo": "baar"
-        }
-      }
-    })
-  }
-}
-
 resource "huaweicloud_cae_component_action" "test" {
   environment_id = "%[2]s"
   application_id = "%[3]s"
   component_id   = huaweicloud_cae_component.test.id
 
   metadata {
-    name = "configure"
+    name = "rollback"
+
+    annotations = {
+      version = "1.0.0"
+    }
   }
 
-  lifecycle {
-    replace_triggered_by = [
-      huaweicloud_cae_component_configurations.test.items
-    ]
-  }
+  spec = jsonencode({
+    "snapshot_index" : 1
+  })
 }
 `, baseConfig, acceptance.HW_CAE_ENVIRONMENT_ID, acceptance.HW_CAE_APPLICATION_ID)
 }
@@ -212,24 +175,6 @@ resource "huaweicloud_cae_component_configurations" "test" {
   component_id   = huaweicloud_cae_component.test.id
 
   items {
-    type = "lifecycle"
-    data = jsonencode({
-      "spec": {
-        "postStart": {
-          "exec": {
-            "command": [
-              "/bin/bash",
-              "-c",
-              "sleep",
-              "10",
-              "done",
-            ]
-          }
-        }
-      }
-    })
-  }
-  items {
     type = "env"
     data = jsonencode({
       "spec": {
@@ -248,7 +193,7 @@ resource "huaweicloud_cae_component_action" "test" {
   component_id   = huaweicloud_cae_component.test.id
 
   metadata {
-    name = "stop"
+    name = "configure"
   }
 
   lifecycle {
@@ -258,4 +203,16 @@ resource "huaweicloud_cae_component_action" "test" {
   }
 }
 `, baseConfig, acceptance.HW_CAE_ENVIRONMENT_ID, acceptance.HW_CAE_APPLICATION_ID)
+}
+
+func testAccComponentAction_basic_step5(baseConfig string) string {
+	return testAccComponentAction_basic_step1(baseConfig, "stop")
+}
+
+func testAccComponentAction_basic_step6(baseConfig string) string {
+	return testAccComponentAction_basic_step1(baseConfig, "start")
+}
+
+func testAccComponentAction_basic_step7(baseConfig string) string {
+	return testAccComponentAction_basic_step1(baseConfig, "restart")
 }
