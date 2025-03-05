71972e7b929a9ac50e66e5a5fd6fecafca3f7620
saf3dfsa <caiwanting@huawei.com>
2025-03-05 10:34:23 +0800

    feat(EIP): support to get common pools

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1216,6 +1216,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_vpc_bandwidths":             eip.DataSourceBandWidths(),
 			"huaweicloud_vpc_eip":                    eip.DataSourceVpcEip(),
 			"huaweicloud_vpc_eips":                   eip.DataSourceVpcEips(),
+			"huaweicloud_vpc_eip_common_pools":       eip.DataSourceVpcEipCommonPools(),
 			"huaweicloud_vpc_internet_gateways":      eip.DataSourceVPCInternetGateways(),
 			"huaweicloud_global_eip_pools":           eip.DataSourceGlobalEIPPools(),
 			"huaweicloud_global_eip_access_sites":    eip.DataSourceGlobalEIPAccessSites(),
