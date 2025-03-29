dfdc752bd2aacffceddbeb19102ab6f5e1d05cbd
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2025-03-29 10:36:39 +0800

    feat(ELB): add params to lb monitor resource (#6568)

--- huaweicloud/services/lb/resource_huaweicloud_lb_monitor.go
+++ huaweicloud/services/lb/resource_huaweicloud_lb_monitor.go
@@ -2,25 +2,21 @@ package lb
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
-	"github.com/chnsz/golangsdk/openstack/elb/v2/monitors"
+	"github.com/chnsz/golangsdk"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils/fmtp"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils/logp"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
 // @API ELB POST /v2/{project_id}/elb/healthmonitors
-// @API ELB GET /v2/{project_id}/elb/loadbalancers/{loadbalancer_id}
-// @API ELB GET /v2/{project_id}/elb/listeners/{listener_id}
-// @API ELB GET /v2/{project_id}/elb/pools/{pool_id}
 // @API ELB GET /v2/{project_id}/elb/healthmonitors/{healthmonitor_id}
 // @API ELB PUT /v2/{project_id}/elb/healthmonitors/{healthmonitor_id}
 // @API ELB DELETE /v2/{project_id}/elb/healthmonitors/{healthmonitor_id}
@@ -47,68 +43,61 @@ func ResourceMonitorV2() *schema.Resource {
 				Computed: true,
 				ForceNew: true,
 			},
-
 			"name": {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
-
 			"pool_id": {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
 			},
-
 			"type": {
 				Type:     schema.TypeString,
 				Required: true,
-				ForceNew: true,
 			},
-
 			"delay": {
 				Type:     schema.TypeInt,
 				Required: true,
 			},
-
 			"timeout": {
 				Type:     schema.TypeInt,
 				Required: true,
 			},
-
 			"max_retries": {
 				Type:     schema.TypeInt,
 				Required: true,
 			},
-
 			"port": {
 				Type:     schema.TypeInt,
 				Optional: true,
 			},
-
 			"url_path": {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: true,
 			},
-
 			"http_method": {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: true,
 			},
-
 			"expected_codes": {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: true,
 			},
-
-			"admin_state_up": {
-				Type:     schema.TypeBool,
-				Default:  true,
+			"domain_name": {
+				Type:     schema.TypeString,
 				Optional: true,
+				Computed: true,
 			},
-
+			"admin_state_up": {
+				Type:       schema.TypeBool,
+				Default:    true,
+				Optional:   true,
+				Deprecated: "tenant_id is deprecated",
+			},
 			"tenant_id": {
 				Type:       schema.TypeString,
 				Optional:   true,
@@ -121,190 +110,188 @@ func ResourceMonitorV2() *schema.Resource {
 }
 
 func resourceMonitorV2Create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	config := meta.(*config.Config)
-	lbClient, err := config.LoadBalancerClient(config.GetRegion(d))
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		httpUrl = "v2/{project_id}/elb/healthmonitors"
+		product = "elb"
+	)
+	client, err := cfg.NewServiceClient(product, region)
 	if err != nil {
-		return fmtp.DiagErrorf("Error creating HuaweiCloud elb client: %s", err)
+		return diag.Errorf("error creating ELB client: %s", err)
 	}
 
-	adminStateUp := d.Get("admin_state_up").(bool)
-	createOpts := monitors.CreateOpts{
-		PoolID:        d.Get("pool_id").(string),
-		TenantID:      d.Get("tenant_id").(string),
-		Type:          d.Get("type").(string),
-		Delay:         d.Get("delay").(int),
-		Timeout:       d.Get("timeout").(int),
-		MaxRetries:    d.Get("max_retries").(int),
-		URLPath:       d.Get("url_path").(string),
-		HTTPMethod:    d.Get("http_method").(string),
-		ExpectedCodes: d.Get("expected_codes").(string),
-		Name:          d.Get("name").(string),
-		MonitorPort:   d.Get("port").(int),
-		AdminStateUp:  &adminStateUp,
-	}
+	createPath := client.Endpoint + httpUrl
+	createPath = strings.ReplaceAll(createPath, "{project_id}", client.ProjectID)
 
-	timeout := d.Timeout(schema.TimeoutCreate)
-	poolID := createOpts.PoolID
-	err = waitForLBV2viaPool(ctx, lbClient, poolID, "ACTIVE", timeout)
+	createOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+	createOpt.JSONBody = utils.RemoveNil(buildCreateMonitorBodyParams(d))
+	createResp, err := client.Request("POST", createPath, &createOpt)
 	if err != nil {
-		return diag.FromErr(err)
+		return diag.Errorf("error creating ELB monitor: %s", err)
 	}
 
-	logp.Printf("[DEBUG] Create Options: %#v", createOpts)
-	monitor, err := monitors.Create(lbClient, createOpts).Extract()
+	createRespBody, err := utils.FlattenResponse(createResp)
 	if err != nil {
-		return fmtp.DiagErrorf("Unable to create monitor: %s", err)
+		return diag.Errorf("error retrieving ELB monitor: %s", err)
+	}
+	monitorId := utils.PathSearch("healthmonitor.id", createRespBody, "").(string)
+	if monitorId == "" {
+		return diag.Errorf("error creating ELB monitor: ID is not found in API response")
 	}
 
-	err = waitForLBV2viaPool(ctx, lbClient, poolID, "ACTIVE", timeout)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	d.SetId(monitor.ID)
+	d.SetId(monitorId)
 
 	return resourceMonitorV2Read(ctx, d, meta)
+}
+
+func buildCreateMonitorBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"pool_id":        d.Get("pool_id"),
+		"type":           d.Get("type"),
+		"delay":          d.Get("delay"),
+		"timeout":        d.Get("timeout"),
+		"max_retries":    d.Get("max_retries"),
+		"domain_name":    utils.ValueIgnoreEmpty(d.Get("domain_name")),
+		"tenant_id":      utils.ValueIgnoreEmpty(d.Get("tenant_id")),
+		"url_path":       utils.ValueIgnoreEmpty(d.Get("url_path")),
+		"http_method":    utils.ValueIgnoreEmpty(d.Get("http_method")),
+		"expected_codes": utils.ValueIgnoreEmpty(d.Get("expected_codes")),
+		"name":           utils.ValueIgnoreEmpty(d.Get("name")),
+		"monitor_port":   utils.ValueIgnoreEmpty(d.Get("port")),
+		"admin_state_up": utils.ValueIgnoreEmpty(d.Get("admin_state_up")),
+	}
+
+	return map[string]interface{}{"healthmonitor": bodyParams}
 }
 
 func resourceMonitorV2Read(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	config := meta.(*config.Config)
-	lbClient, err := config.LoadBalancerClient(config.GetRegion(d))
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var mErr *multierror.Error
+
+	var (
+		httpUrl = "v2/{project_id}/elb/healthmonitors/{healthmonitor_id}"
+		product = "elb"
+	)
+	client, err := cfg.NewServiceClient(product, region)
 	if err != nil {
-		return fmtp.DiagErrorf("Error creating HuaweiCloud elb client: %s", err)
+		return diag.Errorf("error creating ELB client: %s", err)
 	}
 
-	monitor, err := monitors.Get(lbClient, d.Id()).Extract()
-	if err != nil {
-		return common.CheckDeletedDiag(d, err, "Error retrieving monitor")
+	getPath := client.Endpoint + httpUrl
+	getPath = strings.ReplaceAll(getPath, "{project_id}", client.ProjectID)
+	getPath = strings.ReplaceAll(getPath, "{healthmonitor_id}", d.Id())
+
+	getOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
 	}
 
-	logp.Printf("[DEBUG] Retrieved monitor %s: %#v", d.Id(), monitor)
+	getResp, err := client.Request("GET", getPath, &getOpt)
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "error retrieving ELB monitor")
+	}
 
-	mErr := multierror.Append(nil,
-		d.Set("region", config.GetRegion(d)),
-		d.Set("tenant_id", monitor.TenantID),
-		d.Set("type", monitor.Type),
-		d.Set("delay", monitor.Delay),
-		d.Set("timeout", monitor.Timeout),
-		d.Set("max_retries", monitor.MaxRetries),
-		d.Set("url_path", monitor.URLPath),
-		d.Set("http_method", monitor.HTTPMethod),
-		d.Set("expected_codes", monitor.ExpectedCodes),
-		d.Set("admin_state_up", monitor.AdminStateUp),
-		d.Set("name", monitor.Name),
+	getRespBody, err := utils.FlattenResponse(getResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	mErr = multierror.Append(
+		mErr,
+		d.Set("region", region),
+		d.Set("tenant_id", utils.PathSearch("healthmonitor.tenant_id", getRespBody, nil)),
+		d.Set("type", utils.PathSearch("healthmonitor.type", getRespBody, nil)),
+		d.Set("delay", utils.PathSearch("healthmonitor.delay", getRespBody, nil)),
+		d.Set("timeout", utils.PathSearch("healthmonitor.timeout", getRespBody, nil)),
+		d.Set("max_retries", utils.PathSearch("healthmonitor.max_retries", getRespBody, nil)),
+		d.Set("url_path", utils.PathSearch("healthmonitor.url_path", getRespBody, nil)),
+		d.Set("http_method", utils.PathSearch("healthmonitor.http_method", getRespBody, nil)),
+		d.Set("expected_codes", utils.PathSearch("healthmonitor.expected_codes", getRespBody, nil)),
+		d.Set("admin_state_up", utils.PathSearch("healthmonitor.admin_state_up", getRespBody, nil)),
+		d.Set("name", utils.PathSearch("healthmonitor.name", getRespBody, nil)),
+		d.Set("pool_id", utils.PathSearch("healthmonitor.pools[0].id", getRespBody, nil)),
+		d.Set("port", utils.PathSearch("healthmonitor.monitor_port", getRespBody, nil)),
+		d.Set("domain_name", utils.PathSearch("healthmonitor.domain_name", getRespBody, nil)),
 	)
 
-	if len(monitor.Pools) != 0 {
-		mErr = multierror.Append(mErr, d.Set("pool_id", monitor.Pools[0].ID))
-	}
-
-	if monitor.MonitorPort != 0 {
-		mErr = multierror.Append(mErr, d.Set("port", monitor.MonitorPort))
-	}
-
-	if err = mErr.ErrorOrNil(); err != nil {
-		return fmtp.DiagErrorf("Error setting monitor fields: %s", err)
-	}
-
-	return nil
+	return diag.FromErr(mErr.ErrorOrNil())
 }
 
 func resourceMonitorV2Update(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	config := meta.(*config.Config)
-	lbClient, err := config.LoadBalancerClient(config.GetRegion(d))
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		httpUrl = "v2/{project_id}/elb/healthmonitors/{healthmonitor_id}"
+		product = "elb"
+	)
+	client, err := cfg.NewServiceClient(product, region)
 	if err != nil {
-		return fmtp.DiagErrorf("Error creating HuaweiCloud elb client: %s", err)
+		return diag.Errorf("error creating ELB client: %s", err)
 	}
 
-	var updateOpts monitors.UpdateOpts
-	if d.HasChange("url_path") {
-		updateOpts.URLPath = d.Get("url_path").(string)
-	}
-	if d.HasChange("expected_codes") {
-		updateOpts.ExpectedCodes = d.Get("expected_codes").(string)
-	}
-	if d.HasChange("delay") {
-		updateOpts.Delay = d.Get("delay").(int)
-	}
-	if d.HasChange("timeout") {
-		updateOpts.Timeout = d.Get("timeout").(int)
-	}
-	if d.HasChange("max_retries") {
-		updateOpts.MaxRetries = d.Get("max_retries").(int)
-	}
-	if d.HasChange("admin_state_up") {
-		asu := d.Get("admin_state_up").(bool)
-		updateOpts.AdminStateUp = &asu
-	}
-	if d.HasChange("name") {
-		updateOpts.Name = d.Get("name").(string)
-	}
-	if d.HasChange("port") {
-		updateOpts.MonitorPort = d.Get("port").(int)
-	}
-	if d.HasChange("http_method") {
-		updateOpts.HTTPMethod = d.Get("http_method").(string)
-	}
+	updatePath := client.Endpoint + httpUrl
+	updatePath = strings.ReplaceAll(updatePath, "{project_id}", client.ProjectID)
+	updatePath = strings.ReplaceAll(updatePath, "{healthmonitor_id}", d.Id())
 
-	logp.Printf("[DEBUG] Updating monitor %s with options: %#v", d.Id(), updateOpts)
-	timeout := d.Timeout(schema.TimeoutUpdate)
-	poolID := d.Get("pool_id").(string)
-	err = waitForLBV2viaPool(ctx, lbClient, poolID, "ACTIVE", timeout)
-	if err != nil {
-		return diag.FromErr(err)
+	updateOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
 	}
-	//lintignore:R006
-	err = resource.RetryContext(ctx, timeout, func() *resource.RetryError {
-		_, err = monitors.Update(lbClient, d.Id(), updateOpts).Extract()
-		if err != nil {
-			return common.CheckForRetryableError(err)
-		}
-		return nil
-	})
-
+	updateOpt.JSONBody = utils.RemoveNil(buildUpdateMonitorBodyParams(d))
+	_, err = client.Request("PUT", updatePath, &updateOpt)
 	if err != nil {
-		return fmtp.DiagErrorf("Unable to update monitor %s: %s", d.Id(), err)
-	}
-
-	// Wait for LB to become active before continuing
-	err = waitForLBV2viaPool(ctx, lbClient, poolID, "ACTIVE", timeout)
-	if err != nil {
-		return diag.FromErr(err)
+		return diag.Errorf("error updating ELB monitor: %s", err)
 	}
 
 	return resourceMonitorV2Read(ctx, d, meta)
 }
 
-func resourceMonitorV2Delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	config := meta.(*config.Config)
-	lbClient, err := config.LoadBalancerClient(config.GetRegion(d))
+func buildUpdateMonitorBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"type":           d.Get("type"),
+		"delay":          d.Get("delay"),
+		"timeout":        d.Get("timeout"),
+		"max_retries":    d.Get("max_retries"),
+		"domain_name":    utils.ValueIgnoreEmpty(d.Get("domain_name")),
+		"url_path":       utils.ValueIgnoreEmpty(d.Get("url_path")),
+		"http_method":    utils.ValueIgnoreEmpty(d.Get("http_method")),
+		"expected_codes": utils.ValueIgnoreEmpty(d.Get("expected_codes")),
+		"name":           utils.ValueIgnoreEmpty(d.Get("name")),
+		"monitor_port":   utils.ValueIgnoreEmpty(d.Get("port")),
+		"admin_state_up": utils.ValueIgnoreEmpty(d.Get("admin_state_up")),
+	}
+	return map[string]interface{}{"healthmonitor": bodyParams}
+}
+
+func resourceMonitorV2Delete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		httpUrl = "v2/{project_id}/elb/healthmonitors/{healthmonitor_id}"
+		product = "elb"
+	)
+
+	client, err := cfg.NewServiceClient(product, region)
 	if err != nil {
-		return fmtp.DiagErrorf("Error creating HuaweiCloud elb client: %s", err)
+		return diag.Errorf("error creating ELB client: %s", err)
 	}
 
-	logp.Printf("[DEBUG] Deleting monitor %s", d.Id())
-	timeout := d.Timeout(schema.TimeoutUpdate)
-	poolID := d.Get("pool_id").(string)
-	err = waitForLBV2viaPool(ctx, lbClient, poolID, "ACTIVE", timeout)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-	//lintignore:R006
-	err = resource.RetryContext(ctx, timeout, func() *resource.RetryError {
-		err = monitors.Delete(lbClient, d.Id()).ExtractErr()
-		if err != nil {
-			return common.CheckForRetryableError(err)
-		}
-		return nil
-	})
+	deletePath := client.Endpoint + httpUrl
+	deletePath = strings.ReplaceAll(deletePath, "{project_id}", client.ProjectID)
+	deletePath = strings.ReplaceAll(deletePath, "{healthmonitor_id}", d.Id())
 
-	if err != nil {
-		return fmtp.DiagErrorf("Unable to delete monitor %s: %s", d.Id(), err)
+	deleteOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
 	}
-
-	err = waitForLBV2viaPool(ctx, lbClient, poolID, "ACTIVE", timeout)
+	_, err = client.Request("DELETE", deletePath, &deleteOpt)
 	if err != nil {
-		return diag.FromErr(err)
+		return common.CheckDeletedDiag(d, err, "error deleting ELB monitor")
 	}
 
 	return nil
