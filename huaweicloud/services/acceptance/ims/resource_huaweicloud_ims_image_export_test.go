f2192010837921822777e92abc6ef598b7a5aa43
jinyangyang222 <jinyangyang222@outlook.com>
2025-04-17 15:06:53 +0800

    refactor(ims): refactor `ims_image_export` resource code style (#6688)

--- huaweicloud/services/acceptance/ims/resource_huaweicloud_ims_image_export_test.go
+++ huaweicloud/services/acceptance/ims/resource_huaweicloud_ims_image_export_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance/common"
 )
 
 func TestAccImageExport_basic(t *testing.T) {
@@ -52,6 +53,43 @@ func TestAccImageExport_with_isQuickExport(t *testing.T) {
 	})
 }
 
+func testAccImageExport_base(rName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+data "huaweicloud_availability_zones" "test" {}
+
+data "huaweicloud_compute_flavors" "test" {
+  availability_zone = data.huaweicloud_availability_zones.test.names[0]
+  performance_type  = "normal"
+  cpu_core_count    = 2
+  memory_size       = 4
+}
+
+resource "huaweicloud_compute_instance" "test" {
+  name               = "%[2]s"
+  image_name         = "Ubuntu 18.04 server 64bit"
+  flavor_id          = data.huaweicloud_compute_flavors.test.ids[0]
+  security_group_ids = [huaweicloud_networking_secgroup.test.id]
+  availability_zone  = data.huaweicloud_availability_zones.test.names[0]
+
+  network {
+    uuid = huaweicloud_vpc_subnet.test.id
+  }
+}
+
+resource "huaweicloud_ims_ecs_system_image" "test" {
+  name        = "%[2]s"
+  instance_id = huaweicloud_compute_instance.test.id
+
+  tags = {
+    foo = "bar"
+    key = "value"
+  }
+}
+`, common.TestBaseNetwork(rName), rName)
+}
+
 func testAccImageExport_basic() string {
 	rName := acceptance.RandomAccResourceName()
 	rNameSuffix := acctest.RandString(4)
@@ -64,7 +102,7 @@ resource "huaweicloud_ims_image_export" "test" {
   bucket_url  = "%[2]s_%[3]s"
   file_format = "qcow2"
 }
-`, testAccEcsSystemImage_basic(rName), acceptance.HW_IMS_IMAGE_URL, rNameSuffix)
+`, testAccImageExport_base(rName), acceptance.HW_IMS_IMAGE_URL, rNameSuffix)
 }
 
 func testAccImageExport_with_isQuickExport() string {
@@ -79,5 +117,5 @@ resource "huaweicloud_ims_image_export" "test" {
   bucket_url      = "%[2]s_%[3]s"
   is_quick_export = true
 }
-`, testAccEcsSystemImage_basic(rName), acceptance.HW_IMS_IMAGE_URL, rNameSuffix)
+`, testAccImageExport_base(rName), acceptance.HW_IMS_IMAGE_URL, rNameSuffix)
 }
