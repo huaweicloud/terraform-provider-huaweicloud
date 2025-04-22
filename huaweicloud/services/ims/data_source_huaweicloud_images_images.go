cdf4f2ba3f0181e00362d10b5fd9ef8ea7b64cd2
jinyangyang222 <jinyangyang222@outlook.com>
2025-04-22 15:28:46 +0800

    refactor(ims): refactor images data source code style (#6712)
    * refactor(ims): refactor `images_images` data source code style
    
    * refactor(ims): refactor `images_image` data source code style

--- huaweicloud/services/ims/data_source_huaweicloud_images_images.go
+++ huaweicloud/services/ims/data_source_huaweicloud_images_images.go
@@ -2,19 +2,20 @@ package ims
 
 import (
 	"context"
+	"fmt"
 	"regexp"
 	"strconv"
-	"time"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 
-	"github.com/chnsz/golangsdk/openstack/ims/v2/cloudimages"
+	"github.com/chnsz/golangsdk"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/helper/hashcode"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
 // @API IMS GET /v2/cloudimages
@@ -217,8 +218,7 @@ func ImagesImageRefSchema() *schema.Resource {
 	}
 }
 
-func buildImageTypeParam(d *schema.ResourceData) string {
-	imageType := d.Get("visibility").(string)
+func buildImageTypeParam(imageType string) string {
 	if imageType == "public" {
 		imageType = "gold"
 	}
@@ -226,44 +226,111 @@ func buildImageTypeParam(d *schema.ResourceData) string {
 	return imageType
 }
 
-// dataSourceImagesImagesRead performs the image lookup.
+func buildImagesQueryParamsWithMarker(requestPath, marker string) string {
+	if marker == "" {
+		return requestPath
+	}
+
+	return fmt.Sprintf("%s&marker=%s", requestPath, marker)
+}
+
+func buildImagesQueryParams(d *schema.ResourceData, epsId string) string {
+	queryParams := fmt.Sprintf("?status=active&enterprise_project_id=%v", epsId)
+
+	if v, ok := d.GetOk("image_id"); ok {
+		queryParams = fmt.Sprintf("%s&id=%v", queryParams, v)
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		queryParams = fmt.Sprintf("%s&name=%v", queryParams, v)
+	}
+
+	if v, ok := d.GetOk("image_type"); ok {
+		queryParams = fmt.Sprintf("%s&virtual_env_type=%v", queryParams, v)
+	}
+
+	if d.Get("is_whole_image").(bool) {
+		queryParams = fmt.Sprintf("%s&__whole_image=%v", queryParams, true)
+	}
+
+	if v, ok := d.GetOk("visibility"); ok {
+		queryParams = fmt.Sprintf("%s&__imagetype=%v", queryParams, buildImageTypeParam(v.(string)))
+	}
+
+	if v, ok := d.GetOk("owner"); ok {
+		queryParams = fmt.Sprintf("%s&owner=%v", queryParams, v)
+	}
+
+	if v, ok := d.GetOk("flavor_id"); ok {
+		queryParams = fmt.Sprintf("%s&flavor_id=%v", queryParams, v)
+	}
+
+	if v, ok := d.GetOk("sort_key"); ok {
+		queryParams = fmt.Sprintf("%s&sort_key=%v", queryParams, v)
+	}
+
+	if v, ok := d.GetOk("sort_direction"); ok {
+		queryParams = fmt.Sprintf("%s&sort_dir=%v", queryParams, v)
+	}
+
+	if v, ok := d.GetOk("os"); ok {
+		queryParams = fmt.Sprintf("%s&__platform=%v", queryParams, v)
+	}
+
+	if v, ok := d.GetOk("architecture"); ok {
+		queryParams = fmt.Sprintf("%s&architecture=%v", queryParams, v)
+	}
+
+	if v, ok := d.GetOk("tag"); ok {
+		queryParams = fmt.Sprintf("%s&tag=%v", queryParams, v)
+	}
+
+	return queryParams
+}
+
 func dataSourceImagesImagesRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var (
-		cfg    = meta.(*config.Config)
-		region = cfg.GetRegion(d)
+		cfg       = meta.(*config.Config)
+		region    = cfg.GetRegion(d)
+		product   = "ims"
+		httpUrl   = "v2/cloudimages"
+		epsId     = cfg.GetEnterpriseProjectID(d, "all_granted_eps")
+		allImages = make([]interface{}, 0)
+		marker    = ""
 	)
 
-	imageClient, err := cfg.ImageV2Client(region)
+	client, err := cfg.NewServiceClient(product, region)
 	if err != nil {
-		return diag.Errorf("error creating IMS v2 client: %s", err)
+		return diag.Errorf("error creating IMS client: %s", err)
 	}
 
-	listOpts := cloudimages.ListOpts{
-		ID:                  d.Get("image_id").(string),
-		Name:                d.Get("name").(string),
-		VirtualEnvType:      d.Get("image_type").(string),
-		WholeImage:          d.Get("is_whole_image").(bool),
-		Imagetype:           buildImageTypeParam(d),
-		Owner:               d.Get("owner").(string),
-		FlavorId:            d.Get("flavor_id").(string),
-		SortKey:             d.Get("sort_key").(string),
-		SortDir:             d.Get("sort_direction").(string),
-		Platform:            d.Get("os").(string),
-		Architecture:        d.Get("architecture").(string),
-		Tag:                 d.Get("tag").(string),
-		EnterpriseProjectID: cfg.GetEnterpriseProjectID(d, "all_granted_eps"),
-		// Default query status for **active** images.
-		Status: "active",
+	getPath := client.Endpoint + httpUrl
+	getPath += buildImagesQueryParams(d, epsId)
+	getOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
 	}
 
-	allPages, err := cloudimages.List(imageClient, listOpts).AllPages()
-	if err != nil {
-		return diag.Errorf("unable to query images: %s", err)
-	}
+	for {
+		getPathWithMarker := buildImagesQueryParamsWithMarker(getPath, marker)
+		getResp, err := client.Request("GET", getPathWithMarker, &getOpt)
+		if err != nil {
+			return diag.Errorf("error retrieving IMS images: %s", err)
+		}
 
-	allImages, err := cloudimages.ExtractImages(allPages)
-	if err != nil {
-		return diag.Errorf("unable to extract images: %s", err)
+		getRespBody, err := utils.FlattenResponse(getResp)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		imagesResp := utils.PathSearch("images", getRespBody, make([]interface{}, 0)).([]interface{})
+		if len(imagesResp) == 0 {
+			break
+		}
+
+		allImages = append(allImages, imagesResp...)
+		// Because the API does not return the value of the `marker` field,
+		// we can only manually retrieve the last data from the previous page.
+		marker = utils.PathSearch("id", imagesResp[len(imagesResp)-1], "").(string)
 	}
 
 	// Filter images by `name_regex`.
@@ -276,21 +343,23 @@ func dataSourceImagesImagesRead(_ context.Context, d *schema.ResourceData, meta 
 	}
 	var resultImages []interface{}
 	var ids []string
-	for _, item := range allImages {
-		image := item
-		if nameRegexRes != nil && !nameRegexRes.MatchString(image.Name) {
+	for _, image := range allImages {
+		if nameRegexRes != nil &&
+			!nameRegexRes.MatchString(utils.PathSearch("name", image, "").(string)) {
 			continue
 		}
 
-		if v, ok := d.GetOk("__support_agent_list"); ok && v.(string) != item.SupportAgentList {
+		if v, ok := d.GetOk("__support_agent_list"); ok &&
+			v.(string) != utils.PathSearch("__support_agent_list", image, "").(string) {
 			continue
 		}
 
-		ids = append(ids, image.ID)
-		resultImages = append(resultImages, flattenImage(&image))
+		ids = append(ids, utils.PathSearch("id", image, "").(string))
+		resultImages = append(resultImages, flattenImage(image))
 	}
 
 	d.SetId(hashcode.Strings(ids))
+
 	mErr := multierror.Append(
 		d.Set("region", region),
 		d.Set("images", resultImages),
@@ -299,40 +368,40 @@ func dataSourceImagesImagesRead(_ context.Context, d *schema.ResourceData, meta 
 	return diag.FromErr(mErr.ErrorOrNil())
 }
 
-func flattenImage(image *cloudimages.Image) map[string]interface{} {
-	res := map[string]interface{}{
-		"id":                    image.ID,
-		"name":                  image.Name,
-		"image_type":            image.VirtualEnvType,
-		"visibility":            flattenVisibility(image.Imagetype),
-		"owner":                 image.Owner,
-		"os":                    image.Platform,
-		"architecture":          flattenArchitecture(image.SupportArm),
-		"enterprise_project_id": image.EnterpriseProjectID,
-		"os_version":            image.OsVersion,
-		"file":                  image.File,
-		"schema":                image.Schema,
-		"status":                image.Status,
-		"description":           image.Description,
-		"protected":             image.Protected,
-		"container_format":      image.ContainerFormat,
-		"min_ram_mb":            image.MinRam,
-		"max_ram_mb":            flattenMaxRAM(image.MaxRam),
-		"min_disk_gb":           image.MinDisk,
-		"disk_format":           image.DiskFormat,
-		"data_origin":           image.DataOrigin,
-		"backup_id":             image.BackupID,
-		"active_at":             image.ActiveAt,
-		"created_at":            image.CreatedAt.Format(time.RFC3339),
-		"updated_at":            image.UpdatedAt.Format(time.RFC3339),
-		"__support_agent_list":  image.SupportAgentList,
+func flattenImage(image interface{}) map[string]interface{} {
+	result := map[string]interface{}{
+		"id":                    utils.PathSearch("id", image, nil),
+		"name":                  utils.PathSearch("name", image, nil),
+		"image_type":            utils.PathSearch("virtual_env_type", image, nil),
+		"visibility":            flattenVisibility(utils.PathSearch("__imagetype", image, "").(string)),
+		"owner":                 utils.PathSearch("owner", image, nil),
+		"os":                    utils.PathSearch("__platform", image, nil),
+		"architecture":          flattenArchitecture(utils.PathSearch("__support_arm", image, "").(string)),
+		"enterprise_project_id": utils.PathSearch("enterprise_project_id", image, nil),
+		"os_version":            utils.PathSearch("__os_version", image, nil),
+		"file":                  utils.PathSearch("file", image, nil),
+		"schema":                utils.PathSearch("schema", image, nil),
+		"status":                utils.PathSearch("status", image, nil),
+		"description":           utils.PathSearch("__description", image, nil),
+		"protected":             utils.PathSearch("protected", image, nil),
+		"container_format":      utils.PathSearch("container_format", image, nil),
+		"min_ram_mb":            utils.PathSearch("min_ram", image, nil),
+		"max_ram_mb":            flattenMaxRAM(utils.PathSearch("max_ram", image, "").(string)),
+		"min_disk_gb":           utils.PathSearch("min_disk", image, nil),
+		"disk_format":           utils.PathSearch("disk_format", image, nil),
+		"data_origin":           utils.PathSearch("__data_origin", image, nil),
+		"backup_id":             utils.PathSearch("__backup_id", image, nil),
+		"active_at":             utils.PathSearch("active_at", image, nil),
+		"created_at":            utils.PathSearch("created_at", image, nil),
+		"updated_at":            utils.PathSearch("updated_at", image, nil),
+		"__support_agent_list":  utils.PathSearch("__support_agent_list", image, nil),
 	}
 
-	if size, err := strconv.Atoi(image.ImageSize); err == nil {
-		res["size_bytes"] = size
+	if size, err := strconv.Atoi(utils.PathSearch("__image_size", image, "").(string)); err == nil {
+		result["size_bytes"] = size
 	}
 
-	return res
+	return result
 }
 
 func flattenVisibility(imageType string) string {
