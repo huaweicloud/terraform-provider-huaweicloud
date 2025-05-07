4fa3af55fc665dffda887bfc8075519d6fd665e2
zyb <zyb_560@qq.com>
2025-05-07 14:48:21 +0800

    (fix): fix error of the CBH DataSourceCbhFlavors API comment

--- huaweicloud/services/cbh/data_source_huaweicloud_cbh_flavors.go
+++ huaweicloud/services/cbh/data_source_huaweicloud_cbh_flavors.go
@@ -16,7 +16,7 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
-// @API CBH GET /v2/{project_id}/instance/specification
+// @API CBH GET /v2/{project_id}/cbs/instance/specification
 func DataSourceCbhFlavors() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: datasourceFlavorsRead,
