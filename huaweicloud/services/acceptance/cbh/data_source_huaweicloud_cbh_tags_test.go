1de212139a26b09e2f3cceba3dbacb2e8a1e32e4
zyb <zyb_560@qq.com>
2025-05-08 20:49:03 +0800

    modify the format

--- huaweicloud/services/acceptance/cbh/data_source_huaweicloud_cbh_tags_test.go
+++ huaweicloud/services/acceptance/cbh/data_source_huaweicloud_cbh_tags_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
