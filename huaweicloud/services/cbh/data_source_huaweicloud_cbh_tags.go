5a68fb3fdd067af4ecd2652e42b4badade89ebc5
zyb <zyb_560@qq.com>
2025-05-08 21:06:16 +0800

    change format

--- huaweicloud/services/cbh/data_source_huaweicloud_cbh_tags.go
+++ huaweicloud/services/cbh/data_source_huaweicloud_cbh_tags.go
@@ -9,6 +9,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
+	"github.com/chnsz/golangsdk"
+
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
