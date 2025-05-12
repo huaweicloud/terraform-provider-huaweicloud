13424f19d0c1d61cbd7c657b4ff093638bfcd161
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-05-12 16:06:47 +0800

    feat(apig): add new data source to get availability zones

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -480,6 +480,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_apig_api":                                apig.DataSourceApi(),
 			"huaweicloud_apig_appcodes":                           apig.DataSourceAppcodes(),
 			"huaweicloud_apig_applications":                       apig.DataSourceApplications(),
+			"huaweicloud_apig_availability_zones":                 apig.DataSourceAvailabilityZones(),
 			"huaweicloud_apig_application_acl":                    apig.DataSourceApplicationAcl(),
 			"huaweicloud_apig_application_quotas":                 apig.DataSourceApigApplicationQuotas(),
 			"huaweicloud_apig_channels":                           apig.DataSourceChannels(),
