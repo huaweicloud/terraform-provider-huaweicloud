7dffaf12fbcd70c2fd020e69539590d74a61a091
Lance <Overlord52259@gmail.com>
2025-05-14 14:54:50 +0800

    feat(ram): set endpoint as global for EU region (#6788)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2969,6 +2969,8 @@ func configureProvider(_ context.Context, d *schema.ResourceData, terraformVersi
 	if conf.Cloud == defaultEuropeCloud {
 		cdnEndpoint := fmt.Sprintf("https://cdn.%s/", conf.Cloud)
 		conf.SetServiceEndpoint("cdn", cdnEndpoint)
+		ramEndpoint := fmt.Sprintf("https://ram.%s/", conf.Cloud)
+		conf.SetServiceEndpoint("ram", ramEndpoint)
 	}
 
 	return &conf, config.CheckUpgrade(d, Version)
