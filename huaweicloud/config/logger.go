404beadf184105533701b0a218b89507c379959d
Lance <Overlord52259@gmail.com>
2025-05-15 17:32:48 +0800

    fix(logger): the content type match rule should be prefix (#6798)

--- huaweicloud/config/logger.go
+++ huaweicloud/config/logger.go
@@ -146,7 +146,7 @@ func (*LogRoundTripper) dumpRequest(original io.ReadCloser, bs *bytes.Buffer) (i
 // logRequest will log the HTTP Request details.
 // If the body is JSON, it will attempt to be pretty-formatted.
 func (*LogRoundTripper) logRequest(bs *bytes.Buffer, contentType, logId string) error {
-	isJSONFormat := regexp.MustCompile(`^application/(merge-patch\+)?json$`).Match([]byte(contentType))
+	isJSONFormat := regexp.MustCompile(`^application/(merge-patch\+)?json`).Match([]byte(contentType))
 	isXMLFormat := strings.HasPrefix(bs.String(), "<") && !strings.HasPrefix(bs.String(), "<html>")
 	// Handle request contentType
 	switch {
