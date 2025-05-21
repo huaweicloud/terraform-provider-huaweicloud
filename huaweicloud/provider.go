397ebe410749e8c9ad2dbba4d736d7bac7e7fdf5
hwbyuehen <b724651851@163.com>
2025-05-21 15:24:31 +0800

    feat(COC): add coc script orders data source (#6818)
    Co-authored-by: huweibo <724651851@qq.com>

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -648,8 +648,9 @@ func Provider() *schema.Provider {
 			"huaweicloud_cnad_advanced_available_objects":   cnad.DataSourceAvailableProtectedObjects(),
 			"huaweicloud_cnad_advanced_protected_objects":   cnad.DataSourceProtectedObjects(),
 
-			"huaweicloud_coc_applications": coc.DataSourceCocApplications(),
-			"huaweicloud_coc_scripts":      coc.DataSourceCocScripts(),
+			"huaweicloud_coc_applications":  coc.DataSourceCocApplications(),
+			"huaweicloud_coc_scripts":       coc.DataSourceCocScripts(),
+			"huaweicloud_coc_script_orders": coc.DataSourceCocScriptOrders(),
 
 			"huaweicloud_compute_flavors":                 ecs.DataSourceEcsFlavors(),
 			"huaweicloud_compute_instance":                ecs.DataSourceComputeInstance(),
