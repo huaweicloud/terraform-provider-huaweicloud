52675309f81b2eec05af66bb75305c3461c640da
luxiaohang <deerhang@163.com>
2025-05-22 16:58:18 +0800

    fix(AAD): add field enable_force_new

--- huaweicloud/services/aad/resource_huaweicloud_aad_change_specification.go
+++ huaweicloud/services/aad/resource_huaweicloud_aad_change_specification.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 
 	"github.com/chnsz/golangsdk"
 
@@ -78,6 +79,12 @@ func ResourceChangeSpecification() *schema.Resource {
 						},
 					},
 				},
+			},
+			"enable_force_new": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice([]string{"true", "false"}, false),
+				Description:  utils.SchemaDesc("", utils.SchemaDescInput{Internal: true}),
 			},
 		},
 	}
