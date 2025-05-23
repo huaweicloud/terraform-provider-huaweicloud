2a246a63f84f711242a6a153ded7696256e55ccf
shi2duxinxia <37185164+shi2duxinxia@users.noreply.github.com>
2025-05-23 09:50:46 +0800

    feat(dew): add a data source to get list of user quotas. (#6839)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1039,6 +1039,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_kms_grants":        dew.DataSourceKmsGrants(),
 			"huaweicloud_kms_key":           dew.DataSourceKmsKey(),
 			"huaweicloud_kms_keys":          dew.DataSourceKmsKeys(),
+			"huaweicloud_kms_quotas":        dew.DataSourceKMSQuotas(),
 			"huaweicloud_kps_failed_tasks":  dew.DataSourceDewKpsFailedTasks(),
 			"huaweicloud_kps_running_tasks": dew.DataSourceDewKpsRunningTasks(),
 			"huaweicloud_kps_keypairs":      dew.DataSourceKeypairs(),
