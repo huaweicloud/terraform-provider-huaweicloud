eebb2dde0124d480697b9d6893f667ebb46ea86c
shi2duxinxia <37185164+shi2duxinxia@users.noreply.github.com>
2025-06-03 10:30:41 +0800

    feat(aad): add a data source to get unblock quota statistics (#6876)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -452,9 +452,10 @@ func Provider() *schema.Provider {
 			"huaweicloud_access_analyzers":              accessanalyzer.DataSourceAccessAnalyzers(),
 			"huaweicloud_access_analyzer_archive_rules": accessanalyzer.DataSourceAccessAnalyzerArchiveRules(),
 
-			"huaweicloud_aad_instances":               aad.DataSourceAADInstances(),
-			"huaweicloud_aad_black_white_lists":       aad.DataSourceAadBlackWhiteLists(),
-			"huaweicloud_aad_web_protection_policies": aad.DataSourceAadWebProtectionPolicies(),
+			"huaweicloud_aad_instances":                aad.DataSourceAADInstances(),
+			"huaweicloud_aad_unblock_quota_statistics": aad.DataSourceUnblockQuotaStatistics(),
+			"huaweicloud_aad_black_white_lists":        aad.DataSourceAadBlackWhiteLists(),
+			"huaweicloud_aad_web_protection_policies":  aad.DataSourceAadWebProtectionPolicies(),
 
 			"huaweicloud_antiddos_config_ranges":                antiddos.DataSourceConfigRanges(),
 			"huaweicloud_antiddos_weekly_protection_statistics": antiddos.DataSourceWeeklyProtectionStatistics(),
