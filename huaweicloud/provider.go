1abe24574c079f9b6d9192dc4e9836ac01d8f6c9
Lance <Overlord52259@gmail.com>
2025-06-03 14:08:46 +0800

    feat(smn): add new resource to set topic attributes (#6901)
    Co-authored-by: Lance Huang <lance@LancedeMacBook-Air.local>

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2455,6 +2455,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_smn_message_detection":          smn.ResourceMessageDetection(),
 			"huaweicloud_smn_message_publish":            smn.ResourceMessagePublish(),
 			"huaweicloud_smn_subscription_filter_policy": smn.ResourceSubscriptionFilterPolicy(),
+			"huaweicloud_smn_topic_attributes":           smn.ResourceTopicAttributes(),
 
 			"huaweicloud_sms_server_template": sms.ResourceServerTemplate(),
 			"huaweicloud_sms_task":            sms.ResourceMigrateTask(),
