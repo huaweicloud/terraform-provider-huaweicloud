3755fc4114eb92585740f6fa0a7bcefddb609c18
Lance52259 <Overlord52259@gmail.com>
2025-06-04 21:15:30 +0800

    chore(fgs): do not report error if lts log is enabled

---
+++ huaweicloud/services/acceptance/fgs/resource_huaweicloud_fgs_lts_log_enable_test.go
@@ -0,0 +1,28 @@
+package fgs
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccLtsLogEnable_basic(t *testing.T) {
+	// lintignore:AT001
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLtsLogEnable_basic,
+			},
+		},
+	})
+}
+
+const testAccLtsLogEnable_basic string = `
+resource "huaweicloud_fgs_lts_log_enable" "test" {}
+`
