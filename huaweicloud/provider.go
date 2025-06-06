931956003a83b9dac4c5a8857ae2fe87cfae7396
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-06-06 18:26:18 +0800

    feat(lts): add new data source to get all alarms

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1088,6 +1088,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_live_channels":             live.DataSourceLiveChannels(),
 			"huaweicloud_live_cdn_ips":              live.DataSourceLiveCdnIps(),
 
+			"huaweicloud_lts_alarms":                       lts.DataSourceAlarms(),
 			"huaweicloud_lts_aom_accesses":                 lts.DataSourceAOMAccesses(),
 			"huaweicloud_lts_cce_accesses":                 lts.DataSourceCceAccesses(),
 			"huaweicloud_lts_groups":                       lts.DataSourceLtsGroups(),
