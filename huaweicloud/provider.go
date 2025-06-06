ad21294a5402e3e5c04e3f3dab8e06b5490243dd
luoping-12345 <luoping-12345@outlook.com>
2025-06-06 17:24:54 +0800

    feat(cci): add data source CCI storage classes

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -598,6 +598,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_cciv2_pods":                     cci.DataSourceV2Pods(),
 			"huaweicloud_cciv2_persistent_volumes":       cci.DataSourceV2PersistentVolumes(),
 			"huaweicloud_cciv2_persistent_volume_claims": cci.DataSourceV2PersistentVolumeClaims(),
+			"huaweicloud_cciv2_storage_classes":          cci.DataSourceV2StorageClasses(),
 
 			"huaweicloud_ccm_certificates":               ccm.DataSourceCertificates(),
 			"huaweicloud_ccm_certificate_export":         ccm.DataSourceCertificateExport(),
