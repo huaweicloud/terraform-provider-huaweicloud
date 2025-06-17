ab2935f91f97f296407b7a97512a12c24fdf009c
shi2duxinxia <738496663@qq.com>
2025-06-17 20:16:42 +0800

    feat(hss): add new data source to query hss asset port statistics

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1034,6 +1034,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_hss_event_unblock_ips":              hss.DataSourceEventUnblockIps(),
 			"huaweicloud_hss_asset_apps":                     hss.DataSourceAssetApps(),
 			"huaweicloud_hss_agent_install_script":           hss.DataSourceAgentInstallScript(),
+			"huaweicloud_hss_asset_port_statistics":          hss.DataSourceAssetPortStatistics(),
 
 			"huaweicloud_identity_permissions": iam.DataSourceIdentityPermissions(),
 			"huaweicloud_identity_role":        iam.DataSourceIdentityRole(),
