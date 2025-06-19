2741dc1169217c9e46ef7da26869fb1e9bb5c08e
saf3dfsa <caiwanting@huawei.com>
2025-06-19 11:17:47 +0800

    feat(CodeArts/Pipeline): add resource pipeline action

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2679,6 +2679,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_codearts_inspector_host":         codeartsinspector.ResourceInspectorHost(),
 
 			"huaweicloud_codearts_pipeline":                  codeartspipeline.ResourceCodeArtsPipeline(),
+			"huaweicloud_codearts_pipeline_action":           codeartspipeline.ResourceCodeArtsPipelineAction(),
 			"huaweicloud_codearts_pipeline_template":         codeartspipeline.ResourceCodeArtsPipelineTemplate(),
 			"huaweicloud_codearts_pipeline_service_endpoint": codeartspipeline.ResourceCodeArtsPipelineServiceEndpoint(),
 
