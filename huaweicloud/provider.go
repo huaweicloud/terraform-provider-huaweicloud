02ba9b54efd9ab127c8c410f708a73d08e30a0b2
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-06-20 09:52:23 +0800

    feat(modelarts): add new resource to manage v2 resource pool

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2378,6 +2378,7 @@ func Provider() *schema.Provider {
 			// Resource management via V2 APIs.
 			"huaweicloud_modelartsv2_node_batch_delete":      modelarts.ResourceV2NodeBatchDelete(),
 			"huaweicloud_modelartsv2_node_batch_unsubscribe": modelarts.ResourceV2NodeBatchUnsubscribe(),
+			"huaweicloud_modelartsv2_resource_pool":          modelarts.Resourcev2ResourcePool(),
 			"huaweicloud_modelartsv2_service":                modelarts.ResourceV2Service(),
 			"huaweicloud_modelartsv2_service_action":         modelarts.ResourceV2ServiceAction(),
 
