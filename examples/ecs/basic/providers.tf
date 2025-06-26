8a8c21489cd8d9908df4c7fe2713b00c0e927696
Lance52259 <Overlord52259@gmail.com>
2025-06-26 14:44:27 +0800

    style(example/ecs): using hcbp rules to format terraform scripts

--- examples/ecs/basic/providers.tf
+++ examples/ecs/basic/providers.tf
@@ -7,21 +7,6 @@ terraform {
   }
 }
 
-variable "region_name" {
-  description = "The region where the ECS instance is located"
-  type        = string
-}
-
-variable "access_key" {
-  description = "The access key of the IAM user"
-  type        = string
-}
-
-variable "secret_key" {
-  description = "The secret key of the IAM user"
-  type        = string
-}
-
 provider "huaweicloud" {
   region     = var.region_name
   access_key = var.access_key
--- examples/ecs/basic/variables.tf
+++ examples/ecs/basic/variables.tf
@@ -1,11 +1,87 @@
+# Variable definitions for authentication
+variable "region_name" {
+  description = "The region where the ECS instance is located"
+  type        = string
+}
+
+variable "access_key" {
+  description = "The access key of the IAM user"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "The secret key of the IAM user"
+  type        = string
+}
+
+# Variable definitions for resources/data sources
+variable "instance_flavor_id" {
+  description = "The ID of the ECS instance flavor"
+  type        = string
+  default     = ""
+}
+
+variable "instance_flavor_performance_type" {
+  description = "The performance type of the ECSinstance flavor"
+  type        = string
+  default     = "normal"
+}
+
+variable "instance_flavor_cpu_core_count" {
+  description = "The CPU core count of the ECS instance flavor"
+  type        = number
+  default     = 2
+}
+
+variable "instance_flavor_memory_size" {
+  description = "The memory size of the ECS instance flavor"
+  type        = number
+  default     = 4
+}
+
+variable "instance_image_id" {
+  description = "The ID of the ECS instance image"
+  type        = string
+  default     = ""
+}
+
+variable "instance_image_visibility" {
+  description = "The visibility of the ECS instance image"
+  type        = string
+  default     = "public"
+}
+
+variable "instance_image_os" {
+  description = "The OS of the ECS instance image"
+  type        = string
+  default     = "Ubuntu"
+}
+
 variable "vpc_name" {
   description = "The name of the VPC"
   type        = string
 }
 
-variable "subnet_name" {
-  description = "The name of the subnet"
+variable "vpc_cidr" {
+  description = "The CIDR block of the VPC"
   type        = string
+}
+
+variable "subnet_name" {
+  description = "The name of the VPC subnet"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "The CIDR block of the VPC subnet"
+  type        = string
+  default     = ""
+}
+
+variable "subnet_gateway_ip" {
+  description = "The gateway IP of the VPC subnet"
+  type        = string
+  default     = ""
 }
 
 variable "security_group_name" {
