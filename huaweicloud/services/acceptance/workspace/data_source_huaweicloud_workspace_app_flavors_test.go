016c1f9f7e088c3956bdf35c8f54c25d3deb4c4f
Zippo-Wang <852420284@qq.com>
2025-07-07 20:13:07 +0800

    feat(workspace): add data source query flavors

---
+++ huaweicloud/services/acceptance/workspace/data_source_huaweicloud_workspace_app_flavors_test.go
@@ -0,0 +1,151 @@
+package workspace
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceAppFlavors_basic(t *testing.T) {
+	var (
+		dataSourceName = "data.huaweicloud_workspace_app_flavors.test"
+		dc             = acceptance.InitDataSourceCheck(dataSourceName)
+
+		byProductId   = "data.huaweicloud_workspace_app_flavors.filter_by_product_id"
+		dcByProductId = acceptance.InitDataSourceCheck(byProductId)
+
+		byFlavorId   = "data.huaweicloud_workspace_app_flavors.filter_by_flavor_id"
+		dcByFlavorId = acceptance.InitDataSourceCheck(byFlavorId)
+
+		byAvailabilityZone   = "data.huaweicloud_workspace_app_flavors.filter_by_az"
+		dcByAvailabilityZone = acceptance.InitDataSourceCheck(byAvailabilityZone)
+
+		byChargeMode   = "data.huaweicloud_workspace_app_flavors.filter_by_charge_mode"
+		dcByChargeMode = acceptance.InitDataSourceCheck(byChargeMode)
+
+		byOsType   = "data.huaweicloud_workspace_app_flavors.filter_by_os_type"
+		dcByOsType = acceptance.InitDataSourceCheck(byOsType)
+
+		byArchitectureX86   = "data.huaweicloud_workspace_app_flavors.filter_by_architecture_x86"
+		dcByArchitectureX86 = acceptance.InitDataSourceCheck(byArchitectureX86)
+
+		byArchitectureARM   = "data.huaweicloud_workspace_app_flavors.filter_by_architecture_arm"
+		dcByArchitectureARM = acceptance.InitDataSourceCheck(byArchitectureARM)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceAppFlavors_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestMatchResourceAttr("data.huaweicloud_workspace_app_flavors.test", "flavors.#",
+						regexp.MustCompile(`^[1-9]([0-9]*)?$`)),
+					dcByProductId.CheckResourceExists(),
+					resource.TestCheckOutput("is_product_id_filter_useful", "true"),
+					dcByFlavorId.CheckResourceExists(),
+					resource.TestCheckOutput("is_flavor_id_filter_useful", "true"),
+					dcByAvailabilityZone.CheckResourceExists(),
+					resource.TestCheckOutput("is_az_filter_useful", "true"),
+					dcByChargeMode.CheckResourceExists(),
+					resource.TestCheckOutput("is_charge_mode_filter_useful", "true"),
+					dcByOsType.CheckResourceExists(),
+					resource.TestCheckOutput("is_os_type_filter_useful", "true"),
+					dcByArchitectureX86.CheckResourceExists(),
+					resource.TestCheckOutput("is_architecture_x86_filter_useful", "true"),
+					dcByArchitectureARM.CheckResourceExists(),
+					resource.TestCheckOutput("is_architecture_arm_filter_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceAppFlavors_basic() string {
+	return fmt.Sprintf(`
+data "huaweicloud_workspace_app_flavors" "test" {}
+
+# By product ID filter
+locals {
+  product_id = data.huaweicloud_workspace_app_flavors.test.flavors[0].product_id
+}
+
+data "huaweicloud_workspace_app_flavors" "filter_by_product_id" {
+  product_id = local.product_id
+}
+
+output "is_product_id_filter_useful" {
+  value = length(data.huaweicloud_workspace_app_flavors.filter_by_product_id.flavors) > 0 && alltrue(
+    [for v in data.huaweicloud_workspace_app_flavors.filter_by_product_id.flavors[*].product_id : v == local.product_id]
+  )
+}
+
+# By flavor ID filter
+locals {
+  flavor_id = data.huaweicloud_workspace_app_flavors.test.flavors[0].flavor_id
+}
+
+data "huaweicloud_workspace_app_flavors" "filter_by_flavor_id" {
+  flavor_id = local.flavor_id
+}
+
+output "is_flavor_id_filter_useful" {
+  value = length(data.huaweicloud_workspace_app_flavors.filter_by_flavor_id.flavors) > 0 && alltrue(
+    [for v in data.huaweicloud_workspace_app_flavors.filter_by_flavor_id.flavors[*].flavor_id : v == local.flavor_id]
+  )
+}
+
+# By charge mode filter
+data "huaweicloud_workspace_app_flavors" "filter_by_charge_mode" {
+  charge_mode = "1"
+}
+
+output "is_charge_mode_filter_useful" {
+  value = length(data.huaweicloud_workspace_app_flavors.filter_by_charge_mode.flavors) > 0 && alltrue(
+    [for v in data.huaweicloud_workspace_app_flavors.filter_by_charge_mode.flavors[*].charge_mode : v == "1"]
+  )
+}
+
+# By availability zone filter
+data "huaweicloud_workspace_app_flavors" "filter_by_az" {
+  availability_zone = "%[1]s"
+}
+
+output "is_az_filter_useful" {
+  value = data.huaweicloud_workspace_app_flavors.filter_by_az.availability_zone == "%[1]s"
+}
+
+# By OS type filter
+data "huaweicloud_workspace_app_flavors" "filter_by_os_type" {
+  os_type = "Windows"
+}
+
+output "is_os_type_filter_useful" {
+  value = data.huaweicloud_workspace_app_flavors.filter_by_os_type.os_type == "Windows"
+}
+
+# By architecture x86 filter
+data "huaweicloud_workspace_app_flavors" "filter_by_architecture_x86" {
+  architecture = "x86"
+}
+
+output "is_architecture_x86_filter_useful" {
+  value = data.huaweicloud_workspace_app_flavors.filter_by_architecture_x86.architecture == "x86"
+}
+
+# By architecture arm filter
+data "huaweicloud_workspace_app_flavors" "filter_by_architecture_arm" {
+  architecture = "arm"
+}
+
+output "is_architecture_arm_filter_useful" {
+  value = data.huaweicloud_workspace_app_flavors.filter_by_architecture_arm.architecture == "arm"
+}
+`, acceptance.HW_AVAILABILITY_ZONE)
+}
---
+++ huaweicloud/services/workspace/data_source_huaweicloud_workspace_app_flavors.go
@@ -0,0 +1,292 @@
+package workspace
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+// @API Workspace GET /v1/{project_id}/product
+func DataSourceAppFlavors() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceAppFlavorsRead,
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				Description: `The region in which to query the resource.`,
+			},
+			"product_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `The product ID used to filter the app flavor list.`,
+			},
+			"flavor_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `The flavor ID used to filter the app flavor list.`,
+			},
+			"availability_zone": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				Description: `The availability zone used to filter the app flavor list.`,
+			},
+			"os_type": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				Description: `The operating system type used to filter the app flavor list.`,
+			},
+			"charge_mode": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `The charge mode used to filter the app flavor list.`,
+			},
+			"architecture": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: `The architecture type used to filter the app flavor list.`,
+			},
+
+			// attributes
+			"flavors": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: `The list of app flavors that matched filter parameters.`,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"product_id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The product ID of app flavors that matched filter parameters.`,
+						},
+						"flavor_id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The flavor ID of app flavors that matched filter parameters.`,
+						},
+						"type": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The flavor type.`,
+						},
+						"architecture": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The flavor architecture of app flavors that matched filter parameters.`,
+						},
+						"cpu": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The CPU core count.`,
+						},
+						"memory": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The memory size in MB.`,
+						},
+						"is_gpu": {
+							Type:        schema.TypeBool,
+							Computed:    true,
+							Description: `Whether the flavor is GPU type.`,
+						},
+						"system_disk_type": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The system disk type.`,
+						},
+						"system_disk_size": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The system disk size.`,
+						},
+						"descriptions": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The flavor description.`,
+						},
+						"charge_mode": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The charge mode of app flavors that matched filter parameters.`,
+						},
+						"contain_data_disk": {
+							Type:        schema.TypeBool,
+							Computed:    true,
+							Description: `Whether the flavor includes data disk.`,
+						},
+						"resource_type": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The resource type.`,
+						},
+						"cloud_service_type": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The cloud service type.`,
+						},
+						"volume_product_type": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The volume product type.`,
+						},
+						"sessions": {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: `The maximum number of sessions supported by the flavor.`,
+						},
+						"status": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The flavor status.`,
+						},
+						"cond_operation_az": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The flavor status in availability zones.`,
+						},
+						"domain_ids": {
+							Type:        schema.TypeList,
+							Computed:    true,
+							Elem:        &schema.Schema{Type: schema.TypeString},
+							Description: `The domain IDs that the flavor belongs to.`,
+						},
+						"package_type": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `The package type.`,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func buildAppFlavorsQueryParams(d *schema.ResourceData) string {
+	res := ""
+
+	if v, ok := d.GetOk("product_id"); ok {
+		res = fmt.Sprintf("%s&product_id=%v", res, v)
+	}
+	if v, ok := d.GetOk("flavor_id"); ok {
+		res = fmt.Sprintf("%s&flavor_id=%v", res, v)
+	}
+	if v, ok := d.GetOk("availability_zone"); ok {
+		res = fmt.Sprintf("%s&availability_zone=%v", res, v)
+	}
+	if v, ok := d.GetOk("os_type"); ok {
+		res = fmt.Sprintf("%s&os_type=%v", res, v)
+	}
+	if v, ok := d.GetOk("charge_mode"); ok {
+		res = fmt.Sprintf("%s&charge_mode=%v", res, v)
+	}
+	if v, ok := d.GetOk("architecture"); ok {
+		res = fmt.Sprintf("%s&architecture=%v", res, v)
+	}
+
+	return res
+}
+
+func getAppFlavors(client *golangsdk.ServiceClient, d *schema.ResourceData) ([]interface{}, error) {
+	httpUrl := "v1/{project_id}/product"
+	queryParams := buildAppFlavorsQueryParams(d)
+	if queryParams != "" {
+		httpUrl = httpUrl + "?" + queryParams[1:]
+	}
+	getPath := client.Endpoint + httpUrl
+	getPath = strings.ReplaceAll(getPath, "{project_id}", client.ProjectID)
+
+	opt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		MoreHeaders: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}
+
+	requestResp, err := client.Request("GET", getPath, &opt)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody, err := utils.FlattenResponse(requestResp)
+	if err != nil {
+		return nil, err
+	}
+
+	return utils.PathSearch("products", respBody, make([]interface{}, 0)).([]interface{}), nil
+}
+
+func dataSourceAppFlavorsRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	client, err := cfg.NewServiceClient("appstream", region)
+	if err != nil {
+		return diag.Errorf("error creating Workspace app client: %s", err)
+	}
+
+	flavors, err := getAppFlavors(client, d)
+	if err != nil {
+		return diag.Errorf("error querying Workspace app flavors: %s", err)
+	}
+
+	randomUUID, err := uuid.GenerateUUID()
+	if err != nil {
+		return diag.Errorf("unable to generate ID: %s", err)
+	}
+	d.SetId(randomUUID)
+
+	mErr := multierror.Append(nil,
+		d.Set("region", region),
+		d.Set("flavors", flattenAppFlavors(flavors)),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func flattenAppFlavors(flavors []interface{}) []map[string]interface{} {
+	if len(flavors) < 1 {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(flavors))
+	for _, flavor := range flavors {
+		result = append(result, map[string]interface{}{
+			"product_id":          utils.PathSearch("product_id", flavor, nil),
+			"flavor_id":           utils.PathSearch("flavor_id", flavor, nil),
+			"type":                utils.PathSearch("type", flavor, nil),
+			"architecture":        utils.PathSearch("architecture", flavor, nil),
+			"cpu":                 utils.PathSearch("cpu", flavor, nil),
+			"memory":              utils.PathSearch("memory", flavor, nil),
+			"is_gpu":              utils.PathSearch("is_gpu", flavor, false),
+			"system_disk_type":    utils.PathSearch("system_disk_type", flavor, nil),
+			"system_disk_size":    utils.PathSearch("system_disk_size", flavor, nil),
+			"descriptions":        utils.PathSearch("descriptions", flavor, nil),
+			"charge_mode":         utils.PathSearch("charge_mode", flavor, nil),
+			"contain_data_disk":   utils.PathSearch("contain_data_disk", flavor, false),
+			"resource_type":       utils.PathSearch("resource_type", flavor, nil),
+			"cloud_service_type":  utils.PathSearch("cloud_service_type", flavor, nil),
+			"volume_product_type": utils.PathSearch("volume_product_type", flavor, nil),
+			"sessions":            utils.PathSearch("sessions", flavor, 0),
+			"status":              utils.PathSearch("status", flavor, nil),
+			"cond_operation_az":   utils.PathSearch("cond_operation_az", flavor, nil),
+			"domain_ids":          utils.PathSearch("domain_ids", flavor, nil),
+			"package_type":        utils.PathSearch("package_type", flavor, nil),
+		})
+	}
+
+	return result
+}
