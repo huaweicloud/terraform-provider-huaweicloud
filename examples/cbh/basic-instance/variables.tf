c3cf30071348391d4c35aa35b2c098061382d86e
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-07-21 19:07:29 +0800

    chore(examples/cbh): adjust script and readme of the best practices

---
+++ examples/cbh/basic-instance/variables.tf
@@ -0,0 +1,102 @@
+# Variable definitions for authentication
+variable "region_name" {
+  description = "The region where the CBH instance is located"
+  type        = string
+}
+
+variable "access_key" {
+  description = "The access key of the IAM user"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "The secret key of the IAM user"
+  type        = string
+}
+
+# Variable definitions for resources/data sources
+variable "availability_zone" {
+  description = "The availability zone to which the CBH instance belongs"
+  type        = string
+  default     = ""
+}
+
+variable "instance_flavor_id" {
+  description = "The flavor ID of the CBH instance"
+  type        = string
+  default     = ""
+}
+
+variable "instance_flavor_type" {
+  description = "The flavor type of the CBH instance"
+  type        = string
+  default     = "basic"
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block of the VPC"
+  type        = string
+  default     = "192.168.0.0/16"
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "The CIDR block of the subnet"
+  type        = string
+  default     = ""
+}
+
+variable "subnet_gateway_ip" {
+  description = "The gateway IP of the subnet"
+  type        = string
+  default     = ""
+}
+
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "The name of the CBH instance"
+  type        = string
+}
+
+variable "instance_password" {
+  description = "The login password of the CBH instance"
+  type        = string
+  sensitive   = true
+}
+
+variable "charging_mode" {
+  description = "The charging mode of the CBH instance"
+  type        = string
+  default     = "prePaid"
+}
+
+variable "period_unit" {
+  description = "The charging period unit of the CBH instance"
+  type        = string
+  default     = "month"
+}
+
+variable "period" {
+  description = "The charging period of the CBH instance"
+  type        = number
+  default     = 1
+}
+
+variable "auto_renew" {
+  description = "Whether to enable auto-renew for the CBH instance"
+  type        = string
+  default     = "false"
+}
---
+++ examples/cbh/ha-instance/terraform.tfvars
@@ -0,0 +1,5 @@
+vpc_name            = "tf_test_cbh_ha_vpc"
+subnet_name         = "tf_test_cbh_ha_subnet"
+security_group_name = "tf_test_cbh_ha_sg"
+instance_name       = "tf_test_cbh_ha_instance"
+instance_password   = "YourCBHHAInstancePassword!"
