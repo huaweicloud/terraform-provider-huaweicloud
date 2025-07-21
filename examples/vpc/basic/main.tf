29e8ae24e56f21f4556e41da21ce13a9250deb46
Lance52259 <Overlord52259@gmail.com>
2025-07-21 11:45:09 +0800

    chore(example/vpc): refactor the basic best practice

--- examples/vpc/basic/main.tf
+++ examples/vpc/basic/main.tf
@@ -1,12 +1,13 @@
-resource "huaweicloud_vpc" "vpc_1" {
-  name = var.vpc_name
-  cidr = var.vpc_cidr
+resource "huaweicloud_vpc" "test" {
+  name                  = var.vpc_name
+  cidr                  = var.vpc_cidr
+  enterprise_project_id = var.enterprise_project_id
 }
 
-resource "huaweicloud_vpc_subnet" "subnet_1" {
-  vpc_id      = huaweicloud_vpc.vpc_1.id
-  name        = var.subnet_name
-  cidr        = var.subnet_cidr
-  gateway_ip  = var.subnet_gateway
-  primary_dns = var.primary_dns
+resource "huaweicloud_vpc_subnet" "test" {
+  vpc_id     = huaweicloud_vpc.test.id
+  name       = var.subnet_name
+  cidr       = var.subnet_cidr
+  gateway_ip = var.subnet_gateway
+  dns_list   = var.dns_list
 }
