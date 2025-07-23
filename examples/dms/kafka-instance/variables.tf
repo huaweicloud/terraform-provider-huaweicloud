6791e314cbb88801e38244291ba5ffff2da3df86
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-07-23 17:07:37 +0800

    refactor(examples/dms): refactor kafka instace best practice

--- examples/dms/kafka-instance/variables.tf
+++ examples/dms/kafka-instance/variables.tf
@@ -1,34 +1,146 @@
+# Variable definitions for authentication
+variable "region_name" {
+  description = "The region where the Kafka instance is located"
+  type        = string
+}
+
+variable "access_key" {
+  description = "The access key of the IAM user"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "The secret key of the IAM user"
+  type        = string
+}
+
+# Variable definitions for resources/data sources
+variable "availability_zones" {
+  description = "The availability zones to which the Kafka instance belongs"
+  type        = list(string)
+  default     = []
+}
+
 variable "vpc_name" {
-  description = "The name of the Huaweicloud VPC"
-  default     = "tf_vpc_demo"
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block of the VPC"
+  type        = string
+  default     = "192.168.0.0/16"
 }
 
 variable "subnet_name" {
-  description = "The name of the Huaweicloud Subnet"
-  default     = "tf_subnet_demo"
+  description = "The name of the subnet"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "The CIDR block of the subnet"
+  type        = string
+  default     = ""
+}
+
+variable "subnet_gateway_ip" {
+  description = "The gateway IP of the subnet"
+  type        = string
+  default     = ""
 }
 
 variable "security_group_name" {
-  description = "The name of the Huaweicloud Security Group"
-  default     = "tf_secgroup_demo"
+  description = "The name of the security group"
+  type        = string
 }
 
-variable "access_user_name" {
+variable "instance_flavor_id" {
+  description = "The flavor ID of the Kafka instance"
+  type        = string
+  default     = ""
+}
+
+variable "instance_flavor_type" {
+  description = "The flavor type of the Kafka instance"
+  type        = string
+  default     = "cluster"
+}
+
+variable "instance_storage_spec_code" {
+  description = "The storage specification code of the Kafka instance"
+  type        = string
+  default     = "dms.physical.storage.ultra.v2"
+}
+
+variable "instance_name" {
+  description = "The name of the Kafka instance"
+  type        = string
+}
+
+variable "instance_engine_version" {
+  description = "The engine version of the Kafka instance"
+  type        = string
+  default     = "2.7"
+}
+
+variable "instance_storage_space" {
+  description = "The storage space of the Kafka instance"
+  type        = number
+  default     = 600
+}
+
+variable "instance_broker_num" {
+  description = "The number of brokers of the Kafka instance"
+  type        = number
+  default     = 3
+}
+
+variable "instance_ssl_enable" {
+  description = "The SSL enable of the Kafka instance"
+  type        = bool
+  default     = false
+}
+
+variable "instance_access_user_name" {
   description = "The access user of the Kafka instance"
-  default     = "user"
+  type        = string
+  default     = ""
 }
 
-variable "access_user_password" {
+variable "instance_access_user_password" {
   description = "The access password of the Kafka instance"
-  default     = "Kafkatest@123"
+  sensitive   = true
+  type        = string
+  default     = ""
 }
 
-variable "manager_user" {
-  description = "The manager user of the Kafka instance"
-  default     = "kafka-user"
+variable "instance_description" {
+  description = "The description of the Kafka instance"
+  type        = string
+  default     = ""
 }
 
-variable "manager_password" {
-  description = "The manager password of the Kafka instance"
-  default     = "Kafkatest@123"
+variable "charging_mode" {
+  description = "The charging mode of the Kafka instance"
+  type        = string
+  default     = "postPaid"
 }
+
+variable "period_unit" {
+  description = "The period unit of the Kafka instance"
+  type        = string
+  default     = null
+}
+
+variable "period" {
+  description = "The period of the Kafka instance"
+  type        = number
+  default     = null
+}
+
+variable "auto_renew" {
+  description = "The auto renew of the Kafka instance"
+  type        = string
+  default     = "false"
+}
+
