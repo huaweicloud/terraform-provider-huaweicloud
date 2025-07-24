00210540a30d3d74b6c6fa80e5ce3d1e265cec4e
Lance <Overlord52259@gmail.com>
2025-07-24 17:32:54 +0800

    chore(example/sms): refactor the task best practice (#7341)

--- examples/sms/task/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "sms_source_server_name" {
-  description = "The name of the SMS source server"
-  default     = "tf_sms_source_server_name"
-}
-
-variable "sms_server_template_name" {
-  description = "The name of the SMS server template"
-  default     = "tf_sms_server_template_name"
-}
