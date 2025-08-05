850ee6e61742a3c570a4f5068424df575e997de4
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-08-05 19:40:36 +0800

    refactor(ecs/associate_eip): adjust best practice for instance associate with eip (#7425)

---
+++ examples/ecs/instance-associate-eip/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.1"
+  required_providers {
+    huaweicloud = {
+      source  = "huaweicloud/huaweicloud"
+      version = ">=1.57.0"
+    }
+  }
+}
+
+provider "huaweicloud" {
+  region     = var.region_name
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
