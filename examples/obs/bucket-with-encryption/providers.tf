51d26e2ebca28263056be937fed983f359820ab6
Lance52259 <Overlord52259@gmail.com>
2025-08-06 11:59:33 +0800

    docs(obs/bucket): supports new best practice for encryption bucket

---
+++ examples/obs/bucket-with-encryption/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 0.12.0"
+
+  required_providers {
+    huaweicloud = {
+      source  = "huaweicloud/huaweicloud"
+      version = ">= 1.64.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "huaweicloud" {
+  region     = var.region_name
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
