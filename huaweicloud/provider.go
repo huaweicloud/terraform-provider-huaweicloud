5d95068338b3182a339794cd62f7f49f6a2bc09b
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2025-08-20 16:56:50 +0800

    feat(Config): add config policy assignment evaluate status data source (#7581)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1410,6 +1410,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_rms_resource_aggregation_pending_requests":    rms.DataSourceRmsAggregationPendingRequests(),
 			"huaweicloud_rms_resource_aggregator_source_statuses":      rms.DataSourceRmsAggregatorSourceStatuses(),
 			"huaweicloud_rms_policy_states":                            rms.DataSourcePolicyStates(),
+			"huaweicloud_rms_policy_assignment_evaluate_status":        rms.DataSourceRmsPolicyAssignmentEvaluateStatus(),
 			"huaweicloud_rms_assignment_package_scores":                rms.DataSourceRmsAssignmentPackageScores(),
 			"huaweicloud_rms_assignment_package_results":               rms.DataSourceRmsAssignmentPackageResults(),
 			"huaweicloud_rms_resource_aggregator_discovered_resources": rms.DataSourceAggregatorDiscoveredResources(),
