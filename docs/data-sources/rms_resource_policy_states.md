b43358c731a75a117653c6e7607bb025c847209b
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2025-08-21 19:24:53 +0800

    feat(Config): add config resource policy states data source (#7603)

---
+++ docs/data-sources/rms_resource_policy_states.md
@@ -0,0 +1,69 @@
+---
+subcategory: "rms"
+layout: "huaweicloud"
+page_title: "HuaweiCloud: huaweicloud_rms_resource_policy_states"
+description: |-
+  Use this data source to query policy states of a resource.
+---
+
+# huaweicloud_rms_resource_policy_states
+
+Use this data source to query policy states of a resource.
+
+## Example Usage
+
+```hcl
+variable "resource_id" {}
+
+data "huaweicloud_rms_resource_policy_states" "test" {
+  resource_id = var.resource_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the resource.
+  If omitted, the provider-level region will be used.
+
+* `resource_id` - (Required, String) Specifies the resource ID.
+
+* `compliance_state` - (Optional, String) Specifies the compliance status.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `value` - Indicates the return value of querying the compliance result.
+
+  The [value](#value_struct) structure is documented below.
+
+<a name="value_struct"></a>
+The `value` block supports:
+
+* `domain_id` - Indicates the user ID.
+
+* `region_id` - Indicates the ID of the region the resource belongs to.
+
+* `resource_id` - Indicates the resource ID.
+
+* `resource_name` - Indicates the resource name.
+
+* `resource_provider` - Indicates the cloud service name.
+
+* `resource_type` - Indicates the resource type.
+
+* `trigger_type` - Indicates the trigger type. The value can be **resource** or **period**.
+
+* `compliance_state` - Indicates the compliance state.
+
+* `policy_assignment_id` - Indicates the rule ID.
+
+* `policy_assignment_name` - Indicates the rule name.
+
+* `policy_definition_id` - Indicates the policy ID.
+
+* `evaluation_time` - Indicates the compliance state evaluation time.
---
+++ huaweicloud/services/acceptance/rms/data_source_huaweicloud_rms_resource_policy_states_test.go
@@ -0,0 +1,70 @@
+package rms
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceRmsResourcePolicyStates_basic(t *testing.T) {
+	dataSource := "data.huaweicloud_rms_resource_policy_states.test"
+	dc := acceptance.InitDataSourceCheck(dataSource)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceRmsResourcePolicyStates_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "region"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.#"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.0.domain_id"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.0.region_id"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.0.resource_id"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.0.resource_name"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.0.resource_provider"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.0.resource_type"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.0.trigger_type"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.0.compliance_state"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.0.policy_assignment_id"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.0.policy_assignment_name"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.0.policy_definition_id"),
+					resource.TestCheckResourceAttrSet(dataSource, "value.0.evaluation_time"),
+					resource.TestCheckOutput("compliance_state_filter_is_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testDataSourceRmsResourcePolicyStates_basic() string {
+	return `
+data "huaweicloud_rms_resources" "test" {
+  type = "kms.keys"
+}
+
+data "huaweicloud_rms_resource_policy_states" "test" {
+  resource_id = data.huaweicloud_rms_resources.test.resources[0].id
+}
+
+locals {
+  compliance_state = data.huaweicloud_rms_resource_policy_states.test.value[0].compliance_state
+}
+data "huaweicloud_rms_resource_policy_states" "compliance_state_filter" {
+  resource_id      = data.huaweicloud_rms_resources.test.resources[0].id
+  compliance_state = data.huaweicloud_rms_resource_policy_states.test.value[0].compliance_state
+}
+output "compliance_state_filter_is_useful" {
+  value = length(data.huaweicloud_rms_resource_policy_states.compliance_state_filter.value) > 0 && alltrue(
+  [for v in data.huaweicloud_rms_resource_policy_states.compliance_state_filter.value[*].compliance_state :
+  v == local.compliance_state]
+  )
+}
+`
+}
