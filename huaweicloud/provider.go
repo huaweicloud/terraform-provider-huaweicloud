ba9fadd433771bf06e0ebf9058cdb799c8526e4c
jinyangyang222 <jinyangyang222@outlook.com>
2025-08-22 17:29:31 +0800

    feat(aad): support `aad_ip_ddos_statistics` data source

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -476,6 +476,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_aad_bandwidth_curve":          aad.DataSourceBandwidthCurve(),
 			"huaweicloud_aad_custom_rules":             aad.DataSourceCustomRules(),
 			"huaweicloud_aad_frequency_control_rules":  aad.DataSourceFrequencyControlRules(),
+			"huaweicloud_aad_ip_ddos_statistics":       aad.DataSourceIpDdosStatistics(),
 
 			"huaweicloud_antiddos_config_ranges":                antiddos.DataSourceConfigRanges(),
 			"huaweicloud_antiddos_weekly_protection_statistics": antiddos.DataSourceWeeklyProtectionStatistics(),
