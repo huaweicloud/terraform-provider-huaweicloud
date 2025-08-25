cd2e34a2575ce4970696274f9d05f8933b3a326d
hwbyuehen <724651851@qq.com>
2025-08-25 15:00:51 +0800

    feat(COC): add COC application resource (#7626)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2099,6 +2099,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_coc_war_room":               coc.ResourceWarRoom(),
 			"huaweicloud_coc_custom_event_report":    coc.ResourceCustomEventReport(),
 			"huaweicloud_coc_document":               coc.ResourceDocument(),
+			"huaweicloud_coc_application":            coc.ResourceApplication(),
 
 			"huaweicloud_cph_server":         cph.ResourceCphServer(),
 			"huaweicloud_cph_adb_command":    cph.ResourceAdbCommand(),
