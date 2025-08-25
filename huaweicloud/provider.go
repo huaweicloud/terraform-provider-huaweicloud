6e948a280ebbe27c8e439c48758d91af5f714831
ruwenqiang123 <ruwenqiang123@outlook.com>
2025-08-25 11:02:04 +0800

    feat(waf): add new data source to query the top service exceptions

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1635,6 +1635,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_waf_geolocation_detail":                   waf.DataSourceGeolocationDetail(),
 			"huaweicloud_waf_instance_groups":                      waf.DataSourceWafInstanceGroups(),
 			"huaweicloud_waf_overviews_bandwidth_timeline":         waf.DataSourceWafOverviewsBandwidthTimeline(),
+			"huaweicloud_waf_overviews_abnormal":                   waf.DataSourceWafOverviewsAbnormal(),
 			"huaweicloud_waf_overviews_classification":             waf.DataSourceWafOverviewsClassification(),
 			"huaweicloud_waf_overviews_qps_timeline":               waf.DataSourceWafOverviewsQPSTimeline(),
 			"huaweicloud_waf_overviews_request_timeline":           waf.DataSourceWafOverviewsRequestTimeline(),
---
+++ huaweicloud/services/acceptance/waf/data_source_huaweicloud_waf_overviews_abnormal_test.go
@@ -0,0 +1,54 @@
+package waf
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceOverviewsAbnormal_basic(t *testing.T) {
+	var (
+		dataSourceName = "data.huaweicloud_waf_overviews_abnormal.test"
+		dc             = acceptance.InitDataSourceCheck(dataSourceName)
+		startTime      = time.Now().Add(-24*time.Hour).UnixNano() / int64(time.Millisecond)
+		endTime        = time.Now().UnixNano() / int64(time.Millisecond)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			// Because there is no available data for testing, the test case is only
+			// used to verify that the API can be invoked.
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPrecheckWafInstance(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceOverviewsAbnormal_basic(startTime, endTime),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSourceName, "items.#"),
+
+					resource.TestCheckOutput("test_verify", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testDataSourceOverviewsAbnormal_basic(startTime, endTime int64) string {
+	return fmt.Sprintf(`
+data "huaweicloud_waf_overviews_abnormal" "test" {
+  from = %[1]d
+  to   = %[2]d
+}
+
+output "test_verify" {
+  value = length(data.huaweicloud_waf_overviews_abnormal.test.items) == 0
+}
+`, startTime, endTime)
+}
