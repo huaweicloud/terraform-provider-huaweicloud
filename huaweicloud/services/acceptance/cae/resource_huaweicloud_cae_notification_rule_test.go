e8a9ce26654e974dc028dcab3790155542d5883d
Lance52259 <Overlord52259@gmail.com>
2025-08-28 11:07:13 +0800

    refactor(cae): notification rule refactor and supports EPS ID

--- huaweicloud/services/acceptance/cae/resource_huaweicloud_cae_notification_rule_test.go
+++ huaweicloud/services/acceptance/cae/resource_huaweicloud_cae_notification_rule_test.go
@@ -12,46 +12,53 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/cae"
 )
 
-func getResourceCaeNotificationRuleFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
+func getNotificationRuleFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
 	client, err := cfg.NewServiceClient("cae", acceptance.HW_REGION_NAME)
 	if err != nil {
 		return nil, fmt.Errorf("error creating CAE client: %s", err)
 	}
 
-	return cae.GetEventNotificationRuleById(client, state.Primary.ID)
+	return cae.GetEventNotificationRuleById(client, state.Primary.ID, state.Primary.Attributes["enterprise_project_id"])
 }
 
-func TestAccResourceNotificationRule_basic(t *testing.T) {
+func TestAccNotificationRule_basic(t *testing.T) {
 	var (
+		obj interface{}
+
 		rNameWithEnv = "huaweicloud_cae_notification_rule.env"
+		rcWithEnv    = acceptance.InitResourceCheck(rNameWithEnv, &obj, getNotificationRuleFunc)
+
 		rNameWithApp = "huaweicloud_cae_notification_rule.app"
+		rcWithApp    = acceptance.InitResourceCheck(rNameWithApp, &obj, getNotificationRuleFunc)
+
 		rNameWithCom = "huaweicloud_cae_notification_rule.com"
-		name         = acceptance.RandomAccResourceNameWithDash()
-		obj          interface{}
-		rcWithEnv    = acceptance.InitResourceCheck(rNameWithEnv, &obj, getResourceCaeNotificationRuleFunc)
-		rcWithApp    = acceptance.InitResourceCheck(rNameWithApp, &obj, getResourceCaeNotificationRuleFunc)
-		rcWithCom    = acceptance.InitResourceCheck(rNameWithCom, &obj, getResourceCaeNotificationRuleFunc)
+		rcWithCom    = acceptance.InitResourceCheck(rNameWithCom, &obj, getNotificationRuleFunc)
+
+		name = acceptance.RandomAccResourceNameWithDash()
 	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acceptance.TestAccPreCheck(t)
-			acceptance.TestAccPreCheckCaeEnvironment(t)
-			acceptance.TestAccPreCheckCaeApplication(t)
+			acceptance.TestAccPreCheckCaeEnvironmentIds(t, 1)
 		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
-		CheckDestroy:      rcWithEnv.CheckResourceDestroy(),
-
+		CheckDestroy: resource.ComposeTestCheckFunc(
+			rcWithEnv.CheckResourceDestroy(),
+			rcWithApp.CheckResourceDestroy(),
+			rcWithCom.CheckResourceDestroy(),
+		),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceNotificationRule_basic_step1(name),
+				Config: testAccNotificationRule_basic_step1(name),
 				// For all components in the environment.
 				Check: resource.ComposeTestCheckFunc(
 					rcWithEnv.CheckResourceExists(),
 					resource.TestCheckResourceAttr(rNameWithEnv, "name", name),
 					resource.TestCheckResourceAttr(rNameWithEnv, "event_name", "Started,Healthy"),
 					resource.TestCheckResourceAttr(rNameWithEnv, "scope.0.type", "environments"),
-					resource.TestCheckResourceAttr(rNameWithEnv, "scope.0.environments.0", acceptance.HW_CAE_ENVIRONMENT_ID),
+					resource.TestCheckResourceAttr(rNameWithEnv, "scope.0.environments.#", "1"),
+					resource.TestCheckResourceAttrSet(rNameWithEnv, "scope.0.environments.0"),
 					resource.TestCheckResourceAttr(rNameWithEnv, "trigger_policy.0.type", "accumulative"),
 					resource.TestCheckResourceAttr(rNameWithEnv, "trigger_policy.0.period", "300"),
 					resource.TestCheckResourceAttr(rNameWithEnv, "trigger_policy.0.count", "50"),
@@ -64,7 +71,9 @@ func TestAccResourceNotificationRule_basic(t *testing.T) {
 					rcWithApp.CheckResourceExists(),
 					resource.TestCheckResourceAttr(rNameWithApp, "event_name", "Unhealthy"),
 					resource.TestCheckResourceAttr(rNameWithApp, "scope.0.type", "applications"),
-					resource.TestCheckResourceAttr(rNameWithApp, "scope.0.applications.0", acceptance.HW_CAE_APPLICATION_ID),
+					resource.TestCheckResourceAttr(rNameWithApp, "scope.0.applications.#", "2"),
+					resource.TestCheckResourceAttrPair(rNameWithApp, "scope.0.applications.0", "huaweicloud_cae_application.test.0", "id"),
+					resource.TestCheckResourceAttrPair(rNameWithApp, "scope.0.applications.1", "huaweicloud_cae_application.test.1", "id"),
 					resource.TestCheckResourceAttr(rNameWithApp, "trigger_policy.0.type", "immediately"),
 					resource.TestCheckResourceAttr(rNameWithApp, "notification.0.protocol", "sms"),
 					resource.TestCheckResourceAttr(rNameWithApp, "notification.0.endpoint", "12345678987"),
@@ -85,7 +94,7 @@ func TestAccResourceNotificationRule_basic(t *testing.T) {
 				),
 			},
 			{
-				Config: testAccResourceNotificationRule_basic_step2(name),
+				Config: testAccNotificationRule_basic_step2(name),
 				Check: resource.ComposeTestCheckFunc(
 					// Updates from all components in the environment to the specified components.
 					rcWithEnv.CheckResourceExists(),
@@ -98,7 +107,9 @@ func TestAccResourceNotificationRule_basic(t *testing.T) {
 					rcWithApp.CheckResourceExists(),
 					resource.TestCheckResourceAttr(rNameWithApp, "event_name", "FailedPullImage"),
 					resource.TestCheckResourceAttr(rNameWithApp, "scope.0.type", "environments"),
-					resource.TestCheckResourceAttr(rNameWithApp, "scope.0.environments.0", acceptance.HW_CAE_ENVIRONMENT_ID),
+					resource.TestCheckResourceAttr(rNameWithApp, "scope.0.environments.#", "1"),
+					resource.TestCheckResourceAttrPair(rNameWithApp, "scope.0.environments.0",
+						"data.huaweicloud_cae_environments.test", "environments.0.id"),
 					resource.TestCheckResourceAttr(rNameWithApp, "trigger_policy.0.type", "accumulative"),
 					resource.TestCheckResourceAttr(rNameWithApp, "trigger_policy.0.period", "86400"),
 					resource.TestCheckResourceAttr(rNameWithApp, "trigger_policy.0.count", "10"),
@@ -108,9 +119,16 @@ func TestAccResourceNotificationRule_basic(t *testing.T) {
 					rcWithCom.CheckResourceExists(),
 					resource.TestCheckResourceAttr(rNameWithCom, "event_name", "FailedMount"),
 					resource.TestCheckResourceAttr(rNameWithCom, "scope.0.type", "applications"),
-					resource.TestCheckResourceAttr(rNameWithCom, "scope.0.applications.0", acceptance.HW_CAE_APPLICATION_ID),
+					resource.TestCheckResourceAttr(rNameWithCom, "scope.0.applications.#", "2"),
+					resource.TestCheckResourceAttrPair(rNameWithCom, "scope.0.applications.0", "huaweicloud_cae_application.test.0", "id"),
+					resource.TestCheckResourceAttrPair(rNameWithCom, "scope.0.applications.1", "huaweicloud_cae_application.test.1", "id"),
 					resource.TestCheckResourceAttr(rNameWithCom, "trigger_policy.0.type", "immediately"),
 				),
+			},
+			{
+				ResourceName:      rNameWithEnv,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 			{
 				ResourceName:      rNameWithEnv,
@@ -122,7 +140,17 @@ func TestAccResourceNotificationRule_basic(t *testing.T) {
 				ResourceName:      rNameWithApp,
 				ImportState:       true,
 				ImportStateVerify: true,
+			},
+			{
+				ResourceName:      rNameWithApp,
+				ImportState:       true,
+				ImportStateVerify: true,
 				ImportStateIdFunc: testAccNotificationRuleImportFunc(rNameWithApp),
+			},
+			{
+				ResourceName:      rNameWithCom,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 			{
 				ResourceName:      rNameWithCom,
@@ -134,7 +162,63 @@ func TestAccResourceNotificationRule_basic(t *testing.T) {
 	})
 }
 
-func testAccResourceNotificationRule_basic_step1(name string) string {
+func testAccNotificationRule_base(name string) string {
+	return fmt.Sprintf(`
+locals {
+  env_ids = split(",", "%[1]s")
+}
+
+# Query by environment ID under default enterprise project ID.
+data "huaweicloud_cae_environments" "test" {
+  environment_id = local.env_ids[0]
+}
+
+data "huaweicloud_swr_repositories" "test" {}
+
+locals {
+  swr_repositories = [for v in data.huaweicloud_swr_repositories.test.repositories : v if length(v.tags) > 0][0]
+}
+
+resource "huaweicloud_cae_application" "test" {
+  count = 2
+
+  environment_id = try(data.huaweicloud_cae_environments.test.environments[0].id, "NOT_FOUND")
+  name           = format("%[2]s-%%d", count.index)
+}
+
+resource "huaweicloud_cae_component" "test" {
+  count = 2
+
+  environment_id = try(data.huaweicloud_cae_environments.test.environments[0].id, "NOT_FOUND")
+  application_id = huaweicloud_cae_application.test[count.index].id
+
+  metadata {
+    name = format("%[2]s-%%d", count.index)
+
+    annotations = {
+      version = "1.0.0"
+    }
+  }
+
+  spec {
+    replica = 1
+    runtime = "Docker"
+
+    source {
+      type = "image"
+      url  = format("%%s:%%s", local.swr_repositories.path, local.swr_repositories.tags[0])
+    }
+
+    resource_limit {
+      cpu    = "1000m"
+      memory = "4Gi"
+    }
+  }
+}
+`, acceptance.HW_CAE_ENVIRONMENT_IDS, name)
+}
+
+func testAccNotificationRule_basic_step1(name string) string {
 	return fmt.Sprintf(`
 %[1]s
 
@@ -145,7 +229,7 @@ resource "huaweicloud_cae_notification_rule" "env" {
 
   scope {
     type         = "environments"
-    environments = ["%[3]s"]
+    environments = data.huaweicloud_cae_environments.test.environments[*].id
   }
 
   trigger_policy {
@@ -171,7 +255,7 @@ resource "huaweicloud_cae_notification_rule" "app" {
 
   scope {
     type         = "applications"
-    applications = ["%[4]s"]
+    applications = huaweicloud_cae_application.test[*].id
   }
 
   trigger_policy {
@@ -208,10 +292,10 @@ resource "huaweicloud_cae_notification_rule" "com" {
     template = "ZH"
   }
 }
-`, testAccResourceNotificationRule_base(name), name, acceptance.HW_CAE_ENVIRONMENT_ID, acceptance.HW_CAE_APPLICATION_ID)
+`, testAccNotificationRule_base(name), name)
 }
 
-func testAccResourceNotificationRule_basic_step2(name string) string {
+func testAccNotificationRule_basic_step2(name string) string {
 	return fmt.Sprintf(`
 %[1]s
 
@@ -245,7 +329,7 @@ resource "huaweicloud_cae_notification_rule" "app" {
 
   scope {
     type         = "environments"
-    environments = ["%[4]s"]
+    environments = data.huaweicloud_cae_environments.test.environments[*].id
   }
 
   trigger_policy {
@@ -271,7 +355,7 @@ resource "huaweicloud_cae_notification_rule" "com" {
 
   scope {
     type         = "applications"
-    applications = ["%[3]s"]
+    applications = huaweicloud_cae_application.test[*].id
   }
 
   trigger_policy {
@@ -284,47 +368,9 @@ resource "huaweicloud_cae_notification_rule" "com" {
     template = "ZH"
   }
 }
-`, testAccResourceNotificationRule_base(name), name, acceptance.HW_CAE_APPLICATION_ID, acceptance.HW_CAE_ENVIRONMENT_ID)
+`, testAccNotificationRule_base(name), name)
 }
 
-func testAccResourceNotificationRule_base(name string) string {
-	return fmt.Sprintf(`
-data "huaweicloud_swr_repositories" "test" {}
-
-locals {
-  swr_repositories = [for v in data.huaweicloud_swr_repositories.test.repositories : v if length(v.tags) > 0][0]
-}
-
-resource "huaweicloud_cae_component" "test" {
-  count          = 2
-  environment_id = "%[1]s"
-  application_id = "%[2]s"
-
-  metadata {
-    name = "%[3]s${count.index}"
-
-    annotations = {
-      version = "1.0.0"
-    }
-  }
-
-  spec {
-    replica = 1
-    runtime = "Docker"
-
-    source {
-      type = "image"
-      url  = format("%%s:%%s", local.swr_repositories.path, local.swr_repositories.tags[0])
-    }
-
-    resource_limit {
-      cpu    = "1000m"
-      memory = "4Gi"
-    }
-  }
-}
-`, acceptance.HW_CAE_ENVIRONMENT_ID, acceptance.HW_CAE_APPLICATION_ID, name)
-}
 func testAccNotificationRuleImportFunc(name string) resource.ImportStateIdFunc {
 	return func(s *terraform.State) (string, error) {
 		rs, ok := s.RootModule().Resources[name]
@@ -338,4 +384,77 @@ func testAccNotificationRuleImportFunc(name string) resource.ImportStateIdFunc {
 		}
 		return name, nil
 	}
+}
+
+func TestAccNotificationRule_withEpsId(t *testing.T) {
+	var (
+		obj interface{}
+
+		rNameWithEnvUnderEps = "huaweicloud_cae_notification_rule.env"
+		rcWithEnvUnderEps    = acceptance.InitResourceCheck(rNameWithEnvUnderEps, &obj, getNotificationRuleFunc)
+
+		name = acceptance.RandomAccResourceNameWithDash()
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckCaeEnvironmentIds(t, 2)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      rcWithEnvUnderEps.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNotificationRule_withEpsId_step1(name),
+				Check: resource.ComposeTestCheckFunc(
+					rcWithEnvUnderEps.CheckResourceExists(),
+				),
+			},
+		},
+	})
+}
+
+func testAccNotificationRule_withEpsId_base() string {
+	return fmt.Sprintf(`
+locals {
+  env_ids = split(",", "%[1]s")
+}
+
+# Query by environment ID under non-default enterprise project ID.
+data "huaweicloud_cae_environments" "test" {
+  environment_id = local.env_ids[1]
+}
+`, acceptance.HW_CAE_ENVIRONMENT_IDS)
+}
+
+func testAccNotificationRule_withEpsId_step1(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+# For all components in the environment.
+resource "huaweicloud_cae_notification_rule" "env" {
+  name       = "%[2]s"
+  event_name = "Started,Healthy"
+
+  scope {
+    type         = "environments"
+    environments = data.huaweicloud_cae_environments.test.environments[*].id
+  }
+
+  trigger_policy {
+    type     = "accumulative"
+    period   = 300
+    count    = 50
+    operator = ">="
+  }
+
+  notification {
+    protocol = "email"
+    endpoint = "terraform@test.com"
+    template = "EN"
+  }
+
+  enabled = true
+}
+`, testAccNotificationRule_withEpsId_base(), name)
 }
