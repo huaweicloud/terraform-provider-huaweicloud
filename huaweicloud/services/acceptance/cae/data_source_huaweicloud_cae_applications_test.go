0b37dc23e33ef68a16e2a1fc3bdc4780dbe672dc
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-08-28 10:46:32 +0800

    feat(cae/application): add enterprise_project_id for resource and data source (#7663)
    * feat(cae/application): add enterprise_project_id for resource
    
    * feat(cae/applications): the data source supports enterprise_project_id

--- huaweicloud/services/acceptance/cae/data_source_huaweicloud_cae_applications_test.go
+++ huaweicloud/services/acceptance/cae/data_source_huaweicloud_cae_applications_test.go
@@ -2,6 +2,7 @@ package cae
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -10,42 +11,129 @@ import (
 )
 
 func TestAccDatasourceApplications_basic(t *testing.T) {
-	rName := "data.huaweicloud_cae_applications.test"
-	dc := acceptance.InitDataSourceCheck(rName)
+	var (
+		name = acceptance.RandomAccResourceNameWithDash()
+
+		resourceName = "data.huaweicloud_cae_applications.test"
+		all          = acceptance.InitDataSourceCheck(resourceName)
+
+		byApplicationId   = "data.huaweicloud_cae_applications.filter_by_application_id"
+		dcByApplicationId = acceptance.InitDataSourceCheck(byApplicationId)
+
+		byName   = "data.huaweicloud_cae_applications.filter_by_name"
+		dcByName = acceptance.InitDataSourceCheck(byName)
+
+		byEnterpriseProjectId   = "data.huaweicloud_cae_applications.filter_by_enterprise_project_id"
+		dcByEnterpriseProjectId = acceptance.InitDataSourceCheck(byEnterpriseProjectId)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acceptance.TestAccPreCheck(t)
-			acceptance.TestAccPreCheckCaeEnvironment(t)
-			acceptance.TestAccPreCheckCaeApplication(t)
+			acceptance.TestAccPreCheckCaeEnvironmentIds(t, 2)
 		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDatasourceApplications_basic(),
+				Config: testAccDatasourceApplications_basic(name),
 				Check: resource.ComposeTestCheckFunc(
-					dc.CheckResourceExists(),
-
+					all.CheckResourceExists(),
+					resource.TestMatchResourceAttr(resourceName, "applications.#", regexp.MustCompile(`^[1-9]([0-9]*)?$`)),
+					dcByApplicationId.CheckResourceExists(),
 					resource.TestCheckOutput("application_id_filter_is_useful", "true"),
+					resource.TestCheckResourceAttrSet(resourceName, "applications.0.id"),
+					resource.TestCheckResourceAttrSet(resourceName, "applications.0.name"),
+					resource.TestCheckResourceAttrSet(resourceName, "applications.0.created_at"),
+					resource.TestCheckResourceAttrSet(resourceName, "applications.0.updated_at"),
+					dcByName.CheckResourceExists(),
+					resource.TestCheckOutput("name_filter_is_useful", "true"),
+					dcByEnterpriseProjectId.CheckResourceExists(),
+					resource.TestCheckOutput("enterprise_project_id_filter_is_useful", "true"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDatasourceApplications_basic() string {
+func testAccDatasourceApplications_base(name string) string {
 	return fmt.Sprintf(`
+locals {
+  env_ids = split(",", "%[1]s")
+}
+
+# Query the enterprise project ID of the environment.
+data "huaweicloud_cae_environments" "test" {
+  environment_id = local.env_ids[1]
+}
+
+# The first application belongs to the default enterprise project.
+# The second application belongs to the non-default enterprise project.
+resource "huaweicloud_cae_application" "test" {
+  count = 2
+
+  environment_id        = local.env_ids[count.index]
+  name                  = "%[2]s${count.index}"
+  enterprise_project_id = count.index == 1 ? try(data.huaweicloud_cae_environments.test.environments[0].annotations.enterprise_project_id,
+  null) : null
+}
+`, acceptance.HW_CAE_ENVIRONMENT_IDs, name)
+}
+
+func testAccDatasourceApplications_basic(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
 data "huaweicloud_cae_applications" "test" {
-  environment_id = "%[1]s"
-  application_id = "%[2]s"
+  environment_id = huaweicloud_cae_application.test[0].environment_id
+}
+
+# Filter by application ID.
+locals {
+  application_id = huaweicloud_cae_application.test[0].id
+}
+
+data "huaweicloud_cae_applications" "filter_by_application_id" {
+  environment_id = huaweicloud_cae_application.test[0].environment_id
+  application_id = local.application_id
 }
 
 locals {
-  application_id_filter_result = [for v in data.huaweicloud_cae_applications.test.applications : v.id == "%[2]s"]
+  application_id_filter_result = [for v in data.huaweicloud_cae_applications.filter_by_application_id.applications[*].id : v == local.application_id]
 }
 
 output "application_id_filter_is_useful" {
-  value = alltrue(local.application_id_filter_result) && length(local.application_id_filter_result) > 0
+  value = length(local.application_id_filter_result) > 0 && alltrue(local.application_id_filter_result)
 }
-`, acceptance.HW_CAE_ENVIRONMENT_ID, acceptance.HW_CAE_APPLICATION_ID)
+
+# Filter by application name.
+locals {
+  application_name = huaweicloud_cae_application.test[0].name
+}
+
+data "huaweicloud_cae_applications" "filter_by_name" {
+  environment_id = huaweicloud_cae_application.test[0].environment_id
+  name           = local.application_name
+
+  depends_on = [huaweicloud_cae_application.test]
+}
+
+locals {
+  name_filter_result = [for v in data.huaweicloud_cae_applications.filter_by_name.applications[*].name : v == local.application_name]
+}
+
+output "name_filter_is_useful" {
+  value = length(local.name_filter_result) > 0 && alltrue(local.name_filter_result)
+}
+
+# Filter by enterprise project ID.	
+data "huaweicloud_cae_applications" "filter_by_enterprise_project_id" {
+  environment_id        = huaweicloud_cae_application.test[1].environment_id
+  enterprise_project_id = huaweicloud_cae_application.test[1].enterprise_project_id
+}
+
+# Only check the enterprise_project_id parameter is valid.
+output "enterprise_project_id_filter_is_useful" {
+  value = length(data.huaweicloud_cae_applications.filter_by_enterprise_project_id.applications) > 0
+}
+`, testAccDatasourceApplications_base(name))
 }
--- huaweicloud/services/acceptance/cae/resource_huaweicloud_cae_application_test.go
+++ huaweicloud/services/acceptance/cae/resource_huaweicloud_cae_application_test.go
@@ -19,53 +19,72 @@ func getApplicationFunc(cfg *config.Config, state *terraform.ResourceState) (int
 		return nil, fmt.Errorf("error creating CAE client: %s", err)
 	}
 
-	envId := state.Primary.Attributes["environment_id"]
-	return cae.GetApplicationById(client, envId, state.Primary.ID)
+	return cae.GetApplicationById(
+		client,
+		state.Primary.Attributes["environment_id"],
+		state.Primary.ID,
+		state.Primary.Attributes["enterprise_project_id"],
+	)
 }
 
 func TestAccApplication_basic(t *testing.T) {
 	var (
-		obj interface{}
-
-		resourceName = "huaweicloud_cae_application.test"
-		rc           = acceptance.InitResourceCheck(resourceName, &obj, getApplicationFunc)
-
 		invalidName = "-tf-test-invalid-name"
 		name        = acceptance.RandomAccResourceNameWithDash()
-		baseConfig  = testAccApplication_base(name)
+
+		obj          interface{}
+		resourceName = "huaweicloud_cae_application.test.0"
+		rc           = acceptance.InitResourceCheck(resourceName, &obj, getApplicationFunc)
+
+		withNotDefaultEpsId   = "huaweicloud_cae_application.test.1"
+		rcWithNotDefaultEpsId = acceptance.InitResourceCheck(withNotDefaultEpsId, &obj, getApplicationFunc)
 	)
 
-	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckCaeEnvironmentIds(t, 2)
+		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config:      testAccApplication_basic(baseConfig, invalidName),
+				Config:      testAccApplication_basic(invalidName),
 				ExpectError: regexp.MustCompile(`CAE.01500214`),
 			},
 			{
-				Config: testAccApplication_basic(baseConfig, name),
+				Config: testAccApplication_basic(name),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestMatchResourceAttr(resourceName, "name", regexp.MustCompile(fmt.Sprintf("^%s\\d$", name))),
+					resource.TestCheckNoResourceAttr(resourceName, "enterprise_project_id"),
 					resource.TestMatchResourceAttr(resourceName, "created_at",
 						regexp.MustCompile(`^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}?(Z|([+-]\d{2}:\d{2}))$`)),
 					resource.TestMatchResourceAttr(resourceName, "updated_at",
 						regexp.MustCompile(`^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}?(Z|([+-]\d{2}:\d{2}))$`)),
+					rcWithNotDefaultEpsId.CheckResourceExists(),
+					resource.TestMatchResourceAttr(withNotDefaultEpsId, "name", regexp.MustCompile(fmt.Sprintf("^%s\\d$", name))),
+					resource.TestCheckResourceAttrPair(withNotDefaultEpsId, "enterprise_project_id",
+						"data.huaweicloud_cae_environments.test", "environments.0.annotations.enterprise_project_id"),
 				),
 			},
 			{
-				ResourceName:      resourceName,
+				ResourceName:      "huaweicloud_cae_application.test[0]",
 				ImportState:       true,
 				ImportStateVerify: true,
-				ImportStateIdFunc: testAccApplicationImportStateFunc(resourceName),
+				ImportStateIdFunc: testAccApplicationImportStateFunc(resourceName, false),
+			},
+			{
+				ResourceName:      "huaweicloud_cae_application.test[1]",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateIdFunc: testAccApplicationImportStateFunc(withNotDefaultEpsId, true),
 			},
 		},
 	})
 }
 
-func testAccApplicationImportStateFunc(rsName string) resource.ImportStateIdFunc {
+func testAccApplicationImportStateFunc(rsName string, isNotDefaultEpsId bool) resource.ImportStateIdFunc {
 	return func(s *terraform.State) (string, error) {
 		rs, ok := s.RootModule().Resources[rsName]
 		if !ok {
@@ -81,57 +100,37 @@ func testAccApplicationImportStateFunc(rsName string) resource.ImportStateIdFunc
 				environmentId, applicationId)
 		}
 
+		if isNotDefaultEpsId {
+			epsId := rs.Primary.Attributes["enterprise_project_id"]
+			if epsId == "" {
+				return "", fmt.Errorf("some import IDs are missing, want '<environment_id>/<id>/<enterprise_project_id>', but got '%s/%s/%s'",
+					environmentId, applicationId, epsId)
+			}
+			return fmt.Sprintf("%s/%s/%s", environmentId, applicationId, epsId), nil
+		}
+
 		return fmt.Sprintf("%s/%s", environmentId, applicationId), nil
 	}
 }
 
-func testAccApplication_base(name string) string {
+func testAccApplication_basic(name string) string {
 	return fmt.Sprintf(`
-resource "huaweicloud_vpc" "test" {
-  name = "%[1]s"
-  cidr = "192.168.0.0/16"
+locals {
+  env_ids = split(",", "%[1]s")
 }
 
-resource "huaweicloud_vpc_subnet" "test" {
-  vpc_id     = huaweicloud_vpc.test.id
-  name       = "%[1]s"
-  cidr       = cidrsubnet(huaweicloud_vpc.test.cidr, 4, 1)
-  gateway_ip = cidrhost(cidrsubnet(huaweicloud_vpc.test.cidr, 4, 1), 1)
+# Query by environment ID under non-default enterprise project ID.
+data "huaweicloud_cae_environments" "test" {
+  environment_id = local.env_ids[1]
 }
-
-resource "huaweicloud_networking_secgroup" "test" {
-  name                 = "%[1]s"
-  delete_default_rules = true
-}
-
-resource "huaweicloud_swr_organization" "test" {
-  name = "%[1]s"
-}
-
-resource "huaweicloud_cae_environment" "test" {
-  name = "%[1]s"
-
-  annotations = {
-    type              = "exclusive"
-    vpc_id            = huaweicloud_vpc.test.id
-    subnet_id         = huaweicloud_vpc_subnet.test.id
-    security_group_id = huaweicloud_networking_secgroup.test.id
-    group_name        = huaweicloud_swr_organization.test.name
-  }
-
-  // To avoid k8s container deploy failed.
-  max_retries = 1
-}
-`, name)
-}
-
-func testAccApplication_basic(baseConfig, name string) string {
-	return fmt.Sprintf(`
-%[1]s
 
 resource "huaweicloud_cae_application" "test" {
-  environment_id = huaweicloud_cae_environment.test.id
-  name           = "%[2]s"
+  count = 2
+
+  environment_id        = local.env_ids[count.index]
+  name                  = "%[2]s${count.index}"
+  enterprise_project_id = count.index == 1 ? try(data.huaweicloud_cae_environments.test.environments[0].annotations.enterprise_project_id,
+  null) : null
 }
-`, baseConfig, name)
+`, acceptance.HW_CAE_ENVIRONMENT_IDs, name)
 }
