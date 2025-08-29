55f2ff30d951cfae5bc1f4a5dd1cb5b620724bbb
jinyangyang222 <jinyangyang222@outlook.com>
2025-08-29 14:40:50 +0800

    feat(aad): support `aad_connection_numbers` data source (#7678)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -481,6 +481,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_aad_flow_block":                  aad.DataSourceFlowBlock(),
 			"huaweicloud_aad_ddos_attack_protection_info": aad.DataSourceDdosAttackProtectionInfo(),
 			"huaweicloud_aad_attack_events":               aad.DataSourceAttackEvents(),
+			"huaweicloud_aad_connection_numbers":          aad.DataSourceConnectionNumbers(),
 
 			"huaweicloud_antiddos_config_ranges":                antiddos.DataSourceConfigRanges(),
 			"huaweicloud_antiddos_weekly_protection_statistics": antiddos.DataSourceWeeklyProtectionStatistics(),
