dcf11494f96eb41961a96aee59eaa1669d82bdde
hwbyuehen <724651851@qq.com>
2025-09-02 15:32:39 +0800

    feat(COC): add COC instance batches data source (#7698)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -775,6 +775,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_coc_application_next_models":       coc.DataSourceCocApplicationNextModels(),
 			"huaweicloud_coc_components":                    coc.DataSourceCocComponents(),
 			"huaweicloud_coc_groups":                        coc.DataSourceCocGroups(),
+			"huaweicloud_coc_instance_batches":              coc.DataSourceCocInstanceBatches(),
 
 			"huaweicloud_compute_flavors":                 ecs.DataSourceEcsFlavors(),
 			"huaweicloud_compute_instance":                ecs.DataSourceComputeInstance(),
