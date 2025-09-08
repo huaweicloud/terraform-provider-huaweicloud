60f4b31d71286efa7ad1ade63310f1a751eb3837
Lance52259 <Overlord52259@gmail.com>
2025-09-08 10:45:07 +0800

    feat(fgs): add new best practice for CTS trigger

---
+++ examples/fgs/triggers/cts/terraform.tfvars
@@ -0,0 +1,6 @@
+function_name        = "tf_test_cts_function"
+function_agency_name = "function_all_trust"
+trigger_name         = "tf_test_cts_trigger"
+trigger_operations   = [
+  "FunctionGraph:Functions:createFunction"
+]
---
+++ examples/fgs/triggers/cts/variables.tf
@@ -0,0 +1,107 @@
+# Variable definitions for authentication
+variable "region_name" {
+  description = "The region where the FunctionGraph service is located"
+  type        = string
+}
+
+variable "access_key" {
+  description = "The access key of the IAM user"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "The secret key of the IAM user"
+  type        = string
+}
+
+# Variable definitions for FunctionGraph resources
+variable "function_name" {
+  description = "The name of the FunctionGraph function"
+  type        = string
+}
+
+variable "function_agency_name" {
+  description = "The agency name of the FunctionGraph function"
+  type        = string
+}
+
+variable "function_memory_size" {
+  description = "The memory size of the function in MB"
+  type        = number
+  default     = 128
+}
+
+variable "function_timeout" {
+  description = "The timeout of the function in seconds"
+  type        = number
+  default     = 10
+}
+
+variable "function_runtime" {
+  description = "The runtime of the function"
+  type        = string
+  default     = "Python2.7"
+}
+
+variable "function_code" {
+  description = "The source code of the function"
+  type        = string
+  default     = <<EOT
+# -*- coding:utf-8 -*-
+'''
+CTS trigger event:
+{
+  "cts":  {
+        "time": "",
+        "user": {
+            "name": "userName",
+            "id": "",
+            "domain": {
+                "name": "domainName",
+                "id": ""
+            }
+        },
+        "request": {},
+        "response": {},
+        "code": 204,
+        "service_type": "FunctionGraph",
+        "resource_type": "",
+        "resource_name": "",
+        "resource_id": {},
+        "trace_name": "",
+        "trace_type": "ConsoleAction",
+        "record_time": "",
+        "trace_id": "",
+        "trace_status": "normal"
+    }
+}
+'''
+def handler (event, context):
+    trace_name = event["cts"]["resource_name"]
+    timeinfo = event["cts"]["time"]
+    print(timeinfo+' '+trace_name)
+EOT
+}
+
+variable "function_description" {
+  description = "The description of the function"
+  type        = string
+  default     = ""
+}
+
+variable "trigger_status" {
+  description = "The status of the FunctionGraph CTS trigger"
+  type        = string
+  default     = "ACTIVE"
+}
+
+variable "trigger_name" {
+  description = "The name of the FunctionGraph CTS trigger"
+  type        = string
+}
+
+variable "trigger_operations" {
+  description = "The operations to monitor for the FunctionGraph CTS trigger"
+  type        = list(string)
+  nullable    = false
+}
