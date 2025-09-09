322b053dd9a53747b1e7ec92edcff27d984f8d1a
ruwenqiang123 <ruwenqiang123@outlook.com>
2025-09-09 15:17:05 +0800

    feat(secmaster): add new resource to manage collector channel group (#7781)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2981,6 +2981,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_secmaster_alert":                       secmaster.ResourceAlert(),
 			"huaweicloud_secmaster_alert_rule":                  secmaster.ResourceAlertRule(),
 			"huaweicloud_secmaster_clone_playbook_version":      secmaster.ResourceClonePlaybookAndVersion(),
+			"huaweicloud_secmaster_collector_channel_group":     secmaster.ResourceCollectorChannelGroup(),
 			"huaweicloud_secmaster_data_object_relations":       secmaster.ResourceDataObjectRelations(),
 			"huaweicloud_secmaster_dataspace":                   secmaster.ResourceDataspace(),
 			"huaweicloud_secmaster_delete_policies":             secmaster.ResourceDeletePolicies(),
