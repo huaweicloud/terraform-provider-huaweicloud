ae7843ab2e12066d610ad8833fdb657c121ec870
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-09-11 11:24:32 +0800

    feat(examples/rocketmq): add best practice for consumer group (#7784)

---
+++ examples/dms/rocketmq/consumer-group/main.tf
@@ -0,0 +1,71 @@
+resource "huaweicloud_vpc" "test" {
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+}
+
+resource "huaweicloud_vpc_subnet" "test" {
+  vpc_id     = huaweicloud_vpc.test.id
+  name       = var.subnet_name
+  cidr       = var.subnet_cidr == "" ? cidrsubnet(huaweicloud_vpc.test.cidr, 8, 0) : var.subnet_cidr
+  gateway_ip = var.subnet_gateway_ip == "" ? cidrhost(cidrsubnet(huaweicloud_vpc.test.cidr, 8, 0), 1) : var.subnet_gateway_ip
+}
+
+resource "huaweicloud_networking_secgroup" "test" {
+  name                 = var.security_group_name
+  delete_default_rules = true
+}
+
+data "huaweicloud_dms_rocketmq_availability_zones" "test" {
+  count = length(var.availability_zones) == 0 ? 1 : 0
+}
+
+data "huaweicloud_dms_rocketmq_flavors" "test" {
+  count = var.instance_flavor_id == "" ? 1 : 0
+
+  type               = var.instance_flavor_type
+  availability_zones = length(var.availability_zones) != 0 ? var.availability_zones : slice(data.huaweicloud_dms_rocketmq_availability_zones.test[0].availability_zones[*].code, 0, var.availability_zones_count)
+}
+
+resource "huaweicloud_dms_rocketmq_instance" "test" {
+  name                  = var.instance_name
+  flavor_id             = var.instance_flavor_id != "" ? var.instance_flavor_id : try(data.huaweicloud_dms_rocketmq_flavors.test[0].flavors[0].id, null)
+  engine_version        = var.instance_engine_version != "" ? var.instance_engine_version : try(data.huaweicloud_dms_rocketmq_flavors.test[0].versions[0], null)
+  storage_spec_code     = var.instance_storage_spec_code != "" ? var.instance_storage_spec_code : try(data.huaweicloud_dms_rocketmq_flavors.test[0].flavors[0].ios[0].storage_spec_code, null)
+  storage_space         = var.instance_storage_space
+  availability_zones    = length(var.availability_zones) != 0 ? var.availability_zones : slice(data.huaweicloud_dms_rocketmq_availability_zones.test[0].availability_zones[*].code, 0, var.availability_zones_count)
+  vpc_id                = huaweicloud_vpc.test.id
+  subnet_id             = huaweicloud_vpc_subnet.test.id
+  security_group_id     = huaweicloud_networking_secgroup.test.id
+  broker_num            = var.instance_broker_num
+  description           = var.instance_description
+  tags                  = var.instance_tags
+  enterprise_project_id = var.enterprise_project_id
+  enable_acl            = var.instance_enable_acl
+  tls_mode              = var.instance_tls_mode
+
+  dynamic "configs" {
+    for_each = var.instance_configs
+
+    content {
+      name  = configs.value.name
+      value = configs.value.value
+    }
+  }
+}
+
+data "huaweicloud_dms_rocketmq_broker" "test" {
+  count = length(var.consumer_group_brokers) == 0 && huaweicloud_dms_rocketmq_instance.test.engine_version == "4.8.0" ? 1 : 0
+
+  instance_id = huaweicloud_dms_rocketmq_instance.test.id
+}
+
+resource "huaweicloud_dms_rocketmq_consumer_group" "test" {
+  instance_id     = huaweicloud_dms_rocketmq_instance.test.id
+  name            = var.consumer_group_name
+  retry_max_times = var.consumer_group_retry_max_times
+  enabled         = var.consumer_group_enabled
+  broadcast       = var.consumer_group_broadcast
+  description     = var.consumer_group_description
+  brokers         = length(var.consumer_group_brokers) > 0 ? var.consumer_group_brokers : try(data.huaweicloud_dms_rocketmq_broker.test[0].brokers, [])
+  consume_orderly = var.consumer_group_consume_orderly
+}
---
+++ examples/dms/rocketmq/consumer-group/terraform.tfvars
@@ -0,0 +1,6 @@
+vpc_name            = "tf_test_vpc"
+subnet_name         = "tf_test_subnet"
+security_group_name = "tf_test_security_group"
+instance_name       = "tf_test_instance"
+instance_broker_num = 1
+consumer_group_name = "tf_test_consumer_group"
