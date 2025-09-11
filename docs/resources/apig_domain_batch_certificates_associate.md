115efbfe12d4e2b4911d2ab762e13a76917e95b8
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-09-11 10:33:57 +0800

    feat(apig): add new resource associate the certificates with the domain

---
+++ docs/resources/apig_domain_batch_certificates_associate.md
@@ -0,0 +1,65 @@
+---
+subcategory: "API Gateway (Dedicated APIG)"
+layout: "huaweicloud"
+page_title: "HuaweiCloud: huaweicloud_apig_domain_batch_certificates_associate"
+description: |-
+  Use this resource to associate SSL certificates to the specified domain within HuaweiCloud.
+---
+
+# huaweicloud_apig_domain_batch_certificates_associate
+
+Use this resource to associate SSL certificates to the specified domain within HuaweiCloud.
+
+-> Currently, one certificate can only be associated with one domain. For instance with custom inbound ports,
+   the same domain name is bound to a certificate at the same time. Enabling or disabling client verification
+   takes effect for different ports of the same domain name.
+
+## Example Usage
+
+```hcl
+variable "instance_id" {}
+variable "group_id" {}
+variable "domain_id" {}
+variable "certificate_ids" {}
+
+resource "huaweicloud_apig_domain_batch_certificates_associate" "test" {
+  instance_id     = var.instance_id
+  group_id        = var.group_id
+  domain_id       = var.domain_id
+  certificate_ids = var.certificate_ids
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region where the domain and certificates are located.  
+  If omitted, the provider-level region will be used. Changing this will create a new resource.
+
+* `instance_id` - (Required, String, NonUpdatable) Specifies the ID of the dedicated instance to which the domain belongs.
+
+* `group_id` - (Required, String, NonUpdatable) Specifies the ID of the API group to which the domain belongs.
+
+* `domain_id` - (Required, String, NonUpdatable) Specifies the ID of the domain.
+
+* `certificate_ids` - (Required, List, NonUpdatable) Specifies the list of the certificate IDs to associate with the domain.
+
+* `verified_client_certificate_enabled` - (Optional, Bool) Specifies whether to enable verified client certificate.  
+  Default to **false**.  
+  This parameter only can be set **true** only when the SSL certificate contains a trusted root CA.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID,the format is `<instance_id>/<group_id>/<domain_id>`.
+
+## Import
+
+This resource can be imported using its `id` (consists of `instance_id`, `group_id` and `domain_id`, separated by the
+slashes (/)), e.g.
+
+```shell
+$ terraform import huaweicloud_apig_domain_batch_certificates_associate.test <instance_id>/<group_id>/<domain_id>
+```
---
+++ huaweicloud/services/apig/resource_huaweicloud_apig_domain_batch_certificates_associate.go
@@ -0,0 +1,308 @@
+package apig
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+var domainBatchCertificatesAssociateNonUpdatableParams = []string{
+	"instance_id",
+	"group_id",
+	"domain_id",
+	"certificate_ids",
+}
+
+// @API APIG POST /v2/{project_id}/apigw/instances/{instance_id}/api-groups/{group_id}/domains/{domain_id}/certificates/attach
+// @API APIG GET /v2/{project_id}/apigw/instances/{instance_id}/api-groups/{group_id}
+// @API APIG POST /v2/{project_id}/apigw/instances/{instance_id}/api-groups/{group_id}/domains/{domain_id}/certificates/detach
+func ResourceDomainBatchCertificatesAssociate() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceDomainBatchCertificatesAssociateCreate,
+		ReadContext:   resourceDomainBatchCertificatesAssociateRead,
+		UpdateContext: resourceDomainBatchCertificatesAssociateUpdate,
+		DeleteContext: resourceDomainBatchCertificatesAssociateDelete,
+
+		CustomizeDiff: config.FlexibleForceNew(domainBatchCertificatesAssociateNonUpdatableParams),
+
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceDomainBatchCertificatesAssociateImportState,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				ForceNew:    true,
+				Description: "The region where the domain and certificates are located.",
+			},
+			"instance_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The ID of the dedicated instance to which the domain belongs.",
+			},
+			"group_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The ID of the API group to which the domain belongs.",
+			},
+			"domain_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The ID of the domain.",
+			},
+			"certificate_ids": {
+				Type:        schema.TypeSet,
+				Required:    true,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Description: "The list of the certificate IDs to associate with the domain.",
+			},
+			"verified_client_certificate_enabled": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: "Whether to enable verified client certificate.",
+			},
+			// Internal parameter(s).
+			"enable_force_new": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice([]string{"true", "false"}, false),
+				Description:  utils.SchemaDesc("", utils.SchemaDescInput{Internal: true}),
+			},
+		},
+	}
+}
+
+func buildAssociateCertificatesToDomainBodyParams(certificateIds []interface{}, verififyEnabled bool) map[string]interface{} {
+	return map[string]interface{}{
+		"certificate_ids":                     certificateIds,
+		"verified_client_certificate_enabled": verififyEnabled,
+	}
+}
+
+func associateCertificatesToDomain(client *golangsdk.ServiceClient, instanceId, groupId, domainId string, certificateIds []interface{},
+	verififyEnabled bool) error {
+	httpUrl := "v2/{project_id}/apigw/instances/{instance_id}/api-groups/{group_id}/domains/{domain_id}/certificates/attach"
+	createPath := client.Endpoint + httpUrl
+	createPath = strings.ReplaceAll(createPath, "{project_id}", client.ProjectID)
+	createPath = strings.ReplaceAll(createPath, "{instance_id}", instanceId)
+	createPath = strings.ReplaceAll(createPath, "{group_id}", groupId)
+	createPath = strings.ReplaceAll(createPath, "{domain_id}", domainId)
+
+	opt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		MoreHeaders: map[string]string{
+			"Content-Type": "application/json",
+		},
+		OkCodes:  []int{204},
+		JSONBody: buildAssociateCertificatesToDomainBodyParams(certificateIds, verififyEnabled),
+	}
+
+	_, err := client.Request("POST", createPath, &opt)
+	if err != nil {
+		return fmt.Errorf("error associating certificates to domain (%s): %s", domainId, err)
+	}
+
+	return nil
+}
+
+func resourceDomainBatchCertificatesAssociateCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg        = meta.(*config.Config)
+		region     = cfg.GetRegion(d)
+		instanceId = d.Get("instance_id").(string)
+		groupId    = d.Get("group_id").(string)
+	)
+
+	client, err := cfg.NewServiceClient("apig", region)
+	if err != nil {
+		return diag.Errorf("error creating APIG client: %s", err)
+	}
+
+	err = associateCertificatesToDomain(
+		client,
+		instanceId,
+		groupId,
+		d.Get("domain_id").(string),
+		d.Get("certificate_ids").(*schema.Set).List(),
+		d.Get("verified_client_certificate_enabled").(bool),
+	)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(fmt.Sprintf("%s/%s/%s", instanceId, groupId, d.Get("domain_id").(string)))
+
+	return resourceDomainBatchCertificatesAssociateRead(ctx, d, meta)
+}
+
+func getDomainAssociatedCertificates(client *golangsdk.ServiceClient, instanceId, groupId, domainId string) (interface{}, error) {
+	httpUrl := "v2/{project_id}/apigw/instances/{instance_id}/api-groups/{group_id}"
+	getPath := client.Endpoint + httpUrl
+	getPath = strings.ReplaceAll(getPath, "{project_id}", client.ProjectID)
+	getPath = strings.ReplaceAll(getPath, "{instance_id}", instanceId)
+	getPath = strings.ReplaceAll(getPath, "{group_id}", groupId)
+
+	opt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		MoreHeaders: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}
+
+	requestResp, err := client.Request("GET", getPath, &opt)
+	if err != nil {
+		return nil, err
+	}
+	respBody, err := utils.FlattenResponse(requestResp)
+	if err != nil {
+		return nil, err
+	}
+
+	return utils.PathSearch(fmt.Sprintf("url_domains[?id=='%s']|[0]", domainId), respBody, nil), nil
+}
+
+func GetDomainAssociatedCertificatesByDomainId(client *golangsdk.ServiceClient, instanceId, groupId, domainId string) (interface{}, error) {
+	domainInfos, err := getDomainAssociatedCertificates(client, instanceId, groupId, domainId)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(utils.PathSearch("ssl_infos", domainInfos, make([]interface{}, 0)).([]interface{})) == 0 {
+		return nil, golangsdk.ErrDefault404{}
+	}
+
+	return domainInfos, nil
+}
+
+func resourceDomainBatchCertificatesAssociateRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg        = meta.(*config.Config)
+		region     = cfg.GetRegion(d)
+		instanceId = d.Get("instance_id").(string)
+		groupId    = d.Get("group_id").(string)
+		domainId   = d.Get("domain_id").(string)
+	)
+	client, err := cfg.NewServiceClient("apig", region)
+	if err != nil {
+		return diag.Errorf("error creating APIG client: %s", err)
+	}
+
+	certificates, err := GetDomainAssociatedCertificatesByDomainId(client, instanceId, groupId, domainId)
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, fmt.Sprintf("unable to query the certificate associated with the domain (%s)", domainId))
+	}
+
+	mErr := multierror.Append(
+		d.Set("region", region),
+		d.Set("certificate_ids", utils.PathSearch("ssl_infos[*].ssl_id", certificates, nil)),
+		d.Set("verified_client_certificate_enabled", utils.PathSearch("verified_client_certificate_enabled", certificates, nil)),
+	)
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+// Currently, a certificate can only be associated with one domain name. After multiple certificates are supported in the future,
+// the logic for updating certificate IDs will be supplemented.
+func resourceDomainBatchCertificatesAssociateUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg      = meta.(*config.Config)
+		region   = cfg.GetRegion(d)
+		groupId  = d.Get("group_id").(string)
+		domainId = d.Get("domain_id").(string)
+	)
+	client, err := cfg.NewServiceClient("apig", region)
+	if err != nil {
+		return diag.Errorf("error creating APIG client: %s", err)
+	}
+
+	if d.HasChanges("verified_client_certificate_enabled") {
+		err = associateCertificatesToDomain(
+			client,
+			d.Get("instance_id").(string),
+			groupId,
+			domainId,
+			d.Get("certificate_ids").(*schema.Set).List(),
+			d.Get("verified_client_certificate_enabled").(bool),
+		)
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("error updating client verified function domain (%s) under group (%s): %s", domainId, groupId, err))
+		}
+	}
+
+	return resourceDomainBatchCertificatesAssociateRead(ctx, d, meta)
+}
+
+func disassociateCertificatesFromDomain(client *golangsdk.ServiceClient, instanceId, groupId, domainId string, certificateIds []interface{},
+	verifiedClientCertificateEnabled bool) error {
+	httpUrl := "v2/{project_id}/apigw/instances/{instance_id}/api-groups/{group_id}/domains/{domain_id}/certificates/detach"
+	createPath := client.Endpoint + httpUrl
+	createPath = strings.ReplaceAll(createPath, "{project_id}", client.ProjectID)
+	createPath = strings.ReplaceAll(createPath, "{instance_id}", instanceId)
+	createPath = strings.ReplaceAll(createPath, "{group_id}", groupId)
+	createPath = strings.ReplaceAll(createPath, "{domain_id}", domainId)
+
+	opt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		MoreHeaders: map[string]string{
+			"Content-Type": "application/json",
+		},
+		OkCodes:  []int{204},
+		JSONBody: buildAssociateCertificatesToDomainBodyParams(certificateIds, verifiedClientCertificateEnabled),
+	}
+
+	_, err := client.Request("POST", createPath, &opt)
+	return err
+}
+
+func resourceDomainBatchCertificatesAssociateDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg      = meta.(*config.Config)
+		region   = cfg.GetRegion(d)
+		domainId = d.Get("domain_id").(string)
+	)
+	client, err := cfg.NewServiceClient("apig", region)
+	if err != nil {
+		return diag.Errorf("error creating APIG client: %s", err)
+	}
+
+	err = disassociateCertificatesFromDomain(
+		client,
+		d.Get("instance_id").(string),
+		d.Get("group_id").(string),
+		domainId,
+		d.Get("certificate_ids").(*schema.Set).List(),
+		d.Get("verified_client_certificate_enabled").(bool),
+	)
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, fmt.Sprintf("error disassociating certificates from domain (%s)", domainId))
+	}
+	return nil
+}
+
+func resourceDomainBatchCertificatesAssociateImportState(_ context.Context, d *schema.ResourceData, _ interface{}) ([]*schema.ResourceData, error) {
+	importedId := d.Id()
+	parts := strings.Split(importedId, "/")
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("invalid format specified for import ID, want '<instance_id>/<group_id>/<domain_id>', but got '%s'",
+			importedId)
+	}
+
+	mErr := multierror.Append(
+		d.Set("instance_id", parts[0]),
+		d.Set("group_id", parts[1]),
+		d.Set("domain_id", parts[2]),
+	)
+	return []*schema.ResourceData{d}, mErr.ErrorOrNil()
+}
