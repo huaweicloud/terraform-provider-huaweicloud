8d0aeff02a9b79da77c7d972368a882de0f9998f
Lance <Overlord52259@gmail.com>
2025-09-12 09:32:36 +0800

    feat(apig): add new data source to query associable APIs for plugin (#7811)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -586,6 +586,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_apig_orchestration_rules":                apig.DataSourceOrchestrationRules(),
 			"huaweicloud_apig_orchestration_rule_associated_apis": apig.DataSourceOrchestrationRuleAssociatedApis(),
 			"huaweicloud_apig_plugins":                            apig.DataSourcePlugins(),
+			"huaweicloud_apig_plugin_associable_apis":             apig.DataSourcePluginAssociableApis(),
 			"huaweicloud_apig_quotas":                             apig.DataSourceQuotas(),
 			"huaweicloud_apig_signatures":                         apig.DataSourceSignatures(),
 			"huaweicloud_apig_throttling_policies":                apig.DataSourceThrottlingPolicies(),
