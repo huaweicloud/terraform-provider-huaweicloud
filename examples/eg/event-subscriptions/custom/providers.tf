4244feba5ba4beb2f120c34697b2286b77e8c9ef
Zippo-Wang <852420284@qq.com>
2025-09-12 11:17:49 +0800

    feat(eg): add new best practice for custom event subscription

---
+++ examples/eg/event-subscriptions/custom/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    required_version = ">= 1.1.0"
+
+    huaweicloud = {
+      source  = "huaweicloud/huaweicloud"
+      version = ">= 1.77.4"
+    }
+  }
+}
+
+provider "huaweicloud" {
+  region     = var.region_name
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
