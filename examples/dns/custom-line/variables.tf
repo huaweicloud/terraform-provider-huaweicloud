c313a3fd34b20c27a06335f5a02206dd174efb12
saf3dfsa <my.ctw@outlook.com>
2025-09-12 14:55:59 +0800

    feat(DNS): support custom line example

---
+++ examples/dns/custom-line/variables.tf
@@ -0,0 +1,25 @@
+# Variable definitions for authentication
+variable "region_name" {
+  description = "The region where the DNS service is located"
+  type        = string
+}
+
+variable "access_key" {
+  description = "The access key of the IAM user"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "The secret key of the IAM user"
+  type        = string
+}
+
+variable "dns_custom_line_name" {
+  description = "The custom line name"
+  type        = string
+}
+
+variable "dns_custom_line_ip_segments" {
+  description = "The IP address range"
+  type        = list(string)
+}
