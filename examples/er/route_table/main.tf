443a01cb23aaabd8409ec449677f09c5140b1acd
CaiWanting <my.ctw@outlook.com>
2025-09-15 16:16:51 +0800

    feat(ER): support route table example (#7864)

---
+++ examples/er/route_table/main.tf
@@ -0,0 +1,25 @@
+# Create a route table with ER instance
+data "huaweicloud_er_availability_zones" "test" {}
+
+resource "huaweicloud_vpc" "test" {
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+}
+
+resource "huaweicloud_vpc_subnet" "test" {
+  vpc_id     = huaweicloud_vpc.test.id
+  name       = var.subnet_name
+  cidr       = var.subnet_cidr == "" ? cidrsubnet(huaweicloud_vpc.test.cidr, 8, 0) : var.subnet_cidr
+  gateway_ip = var.subnet_gateway_ip == "" ? cidrhost(cidrsubnet(huaweicloud_vpc.test.cidr, 8, 0), 1) : var.subnet_gateway_ip
+}
+
+resource "huaweicloud_er_instance" "test" {
+  availability_zones = slice(data.huaweicloud_er_availability_zones.test.names, 0, 1)
+  name               = var.er_instance_name
+  asn                = var.er_instance_asn
+}
+
+resource "huaweicloud_er_route_table" "test" {
+  instance_id = huaweicloud_er_instance.test.id
+  name        = var.er_route_table_name
+}
---
+++ examples/er/route_table/terraform.tfvars
@@ -0,0 +1,5 @@
+vpc_name            = "tf_test_er_instance_vpc"
+subnet_name         = "tf_test_er_instance_subnet"
+er_instance_name    = "tf_test_er_instance"
+er_instance_asn     = 64512
+er_route_table_name = "tf_test_er_route_table"
