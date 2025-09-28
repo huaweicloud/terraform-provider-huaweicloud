6e5aea02787e32bedabf5ef7d9302037ed7123a4
ruwenqiang123 <ruwenqiang123@outlook.com>
2025-09-28 14:22:37 +0800

    feat(waf): add new data source to get all web tamper protection rules

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1825,6 +1825,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_waf_all_policy_cc_rules":                  waf.DataSourceAllPolicyCcRules(),
 			"huaweicloud_waf_all_domains":                          waf.DataSourceWafAllDomains(),
 			"huaweicloud_waf_all_precise_protection_rules":         waf.DataSourceAllPreciseProtectionRules(),
+			"huaweicloud_waf_all_web_antitamper_rules":             waf.DataSourceAllWebAntitamperRules(),
 			"huaweicloud_waf_all_whiteblackip_rules":               waf.DataSourceAllWhiteblackipRules(),
 			"huaweicloud_waf_bundle":                               waf.DataSourceUserBundle(),
 			"huaweicloud_waf_certificate":                          waf.DataSourceWafCertificate(),
---
+++ huaweicloud/services/acceptance/waf/data_source_huaweicloud_waf_all_web_antitamper_rules_test.go
@@ -0,0 +1,69 @@
+package waf
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceAllWebAntitamperRules_basic(t *testing.T) {
+	var (
+		dataSourceName = "data.huaweicloud_waf_all_web_antitamper_rules.test"
+		dc             = acceptance.InitDataSourceCheck(dataSourceName)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			// Prepare a WAF web tamper protection rule before test
+			acceptance.TestAccPrecheckWafInstance(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceAllWebAntitamperRules_basic,
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSourceName, "items.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "items.0.id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "items.0.policyid"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "items.0.url"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "items.0.timestamp"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "items.0.status"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "items.0.description"),
+
+					resource.TestCheckOutput("policyid_filter_is_useful", "true"),
+					resource.TestCheckOutput("eps_filter_is_useful", "true"),
+				),
+			},
+		},
+	})
+}
+
+const testAccDataSourceAllWebAntitamperRules_basic = `
+data "huaweicloud_waf_all_web_antitamper_rules" "test" {}
+
+locals {
+  policyid = data.huaweicloud_waf_all_web_antitamper_rules.test.items[0].policyid
+}
+
+data "huaweicloud_waf_all_web_antitamper_rules" "filter_by_policyid" {
+  policyids = local.policyid
+}
+
+output "policyid_filter_is_useful" {
+  value = length(data.huaweicloud_waf_all_web_antitamper_rules.filter_by_policyid.items) > 0 && alltrue(
+    [for v in data.huaweicloud_waf_all_web_antitamper_rules.filter_by_policyid.items[*].policyid : v == local.policyid]
+  )
+}
+
+data "huaweicloud_waf_all_web_antitamper_rules" "filter_by_eps" {
+  enterprise_project_id = "0"
+}
+
+output "eps_filter_is_useful" {
+  value = length(data.huaweicloud_waf_all_web_antitamper_rules.filter_by_eps.items) > 0
+}
+`
