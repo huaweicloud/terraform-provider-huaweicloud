2687c03246ac5cdff7d046fe73680181b03fcd07
jinyangyang222 <jinyangyang222@outlook.com>
2025-09-29 11:20:14 +0800

    feat(hss/hss_cicd_configuration): support `hss_cicd_configuration` resource

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2723,6 +2723,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_hss_container_kubernetes_sync_mccs":                 hss.ResourceContainerKubernetesSyncMccs(),
 			"huaweicloud_hss_container_kubernetes_cluster_daemonset":         hss.ResourceContainerKubernetesClusterDaemonset(),
 			"huaweicloud_hss_container_kubernetes_cluster_protection_enable": hss.ResourceContainerKubernetesClusterProtectionEnable(),
+			"huaweicloud_hss_cicd_configuration":                             hss.ResourceCiCdConfiguration(),
 			"huaweicloud_hss_honeypot_port_policy":                           hss.ResourceHoneypotPortPolicy(),
 			"huaweicloud_hss_host_protection":                                hss.ResourceHostProtection(),
 			"huaweicloud_hss_webtamper_protection":                           hss.ResourceWebTamperProtection(),
