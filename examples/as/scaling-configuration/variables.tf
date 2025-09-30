710ab40d3dd48b70546c33e0774b5f30872c2f3f
Lance52259 <Overlord52259@gmail.com>
2025-09-30 17:09:22 +0800

    docs(as): refactor the best practice of the scaling configuration

--- examples/as/scaling-configuration/variables.tf
+++ examples/as/scaling-configuration/variables.tf
@@ -1,6 +1,6 @@
 # Variables definitions for authorization
 variable "region_name" {
-  description = "The region where the AS configuration is located"
+  description = "The region where the scaling configuration is located"
   type        = string
 }
 
@@ -18,37 +18,37 @@ variable "secret_key" {
 
 # Variables definitions for resource/data source
 variable "availability_zone" {
-  description = "The availability zone to which the AS configuration belongs"
+  description = "The availability zone to which the scaling configuration belongs"
   type        = string
   default     = ""
 }
 
 variable "configuration_flavor_id" {
-  description = "The flavor ID of the AS configuration"
+  description = "The flavor ID of the scaling configuration"
   type        = string
   default     = ""
 }
 
 variable "configuration_flavor_performance_type" {
-  description = "The performance type of the AS configuration"
+  description = "The performance type of the scaling configuration"
   type        = string
   default     = "normal"
 }
 
 variable "configuration_flavor_cpu_core_count" {
-  description = "The CPU core count of the AS configuration"
+  description = "The CPU core count of the scaling configuration"
   type        = number
   default     = 2
 }
 
 variable "configuration_flavor_memory_size" {
-  description = "The memory size of the AS configuration"
+  description = "The memory size of the scaling configuration"
   type        = number
   default     = 4
 }
 
 variable "configuration_image_id" {
-  description = "The image ID of the AS configuration"
+  description = "The image ID of the scaling configuration"
   type        = string
   default     = ""
 }
@@ -82,23 +82,23 @@ variable "keypair_public_key" {
 }
 
 variable "configuration_name" {
-  description = "The name of the AS configuration"
+  description = "The name of the scaling configuration"
   type        = string
 }
 
 variable "configuration_metadata" {
-  description = "The metadata for the instance"
+  description = "The metadata for the scaling configuration instances"
   type        = map(string)
 }
 
 variable "configuration_user_data" {
-  description = "The user data script for instance initialization"
+  description = "The user data script for scaling configuration instances initialization"
   type        = string
 }
 
 variable "configuration_disks" {
-  description = "The disk configurations for the instance"
-  type = list(object({
+  description = "The disk configurations for the scaling configuration instances"
+  type        = list(object({
     size        = number
     volume_type = string
     disk_type   = string
@@ -107,19 +107,17 @@ variable "configuration_disks" {
   nullable = false
 }
 
-variable "configuration_public_ip" {
-  description = "The public IP configuration for the instance"
-
-  type = list(object({
-    eip = object({
-      ip_type = string
-      bandwidth = object({
-        size          = number
-        share_type    = string
-        charging_mode = string
-      })
+variable "configuration_public_eip_settings" {
+  description = "The public IP settings for the scaling configuration instances"
+  type        = list(object({
+    ip_type   = string
+    bandwidth = object({
+      size          = number
+      share_type    = string
+      charging_mode = string
     })
   }))
 
   nullable = false
+  default  = []
 }
