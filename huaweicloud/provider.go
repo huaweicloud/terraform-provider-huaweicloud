e4dbc8d8d6a94872894e540397c20f85ab54c0a8
allium <deerhang@163.com>
2025-09-30 15:38:41 +0800

    feat(hss): new resource to record vulnerability task user trace (#8010)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2752,6 +2752,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_hss_setting_two_factor_login_config":                hss.ResourceSettingTwoFactorLoginConfig(),
 			"huaweicloud_hss_switch_honeypot_port_policy":                    hss.ResourceSwitchHoneypotPortPolicy(),
 			"huaweicloud_hss_vulnerability_information_export":               hss.ResourceVulnerabilityInformationExport(),
+			"huaweicloud_hss_vulnerability_task_user_trace":                  hss.ResourceVulnerabilityTaskUserTrace(),
 			"huaweicloud_hss_file_download":                                  hss.ResourceFileDownload(),
 			"huaweicloud_hss_ignore_failed_pcc":                              hss.ResourceIgnoreFailedPCC(),
 			"huaweicloud_hss_antivirus_create_virus_scan_task":               hss.ResourceCreateVirusScanTask(),
