29ed4cf7424b654ded94264df720322b7ba90b68
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-10-13 19:51:13 +0800

    feat(kafka): add a one-time resource to delete topic messages

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2615,6 +2615,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_dms_kafka_smart_connect_task_action":         kafka.ResourceDmsKafkaSmartConnectTaskAction(),
 			"huaweicloud_dms_kafka_smart_connector_validate":          kafka.ResourceSmartConnectorValidate(),
 			"huaweicloud_dms_kafka_topic":                             kafka.ResourceDmsKafkaTopic(),
+			"huaweicloud_dms_kafka_topic_message_batch_delete":        kafka.ResourceTopicMessageBatchDelete(),
 			"huaweicloud_dms_kafka_topic_quota":                       kafka.ResourceTopicQuota(),
 			"huaweicloud_dms_kafka_user":                              kafka.ResourceDmsKafkaUser(),
 			"huaweicloud_dms_kafka_user_client_quota":                 kafka.ResourceDmsKafkaUserClientQuota(),
