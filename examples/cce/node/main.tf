9c0d9932ba412056bfabd00804e6aee93755fef2
Lance <Overlord52259@gmail.com>
2025-10-16 14:27:25 +0800

    docs(cce): add new best practice for node (#8053)
    * docs(cce): add new best practice for node
    
    * docs(cce): remove the CCE basic example

---
+++ examples/cce/node/main.tf
@@ -0,0 +1,77 @@
+data "huaweicloud_availability_zones" "test" {}
+
+resource "huaweicloud_vpc" "test" {
+  count = var.vpc_id == "" && var.subnet_id == "" ? 1 : 0
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+}
+
+resource "huaweicloud_vpc_subnet" "test" {
+  count = var.subnet_id == "" ? 1 : 0
+
+  vpc_id            = var.vpc_id != "" ? var.vpc_id : huaweicloud_vpc.test[0].id
+  name              = var.subnet_name
+  cidr              = var.subnet_cidr != "" ? var.subnet_cidr : cidrsubnet(huaweicloud_vpc.test[0].cidr, 4, 0)
+  gateway_ip        = var.subnet_gateway_ip != "" ? var.subnet_gateway_ip : var.subnet_cidr != "" ? cidrhost(var.subnet_cidr, 1) : cidrhost(cidrsubnet(huaweicloud_vpc.test[0].cidr, 4, 0), 1)
+  availability_zone = var.availability_zone != "" ? var.availability_zone : try(data.huaweicloud_availability_zones.test.names[0], null)
+}
+
+resource "huaweicloud_vpc_eip" "test" {
+  count = var.eip_address == "" ? 1 : 0
+
+  publicip {
+    type = var.eip_type
+  }
+
+  bandwidth {
+    name        = var.bandwidth_name
+    size        = var.bandwidth_size
+    share_type  = var.bandwidth_share_type
+    charge_mode = var.bandwidth_charge_mode
+  }
+}
+
+resource "huaweicloud_cce_cluster" "test" {
+  name                   = var.cluster_name
+  flavor_id              = var.cluster_flavor_id
+  cluster_version        = var.cluster_version
+  cluster_type           = var.cluster_type
+  container_network_type = var.container_network_type
+  vpc_id                 = var.vpc_id != "" ? var.vpc_id : huaweicloud_vpc.test[0].id
+  subnet_id              = var.subnet_id != "" ? var.subnet_id : huaweicloud_vpc_subnet.test[0].id
+  eip                    = var.eip_address != "" ? var.eip_address : huaweicloud_vpc_eip.test[0].address
+}
+
+data "huaweicloud_compute_flavors" "test" {
+  performance_type  = var.node_performance_type
+  cpu_core_count    = var.node_cpu_core_count
+  memory_size       = var.node_memory_size
+  availability_zone = try(data.huaweicloud_availability_zones.test.names[0], null)
+}
+
+resource "huaweicloud_kps_keypair" "test" {
+  name = var.keypair_name
+}
+
+resource "huaweicloud_cce_node" "test" {
+  cluster_id        = huaweicloud_cce_cluster.test.id
+  name              = var.node_name
+  flavor_id         = try(data.huaweicloud_compute_flavors.test.flavors[0].id, null)
+  availability_zone = try(data.huaweicloud_availability_zones.test.names[0], null)
+  key_pair          = huaweicloud_kps_keypair.test.name
+
+  root_volume {
+    volumetype = var.root_volume_type
+    size       = var.root_volume_size
+  }
+
+  dynamic "data_volumes" {
+    for_each = var.data_volumes_configuration
+
+    content {
+      volumetype = data_volumes.value.volumetype
+      size       = data_volumes.value.size
+    }
+  }
+}
