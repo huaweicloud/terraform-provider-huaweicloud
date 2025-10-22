8641e61a5bd7df4f395eb41afbf524982b773c86
jinyangyang222 <jinyangyang222@outlook.com>
2025-10-22 17:19:57 +0800

    feat(evs): support `evs_recycle_bin_volume_delete` resource (#8120)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2723,6 +2723,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_evs_unsubscribe_prepaid_volume": evs.ResourceUnsubscribePrepaidVolume(),
 			"huaweicloud_evs_volumes_batch_expand":       evs.ResourceVolumesBatchExpand(),
 			"huaweicloud_evs_recycle_bin_policy":         evs.ResourceRecycleBinPolicy(),
+			"huaweicloud_evs_recycle_bin_volume_delete":  evs.ResourceRecycleBinVolumeDelete(),
 
 			"huaweicloud_fgs_application":                    fgs.ResourceApplication(),
 			"huaweicloud_fgs_async_invoke_configuration":     fgs.ResourceAsyncInvokeConfiguration(),
