0fbca3e72a2b19c7a5626b5447cbc3c154903184
wuzhuanhong <wuzhuanhong1@outlook.com>
2025-10-24 14:27:37 +0800

    refactor(examples/cce): adjust the best practice for pvc with existing obs

---
+++ examples/cce/kubenetes/pvc-with-existing-obs-bucket/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.9.0"
+
+  required_providers {
+    huaweicloud = {
+      source  = "huaweicloud/huaweicloud"
+      version = ">= 1.57.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 1.11.4"
+    }
+  }
+}
+
+provider "huaweicloud" {
+  region     = var.region_name
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+provider "kubernetes" {
+  host                   = "https://%{if var.eip_address != ""}${var.eip_address}:5443%{else}${huaweicloud_vpc_eip.test[0].address}:5443%{endif}"
+  cluster_ca_certificate = base64decode(huaweicloud_cce_cluster.test.certificate_clusters[0].certificate_authority_data)
+  client_certificate     = base64decode(huaweicloud_cce_cluster.test.certificate_users[0].client_certificate_data)
+  client_key             = base64decode(huaweicloud_cce_cluster.test.certificate_users[0].client_key_data)
+}
