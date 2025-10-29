3ebf7615024c20205569bf3c1b6e139acea15544
ruwenqiang123 <ruwenqiang123@outlook.com>
2025-10-29 17:18:23 +0800

    feat(dew/kms): add new resource to create EC datakey pair (#8165)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2954,6 +2954,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_kms_alias":                     dew.ResourceKmsAlias(),
 			"huaweicloud_kms_alias_associate":           dew.ResourceKmsAliasAssociate(),
 			"huaweicloud_kms_dedicated_keystore":        dew.ResourceKmsDedicatedKeystore(),
+			"huaweicloud_kms_ec_datakey_pair":           dew.ResourceEcDatakeyPair(),
 			"huaweicloud_kms_key_material":              dew.ResourceKmsKeyMaterial(),
 			"huaweicloud_kms_encrypt_datakey":           dew.ResourceKmsEncryptDatakey(),
 			"huaweicloud_kms_decrypt_datakey":           dew.ResourceKmsDecryptDatakey(),
---
+++ huaweicloud/services/acceptance/dew/resource_huaweicloud_kms_ec_datakey_pair_test.go
@@ -0,0 +1,45 @@
+package dew
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccEcDatakeyPair_basic(t *testing.T) {
+	rName := "huaweicloud_kms_ec_datakey_pair.test"
+	// lintignore:AT001
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			// Please prepare a KMS Key ID
+			acceptance.TestAccPreCheckKmsKeyID(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEcDatakeyPair_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(rName, "public_key"),
+					resource.TestCheckResourceAttrSet(rName, "private_key_cipher_text"),
+					resource.TestCheckResourceAttrSet(rName, "private_key_plain_text"),
+				),
+			},
+		},
+	})
+}
+
+func testAccEcDatakeyPair_basic() string {
+	return fmt.Sprintf(`
+resource "huaweicloud_kms_ec_datakey_pair" "test" {
+  key_id                        = "%s"
+  key_spec                      = "ECC_NIST_P521"
+  with_plain_text               = true
+  additional_authenticated_data = "terraform test"
+  sequence                      = "919c82d4-8046-4722-9094-35c3c6524cff"
+}
+`, acceptance.HW_KMS_KEY_ID)
+}
