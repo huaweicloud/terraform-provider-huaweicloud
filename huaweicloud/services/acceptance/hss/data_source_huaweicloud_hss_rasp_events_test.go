b607bc262d7028a24e0d2cad7922f808938b1bb6
ruwenqiang123 <ruwenqiang123@outlook.com>
2025-11-07 16:12:03 +0800

    feat(hss): add data source to get list of application protection events

---
+++ huaweicloud/services/acceptance/hss/data_source_huaweicloud_hss_rasp_events_test.go
@@ -0,0 +1,51 @@
+package hss
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceRaspEvents_basic(t *testing.T) {
+	var (
+		dataSource = "data.huaweicloud_hss_rasp_events.test"
+		dc         = acceptance.InitDataSourceCheck(dataSource)
+		startTime  = time.Now().Add(-8*time.Hour).UnixNano() / int64(time.Millisecond)
+		endTime    = time.Now().UnixNano() / int64(time.Millisecond)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			// Because there is no available data for testing, the test case is only
+			// used to verify that the API can be invoked.
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckHSSHostProtectionHostId(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+
+			{
+				Config: testAccDataSourceRaspEvents_basic(startTime, endTime),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "data_list.#"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceRaspEvents_basic(startTime, endTime int64) string {
+	return fmt.Sprintf(`
+data "huaweicloud_hss_rasp_events" "test" {
+  host_id               = "%[1]s"
+  start_time            = %[2]d
+  end_time              = %[3]d
+  enterprise_project_id = "0"
+}
+`, acceptance.HW_HSS_HOST_PROTECTION_HOST_ID, startTime, endTime)
+}
