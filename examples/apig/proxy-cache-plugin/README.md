e77c856a4508fb2f963b77e84178c7cefacf1a18
Lance <Overlord52259@gmail.com>
2025-11-13 15:17:32 +0800

    docs(apig): refactor the best practice for proxy cache plugin (#8293)

--- examples/apig/proxy-cache-plugin/README.md
+++ examples/apig/proxy-cache-plugin/README.md
@@ -23,23 +23,30 @@ The following variables need to be configured:
 
 #### Required Variables
 
-* `vpc_name` - The name of the VPC
-* `subnet_name` - The name of the subnet
 * `security_group_name` - The name of the security group
 * `instance_name` - The name of the APIG instance
 * `plugin_name` - The name of the proxy cache plugin
 
 #### Optional Variables
 
-* `vpc_cidr` - The CIDR block of the VPC (default: "192.168.0.0/16")
-* `subnet_cidr` - The CIDR block of the subnet (default: "")
-* `subnet_gateway_ip` - The gateway IP address of the subnet (default: "")
 * `availability_zones` - The availability zones to which the instance belongs (default: [])  
   If not specified, will be automatically allocated based on the number of availability_zones_count
 * `availability_zones_count` - The number of availability zones to which the instance belongs (default: 1)
+* `vpc_id` - The ID of the VPC (required if `vpc_name` is not provided)
+* `subnet_id` - The ID of the subnet (required if `subnet_name` is not provided)
+* `vpc_name` - The name of the VPC (required if `vpc_id` is not provided)
+* `vpc_cidr` - The CIDR block of the VPC (default: "192.168.0.0/16")
+* `subnet_name` - The name of the subnet (required if `subnet_id` is not provided)
+* `subnet_cidr` - The CIDR block of the subnet (default: "")
+* `subnet_gateway_ip` - The gateway IP address of the subnet (default: "")
 * `instance_edition` - The edition of the APIG instance (default: "BASIC")
 * `enterprise_project_id` - The ID of the enterprise project, required for enterprise users (default: null)
 * `plugin_description` - The description of the proxy cache plugin (default: null)
+
+#### Notes
+
+* Either `vpc_id` or `vpc_name` must be provided, but not both
+* Either `subnet_id` or `subnet_name` must be provided, but not both
 
 ## Usage
 
@@ -50,6 +57,16 @@ The following variables need to be configured:
   ```hcl
   vpc_name            = "your_vpc_name"
   subnet_name         = "your_subnet_name"
+  security_group_name = "your_security_group_name"
+  instance_name       = "your_apig_instance_name"
+  plugin_name         = "your_plugin_name"
+  ```
+
+  Or use existing VPC and subnet by providing their IDs:
+
+  ```hcl
+  vpc_id              = "your_existing_vpc_id"
+  subnet_id           = "your_existing_subnet_id"
   security_group_name = "your_security_group_name"
   instance_name       = "your_apig_instance_name"
   plugin_name         = "your_plugin_name"
@@ -100,12 +117,14 @@ The proxy cache plugin is configured with the following settings:
 
 * Make sure to keep your credentials secure and never commit them to version control
 * All resources will be created in the specified region
-* The APIG instance is created with BASIC edition
-* The APIG instance will be deployed in the first available zone
+* The APIG instance is created with BASIC edition by default
+* The APIG instance will be deployed in the first available zone if `availability_zones` is not specified
+* You can use existing VPC and subnet by providing `vpc_id` and `subnet_id` instead of creating new ones
+* When using existing VPC/subnet, make sure they exist before running this example
 
 ## Requirements
 
 | Name        | Version   |
 |-------------|-----------|
-| terraform   | >= 1.1.0 |
+| terraform   | >= 1.9.0 |
 | huaweicloud | >= 1.49.0 |
--- examples/apig/proxy-cache-plugin/main.tf
+++ examples/apig/proxy-cache-plugin/main.tf
@@ -1,17 +1,22 @@
 data "huaweicloud_availability_zones" "test" {
-  count = length(var.availability_zones) == 0 ? 1 : 0
+  count = length(var.availability_zones) < var.availability_zones_count ? 1 : 0
 }
 
 resource "huaweicloud_vpc" "test" {
+  count = var.vpc_id == "" && var.subnet_id == "" ? 1 : 0
+
   name = var.vpc_name
   cidr = var.vpc_cidr
 }
 
 resource "huaweicloud_vpc_subnet" "test" {
-  vpc_id     = huaweicloud_vpc.test.id
-  name       = var.subnet_name
-  cidr       = var.subnet_cidr == "" ? cidrsubnet(huaweicloud_vpc.test.cidr, 8, 0) : var.subnet_cidr
-  gateway_ip = var.subnet_gateway_ip == "" ? cidrhost(cidrsubnet(huaweicloud_vpc.test.cidr, 8, 0), 1) : var.subnet_gateway_ip
+  count = var.subnet_id == "" ? 1 : 0
+
+  vpc_id            = var.vpc_id != "" ? var.vpc_id : huaweicloud_vpc.test[0].id
+  name              = var.subnet_name
+  cidr              = var.subnet_cidr != "" ? var.subnet_cidr : cidrsubnet(huaweicloud_vpc.test[0].cidr, 4, 0)
+  gateway_ip        = var.subnet_gateway_ip != "" ? var.subnet_gateway_ip : var.subnet_cidr != "" ? cidrhost(var.subnet_cidr, 1) : cidrhost(cidrsubnet(huaweicloud_vpc.test[0].cidr, 4, 0), 1)
+  availability_zone = length(var.availability_zones) > 0 ? try(var.availability_zones[0], null) : try(data.huaweicloud_availability_zones.test[0].names[0], null)
 }
 
 resource "huaweicloud_networking_secgroup" "test" {
@@ -22,11 +27,17 @@ resource "huaweicloud_networking_secgroup" "test" {
 resource "huaweicloud_apig_instance" "test" {
   name                  = var.instance_name
   edition               = var.instance_edition
-  vpc_id                = huaweicloud_vpc.test.id
-  subnet_id             = huaweicloud_vpc_subnet.test.id
+  vpc_id                = var.vpc_id != "" ? var.vpc_id : huaweicloud_vpc.test[0].id
+  subnet_id             = var.subnet_id != "" ? var.subnet_id : huaweicloud_vpc_subnet.test[0].id
   security_group_id     = huaweicloud_networking_secgroup.test.id
-  availability_zones    = length(var.availability_zones) == 0 ? try(slice(data.huaweicloud_availability_zones.test[0].names, 0, var.availability_zones_count), null) : var.availability_zones
+  availability_zones    = length(var.availability_zones) < var.availability_zones_count ? concat(var.availability_zones, try(slice([for v in try(data.huaweicloud_availability_zones.test[0].names, []) : v if !contains(var.availability_zones, v)], 0, var.availability_zones_count - length(var.availability_zones)), [])) : var.availability_zones
   enterprise_project_id = var.enterprise_project_id
+
+  lifecycle {
+    ignore_changes = [
+      availability_zones
+    ]
+  }
 }
 
 resource "huaweicloud_apig_plugin" "test" {
@@ -36,12 +47,12 @@ resource "huaweicloud_apig_plugin" "test" {
   description = var.plugin_description
 
   content = jsonencode({
-    cache_key = {
+    cache_key                 = {
       system_params = [],
-      parameters = [
+      parameters    = [
         "custom_param"
       ],
-      headers = []
+      headers       = []
     },
     cache_http_status_and_ttl = [
       {
@@ -49,14 +60,14 @@ resource "huaweicloud_apig_plugin" "test" {
           202,
           203
         ],
-        ttl = 5
+        ttl         = 5
       }
     ],
-    client_cache_control = {
+    client_cache_control      = {
       mode  = "off",
       datas = []
     },
-    cacheable_headers = [
+    cacheable_headers         = [
       "X-Custom-Header"
     ]
   })
