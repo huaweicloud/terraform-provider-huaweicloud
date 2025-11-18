bb8946d1cf164fd2d115d8513df6b6cc978aa284
ruwenqiang123 <ruwenqiang123@outlook.com>
2025-11-18 11:20:09 +0800

    feat(hss): add data source to get list of vulnerability cve information

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1397,6 +1397,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_hss_host_groups":                                hss.DataSourceHostGroups(),
 			"huaweicloud_hss_host_vulnerabilities":                       hss.DataSourceHssHostVulnerabilities(),
 			"huaweicloud_hss_hosts":                                      hss.DataSourceHosts(),
+			"huaweicloud_hss_image_vulnerability_cves":                   hss.DataSourceImageVulnerabilityCves(),
 			"huaweicloud_hss_kubernetes_services":                        hss.DataSourceKubernetesServices(),
 			"huaweicloud_hss_kubernetes_pod_detail":                      hss.DataSourceKubernetesPodDetail(),
 			"huaweicloud_hss_kubernetes_jobs":                            hss.DataSourceKubernetesJobs(),
