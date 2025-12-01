74fe1257ce5d1e2cf69c8579fba085f9169e4ca7
jinyangyang222 <jinyangyang222@outlook.com>
2025-12-01 15:43:36 +0800

    feat(hss): support `hss_security_check_config` data source (#8471)

---
+++ huaweicloud/services/acceptance/hss/data_source_huaweicloud_hss_security_check_config_test.go
@@ -0,0 +1,39 @@
+package hss
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceSecurityCheckConfig_basic(t *testing.T) {
+	var (
+		dataSource = "data.huaweicloud_hss_security_check_config.test"
+		dc         = acceptance.InitDataSourceCheck(dataSource)
+	)
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceSecurityCheckConfig_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "status"),
+				),
+			},
+		},
+	})
+}
+
+func testDataSourceSecurityCheckConfig_basic() string {
+	return `
+data "huaweicloud_hss_security_check_config" "test" {
+  enterprise_project_id = "0"
+}
+`
+}
