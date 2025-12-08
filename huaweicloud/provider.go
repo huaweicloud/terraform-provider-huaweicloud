9812b85b60ac6814816424065f043941af2d2c00
ruwenqiang123 <ruwenqiang123@outlook.com>
2025-12-08 12:07:08 +0800

    feat(hss): add new data source to get the list of image scan tasks (#8557)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1446,6 +1446,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_hss_host_vulnerabilities":                       hss.DataSourceHssHostVulnerabilities(),
 			"huaweicloud_hss_hosts":                                      hss.DataSourceHosts(),
 			"huaweicloud_hss_hosts_risk":                                 hss.DataSourceHostsRisk(),
+			"huaweicloud_hss_image_tasks":                                hss.DataSourceImageTasks(),
 			"huaweicloud_hss_image_vulnerability_cves":                   hss.DataSourceImageVulnerabilityCves(),
 			"huaweicloud_hss_image_whitelists":                           hss.DataSourceImageWhitelists(),
 			"huaweicloud_hss_kubernetes_services":                        hss.DataSourceKubernetesServices(),
