ae6f4b7f3aafb84cf61f83231c81ff00f5d05911
ruwenqiang123 <ruwenqiang123@outlook.com>
2025-12-22 13:25:21 +0800

    feat(secmaster): the dataspace resource supports new APIs (#8666)

--- huaweicloud/services/acceptance/secmaster/resource_huaweicloud_secmaster_dataspace_test.go
+++ huaweicloud/services/acceptance/secmaster/resource_huaweicloud_secmaster_dataspace_test.go
@@ -5,12 +5,34 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/secmaster"
 )
 
+func getResourceDataspaceFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
+	client, err := cfg.NewServiceClient("secmaster", acceptance.HW_REGION_NAME)
+	if err != nil {
+		return nil, fmt.Errorf("error creating SecMaster client: %s", err)
+	}
+
+	return secmaster.GetDataspaceInfo(client, state.Primary.Attributes["workspace_id"], state.Primary.ID)
+}
+
 func TestAccDataspace_basic(t *testing.T) {
-	name := acceptance.RandomAccResourceName()
+	var (
+		rName = "huaweicloud_secmaster_dataspace.test"
+		name  = acceptance.RandomAccResourceNameWithDash()
+
+		object interface{}
+		rc     = acceptance.InitResourceCheck(
+			rName,
+			&object,
+			getResourceDataspaceFunc,
+		)
+	)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
@@ -18,10 +40,34 @@ func TestAccDataspace_basic(t *testing.T) {
 			acceptance.TestAccPreCheckSecMasterWorkspaceID(t)
 		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
-		CheckDestroy:      nil,
+		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataspace_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(rName, "workspace_id", acceptance.HW_SECMASTER_WORKSPACE_ID),
+					resource.TestCheckResourceAttr(rName, "dataspace_name", name),
+					resource.TestCheckResourceAttr(rName, "description", "test description"),
+					resource.TestCheckResourceAttrSet(rName, "dataspace_type"),
+					resource.TestCheckResourceAttrSet(rName, "create_by"),
+					resource.TestCheckResourceAttrSet(rName, "create_time"),
+				),
+			},
+			{
+				Config: testAccDataspace_update(name),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(rName, "description", "test terraform"),
+					resource.TestCheckResourceAttrSet(rName, "update_by"),
+					resource.TestCheckResourceAttrSet(rName, "update_time"),
+				),
+			},
+			{
+				ResourceName:      rName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateIdFunc: testAccDataspaceImportStateFunc(rName),
 			},
 		},
 	})
@@ -35,4 +81,34 @@ resource "huaweicloud_secmaster_dataspace" "test" {
   description    = "test description"
 }
 `, acceptance.HW_SECMASTER_WORKSPACE_ID, name)
+}
+
+func testAccDataspace_update(name string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_secmaster_dataspace" "test" {
+  workspace_id   = "%[1]s"
+  dataspace_name = "%[2]s"
+  description    = "test terraform"
+}
+`, acceptance.HW_SECMASTER_WORKSPACE_ID, name)
+}
+
+func testAccDataspaceImportStateFunc(rName string) resource.ImportStateIdFunc {
+	return func(s *terraform.State) (string, error) {
+		var workspaceId, dataspaceId string
+		rs, ok := s.RootModule().Resources[rName]
+		if !ok {
+			return "", fmt.Errorf("resource (%s) not found: %s", rName, rs)
+		}
+
+		workspaceId = rs.Primary.Attributes["workspace_id"]
+		dataspaceId = rs.Primary.ID
+
+		if workspaceId == "" || dataspaceId == "" {
+			return "", fmt.Errorf("invalid format specified for import ID, want '<workspace_id>/<id>', but got '%s/%s'",
+				workspaceId, dataspaceId)
+		}
+
+		return fmt.Sprintf("%s/%s", workspaceId, dataspaceId), nil
+	}
 }
--- huaweicloud/services/secmaster/resource_huaweicloud_secmaster_dataspace.go
+++ huaweicloud/services/secmaster/resource_huaweicloud_secmaster_dataspace.go
@@ -2,27 +2,37 @@ package secmaster
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 
 	"github.com/chnsz/golangsdk"
 
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
-var dataSpaceNonUpdatableParams = []string{"workspace_id", "dataspace_name", "description"}
+var dataSpaceNonUpdatableParams = []string{"workspace_id", "dataspace_name"}
 
 // @API SecMaster POST /v1/{project_id}/workspaces/{workspace_id}/siem/dataspaces
+// @API SecMaster GET /v1/{project_id}/workspaces/{workspace_id}/siem/dataspaces/{dataspace_id}
+// @API SecMaster PUT /v1/{project_id}/workspaces/{workspace_id}/siem/dataspaces/{dataspace_id}
+// @API SecMaster DELETE /v1/{project_id}/workspaces/{workspace_id}/siem/dataspaces/{dataspace_id}
 func ResourceDataspace() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceDataspaceCreate,
 		ReadContext:   resourceDataspaceRead,
 		UpdateContext: resourceDataspaceUpdate,
 		DeleteContext: resourceDataspaceDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceDataspaceImportState,
+		},
 
 		CustomizeDiff: config.FlexibleForceNew(dataSpaceNonUpdatableParams),
 
@@ -51,6 +61,34 @@ func ResourceDataspace() *schema.Resource {
 				ValidateFunc: validation.StringInSlice([]string{"true", "false"}, false),
 				Description:  utils.SchemaDesc("", utils.SchemaDescInput{Internal: true}),
 			},
+			"dataspace_type": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"domain_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"project_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"create_by": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"update_by": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"create_time": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"update_time": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
 		},
 	}
 }
@@ -64,7 +102,7 @@ func buildDataspaceBodyParams(d *schema.ResourceData) map[string]interface{} {
 	return bodyParams
 }
 
-func resourceDataspaceCreate(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceDataspaceCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var (
 		cfg           = meta.(*config.Config)
 		region        = cfg.GetRegion(d)
@@ -84,7 +122,7 @@ func resourceDataspaceCreate(_ context.Context, d *schema.ResourceData, meta int
 	createOpt := golangsdk.RequestOpts{
 		KeepResponseBody: true,
 		MoreHeaders:      map[string]string{"Content-Type": "application/json"},
-		JSONBody:         utils.RemoveNil(buildDataspaceBodyParams(d)),
+		JSONBody:         buildDataspaceBodyParams(d),
 	}
 
 	createResp, err := client.Request("POST", createPath, &createOpt)
@@ -104,17 +142,146 @@ func resourceDataspaceCreate(_ context.Context, d *schema.ResourceData, meta int
 
 	d.SetId(dataspaceId)
 
+	return resourceDataspaceRead(ctx, d, meta)
+}
+
+func resourceDataspaceRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg         = meta.(*config.Config)
+		region      = cfg.GetRegion(d)
+		workspaceId = d.Get("workspace_id").(string)
+	)
+
+	client, err := cfg.NewServiceClient("secmaster", region)
+	if err != nil {
+		return diag.Errorf("error creating SecMaster client: %s", err)
+	}
+
+	dataspace, err := GetDataspaceInfo(client, workspaceId, d.Id())
+	if err != nil {
+		// When the dataspace does not exist, the response HTTP status code of the query API is 404
+		return common.CheckDeletedDiag(d, err, "error retrieving dataspace")
+	}
+
+	mErr := multierror.Append(nil,
+		d.Set("region", region),
+		d.Set("dataspace_name", utils.PathSearch("dataspace_name", dataspace, nil)),
+		d.Set("description", utils.PathSearch("description", dataspace, nil)),
+		d.Set("dataspace_type", utils.PathSearch("dataspace_type", dataspace, nil)),
+		d.Set("domain_id", utils.PathSearch("domain_id", dataspace, nil)),
+		d.Set("project_id", utils.PathSearch("project_id", dataspace, nil)),
+		d.Set("create_by", utils.PathSearch("create_by", dataspace, nil)),
+		d.Set("update_by", utils.PathSearch("update_by", dataspace, nil)),
+		d.Set("create_time", utils.PathSearch("create_time", dataspace, nil)),
+		d.Set("update_time", utils.PathSearch("update_time", dataspace, nil)),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func GetDataspaceInfo(client *golangsdk.ServiceClient, workspaceId, dataspaceId string) (interface{}, error) {
+	httpUrl := "v1/{project_id}/workspaces/{workspace_id}/siem/dataspaces/{dataspace_id}"
+	getPath := client.Endpoint + httpUrl
+	getPath = strings.ReplaceAll(getPath, "{project_id}", client.ProjectID)
+	getPath = strings.ReplaceAll(getPath, "{workspace_id}", workspaceId)
+	getPath = strings.ReplaceAll(getPath, "{dataspace_id}", dataspaceId)
+	getOpts := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+
+	resp, err := client.Request("GET", getPath, &getOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	return utils.FlattenResponse(resp)
+}
+
+func buildUpdateDataspaceBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"description": d.Get("description"),
+	}
+
+	return bodyParams
+}
+
+func resourceDataspaceUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg         = meta.(*config.Config)
+		region      = cfg.GetRegion(d)
+		workspaceId = d.Get("workspace_id").(string)
+		httpUrl     = "v1/{project_id}/workspaces/{workspace_id}/siem/dataspaces/{dataspace_id}"
+	)
+
+	client, err := cfg.NewServiceClient("secmaster", region)
+	if err != nil {
+		return diag.Errorf("error creating SecMaster client: %s", err)
+	}
+
+	updatePath := client.Endpoint + httpUrl
+	updatePath = strings.ReplaceAll(updatePath, "{project_id}", client.ProjectID)
+	updatePath = strings.ReplaceAll(updatePath, "{workspace_id}", workspaceId)
+	updatePath = strings.ReplaceAll(updatePath, "{dataspace_id}", d.Id())
+
+	if d.HasChange("description") {
+		updateOpt := golangsdk.RequestOpts{
+			KeepResponseBody: true,
+			JSONBody:         buildUpdateDataspaceBodyParams(d),
+		}
+
+		_, err = client.Request("PUT", updatePath, &updateOpt)
+		if err != nil {
+			return diag.Errorf("error updating dataspace: %s", err)
+		}
+	}
+
+	return resourceDataspaceRead(ctx, d, meta)
+}
+
+func resourceDataspaceDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg         = meta.(*config.Config)
+		region      = cfg.GetRegion(d)
+		workspaceId = d.Get("workspace_id").(string)
+		httpUrl     = "v1/{project_id}/workspaces/{workspace_id}/siem/dataspaces/{dataspace_id}"
+	)
+
+	client, err := cfg.NewServiceClient("secmaster", region)
+	if err != nil {
+		return diag.Errorf("error creating SecMaster client: %s", err)
+	}
+
+	deletePath := client.Endpoint + httpUrl
+	deletePath = strings.ReplaceAll(deletePath, "{project_id}", client.ProjectID)
+	deletePath = strings.ReplaceAll(deletePath, "{workspace_id}", workspaceId)
+	deletePath = strings.ReplaceAll(deletePath, "{dataspace_id}", d.Id())
+	deleteOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+
+	_, err = client.Request("DELETE", deletePath, &deleteOpt)
+	if err != nil {
+		// If the dataspace does not exist, the response HTTP status code of the deletion API is 404.
+		return common.CheckDeletedDiag(d, err, "error deleting dataspace")
+	}
+
 	return nil
 }
 
-func resourceDataspaceRead(_ context.Context, _ *schema.ResourceData, _ interface{}) diag.Diagnostics {
-	return nil
-}
+func resourceDataspaceImportState(_ context.Context, d *schema.ResourceData, _ interface{}) ([]*schema.ResourceData,
+	error) {
+	importedId := d.Id()
+	parts := strings.Split(importedId, "/")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid format specified for import ID, want '<workspace_id>/<id>', but got '%s'",
+			importedId)
+	}
 
-func resourceDataspaceUpdate(_ context.Context, _ *schema.ResourceData, _ interface{}) diag.Diagnostics {
-	return nil
-}
+	d.SetId(parts[1])
 
-func resourceDataspaceDelete(_ context.Context, _ *schema.ResourceData, _ interface{}) diag.Diagnostics {
-	return nil
+	mErr := multierror.Append(nil,
+		d.Set("workspace_id", parts[0]),
+	)
+
+	return []*schema.ResourceData{d}, mErr.ErrorOrNil()
 }
