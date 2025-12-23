c210d08d62abfb827783f70e78414bbba2e74049
greyworld <huawei@ecs-yanghongyu>
2025-12-23 20:17:53 +0800

    fix(nat): fix the best-practice coding standards for DNAT basic

--- examples/nat/dnat-basic/main.tf
+++ examples/nat/dnat-basic/main.tf
@@ -1,120 +1,104 @@
-data "huaweicloud_availability_zones" "newAZ_Example" {}
-
-data "huaweicloud_images_image" "newIMS_Example" {
-  name        = var.ims_name
-  visibility  = "public"
-  most_recent = true
-}
-
-resource "random_password" "password" {
-  length           = 16
-  special          = true
-  override_special = "!@$%^-_=+[{}]:,./?~#*"
-  min_numeric      = 1
-  min_lower        = 1
-  min_special      = 1
-}
-
-resource "huaweicloud_compute_instance" "newCompute_Example" {
-  name              = var.ecs_name
-  image_id          = data.huaweicloud_images_image.newIMS_Example.id
-  flavor_id         = "s6.small.1"
-  security_groups   = [huaweicloud_networking_secgroup.newSecgroup_Example.name]
-  admin_pass        = random_password.password.result
-  availability_zone = data.huaweicloud_availability_zones.newAZ_Example.names[0]
-
-  system_disk_type = "SSD"
-  system_disk_size = 40
-
-  network {
-    uuid = huaweicloud_vpc_subnet.newSubnet_Example.id
-  }
-}
-
-resource "huaweicloud_vpc_eip" "newEIP_Example" {
-  publicip {
-    type = "5_bgp"
-  }
-  bandwidth {
-    name        = var.bandwidth_name
-    size        = 5
-    share_type  = "PER"
-    charge_mode = "traffic"
-  }
-}
-
-resource "huaweicloud_vpc" "newVPC_Example" {
+resource "huaweicloud_vpc" "test" {
   name = var.vpc_name
   cidr = var.vpc_cidr
 }
 
-resource "huaweicloud_vpc_subnet" "newSubnet_Example" {
-  name          = var.subnet_name
-  cidr          = var.subnet_cidr
-  gateway_ip    = var.subnet_gateway_ip
-  vpc_id        = huaweicloud_vpc.newVPC_Example.id
-  primary_dns   = "100.125.129.250"
-  secondary_dns = "100.125.1.250"
+resource "huaweicloud_vpc_subnet" "test" {
+  vpc_id     = huaweicloud_vpc.test.id
+  name       = var.subnet_name
+  cidr       = var.subnet_cidr != "" ? var.subnet_cidr : cidrsubnet(huaweicloud_vpc.test.cidr, 8, 0)
+  gateway_ip = var.subnet_gateway_ip != "" ? var.subnet_gateway_ip : cidrhost(cidrsubnet(huaweicloud_vpc.test.cidr, 8, 0), 1)
 }
 
-resource "huaweicloud_networking_secgroup" "newSecgroup_Example" {
-  name        = var.secgroup_name
-  description = "This is a security group"
+# Create NAT Gateway And DNAT Rule
+resource "huaweicloud_nat_gateway" "test" {
+  name        = var.nat_gateway_name
+  description = var.nat_gateway_description
+  spec        = var.nat_gateway_spec
+  vpc_id      = huaweicloud_vpc.test.id
+  subnet_id   = huaweicloud_vpc_subnet.test.id
 }
 
-resource "huaweicloud_networking_secgroup_rule" "newSecgroupRule_Example" {
-  count = length(var.security_group_rule)
-
-  direction         = lookup(var.security_group_rule[count.index], "direction", null)
-  ethertype         = lookup(var.security_group_rule[count.index], "ethertype", null)
-  protocol          = lookup(var.security_group_rule[count.index], "protocol", null)
-  port_range_min    = lookup(var.security_group_rule[count.index], "port_range_min", null)
-  port_range_max    = lookup(var.security_group_rule[count.index], "port_range_max", null)
-  remote_ip_prefix  = lookup(var.security_group_rule[count.index], "remote_ip_prefix", null)
-  security_group_id = huaweicloud_networking_secgroup.newSecgroup_Example.id
-}
-
-resource "huaweicloud_nat_gateway" "newNet_gateway_Example" {
-  name                = var.net_gateway_name
-  description         = "example for net test"
-  spec                = "1"
-  router_id           = huaweicloud_vpc.newVPC_Example.id
-  internal_network_id = huaweicloud_vpc_subnet.newSubnet_Example.id
-}
-
-resource "huaweicloud_nat_snat_rule" "newSNATRule_Example" {
-  nat_gateway_id = huaweicloud_nat_gateway.newNet_gateway_Example.id
-  network_id     = huaweicloud_vpc_subnet.newSubnet_Example.id
-  floating_ip_id = huaweicloud_vpc_eip.newEIP_Example.id
-}
-
-resource "huaweicloud_nat_dnat_rule" "newDNATRule_Example" {
-  count = length(var.example_dnat_rule)
-
-  floating_ip_id = huaweicloud_vpc_eip.newEIP_Example.id
-  nat_gateway_id = huaweicloud_nat_gateway.newNet_gateway_Example.id
-  port_id        = huaweicloud_compute_instance.newCompute_Example.network[0].port
-
-  internal_service_port = lookup(var.example_dnat_rule[count.index], "internal_service_port", null)
-  protocol              = lookup(var.example_dnat_rule[count.index], "protocol", null)
-  external_service_port = lookup(var.example_dnat_rule[count.index], "external_service_port", null)
-}
-
-resource "null_resource" "provision" {
-  depends_on = [huaweicloud_nat_snat_rule.newSNATRule_Example, huaweicloud_nat_dnat_rule.newDNATRule_Example]
-
-  provisioner "remote-exec" {
-    connection {
-      user     = "root"
-      password = random_password.password.result
-      host     = huaweicloud_vpc_eip.newEIP_Example.address
-      port     = var.ecs_ssh_port
-    }
-    inline = [
-      "yum -y install nginx",
-      "systemctl enable nginx",
-      "systemctl start nginx",
-      "systemctl status nginx",
-    ]
+resource "huaweicloud_vpc_eip" "test" {
+  publicip {
+    type = "5_bgp"
   }
+
+  bandwidth {
+    name        = var.eip_bandwidth_name
+    size        = var.eip_bandwidth_size
+    share_type  = var.eip_bandwidth_share_type
+    charge_mode = var.eip_bandwidth_charge_mode
+  }
+}
+
+resource "huaweicloud_nat_dnat_rule" "test" {
+  nat_gateway_id        = huaweicloud_nat_gateway.test.id
+  floating_ip_id        = huaweicloud_vpc_eip.test.id
+  port_id               = try(huaweicloud_compute_instance.test.network[0].port, null)
+  protocol              = var.frontend_protocol
+  internal_service_port = var.backend_port
+  external_service_port = var.frontend_port
+}
+
+# Create ECS Instance
+data "huaweicloud_availability_zones" "test" {}
+
+data "huaweicloud_compute_flavors" "test" {
+  availability_zone = try(data.huaweicloud_availability_zones.test.names[0], null)
+  performance_type  = var.ecs_flavor_performance_type
+  cpu_core_count    = var.ecs_flavor_cpu_core_count
+  memory_size       = var.ecs_flavor_memory_size
+}
+
+data "huaweicloud_images_images" "test" {
+  flavor_id  = var.ecs_flavor_id != "" ? var.ecs_flavor_id : try(data.huaweicloud_compute_flavors.test.flavors[0].id, null)
+  visibility = var.ecs_image_visibility
+  os         = var.ecs_image_os
+}
+
+resource "huaweicloud_networking_secgroup" "test" {
+  name                 = var.security_group_name
+  delete_default_rules = true
+}
+
+# ST.001 Disable
+resource "huaweicloud_networking_secgroup_rule" "ingress" {
+  security_group_id = huaweicloud_networking_secgroup.test.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = var.backend_protocol
+  port_range_min    = var.backend_port
+  port_range_max    = var.backend_port
+  remote_ip_prefix  = var.ingress_cidr
+}
+
+resource "huaweicloud_networking_secgroup_rule" "egress" {
+  security_group_id = huaweicloud_networking_secgroup.test.id
+  direction         = "egress"
+  ethertype         = "IPv4"
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+# ST.001 Enable
+
+resource "huaweicloud_compute_instance" "test" {
+  name               = var.instance_name
+  availability_zone  = try(data.huaweicloud_availability_zones.test.names[0], null)
+  flavor_id          = var.ecs_flavor_id != "" ? var.ecs_flavor_id : try(data.huaweicloud_compute_flavors.test.flavors[0].id, "")
+  image_id           = var.ecs_image_id != "" ? var.ecs_image_id : try(data.huaweicloud_images_images.test.images[0].id, "")
+  security_group_ids = [huaweicloud_networking_secgroup.test.id]
+  system_disk_type   = var.ecs_system_disk_type
+  system_disk_size   = var.ecs_system_disk_size
+
+  network {
+    uuid = huaweicloud_vpc_subnet.test.id
+  }
+
+  admin_pass = var.ecs_admin_password
+  tags       = var.ecs_instance_tags
+
+  depends_on = [
+    huaweicloud_nat_gateway.test,
+    huaweicloud_vpc_eip.test
+  ]
 }
