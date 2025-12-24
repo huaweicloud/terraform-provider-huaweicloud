730056d0dd906760e1efea1f3de40116911d3db3
jinyangyang222 <jinyangyang222@outlook.com>
2025-12-24 15:16:46 +0800

    feat(hss): support `hss_image_local_hosts` data source (#8690)

---
+++ huaweicloud/services/hss/data_source_huaweicloud_hss_image_local_hosts.go
@@ -0,0 +1,215 @@
+package hss
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+// @API HSS GET /v5/{project_id}/image/local/hosts
+func DataSourceImageLocalHosts() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceImageLocalHostsRead,
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"enterprise_project_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"image_digest": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"image_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"image_version": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"host_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"agent_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"total_num": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"data_list": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"agent_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"agent_status": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"host_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"host_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"version": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"private_ip": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"public_ip": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"docker_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"docker_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func buildImageLocalHostsQueryParams(d *schema.ResourceData, epsId string) string {
+	queryParams := "?limit=200"
+
+	if epsId != "" {
+		queryParams = fmt.Sprintf("%s&enterprise_project_id=%v", queryParams, epsId)
+	}
+	if v, ok := d.GetOk("image_digest"); ok {
+		queryParams = fmt.Sprintf("%s&image_digest=%v", queryParams, v)
+	}
+	if v, ok := d.GetOk("image_name"); ok {
+		queryParams = fmt.Sprintf("%s&image_name=%v", queryParams, v)
+	}
+	if v, ok := d.GetOk("image_version"); ok {
+		queryParams = fmt.Sprintf("%s&image_version=%v", queryParams, v)
+	}
+	if v, ok := d.GetOk("host_name"); ok {
+		queryParams = fmt.Sprintf("%s&host_name=%v", queryParams, v)
+	}
+	if v, ok := d.GetOk("agent_id"); ok {
+		queryParams = fmt.Sprintf("%s&agent_id=%v", queryParams, v)
+	}
+
+	return queryParams
+}
+
+func dataSourceImageLocalHostsRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var (
+		cfg      = meta.(*config.Config)
+		region   = cfg.GetRegion(d)
+		product  = "hss"
+		epsId    = cfg.GetEnterpriseProjectID(d)
+		httpUrl  = "v5/{project_id}/image/local/hosts"
+		result   = make([]interface{}, 0)
+		offset   = 0
+		totalNum float64
+	)
+
+	client, err := cfg.NewServiceClient(product, region)
+	if err != nil {
+		return diag.Errorf("error creating HSS client: %s", err)
+	}
+
+	requestPath := client.Endpoint + httpUrl
+	requestPath = strings.ReplaceAll(requestPath, "{project_id}", client.ProjectID)
+	requestPath += buildImageLocalHostsQueryParams(d, epsId)
+	requestOpts := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		MoreHeaders:      map[string]string{"region": region},
+	}
+
+	for {
+		requestPathWithOffset := fmt.Sprintf("%s&offset=%v", requestPath, offset)
+		resp, err := client.Request("GET", requestPathWithOffset, &requestOpts)
+		if err != nil {
+			return diag.Errorf("error retrieving HSS image local hosts: %s", err)
+		}
+
+		respBody, err := utils.FlattenResponse(resp)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		totalNum = utils.PathSearch("total_num", respBody, float64(0)).(float64)
+		dataListResp := utils.PathSearch("data_list", respBody, make([]interface{}, 0)).([]interface{})
+		if len(dataListResp) == 0 {
+			break
+		}
+
+		result = append(result, dataListResp...)
+		offset += len(dataListResp)
+	}
+
+	generateUUID, err := uuid.GenerateUUID()
+	if err != nil {
+		return diag.Errorf("unable to generate ID: %s", err)
+	}
+
+	d.SetId(generateUUID)
+
+	mErr := multierror.Append(nil,
+		d.Set("region", region),
+		d.Set("total_num", totalNum),
+		d.Set("data_list", flattenImageLocalHostsDataList(result)),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func flattenImageLocalHostsDataList(resp []interface{}) []interface{} {
+	if len(resp) == 0 {
+		return nil
+	}
+
+	result := make([]interface{}, 0, len(resp))
+	for _, v := range resp {
+		result = append(result, map[string]interface{}{
+			"agent_id":     utils.PathSearch("agent_id", v, nil),
+			"agent_status": utils.PathSearch("agent_status", v, nil),
+			"host_name":    utils.PathSearch("host_name", v, nil),
+			"host_id":      utils.PathSearch("host_id", v, nil),
+			"version":      utils.PathSearch("version", v, nil),
+			"private_ip":   utils.PathSearch("private_ip", v, nil),
+			"public_ip":    utils.PathSearch("public_ip", v, nil),
+			"docker_name":  utils.PathSearch("docker_name", v, nil),
+			"docker_type":  utils.PathSearch("docker_type", v, nil),
+		})
+	}
+
+	return result
+}
