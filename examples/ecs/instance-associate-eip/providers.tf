b304602d24b704d47169a67b03bef2c0b40cdf83
Lance52259 <Overlord52259@gmail.com>
2025-12-26 09:31:02 +0800

    fix(hcbp): fix all ST.007 issues under the examples folder

--- examples/ecs/instance-associate-eip/providers.tf
+++ examples/ecs/instance-associate-eip/providers.tf
@@ -1,5 +1,6 @@
 terraform {
   required_version = ">= 1.2.0"
+
   required_providers {
     huaweicloud = {
       source  = "huaweicloud/huaweicloud"
--- examples/ecs/instance-with-userdata/providers.tf
+++ examples/ecs/instance-with-userdata/providers.tf
@@ -1,5 +1,6 @@
 terraform {
   required_version = ">= 1.1"
+
   required_providers {
     huaweicloud = {
       source  = "huaweicloud/huaweicloud"
