750f7656686c721589aec7f30ec02b0edf644c41
ruwenqiang123 <ruwenqiang123@outlook.com>
2026-01-05 19:34:17 +0800

    feat(secmaster): add new resource to manage workflow version validation

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -3751,6 +3751,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_secmaster_soc_mapping_delete":          secmaster.ResourceSocMappingDelete(),
 			"huaweicloud_secmaster_workflow_action":             secmaster.ResourceWorkflowAction(),
 			"huaweicloud_secmaster_workflow_version_approval":   secmaster.ResourceWorkflowVersionApproval(),
+			"huaweicloud_secmaster_workflow_version_validation": secmaster.ResourceWorkflowVersionValidation(),
 			"huaweicloud_secmaster_workflow_version":            secmaster.ResourceWorkflowVersion(),
 			"huaweicloud_secmaster_workflow":                    secmaster.ResourceWorkflow(),
 			"huaweicloud_secmaster_workspace":                   secmaster.ResourceWorkspace(),
