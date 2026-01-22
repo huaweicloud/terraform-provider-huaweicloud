6ebc84e1056141b1b90266cf659b2aaa78deba25
wuzhuanhong <wuzhuanhong1@outlook.com>
2026-01-22 17:47:26 +0800

    test(iam): optimize the acceptance case for v5 user password

--- huaweicloud/services/acceptance/iam/resource_huaweicloud_identityv5_user_password_test.go
+++ huaweicloud/services/acceptance/iam/resource_huaweicloud_identityv5_user_password_test.go
@@ -9,35 +9,87 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccIdentityV5UserPassword_basic(t *testing.T) {
-	password := "TestPassword#$%111"
-	originalPassword := "TestPassword#$%"
-	resourceName := "huaweicloud_identityv5_user_password.test"
+func TestAccV5UserPassword_basic(t *testing.T) {
+	var (
+		name = acceptance.RandomAccResourceName()
+
+		rName = "huaweicloud_identityv5_user_password.test"
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: func() {
-			acceptance.TestAccPreCheck(t)
-		},
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
 		ProviderFactories: acceptance.TestAccProviderFactories,
-		CheckDestroy:      nil,
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"random": {
+				Source: "hashicorp/random",
+				// The version of the random provider must be greater than 3.3.0 to support the 'numeric' parameter.
+				VersionConstraint: "3.3.0",
+			},
+		},
+		// This is a one-time resource without delete logic.
+		CheckDestroy: nil,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccIdentityV5UserPassword_basic(password, originalPassword),
+				Config: testAccV5UserPassword_basic_step1(name),
+			},
+			{
+				Config: testAccV5UserPassword_basic_step2(name),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(resourceName, "id"),
-					resource.TestCheckResourceAttr(resourceName, "new_password", password),
-					resource.TestCheckResourceAttr(resourceName, "old_password", originalPassword),
+					resource.TestCheckResourceAttrPair(rName, "old_password", "random_string.test.0", "result"),
+					resource.TestCheckResourceAttrPair(rName, "new_password", "random_string.test.1", "result"),
 				),
 			},
 		},
 	})
 }
 
-func testAccIdentityV5UserPassword_basic(newPassword, oldPassword string) string {
+func testAccV5UserPassword_basic_step1(name string) string {
 	return fmt.Sprintf(`
-resource "huaweicloud_identityv5_user_password" "test" {
-  new_password = "%[1]s"
-  old_password = "%[2]s"
+resource "huaweicloud_identityv5_user" "test" {
+  name = "%[1]s"
 }
-`, newPassword, oldPassword)
+
+resource "huaweicloud_identityv5_access_key" "test" {
+  user_id = huaweicloud_identityv5_user.test.id
+}
+
+resource "random_string" "test" {
+  count = 2
+
+  length           = 10
+  min_numeric      = 1
+  min_special      = 2
+  min_lower        = 1
+  override_special = "@!#&"
+}
+
+resource "huaweicloud_identityv5_login_profile" "test" {
+  user_id  = huaweicloud_identityv5_user.test.id
+  password = random_string.test[0].result
+}
+`, name)
+}
+
+// lintignore:AT004
+func testAccV5UserPassword_basic_step2(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+provider "huaweicloud" {
+  alias  = "new_user"
+  region = "%[2]s"
+
+  access_key = huaweicloud_identityv5_access_key.test.id
+  secret_key = huaweicloud_identityv5_access_key.test.secret_access_key
+}
+
+resource "huaweicloud_identityv5_user_password" "test" {
+  provider = huaweicloud.new_user
+
+  new_password = random_string.test[1].result
+  old_password = huaweicloud_identityv5_login_profile.test.password
+
+  depends_on = [huaweicloud_identityv5_login_profile.test]
+}
+`, testAccV5UserPassword_basic_step1(name), acceptance.HW_REGION_NAME)
 }
