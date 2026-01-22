7ccad86ee856f4373c5a05f5e4cc188cc8636b7e
Lance <Overlord52259@gmail.com>
2026-01-22 17:00:13 +0800

    test(iam): optimize the acceptance case design for group (#8870)

--- huaweicloud/services/acceptance/iam/resource_huaweicloud_identity_group_test.go
+++ huaweicloud/services/acceptance/iam/resource_huaweicloud_identity_group_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func getIdentityGroupResourceFunc(c *config.Config, state *terraform.ResourceState) (interface{}, error) {
+func getGroupResourceFunc(c *config.Config, state *terraform.ResourceState) (interface{}, error) {
 	client, err := c.IdentityV3Client(acceptance.HW_REGION_NAME)
 	if err != nil {
 		return nil, fmt.Errorf("error creating IAM client: %s", err)
@@ -21,15 +21,15 @@ func getIdentityGroupResourceFunc(c *config.Config, state *terraform.ResourceSta
 	return groups.Get(client, state.Primary.ID).Extract()
 }
 
-func TestAccIdentityGroup_basic(t *testing.T) {
-	var group groups.Group
-	groupName := acceptance.RandomAccResourceName()
-	resourceName := "huaweicloud_identity_group.group_1"
+func TestAccGroup_basic(t *testing.T) {
+	var (
+		obj interface{}
 
-	rc := acceptance.InitResourceCheck(
-		resourceName,
-		&group,
-		getIdentityGroupResourceFunc,
+		resourceName = "huaweicloud_identity_group.test"
+		rc           = acceptance.InitResourceCheck(resourceName, &obj, getGroupResourceFunc)
+
+		name       = acceptance.RandomAccResourceName()
+		updateName = acceptance.RandomAccResourceName()
 	)
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -41,11 +41,19 @@ func TestAccIdentityGroup_basic(t *testing.T) {
 		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccIdentityGroup_basic(groupName),
+				Config: testAccGroup_basic_step1(name),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(resourceName, "name", groupName),
-					resource.TestCheckResourceAttr(resourceName, "description", "An ACC test group"),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "description", "Created by terraform script"),
+				),
+			},
+			{
+				Config: testAccGroup_basic_step2(updateName),
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(resourceName, "name", updateName),
+					resource.TestCheckResourceAttr(resourceName, "description", "Updated by terraform script"),
 				),
 			},
 			{
@@ -53,32 +61,24 @@ func TestAccIdentityGroup_basic(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
-			{
-				Config: testAccIdentityGroup_update(groupName),
-				Check: resource.ComposeTestCheckFunc(
-					rc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(resourceName, "name", groupName),
-					resource.TestCheckResourceAttr(resourceName, "description", "An ACC update group"),
-				),
-			},
 		},
 	})
 }
 
-func testAccIdentityGroup_basic(groupName string) string {
+func testAccGroup_basic_step1(name string) string {
 	return fmt.Sprintf(`
-resource "huaweicloud_identity_group" "group_1" {
-  name        = "%s"
-  description = "An ACC test group"
+resource "huaweicloud_identity_group" "test" {
+  name        = "%[1]s"
+  description = "Created by terraform script"
 }
-`, groupName)
+`, name)
 }
 
-func testAccIdentityGroup_update(groupName string) string {
+func testAccGroup_basic_step2(name string) string {
 	return fmt.Sprintf(`
-resource "huaweicloud_identity_group" "group_1" {
-  name        = "%s"
-  description = "An ACC update group"
+resource "huaweicloud_identity_group" "test" {
+  name        = "%[1]s"
+  description = "Updated by terraform script"
 }
-`, groupName)
+`, name)
 }
