02e4781af59ee25adcf2c10fb6d85d5050892f0b
greyworld <huawei@ecs-yanghongyu>
2026-01-23 17:06:32 +0800

    test(iam): optimize the acceptance case design for catalog datasource 1. update the check items and function naming 2. fix test case bug, catalog datasource need project token. so, the `huaweicloud_identity_user_token` must set parameter `project_name`

--- huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_catalog_test.go
+++ huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_catalog_test.go
@@ -2,6 +2,7 @@ package iam
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -9,35 +10,64 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccIdentityCatalogDataSource_basic(t *testing.T) {
-	userName := acceptance.RandomAccResourceName()
-	initPassword := acceptance.RandomPassword()
-	resourceName := "data.huaweicloud_identity_catalog.test"
-	dc := acceptance.InitDataSourceCheck(resourceName)
+func TestAccDataCatalog_basic(t *testing.T) {
+	var (
+		userName = acceptance.RandomAccResourceName()
+		password = acceptance.RandomPassword()
+
+		all = "data.huaweicloud_identity_catalog.all"
+		dc  = acceptance.InitDataSourceCheck(all)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckAdminOnly(t)
+			acceptance.TestAccPrecheckDomainName(t)
 		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccIdentityCatalogDataSource_basic(userName, initPassword),
+				Config: testAccDataCatalog_basic(userName, password),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttrSet(resourceName, "catalog.#"),
+					resource.TestMatchResourceAttr(all, "catalog.#", regexp.MustCompile(`^[1-9]([0-9]*)?$`)),
+					resource.TestCheckResourceAttrSet(all, "catalog.0.id"),
+					resource.TestCheckResourceAttrSet(all, "catalog.0.name"),
+					resource.TestCheckResourceAttrSet(all, "catalog.0.type"),
 				),
 			},
 		},
 	})
 }
 
-func testAccIdentityCatalogDataSource_basic(userName, initPassword string) string {
+func testAccDataCatalog_base(name, password string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_identity_user" "test" {
+  name        = "%[1]s"
+  password    = "%[2]s"
+  enabled     = true
+  email       = "%[1]s@abc.com"
+  description = "tested by terraform"
+
+  login_protect_verification_method = "email"
+}
+
+resource "huaweicloud_identity_user_token" "test" {
+  user_name    = huaweicloud_identity_user.test.name
+  password     = "%[2]s"
+  account_name = "%[3]s"
+  project_name = "%[4]s"
+}
+`, name, password, acceptance.HW_DOMAIN_NAME, acceptance.HW_REGION_NAME)
+}
+
+func testAccDataCatalog_basic(name, password string) string {
 	return fmt.Sprintf(`
 %[1]s
 
-data "huaweicloud_identity_catalog" "test" {
+data "huaweicloud_identity_catalog" "all" {
   project_token = huaweicloud_identity_user_token.test.token
 }
-`, testAccIdentityUserToken_project(userName, initPassword))
+`, testAccDataCatalog_base(name, password))
 }
