6f936717ae820441d0aa1cf29f4e4d187967066b
Lance52259 <Overlord52259@gmail.com>
2026-01-23 16:37:13 +0800

    test(iam): optimize the acceptace case for provider protocol

--- huaweicloud/services/acceptance/iam/resource_huaweicloud_identity_provider_protocol_test.go
+++ huaweicloud/services/acceptance/iam/resource_huaweicloud_identity_provider_protocol_test.go
@@ -7,8 +7,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
-	"github.com/chnsz/golangsdk/openstack/identity/federatedauth/providers"
-
 	"github.com/huaweicloud/huaweicloud-sdk-go-v3/services/iam/v3/model"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
@@ -26,24 +24,19 @@ func getProviderProtocolFunc(c *config.Config, state *terraform.ResourceState) (
 	}
 	response, err := iamV3Client.KeystoneShowProtocol(request)
 	if err != nil {
-		return fmt.Errorf("KeystoneShowProtocol error : %s", err), err
+		return nil, err
 	}
 	return response.Protocol, nil
 }
 
-func generateTestMappingID(providerID string) string {
-	return "test_" + providerID
-}
+func TestAccProviderProtocol_basic(t *testing.T) {
+	var (
+		obj interface{}
 
-func TestAccIdentityProviderProtocol_basic(t *testing.T) {
-	var provider providers.Provider
-	var randomName = acceptance.RandomAccResourceName()
-	resourceName := "huaweicloud_identity_provider_protocol.protocol"
+		resourceName = "huaweicloud_identity_provider_protocol.test"
+		rc           = acceptance.InitResourceCheck(resourceName, &obj, getProviderProtocolFunc)
 
-	rc := acceptance.InitResourceCheck(
-		resourceName,
-		&provider,
-		getProviderProtocolFunc,
+		name = acceptance.RandomAccResourceName()
 	)
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -54,12 +47,12 @@ func TestAccIdentityProviderProtocol_basic(t *testing.T) {
 		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccIdentityProviderProtocolCreate(randomName),
+				Config: testAccProviderProtocol_basic_step1(name),
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(resourceName, "provider_id", randomName),
+					resource.TestCheckResourceAttr(resourceName, "provider_id", name),
 					resource.TestCheckResourceAttr(resourceName, "protocol_id", "saml"),
-					resource.TestCheckResourceAttr(resourceName, "mapping_id", "mapping_"+generateTestMappingID(randomName)),
+					resource.TestCheckResourceAttrPair(resourceName, "mapping_id", "huaweicloud_identity_provider_conversion.test", "id"),
 					resource.TestCheckResourceAttr(resourceName, "links.#", "1"),
 				),
 			},
@@ -72,33 +65,34 @@ func TestAccIdentityProviderProtocol_basic(t *testing.T) {
 	})
 }
 
-// huaweicloud_identity_provider创建一个saml协议的idp，mapping_id为mapping_{provider_id}
-// huaweicloud_identity_provider_conversion创建一个新的mapping，mapping_id为mapping_generateTestMappingID(name)
-// huaweicloud_identity_provider_protocol更新协议，修改mapping为mapping_generateTestMappingID(name)
-func testAccIdentityProviderProtocolCreate(name string) string {
+func testAccProviderProtocol_basic_step1(name string) string {
 	return fmt.Sprintf(`
-resource "huaweicloud_identity_provider" "provider1" {
-  name     = "%s"
-  protocol = "saml"
+resource "huaweicloud_identity_provider" "test" {
+  name = "%[1]s"
+
+  lifecycle {
+    ignore_changes = [conversion_rules]
+  }
 }
 
-resource "huaweicloud_identity_provider_conversion" "conversion" {
-  provider_id = "%s"
+resource "huaweicloud_identity_provider_conversion" "test" {
+  provider_id = huaweicloud_identity_provider.test.id
 
   conversion_rules {
     local {
       username = "Tom"
     }
+
     remote {
       attribute = "Tom"
     }
   }
 }
 
-resource "huaweicloud_identity_provider_protocol" "protocol" {
-  provider_id = huaweicloud_identity_provider.provider1.id
+resource "huaweicloud_identity_provider_protocol" "test" {
+  provider_id = huaweicloud_identity_provider.test.id
   protocol_id = "saml"
-  mapping_id  = huaweicloud_identity_provider_conversion.conversion.id
+  mapping_id  = huaweicloud_identity_provider_conversion.test.id
 }
-`, name, generateTestMappingID(name))
+`, name)
 }
