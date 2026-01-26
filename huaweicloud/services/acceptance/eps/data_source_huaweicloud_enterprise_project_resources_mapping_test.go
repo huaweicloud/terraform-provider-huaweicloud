72d5f70daeb7b19d64c449c590d01f1741ca342c
allium <deerhang@163.com>
2026-01-26 11:47:46 +0800

    fix(eps): eliminate potential vulnerabilities datasource resources mapping

--- huaweicloud/services/acceptance/eps/data_source_huaweicloud_enterprise_project_resources_mapping_test.go
+++ huaweicloud/services/acceptance/eps/data_source_huaweicloud_enterprise_project_resources_mapping_test.go
@@ -9,8 +9,10 @@ import (
 )
 
 func TestAccDataEnterpriseProjectResourcesMapping_basic(t *testing.T) {
-	all := "data.huaweicloud_enterprise_project_resources_mapping.test"
-	dc := acceptance.InitDataSourceCheck(all)
+	var (
+		dataSourceName = "data.huaweicloud_enterprise_project_resources_mapping.test"
+		dc             = acceptance.InitDataSourceCheck(dataSourceName)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
@@ -20,8 +22,7 @@ func TestAccDataEnterpriseProjectResourcesMapping_basic(t *testing.T) {
 				Config: testAccDataEnterpriseProjectResourcesMapping_basic,
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(all, "resource_mapping.vpcs", "vpc"),
-					resource.TestCheckOutput("huaweicloud_enterprise_project_resources_mapping", "true"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "resource_mapping.%"),
 				),
 			},
 		},
@@ -30,8 +31,4 @@ func TestAccDataEnterpriseProjectResourcesMapping_basic(t *testing.T) {
 
 const testAccDataEnterpriseProjectResourcesMapping_basic = `
 data "huaweicloud_enterprise_project_resources_mapping" "test" {}
-
-output "huaweicloud_enterprise_project_resources_mapping" {
-  value = length(data.huaweicloud_enterprise_project_resources_mapping.test.resource_mapping) > 0
-}
 `
