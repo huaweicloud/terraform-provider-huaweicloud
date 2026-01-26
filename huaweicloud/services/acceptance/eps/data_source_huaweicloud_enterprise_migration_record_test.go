48e54050ad12024bb2a2bb3e4d1370386a3a9a52
allium <deerhang@163.com>
2026-01-26 09:48:56 +0800

    fix(eps): eliminate potential vulnerabilities in the datasource migrate records (#8897)

--- huaweicloud/services/acceptance/eps/data_source_huaweicloud_enterprise_migration_record_test.go
+++ huaweicloud/services/acceptance/eps/data_source_huaweicloud_enterprise_migration_record_test.go
@@ -1,6 +1,7 @@
 package eps
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -9,34 +10,80 @@ import (
 )
 
 func TestAccDataEnterpriseProjectMigrationRecord_basic(t *testing.T) {
-	all := "data.huaweicloud_enterprise_project_migrate_record.test"
-	dc := acceptance.InitDataSourceCheck(all)
+	var (
+		dataSourceName = "data.huaweicloud_enterprise_project_migrate_record.test"
+		dc             = acceptance.InitDataSourceCheck(dataSourceName)
+		name           = acceptance.RandomAccResourceName()
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckMigrateEpsID(t)
+		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataEnterpriseProjectMigrationRecord_basic_test(),
+				Config: testAccDataEnterpriseProjectMigrationRecord_base(name),
+			},
+			{
+				Config: testAccDataEnterpriseProjectMigrationRecord_basic(name),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttrSet(all, "records.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "records.0.event_time"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "records.0.operate_type"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "records.0.project_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "records.0.project_name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "records.0.region_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "records.0.resource_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "records.0.resource_type"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "records.0.user_name"),
+
+					resource.TestCheckOutput("resource_id_filter_is_useful", "true"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataEnterpriseProjectMigrationRecord_basic_test() string {
-	return `
-data "huaweicloud_enterprise_project_migrate_record" "test" {
-  start_time  = "2020-01-01 00:00:00"
-  end_time    = "2025-11-19 15:30:30"
-  resource_id = "3dde353d-0117-4e1a-a09c-4750f61a3c5d"
+func testAccDataEnterpriseProjectMigrationRecord_base(name string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_cc_connection" "test" {
+  name                  = "%s"
+  enterprise_project_id = "%s"
+}
+`, name, acceptance.HW_ENTERPRISE_PROJECT_ID_TEST)
 }
 
-output "huaweicloud_enterprise_project_migrate_record" {
-  value = data.huaweicloud_enterprise_project_migrate_record.test
+func testAccDataEnterpriseProjectMigrationRecord_basic(name string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_cc_connection" "test" {
+  name                  = "%s"
+  enterprise_project_id = "%s"
 }
-`
+
+data "huaweicloud_enterprise_project_migrate_record" "test" {
+  depends_on = [huaweicloud_cc_connection.test]
+}
+
+# Filter by resource_id
+locals {
+  resource_id = data.huaweicloud_enterprise_project_migrate_record.test.records[0].resource_id
+}
+
+data "huaweicloud_enterprise_project_migrate_record" "filter_by_resource_id" {
+  resource_id = local.resource_id
+}
+
+locals {
+  resource_id_filter_result = [
+    for v in data.huaweicloud_enterprise_project_migrate_record.filter_by_resource_id.records[*].resource_id : v == local.resource_id
+  ]
+}
+
+output "resource_id_filter_is_useful" {
+  value = alltrue(local.resource_id_filter_result) && length(local.resource_id_filter_result) > 0
+}
+
+`, name, acceptance.HW_ENTERPRISE_MIGRATE_PROJECT_ID_TEST)
 }
