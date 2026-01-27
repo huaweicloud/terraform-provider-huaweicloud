3fa581c494fc001a13877821840db2d8389d172e
greyworld <huawei@ecs-yanghongyu>
2026-01-27 11:36:08 +0800

    test(iam): optimize the acceptance case design for custom role datasource

--- huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_custom_role_test.go
+++ huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_custom_role_test.go
@@ -2,6 +2,7 @@ package iam
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -9,10 +10,13 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccIdentityCustomRoleDataSource_basic(t *testing.T) {
-	dataSourceName := "data.huaweicloud_identity_custom_role.role_1"
-	rName := acceptance.RandomAccResourceName()
-	dc := acceptance.InitDataSourceCheck(dataSourceName)
+func TestAccDataCustomRole_basic(t *testing.T) {
+	var (
+		name = acceptance.RandomAccResourceName()
+
+		all = "data.huaweicloud_identity_custom_role.all"
+		dc  = acceptance.InitDataSourceCheck(all)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
@@ -22,20 +26,21 @@ func TestAccIdentityCustomRoleDataSource_basic(t *testing.T) {
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccIdentityCustomRoleDataSource_basic(rName),
+				Config: testAccDataCustomRole_basic(name),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "name", rName),
+					resource.TestMatchResourceAttr(all, "id", regexp.MustCompile(`^[a-zA-Z0-9-]+$`)),
+					resource.TestCheckOutput("is_name_filter_useful", "true"),
 				),
 			},
 		},
 	})
 }
 
-func testAccIdentityCustomRoleDataSource_basic(rName string) string {
+func testAccDataCustomRole_base(name string) string {
 	return fmt.Sprintf(`
-resource "huaweicloud_identity_role" test {
-  name        = "%s"
+resource "huaweicloud_identity_role" "test" {
+  name        = "%[1]s"
   description = "created by terraform"
   type        = "AX"
   policy      = <<EOF
@@ -55,13 +60,19 @@ resource "huaweicloud_identity_role" test {
 }
 EOF
 }
-
-data "huaweicloud_identity_custom_role" "role_1" {
-  name = huaweicloud_identity_role.test.name
-
-  depends_on = [
-	huaweicloud_identity_role.test
-  ]
+`, name)
 }
-`, rName)
+
+func testAccDataCustomRole_basic(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+data "huaweicloud_identity_custom_role" "all" {
+  name = huaweicloud_identity_role.test.name
+}
+
+output "is_name_filter_useful" {
+  value = data.huaweicloud_identity_custom_role.all.name == "%[2]s"
+}
+`, testAccDataCustomRole_base(name), name)
 }
