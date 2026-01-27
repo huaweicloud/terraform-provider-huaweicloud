ccb4a0a539e467a03145be38bdf10162a49c520c
houpeng80 <hp_monkey@163.com>
2026-01-27 15:02:36 +0800

    fix(CCE): cce autopilot cluster log configs fix test and doc

--- huaweicloud/services/acceptance/cceautopilot/data_source_huaweicloud_cce_autopilot_cluster_log_configs_test.go
+++ huaweicloud/services/acceptance/cceautopilot/data_source_huaweicloud_cce_autopilot_cluster_log_configs_test.go
@@ -1,6 +1,7 @@
 package cceautopilot
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -15,26 +16,37 @@ func TestAccDataSourceCceAutopilotClusterLogConfigs_basic(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckCceClusterId(t)
 		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testDataSourceCceAutopilotClusterLogsConfigs_basic,
+				Config: testAccDataSourceCceAutopilotClusterLogConfigs_basic(),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckOutput("is_results_not_empty", "true"),
+					resource.TestCheckResourceAttrSet(dataSource, "ttl_in_days"),
+					resource.TestCheckResourceAttrSet(dataSource, "log_configs.#"),
+					resource.TestCheckResourceAttrSet(dataSource, "log_configs.0.name"),
+					resource.TestCheckResourceAttrSet(dataSource, "log_configs.0.enable"),
+					resource.TestCheckResourceAttrSet(dataSource, "log_configs.0.name"),
+					resource.TestCheckOutput("type_filter_is_useful", "true"),
 				),
 			},
 		},
 	})
 }
 
-const testDataSourceCceAutopilotClusterLogsConfigs_basic = `
+func testAccDataSourceCceAutopilotClusterLogConfigs_basic() string {
+	return fmt.Sprintf(`
 data "huaweicloud_cce_autopilot_cluster_log_configs" "test" {
-  cluster_id = "83a085fe-d4f1-11f0-80d0-0255ac10178d"
+  cluster_id = "%[1]s"
 }
-
-output "is_results_not_empty" {
-  value = length(data.huaweicloud_cce_autopilot_cluster_log_configs.test.log_configs) > 0
+data "huaweicloud_cce_autopilot_cluster_log_configs" "type_filter" {
+  cluster_id = "%[1]s"
+  type       = "control"
 }
-`
+output "type_filter_is_useful" {
+  value = length(data.huaweicloud_cce_autopilot_cluster_log_configs.type_filter.log_configs) > 0
+}
+`, acceptance.HW_CCE_CLUSTER_ID)
+}
