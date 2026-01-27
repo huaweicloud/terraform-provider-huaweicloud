23bb0886876aa684a23cecb1f353b2514f191833
jinyangyang222 <jinyangyang222@outlook.com>
2026-01-27 15:34:54 +0800

    fix(ram): the `ram_distinct_shared_principals` dataSource quality improvement

--- huaweicloud/services/ram/data_source_huaweicloud_ram_distinct_shared_principals.go
+++ huaweicloud/services/ram/data_source_huaweicloud_ram_distinct_shared_principals.go
@@ -26,9 +26,7 @@ func DataSourceDistinctSharedPrincipals() *schema.Resource {
 			"principals": {
 				Type:     schema.TypeList,
 				Optional: true,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-				},
+				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
 			"resource_urn": {
 				Type:     schema.TypeString,
@@ -61,45 +59,42 @@ func distinctSharedPrincipalSchema() *schema.Resource {
 }
 
 func dataSourceDistinctSharedPrincipalsRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	cfg := meta.(*config.Config)
-	region := cfg.GetRegion(d)
-
 	var (
-		listDistinctSharedPrincipalsHttpUrl = "v1/shared-principals/search-distinct-principal"
-		listDistinctSharedPrincipalsProduct = "ram"
+		cfg     = meta.(*config.Config)
+		region  = cfg.GetRegion(d)
+		httpUrl = "v1/shared-principals/search-distinct-principal"
+		product = "ram"
+		result  []interface{}
+		marker  string
+		limit   = 2000
 	)
-	listDistinctSharedPrincipalsClient, err := cfg.NewServiceClient(listDistinctSharedPrincipalsProduct, region)
+
+	client, err := cfg.NewServiceClient(product, region)
 	if err != nil {
 		return diag.Errorf("error creating RAM client: %s", err)
 	}
 
-	listDistinctSharedPrincipalsHttpPath := listDistinctSharedPrincipalsClient.Endpoint + listDistinctSharedPrincipalsHttpUrl
-
-	var distinctSharedPrincipals []interface{}
-	var marker string
-	var limit = 200
-
 	for {
-		listDistinctSharedPrincipalsHttpOpt := golangsdk.RequestOpts{
+		requestPath := client.Endpoint + httpUrl
+		requestOpts := golangsdk.RequestOpts{
 			KeepResponseBody: true,
-			JSONBody:         buildDistinctSharedPrincipalsBody(d, limit, marker),
+			JSONBody:         utils.RemoveNil(buildDistinctSharedPrincipalsBodyParams(d, limit, marker)),
 		}
-		listDistinctSharedPrincipalsHttpResp, err := listDistinctSharedPrincipalsClient.Request(
-			"POST",
-			listDistinctSharedPrincipalsHttpPath,
-			&listDistinctSharedPrincipalsHttpOpt,
-		)
+
+		resp, err := client.Request("POST", requestPath, &requestOpts)
 		if err != nil {
-			return diag.Errorf("error retrieving RAM distinct shared principles, %s", err)
+			return diag.Errorf("error retrieving RAM distinct shared principles: %s", err)
 		}
-		listDistinctSharedPrincipalsRespBody, err := utils.FlattenResponse(listDistinctSharedPrincipalsHttpResp)
+
+		respBody, err := utils.FlattenResponse(resp)
 		if err != nil {
 			return diag.FromErr(err)
 		}
 
-		onePageSearchDistinctResource := flattenDistinctSharedPrincipalsResp(listDistinctSharedPrincipalsRespBody)
-		distinctSharedPrincipals = append(distinctSharedPrincipals, onePageSearchDistinctResource...)
-		marker = utils.PathSearch("page_info.next_marker", onePageSearchDistinctResource, "").(string)
+		principalsResp := utils.PathSearch(
+			"distinct_shared_principals", respBody, make([]interface{}, 0)).([]interface{})
+		result = append(result, principalsResp...)
+		marker = utils.PathSearch("page_info.next_marker", respBody, "").(string)
 		if marker == "" {
 			break
 		}
@@ -109,51 +104,49 @@ func dataSourceDistinctSharedPrincipalsRead(_ context.Context, d *schema.Resourc
 	if err != nil {
 		return diag.Errorf("unable to generate ID: %s", err)
 	}
+
 	d.SetId(generateUUID)
 
 	mErr := multierror.Append(nil,
-		d.Set("distinct_shared_principals", distinctSharedPrincipals),
+		d.Set("distinct_shared_principals", flattenDistinctSharedPrincipals(result)),
 	)
 
 	return diag.FromErr(mErr.ErrorOrNil())
 }
 
-func buildDistinctSharedPrincipalsBody(d *schema.ResourceData, limit int, marker string) map[string]interface{} {
-	params := make(map[string]interface{})
+func buildDistinctSharedPrincipalsBodyParams(d *schema.ResourceData, limit int, marker string) map[string]interface{} {
+	params := map[string]interface{}{
+		"resource_owner": d.Get("resource_owner"),
+		"limit":          limit,
+	}
 
-	params["limit"] = limit
 	if marker != "" {
 		params["marker"] = marker
 	}
 
 	if v, ok := d.GetOk("principals"); ok {
-		params["principals"] = v
+		params["principals"] = utils.ExpandToStringList(v.([]interface{}))
 	}
 
 	if v, ok := d.GetOk("resource_urn"); ok {
 		params["resource_urn"] = v
 	}
 
-	if v, ok := d.GetOk("resource_owner"); ok {
-		params["resource_owner"] = v
-	}
-
 	return params
 }
 
-func flattenDistinctSharedPrincipalsResp(resp interface{}) []interface{} {
-	if resp == nil {
+func flattenDistinctSharedPrincipals(resp []interface{}) []interface{} {
+	if len(resp) == 0 {
 		return nil
 	}
 
-	curJson := utils.PathSearch("distinct_shared_principals", resp, make([]interface{}, 0))
-	curArray := curJson.([]interface{})
-	rst := make([]interface{}, 0, len(curArray))
-	for _, v := range curArray {
+	rst := make([]interface{}, 0, len(resp))
+	for _, v := range resp {
 		rst = append(rst, map[string]interface{}{
 			"id":         utils.PathSearch("id", v, nil),
 			"updated_at": utils.PathSearch("updated_at", v, nil),
 		})
 	}
+
 	return rst
 }
