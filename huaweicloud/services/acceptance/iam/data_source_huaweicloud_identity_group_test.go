b7853c626860cf0c121f5ab1adb6ed87031af8ba
greyworld <huawei@ecs-yanghongyu>
2026-01-27 16:24:38 +0800

    test(iam): optimize the acceptance case design for group datasource

--- huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_group_test.go
+++ huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_group_test.go
@@ -9,10 +9,14 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccIdentityGroupDataSource_basic(t *testing.T) {
-	dataSourceName := "data.huaweicloud_identity_group.test"
-	rName := acceptance.RandomAccResourceName()
-	dc := acceptance.InitDataSourceCheck(dataSourceName)
+func TestAccDataGroup_basic(t *testing.T) {
+	var (
+		name     = acceptance.RandomAccResourceName()
+		password = acceptance.RandomPassword()
+
+		all = "data.huaweicloud_identity_group.all"
+		dc  = acceptance.InitDataSourceCheck(all)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
@@ -22,61 +26,21 @@ func TestAccIdentityGroupDataSource_basic(t *testing.T) {
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccIdentityGroupDataSource_by_name(rName),
+				Config: testAccDataGroup_basic(name, password),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "name", rName),
-					resource.TestCheckResourceAttr(dataSourceName, "users.#", "0"),
+					resource.TestCheckResourceAttr(all, "name", name),
+					resource.TestCheckResourceAttr(all, "users.#", "2"),
+					resource.TestCheckResourceAttrSet(all, "users.0.id"),
+					resource.TestCheckResourceAttrSet(all, "users.0.name"),
+					resource.TestCheckResourceAttrSet(all, "users.0.enabled"),
 				),
 			},
 		},
 	})
 }
 
-func TestAccIdentityGroupDataSource_with_user(t *testing.T) {
-	dataSourceName := "data.huaweicloud_identity_group.test"
-	rName := acceptance.RandomAccResourceName()
-	password := acceptance.RandomPassword()
-	dc := acceptance.InitDataSourceCheck(dataSourceName)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: func() {
-			acceptance.TestAccPreCheck(t)
-			acceptance.TestAccPreCheckAdminOnly(t)
-		},
-		ProviderFactories: acceptance.TestAccProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccIdentityGroupDataSource_with_user(rName, password),
-				Check: resource.ComposeTestCheckFunc(
-					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "name", rName),
-					resource.TestCheckResourceAttr(dataSourceName, "users.#", "2"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "users.0.id"),
-				),
-			},
-		},
-	})
-}
-
-func testAccIdentityGroupDataSource_by_name(rName string) string {
-	return fmt.Sprintf(`
-resource "huaweicloud_identity_group" "test" {
-  name        = "%s"
-  description = "An ACC test group"
-}
-
-data "huaweicloud_identity_group" "test" {
-  name = huaweicloud_identity_group.test.name
-  
-  depends_on = [
-    huaweicloud_identity_group.test
-  ]
-}
-`, rName)
-}
-
-func testAccIdentityGroupDataSource_with_user(rName, password string) string {
+func testAccDataGroup_base(name, password string) string {
 	return fmt.Sprintf(`
 resource "huaweicloud_identity_group" "test" {
   name        = "%[1]s"
@@ -92,15 +56,25 @@ resource "huaweicloud_identity_user" "test" {
 
 resource "huaweicloud_identity_group_membership" "test" {
   group = huaweicloud_identity_group.test.id
-  users = huaweicloud_identity_user.test.*.id
+  
+  users = [
+    for user in huaweicloud_identity_user.test : user.id
+  ]
+}
+`, name, password)
 }
 
-data "huaweicloud_identity_group" "test" {
+func testAccDataGroup_basic(name, password string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+data "huaweicloud_identity_group" "all" {
   name = huaweicloud_identity_group.test.name
 
+  # Waiting for the group membership to be created
   depends_on = [
     huaweicloud_identity_group_membership.test
   ]
 }
-`, rName, password)
+`, testAccDataGroup_base(name, password))
 }
