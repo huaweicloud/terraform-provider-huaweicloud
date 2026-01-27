c094a7465711c527d7283cd549149405327b861c
Zippo-Wang <852420284@qq.com>
2026-01-27 15:23:59 +0800

    chore(iam): optimize the schema and docs for getting regions

--- huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_regions_test.go
+++ huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_regions_test.go
@@ -9,9 +9,12 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccDataSourceIdentityRegions_basic(t *testing.T) {
-	dataSourceName := "data.huaweicloud_identity_regions.test"
-	dc := acceptance.InitDataSourceCheck(dataSourceName)
+func TestAccDataRegions_basic(t *testing.T) {
+	var (
+		dcName = "data.huaweicloud_identity_regions.test"
+
+		dc = acceptance.InitDataSourceCheck(dcName)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
@@ -20,63 +23,63 @@ func TestAccDataSourceIdentityRegions_basic(t *testing.T) {
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testTestDataSourceIdentityRegions,
+				Config: testAccDataRegions,
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.#", "26"),
+					resource.TestCheckResourceAttr(dcName, "regions.#", "26"),
 				),
 			},
 			{
-				Config: testTestDataSourceIdentityRegionsWithRegionId1,
+				Config: testAccDataRegionsWithRegionId1,
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.#", "1"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.id", "cn-north-4"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.type", "public"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.description", ""),
-					resource.TestMatchResourceAttr(dataSourceName, "regions.0.link",
+					resource.TestCheckResourceAttr(dcName, "regions.#", "1"),
+					resource.TestCheckResourceAttr(dcName, "regions.0.id", "cn-north-4"),
+					resource.TestCheckResourceAttr(dcName, "regions.0.type", "public"),
+					resource.TestCheckResourceAttr(dcName, "regions.0.description", ""),
+					resource.TestMatchResourceAttr(dcName, "regions.0.link",
 						regexp.MustCompile("https://iam.*.myhuaweicloud.com/v3/regions/cn-north-4")),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.locales.%", "5"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.locales.zh-cn", "华北-北京四"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.locales.en-us", "CN North-Beijing4"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.locales.pt-br", ""),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.locales.es-us", ""),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.locales.es-es", ""),
+					resource.TestMatchResourceAttr(dcName, "regions.0.locales.%", regexp.MustCompile(`[1-9]([0-9]*)?`)),
+					resource.TestCheckResourceAttr(dcName, "regions.0.locales.zh-cn", "华北-北京四"),
+					resource.TestCheckResourceAttr(dcName, "regions.0.locales.en-us", "CN North-Beijing4"),
+					resource.TestCheckResourceAttr(dcName, "regions.0.locales.pt-br", ""),
+					resource.TestCheckResourceAttr(dcName, "regions.0.locales.es-us", ""),
+					resource.TestCheckResourceAttr(dcName, "regions.0.locales.es-es", ""),
 				),
 			},
 			{
-				Config: testTestDataSourceIdentityRegionsWithRegionId2,
+				Config: testAccDataRegionsWithRegionId2,
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.#", "1"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.id", "la-south-2"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.type", "public"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.description", ""),
-					resource.TestMatchResourceAttr(dataSourceName, "regions.0.link",
+					resource.TestCheckResourceAttr(dcName, "regions.#", "1"),
+					resource.TestCheckResourceAttr(dcName, "regions.0.id", "la-south-2"),
+					resource.TestCheckResourceAttr(dcName, "regions.0.type", "public"),
+					resource.TestCheckResourceAttr(dcName, "regions.0.description", ""),
+					resource.TestMatchResourceAttr(dcName, "regions.0.link",
 						regexp.MustCompile("https://iam.*.myhuaweicloud.com/v3/regions/la-south-2")),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.locales.%", "5"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.locales.en-us", "LA-Santiago"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.locales.pt-br", "AL-Santiago"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.locales.es-us", "AL-Santiago de Chile1"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.locales.es-es", "LA-Santiago"),
-					resource.TestCheckResourceAttr(dataSourceName, "regions.0.locales.zh-cn", "拉美-圣地亚哥"),
+					resource.TestMatchResourceAttr(dcName, "regions.0.locales.%", regexp.MustCompile(`[1-9]([0-9]*)?`)),
+					resource.TestCheckResourceAttr(dcName, "regions.0.locales.en-us", "LA-Santiago"),
+					resource.TestCheckResourceAttr(dcName, "regions.0.locales.pt-br", "AL-Santiago"),
+					resource.TestCheckResourceAttr(dcName, "regions.0.locales.es-us", "AL-Santiago de Chile1"),
+					resource.TestCheckResourceAttr(dcName, "regions.0.locales.es-es", "LA-Santiago"),
+					resource.TestCheckResourceAttr(dcName, "regions.0.locales.zh-cn", "拉美-圣地亚哥"),
 				),
 			},
 		},
 	})
 }
 
-const testTestDataSourceIdentityRegions = `
+const testAccDataRegions = `
 data "huaweicloud_identity_regions" "test" {}
 `
 
-const testTestDataSourceIdentityRegionsWithRegionId1 = `
+const testAccDataRegionsWithRegionId1 = `
 data "huaweicloud_identity_regions" "test" {
   region_id = "cn-north-4"
 }
 `
 
-const testTestDataSourceIdentityRegionsWithRegionId2 = `
+const testAccDataRegionsWithRegionId2 = `
 data "huaweicloud_identity_regions" "test" {
   region_id = "la-south-2"
 }
