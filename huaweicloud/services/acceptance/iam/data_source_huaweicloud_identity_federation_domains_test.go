c426f913f64076b27d6ea85220fed73f72aa0120
greyworld <huawei@ecs-yanghongyu>
2026-01-28 15:08:36 +0800

    test(iam): optimize the acceptance case design for federation domains datasource

--- huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_federation_domains_test.go
+++ huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_federation_domains_test.go
@@ -2,6 +2,7 @@ package iam
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -9,12 +10,15 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccDataSourceIdentityFederationDomains_basic(t *testing.T) {
-	idpId := "YourIdpId"
-	protocolId := "YourProtocolId"
-	idToken := "YourIdToken"
-	dataSourceName := "data.huaweicloud_identity_federation_domains.test"
-	dc := acceptance.InitDataSourceCheck(dataSourceName)
+func TestAccDataFederationDomains_basic(t *testing.T) {
+	var (
+		idpId      = "YourIdpId"
+		protocolId = "YourProtocolId"
+		idToken    = "YourIdToken"
+
+		all = "data.huaweicloud_identity_federation_domains.all"
+		dc  = acceptance.InitDataSourceCheck(all)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
@@ -23,30 +27,30 @@ func TestAccDataSourceIdentityFederationDomains_basic(t *testing.T) {
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testTestDataSourceIdentityFederationDomains(idpId, protocolId, idToken),
+				Config: testAccDataFederationDomains_basic(idpId, protocolId, idToken),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttrSet(dataSourceName, "domains.#"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "domains.0.id"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "domains.0.name"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "domains.0.enabled"),
-					resource.TestCheckResourceAttr(dataSourceName, "domains.0.description", ""),
+					resource.TestMatchResourceAttr(all, "domains.#", regexp.MustCompile(`^[1-9]([0-9]*)?$`)),
+					resource.TestCheckResourceAttrSet(all, "domains.0.id"),
+					resource.TestCheckResourceAttrSet(all, "domains.0.name"),
+					resource.TestCheckResourceAttrSet(all, "domains.0.enabled"),
+					resource.TestCheckResourceAttr(all, "domains.0.description", ""),
 				),
 			},
 		},
 	})
 }
 
-func testTestDataSourceIdentityFederationDomains(idpId, protocolId, idToken string) string {
+func testAccDataFederationDomains_basic(idpId, protocolId, idToken string) string {
 	return fmt.Sprintf(`
 resource "huaweicloud_identity_unscoped_token_with_id_token" "test" {
- idp_id      = "%[1]s"
- protocol_id = "%[2]s"
- id_token    = "%[3]s"
+  idp_id      = "%[1]s"
+  protocol_id = "%[2]s"
+  id_token    = "%[3]s"
 }
 
-data "huaweicloud_identity_federation_domains" "test" {
- federation_token = huaweicloud_identity_unscoped_token_with_id_token.test.token
+data "huaweicloud_identity_federation_domains" "all" {
+  federation_token = huaweicloud_identity_unscoped_token_with_id_token.test.token
 }
 `, idpId, protocolId, idToken)
 }
