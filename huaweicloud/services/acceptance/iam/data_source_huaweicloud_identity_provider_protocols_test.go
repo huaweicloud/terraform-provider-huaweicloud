b3c308ed07d50773356738321287fd723c7bc3cc
greyworld <huawei@ecs-yanghongyu>
2026-01-28 10:24:22 +0800

    test(iam): optimize the acceptance case design for provider protocols datasource

--- huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_provider_protocols_test.go
+++ huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_provider_protocols_test.go
@@ -2,6 +2,7 @@ package iam
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -9,9 +10,16 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccDataSourceIamIdentityProviderProtocols_basic(t *testing.T) {
-	dataSourceName := "data.huaweicloud_identity_provider_protocols.test"
-	dc := acceptance.InitDataSourceCheck(dataSourceName)
+func TestAccDataSourceProviderProtocols_basic(t *testing.T) {
+	var (
+		name = acceptance.RandomAccResourceName()
+
+		dcName = "data.huaweicloud_identity_provider_protocols.test"
+		dc     = acceptance.InitDataSourceCheck(dcName)
+
+		dcNameByProtocolId = "data.huaweicloud_identity_provider_protocols.filter_by_protocol_id"
+		dcByProtocolId     = acceptance.InitDataSourceCheck(dcNameByProtocolId)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
@@ -20,47 +28,61 @@ func TestAccDataSourceIamIdentityProviderProtocols_basic(t *testing.T) {
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testDataSourceIamIdentityProviderProtocols(acceptance.RandomAccResourceName()),
+				Config: testAccDataSourceProviderProtocolsBasicStep1(name),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "protocols.#", "1"),
+					resource.TestMatchResourceAttr(dcName, "protocols.#", regexp.MustCompile(`^[1-9]([0-9]*)?$`)),
+					resource.TestCheckResourceAttrSet(dcName, "protocols.0.id"),
 				),
 			},
 			{
-				Config: testDataSourceIamIdentityProviderProtocolsWithProtocolId(acceptance.RandomAccResourceName()),
+				Config: testAccDataSourceProviderProtocolsBasicStep2(name),
 				Check: resource.ComposeTestCheckFunc(
-					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "protocols.#", "1"),
-					resource.TestCheckResourceAttr(dataSourceName, "protocols.0.id", "saml"),
+					dcByProtocolId.CheckResourceExists(),
+					resource.TestMatchResourceAttr(dcNameByProtocolId, "protocols.#", regexp.MustCompile(`^[1-9]([0-9]*)?$`)),
+					resource.TestCheckOutput("is_protocol_id_filter_useful", "true"),
 				),
 			},
 		},
 	})
 }
 
-func testDataSourceIamIdentityProviderProtocols(name string) string {
+func testAccDataSourceProviderProtocolsBase(name string) string {
 	return fmt.Sprintf(`
-resource "huaweicloud_identity_provider" "provider1" {
-  name     = "%s"
-  protocol = "oidc"
-}
-
-data "huaweicloud_identity_provider_protocols" "test" {
-  provider_id = huaweicloud_identity_provider.provider1.id
-}
-`, name)
-}
-
-func testDataSourceIamIdentityProviderProtocolsWithProtocolId(name string) string {
-	return fmt.Sprintf(`
-resource "huaweicloud_identity_provider" "provider1" {
-  name     = "%s"
+resource "huaweicloud_identity_provider" "test" {
+  name     = "%[1]s"
   protocol = "saml"
 }
+`, name)
+}
+
+func testAccDataSourceProviderProtocolsBasicStep1(name string) string {
+	return fmt.Sprintf(`
+%[1]s
 
 data "huaweicloud_identity_provider_protocols" "test" {
-  provider_id = huaweicloud_identity_provider.provider1.id
+  provider_id = huaweicloud_identity_provider.test.id
+}
+`, testAccDataSourceProviderProtocolsBase(name))
+}
+
+func testAccDataSourceProviderProtocolsBasicStep2(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+data "huaweicloud_identity_provider_protocols" "filter_by_protocol_id" {
+  provider_id = huaweicloud_identity_provider.test.id
   protocol_id = "saml"
 }
-`, name)
+
+locals {
+  protocol_id_filter_result = [
+    for o in data.huaweicloud_identity_provider_protocols.filter_by_protocol_id.protocols : o.id == "saml"
+  ]
+}
+
+output "is_protocol_id_filter_useful" {
+  value = length(local.protocol_id_filter_result) >= 1 && alltrue(local.protocol_id_filter_result)
+}
+`, testAccDataSourceProviderProtocolsBase(name))
 }
