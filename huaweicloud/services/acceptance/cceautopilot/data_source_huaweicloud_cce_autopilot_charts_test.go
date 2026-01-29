bd1a496a21722701a18d1b1016e9a5e5a58e5da2
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2026-01-29 19:31:44 +0800

    fix(CCE): cce autopilot charts fix test and doc (#8928)

--- huaweicloud/services/acceptance/cceautopilot/data_source_huaweicloud_cce_autopilot_charts_test.go
+++ huaweicloud/services/acceptance/cceautopilot/data_source_huaweicloud_cce_autopilot_charts_test.go
@@ -1,6 +1,7 @@
 package cceautopilot
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -8,31 +9,45 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccDataSourceCceAutopilotCharts_basic(t *testing.T) {
+func TestAccDataSourceCharts_basic(t *testing.T) {
 	dataSource := "data.huaweicloud_cce_autopilot_charts.test"
 	dc := acceptance.InitDataSourceCheck(dataSource)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckCceChartPath(t)
 		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testDataSourceCceAutopilotCharts_basic,
+				Config: testAccDataSourceCharts_basic(),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckOutput("is_results_not_empty", "true"),
+					resource.TestCheckResourceAttrSet(dataSource, "charts.#"),
+					resource.TestCheckResourceAttrSet(dataSource, "charts.0.id"),
+					resource.TestCheckResourceAttrSet(dataSource, "charts.0.name"),
+					resource.TestCheckResourceAttrSet(dataSource, "charts.0.values"),
+					resource.TestCheckResourceAttrSet(dataSource, "charts.0.instruction"),
+					resource.TestCheckResourceAttrSet(dataSource, "charts.0.version"),
+					resource.TestCheckResourceAttrSet(dataSource, "charts.0.description"),
+					resource.TestCheckResourceAttrSet(dataSource, "charts.0.source"),
+					resource.TestCheckResourceAttrSet(dataSource, "charts.0.icon_url"),
+					resource.TestCheckResourceAttrSet(dataSource, "charts.0.chart_url"),
+					resource.TestCheckResourceAttrSet(dataSource, "charts.0.create_at"),
+					resource.TestCheckResourceAttrSet(dataSource, "charts.0.update_at"),
 				),
 			},
 		},
 	})
 }
 
-const testDataSourceCceAutopilotCharts_basic = `
-data "huaweicloud_cce_autopilot_charts" "test" {}
+func testAccDataSourceCharts_basic() string {
+	return fmt.Sprintf(`
+%s
 
-output "is_results_not_empty" {
-  value = length(data.huaweicloud_cce_autopilot_charts.test.charts) > 0
+data "huaweicloud_cce_autopilot_charts" "test" {
+  depends_on = [huaweicloud_cce_autopilot_chart.test]
 }
-`
+`, testAccAutopilotChart_basic())
+}
