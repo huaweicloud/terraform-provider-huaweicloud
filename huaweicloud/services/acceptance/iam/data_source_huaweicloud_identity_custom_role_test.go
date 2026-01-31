dd25704617708725b5ec2e4f175ee38d1ece8b81
GreyWorld <946596029@qq.com>
2026-01-31 11:48:21 +0800

    test(iam): optimize the acceptance case design for custom role datasource (#8922)
    Co-authored-by: greyworld <huawei@ecs-yanghongyu>

--- huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_custom_role_test.go
+++ huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_custom_role_test.go
@@ -9,33 +9,42 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccIdentityCustomRoleDataSource_basic(t *testing.T) {
-	dataSourceName := "data.huaweicloud_identity_custom_role.role_1"
-	rName := acceptance.RandomAccResourceName()
-	dc := acceptance.InitDataSourceCheck(dataSourceName)
+func TestAccDataCustomRole_basic(t *testing.T) {
+	var (
+		name = acceptance.RandomAccResourceName()
+
+		byName   = "data.huaweicloud_identity_custom_role.filter_by_name"
+		dcByName = acceptance.InitDataSourceCheck(byName)
+
+		byId   = "data.huaweicloud_identity_custom_role.filter_by_id"
+		dcById = acceptance.InitDataSourceCheck(byId)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acceptance.TestAccPreCheck(t)
 			acceptance.TestAccPreCheckAdminOnly(t)
+			acceptance.TestAccPrecheckDomainId(t)
 		},
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccIdentityCustomRoleDataSource_basic(rName),
+				Config: testAccDataCustomRole_basic(name),
 				Check: resource.ComposeTestCheckFunc(
-					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "name", rName),
+					dcByName.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(byName, "catalog"),
+					dcById.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(byId, "catalog"),
 				),
 			},
 		},
 	})
 }
 
-func testAccIdentityCustomRoleDataSource_basic(rName string) string {
+func testAccDataCustomRole_base(name string) string {
 	return fmt.Sprintf(`
-resource "huaweicloud_identity_role" test {
-  name        = "%s"
+resource "huaweicloud_identity_role" "test" {
+  name        = "%[1]s"
   description = "created by terraform"
   type        = "AX"
   policy      = <<EOF
@@ -55,13 +64,35 @@ resource "huaweicloud_identity_role" test {
 }
 EOF
 }
-
-data "huaweicloud_identity_custom_role" "role_1" {
-  name = huaweicloud_identity_role.test.name
-
-  depends_on = [
-	huaweicloud_identity_role.test
-  ]
+`, name)
 }
-`, rName)
+
+func testAccDataCustomRole_basic(name string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+# Filter by name
+locals {
+  name = huaweicloud_identity_role.test.name
+}
+
+data "huaweicloud_identity_custom_role" "filter_by_name" {
+  name = local.name
+
+  # Waiting for the role to be created.
+  depends_on = [huaweicloud_identity_role.test]
+}
+
+# Filter by ID
+locals {
+  id = huaweicloud_identity_role.test.id
+}
+
+data "huaweicloud_identity_custom_role" "filter_by_id" {
+  id = local.id
+
+  # Waiting for the role to be created.
+  depends_on = [huaweicloud_identity_role.test]
+}
+`, testAccDataCustomRole_base(name))
 }
