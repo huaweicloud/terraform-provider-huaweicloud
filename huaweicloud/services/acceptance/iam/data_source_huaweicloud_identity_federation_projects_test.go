fa882760dddc3bf92c78a2ee5e813979840fda4c
GreyWorld <946596029@qq.com>
2026-01-31 16:08:50 +0800

    test(iam): optimize the acceptance case design for federation projects datasource (#8960)
    Co-authored-by: greyworld <huawei@ecs-yanghongyu>

--- huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_federation_projects_test.go
+++ huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_federation_projects_test.go
@@ -2,6 +2,7 @@ package iam
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -9,12 +10,15 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccDataSourceIdentityFederationProjects_basic(t *testing.T) {
-	idpId := "YourIdpId"
-	protocolId := "YourProtocolId"
-	idToken := "YourIdToken"
-	dataSourceName := "data.huaweicloud_identity_federation_projects.test"
-	dc := acceptance.InitDataSourceCheck(dataSourceName)
+func TestAccDataFederationProjects_basic(t *testing.T) {
+	var (
+		idpId      = "YourIdpId"
+		protocolId = "YourProtocolId"
+		idToken    = "YourIdToken"
+
+		all = "data.huaweicloud_identity_federation_projects.all"
+		dc  = acceptance.InitDataSourceCheck(all)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
@@ -23,29 +27,29 @@ func TestAccDataSourceIdentityFederationProjects_basic(t *testing.T) {
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testTestDataSourceIdentityFederationProjects(idpId, protocolId, idToken),
+				Config: testAccDataFederationProjects_basic(idpId, protocolId, idToken),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttrSet(dataSourceName, "projects.#"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "projects.0.id"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "projects.0.name"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "projects.0.enabled"),
+					resource.TestMatchResourceAttr(all, "projects.#", regexp.MustCompile(`^[1-9]([0-9]*)?$`)),
+					resource.TestCheckResourceAttrSet(all, "projects.0.id"),
+					resource.TestCheckResourceAttrSet(all, "projects.0.name"),
+					resource.TestCheckResourceAttrSet(all, "projects.0.enabled"),
 				),
 			},
 		},
 	})
 }
 
-func testTestDataSourceIdentityFederationProjects(idpId, protocolId, idToken string) string {
+func testAccDataFederationProjects_basic(idpId, protocolId, idToken string) string {
 	return fmt.Sprintf(`
 resource "huaweicloud_identity_unscoped_token_with_id_token" "test" {
-idp_id      = "%[1]s"
-protocol_id = "%[2]s"
-id_token    = "%[3]s"
+  idp_id      = "%[1]s"
+  protocol_id = "%[2]s"
+  id_token    = "%[3]s"
 }
 
-data "huaweicloud_identity_federation_projects" "test" {
-federation_token = huaweicloud_identity_unscoped_token_with_id_token.test.token
+data "huaweicloud_identity_federation_projects" "all" {
+  federation_token = huaweicloud_identity_unscoped_token_with_id_token.test.token
 }
 `, idpId, protocolId, idToken)
 }
