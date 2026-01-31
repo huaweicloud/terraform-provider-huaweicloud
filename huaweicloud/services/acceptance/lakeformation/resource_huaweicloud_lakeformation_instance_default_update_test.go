8a34faa6c9b72edf2b676b67a7c23c696d21995c
GreyWorld <946596029@qq.com>
2026-01-31 17:17:06 +0800

    feat(lakeformation): add action resource for update default instance (#8980)
    Co-authored-by: greyworld <huawei@ecs-yanghongyu>

---
+++ huaweicloud/services/acceptance/lakeformation/resource_huaweicloud_lakeformation_instance_default_update_test.go
@@ -0,0 +1,36 @@
+package lakeformation
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccInstanceDefaultUpdate_basic(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckLakeFormationInstanceId(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		// This resource is a one-time action resource and there is no logic in the delete method.
+		// lintignore:AT001
+		CheckDestroy: nil,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccInstanceDefaultUpdate_basic(),
+			},
+		},
+	})
+}
+
+func testAccInstanceDefaultUpdate_basic() string {
+	return fmt.Sprintf(`
+resource "huaweicloud_lakeformation_instance_default_update" "test" {
+  instance_id = "%[1]s"
+}
+`, acceptance.HW_LAKE_FORMATION_INSTANCE_ID)
+}
