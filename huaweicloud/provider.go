140a8522428d67449e9318c560cdcd9f9da86a1d
allium <deerhang@163.com>
2026-02-02 10:41:02 +0800

    feat(ccm): new datasource to query private ca quota (#8974)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -767,6 +767,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_ccm_certificate_export":         ccm.DataSourceCertificateExport(),
 			"huaweicloud_ccm_private_cas":                ccm.DataSourcePrivateCas(),
 			"huaweicloud_ccm_private_ca_export":          ccm.DataSourcePrivateCaExport(),
+			"huaweicloud_ccm_private_ca_quota":           ccm.DataSourceCcmPrivateCaQuota(),
 			"huaweicloud_ccm_private_certificates":       ccm.DataSourcePrivateCertificates(),
 			"huaweicloud_ccm_private_certificate_export": ccm.DataSourcePrivateCertificateExport(),
 
