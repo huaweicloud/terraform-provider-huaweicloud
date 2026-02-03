ce03713bda377e4a68d0f4115411f64b8533d073
wuzhuanhong <wuzhuanhong1@outlook.com>
2026-02-03 20:16:16 +0800

    test(iam): optimize tests for v5 attached policies data sources (user/agency/group) (#8899)

--- huaweicloud/services/acceptance/iam/data_source_huaweicloud_identityv5_agency_attached_policies_test.go
+++ huaweicloud/services/acceptance/iam/data_source_huaweicloud_identityv5_agency_attached_policies_test.go
@@ -2,6 +2,7 @@ package iam
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -9,32 +10,67 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccDataSourceIdentityV5AgencyAttachedPolicies_basic(t *testing.T) {
-	dataSourceName := "data.huaweicloud_identityv5_agency_attached_policies.test"
-	rName := acceptance.RandomAccResourceName()
+// Please ensure that the user executing the acceptance test has 'admin' permission.
+func TestAccDataV5AgencyAttachedPolicies_basic(t *testing.T) {
+	var (
+		name = acceptance.RandomAccResourceName()
 
-	resource.Test(t, resource.TestCase{
+		all = "data.huaweicloud_identityv5_agency_attached_policies.test"
+		dc  = acceptance.InitDataSourceCheck(all)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceIdentityV5AgencyAttachedPolicies_basic(rName),
+				Config: testAccDataV5AgencyAttachedPolicies_basic(name),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(dataSourceName, "agency_id"),
-					resource.TestCheckResourceAttr(dataSourceName, "attached_policies.0.policy_id", "NATReadOnlyPolicy"),
-					resource.TestCheckResourceAttr(dataSourceName, "attached_policies.0.policy_name", "NATReadOnlyPolicy"),
+					dc.CheckResourceExists(),
+					resource.TestMatchResourceAttr(all, "attached_policies.#", regexp.MustCompile(`^[1-9]([0-9]*)?$`)),
+					resource.TestCheckResourceAttrSet(all, "attached_policies.0.policy_id"),
+					resource.TestCheckResourceAttrSet(all, "attached_policies.0.policy_name"),
+					resource.TestCheckResourceAttrSet(all, "attached_policies.0.urn"),
+					resource.TestCheckResourceAttrSet(all, "attached_policies.0.attached_at"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceIdentityV5AgencyAttachedPolicies_basic(agencyName string) string {
+func testAccDataV5AgencyAttachedPolicies_base(name string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_identity_trust_agency" "test" {
+  name         = "%[1]s"
+  policy_names = ["NATReadOnlyPolicy"]
+  trust_policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = [
+            "sts:agencies:assume",
+          ]
+          Effect = "Allow"
+          Principal = {
+            Service = [
+              "service.OBS",
+            ]
+          }
+        },
+      ]
+      Version = "5.0"
+    }
+  )
+}
+`, name)
+}
+
+func testAccDataV5AgencyAttachedPolicies_basic(name string) string {
 	return fmt.Sprintf(`
 %[1]s
 
 data "huaweicloud_identityv5_agency_attached_policies" "test" {
   agency_id = huaweicloud_identity_trust_agency.test.id
 }
-`, testAccIdentityTrustAgency_basic(agencyName))
+`, testAccDataV5AgencyAttachedPolicies_base(name))
 }
