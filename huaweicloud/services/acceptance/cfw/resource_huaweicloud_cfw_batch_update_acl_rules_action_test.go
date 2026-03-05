91da1ff5a73bf304fdd72e099729c7fcd285aaf6
jinyangyang222 <jinyangyang222@outlook.com>
2026-03-05 11:32:06 +0800

    feat(cfw): support `cfw_batch_update_acl_rules_action` resource

---
+++ huaweicloud/services/acceptance/cfw/resource_huaweicloud_cfw_batch_update_acl_rules_action_test.go
@@ -0,0 +1,53 @@
+package cfw
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccBatchUpdateAclRulesAction_basic(t *testing.T) {
+	// lintignore:AT001
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			// This test case requires setting the firewall ID, ACL rule ID, and enterprise project ID for CFW,
+			// and ensuring that they are all under the same enterprise project.
+			acceptance.TestAccPreCheckCfw(t)
+			acceptance.TestAccPreCheckCfwAclRuleId(t)
+			acceptance.TestAccPreCheckEpsID(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testBatchUpdateAclRulesAction_basic(),
+			},
+		},
+	})
+}
+
+func testBatchUpdateAclRulesAction_base() string {
+	return fmt.Sprintf(`
+data "huaweicloud_cfw_firewalls" "test" {
+  fw_instance_id = "%s"
+}
+`, acceptance.HW_CFW_INSTANCE_ID)
+}
+
+func testBatchUpdateAclRulesAction_basic() string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "huaweicloud_cfw_batch_update_acl_rules_action" "test" {
+  object_id             = data.huaweicloud_cfw_firewalls.test.records[0].protect_objects[0].object_id
+  action                = "disable"
+  rule_ids              = ["%[2]s"]
+  fw_instance_id        = "%[3]s"
+  enterprise_project_id = "%[4]s"
+}
+`, testBatchUpdateAclRulesAction_base(), acceptance.HW_CFW_ACL_RULE_ID, acceptance.HW_CFW_INSTANCE_ID,
+		acceptance.HW_ENTERPRISE_PROJECT_ID_TEST)
+}
