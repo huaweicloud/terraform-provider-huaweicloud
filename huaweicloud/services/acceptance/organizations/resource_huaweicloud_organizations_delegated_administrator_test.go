bc0d50a597ebbbcd9714c09fc87acdca7a78846e
wuzhuanhong <wuzhuanhong1@outlook.com>
2026-03-10 10:08:56 +0800

    refactor(organizations): optimize code and docs for delegated administrator (#9110)

--- huaweicloud/services/acceptance/organizations/resource_huaweicloud_organizations_delegated_administrator_test.go
+++ huaweicloud/services/acceptance/organizations/resource_huaweicloud_organizations_delegated_administrator_test.go
@@ -1,75 +1,25 @@
 package organizations
 
 import (
-	"encoding/json"
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
-	"github.com/chnsz/golangsdk"
-	"github.com/chnsz/golangsdk/pagination"
-
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/organizations"
 )
 
 func getDelegatedAdministratorResourceFunc(cfg *config.Config, state *terraform.ResourceState) (interface{}, error) {
-	region := acceptance.HW_REGION_NAME
-	// getDelegatedAdministrator: Query Organizations delegated administrator
-	var (
-		getDelegatedAdministratorHttpUrl = "v1/organizations/delegated-administrators"
-		getDelegatedAdministratorProduct = "organizations"
-	)
-	getDelegatedAdministratorClient, err := cfg.NewServiceClient(getDelegatedAdministratorProduct, region)
+	client, err := cfg.NewServiceClient("organizations", acceptance.HW_REGION_NAME)
 	if err != nil {
 		return nil, fmt.Errorf("error creating Organizations client: %s", err)
 	}
 
-	parts := strings.Split(state.Primary.ID, "/")
-	if len(parts) != 2 {
-		return nil, fmt.Errorf("invalid id format, must be <account_id>/<service_principal>")
-	}
-	accountID := parts[0]
-	servicePrincipal := parts[1]
-
-	getDelegatedAdministratorPath := getDelegatedAdministratorClient.Endpoint + getDelegatedAdministratorHttpUrl
-	getDelegatedAdministratorQueryParams := buildGetDelegatedAdministratorQueryParams(servicePrincipal)
-	getDelegatedAdministratorPath += getDelegatedAdministratorQueryParams
-
-	getDelegatedAdministratorResp, err := pagination.ListAllItems(
-		getDelegatedAdministratorClient,
-		"marker",
-		getDelegatedAdministratorPath,
-		&pagination.QueryOpts{MarkerField: "account_id"})
-	if err != nil {
-		return nil, fmt.Errorf("error retrieving Organizations delegated administrator: %s", err)
-	}
-
-	getDelegatedAdministratorRespJson, err := json.Marshal(getDelegatedAdministratorResp)
-	if err != nil {
-		return nil, fmt.Errorf("error retrieving Organizations delegated administrator: %s", err)
-	}
-	var getDelegatedAdministratorRespBody interface{}
-	err = json.Unmarshal(getDelegatedAdministratorRespJson, &getDelegatedAdministratorRespBody)
-	if err != nil {
-		return nil, fmt.Errorf("error retrieving Organizations delegated administrator: %s", err)
-	}
-
-	delegatedAdministrator := utils.PathSearch(fmt.Sprintf("delegated_administrators|[?account_id=='%s']|[0]",
-		accountID), getDelegatedAdministratorRespBody, nil)
-	if delegatedAdministrator == nil {
-		return nil, golangsdk.ErrDefault404{}
-	}
-
-	return delegatedAdministrator, nil
-}
-
-func buildGetDelegatedAdministratorQueryParams(servicePrincipal string) string {
-	return fmt.Sprintf("?service_principal=%v", servicePrincipal)
+	return organizations.GetDelegatedAdministrator(client, state.Primary.Attributes["account_id"],
+		state.Primary.Attributes["service_principal"])
 }
 
 func TestAccDelegatedAdministrator_basic(t *testing.T) {
