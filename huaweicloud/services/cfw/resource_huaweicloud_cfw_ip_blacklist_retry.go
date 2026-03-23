a4205eba249403fbb1ac737450b8053d15b7d1c2
jinyangyang222 <jinyangyang222@outlook.com>
2026-03-23 17:51:08 +0800

    fix(cfw/ip_blacklist_retry): add forgotten parameter `enable_force_new` to schema

--- huaweicloud/services/cfw/resource_huaweicloud_cfw_ip_blacklist_retry.go
+++ huaweicloud/services/cfw/resource_huaweicloud_cfw_ip_blacklist_retry.go
@@ -7,10 +7,12 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 
 	"github.com/chnsz/golangsdk"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
 var ipBlacklistRetryNonUpdatableParams = []string{"fw_instance_id", "name"}
@@ -40,6 +42,12 @@ func ResourceIpBlacklistRetry() *schema.Resource {
 			"name": {
 				Type:     schema.TypeString,
 				Required: true,
+			},
+			"enable_force_new": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice([]string{"true", "false"}, false),
+				Description:  utils.SchemaDesc("", utils.SchemaDescInput{Internal: true}),
 			},
 		},
 	}
