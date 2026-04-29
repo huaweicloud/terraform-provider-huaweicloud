17814707df7d2d5f9e98ca64424e7e4ed729932a
wangyan <dursinlabiaus@hotmail.com>
2026-04-29 11:14:04 +0800

    feat(eip): add vpc eip quotas datasource

--- huaweicloud/services/acceptance/eip/data_source_huaweicloud_vpc_eip_quotas_test.go
+++ huaweicloud/services/acceptance/eip/data_source_huaweicloud_vpc_eip_quotas_test.go
@@ -22,7 +22,6 @@ func TestAccDataSourceVpcEipQuotas_basic(t *testing.T) {
 				Config: testAccDataSourceVpcEipQuotas_basic(),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					// 检查核心配额字段是否存在
 					resource.TestCheckResourceAttrSet(dataSource, "quotas.0.type"),
 					resource.TestCheckResourceAttrSet(dataSource, "quotas.0.used"),
 					resource.TestCheckResourceAttrSet(dataSource, "quotas.0.quota"),
@@ -32,7 +31,6 @@ func TestAccDataSourceVpcEipQuotas_basic(t *testing.T) {
 	})
 }
 
-// 基础测试配置：查询所有EIP相关配额
 func testAccDataSourceVpcEipQuotas_basic() string {
 	return `
 data "huaweicloud_vpc_eip_quotas" "test" {}
