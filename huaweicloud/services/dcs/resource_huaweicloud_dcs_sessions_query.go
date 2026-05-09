cdb6d00d7ded494e1ad16c738aad91296692c305
zhujingbo <95921153+personSE@users.noreply.github.com>
2026-05-09 14:16:47 +0800

    feat(dcs): add source to sessions query (#9501)

---
+++ huaweicloud/services/dcs/resource_huaweicloud_dcs_sessions_query.go
@@ -0,0 +1,124 @@
+package dcs
+
+import (
+	"context"
+	"strings"
+
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+var dcsSessionsQueryNonUpdatableParams = []string{"instance_id", "node_id", "clean_cache"}
+
+// @API DCS POST /v2/{project_id}/instances/{instance_id}/clients
+func ResourceDcsSessionsQuery() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceDcsSessionsQueryCreate,
+		ReadContext:   resourceDcsSessionsQueryRead,
+		UpdateContext: resourceDcsSessionsQueryUpdate,
+		DeleteContext: resourceDcsSessionsQueryDelete,
+
+		CustomizeDiff: config.FlexibleForceNew(dcsSessionsQueryNonUpdatableParams),
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"instance_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"node_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"clean_cache": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"enable_force_new": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice([]string{"true", "false"}, false),
+				Description:  utils.SchemaDesc("", utils.SchemaDescInput{Internal: true}),
+			},
+		},
+	}
+}
+
+func resourceDcsSessionsQueryCreate(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		httpUrl = "v2/{project_id}/instances/{instance_id}/clients"
+		product = "dcs"
+	)
+
+	client, err := cfg.NewServiceClient(product, region)
+	if err != nil {
+		return diag.Errorf("error creating DCS client: %s", err)
+	}
+
+	instanceID := d.Get("instance_id").(string)
+
+	createPath := client.Endpoint + httpUrl
+	createPath = strings.ReplaceAll(createPath, "{project_id}", client.ProjectID)
+	createPath = strings.ReplaceAll(createPath, "{instance_id}", instanceID)
+
+	reqBody := make(map[string]interface{})
+	if v, ok := d.GetOk("node_id"); ok {
+		reqBody["node_id"] = v.(string)
+	}
+	if v, ok := d.GetOk("clean_cache"); ok {
+		reqBody["clean_cache"] = v.(bool)
+	}
+
+	createOpt := golangsdk.RequestOpts{
+		JSONBody: reqBody,
+		MoreHeaders: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}
+
+	_, err = client.Request("POST", createPath, &createOpt)
+	if err != nil {
+		return diag.Errorf("error creating DCS sessions query resource: %s", err)
+	}
+
+	generateUUID, err := uuid.GenerateUUID()
+	if err != nil {
+		return diag.Errorf("unable to generate ID: %s", err)
+	}
+
+	d.SetId(generateUUID)
+
+	return nil
+}
+
+func resourceDcsSessionsQueryRead(_ context.Context, _ *schema.ResourceData, _ interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceDcsSessionsQueryUpdate(_ context.Context, _ *schema.ResourceData, _ interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceDcsSessionsQueryDelete(_ context.Context, _ *schema.ResourceData, _ interface{}) diag.Diagnostics {
+	errorMsg := "Deleting DCS sessions query resource is not supported. The resource is only removed from the state"
+	return diag.Diagnostics{
+		diag.Diagnostic{
+			Severity: diag.Warning,
+			Summary:  errorMsg,
+		},
+	}
+}
