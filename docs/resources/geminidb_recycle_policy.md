de4b9647f76cb6cddca0c48e6eb31f8703c06bcd
qigaoneng <qigaoneng@outlook.com>
2026-05-14 20:25:00 +0800

    feat(geminidb): add resource geminidb recycle policy (#9580)

---
+++ docs/resources/geminidb_recycle_policy.md
@@ -0,0 +1,43 @@
+---
+subcategory: "GeminiDB"
+layout: "huaweicloud"
+page_title: "HuaweiCloud: huaweicloud_geminidb_recycle_policy"
+description: |-
+  Manages a GeminiDB recycle policy resource within HuaweiCloud.
+---
+
+# huaweicloud_geminidb_recycle_policy
+
+Manages a GeminiDB recycle policy resource within HuaweiCloud.
+
+## Example Usage
+
+```hcl
+resource "huaweicloud_geminidb_recycle_policy" "test" {
+  retention_period_in_days = 7
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region in which to create the resource.
+  If omitted, the provider-level region will be used. Chenge this parameter will create a new resource.
+
+* `retention_period_in_days` - (Required, Int) Specifies the retention period for deleted instances in the recycle bin.
+  The value ranges from 1 to 7 days. The default value is 7 days.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The resource ID. The value is the project ID of the region.
+
+## Import
+
+The GeminiDB recycle policy can be imported using the `id`, e.g.
+
+```
+$ terraform import huaweicloud_gemini_db_recycle_policy.test <id>
+```
---
+++ huaweicloud/services/geminidb/resource_huaweicloud_geminidb_recycle_policy.go
@@ -0,0 +1,135 @@
+package geminidb
+
+import (
+	"context"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/common"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+// @API GaussDBforNoSQL PUT /v3/{project_id}/instances/recycle-policy
+// @API GaussDBforNoSQL GET /v3/{project_id}/instances/recycle-policy
+func ResourceGeminiDBRecyclePolicy() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceGeminiDBRecyclePolicyCreateOrUpdate,
+		UpdateContext: resourceGeminiDBRecyclePolicyCreateOrUpdate,
+		ReadContext:   resourceGeminiDBRecyclePolicyRead,
+		DeleteContext: resourceGeminiDBRecyclePolicyDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+			},
+			"retention_period_in_days": {
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceGeminiDBRecyclePolicyCreateOrUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		httpUrl = "v3/{project_id}/instances/recycle-policy"
+		product = "geminidb"
+	)
+
+	client, err := cfg.NewServiceClient(product, region)
+	if err != nil {
+		return diag.Errorf("error creating GeminiDB client: %s", err)
+	}
+
+	updatePath := client.Endpoint + httpUrl
+	updatePath = strings.ReplaceAll(updatePath, "{project_id}", client.ProjectID)
+
+	updateOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		MoreHeaders:      map[string]string{"Content-Type": "application/json"},
+		JSONBody:         buildCreateGeminiDBRecyclingPolicyBodyParams(d),
+	}
+
+	_, err = client.Request("PUT", updatePath, &updateOpt)
+	if err != nil {
+		return diag.Errorf("error updating GeminiDB recycle policy: %s", err)
+	}
+	d.SetId(client.ProjectID)
+
+	return resourceGeminiDBRecyclePolicyRead(ctx, d, meta)
+}
+
+func buildCreateGeminiDBRecyclingPolicyBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"recycle_policy": map[string]interface{}{
+			"retention_period_in_days": d.Get("retention_period_in_days"),
+		},
+	}
+	return bodyParams
+}
+
+func resourceGeminiDBRecyclePolicyRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var (
+		httpUrl = "v3/{project_id}/instances/recycle-policy"
+		product = "geminidb"
+	)
+
+	client, err := cfg.NewServiceClient(product, region)
+	if err != nil {
+		return diag.Errorf("error creating GeminiDB client: %s", err)
+	}
+
+	getPath := client.Endpoint + httpUrl
+	getPath = strings.ReplaceAll(getPath, "{project_id}", client.ProjectID)
+
+	getOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		MoreHeaders:      map[string]string{"Content-Type": "application/json"},
+	}
+
+	getResp, err := client.Request("GET", getPath, &getOpt)
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "error retrieving GeminiDB recycle policy")
+	}
+
+	respBody, err := utils.FlattenResponse(getResp)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	mErr := multierror.Append(
+		d.Set("region", region),
+		d.Set("retention_period_in_days", utils.PathSearch("recycle_policy.retention_period_in_days", respBody, nil)),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func resourceGeminiDBRecyclePolicyDelete(_ context.Context, _ *schema.ResourceData, _ interface{}) diag.Diagnostics {
+	errorMsg := "Deleting recycle policy is not supported. The recycle policy is only removed from the state," +
+		" but it remains in the cloud. And the recycle policy doesn't return to the original state."
+	return diag.Diagnostics{
+		diag.Diagnostic{
+			Severity: diag.Warning,
+			Summary:  errorMsg,
+		},
+	}
+}
