27611b73ae8539d4a988bb45964f04f1cda72bf9
Lance <Overlord52259@gmail.com>
2026-05-15 14:24:53 +0800

    feat(modelarts): supports new attributes for image element (#9586)

--- huaweicloud/services/acceptance/modelarts/data_source_huaweicloud_modelarts_notebook_images_test.go
+++ huaweicloud/services/acceptance/modelarts/data_source_huaweicloud_modelarts_notebook_images_test.go
@@ -1,7 +1,7 @@
 package modelarts
 
 import (
-	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -10,47 +10,129 @@ import (
 )
 
 func TestAccNotebookImages_basic(t *testing.T) {
-	dataSourceName := "data.huaweicloud_modelarts_notebook_images.test"
+	var (
+		all = "data.huaweicloud_modelarts_notebook_images.all"
+		dc  = acceptance.InitDataSourceCheck(all)
 
-	dc := acceptance.InitDataSourceCheck(dataSourceName)
+		byName   = "data.huaweicloud_modelarts_notebook_images.filter_by_name"
+		dcByName = acceptance.InitDataSourceCheck(byName)
+
+		byType   = "data.huaweicloud_modelarts_notebook_images.filter_by_type"
+		dcByType = acceptance.InitDataSourceCheck(byType)
+
+		byWorkspaceId   = "data.huaweicloud_modelarts_notebook_images.filter_by_workspace_id"
+		dcByWorkspaceId = acceptance.InitDataSourceCheck(byWorkspaceId)
+
+		byCpuArch   = "data.huaweicloud_modelarts_notebook_images.filter_by_cpu_arch"
+		dcByCpuArch = acceptance.InitDataSourceCheck(byCpuArch)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceImages_basic("BUILD_IN", "x86_64"),
+				Config: testAccDataSourceImages_basic,
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "images.0.type", "BUILD_IN"),
-					resource.TestCheckResourceAttr(dataSourceName, "images.0.cpu_arch", "x86_64"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "images.0.id"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "images.0.name"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "images.0.swr_path"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "images.0.description"),
-				),
-			},
-			{
-				Config: testAccDataSourceImages_basic("BUILD_IN", "aarch64"),
-				Check: resource.ComposeTestCheckFunc(
-					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "images.0.type", "BUILD_IN"),
-					resource.TestCheckResourceAttr(dataSourceName, "images.0.cpu_arch", "aarch64"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "images.0.id"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "images.0.name"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "images.0.swr_path"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "images.0.description"),
+					resource.TestMatchResourceAttr(all, "images.#", regexp.MustCompile(`^[1-9]([0-9]*)?$`)),
+					resource.TestCheckResourceAttr(all, "images.0.type", "BUILD_IN"),
+					resource.TestCheckResourceAttr(all, "images.0.cpu_arch", "x86_64"),
+					resource.TestCheckResourceAttrSet(all, "images.0.id"),
+					resource.TestCheckResourceAttrSet(all, "images.0.name"),
+					resource.TestCheckResourceAttrSet(all, "images.0.swr_path"),
+					resource.TestCheckResourceAttrSet(all, "images.0.description"),
+					dcByName.CheckResourceExists(),
+					resource.TestCheckOutput("is_name_filter_useful", "true"),
+					dcByType.CheckResourceExists(),
+					resource.TestCheckOutput("is_type_filter_useful", "true"),
+					dcByWorkspaceId.CheckResourceExists(),
+					resource.TestCheckOutput("is_workspace_id_filter_useful", "true"),
+					dcByCpuArch.CheckResourceExists(),
+					resource.TestCheckOutput("is_cpu_arch_filter_useful", "true"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceImages_basic(imageType, cpuArch string) string {
-	return fmt.Sprintf(`
-data "huaweicloud_modelarts_notebook_images" "test" {
-  type     = "%s"
-  cpu_arch = "%s"
+const testAccDataSourceImages_basic string = `
+# Query all notebook images without any filter.
+data "huaweicloud_modelarts_notebook_images" "all" {}
+
+# Filter by parameter 'name'.
+locals {
+  image_name = data.huaweicloud_modelarts_notebook_images.all.images[0].name
 }
-`, imageType, cpuArch)
+
+data "huaweicloud_modelarts_notebook_images" "filter_by_name" {
+  name = local.image_name
 }
+
+locals {
+  filter_by_name_result = [
+    for v in data.huaweicloud_modelarts_notebook_images.filter_by_name.images[*].name : v == local.image_name
+  ]
+}
+
+output "is_name_filter_useful" {
+  value = length(local.filter_by_name_result) > 0 && alltrue(local.filter_by_name_result)
+}
+
+# Filter by parameter 'type'.
+locals {
+  image_type = data.huaweicloud_modelarts_notebook_images.all.images[0].type
+}
+
+data "huaweicloud_modelarts_notebook_images" "filter_by_type" {
+  type = local.image_type
+}
+
+locals {
+  filter_by_type_result = [
+    for v in data.huaweicloud_modelarts_notebook_images.filter_by_type.images[*].type : v == local.image_type
+  ]
+}
+
+output "is_type_filter_useful" {
+  value = length(local.filter_by_type_result) > 0 && alltrue(local.filter_by_type_result)
+}
+
+# Filter by parameter 'workspace_id'.
+locals {
+  image_workspace_id = data.huaweicloud_modelarts_notebook_images.all.images[0].workspace_id
+}
+
+data "huaweicloud_modelarts_notebook_images" "filter_by_workspace_id" {
+  workspace_id = local.image_workspace_id
+}
+
+locals {
+  filter_by_workspace_id_result = [
+    for v in data.huaweicloud_modelarts_notebook_images.filter_by_workspace_id.images[*].workspace_id : v == local.image_workspace_id
+  ]
+}
+
+output "is_workspace_id_filter_useful" {
+  value = length(local.filter_by_workspace_id_result) > 0 && alltrue(local.filter_by_workspace_id_result)
+}
+
+# Filter by parameter 'cpu_arch'.
+locals {
+  image_cpu_arch = data.huaweicloud_modelarts_notebook_images.all.images[0].cpu_arch
+}
+
+data "huaweicloud_modelarts_notebook_images" "filter_by_cpu_arch" {
+  cpu_arch = local.image_cpu_arch
+}
+
+locals {
+  filter_by_cpu_arch_result = [
+    for v in data.huaweicloud_modelarts_notebook_images.filter_by_cpu_arch.images[*].cpu_arch : v == local.image_cpu_arch
+  ]
+}
+
+output "is_cpu_arch_filter_useful" {
+  value = length(local.filter_by_cpu_arch_result) > 0 && alltrue(local.filter_by_cpu_arch_result)
+}
+`
