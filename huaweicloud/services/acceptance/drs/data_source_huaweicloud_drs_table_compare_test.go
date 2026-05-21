7db57c174213f824a918b3b957f5c9c149b5ef5b
allium <deerhang@163.com>
2026-05-21 14:56:57 +0800

    feat(drs): new datasource to query table compare (#9649)

---
+++ huaweicloud/services/acceptance/drs/data_source_huaweicloud_drs_table_compare_test.go
@@ -0,0 +1,42 @@
+package drs
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceDrsTableCompare_basic(t *testing.T) {
+	var (
+		dataSourceName = "data.huaweicloud_drs_table_compare.test"
+		dc             = acceptance.InitDataSourceCheck(dataSourceName)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckDrsJobId(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceDrsTableCompare_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSourceName, "compare_jobs.#"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceDrsTableCompare_basic() string {
+	return fmt.Sprintf(`
+data "huaweicloud_drs_table_compare" "test" {
+  job_id = "%s"
+}
+`, acceptance.HW_DRS_JOB_ID)
+}
