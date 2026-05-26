f71c00946c3d3406ae57c2aa263f8d74ce934382
Lance52259 <Overlord52259@gmail.com>
2026-05-26 16:19:15 +0800

    feat(dataarts): supports new data source to query architecture directories

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1089,6 +1089,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_dataarts_architecture_business_metrics":                 dataarts.DataSourceArchitectureBusinessMetrics(),
 			"huaweicloud_dataarts_architecture_data_standard_template_customs":   dataarts.DataSourceArchitectureDataStandardTemplateCustoms(),
 			"huaweicloud_dataarts_architecture_data_standard_template_optionals": dataarts.DataSourceArchitectureDataStandardTemplateOptionals(),
+			"huaweicloud_dataarts_architecture_directories":                      dataarts.DataSourceArchitectureDirectories(),
 			"huaweicloud_dataarts_architecture_models":                           dataarts.DataSourceArchitectureModels(),
 			"huaweicloud_dataarts_architecture_model_statistic":                  dataarts.DataSourceArchitectureModelStatistic(),
 			"huaweicloud_dataarts_architecture_processes":                        dataarts.DataSourceArchitectureProcesses(),
---
+++ huaweicloud/services/acceptance/dataarts/data_source_huaweicloud_dataarts_architecture_directories_test.go
@@ -0,0 +1,95 @@
+package dataarts
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataArchitectureDirectories_basic(t *testing.T) {
+	var (
+		name = acceptance.RandomAccResourceName()
+
+		all = "data.huaweicloud_dataarts_architecture_directories.all"
+		dc  = acceptance.InitDataSourceCheck(all)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckDataArtsWorkSpaceID(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccDataArchitectureDirectories_nonExistentWorkspace(),
+				ExpectError: regexp.MustCompile("error querying DataArts Architecture directories"),
+			},
+			{
+				Config: testAccDataArchitectureDirectories_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					// Query all directories
+					dc.CheckResourceExists(),
+					resource.TestMatchResourceAttr(all, "directories.#", regexp.MustCompile(`^[1-9]([0-9]*)?$`)),
+					resource.TestCheckOutput("is_all_directories_queried", "true"),
+					resource.TestCheckResourceAttrSet(all, "directories.0.id"),
+					resource.TestCheckResourceAttrSet(all, "directories.0.name"),
+					resource.TestCheckResourceAttr(all, "directories.0.type", "STANDARD_ELEMENT"),
+					resource.TestCheckResourceAttrSet(all, "directories.0.qualified_name"),
+					resource.TestCheckResourceAttrSet(all, "directories.0.created_by"),
+					resource.TestCheckResourceAttrSet(all, "directories.0.updated_by"),
+					resource.TestMatchResourceAttr(all, "directories.0.created_at",
+						regexp.MustCompile(`^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}?(Z|([+-]\d{2}:\d{2}))$`)),
+					resource.TestMatchResourceAttr(all, "directories.0.updated_at",
+						regexp.MustCompile(`^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}?(Z|([+-]\d{2}:\d{2}))$`)),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataArchitectureDirectories_nonExistentWorkspace() string {
+	randUUID, _ := uuid.GenerateUUID()
+
+	return fmt.Sprintf(`
+data "huaweicloud_dataarts_architecture_directories" "test" {
+  workspace_id = "%[1]s"
+  type         = "STANDARD_ELEMENT"
+}
+`, randUUID)
+}
+
+func testAccDataArchitectureDirectories_basic(name string) string {
+	return fmt.Sprintf(`
+resource "huaweicloud_dataarts_architecture_directory" "test" {
+  count = 2
+
+  workspace_id = "%[1]s"
+  name         = format("%[2]s_standard_%%d", count.index)
+  type         = "STANDARD_ELEMENT"
+}
+
+data "huaweicloud_dataarts_architecture_directories" "all" {
+  depends_on = [huaweicloud_dataarts_architecture_directory.test]
+
+  workspace_id = "%[1]s"
+  type         = "STANDARD_ELEMENT"
+}
+
+locals {
+  all_directories_queried = [
+    for v in huaweicloud_dataarts_architecture_directory.test[*].id :
+      contains(data.huaweicloud_dataarts_architecture_directories.all.directories[*].id, v)
+  ]
+}
+
+output "is_all_directories_queried" {
+  value = length(local.all_directories_queried) > 0 && alltrue(local.all_directories_queried)
+}
+`, acceptance.HW_DATAARTS_WORKSPACE_ID, name)
+}
