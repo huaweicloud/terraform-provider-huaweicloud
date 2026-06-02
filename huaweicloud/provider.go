2ee83a886a61a77b266af5a2ae81228e68c3f89b
jinyangyang222 <jinyangyang222@outlook.com>
2026-06-02 09:11:34 +0800

    feat(drs): support `drs_update_data_progress_rules` resource

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -3631,6 +3631,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_drs_batch_pause_task":               drs.ResourceBatchPauseTask(),
 			"huaweicloud_drs_batch_retry_task":               drs.ResourceBatchRetryTask(),
 			"huaweicloud_drs_check_data_filter":              drs.ResourceDrsCheckDataFilter(),
+			"huaweicloud_drs_update_data_progress_rules":     drs.ResourceDrsUpdateDataProgressRules(),
 			"huaweicloud_drs_compare_job_cancel":             drs.ResourceCancelCompareJob(),
 			"huaweicloud_drs_compare_policy":                 drs.ResourceDrsComparePolicy(),
 			"huaweicloud_drs_download_batch_create_template": drs.ResourceDownloadBatchCreateTemplate(),
