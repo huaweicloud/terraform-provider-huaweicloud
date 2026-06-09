cd71b6b6566a1034d0a11b3a3d32ceed391244fd
personSE <714600523@qq.com>
2026-06-09 14:53:51 +0800

    feat(gaussdb): add data source to gaussdb instance metrics

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1576,6 +1576,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_gaussdb_sql_throttling_tasks":            gaussdb.DataSourceGaussDbSqlThrottlingTasks(),
 			"huaweicloud_gaussdb_plugins":                         gaussdb.DataSourceGaussDbPlugins(),
 			"huaweicloud_gaussdb_all_instances_metrics":           gaussdb.DataSourceGaussDbAllInstancesMetrics(),
+			"huaweicloud_gaussdb_instance_metrics":                gaussdb.DataSourceInstanceMetrics(),
 			"huaweicloud_gaussdb_asp_collection_results":          gaussdb.DataSourceGaussDbAspCollectionResults(),
 			"huaweicloud_gaussdb_wdr_snapshot_collection_results": gaussdb.DataSourceGaussDbWdrSnapshotCollectionResults(),
 			"huaweicloud_gaussdb_slow_sql_list":                   gaussdb.DataSourceGaussDbSlowSqlList(),
