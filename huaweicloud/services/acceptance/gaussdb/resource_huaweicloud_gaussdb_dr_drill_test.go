961dbb4a3c5b15868fd30f6f9d2f4a15266b38fa
houpeng80 <114376095+houpeng80@users.noreply.github.com>
2026-06-10 14:24:51 +0800

    feat(GaussDB): add gaussdb dr drill resource (#9874)

---
+++ huaweicloud/services/acceptance/gaussdb/resource_huaweicloud_gaussdb_dr_drill_test.go
@@ -0,0 +1,50 @@
+package gaussdb
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccGaussDbDrDrill_basic(t *testing.T) {
+	name := acceptance.RandomAccResourceName()
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      nil,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGaussDbDrDrill_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.huaweicloud_gaussdb_dr_relationships.test",
+						"relations.0.status", "simulation"),
+				),
+			},
+		},
+	})
+}
+
+func testAccGaussDbDrDrill_basic(name string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "huaweicloud_gaussdb_dr_drill" "test" {
+  depends_on = [huaweicloud_gaussdb_dr_relationship.test]
+
+  instance_id     = huaweicloud_gaussdb_instance.test[0].id
+  disaster_type   = "stream"
+  xlog_keep_ratio = 50
+}
+
+data "huaweicloud_gaussdb_dr_relationships" "test" {
+  depends_on = [huaweicloud_gaussdb_dr_drill.test]
+
+  instance_id = huaweicloud_gaussdb_instance.test[0].id
+}
+`, testGaussDbDrRelationship_basic(name))
+}
