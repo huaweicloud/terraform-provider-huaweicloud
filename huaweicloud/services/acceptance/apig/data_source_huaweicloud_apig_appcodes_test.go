d82f8caedcde64d15958f419fc35904522d23acf
Lance52259 <Overlord52259@gmail.com>
2026-06-11 15:22:00 +0800

    refactor(apig): using auto-gen style to rewrite appcode data source

--- huaweicloud/services/acceptance/apig/data_source_huaweicloud_apig_appcodes_test.go
+++ huaweicloud/services/acceptance/apig/data_source_huaweicloud_apig_appcodes_test.go
@@ -2,21 +2,24 @@ package apig
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
+	"github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
-	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance/common"
 )
 
-func TestAccDataSourceAppcodes_basic(t *testing.T) {
+func TestAccDataAppcodes_basic(t *testing.T) {
 	var (
-		rName = "data.huaweicloud_apig_appcodes.test"
-		dc    = acceptance.InitDataSourceCheck(rName)
+		rName = acceptance.RandomAccResourceName()
 
-		rNameNotFound = "data.huaweicloud_apig_appcodes.not_found"
-		dcNotFound    = acceptance.InitDataSourceCheck(rNameNotFound)
+		dataSource = "data.huaweicloud_apig_appcodes.test"
+		dc         = acceptance.InitDataSourceCheck(dataSource)
+
+		notFound   = "data.huaweicloud_apig_appcodes.not_found"
+		dcNotFound = acceptance.InitDataSourceCheck(notFound)
 	)
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -27,26 +30,59 @@ func TestAccDataSourceAppcodes_basic(t *testing.T) {
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceAppcodes_basic(),
+				Config:      testAccDataAppcodes_nonExistentInstance(),
+				ExpectError: regexp.MustCompile(`error querying APPCODEs`),
+			},
+			{
+				Config:      testAccDataAppcodes_nonExistentApplication(),
+				ExpectError: regexp.MustCompile(`App [0-9a-fA-F-]{36} does not exist`),
+			},
+			{
+				Config: testAccDataAppcodes_basic(rName),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(rName, "appcodes.#", "1"),
+					resource.TestCheckResourceAttr(dataSource, "instance_id", acceptance.HW_APIG_DEDICATED_INSTANCE_ID),
+					resource.TestCheckResourceAttrPair(dataSource, "application_id", "huaweicloud_apig_application.test.0", "id"),
+					resource.TestCheckResourceAttr(dataSource, "appcodes.#", "1"),
+					resource.TestCheckResourceAttrSet(dataSource, "appcodes.0.id"),
+					resource.TestCheckResourceAttrSet(dataSource, "appcodes.0.value"),
+					resource.TestCheckResourceAttrPair(dataSource, "appcodes.0.application_id", "huaweicloud_apig_application.test.0", "id"),
+					resource.TestMatchResourceAttr(dataSource, "appcodes.0.created_at",
+						regexp.MustCompile(`^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}?(Z|([+-]\d{2}:\d{2}))$`)),
 					dcNotFound.CheckResourceExists(),
-					resource.TestCheckResourceAttr(rNameNotFound, "appcodes.#", "0"),
+					resource.TestCheckResourceAttr(notFound, "appcodes.#", "0"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceAppcodes_basic_base() string {
-	name := acceptance.RandomAccResourceName()
+func testAccDataAppcodes_nonExistentInstance() string {
+	randomUUID, _ := uuid.GenerateUUID()
 
 	return fmt.Sprintf(`
-%[1]s
+data "huaweicloud_apig_appcodes" "test" {
+  instance_id    = "%[1]s"
+  application_id = "%[1]s"
+}
+`, randomUUID)
+}
 
+func testAccDataAppcodes_nonExistentApplication() string {
+	randomUUID, _ := uuid.GenerateUUID()
+
+	return fmt.Sprintf(`
+data "huaweicloud_apig_appcodes" "test" {
+  instance_id    = "%[1]s"
+  application_id = "%[2]s"
+}
+`, acceptance.HW_APIG_DEDICATED_INSTANCE_ID, randomUUID)
+}
+
+func testAccDataAppcodes_base(name string) string {
+	return fmt.Sprintf(`
 data "huaweicloud_apig_instances" "test" {
-  instance_id = "%[2]s"
+  instance_id = "%[1]s"
 }
 
 locals {
@@ -57,23 +93,23 @@ resource "huaweicloud_apig_application" "test" {
   count = 2
 
   instance_id = local.instance_id
-  name        = format("%[3]s_%%d", count.index)
+  name        = format("%[2]s_%%d", count.index)
 }
 
 resource "huaweicloud_apig_appcode" "test" {
   instance_id    = local.instance_id
   application_id = huaweicloud_apig_application.test[0].id
 }
-`, common.TestBaseNetwork(name), acceptance.HW_APIG_DEDICATED_INSTANCE_ID, name)
+`, acceptance.HW_APIG_DEDICATED_INSTANCE_ID, name)
 }
 
-func testAccDataSourceAppcodes_basic() string {
+func testAccDataAppcodes_basic(name string) string {
 	return fmt.Sprintf(`
 %s
 
 data "huaweicloud_apig_appcodes" "test" {
   depends_on = [
-    huaweicloud_apig_appcode.test
+    huaweicloud_apig_appcode.test,
   ]
 
   instance_id    = local.instance_id
@@ -84,5 +120,5 @@ data "huaweicloud_apig_appcodes" "not_found" {
   instance_id    = local.instance_id
   application_id = huaweicloud_apig_application.test[1].id
 }
-`, testAccDataSourceAppcodes_basic_base())
+`, testAccDataAppcodes_base(name))
 }
