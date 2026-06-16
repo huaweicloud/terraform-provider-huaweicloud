41ce1bda0d42e84ff6612ea31b711487336b644f
wuzhuanhong <wuzhuanhong1@outlook.com>
2026-06-16 17:08:41 +0800

    feat(kafka): add new one-time action resource to restore instance from recycle bin (#9933)

---
+++ huaweicloud/services/acceptance/kafka/resource_huaweicloud_dms_kafka_recycle_instance_restore_test.go
@@ -0,0 +1,49 @@
+package kafka
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccResourceRecycleInstanceRestore_basic(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckDMSKafkaRecycleBinInstanceId(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		CheckDestroy:      nil,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccResourceRecycleInstanceRestore_instanceNotFound(),
+				ExpectError: regexp.MustCompile("This DMS instance does not exist"),
+			},
+			{
+				Config: testAccResourceRecycleInstanceRestore_basic(),
+			},
+		},
+	})
+}
+
+func testAccResourceRecycleInstanceRestore_instanceNotFound() string {
+	randomUUID, _ := uuid.GenerateUUID()
+	return fmt.Sprintf(`
+resource "huaweicloud_dms_kafka_recycle_instance_restore" "test" {
+  instance_id = "%s"
+}
+`, randomUUID)
+}
+
+func testAccResourceRecycleInstanceRestore_basic() string {
+	return fmt.Sprintf(`
+resource "huaweicloud_dms_kafka_recycle_instance_restore" "test" {
+  instance_id = "%s"
+}
+`, acceptance.HW_DMS_KAFKA_RECYCLE_BIN_INSTANCE_ID)
+}
