e122eaa99fd6da5a4ceab68f246ef3d7a8d655c9
qigaoneng <qigaoneng@outlook.com>
2026-06-22 15:25:01 +0800

    fix(geminidb): update slow logs data source uuid (#9973)

--- huaweicloud/services/geminidb/data_source_huaweicloud_geminidb_slow_logs.go
+++ huaweicloud/services/geminidb/data_source_huaweicloud_geminidb_slow_logs.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/google/uuid"
 	"github.com/hashicorp/go-multierror"
-	"github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
@@ -124,11 +124,11 @@ func dataSourceGeminiDBSlowLogsRead(_ context.Context, d *schema.ResourceData, m
 
 	slowLogList := utils.PathSearch("slow_log_list", respBody, []interface{}{}).([]interface{})
 
-	dataSourceId, err := uuid.GenerateUUID()
+	randomUUID, err := uuid.NewRandom()
 	if err != nil {
 		return diag.Errorf("unable to generate ID: %s", err)
 	}
-	d.SetId(dataSourceId)
+	d.SetId(randomUUID.String())
 
 	mErr = multierror.Append(
 		d.Set("region", region),
