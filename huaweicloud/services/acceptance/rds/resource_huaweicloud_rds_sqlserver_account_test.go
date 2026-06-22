9b504a9f6410569ecce18c9aeccf2dbb29691d80
greyworld <huawei@ecs-yanghongyu>
2026-06-22 09:51:14 +0800

    fix(rds): fix sql server account acceptance error

--- huaweicloud/services/acceptance/rds/resource_huaweicloud_rds_sqlserver_account_test.go
+++ huaweicloud/services/acceptance/rds/resource_huaweicloud_rds_sqlserver_account_test.go
@@ -119,9 +119,25 @@ func testSQLServerAccount_basic(name, password string) string {
 	return fmt.Sprintf(`
 %[1]s
 
+resource "huaweicloud_networking_secgroup_rule" "ingress" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  ports             = 8634
+  protocol          = "tcp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = data.huaweicloud_networking_secgroup.test.id
+}
+
+data "huaweicloud_rds_flavors" "test" {
+  db_type       = "SQLServer"
+  db_version    = "2022_SE"
+  instance_mode = "single"
+  group_type    = "dedicated"
+}
+
 resource "huaweicloud_rds_instance" "test" {
   name              = "%[2]s"
-  flavor            = "rds.mssql.spec.se.s3.large.2"
+  flavor            = data.huaweicloud_rds_flavors.test.flavors[0].name
   availability_zone = [data.huaweicloud_availability_zones.test.names[0]]
   security_group_id = data.huaweicloud_networking_secgroup.test.id
   subnet_id         = data.huaweicloud_vpc_subnet.test.id
@@ -134,7 +150,7 @@ resource "huaweicloud_rds_instance" "test" {
   }
 
   volume {
-    type = "ULTRAHIGH"
+    type = "CLOUDSSD"
     size = 40
   }
 }
