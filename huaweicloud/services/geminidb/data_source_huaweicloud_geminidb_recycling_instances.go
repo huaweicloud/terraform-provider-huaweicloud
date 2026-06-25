60c5ebb8289018c68e3b040a15aba4f4dd7f2bf8
qigaoneng <qigaoneng@outlook.com>
2026-06-25 09:01:01 +0800

    fix(geminidb): update data source recycle instances url (#10004)

--- huaweicloud/services/geminidb/data_source_huaweicloud_geminidb_recycling_instances.go
+++ huaweicloud/services/geminidb/data_source_huaweicloud_geminidb_recycling_instances.go
@@ -16,7 +16,7 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
-// @API GeminiDB GET /v3/{project_id}/instances/recycle-policy
+// @API GeminiDB GET /v3/{project_id}/instances/recycle-instances
 func DataSourceGeminiDBRecyclingInstances() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: dataSourceGeminiDBRecyclingInstancesRead,
