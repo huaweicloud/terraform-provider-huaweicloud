3fc9d86eb244c7d5ea5b0055508d57929184101d
personSE <714600523@qq.com>
2026-06-29 20:52:43 +0800

    feat(gaussdb): add resource to gaussdb instance role permission

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -3939,6 +3939,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_gaussdb_instance_plugin_license_config":    gaussdb.ResourceGaussDbPluginLicense(),
 			"huaweicloud_gaussdb_instance_upgrade":                  gaussdb.ResourceGaussDbInstanceUpgrade(),
 			"huaweicloud_gaussdb_instance_database_role":            gaussdb.ResourceGaussdbInstanceDatabaseRole(),
+			"huaweicloud_gaussdb_instance_role_permission":          gaussdb.ResourceGaussDBInstanceRolePermission(),
 			"huaweicloud_gaussdb_instance_lts_log_associate":        gaussdb.ResourceGaussdbInstanceLtsLogAssociate(),
 			"huaweicloud_gaussdb_instance_node_startup":             gaussdb.ResourceGaussDbInstanceNodeStartup(),
 			"huaweicloud_gaussdb_instance_node_stop":                gaussdb.ResourceGaussDbInstanceNodeStop(),
