1630bf8610ce61437af5da9a553398463c51d17f
wuzhuanhong <wuzhuanhong1@outlook.com>
2026-06-29 11:00:50 +0800

    fix(modelarts): repair the incorrect API references (#10027)

--- huaweicloud/services/modelarts/resource_huaweicloud_modelartsv2_service.go
+++ huaweicloud/services/modelarts/resource_huaweicloud_modelartsv2_service.go
@@ -34,7 +34,7 @@ var v2ServiceNonUpdatableParams = []string{
 // @API ModelArts POST /v2/{project_id}/services/{service_id}/versions/switch
 // @API ModelArts PUT /v2/{project_id}/services/{service_id}
 // @API ModelArts POST /v2/{project_id}/modelarts-service-v2/{service_id}/tags/create
-// @API ModelArts DELETE /v2/{project_id}/modelarts-service-v2/{service_id}/tags/create
+// @API ModelArts DELETE /v2/{project_id}/modelarts-service-v2/{service_id}/tags/delete
 // @API ModelArts POST /v2/{project_id}/services/delete
 func ResourceV2Service() *schema.Resource {
 	return &schema.Resource{
