1599b3921191090c27d83f66ba029c9de8b05de9
personSE <714600523@qq.com>
2026-07-07 19:28:08 +0800

    feat(gaussdb): add data source to gaussdb instance real time session overview

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1656,6 +1656,7 @@ func Provider() *schema.Provider {
 
 			"huaweicloud_gaussdb_enterprise_project_remaining_quotas": gaussdb.DataSourceGaussDBEnterpriseProjectRemainingQuotas(),
 			"huaweicloud_gaussdb_instance_session_memory_contexts":    gaussdb.DataSourceGaussDBInstanceSessionMemoryContexts(),
+			"huaweicloud_gaussdb_instance_real_time_session_overview": gaussdb.DataSourceGaussDBInstanceRealTimeSessionOverview(),
 
 			"huaweicloud_hss_agent_install_script":                       hss.DataSourceAgentInstallScript(),
 			"huaweicloud_hss_agent_versions":                             hss.DataSourceAgentVersions(),
