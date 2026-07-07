001fa49da99ad194c165185603b56e7be343958e
ruwenqiang123 <ruwenqiang123@outlook.com>
2026-07-07 17:16:49 +0800

    feat(GaussDB): add data source to query full data of a single sql statement (#10103)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1580,6 +1580,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_gaussdb_alarms":                          gaussdb.DataSourceAlarms(),
 			"huaweicloud_gaussdb_backup_configurations":           gaussdb.DataSourceBackupConfigurations(),
 			"huaweicloud_gaussdb_instance_alarm_statistics":       gaussdb.DataSourceInstanceAlarmStatistics(),
+			"huaweicloud_gaussdb_single_full_sqls":                gaussdb.DataSourceSingleFullSqls(),
 			"huaweicloud_gaussdb_sql_explorer_status_records":     gaussdb.DataSourceInstanceSqlExplorerStatusRecords(),
 			"huaweicloud_gaussdb_instance_status_statistics":      gaussdb.DataSourceInstanceStatusStatistics(),
 			"huaweicloud_gaussdb_instance_installed_plugins":      gaussdb.DataSourceInstalledPlugins(),
