90bb580521011966d33c2ad6a090b2a11c3c7113
zhujingbo <95921153+personSE@users.noreply.github.com>
2026-07-07 17:08:50 +0800

    feat(gaussdb): add data source to gaussdb instance real time session overview

---
+++ docs/data-sources/gaussdb_instance_session_memory_contexts.md
@@ -0,0 +1,57 @@
+---
+subcategory: "GaussDB"
+layout: "huaweicloud"
+page_title: "HuaweiCloud: huaweicloud_gaussdb_instance_session_memory_contexts"
+description: |-
+  Use this data source to query the session memory context list of a GaussDB instance within HuaweiCloud.
+---
+
+# huaweicloud_gaussdb_instance_session_memory_contexts
+
+Use this data source to query the session memory context list of a GaussDB instance within HuaweiCloud.
+
+## Example Usage
+
+```hcl
+variable "gaussdb_instance_id" {}
+variable "node_id" {}
+variable "session_id" {}
+
+data "huaweicloud_gaussdb_instance_session_memory_contexts" "test" {
+  instance_id = var.gaussdb_instance_id
+  node_id     = var.node_id
+  session_id  = var.session_id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String) Specifies the region in which to query the session memory contexts.
+  If omitted, the provider-level region will be used.
+
+* `instance_id` - (Required, String) Specifies the ID of the GaussDB instance.
+
+* `node_id` - (Required, String) Specifies the node ID. Only nodes containing CN or DN (primary/standby) components are
+  supported.
+
+* `session_id` - (Required, String) Specifies the session ID.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The data source ID.
+
+* `memory_context_info` - The list of session memory context information.
+  The [memory_context_info](#memory_context_info) structure is documented below.
+
+<a name="memory_context_info"></a>
+The `memory_context_info` block supports:
+
+* `context_name` - The memory context name.
+
+* `amount` - The number of session contexts.
+
+* `size` - The total size of the session memory context.
---
+++ huaweicloud/services/gaussdb/data_source_huaweicloud_gaussdb_instance_session_memory_contexts.go
@@ -0,0 +1,154 @@
+package gaussdb
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/chnsz/golangsdk"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+// @API GaussDB GET /v3/{project_id}/instances/{instance_id}/session/memory-context
+func DataSourceGaussDBInstanceSessionMemoryContexts() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceGaussDBInstanceSessionMemoryContextsRead,
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"instance_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"node_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"session_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"memory_context_info": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     sessionMemoryContextInfoSchema(),
+			},
+		},
+	}
+}
+
+func sessionMemoryContextInfoSchema() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"context_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"amount": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"size": {
+				Type:     schema.TypeFloat,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceGaussDBInstanceSessionMemoryContextsRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+
+	var mErr *multierror.Error
+
+	client, err := cfg.NewServiceClient("opengauss", region)
+	if err != nil {
+		return diag.Errorf("error creating GaussDB client: %s", err)
+	}
+
+	httpUrl := "v3/{project_id}/instances/{instance_id}/session/memory-context"
+	basePath := client.Endpoint + httpUrl
+	basePath = strings.ReplaceAll(basePath, "{project_id}", client.ProjectID)
+	basePath = strings.ReplaceAll(basePath, "{instance_id}", d.Get("instance_id").(string))
+	basePath += buildGetSessionMemoryContextsQueryParams(d)
+
+	var allMemoryContexts []interface{}
+	offset := 0
+	limit := 100
+
+	for {
+		requestPath := fmt.Sprintf("%s&offset=%d&limit=%d", basePath, offset, limit)
+
+		opts := golangsdk.RequestOpts{
+			KeepResponseBody: true,
+			MoreHeaders: map[string]string{
+				"Content-Type": "application/json",
+				"X-Language":   "en-us",
+			},
+		}
+
+		requestResp, err := client.Request("GET", requestPath, &opts)
+		if err != nil {
+			return diag.Errorf("error retrieving GaussDB session memory contexts: %s", err)
+		}
+
+		respBody, err := utils.FlattenResponse(requestResp)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		curJson := utils.PathSearch("memory_context_info", respBody, make([]interface{}, 0))
+		curArray := curJson.([]interface{})
+		if len(curArray) == 0 {
+			break
+		}
+		allMemoryContexts = append(allMemoryContexts, curArray...)
+		offset += limit
+	}
+
+	dataSourceId, err := uuid.NewRandom()
+	if err != nil {
+		return diag.Errorf("unable to generate ID: %s", err)
+	}
+	d.SetId(dataSourceId.String())
+
+	mErr = multierror.Append(
+		d.Set("region", region),
+		d.Set("memory_context_info", flattenGetSessionMemoryContextInfoBody(allMemoryContexts)),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func buildGetSessionMemoryContextsQueryParams(d *schema.ResourceData) string {
+	res := fmt.Sprintf("?node_id=%s&session_id=%s", d.Get("node_id").(string), d.Get("session_id").(string))
+	return res
+}
+
+func flattenGetSessionMemoryContextInfoBody(resp interface{}) []interface{} {
+	curArray, ok := resp.([]interface{})
+	if !ok {
+		return nil
+	}
+	res := make([]interface{}, 0, len(curArray))
+	for _, v := range curArray {
+		res = append(res, map[string]interface{}{
+			"context_name": utils.PathSearch("context_name", v, nil),
+			"amount":       utils.PathSearch("amount", v, nil),
+			"size":         utils.PathSearch("size", v, nil),
+		})
+	}
+	return res
+}
