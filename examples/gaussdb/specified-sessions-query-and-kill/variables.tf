3ee151d408daeec38d63694467c982238af7e3d0
personSE <714600523@qq.com>
2026-07-24 14:27:05 +0800

    feat(examples/gaussdb): add new best practice to gaussdb specified sessions query and kill

---
+++ examples/gaussdb/specified-sessions-query-and-kill/variables.tf
@@ -0,0 +1,30 @@
+# Variable definitions for authentication
+variable "region_name" {
+  description = "The region where the GaussDB instance is located"
+  type        = string
+}
+
+variable "access_key" {
+  description = "The access key of the IAM user"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "The secret key of the IAM user"
+  type        = string
+  sensitive   = true
+}
+
+# Variable definitions for resources
+variable "instance_id" {
+  description = "The ID of the GaussDB instance"
+  type        = string
+  default     = ""
+}
+
+variable "session_ids" {
+  description = "The list of session IDs to be killed"
+  type        = list(string)
+  default     = []
+}
