5c5bc4763e4de6ca091b288636d5ca5e02a36341
qigaoneng <qigaoneng@outlook.com>
2026-08-11 19:59:31 +0800

    feat(geminidb): add a best practice for creating geminidb parameter template reset

---
+++ examples/geminidb/geminidb-parameter-template-reset/main.tf
@@ -0,0 +1,24 @@
+# Create a GeminiDB parameter template with custom parameter values
+resource "huaweicloud_geminidb_parameter_template" "test" {
+  name        = var.template_name
+  description = var.template_description
+
+  datastore {
+    type    = var.datastore_type
+    version = var.datastore_version
+    mode    = var.datastore_mode
+  }
+
+  values = var.parameter_values
+
+  lifecycle {
+    ignore_changes = [
+      datastore,
+    ]
+  }
+}
+
+# Reset the GeminiDB parameter template to its default values
+resource "huaweicloud_geminidb_parameter_template_reset" "test" {
+  config_id = huaweicloud_geminidb_parameter_template.test.id
+}
