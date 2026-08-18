4864f90dae072c03827d09ddce9b73b522a00074
zhujingbo <95921153+personSE@users.noreply.github.com>
2026-08-18 17:54:56 +0800

    feat(examples/gaussdb): add new best practice to dcs hotkey analysis (#10417)

---
+++ examples/dcs/redis-hotkey-analysis/main.tf
@@ -0,0 +1,62 @@
+resource "huaweicloud_vpc" "test" {
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+}
+
+resource "huaweicloud_vpc_subnet" "test" {
+  vpc_id     = huaweicloud_vpc.test.id
+  name       = var.subnet_name
+  cidr       = var.subnet_cidr != "" ? var.subnet_cidr : cidrsubnet(huaweicloud_vpc.test.cidr, 8, 0)
+  gateway_ip = (var.subnet_gateway_ip != "" ? var.subnet_gateway_ip :
+  cidrhost(cidrsubnet(huaweicloud_vpc.test.cidr, 8, 0), 1))
+}
+
+data "huaweicloud_availability_zones" "test" {
+  count = var.availability_zone == "" ? 1 : 0
+}
+
+data "huaweicloud_dcs_flavors" "test" {
+  count = var.instance_flavor_id == "" ? 1 : 0
+
+  cache_mode     = "ha"
+  capacity       = var.instance_capacity
+  engine_version = var.instance_engine_version
+}
+
+# Generate random password if not provided
+resource "random_password" "test" {
+  count = var.instance_password == "" ? 1 : 0
+
+  length           = 12
+  special          = true
+  override_special = "!@%^*-_=+"
+  min_upper        = 1
+  min_lower        = 1
+  min_numeric      = 1
+  min_special      = 1
+}
+
+resource "huaweicloud_dcs_instance" "test" {
+  name               = var.instance_name
+  engine             = "Redis"
+  engine_version     = var.instance_engine_version
+  capacity           = var.instance_capacity
+  vpc_id             = huaweicloud_vpc.test.id
+  subnet_id          = huaweicloud_vpc_subnet.test.id
+  password           = (var.instance_password != "" ? var.instance_password :
+  try(random_password.test[0].result, null))
+  flavor             = (var.instance_flavor_id != "" ? var.instance_flavor_id :
+  try(data.huaweicloud_dcs_flavors.test[0].flavors[0].name, null))
+  availability_zones = (var.availability_zone != "" ? [var.availability_zone] :
+  try(slice(data.huaweicloud_availability_zones.test[0].names, 0, 1), null))
+
+  parameters {
+    id    = "2"
+    name  = "maxmemory-policy"
+    value = "volatile-lfu"
+  }
+}
+
+resource "huaweicloud_dcs_hotkey_analysis" "test" {
+  instance_id = huaweicloud_dcs_instance.test.id
+}
