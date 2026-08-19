1ba0fc59546a610fd75bead7711697569b89e1a2
jinyangyang225 <jinyangyang223@outlook.com>
2026-08-19 10:37:48 +0800

    feat(dws): add a best practice for DWS snapshot

---
+++ examples/dws/snapshot/variables.tf
@@ -0,0 +1,155 @@
+# Variable definitions for authentication
+variable "region_name" {
+  description = "The region where the DWS resources are located"
+  type        = string
+}
+
+variable "access_key" {
+  description = "The access key of the IAM user"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "The secret key of the IAM user"
+  type        = string
+  sensitive   = true
+}
+
+# Variable definitions for resources/data sources
+variable "availability_zone" {
+  description = "The availability zone of the DWS cluster"
+  type        = string
+  default     = ""
+  nullable    = false
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block of the VPC"
+  type        = string
+}
+
+variable "enterprise_project_id" {
+  description = "The ID of the enterprise project"
+  type        = string
+  default     = ""
+  nullable    = false
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "The CIDR block of the subnet"
+  type        = string
+  default     = ""
+  nullable    = false
+}
+
+variable "subnet_gateway_ip" {
+  description = "The gateway IP of the subnet"
+  type        = string
+  default     = ""
+  nullable    = false
+}
+
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+}
+
+variable "security_group_delete_default_rules" {
+  description = "Whether to delete the default rules of the security group"
+  type        = bool
+  default     = true
+}
+
+variable "cluster_node_type" {
+  description = "The flavor of the DWS cluster node"
+  type        = string
+  default     = ""
+  nullable    = false
+}
+
+variable "cluster_version" {
+  description = "The version of the DWS cluster"
+  type        = string
+  default     = ""
+  nullable    = false
+}
+
+variable "cluster_vcpus" {
+  description = "The vcpus of the DWS cluster"
+  type        = number
+  default     = 4
+}
+
+variable "cluster_memory" {
+  description = "The memory of the DWS cluster"
+  type        = number
+  default     = 32
+}
+
+variable "cluster_datastore_type" {
+  description = "The datastore type of the DWS cluster"
+  type        = string
+  default     = "dws"
+}
+
+variable "cluster_name" {
+  description = "The name of the DWS cluster"
+  type        = string
+}
+
+variable "cluster_number_of_node" {
+  description = "The number of nodes in the DWS cluster"
+  type        = number
+  default     = 3
+}
+
+variable "cluster_number_of_cn" {
+  description = "The number of CN nodes in the DWS cluster"
+  type        = number
+  default     = 3
+}
+
+variable "cluster_admin_user_name" {
+  description = "The administrator username of the DWS cluster"
+  type        = string
+}
+
+variable "cluster_admin_user_pwd" {
+  description = "The administrator password of the DWS cluster"
+  type        = string
+  sensitive   = true
+}
+
+variable "cluster_volume_type" {
+  description = "The volume type of the DWS cluster"
+  type        = string
+  default     = "SSD"
+}
+
+variable "cluster_volume_capacity" {
+  description = "The volume capacity of the DWS cluster in GB"
+  type        = string
+  default     = "100"
+}
+
+variable "snapshot_name" {
+  description = "The name of the snapshot"
+  type        = string
+}
+
+variable "snapshot_description" {
+  description = "The description of the snapshot"
+  type        = string
+  default     = ""
+}
