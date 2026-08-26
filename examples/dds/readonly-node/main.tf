8c8744b5e0f659af8e547f5a37727a1e6036636d
ruwenqiang123 <ruwenqiang123@outlook.com>
2026-08-26 17:11:19 +0800

    feat(examples/dds): add a best practice for DDS instance readonly node (#10445)

---
+++ examples/dds/readonly-node/main.tf
@@ -0,0 +1,48 @@
+data "huaweicloud_availability_zones" "test" {
+  count = var.availability_zone == "" ? 1 : 0
+}
+
+resource "huaweicloud_vpc" "test" {
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+}
+
+resource "huaweicloud_vpc_subnet" "test" {
+  vpc_id     = huaweicloud_vpc.test.id
+  name       = var.subnet_name
+  cidr       = var.subnet_cidr == "" ? cidrsubnet(huaweicloud_vpc.test.cidr, 8, 0) : var.subnet_cidr
+  gateway_ip = var.subnet_gateway_ip == "" ? cidrhost(cidrsubnet(huaweicloud_vpc.test.cidr, 8, 0), 1) : var.subnet_gateway_ip
+}
+
+resource "huaweicloud_networking_secgroup" "test" {
+  name                 = var.security_group_name
+  delete_default_rules = true
+}
+
+resource "huaweicloud_dds_instance" "test" {
+  name              = var.instance_name
+  availability_zone = var.availability_zone == "" ? try(data.huaweicloud_availability_zones.test[0].names[0], null) : var.availability_zone
+  vpc_id            = huaweicloud_vpc.test.id
+  subnet_id         = huaweicloud_vpc_subnet.test.id
+  security_group_id = huaweicloud_networking_secgroup.test.id
+  mode              = "ReplicaSet"
+
+  datastore {
+    type           = var.database_type
+    version        = var.database_version
+    storage_engine = var.storage_engine
+  }
+
+  flavor {
+    type      = var.node_type
+    num       = var.node_number
+    spec_code = var.node_spec_code
+    storage   = var.node_storage_type
+    size      = var.node_size
+  }
+}
+
+resource "huaweicloud_dds_readonly_node" "test" {
+  instance_id = huaweicloud_dds_instance.test.id
+  spec_code   = var.node_spec
+}
---
+++ examples/dds/readonly-node/terraform.tfvars
@@ -0,0 +1,4 @@
+vpc_name            = "tf_test_node"
+subnet_name         = "tf_test_node"
+security_group_name = "tf_test_node"
+instance_name       = "tf_test_node"
